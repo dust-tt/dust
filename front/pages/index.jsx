36fe38f6b43d74661bd4e3b845e8bcdc6cc35d97
Stanislas Polu <spolu@proton.me>
2022-09-28 18:41:30 +0200

    landing tweaks

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -13,8 +13,9 @@ export default function Home() {
         <title>Dust - LLM Apps Platform</title>
       </Head>
 
-      <main className="mx-auto mt-16 max-w-5xl px-4 sm:mt-24">
-        <div className="text-center">
+      <main className="mx-auto mt-16 max-w-5xl px-4 mt-12">
+
+        <div className="mb-12 flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div className="flex flex-shrink-0 items-center px-2 py-1 block scale-150">
             <div className="flex rotate-[30deg]">
               <div className="bg-gray-400 w-[4px] h-2 rounded-md"></div>
@@ -26,14 +27,17 @@ export default function Home() {
               DUST
             </div>
           </div>
+        </div>
 
+        <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline">
               Specification language and design platform for
             </span>{" "}
             <span className="block text-indigo-600 xl:inline">LLM Apps</span>
           </h1>
-          <div className="mx-auto mt-5 max-w-md sm:justify-center md:mt-8">
+
+          <div className="mx-auto mt-5 max-w-md sm:justify-center mt-12">
             <button
               className="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
               onClick={() => signIn("github", { callbackUrl: '/api/login' })}
