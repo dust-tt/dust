24207fffb7e22840c61d2e7bf48c83e8b8b35831
Stanislas Polu <spolu@proton.me>
2022-10-17 17:17:57 +0200

    hotfix

--- front/pages/[user]/a/[sId]/datasets/[name]/index.js
+++ front/pages/[user]/a/[sId]/datasets/[name]/index.js
@@ -27,7 +27,6 @@ export default function ViewDatasetView({
   const onUpdate = (valid, dataset) => {
     setDisabled(!valid);
     if (valid) {
-      console.log("STATE SAVED", spec, config);
       setUpdatedDataset(dataset);
     }
   };
