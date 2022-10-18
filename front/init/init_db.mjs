55075f454ae5fbf9e0d50070a281b511d66047df
Stanislas Polu <spolu@proton.me>
2022-10-18 17:03:14 +0200

    fix init

---
+++ front/init/init_db.mjs
@@ -0,0 +1,10 @@
+import { User, App, Dataset, Provider } from "./lib/models.js";
+
+async function main() {
+  await User.sync({ alter: true });
+  await App.sync({ alter: true });
+  await Dataset.sync({ alter: true });
+  await Provider.sync({ alter: true });
+}
+
+await main();
