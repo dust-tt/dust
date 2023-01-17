4e111532c042355e39934b0801554d116c93c507
Stanislas Polu <spolu@proton.me>
2023-01-17 08:16:04 +0100

    specification route

---
+++ front/pages/[user]/a/[sId]/specification.jsx
@@ -0,0 +1,80 @@
+import AppLayout from "../../../../components/app/AppLayout";
+import MainTab from "../../../../components/app/MainTab";
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../../../api/auth/[...nextauth]";
+
+const { URL, DUST_API, GA_TRACKING_ID = null } = process.env;
+
+export default function Specification({
+  user,
+  readOnly,
+  app,
+  specification,
+  ga_tracking_id,
+}) {
+  return (
+    <AppLayout
+      app={{ sId: app.sId, name: app.name, description: app.description }}
+      ga_tracking_id={ga_tracking_id}
+    >
+      <div className="flex flex-col">
+        <div className="flex flex-initial mt-2">
+          <MainTab
+            app={{ sId: app.sId, name: app.name }}
+            current_tab="Specification"
+            user={user}
+            readOnly={readOnly}
+          />
+        </div>
+        <div className="flex flex-auto">
+          <div className="flex flex-auto flex-col mx-2 sm:mx-4 lg:mx-8 my-8">
+            <div className="whitespace-pre font-mono text-sm text-gray-700">
+              {specification}
+            </div>
+          </div>
+        </div>
+      </div>
+    </AppLayout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+
+  let readOnly = !session || context.query.user !== session.user.username;
+
+  const [specRes] = await Promise.all([
+    fetch(
+      `${URL}/api/apps/${context.query.user}/${context.query.sId}/specification`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: context.req.headers.cookie,
+        },
+      }
+    ),
+  ]);
+
+  if (specRes.status === 404) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const [data] = await Promise.all([specRes.json()]);
+
+  return {
+    props: {
+      readOnly,
+      user: context.query.user,
+      app: data.app,
+      specification: data.specification,
+      ga_tracking_id: GA_TRACKING_ID,
+    },
+  };
+}
---
+++ front/pages/api/apps/[user]/[sId]/specification.js
@@ -0,0 +1,75 @@
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../../../auth/[...nextauth]";
+import { User, App } from "../../../../../lib/models";
+import { dumpSpecification } from "../../../../../lib/specification";
+
+const { DUST_API } = process.env;
+
+export default async function handler(req, res) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  let user = await User.findOne({
+    where: {
+      username: req.query.user,
+    },
+  });
+
+  if (!user) {
+    res.status(404).end();
+    return;
+  }
+
+  const readOnly = !(session && session.github.id.toString() === user.githubId);
+
+  let app = await App.findOne({
+    where: readOnly
+      ? {
+          userId: user.id,
+          sId: req.query.sId,
+          visibility: "public",
+        }
+      : {
+          userId: user.id,
+          sId: req.query.sId,
+        },
+  });
+
+  if (!app) {
+    res.status(404).end();
+    return;
+  }
+
+  switch (req.method) {
+    case "GET":
+      console.log(app);
+
+      const datasetsRes = await fetch(
+        `${DUST_API}/projects/${app.dustAPIProjectId}/datasets`,
+        {
+          method: "GET",
+        }
+      );
+      const datasets = await datasetsRes.json();
+      if (datasets.error) {
+        res.status(500).end();
+        return;
+      }
+
+      let latestDatasets = {};
+      for (const d in datasets.response.datasets) {
+        latestDatasets[d] = datasets.response.datasets[d][0].hash;
+      }
+
+      let spec = dumpSpecification(
+        JSON.parse(app.savedSpecification),
+        latestDatasets
+      );
+
+      res.status(200).json({ app, specification: spec });
+      break;
+
+    default:
+      res.status(405).end();
+      break;
+  }
+}
