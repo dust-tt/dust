40bf6311e96c0ca4f83f48230b1db0bc9ad9a8e2
Henry Fontanier <h.fontanier@gmail.com>
2023-03-30 10:47:30 +0200

    migrate keys endpoints to TS

--- front/pages/api/keys/index.ts
+++ front/pages/api/keys/index.ts
@@ -1,9 +1,28 @@
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "@app/pages/api/auth/[...nextauth]";
-import { User, Key } from "@app/lib/models";
+import { Key, User } from "@app/lib/models";
 import { new_id } from "@app/lib/utils";
+import { authOptions } from "@app/pages/api/auth/[...nextauth]";
+import { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth/next";
 
-export default async function handler(req, res) {
+type KeyObject = {
+  id: number;
+  secret: string;
+  status: string;
+  userId: number;
+};
+
+export type GetKeysResponseBody = {
+  keys: KeyObject[];
+};
+
+export type PostKeysResponseBody = {
+  key: KeyObject;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<GetKeysResponseBody | PostKeysResponseBody>
+) {
   const session = await unstable_getServerSession(req, res, authOptions);
   if (!session) {
     res.status(401).end();
