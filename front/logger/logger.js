e2952a8698180763d6ef96f972191afbc6611190
Corentin Dancette <corentin.dancette@gmail.com>
2023-04-04 12:40:04 +0200

    clean pino logging.js

--- front/logger/logger.js
+++ front/logger/logger.js
@@ -1,31 +1,6 @@
 import pino from 'pino'
 
-// // create pino-logflare console stream for serverless functions and send function for browser logs
-// // Browser logs are going to: https://logflare.app/sources/13989
-// // Vercel log drain was setup to send logs here: https://logflare.app/sources/13830
-
-// // const { stream, send } = logflarePinoVercel({
-// //     apiKey: "eA_3wro12LpZ",
-// //     sourceToken: "eb1d841a-e0e4-4d23-af61-84465c808157"
-// // });
-
-// // create pino logger {},  pino.destination("./pino-logger.log")
-
 const logger = pino();
-
-// const { createLogger, format, transports } = require('winston');
-
-// const logger = createLogger({
-//   level: 'info',
-//   exitOnError: false,
-//   format: format.json(),
-//   transports: [
-//     new transports.File({ filename: `${appRoot}/logs/log.log` }),
-//   ],
-// });
-
-// module.exports = logger;
-
 
 export default logger;
 
