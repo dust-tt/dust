ade447efe21c82fa7c98b75772c8f7b7b595f399
Henry Fontanier <h.fontanier@gmail.com>
2023-04-20 12:20:49 +0200

    enh: ts/js/ci setup for connectors

--- connectors/.eslintrc.js
+++ connectors/.eslintrc.js
@@ -1,46 +1,48 @@
-const { builtinModules } = require('module');
+import { builtinModules } from "module";
 
-const ALLOWED_NODE_BUILTINS = new Set(['assert']);
+const ALLOWED_NODE_BUILTINS = new Set(["assert"]);
 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'deprecation'],
+  plugins: ["@typescript-eslint", "deprecation"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   rules: {
     // recommended for safety
-    '@typescript-eslint/no-floating-promises': 'error', // forgetting to await Activities and Workflow APIs is bad
-    'deprecation/deprecation': 'warn',
+    "@typescript-eslint/no-floating-promises": "error", // forgetting to await Activities and Workflow APIs is bad
+    "deprecation/deprecation": "warn",
 
     // code style preference
-    'object-shorthand': ['error', 'always'],
+    "object-shorthand": ["error", "always"],
 
     // relaxed rules, for convenience
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
       {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
       },
     ],
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-explicit-any": "off",
   },
   overrides: [
     {
-      files: ['src/workflows.ts', 'src/workflows-*.ts', 'src/workflows/*.ts'],
+      files: ["src/workflows.ts", "src/workflows-*.ts", "src/workflows/*.ts"],
       rules: {
-        'no-restricted-imports': [
-          'error',
-          ...builtinModules.filter((m) => !ALLOWED_NODE_BUILTINS.has(m)).flatMap((m) => [m, `node:${m}`]),
+        "no-restricted-imports": [
+          "error",
+          ...builtinModules
+            .filter((m) => !ALLOWED_NODE_BUILTINS.has(m))
+            .flatMap((m) => [m, `node:${m}`]),
         ],
       },
     },
