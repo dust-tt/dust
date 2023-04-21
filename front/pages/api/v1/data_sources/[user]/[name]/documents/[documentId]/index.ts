d7eead3c4a2f1a84f5f8b9a33077ae065048b306
Stanislas Polu <spolu@proton.me>
2023-04-21 12:15:23 +0200

    lint

--- front/pages/api/v1/data_sources/[user]/[name]/documents/[documentId]/index.ts
+++ front/pages/api/v1/data_sources/[user]/[name]/documents/[documentId]/index.ts
@@ -2,11 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { APIError } from "@app/lib/error";
 import logger from "@app/logger/logger";
-import {
-  apiError,
-  statsDClient,
-  withLogging,
-} from "@app/logger/withlogging";
+import { apiError, statsDClient, withLogging } from "@app/logger/withlogging";
 import { legacyUserToWorkspace } from "@app/pages/api/v1/legacy_user_to_workspace";
 import wIdHandler from "@app/pages/api/v1/w/[wId]/data_sources/[name]/documents/[documentId]/index";
 import { DataSourceType } from "@app/types/data_source";
