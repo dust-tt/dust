a0d08c832fe9ce4f99c3d39aac2aa583d91e17d0
Aric Lasry <lasry.aric@gmail.com>
2023-04-23 12:47:22 +0200

    Clean up after @spolu's review

--- connectors/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-
-import "@connectors/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
--- front/cicheck.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-npm run format:check
-npm run lint
-echo "All good! 🔥"
