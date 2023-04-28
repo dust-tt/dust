5348c879bafcbe91581a025f52eeb681d1f9738f
Henry Fontanier <h.fontanier@gmail.com>
2023-04-28 16:13:11 +0200

    feat: implement notion restart-all command

--- connectors/src/admin/cli.ts
+++ connectors/src/admin/cli.ts
@@ -1,4 +1,5 @@
 import parseArgs from "minimist";
+import PQueue from "p-queue";
 
 import {
   RESUME_CONNECTOR_BY_TYPE,
@@ -74,6 +75,66 @@ const connectors = async (command: string, args: parseArgs.ParsedArgs) => {
   }
 };
 
+const notion = async (command: string) => {
+  switch (command) {
+    case "restart-all": {
+      const queue = new PQueue({ concurrency: 10 });
+      const promises: Promise<void>[] = [];
+      const connectors = await Connector.findAll({
+        where: {
+          type: "notion",
+        },
+      });
+      for (const connector of connectors) {
+        promises.push(
+          queue.add(async () => {
+            await STOP_CONNECTOR_BY_TYPE[connector.type]({
+              workspaceId: connector.workspaceId,
+              dataSourceName: connector.dataSourceName,
+            });
+            await RESUME_CONNECTOR_BY_TYPE[connector.type](
+              {
+                workspaceId: connector.workspaceId,
+                dataSourceName: connector.dataSourceName,
+                workspaceAPIKey: connector.workspaceAPIKey,
+              },
+              connector.nangoConnectionId
+            );
+          })
+        );
+      }
+      let success = 0;
+      let failed = 0;
+
+      const logInfo = () => {
+        const completed = success + failed;
+
+        if (
+          (completed && completed % 10 === 0) ||
+          completed === connectors.length
+        ) {
+          console.log(
+            `completed ${completed} / ${connectors.length} (${failed} failed)`
+          );
+        }
+      };
+
+      queue.on("completed", () => {
+        success++;
+        logInfo();
+      });
+      queue.on("error", () => {
+        failed++;
+        logInfo();
+      });
+
+      await Promise.all(promises);
+
+      return;
+    }
+  }
+};
+
 const main = async () => {
   const argv = parseArgs(process.argv.slice(2));
 
@@ -94,6 +155,9 @@ const main = async () => {
   switch (objectType) {
     case "connectors":
       await connectors(command, argv);
+      return;
+    case "notion":
+      await notion(command);
       return;
     default:
       throw new Error(`Unknown object type: ${objectType}`);
