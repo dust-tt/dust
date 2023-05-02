7c944782e3aac056a401e8b129a1bb469de4c1a5
Stanislas Polu <spolu@proton.me>
2023-05-02 14:31:52 +0200

    start scripts

--- connectors/admin/prod.sh
+++ connectors/admin/prod.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-npx next build
-
 # See https://docs.datadoghq.com/tracing/trace_collection/library_config/nodejs/
 
 export DD_ENV=prod
-export DD_SERVICE=front
+export DD_SERVICE=connectors
 export NODE_OPTIONS='-r dd-trace/init'
 export DD_LOGS_INJECTION=true
 export DD_RUNTIME_METRICS_ENABLED=true
+export NODE_ENV=production
 
-npx next start 2>&1 | tee /var/log/datadog/dust_front.log
+env $(cat .env.local) npx tsx ./src/start.ts -p 3002 2>&1 | tee /var/log/datadog/connectors_front.log
