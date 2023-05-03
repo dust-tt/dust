bc25780737e43adef8fee6b1edd2afdef233466e
Aric Lasry <lasry.aric@gmail.com>
2023-05-03 15:55:46 +0200

    Monitoring of Temporal activities

--- connectors/src/monitoring/index.ts
+++ connectors/src/monitoring/index.ts
@@ -4,42 +4,36 @@ import StatsDClient from "hot-shots";
 import mainlogger from "@connectors/logger/logger";
 const logger = mainlogger.child({ module: "temporal-monitoring" });
 
-type asyncF<T> = (...args: any[]) => Promise<T>;
+type asyncF<T> = (...args: unknown[]) => Promise<T>;
 
 export const statsDClient = new StatsDClient({});
 
 export function withMonitoring<T>(fn: asyncF<T>): asyncF<T> {
-  return async (...args: any[]): Promise<T> => {
-    const startTime = new Date();
+  return async (...args: unknown[]): Promise<T> => {
     let result: T | undefined = undefined;
 
     try {
+      const startTime = new Date();
       logger.info({ activity: fn.name }, `Activity started`);
-      const endTime = new Date();
-      const elapsedMs = endTime.getTime() - startTime.getTime();
+
       const tags = [`activity:${fn.name}`];
       statsDClient.increment("activities.count", 1, tags);
 
       result = await fn(...args);
+      const endTime = new Date();
+      const elapsedMs = endTime.getTime() - startTime.getTime();
       statsDClient.histogram(`activity.duration`, elapsedMs, tags);
       logger.info(
         { activity: fn.name, elapsedMs: elapsedMs },
-        `activity ended`
+        `Activity ended successfully`
       );
       return result;
     } catch (e) {
       let tags = [`activity:${fn.name}`];
       if (e instanceof MonitoringError) {
-        tags = tags.concat([`known_error:true`, `error_type:${typeof e}`]);
-
-        // got known error
+        tags = tags.concat([`known_error:true`]);
       } else {
-        tags = tags.concat([
-          `activity:${fn.name}`,
-          `known_error:false`,
-          `error_type:${typeof e}`,
-        ]);
-        // got unknown error
+        tags = tags.concat([`activity:${fn.name}`, `known_error:false`]);
       }
       statsDClient.increment("activities_errors.count", 1, tags);
       logger.error({ activity: fn.name }, `activity errored`);
