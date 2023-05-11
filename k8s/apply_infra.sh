7e9451002402e53672af0829d2903ca566a3be41
Henry Fontanier <h.fontanier@gmail.com>
2023-05-11 17:29:08 +0200

    add ingress + service

--- k8s/apply_infra.sh
+++ k8s/apply_infra.sh
@@ -13,3 +13,17 @@ echo "Applying deployments"
 echo "-----------------------------------"
 
 kubectl apply -f "$(dirname "$0")/deployments/front-deployment.yaml"
+
+
+echo "-----------------------------------"
+echo "Applying services"
+echo "-----------------------------------"
+
+kubectl apply -f "$(dirname "$0")/services/front-service.yaml"
+
+
+echo "-----------------------------------"
+echo "Applying ingress"
+echo "-----------------------------------"
+
+kubectl apply -f "$(dirname "$0")/ingress/front-ingress.yaml"
