62ebdf9f98a9f58f5c8a8a503c5e25b68ba2078f
Stanislas Polu <spolu@proton.me>
2023-05-22 17:24:55 +0200

    20220522 incident remediation post slack doc rename

---
+++ front/migrations/20230522_slack_doc_rename_incident.ts
@@ -0,0 +1,106 @@
+import { dustManagedCredentials } from "@app/lib/api/credentials";
+import { CoreAPI } from "@app/lib/core_api";
+import { DataSource } from "@app/lib/models";
+
+async function main() {
+  const dataSources = await DataSource.findAll({
+    where: {
+      connectorProvider: "slack",
+    },
+  });
+
+  await Promise.all(
+    dataSources.map((ds) => {
+      return (async () => {
+        const connectorId = ds.connectorId;
+        const provider = ds.connectorProvider;
+        const workspaceId = ds.workspaceId;
+
+        if (!workspaceId) {
+          throw new Error("No workspaceId found");
+        }
+        if (!connectorId || !provider || provider !== "slack") {
+          throw new Error("No connectorId found");
+        }
+
+        const dds = await CoreAPI.deleteDataSource(
+          ds.dustAPIProjectId,
+          ds.name
+        );
+
+        if (dds.isErr()) {
+          throw new Error("Failed to delete the Data Source from Core");
+        }
+
+        console.log(`Deleting ${ds.name}`);
+
+        await ds.destroy();
+
+        const dataSourceName = `managed-${provider}`;
+        const dataSourceDescription = `Managed Data Source for ${provider}`;
+        const dataSourceProviderId = "openai";
+        const dataSourceModelId = "text-embedding-ada-002";
+        const dataSourceMaxChunkSize = 256;
+
+        const dustProject = await CoreAPI.createProject();
+        if (dustProject.isErr()) {
+          throw new Error("Failed to create new Core project.");
+        }
+
+        // Dust managed credentials: managed data source.
+        const credentials = dustManagedCredentials();
+
+        const dustDataSource = await CoreAPI.createDataSource(
+          dustProject.value.project.project_id.toString(),
+          {
+            dataSourceId: dataSourceName,
+            config: {
+              provider_id: dataSourceProviderId,
+              model_id: dataSourceModelId,
+              splitter_id: "base_v0",
+              max_chunk_size: dataSourceMaxChunkSize,
+              use_cache: false,
+            },
+            credentials,
+          }
+        );
+
+        if (dustDataSource.isErr()) {
+          throw new Error("Failed to create Core DataSource");
+        }
+
+        let dataSource = await DataSource.create({
+          name: dataSourceName,
+          description: dataSourceDescription,
+          visibility: "private",
+          config: JSON.stringify(dustDataSource.value.data_source.config),
+          dustAPIProjectId: dustProject.value.project.project_id.toString(),
+          workspaceId: workspaceId,
+        });
+
+        console.log(
+          `Created new Data Source: wId=${workspaceId} dsId=${dataSource.id} dsName=${dataSource.name}`
+        );
+
+        dataSource = await dataSource.update({
+          connectorId: connectorId,
+          connectorProvider: provider,
+        });
+
+        console.log(
+          `Connected to connectorId=${connectorId} connectorProvider=${provider}`
+        );
+      })();
+    })
+  );
+}
+
+main()
+  .then(() => {
+    console.log("Done");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
