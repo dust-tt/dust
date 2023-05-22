a7e1ffcace34eb5bff52dedfe167fb3252e48d17
Henry Fontanier <h.fontanier@gmail.com>
2023-05-22 14:40:25 +0200

    use GCE load balancer, use HTTPs, managed CERT and infra script

--- k8s/apply_infra.sh
+++ k8s/apply_infra.sh
@@ -2,6 +2,31 @@
 
 set -e
 
+if [ "$1" == "--install-helm-charts" ]; then
+    if helm list -n default | grep -q dust-datadog-agent; then
+        echo "datadog-agent already installed, skipping."
+    else
+        if [ -z ${DD_API_KEY+x} ]; then
+            echo "DD_API_KEY is unset"
+            exit 1
+        fi
+
+        if [ -z ${DD_APP_KEY+x} ]; then
+            echo "DD_APP_KEY is unset"
+            exit 1
+        fi
+        echo "-----------------------------------"
+        echo "Installing datadog-agent"
+        echo "-----------------------------------"
+        helm repo add datadog https://helm.datadoghq.com
+        helm repo update
+        helm install dust-datadog-agent datadog/datadog -f "$(dirname "$0")/datadog-values.yml" \
+            --set datadog.apiKey=$DD_API_KEY \
+            --set datadog.appKey=$DD_APP_KEY 
+    fi
+fi
+
+
 echo "-----------------------------------"
 echo "Applying configmaps"
 echo "-----------------------------------"
@@ -26,4 +51,10 @@ echo "-----------------------------------"
 echo "Applying ingress"
 echo "-----------------------------------"
 
-kubectl apply -f "$(dirname "$0")/ingress/front-ingress.yaml"
+kubectl apply -f "$(dirname "$0")/ingress.yaml"
+
+echo "-----------------------------------"
+echo "Applying backend configs"
+echo "-----------------------------------"
+
+kubectl apply -f "$(dirname "$0")/backend-configs/front-backend-config.yaml"
