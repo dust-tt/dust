48a7c9c440c21f15bb99e572e89da8831f1884ce
Henry Fontanier <h.fontanier@gmail.com>
2023-05-23 15:36:08 +0200

    debug cloudbuild script

--- front/admin/cloud-build.sh
+++ front/admin/cloud-build.sh
@@ -8,8 +8,23 @@ _DOCKERFILE_PATH="${SCRIPT_DIR}/../Dockerfile"
 
 echo "_DOCKERFILE_PATH is ${_DOCKERFILE_PATH}"
 
+# Check if the Dockerfile exists and is readable
+if [ -r "${_DOCKERFILE_PATH}" ]; then
+    echo "Dockerfile exists and is readable"
+else
+    echo "Cannot read Dockerfile at ${_DOCKERFILE_PATH}"
+    exit 1
+fi
+
+# Change the current working directory to the parent directory of the script
+cd "${SCRIPT_DIR}/.."
+
+# Check the current working directory
+echo "Current working directory is $(pwd)"
+
 # Start the build and get its ID
-BUILD_ID=$(gcloud builds submit --quiet --config "${SCRIPT_DIR}/cloudbuild.yaml" --substitutions=SHORT_SHA=$(git rev-parse --short HEAD),_DOCKERFILE_PATH="${_DOCKERFILE_PATH}" --format='value(id)') "${SCRIPT_DIR}/.."
+echo "Starting build..."
+BUILD_ID=$(gcloud builds submit --quiet --config "${SCRIPT_DIR}/cloudbuild.yaml" --substitutions=SHORT_SHA=$(git rev-parse --short HEAD),_DOCKERFILE_PATH="${_DOCKERFILE_PATH}" --format='value(id)' .)
 
 # Print the URL where the logs can be found
 echo "View logs at: https://console.cloud.google.com/cloud-build/builds/$BUILD_ID?project=$GCLOUD_PROJECT_ID"
