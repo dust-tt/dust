10b252dbd704bf2956ae1d8e7f94d9785196eb23
Henry Fontanier <h.fontanier@gmail.com>
2023-05-23 13:54:29 +0200

    fix: Dockerfile path

--- front/admin/cloud-build.sh
+++ front/admin/cloud-build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Start the build and get its ID
-BUILD_ID=$(gcloud builds submit --quiet --config cloudbuild.yaml --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) --format='value(id)' .)
+BUILD_ID=$(gcloud builds submit --quiet --config admin/cloudbuild.yaml --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) --format='value(id)' .),_DOCKERFILE_PATH=$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)/../Dockerfile .
 
 # Print the URL where the logs can be found
 echo "View logs at: https://console.cloud.google.com/cloud-build/builds/$BUILD_ID?project=$GCLOUD_PROJECT_ID"
