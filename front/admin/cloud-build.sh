5fda2e71a9939f36201a9b2c329a720d95cfca38
Henry Fontanier <h.fontanier@gmail.com>
2023-05-23 14:14:01 +0200

    relative paths

--- front/admin/cloud-build.sh
+++ front/admin/cloud-build.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+
 # Start the build and get its ID
-BUILD_ID=$(gcloud builds submit --quiet --config admin/cloudbuild.yaml --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) --format='value(id)' .),_DOCKERFILE_PATH=$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)/../Dockerfile .
+BUILD_ID=$(gcloud builds submit --quiet --config "${SCRIPT_DIR}/cloudbuild.yaml" --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) --format='value(id)' .),_DOCKERFILE_PATH="${SCRIPT_DIR}/../Dockerfile"
 
 # Print the URL where the logs can be found
 echo "View logs at: https://console.cloud.google.com/cloud-build/builds/$BUILD_ID?project=$GCLOUD_PROJECT_ID"
