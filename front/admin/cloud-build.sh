aa4c0f31a39544b3a4da58985cb38fcd567017c1
Henry Fontanier <h.fontanier@gmail.com>
2023-05-23 12:23:48 +0200

    fix: dont stream logs when building on cloud build

--- front/admin/cloud-build.sh
+++ front/admin/cloud-build.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-gcloud builds submit --config cloudbuild.yaml --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) .
+# Start the build and get its ID
+BUILD_ID=$(gcloud builds submit --quiet --config cloudbuild.yaml --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) --format='value(id)' .)
+
+# Print the URL where the logs can be found
+echo "View logs at: https://console.cloud.google.com/cloud-build/builds/$BUILD_ID?project=$GCLOUD_PROJECT_ID"
