bb66802fb7fd94e3b8e897f67058ab785cbd9ba8
Stanislas Polu <spolu@proton.me>
2023-05-30 14:02:17 +0200

    clean-up

--- front/lib/actions_registry.ts
+++ front/lib/actions_registry.ts
@@ -5,28 +5,6 @@ const PRODUCTION_DUST_APPS_WORKSPACE_ID = "78bda07b39";
 export const DustProdActionRegistry: {
   [key: string]: { app: DustAppType; config: { [model: string]: any } };
 } = {
-  "chat-main": {
-    app: {
-      workspaceId: PRODUCTION_DUST_APPS_WORKSPACE_ID,
-      appId: "6fe1383f11",
-      appHash:
-        "14751b358fee71615debe3a367e4f7426f902e91b6248025c5b474b37cb9302b",
-    },
-    config: {
-      MODEL: {
-        provider_id: "openai",
-        model_id: "gpt-3.5-turbo",
-        use_cache: true,
-        use_stream: true,
-      },
-      DATASOURCE: {
-        data_sources: [],
-        top_k: 8,
-        filter: { tags: null, timestamp: null },
-        use_cache: false,
-      },
-    },
-  },
   "chat-retrieval": {
     app: {
       workspaceId: PRODUCTION_DUST_APPS_WORKSPACE_ID,
