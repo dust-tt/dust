b6475fa757405b8edca666f0b3c779720d327c90
Uzay-G <uzgirit@gmail.com>
2023-05-31 11:21:12 -0400

    final linting

--- front/lib/dust_api.ts
+++ front/lib/dust_api.ts
@@ -55,7 +55,11 @@ export type DustAppRunBlockExecutionEvent = {
   content: {
     block_type: string;
     block_name: string;
-    execution: { value: any | null; error: string | null, meta: any | null }[][];
+    execution: {
+      value: any | null;
+      error: string | null;
+      meta: any | null;
+    }[][];
   };
 };
 
