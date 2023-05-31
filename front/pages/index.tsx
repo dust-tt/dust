f65d9c9931bfaf36d45007e73d4578f4286a9f0a
Stanislas Polu <spolu@proton.me>
2023-05-31 14:52:44 +0200

    meta tags

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -137,8 +137,23 @@ export default function Home({
   return (
     <>
       <Head>
-        <title>Dust - Unleash your Company Data with Generative AI</title>
+        <title>Dust - Build Smarter Teams with Generative AI</title>
         <link rel="shortcut icon" href="/static/favicon.png" />
+        <meta
+          id="meta-description"
+          name="description"
+          content="Harnesses the power of LLMs to help your teams craft better content, understand their environment faster, and ultimately take better decisions."
+        />
+        <meta
+          id="og-title"
+          property="og:title"
+          content="Dust - Build Smarter Teams with Generative AI"
+        />
+        <meta
+          id="og-image"
+          property="og:image"
+          content="/static/og_image.png"
+        />
       </Head>
 
       <div className="absolute bottom-0 left-0 right-0 top-0 -z-50 overflow-hidden opacity-5">
