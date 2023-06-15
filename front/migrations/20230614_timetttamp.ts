8d38a709850ab4896d2b47803421e4d1281fa045
Philippe Rolet <pr@dust.tt>
2023-06-15 18:02:28 +0200

    Migration timettamp (#601)
    * added qdrant client to dev dependencies + script draft
    
    * first pass
    
    * migration script
    
    * linting

---
+++ front/migrations/20230614_timetttamp.ts
@@ -0,0 +1,74 @@
+/* Following a typo on points payload in our Qdrant database in which the field
+time*st*amp was written time*t*amp This migration creates a new field for every
+point, correctly named time*st*amp and with the value of the existing time*t*amp
+field*/
+
+import { QdrantClient } from "@qdrant/js-client-rest";
+
+// if reusing, set env vars to those values, don't hardcode the values in the code
+// also as a safety delete the env vars (if persisted one way or another) after the migration
+const { QDRANT_PROD_URL, QDRANT_PROD_API_KEY } = process.env;
+
+const prodClient = new QdrantClient({
+  url: QDRANT_PROD_URL,
+  apiKey: QDRANT_PROD_API_KEY,
+});
+
+async function migrateCollection(
+  client: QdrantClient,
+  collectionName: string,
+  batchSize = 250,
+  offset?: string | number | undefined | null | Record<string, unknown>
+) {
+  let currentTime = Date.now(),
+    points,
+    updated = 0;
+  let nb_points = await client.count(collectionName);
+  console.log(
+    "Migrating Collection ",
+    collectionName,
+    "\nNumber of points: ",
+    nb_points.count
+  );
+  // scroll points excluding those with timestamp field until none are left
+  do {
+    console.log("Current offset: ", offset);
+    const { points, next_page_offset } = await client.scroll(collectionName, {
+      limit: batchSize,
+      offset,
+      with_vector: false,
+      filter: {
+        must: [{ is_empty: { key: "timestamp" } }],
+      },
+    });
+
+    // update the points
+    const updatePromises = points.map(async (point) => {
+      const payload = { timestamp: point.payload?.timetamp };
+      await client.setPayload(collectionName, { payload, points: [point.id] });
+    });
+    // wait for all points of the batch to be updated to avoid flooding the server
+    await Promise.all(updatePromises);
+    offset = next_page_offset;
+    updated += points.length;
+  } while (offset !== null);
+  console.log(
+    "Updated points: ",
+    updated,
+    "\nTime: ",
+    Date.now() - currentTime
+  );
+}
+
+/* Migrate all collections
+ * Points that do not need an update are ignored
+ * Therefore can be safely restarted if it fails
+ */
+async function migrateCollections(client: QdrantClient) {
+  const result = await client.getCollections();
+  for (const collection of result.collections) {
+    await migrateCollection(client, collection.name);
+  }
+}
+
+migrateCollections(prodClient);
