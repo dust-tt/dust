515f1c8cee26fd89c67149c6af2edb86ba6d2433
Aric Lasry <lasry.aric@gmail.com>
2023-06-15 16:20:51 +0200

    Properly rollback on create connector errors

--- connectors/src/connectors/google_drive/index.ts
+++ connectors/src/connectors/google_drive/index.ts
@@ -4,6 +4,7 @@ import {
   sequelize_conn,
 } from "@connectors/lib/models.js";
 import { Err, Ok, type Result } from "@connectors/lib/result.js";
+import logger from "@connectors/logger/logger";
 import type { DataSourceConfig } from "@connectors/types/data_source_config.js";
 
 import { registerWebhook } from "./lib";
@@ -16,54 +17,58 @@ export async function createGoogleDriveConnector(
   dataSourceConfig: DataSourceConfig,
   nangoConnectionId: NangoConnectionId
 ): Promise<Result<string, Error>> {
-  const res = await sequelize_conn.transaction(
-    async (t): Promise<Result<Connector, Error>> => {
-      if (!NANGO_GOOGLE_DRIVE_CONNECTOR_ID) {
-        throw new Error("NANGO_GOOGLE_DRIVE_CONNECTOR_ID is not defined");
-      }
-      const driveClient = await getDriveClient(nangoConnectionId);
-      const sanityCheckRes = await driveClient.about.get({ fields: "*" });
-      if (sanityCheckRes.status !== 200) {
-        return new Err(
-          new Error(
+  try {
+    const connector = await sequelize_conn.transaction(
+      async (t): Promise<Connector> => {
+        if (!NANGO_GOOGLE_DRIVE_CONNECTOR_ID) {
+          throw new Error("NANGO_GOOGLE_DRIVE_CONNECTOR_ID is not defined");
+        }
+        const driveClient = await getDriveClient(nangoConnectionId);
+        const sanityCheckRes = await driveClient.about.get({ fields: "*" });
+        if (sanityCheckRes.status !== 200) {
+          throw new Error(
             `Could not get google drive info. Error message: ${
               sanityCheckRes.statusText || "unknown"
             }`
-          )
-        );
-      }
+          );
+        }
 
-      const connector = await Connector.create(
-        {
-          type: "google_drive",
-          connectionId: nangoConnectionId,
-          workspaceAPIKey: dataSourceConfig.workspaceAPIKey,
-          workspaceId: dataSourceConfig.workspaceId,
-          dataSourceName: dataSourceConfig.dataSourceName,
-        },
-        { transaction: t }
-      );
-
-      const webhookInfo = await registerWebhook(connector.connectionId);
-      if (webhookInfo.isErr()) {
-        return webhookInfo;
-      } else {
-        await GoogleDriveWebhook.create(
+        const connector = await Connector.create(
           {
-            webhookId: webhookInfo.value.id,
-            expiresAt: new Date(webhookInfo.value.expirationTsMs),
-            connectorId: connector.id,
+            type: "google_drive",
+            connectionId: nangoConnectionId,
+            workspaceAPIKey: dataSourceConfig.workspaceAPIKey,
+            workspaceId: dataSourceConfig.workspaceId,
+            dataSourceName: dataSourceConfig.dataSourceName,
           },
           { transaction: t }
         );
+
+        const webhookInfo = await registerWebhook(connector.connectionId);
+        if (webhookInfo.isErr()) {
+          throw webhookInfo.error;
+        } else {
+          await GoogleDriveWebhook.create(
+            {
+              webhookId: webhookInfo.value.id,
+              expiresAt: new Date(webhookInfo.value.expirationTsMs),
+              connectorId: connector.id,
+            },
+            { transaction: t }
+          );
+        }
+
+        return connector;
       }
-
-      return new Ok(connector);
-    }
-  );
-  if (res.isErr()) {
-    return res;
+    );
+    return new Ok(connector.id.toString());
+  } catch (err) {
+    logger.error(
+      {
+        err,
+      },
+      "Error creating Google Drive connector"
+    );
+    return new Err(new Error("Error creating Google Drive connector"));
   }
-
-  return new Ok(res.value.id.toString());
 }
