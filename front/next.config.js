cf5490bc3d8da7b300c3f3631e20608ec77007d5
Aric Lasry <lasry.aric@gmail.com>
2023-06-16 20:23:29 +0200

    Format

--- front/next.config.js
+++ front/next.config.js
@@ -6,15 +6,17 @@ module.exports = removeImports({
   async redirects() {
     return [
       {
-        source: '/privacy',
-        destination: 'https://dust-tt.notion.site/Privacy-1a329ca7b8e349e88b5ec3277fe35189',
+        source: "/privacy",
+        destination:
+          "https://dust-tt.notion.site/Privacy-1a329ca7b8e349e88b5ec3277fe35189",
         permanent: true,
       },
       {
-        source: '/terms',
-        destination: 'https://dust-tt.notion.site/Terms-of-Use-ff8665f52c454e0daf02195ec0d6bafb',
+        source: "/terms",
+        destination:
+          "https://dust-tt.notion.site/Terms-of-Use-ff8665f52c454e0daf02195ec0d6bafb",
         permanent: true,
       },
-    ]
+    ];
   },
 });
