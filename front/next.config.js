f1b4d62db6727de777c133e96a5148e6e1649b5f
Aric Lasry <lasry.aric@gmail.com>
2023-06-16 20:17:59 +0200

    Adding dust.tt URLs for privacy policy and Terms of service.

--- front/next.config.js
+++ front/next.config.js
@@ -3,4 +3,18 @@ const removeImports = require("next-remove-imports")();
 
 module.exports = removeImports({
   experimental: { esmExternals: true },
+  async redirects() {
+    return [
+      {
+        source: '/privacy-policy',
+        destination: 'https://dust-tt.notion.site/Privacy-1a329ca7b8e349e88b5ec3277fe35189',
+        permanent: true,
+      },
+      {
+        source: '/terms-of-service',
+        destination: 'https://dust-tt.notion.site/Terms-of-Use-ff8665f52c454e0daf02195ec0d6bafb',
+        permanent: true,
+      },
+    ]
+  },
 });
