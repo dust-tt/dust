17c40ceef9c2fc984c614a64012dfda509cd1592
Aric Lasry <lasry.aric@gmail.com>
2023-06-19 16:36:33 +0200

    Change name

--- front/pages/nango-oauth-callback.ts
+++ front/pages/nango-oauth-callback.ts
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 
 export default function NangoRedirect() {
   useEffect(() => {
-  const nangoURL = `https://api.nango.dev/oauth/callback${window.location.search}`
-  window.location.replace(nangoURL);
+    const nangoURL = `https://api.nango.dev/oauth/callback${window.location.search}`;
+    window.location.replace(nangoURL);
   }, []);
 
   return null; // Render nothing.
