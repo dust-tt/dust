6f3bfa1aed9191986e8d6b74ce75b694933ac231
Uzay-G <uzgirit@gmail.com>
2023-06-20 06:58:14 -0400

    lint

--- core/src/data_sources/data_source.rs
+++ core/src/data_sources/data_source.rs
@@ -914,7 +914,9 @@ impl DataSource {
                                     {
                                         let c_offset = parsed_results[counter].1;
                                         if chunk.offset < c_offset {
-                                            chunk.text.push_str(&(" ".to_owned() + &parsed_results[counter].0));
+                                            chunk.text.push_str(
+                                                &(" ".to_owned() + &parsed_results[counter].0),
+                                            );
                                         } else {
                                             prepend.push_str(
                                                 &(parsed_results[counter].0.clone() + " "),
