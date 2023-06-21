00d4b632029ce4614fed0a96d807bf2d4255e74b
Henry Fontanier <h.fontanier@gmail.com>
2023-06-21 14:19:37 +0200

    add discussions-related function to github_api.ts

--- connectors/src/connectors/github/lib/github_api.ts
+++ connectors/src/connectors/github/lib/github_api.ts
@@ -1,7 +1,19 @@
 import { createAppAuth } from "@octokit/auth-app";
+import { isLeft } from "fp-ts/lib/Either";
 import fs from "fs-extra";
+import { PathReporter } from "io-ts/lib/PathReporter";
+import * as reporter from "io-ts-reporters";
 import { Octokit } from "octokit";
 
+import {
+  DiscussionCommentNode,
+  DiscussionNode,
+  ErrorPayloadSchema,
+  GetDiscussionCommentRepliesPayloadSchema,
+  GetDiscussionCommentsPayloadSchema,
+  GetDiscussionPayloadSchema,
+  GetRepoDiscussionsPayloadSchema,
+} from "@connectors/connectors/github/lib/github_graphql";
 import logger from "@connectors/logger/logger";
 
 const API_PAGE_SIZE = 100;
@@ -27,7 +39,7 @@ export type GithubUser = {
   login: string;
 };
 
-type GithubIssue = {
+export type GithubIssue = {
   id: number;
   number: number;
   title: string;
@@ -209,6 +221,270 @@ export async function getIssueCommentsPage(
     updatedAt: new Date(c.updated_at),
     body: c.body,
   }));
+}
+
+export async function getRepoDiscussionsPage(
+  installationId: string,
+  repoName: string,
+  login: string,
+  cursor: string | null = null
+): Promise<{ cursor: string | null; discussions: DiscussionNode[] }> {
+  const octokit = await getOctokit(installationId);
+  const d = await octokit.graphql(
+    `
+      query getDiscussions($owner: String!, $repo: String!, $cursor: String) {
+        repository(owner: $owner, name: $repo) {
+          discussions(first:100, after: $cursor) {
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            edges {
+              node {
+                title
+                id
+                number
+                bodyText
+                url
+                createdAt
+                updatedAt
+                author {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      owner: login,
+      repo: repoName,
+      cursor: cursor,
+    }
+  );
+
+  const errorPayloadValidation = ErrorPayloadSchema.decode(d);
+  if (!isLeft(errorPayloadValidation)) {
+    throw new Error(JSON.stringify(errorPayloadValidation.right));
+  }
+
+  const getRepoDiscussionsPayloadValidation =
+    GetRepoDiscussionsPayloadSchema.decode(d);
+
+  if (isLeft(getRepoDiscussionsPayloadValidation)) {
+    const pathError = PathReporter.report(
+      getRepoDiscussionsPayloadValidation
+    ).join(", ");
+    throw new Error(`Unexpected payload: ${pathError}`);
+  }
+
+  const payload = getRepoDiscussionsPayloadValidation.right;
+
+  return {
+    cursor: payload.repository.discussions.pageInfo.hasNextPage
+      ? payload.repository.discussions.pageInfo.endCursor
+      : null,
+    discussions: payload.repository.discussions.edges.map((e) => e.node),
+  };
+}
+
+export async function getDiscussionCommentsPage(
+  installationId: string,
+  repoName: string,
+  login: string,
+  discussionNumber: number,
+  cursor: string | null = null
+): Promise<{ cursor: string | null; comments: DiscussionCommentNode[] }> {
+  const octokit = await getOctokit(installationId);
+  const d = await octokit.graphql(
+    `
+    query getDiscussionComments(
+      $owner: String!
+      $repo: String!
+      $discussionNumber: Int!
+      $cursor: String
+    ) {
+      repository(owner: $owner, name: $repo) {
+        discussion(number: $discussionNumber) {
+          comments(first: 100, after: $cursor) {
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            edges {
+              node {
+                id
+                isAnswer
+                bodyText
+                createdAt
+                updatedAt
+                author {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+    } 
+    `,
+    {
+      owner: login,
+      repo: repoName,
+      discussionNumber,
+      cursor,
+    }
+  );
+
+  const errorPayloadValidation = ErrorPayloadSchema.decode(d);
+  if (!isLeft(errorPayloadValidation)) {
+    throw new Error(JSON.stringify(errorPayloadValidation.right));
+  }
+
+  const getDiscussionsCommentsPayloadValidation =
+    GetDiscussionCommentsPayloadSchema.decode(d);
+
+  if (isLeft(getDiscussionsCommentsPayloadValidation)) {
+    const pathError = reporter.formatValidationErrors(
+      getDiscussionsCommentsPayloadValidation.left
+    );
+    throw new Error(`Unexpected payload: ${pathError.join(", ")}`);
+  }
+
+  const payload = getDiscussionsCommentsPayloadValidation.right;
+
+  return {
+    cursor: payload.repository.discussion.comments.pageInfo.hasNextPage
+      ? payload.repository.discussion.comments.pageInfo.endCursor
+      : null,
+    comments: payload.repository.discussion.comments.edges.map((e) => e.node),
+  };
+}
+
+export async function getDiscussionCommentRepliesPage(
+  installationId: string,
+  nodeID: string,
+  cursor: string | null = null
+): Promise<{ cursor: string | null; comments: DiscussionCommentNode[] }> {
+  const octokit = await getOctokit(installationId);
+
+  const d = await octokit.graphql(
+    `
+    query getDiscussionCommentReplies(
+      $nodeID: ID!
+      $cursor: String
+    ) {
+      node(id: $nodeID) {
+        ... on DiscussionComment {
+          replies(first: 100, after: $cursor) {
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+            edges {
+              node {
+                id
+                isAnswer
+                bodyText
+                createdAt
+                updatedAt
+                author {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    `,
+    {
+      nodeID,
+      cursor,
+    }
+  );
+
+  const errorPayloadValidation = ErrorPayloadSchema.decode(d);
+  if (!isLeft(errorPayloadValidation)) {
+    throw new Error(JSON.stringify(errorPayloadValidation.right));
+  }
+
+  const getDiscussionCommentRepliesPayloadValidation =
+    GetDiscussionCommentRepliesPayloadSchema.decode(d);
+
+  if (isLeft(getDiscussionCommentRepliesPayloadValidation)) {
+    const pathError = reporter.formatValidationErrors(
+      getDiscussionCommentRepliesPayloadValidation.left
+    );
+    throw new Error(`Unexpected payload: ${pathError.join(", ")}`);
+  }
+
+  const payload = getDiscussionCommentRepliesPayloadValidation.right;
+
+  return {
+    cursor: payload.node.replies.pageInfo.hasNextPage
+      ? payload.node.replies.pageInfo.endCursor
+      : null,
+    comments: payload.node.replies.edges.map((e) => e.node),
+  };
+}
+
+export async function getDiscussion(
+  installationId: string,
+  repoName: string,
+  login: string,
+  discussionNumber: number
+): Promise<DiscussionNode> {
+  const octokit = await getOctokit(installationId);
+
+  const d = await octokit.graphql(
+    `
+    query getDiscussion(
+      $owner: String!
+      $repo: String!
+      $discussionNumber: Int!
+    ) {
+      repository(owner: $owner, name: $repo) {
+        discussion(number: $discussionNumber) {
+          title
+          id
+          number
+          bodyText
+          url
+          createdAt
+          updatedAt
+          author {
+            login
+          }
+        }
+      }
+    }
+    `,
+    {
+      owner: login,
+      repo: repoName,
+      discussionNumber,
+    }
+  );
+
+  const errorPayloadValidation = ErrorPayloadSchema.decode(d);
+  if (!isLeft(errorPayloadValidation)) {
+    throw new Error(JSON.stringify(errorPayloadValidation.right));
+  }
+
+  const getDiscussionPayloadValidation = GetDiscussionPayloadSchema.decode(d);
+
+  if (isLeft(getDiscussionPayloadValidation)) {
+    const pathError = reporter.formatValidationErrors(
+      getDiscussionPayloadValidation.left
+    );
+    throw new Error(`Unexpected payload: ${pathError.join(", ")}`);
+  }
+
+  const payload = getDiscussionPayloadValidation.right;
+
+  return payload.repository.discussion;
 }
 
 async function getOctokit(installationId: string): Promise<Octokit> {
