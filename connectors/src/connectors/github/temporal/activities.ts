2778b353b3518b58d0b4ab18c70f8375845c0b4b
Henry Fontanier <h.fontanier@gmail.com>
2023-06-21 16:10:39 +0200

    activities

--- connectors/src/connectors/github/temporal/activities.ts
+++ connectors/src/connectors/github/temporal/activities.ts
@@ -1,8 +1,12 @@
 import PQueue from "p-queue";
 
 import {
+  getDiscussion,
+  getDiscussionCommentRepliesPage,
+  getDiscussionCommentsPage,
   getIssue,
   getIssueCommentsPage,
+  getRepoDiscussionsPage,
   getRepoIssuesPage,
   getReposPage,
   GithubUser,
@@ -153,8 +157,150 @@ export async function githubUpsertIssueActivity(
       repoId: repoId.toString(),
       issueNumber,
       connectorId: connector.id,
+      isDiscussion: false,
     });
   }
+}
+
+export async function githubUpsertDiscussionActivity(
+  installationId: string,
+  repoName: string,
+  repoId: number,
+  login: string,
+  discussionNumber: number,
+  dataSourceConfig: DataSourceConfig,
+  loggerArgs: Record<string, string | number>
+) {
+  const localLogger = logger.child({
+    ...loggerArgs,
+    discussionNumber,
+  });
+
+  localLogger.info("Upserting GitHub discussion.");
+
+  const discussion = await getDiscussion(
+    installationId,
+    repoName,
+    login,
+    discussionNumber
+  );
+
+  let renderedDiscussion = `# ${discussion.title}||\n${discussion.bodyText}||\n`;
+
+  let nextCursor: string | null = null;
+
+  for (;;) {
+    const { cursor, comments } = await getDiscussionCommentsPage(
+      installationId,
+      repoName,
+      login,
+      discussionNumber,
+      nextCursor
+    );
+
+    for (const comment of comments) {
+      renderedDiscussion += `${comment.author.login}: ${comment.bodyText}||`;
+      let nextChildCursor: string | null = null;
+      for (;;) {
+        const { cursor: childCursor, comments: childComments } =
+          await getDiscussionCommentRepliesPage(
+            installationId,
+            comment.id,
+            nextChildCursor
+          );
+        for (const childComment of childComments) {
+          renderedDiscussion += `----${childComment.author.login}: ${childComment.bodyText}||`;
+        }
+
+        if (!childCursor) {
+          break;
+        }
+
+        nextChildCursor = childCursor;
+      }
+    }
+
+    if (!cursor) {
+      break;
+    }
+
+    nextCursor = cursor;
+  }
+
+  const documentId = getDiscussionDocumentId(
+    repoId.toString(),
+    discussionNumber
+  );
+  const tags = [
+    `title:${discussion.title}`,
+    `author:${discussion.author.login}`,
+  ];
+
+  await upsertToDatasource(
+    dataSourceConfig,
+    documentId,
+    renderedDiscussion,
+    discussion.url,
+    new Date(discussion.createdAt).getTime(),
+    tags,
+    3,
+    500,
+    { ...loggerArgs, provider: "github" }
+  );
+
+  const connector = await Connector.findOne({
+    where: {
+      connectionId: installationId,
+    },
+  });
+  if (!connector) {
+    throw new Error("Connector not found");
+  }
+
+  const existingDiscussionInDb = await GithubIssue.findOne({
+    where: {
+      repoId: repoId.toString(),
+      issueNumber: discussionNumber,
+      connectorId: connector.id,
+    },
+  });
+
+  if (!existingDiscussionInDb) {
+    localLogger.info("Creating new GitHub discussion in DB.");
+    await GithubIssue.create({
+      repoId: repoId.toString(),
+      issueNumber: discussionNumber,
+      connectorId: connector.id,
+      isDiscussion: true,
+    });
+  }
+}
+
+export async function githubGetRepoDiscussionsResultPageActivity(
+  installationId: string,
+  repoName: string,
+  login: string,
+  cursor: string | null,
+  loggerArgs: Record<string, string | number>
+): Promise<{ cursor: string | null; discussionNumbers: number[] }> {
+  const localLogger = logger.child({
+    ...loggerArgs,
+    cursor,
+  });
+
+  localLogger.info("Fetching GitHub discussions result page.");
+
+  const { cursor: nextCursor, discussions } = await getRepoDiscussionsPage(
+    installationId,
+    repoName,
+    login,
+    cursor
+  );
+
+  return {
+    cursor: nextCursor,
+    discussionNumbers: discussions.map((discussion) => discussion.number),
+  };
 }
 
 export async function githubSaveStartSyncActivity(
@@ -205,7 +351,7 @@ export async function githubIssueGarbageCollectActivity(
   issueNumber: number,
   loggerArgs: Record<string, string | number>
 ) {
-  await deleteIssue(
+  await deleteDiscussionOrIssue(
     dataSourceConfig,
     installationId,
     repoId,
@@ -243,7 +389,7 @@ export async function githubRepoGarbageCollectActivity(
   for (const issue of issuesInRepo) {
     promises.push(
       queue.add(() =>
-        deleteIssue(
+        deleteDiscussionOrIssue(
           dataSourceConfig,
           installationId,
           repoId,
@@ -257,14 +403,14 @@ export async function githubRepoGarbageCollectActivity(
   await Promise.all(promises);
 }
 
-async function deleteIssue(
+async function deleteDiscussionOrIssue(
   dataSourceConfig: DataSourceConfig,
   installationId: string,
   repoId: string,
-  issueNumber: number,
+  issueOrDiscussionNumber: number,
   loggerArgs: Record<string, string | number>
 ) {
-  const localLogger = logger.child(loggerArgs);
+  const localLogger = logger.child({ ...loggerArgs, issueOrDiscussionNumber });
 
   const connector = await Connector.findOne({
     where: {
@@ -276,17 +422,33 @@ async function deleteIssue(
     throw new Error("Connector not found");
   }
 
-  localLogger.info("Deleting GitHub issue from Dust Data Source.");
-  await deleteFromDataSource(
-    dataSourceConfig,
-    getIssueDocumentId(repoId, issueNumber)
+  const issueInDb = await GithubIssue.findOne({
+    where: {
+      repoId: repoId.toString(),
+      issueNumber: issueOrDiscussionNumber,
+      connectorId: connector.id,
+    },
+  });
+
+  if (!issueInDb) {
+    throw new Error("Issue not found in DB");
+  }
+
+  const documentId = issueInDb.isDiscussion
+    ? getDiscussionDocumentId(repoId.toString(), issueOrDiscussionNumber)
+    : getIssueDocumentId(repoId.toString(), issueOrDiscussionNumber);
+
+  localLogger.info(
+    { documentId },
+    "Deleting GitHub issue from Dust Data Source."
   );
+  await deleteFromDataSource(dataSourceConfig, documentId);
 
   localLogger.info("Deleting GitHub issue from database.");
   await GithubIssue.destroy({
     where: {
       repoId: repoId.toString(),
-      issueNumber,
+      issueNumber: issueOrDiscussionNumber,
       connectorId: connector.id,
     },
   });
@@ -304,4 +466,11 @@ function renderGithubUser(user: GithubUser | null): string {
 
 function getIssueDocumentId(repoId: string, issueNumber: number): string {
   return `github-issue-${repoId}-${issueNumber}`;
+}
+
+function getDiscussionDocumentId(
+  repoId: string,
+  discussionNumber: number
+): string {
+  return `github-discussion-${repoId}-${discussionNumber}`;
 }
