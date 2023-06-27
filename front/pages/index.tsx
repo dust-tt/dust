25ee898c22dfa304e55e4a0afac95ac90f9e77cf
Stanislas Polu <spolu@proton.me>
2023-06-27 14:08:33 +0200

    landing: banner seed round (#690)

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -167,7 +167,51 @@ export default function Home({
       <div className="absolute bottom-0 left-0 right-0 top-0 -z-50 overflow-hidden">
         <Cloud />
       </div>
+
       <main className="z-10 mx-4">
+        <div className="relative isolate -mx-4 -mb-2 flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+          <div
+            className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+            ></div>
+          </div>
+          <div
+            className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+              style={{
+                clipPath:
+                  "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+              }}
+            ></div>
+          </div>
+          <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+            <p className="text-center text-sm leading-6 text-gray-900">
+              <strong className="font-semibold">
+                🥳 Announcing our seed round
+              </strong>{" "}
+              led by Sequoia&nbsp;Capital
+              <a
+                href="https://blog.dust.tt/2023-06-27-announcing-seed"
+                className="ml-2 inline-block flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                target="_blank"
+              >
+                Read more
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-1 justify-end"></div>
+        </div>
+
         <div className="grid grid-cols-5 gap-1">
           <div className="col-span-2 text-left">
             <Logo />
