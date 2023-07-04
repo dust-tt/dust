33127785c916c51c62cb19258ccdc9eede8994bf
Daphné <daph@dust.tt>
2023-07-04 14:06:48 +0200

    Handle error Slack conversation info empty response

--- connectors/src/connectors/slack/temporal/activities.ts
+++ connectors/src/connectors/slack/temporal/activities.ts
@@ -93,6 +93,12 @@ export async function getChannel(
 ): Promise<Channel> {
   const client = getSlackClient(slackAccessToken);
   const res = await client.conversations.info({ channel: channelId });
+  // Despite the typing, in practice `conversations.info` can be undefined at times.
+  if (!res) {
+    throw new Error(
+        "Received unexpected conversations.info replies from Slack API in getChannel (generally transient)"
+    );
+  }
   if (res.error) {
     throw new Error(res.error);
   }
