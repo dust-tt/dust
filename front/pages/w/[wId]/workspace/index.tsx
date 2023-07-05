a369b384f173ab9cc9ff0e577337b4c5306c14b0
Daphné <daph@dust.tt>
2023-07-05 13:22:40 +0200

    Fix: Trim invite email

--- front/pages/w/[wId]/workspace/index.tsx
+++ front/pages/w/[wId]/workspace/index.tsx
@@ -340,7 +340,7 @@ export default function WorkspaceAdmin({
                         value={inviteEmail || ""}
                         onChange={(e) => {
                           if (e.target.value.length > 0) {
-                            setInviteEmail(e.target.value);
+                            setInviteEmail(e.target.value.trim());
                           } else {
                             setInviteEmail("");
                           }
