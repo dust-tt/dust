b83d26a886305b0f2b6677b99201f48bc9e272c4
philipperolet <pr@dust.tt>
2023-07-08 20:10:32 +0200

    eval calls newChat to get answers

--- front/tests/chat/e2e_eval.ts
+++ front/tests/chat/e2e_eval.ts
@@ -11,34 +11,58 @@ import {
   DustProdActionRegistry,
 } from "@app/lib/actions/registry";
 import { runAction } from "@app/lib/actions/server";
-import { Authenticator } from "@app/lib/auth";
+import { Authenticator, getOrCreateSystemApiKey } from "@app/lib/auth";
 import { Workspace } from "@app/lib/models";
 import { Ok } from "@app/lib/result";
 import { RunType } from "@app/types/run";
 import { WorkspaceType } from "@app/types/user";
+import { newChat } from "@app/lib/api/chat";
 
 type ChatEvalInput = {
   question: string;
   rules: string[];
   answer: string;
 };
-async function computeChatAnswer(input: object) {
-  return "I have no clue.";
-}
-
 async function main() {
-  /* Get the localhost owner */
-  const { LOCALHOST_WORKSPACE_ID } = process.env;
-  const workspace = await Workspace.findOne({
-    where: { sId: LOCALHOST_WORKSPACE_ID },
-  });
-  const auth = new Authenticator(workspace, null, "builder");
+  /* Auth */
+  const workspacesId = process.env.LOCALHOST_WORKSPACE_ID as string;
+  const auth = await Authenticator.internalBuilderForWorkspace(workspacesId);
+  const owner = auth.workspace();
+  if (!owner) {
+    throw new Error("Invalid workspace");
+  }
 
-  /* Load the JSON test data from data/chat-eval-inputs.jsonl */
+  async function computeChatAnswer(input: ChatEvalInput) {
+    /* Create a system API key 
+  const keyRes = await getOrCreateSystemApiKey(owner);
+  if (keyRes.isErr()) {
+    throw new Error(`Failed to get system API key: ${keyRes.error}`);
+  }*/
+    /* Call the chat lib */
+    const chatRes = await newChat(auth, {
+      userMessage: input.question,
+      dataSources: null,
+      filter: null,
+      timeZone: "Europe/Paris",
+    });
+    for await (const event of chatRes) {
+      if (
+        event.type === "chat_message_create" &&
+        event.message.role === "assistant"
+      ) {
+        console.log(event.message.message);
+        return event.message.message;
+      }
+    }
+    return "There was an error. Please check with the team";
+  }
 
+  /* Load the JSON test data from data/chat-eval-inputs.jsonl's first 2 elements*/
   const evalData = JSON.parse(
     fs.readFileSync(path.join(__dirname, "data/chat-eval-inputs.json"), "utf8")
-  );
+  ).slice(0, 2) as ChatEvalInput[];
+
+  /* Compute Chat's answers for each input */
   const dataWithAnswers = await Promise.all(
     evalData.map(async (input: ChatEvalInput) => {
       return {
@@ -48,10 +72,11 @@ async function main() {
       };
     })
   );
+
+  /* run the chat eval app on dataWithAnswers */
   const config = cloneBaseConfig(
     DustProdActionRegistry["chat-message-e2e-eval"].config
   );
-  /* run the chat eval app on dataWithAnswers */
   const evalResult = (await runAction(
     auth,
     "chat-message-e2e-eval",
@@ -61,10 +86,13 @@ async function main() {
   if (!evalResult.value.results) {
     throw new Error("No results found");
   }
+
+  /* Print the results */
   for (const result of evalResult.value.results) {
     console.log(result[0].value);
   }
 }
+
 main().catch((err) => {
   console.log(err);
   process.exit(1);
