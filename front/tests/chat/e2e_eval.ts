993abf2b7c4fed5b09ec0dc6739b589589fa696d
philipperolet <pr@dust.tt>
2023-07-10 09:40:36 +0200

    nb of questions arg

--- front/tests/chat/e2e_eval.ts
+++ front/tests/chat/e2e_eval.ts
@@ -1,6 +1,14 @@
 /**
- *  Run an eval of our chat app with a test set of questions
- *  See: https://www.notion.so/dust-tt/Design-Doc-Chat-Eval-0b849d4c66564f1e9c0c31d537c96f78
+ * Run an eval of our chat app with a test set of questions See:
+ * https://www.notion.so/dust-tt/Design-Doc-Chat-Eval-0b849d4c66564f1e9c0c31d537c96f78
+ *
+ *
+ * By default, runs on all questions. Use `--n NUMBER_OF_QUESTIONS` to change that (e.g. smaller evals)
+ *
+ * Usage: from front directory
+ * `./admin/eval.sh [--verbose] [--n NUMBER_OF_QUESTIONS]`
+ *
+ *
  */
 
 import * as fs from "fs";
@@ -21,7 +29,8 @@ type ChatEvalInput = {
   rules: string[];
   answer: string;
 };
-async function chatEval() {
+
+async function chatEval(numberOfQuestions: number | undefined = undefined) {
   /* Auth */
   const workspacesId = process.env.LOCALHOST_WORKSPACE_ID as string;
   const auth = await Authenticator.internalBuilderForWorkspace(workspacesId);
@@ -31,11 +40,6 @@ async function chatEval() {
   }
 
   async function computeChatAnswer(input: ChatEvalInput) {
-    /* Create a system API key 
-  const keyRes = await getOrCreateSystemApiKey(owner);
-  if (keyRes.isErr()) {
-    throw new Error(`Failed to get system API key: ${keyRes.error}`);
-  }*/
     /* Call the chat lib */
     const chatRes = await newChat(
       auth,
@@ -62,14 +66,17 @@ async function chatEval() {
     return answer ? answer : "There was an error. Please check with the team";
   }
 
-  /* Load the JSON test data from data/chat-eval-inputs.jsonl's first 2 elements*/
-  const evalData = JSON.parse(
+  /* Load the JSON test data from data/chat-eval-inputs.jsonl */
+  let evalData = JSON.parse(
     fs.readFileSync(path.join(__dirname, "data/chat-eval-inputs.json"), "utf8")
-  ).slice(0, 2) as ChatEvalInput[];
+  );
+  if (numberOfQuestions)
+    evalData = evalData.slice(0, numberOfQuestions) as ChatEvalInput[];
 
   /* Compute Chat's answers for each input */
   const dataWithAnswers = await Promise.all(
     evalData.map(async (input: ChatEvalInput) => {
+      console.log(`Computing answer for ${input.question}`);
       return {
         question: input.question,
         rules: input.rules,
@@ -88,6 +95,7 @@ async function chatEval() {
     config,
     dataWithAnswers
   )) as Ok<RunType>;
+
   if (!evalResult.value.results) {
     throw new Error("No results found");
   }
@@ -110,7 +118,11 @@ async function chatEval() {
 }
 
 async function main() {
-  let chatEvalResults = await chatEval();
+  const numberOfQuestions = process.argv.includes("--n")
+    ? parseInt(process.argv[process.argv.indexOf("--n") + 1])
+    : undefined;
+
+  let chatEvalResults = await chatEval(numberOfQuestions);
   /* Print the results */
   for (const result of chatEvalResults) {
     console.log(result);
