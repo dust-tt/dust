fa3e4419bbcb4403f82c7c0cb3942db9dbf4c15c
Aric Lasry <lasry.aric@gmail.com>
2023-07-12 14:45:15 +0200

    Fix SlackConfig create

--- connectors/src/connectors/slack/index.ts
+++ connectors/src/connectors/slack/index.ts
@@ -75,6 +75,7 @@ export async function createSlackConnector(
         {
           slackTeamId: teamInfo.team.id,
           connectorId: connector.id,
+          botEnabled: false,
         },
         { transaction: t }
       );
