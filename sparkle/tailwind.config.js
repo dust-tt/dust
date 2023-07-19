f1ac16170a38d87981ea333ed892873b3c946376
édouard wautier <edouardwautier@mbp-de-edouard.home>
2023-07-19 11:10:37 +0200

    Adding custom colors to Tailwind config

--- sparkle/tailwind.config.js
+++ sparkle/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'action': colors.violet,
+        'success': colors.green,
+        'warning': colors.yellow,
+        'danger': colors.red,
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
