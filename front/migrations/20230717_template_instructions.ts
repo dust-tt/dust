b35c90034954c1429bbe7fcbdd6c97e9187d3325
Uzay-G <uzgirit@gmail.com>
2023-07-20 10:00:27 -0400

    prettify

--- front/migrations/20230717_template_instructions.ts
+++ front/migrations/20230717_template_instructions.ts
@@ -9,14 +9,19 @@ async function main() {
   });
   // take all the GensTemplate, set instructions2 to the concatenation of all the values in instructions
 
-  const gensTemplates = (await core_sequelize.query(
-    `SELECT * FROM "gens_templates"`
-  ))[0] as GensTemplate[];
+  const gensTemplates = (
+    await core_sequelize.query(`SELECT * FROM "gens_templates"`)
+  )[0] as GensTemplate[];
 
   for (const gensTemplate of gensTemplates) {
     const instructions = gensTemplate?.instructions || [];
     const instructions2 = instructions.join("\n");
-    console.log("Updating instructions2 for ", gensTemplate.id, "to", instructions2);
+    console.log(
+      "Updating instructions2 for ",
+      gensTemplate.id,
+      "to",
+      instructions2
+    );
     if (LIVE) {
       await core_sequelize.query(
         `UPDATE "gens_templates" SET "instructions2" = :instructions2 WHERE id = :id`,
@@ -27,11 +32,9 @@ async function main() {
           },
         }
       );
-    } 
+    }
   }
 }
-
-
 
 main()
   .then(() => {
