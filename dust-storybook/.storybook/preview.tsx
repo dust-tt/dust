3fd7323606c08a6baa740d45c1c9a1058f7de268
Stanislas Polu <spolu@proton.me>
2023-07-25 14:13:21 +0200

    Dark mode support in storybook (#953)

--- dust-storybook/.storybook/preview.tsx
+++ dust-storybook/.storybook/preview.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +9,16 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    darkMode: {
+      darkClass: ["dark"],
+      lightClass: ["light"],
+      stylePreview: true,
+      dark: { ...themes.dark, appBg: "#292C2E" },
+      light: { ...themes.normal, appBg: "white" },
+    },
+    backgrounds: {
+      values: [{ name: "dark", value: "#1B1C1D" }],
     },
   },
 };
