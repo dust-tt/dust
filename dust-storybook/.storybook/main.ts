c6c3b33a32bed0bb9ca0a640d0c36c57a6d4d059
Stanislas Polu <spolu@proton.me>
2023-07-25 14:09:36 +0200

    Dark mode support in storybook

--- dust-storybook/.storybook/main.ts
+++ dust-storybook/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm",
     "storybook-dark-mode",
   ],
   framework: {
