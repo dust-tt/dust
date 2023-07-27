ae3eeb5761579da0b580b1c5f4c0c8f9a3fa187b
Stanislas Polu <spolu@proton.me>
2023-07-27 12:23:47 +0200

    Delete conversation

---
+++ front/components/sparkle/AppLayoutTitle.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@dust-tt/sparkle";
+import { TrashIcon } from "@heroicons/react/24/solid";
+
+export function AppLayoutTitle({
+  title,
+  onDelete,
+}: {
+  title: string;
+  onDelete?: () => void;
+}) {
+  return (
+    <div className="flex h-full flex-row items-center">
+      <div className="flex flex-initial">
+        <div className="w-56 flex-initial overflow-hidden truncate px-10 font-bold sm:w-96 lg:w-auto lg:px-0">
+          {title}
+        </div>
+      </div>
+      <div className="flex flex-1"></div>
+      {onDelete && (
+        <div className="flex flex-initial text-red-800">
+          <Button
+            type="tertiary"
+            label="Delete"
+            icon={TrashIcon}
+            onClick={onDelete}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
