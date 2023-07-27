f9592b896167ad73cdfaeb23127c7a597a8776a3
Stanislas Polu <spolu@proton.me>
2023-07-27 15:50:13 +0200

    Move to ed fonts

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      roboto: ["Roboto", "sans-serif"],
+      // roboto: ["Roboto", "sans-serif"],
+      sans: ["'darkmode-off-cc'", "'darkmode-on-cc'", "sans-serif"],
     },
     extend: {
       colors: {
