7b3ed1474bc995938935b21181fec2880040cece
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2023-07-27 10:00:22 +0200

    Centering the tooltip (#995)
    Co-authored-by: édouard wautier <edouardwautier@mbp-de-edouard.home>

--- sparkle/src/components/Tooltip.tsx
+++ sparkle/src/components/Tooltip.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { classNames } from "@sparkle/lib/utils";
 
 interface TooltipProps {
   children: React.ReactNode;
@@ -31,10 +32,11 @@ export function Tooltip({ children, label, position = "above" }: TooltipProps) {
   }, [timerId]);
 
   const baseClasses =
-    "s-absolute s-inline-flex s-items-center s-justify-center s-whitespace-nowrap s-z-10 s-px-3 s-py-2 s-text-sm s-rounded-xl s-border s-shadow-md s-transition-all s-duration-500 s-ease-out s-transform s-bg-structure-0 dark:s-bg-structure-0-dark s-text-element-700 dark:s-text-element-700-dark s-left-1/2 s-translate-x-1/2";
+    "s-absolute s-whitespace-nowrap s-z-10 s-px-3 s-py-2 s-text-sm s-rounded-xl s-border s-shadow-md s-transition-all s-duration-500 s-ease-out s-transform s-bg-structure-0 dark:s-bg-structure-0-dark s-text-element-700 dark:s-text-element-700-dark";
   const hiddenClasses = "s-translate-y-2 s-opacity-0";
   const visibleClasses = "-s-translate-y-0 s-opacity-100";
 
+  const tooltipCenterClasses = "s-left-1/2 -s-translate-x-1/2";
   const tooltipPositionClasses =
     position === "above" ? "s-bottom-full s-mb-2" : "s-top-full s-mt-2";
 
@@ -46,9 +48,12 @@ export function Tooltip({ children, label, position = "above" }: TooltipProps) {
     >
       {children}
       <div
-        className={`${
-          isHovered ? visibleClasses : hiddenClasses
-        } ${baseClasses} ${tooltipPositionClasses}`}
+        className={classNames(
+          `${isHovered ? visibleClasses : hiddenClasses}`,
+          baseClasses,
+          tooltipPositionClasses,
+          tooltipCenterClasses
+        )}
         onAnimationEnd={() => setIsHovered(false)}
       >
         {label}
