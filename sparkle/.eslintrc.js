a1689ff80e5ac3c78bddf4ca6acfe79f26b0574d
Henry Fontanier <h.fontanier@gmail.com>
2023-07-27 17:49:21 +0200

    sparkle: fix violations + enforce build and lint (#1012)

--- sparkle/.eslintrc.js
+++ sparkle/.eslintrc.js
@@ -1,47 +1,59 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier", "eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+  ],
   plugins: ["simple-import-sort"],
-  ignorePatterns: ["rollup.config.js", "eslint.js"],
+  ignorePatterns: [
+    "rollup.config.js",
+    "eslint.js",
+    "dist/",
+    "node_modules/",
+    "svgr.config.js",
+    "postcss.config.js",
+    "tailwind.config.js",
+  ],
   rules: {
-    "react/no-unescaped-entities": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "no-case-declarations": 0,
-    "react-hooks/rules-of-hooks": 0,
-    "react-hooks/exhaustive-deps": 0,
-    "@next/next/no-img-element": 0,
     "@typescript-eslint/no-floating-promises": "error",
-    "jsx-a11y/alt-text": 0,
-    "simple-import-sort/imports": ["error", {
-      groups: [
-      // Side effect imports.
-      ["^\\u0000"],
-      // Node.js builtins prefixed with `node:`.
-      ["^node:"],
-      // Packages.
-      // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-      ["^@?\\w"],
-      // @sparkle imports.
-      ["^@sparkle"],
-      // Absolute imports and other imports such as Vue-style `@/foo`.
-      // Anything not matched in another group.
-      ["^"],
-      // Relative imports.
-      // Anything that starts with a dot.
-      ["^\\."]]
-    }],
-    "simple-import-sort/exports": "error"
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Side effect imports.
+          ["^\\u0000"],
+          // Node.js builtins prefixed with `node:`.
+          ["^node:"],
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ["^@?\\w"],
+          // @sparkle imports.
+          ["^@sparkle"],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ["^"],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ["^\\."],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
   },
-  overrides: [{
-    files: ["*.jsx", "*.js", "*.ts", "*.tsx", "**/*.jsx"]
-  }],
+  overrides: [
+    {
+      files: ["*.jsx", "*.js", "*.ts", "*.tsx", "**/*.jsx"],
+    },
+  ],
   env: {
     browser: true,
     node: true,
     es6: true,
-    jest: true
+    jest: true,
   },
   parserOptions: {
     project: "./tsconfig.json",
-    tsconfigRootDir: __dirname
-  }
+    tsconfigRootDir: __dirname,
+  },
 };
--- sparkle/src/stories/Logo.stories.ts
+++ sparkle/src/stories/Logo.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { Logo } from "../index_with_tw_base";
 
 const meta = {
