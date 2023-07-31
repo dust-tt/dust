69de04508deb8a5cf983691ac2478aba70ab4909
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2023-07-31 10:30:40 +0200

    Updated landing (#1036)
    * Updated texts
    
    * Adding the new brand font
    
    * Applying new colors
    
    * Start adding updated illustrations
    
    * last illustration
    
    * updating cloud
    
    * minor adjustments
    
    * fixing knowledge unbreakable space
    
    * Adding particle effect
    
    * Adding a new top bar
    
    * Adding animations at scroll
    
    * adjusting settings
    
    * Fix lint
    
    * Move components out of pages
    
    * Remove debug & ts config for three
    
    * Pick commit ledgibility adjustment from Ed
    
    ---------
    
    Co-authored-by: édouard wautier <edouardwautier@mbp-de-edouard.home>
    Co-authored-by: édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
    Co-authored-by: Daphné Popin <PopDaph@users.noreply.github.com>
    Co-authored-by: PopDaph <daphne.popin@gmail.com>

---
+++ front/components/home/scrollingHeader.tsx
@@ -0,0 +1,67 @@
+import classNames from "classnames";
+import React, { ReactNode, useEffect, useState } from "react";
+
+// Define your scroll limit here
+const SCROLL_LIMIT_1 = 12;
+
+interface ScrollingHeaderProps {
+  children: ReactNode;
+  showItemY?: number; // define here
+}
+
+const ScrollingHeader = ({
+  children,
+  showItemY = 300,
+}: ScrollingHeaderProps) => {
+  const [isScrolled1, setIsScrolled1] = useState(false);
+  const [isScrolled2, setIsScrolled2] = useState(false);
+
+  const checkScroll = () => {
+    setIsScrolled1(window.scrollY > SCROLL_LIMIT_1);
+    setIsScrolled2(window.scrollY > showItemY); // use here
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScroll);
+    return () => window.removeEventListener("scroll", checkScroll);
+  }, [showItemY]); // add here
+
+  useEffect(() => {
+    const invisibleFirstElements = document.querySelectorAll(".invisibleFirst");
+    invisibleFirstElements.forEach((element) => {
+      if (isScrolled2) {
+        element.classList.remove("opacity-0");
+      } else {
+        element.classList.add("opacity-0");
+      }
+    });
+  }, [isScrolled2]);
+
+  // CSS classes defined in strings
+  const baseClasses =
+    "fixed top-0 w-full transition-all duration-500 ease-out border-b z-50";
+  const idleClasses = "h-24 border-transparent";
+  const scrolledClasses = "h-16 border-b-slate-700 backdrop-blur-lg";
+
+  // Combine them depending on state
+  const combinedClasses = classNames(
+    baseClasses,
+    isScrolled1 ? scrolledClasses : idleClasses
+  );
+
+  const invisibleFirstClasses = classNames(
+    "invisibleFirst absolute bottom-0 left-0 right-0 top-0 z-10 bg-slate-800",
+    isScrolled2 ? "opacity-60" : "opacity-0"
+  );
+
+  return (
+    <div className={combinedClasses}>
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-20">
+        {children}
+      </div>
+      <div className={invisibleFirstClasses} />
+    </div>
+  );
+};
+
+export default ScrollingHeader;
