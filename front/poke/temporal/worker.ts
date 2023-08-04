2518529db46802d719919d1956f31fb00d912a97
Henry Fontanier <h.fontanier@gmail.com>
2023-08-04 09:48:32 +0200

    scaffolding poke worker

---
+++ front/poke/temporal/worker.ts
@@ -0,0 +1,27 @@
+import { Context } from "@temporalio/activity";
+import { Worker } from "@temporalio/worker";
+
+import { getTemporalWorkerConnection } from "@app/lib/temporal";
+import { ActivityInboundLogInterceptor } from "@app/lib/temporal_monitoring";
+import logger from "@app/logger/logger";
+import * as activities from "@app/poke/temporal/activities";
+
+export async function runPokeWorker() {
+  const { connection, namespace } = await getTemporalWorkerConnection();
+  const worker = await Worker.create({
+    workflowsPath: require.resolve("./workflows"),
+    activities,
+    taskQueue: "poke-queue",
+    connection,
+    namespace,
+    interceptors: {
+      activityInbound: [
+        (ctx: Context) => {
+          return new ActivityInboundLogInterceptor(ctx, logger);
+        },
+      ],
+    },
+  });
+
+  await worker.run();
+}
