03d1792ba002e7216ac79c2a26da03fc032bb05b
Aric Lasry <lasry.aric@gmail.com>
2023-08-21 19:19:33 +0200

    Remove unused error

--- front/lib/error.ts
+++ front/lib/error.ts
@@ -64,11 +64,3 @@ export type APIErrorWithStatusCode = {
   api_error: APIError;
   status_code: number;
 };
-
-export class HTTPError extends Error {
-  status_code: number;
-  constructor(message: string, status_code: number) {
-    super(message);
-    this.status_code = status_code;
-  }
-}
