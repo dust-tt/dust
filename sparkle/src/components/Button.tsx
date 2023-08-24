7ed0300d47b45f7f4e497c506178b6bf838a33fb
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-08-24 15:08:40 +0200

    Updating the Button component so it makes use of the Icon size prop

--- sparkle/src/components/Button.tsx
+++ sparkle/src/components/Button.tsx
@@ -2,7 +2,7 @@ import React, { ComponentType, MouseEvent } from "react";
 
 import { classNames } from "@sparkle/lib/utils";
 
-import { Icon } from "./Icon";
+import { Icon, IconProps } from "./Icon";
 import { Tooltip } from "./Tooltip";
 
 export type ButtonProps = {
@@ -24,19 +24,13 @@ export type ButtonProps = {
 const sizeClasses = {
   xs: "s-gap-x-1 s-px-3 s-py-1.5 s-text-xs s-font-semibold",
   sm: "s-gap-x-1 s-px-4 s-py-2 s-text-sm s-font-semibold",
-  md: "s-gap-x-1.5 s-px-5 s-py-3 s-text-sm s-font-bold",
+  md: "s-gap-x-1.5 s-px-5 s-py-3 s-text-base s-font-bold",
 };
 
 const containerClasses = {
   xs: "s-px-0.5",
   sm: "s-px-1",
   md: "s-px-1",
-};
-
-const iconSizeClasses = {
-  xs: "s-h-4 s-w-4",
-  sm: "s-h-5 s-w-5",
-  md: "s-h-5 s-w-5",
 };
 
 const typeClasses = {
@@ -65,8 +59,8 @@ const typeClasses = {
     },
   },
   secondary: {
-    base: "s-text-action-500 s-border-structure-300 s-bg-structure-0",
-    hover: "hover:s-bg-action-50 hover:s-border-action-300",
+    base: "s-text-action-500 s-border-structure-200 s-bg-structure-0",
+    hover: "hover:s-bg-action-50 hover:s-border-action-200",
     active: "active:s-bg-action-100 active:s-border-action-500",
     dark: {
       base: "dark:s-text-action-500-dark dark:s-border-structure-300-dark dark:s-bg-structure-50-dark",
@@ -77,8 +71,8 @@ const typeClasses = {
     },
   },
   secondaryWarning: {
-    base: "s-text-warning-500 s-border-structure-300 s-bg-structure-0",
-    hover: "hover:s-bg-warning-50 hover:s-border-warning-300",
+    base: "s-text-warning-500 s-border-structure-200 s-bg-structure-0",
+    hover: "hover:s-bg-warning-50 hover:s-border-warning-200",
     active: "active:s-bg-warning-100 active:s-border-warning-500",
     dark: {
       base: "dark:s-text-warning-500-dark dark:s-border-structure-300-dark dark:s-bg-structure-50-dark",
@@ -89,9 +83,9 @@ const typeClasses = {
     },
   },
   tertiary: {
-    base: "s-text-element-700 s-border-structure-300 s-bg-structure-0",
+    base: "s-text-element-800 s-border-structure-200 s-bg-structure-0",
     hover:
-      "hover:s-text-element-800 hover:s-bg-action-50 hover:s-border-action-300",
+      "hover:s-text-element-700 hover:s-bg-action-50 hover:s-border-action-200",
     active: "active:s-bg-action-100 active:s-border-action-500",
     dark: {
       base: "dark:s-text-element-700-dark dark:s-border-structure-300-dark dark:s-bg-structure-50-dark",
@@ -126,7 +120,6 @@ export function Button({
     className
   );
 
-  const iconClasses = classNames(iconSizeClasses[size]);
   const finalContainerClasses = classNames(containerClasses[size]);
 
   return labelVisible ? (
@@ -137,7 +130,7 @@ export function Button({
       disabled={disabled}
       aria-label={label}
     >
-      {icon && <Icon IconComponent={icon} className={iconClasses} />}
+      {icon && <Icon IconComponent={icon} size={size as IconProps["size"]} />}
       <div className={finalContainerClasses}>{label}</div>
     </button>
   ) : (
@@ -149,7 +142,7 @@ export function Button({
         disabled={disabled}
         aria-label={label}
       >
-        {icon && <Icon IconComponent={icon} className={iconClasses} />}
+        {icon && <Icon IconComponent={icon} size={size as IconProps["size"]} />}
       </button>
     </Tooltip>
   );
