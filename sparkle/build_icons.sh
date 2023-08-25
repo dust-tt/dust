2474dee285122a39f4a33dff215863c8c9801ac1
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-08-25 17:28:26 +0200

    Adding eslint fixing to sh command

--- sparkle/build_icons.sh
+++ sparkle/build_icons.sh
@@ -5,3 +5,6 @@ rm -rf src/icons/solid
 
 npx @svgr/cli --out-dir src/icons/stroke/ src/icons/src/stroke/
 npx @svgr/cli --out-dir src/icons/solid/ src/icons/src/solid/
+
+npx eslint --fix src/icons/stroke
+npx eslint --fix src/icons/solid
