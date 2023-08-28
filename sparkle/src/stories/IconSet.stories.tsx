21965342983aea6e5b861b8c28df9eac7e0ffdd7
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-08-28 15:15:43 +0200

    creating an icon set gallery

---
+++ sparkle/src/stories/IconSet.stories.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import * as SolidIcons from "../icons/solid";
+import * as StrokeIcons from "../icons/stroke";
+import { Icon } from "../index_with_tw_base";
+
+export default {
+  title: "Assets/Icons",
+};
+
+const gridStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(160px, 1fr))",
+  gap: "48px 16px",
+};
+const itemStyle = {
+  marginTop: "12px",
+  textOverflow: "ellipsis",
+  overflow: "hidden", // Add this
+  whiteSpace: "nowrap", // Add this
+  textAlign: "left",
+  width: "100%",
+};
+
+export const SolidIconSet = () => (
+  <div style={gridStyle}>
+    {Object.entries(SolidIcons).map(([iconName, IconComponent]) => {
+      const CurrentIcon = IconComponent.default
+        ? IconComponent.default
+        : IconComponent;
+      return (
+        <div key={iconName}>
+          <Icon visual={CurrentIcon} size="md" />
+          <div style={itemStyle}>{iconName}</div>
+        </div>
+      );
+    })}
+  </div>
+);
+
+export const StrokeIconSet = () => (
+  <div style={gridStyle}>
+    {Object.entries(StrokeIcons).map(([iconName, IconComponent]) => {
+      const CurrentIcon = IconComponent.default
+        ? IconComponent.default
+        : IconComponent;
+      return (
+        <div key={iconName} style={{ textAlign: "center" }}>
+          <Icon visual={CurrentIcon} size="md" />
+          <div style={itemStyle}>{iconName}</div>
+        </div>
+      );
+    })}
+  </div>
+);
