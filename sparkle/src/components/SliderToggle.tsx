395c8b036a435782d1336af9dc62b7f18c0e68e9
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2023-08-29 18:29:35 +0200

    Slider toggle (#1166)
    * Adding new success semantic colors
    
    * creating the new slidertoggle component
    
    * Updating package version
    
    * Correcting typescript problems
    
    ---------
    
    Co-authored-by: édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>

---
+++ sparkle/src/components/SliderToggle.tsx
@@ -0,0 +1,59 @@
+import React, { MouseEventHandler } from "react";
+
+import { classNames } from "@sparkle/lib/utils";
+
+type SliderToggleProps = {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  disabled?: boolean;
+  selected?: boolean;
+};
+
+const baseClasses =
+  "s-box-border s-h-6 s-w-11 s-rounded-full s-border s-border-structure-300 dark:s-border-structure-300-dark s-transition-colors s-duration-300 s-ease-out s-cursor-pointer";
+
+const stateClasses = {
+  idle: "s-bg-structure-200 hover:s-bg-action-400",
+  selected: "s-bg-success-400 hover:s-bg-success-300",
+  disabled: "s-bg-structure-300 s-cursor-not-allowed",
+  dark: {
+    idle: "dark:s-bg-structure-200-dark dark:hover:s-bg-action-400",
+    selected: "dark:s-bg-success-400-dark",
+    disabled: "dark:s-bg-structure-300-dark",
+  },
+};
+
+export function SliderToggle({
+  onClick,
+  disabled = false,
+  className = "",
+  selected = false,
+}: SliderToggleProps) {
+  const combinedStateClasses = classNames(
+    selected ? stateClasses.selected : stateClasses.idle,
+    disabled ? stateClasses.disabled : "",
+    selected ? stateClasses.dark.selected : stateClasses.dark.idle,
+    disabled ? stateClasses.dark.disabled : ""
+  );
+
+  const SliderToggleContent = (
+    <div
+      onClick={(e) => {
+        if (!disabled) {
+          onClick?.(e); // Run passed onClick event
+        }
+      }}
+      className={classNames(className, baseClasses, combinedStateClasses)}
+    >
+      <div
+        id="cursor"
+        className={classNames(
+          "s-m-px s-h-5 s-w-5 s-transform s-rounded-full s-bg-white s-drop-shadow s-transition-transform s-duration-300 s-ease-out",
+          selected ? "s-translate-x-full" : ""
+        )}
+      />
+    </div>
+  );
+
+  return SliderToggleContent;
+}
