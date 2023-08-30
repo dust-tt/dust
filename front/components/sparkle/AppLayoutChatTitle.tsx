6c04fd7b347f2fa9a6bba9eec5f1984769ccb1ad
Stanislas Polu <spolu@proton.me>
2023-08-30 17:36:36 +0200

    use Dialog instead

--- front/components/sparkle/AppLayoutChatTitle.tsx
+++ front/components/sparkle/AppLayoutChatTitle.tsx
@@ -6,7 +6,7 @@ import {
   SliderToggle,
   TrashIcon,
 } from "@dust-tt/sparkle";
-import { Popover, Transition } from "@headlessui/react";
+import { Dialog, Popover, Transition } from "@headlessui/react";
 import React, { Fragment, useEffect, useState } from "react";
 
 import { classNames } from "@app/lib/utils";
@@ -36,6 +36,8 @@ export function AppLayoutChatTitle({
     }, 1000);
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex h-full flex-row items-center">
       <div className="flex flex-initial font-bold">
@@ -64,62 +66,76 @@ export function AppLayoutChatTitle({
         )}
 
         {!readOnly && (
-          <Popover as="div" className="relative">
-            <Popover.Button as="div" className="">
-              <Button
-                size="sm"
-                label="Share"
-                icon={ArrowUpOnSquareIcon}
-                type="secondary"
-              />
-            </Popover.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="transform -translate-y-4 opacity-0 scale-100"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Popover.Panel className="absolute right-0 z-10 mt-2 w-64 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-slate-100 focus:outline-none">
-                <div className="flex flex-col gap-y-3 px-3 py-3">
-                  <div className="flex flex-row">
-                    <div className="flex flex-col">
-                      <div className="text-sm font-medium text-element-800">
-                        Share with the Workspace
-                      </div>
-                      <div className="text-xs font-normal text-element-700">
-                        Make visible to all your co-workers in "All
-                        conversations".
-                      </div>
-                    </div>
-                    <div>
-                      <SliderToggle
-                        selected={visibility === "workspace"}
-                        onClick={() => {
-                          onUpdateVisibility(
-                            visibility === "workspace" ? "private" : "workspace"
-                          );
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex justify-center">
-                    <Button
-                      type="secondary"
-                      size="sm"
-                      label={copyLinkSuccess ? "Copied!" : "Copy the link"}
-                      icon={
-                        copyLinkSuccess ? ClipboardCheckIcon : LinkStrokeIcon
-                      }
-                      onClick={handleClick}
-                    />
+          <>
+            <Button
+              size="sm"
+              label="Share"
+              icon={ArrowUpOnSquareIcon}
+              type="secondary"
+              onClick={() => setIsOpen(true)}
+            />
+
+            <Transition show={isOpen} as={Fragment}>
+              <Dialog onClose={() => setIsOpen(false)}>
+                <div className="fixed inset-0">
+                  <div className="flex w-full flex-col items-end">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="transform -translate-y-4 opacity-0 scale-100"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Dialog.Panel className="mx-16 mt-16 w-64 rounded-md bg-white py-1 shadow-lg ring-1 ring-slate-100 focus:outline-none">
+                        <div className="flex flex-col gap-y-3 px-3 py-3">
+                          <div className="flex flex-row">
+                            <div className="flex flex-col">
+                              <div className="text-sm font-medium text-element-800">
+                                Share with the Workspace
+                              </div>
+                              <div className="text-xs font-normal text-element-700">
+                                Make visible to all your co-workers in "All
+                                conversations".
+                              </div>
+                            </div>
+                            <div>
+                              <SliderToggle
+                                selected={visibility === "workspace"}
+                                onClick={() => {
+                                  onUpdateVisibility(
+                                    visibility === "workspace"
+                                      ? "private"
+                                      : "workspace"
+                                  );
+                                }}
+                              />
+                            </div>
+                          </div>
+                          <div className="flex justify-center">
+                            <Button
+                              type="secondary"
+                              size="sm"
+                              label={
+                                copyLinkSuccess ? "Copied!" : "Copy the link"
+                              }
+                              icon={
+                                copyLinkSuccess
+                                  ? ClipboardCheckIcon
+                                  : LinkStrokeIcon
+                              }
+                              onClick={handleClick}
+                            />
+                          </div>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
                   </div>
                 </div>
-              </Popover.Panel>
+              </Dialog>
             </Transition>
-          </Popover>
+          </>
         )}
       </div>
     </div>
