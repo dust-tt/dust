399fb5d6dfb7ff555aa9d7d785956438440c861c
Henry Fontanier <h.fontanier@gmail.com>
2023-09-01 10:07:35 +0200

    enh: remove asSuperUser param from fromSession (#1214)

--- front/pages/poke/[wId]/memberships.tsx
+++ front/pages/poke/[wId]/memberships.tsx
@@ -38,11 +38,7 @@ export const getServerSideProps: GetServerSideProps<{
     };
   }
 
-  const auth = await Authenticator.fromSession(
-    session,
-    wId,
-    true // asSuperUser
-  );
+  const auth = await Authenticator.fromSuperUserSession(session, wId);
 
   const workspace = auth.workspace();
 
