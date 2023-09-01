edd85076e524468306912b4bd7effa9a313f0975
Stanislas Polu <spolu@proton.me>
2023-09-01 11:12:49 +0200

    update script.rs

--- core/src/deno/script.rs
+++ core/src/deno/script.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use deno_core::{op, Extension, JsRuntime, OpState};
+use deno_core::{op, Extension, JsRuntime, Op, OpState};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::borrow::Cow;
@@ -37,9 +37,11 @@ impl Script {
     }
 
     fn create_script(js_code: String) -> Result<Self> {
-        let ext = Extension::builder("script")
-            .ops(vec![(op_return::decl())])
-            .build();
+        let ext = Extension {
+            name: "script",
+            ops: Cow::Owned(vec![(op_return::DECL)]),
+            ..Default::default()
+        };
 
         let options = deno_core::RuntimeOptions {
             module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
@@ -102,10 +104,6 @@ impl Script {
                 tokio::time::sleep(timeout).await;
                 handle.terminate_execution();
             });
-            // thread::spawn(move || {
-            //     thread::sleep(timeout);
-            //     handle.terminate_execution();
-            // });
         }
 
         // syncing ops is required cause they sometimes change while preparing the engine
