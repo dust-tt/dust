2cf54ed3558c406124b7e9da7fb3201d3d96e015
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-09-05 16:49:44 +0200

    Position of the menu on custom buttons

--- sparkle/src/stories/DropdownMenu.stories.tsx
+++ sparkle/src/stories/DropdownMenu.stories.tsx
@@ -2,9 +2,9 @@ import type { Meta } from "@storybook/react";
 import React from "react";
 
 import {
+  Avatar,
   ChatBubbleBottomCenterTextIcon,
   DropdownMenu,
-  Icon,
   PlanetIcon,
 } from "../index_with_tw_base";
 
@@ -27,7 +27,7 @@ export const DropdownExample = () => (
         </DropdownMenu.Items>
       </DropdownMenu>
     </div>
-    <div className="s-h-8" />
+    <div className="s-h-12" />
     <div className="s-flex s-gap-6">
       <div className="s-text-sm">Top right menu</div>
       <DropdownMenu>
@@ -38,7 +38,7 @@ export const DropdownExample = () => (
         </DropdownMenu.Items>
       </DropdownMenu>
     </div>
-    <div className="s-h-8" />
+    <div className="s-h-12" />
     <div className="s-flex s-gap-6">
       <div className="s-text-sm">Top left menu</div>
       <DropdownMenu>
@@ -53,7 +53,7 @@ export const DropdownExample = () => (
         </DropdownMenu.Items>
       </DropdownMenu>
     </div>
-    <div className="s-h-8" />
+    <div className="s-h-12" />
     <div className="s-flex s-gap-6">
       <div className="s-text-sm">Bottom left menu</div>
       <DropdownMenu>
@@ -64,7 +64,7 @@ export const DropdownExample = () => (
         </DropdownMenu.Items>
       </DropdownMenu>
     </div>
-    <div className="s-h-8" />
+    <div className="s-h-12" />
     <div className="s-flex s-gap-6">
       <div className="s-text-sm">Bottom right menu</div>
       <DropdownMenu>
@@ -75,22 +75,7 @@ export const DropdownExample = () => (
         </DropdownMenu.Items>
       </DropdownMenu>
     </div>
-    <div className="s-h-8" />
-    <div className="s-flex s-gap-6">
-      <div className="s-text-sm">
-        Custom button menu (Takes anything as button)
-      </div>
-      <DropdownMenu>
-        <DropdownMenu.Button tooltip="This is anything">
-          <Icon visual={ChatBubbleBottomCenterTextIcon} size="sm" />
-        </DropdownMenu.Button>
-        <DropdownMenu.Items>
-          <DropdownMenu.Item label="item 1" href="#" />
-          <DropdownMenu.Item label="item 2" href="#" />
-        </DropdownMenu.Items>
-      </DropdownMenu>
-    </div>
-    <div className="s-h-8" />
+    <div className="s-h-12" />
     <div className="s-flex s-gap-6">
       <div className="s-text-sm">Disabled</div>
       <DropdownMenu>
@@ -100,6 +85,19 @@ export const DropdownExample = () => (
           disabled
         />
         <DropdownMenu.Items>
+          <DropdownMenu.Item label="item 1" href="#" />
+          <DropdownMenu.Item label="item 2" href="#" />
+        </DropdownMenu.Items>
+      </DropdownMenu>
+    </div>
+    <div className="s-h-12" />
+    <div className="s-flex s-gap-6">
+      <div className="s-text-sm">With custom button</div>
+      <DropdownMenu>
+        <DropdownMenu.Button>
+          <Avatar name="Dust" size="lg" onClick={() => ""} />
+        </DropdownMenu.Button>
+        <DropdownMenu.Items origin="topRight">
           <DropdownMenu.Item label="item 1" href="#" />
           <DropdownMenu.Item label="item 2" href="#" />
         </DropdownMenu.Items>
