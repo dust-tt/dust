1e7f78b2e9f1728d2b4775611c917c517c7d8e60
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-09-05 14:15:18 +0200

    fixing Item import

--- sparkle/src/_index.ts
+++ sparkle/src/_index.ts
@@ -10,8 +10,8 @@ export { SliderToggle };
 import { Tab } from "./components/Tab";
 export { Tab };
 
-import { Item, ItemSectionHeader, ListItem } from "./components/Item";
-export { Item, ItemSectionHeader, ListItem };
+import { Item } from "./components/Item";
+export { Item };
 
 import { DropdownMenu } from "./components/DropdownMenu";
 export { DropdownMenu };
