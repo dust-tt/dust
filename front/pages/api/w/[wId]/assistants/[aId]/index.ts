eedbda8682d4578009a843d28e64458c2d97de8d
PopDaph <daphne.popin@gmail.com>
2023-09-11 14:13:54 +0200

    Fix error message

--- front/pages/api/w/[wId]/assistants/[aId]/index.ts
+++ front/pages/api/w/[wId]/assistants/[aId]/index.ts
@@ -52,8 +52,7 @@ async function handler(
           status_code: 404,
           api_error: {
             type: "agent_configuration_not_found",
-            message:
-              "The agent configuration you're trying to access was not found.",
+            message: "The Assistant you're trying to access was not found.",
           },
         });
       }
