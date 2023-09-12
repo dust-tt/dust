441671cc25dbfefdf92eb8dd36da287e36ca4a63
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-09-12 13:35:38 +0200

    Adding Springboard icon for apple devices

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -84,6 +84,45 @@ export default function Home({
       <Head>
         <title>Dust - Secure AI assistant with your company's knowledge</title>
         <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="static/AppIcon.png" />
+
+        <meta name="apple-mobile-web-app-title" content="Dust" />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="static/AppIcon_60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="static/AppIcon_76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="static/AppIcon_120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="static/AppIcon_152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="static/AppIcon_167.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="static/AppIcon_180.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="228x228"
+          href="static/AppIcon_228.png"
+        />
+
         <meta
           id="meta-description"
           name="description"
