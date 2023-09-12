b176b3f0f76ee768699c87d418814a144ece16be
Stanislas Polu <spolu@proton.me>
2023-09-12 16:37:47 +0200

    iteration on mentions and handleSubmit

--- front/pages/w/[wId]/assistant/[cId]/index.tsx
+++ front/pages/w/[wId]/assistant/[cId]/index.tsx
@@ -6,12 +6,13 @@ import AssistantInputBar from "@app/components/assistant/InputBar";
 import AppLayout from "@app/components/sparkle/AppLayout";
 import { subNavigationLab } from "@app/components/sparkle/navigation";
 import { Authenticator, getSession, getUserFromSession } from "@app/lib/auth";
+import { MentionType } from "@app/types/assistant/conversation";
 import { UserType, WorkspaceType } from "@app/types/user";
 
 const { GA_TRACKING_ID = "" } = process.env;
 
 export const getServerSideProps: GetServerSideProps<{
-  user: UserType | null;
+  user: UserType;
   owner: WorkspaceType;
   gaTrackingId: string;
   conversationId: string;
@@ -24,7 +25,7 @@ export const getServerSideProps: GetServerSideProps<{
   );
 
   const owner = auth.workspace();
-  if (!owner || !auth.isUser()) {
+  if (!owner || !auth.isUser() || !user) {
     return {
       redirect: {
         destination: "/",
@@ -49,6 +50,33 @@ export default function AssistantConversation({
   gaTrackingId,
   conversationId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const handleSubmit = async (input: string, mentions: MentionType[]) => {
+    // Create a new user message.
+    const mRes = await fetch(
+      `/api/w/${owner.sId}/assistant/conversations/${conversationId}/messages`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: input,
+          context: {
+            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            profilePictureUrl: user.image,
+          },
+          mentions,
+        }),
+      }
+    );
+
+    if (!mRes.ok) {
+      const data = await mRes.json();
+      window.alert(`Error creating message: ${data.error.message}`);
+      return;
+    }
+  };
+
   return (
     <AppLayout
       user={user}
@@ -61,10 +89,10 @@ export default function AssistantConversation({
         title="Welcome to Assistant"
         icon={ChatBubbleBottomCenterTextIcon}
       />
+      <Conversation owner={owner} conversationId={conversationId} />
       <div className="fixed bottom-0 left-0 right-0 z-20 flex-initial lg:left-80">
         <div className="mx-auto max-w-4xl pb-8">
-          <Conversation owner={owner} conversationId={conversationId} />
-          <AssistantInputBar onSubmit={() => console.log("Handle Submit")} />
+          <AssistantInputBar onSubmit={handleSubmit} />
         </div>
       </div>
     </AppLayout>
