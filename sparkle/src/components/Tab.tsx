ef08d1e5c77dd74407b4919cec1f56a33bede80b
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-09-13 13:20:20 +0200

    Animated tab works

--- sparkle/src/components/Tab.tsx
+++ sparkle/src/components/Tab.tsx
@@ -73,22 +73,37 @@ export function Tab({ tabs, onTabClick, className = "" }: TabsProps) {
   const { components } = React.useContext(SparkleContext);
 
   const rectangleRef = React.useRef<HTMLDivElement | null>(null);
-  const tabRefs = tabs.map(() => React.useRef<HTMLElement | null>(null));
+  const tabRefs = tabs.map(() => React.useRef<HTMLDivElement | null>(null));
+
+  const [lastClicked, setLastClicked] = React.useState<number | null>(null);
+
+  const handleTabClick = (
+    tabName: string,
+    event: MouseEvent<HTMLAnchorElement>
+  ) => {
+    const clickedTabIndex = tabs.findIndex((tab) => tab.label === tabName);
+    setLastClicked(clickedTabIndex);
+    onTabClick?.(tabName, event);
+  };
 
   React.useEffect(() => {
     const selectedTabIndex = tabs.findIndex((tab) => tab.current);
+
     if (selectedTabIndex !== -1) {
       const selectedTabRef = tabRefs[selectedTabIndex].current;
+      const selectedTab = tabs[selectedTabIndex];
+      const offsetLeft = selectedTab.hideLabel ? 24 : 16;
+      const offsetWidth = selectedTab.hideLabel ? 16 : 8;
+
       if (selectedTabRef) {
         const rect = selectedTabRef.getBoundingClientRect();
         if (rectangleRef.current) {
-          console.log(rect);
-          rectangleRef.current.style.left = `${rect.left}px`;
-          rectangleRef.current.style.width = `${rect.width}px`;
+          rectangleRef.current.style.left = `${rect.left - offsetLeft}px`;
+          rectangleRef.current.style.width = `${rect.width + offsetWidth}px`;
         }
       }
     }
-  }, [tabs]);
+  }, [tabs, lastClicked]);
 
   const renderTabs = () =>
     tabs.map((tab, i) => {
@@ -100,9 +115,7 @@ export function Tab({ tabs, onTabClick, className = "" }: TabsProps) {
         : iconClasses.default;
 
       const finalTabClasses = classNames(
-        "s-group s-justify-center s-flex s-text-sm s-font-semibold s-px-4 s-py-3 s-transition-all ease-out s-duration-400 s-whitespace-nowrap s-select-none",
-        "s-group s-justify-center s-flex s-text-sm s-font-semibold s-py-3 s-transition-all ease-out s-duration-400 s-whitespace-nowrap s-select-none",
-        tab.icon ? " s-pr-5 s-pl-4" : " s-px-5",
+        "s-group s-justify-center s-flex s-text-sm s-font-semibold s-py-4 s-transition-all ease-out s-duration-400 s-whitespace-nowrap s-select-none",
         tabStateClasses.base,
         tabStateClasses.hover,
         tabStateClasses.dark.base,
@@ -129,7 +142,7 @@ export function Tab({ tabs, onTabClick, className = "" }: TabsProps) {
           key={tab.label}
           className={finalTabClasses}
           aria-current={tab.current ? "page" : undefined}
-          onClick={(event) => onTabClick?.(tab.label ? tab.label : "", event)}
+          onClick={(event) => handleTabClick(tab.label, event)}
           href={tab.href || "#"}
         >
           <div
@@ -149,20 +162,24 @@ export function Tab({ tabs, onTabClick, className = "" }: TabsProps) {
       return tab.hideLabel ? (
         tab.label ? (
           <>
-            <div key={`tab-${i}`}>
+            <div key={`tab-${i}`} ref={tabRefs[i]}>
               <Tooltip label={tab.label}>{content}</Tooltip>
             </div>
             {tab.hasSeparator && <div className="s-flex s-h-full s-grow" />}
           </>
         ) : (
           <>
-            <div key={`tab-${i}`}>{content}</div>
+            <div key={`tab-${i}`} ref={tabRefs[i]}>
+              {content}
+            </div>
             {tab.hasSeparator && <div className="s-flex s-h-full s-grow" />}
           </>
         )
       ) : (
         <>
-          <div key={`tab-${i}`}>{content}</div>
+          <div key={`tab-${i}`} ref={tabRefs[i]}>
+            {content}
+          </div>
           {tab.hasSeparator && <div className="s-flex s-h-full s-grow" />}
         </>
       );
@@ -170,9 +187,12 @@ export function Tab({ tabs, onTabClick, className = "" }: TabsProps) {
 
   return (
     <div className="s-border-b s-border-structure-200 dark:s-border-structure-200-dark">
-      <nav className="s-relative -s-mb-px s-flex s-space-x-0" aria-label="Tabs">
+      <nav
+        className="s-relative -s-mb-px s-flex s-gap-4 s-space-x-0 s-px-4"
+        aria-label="Tabs"
+      >
         <div
-          className="ease-out s-duration-400 s-absolute s-bottom-0 s-h-0.5 s-bg-action-500 s-transition-all"
+          className="ease-out s-duration-400 s-absolute s-bottom-0 s-h-0.5 s-w-10 s-bg-action-500 s-transition-all"
           ref={rectangleRef}
         ></div>
         {renderTabs()}
