befe54c3093c153f1e0bcfd59575101006bf35b9
PopDaph <daphne.popin@gmail.com>
2023-09-14 13:10:35 +0200

    Handle edge cases

--- front/components/RenderMarkdown.tsx
+++ front/components/RenderMarkdown.tsx
@@ -8,6 +8,30 @@ import ReactMarkdown from "react-markdown";
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
+function addClosingBackticks(str: string): string {
+  // Regular expression to find either a single backtick or triple backticks
+  const regex = /(`{1,3})/g;
+  let singleBackticks = 0;
+  let tripleBackticks = 0;
+
+  // Search for all backticks in the string and update counts
+  let match;
+  while ((match = regex.exec(str)) !== null) {
+    if (match[1] === "```") {
+      tripleBackticks++;
+    } else if (match[1] === "`") {
+      singleBackticks++;
+    }
+  }
+  // Append closing backticks if needed
+  if (tripleBackticks % 2 !== 0) {
+    str += str.includes("\n") ? "\n```" : "```";
+  } else if (singleBackticks % 2 !== 0) {
+    str += "`";
+  }
+  return str;
+}
+
 export function RenderMarkdown({ content }: { content: string }) {
   return (
     <ReactMarkdown
@@ -15,10 +39,31 @@ export function RenderMarkdown({ content }: { content: string }) {
       components={{
         pre: PreBlock,
         code: CodeBlock,
+        a: LinkBlock,
+        p: React.Fragment,
       }}
     >
-      {content}
+      {addClosingBackticks(content)}
     </ReactMarkdown>
+  );
+}
+
+function LinkBlock({
+  href,
+  children,
+}: {
+  href?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="break-all text-blue-500 hover:underline"
+    >
+      {children}
+    </a>
   );
 }
 
@@ -36,17 +81,33 @@ function PreBlock({ children }: { children: React.ReactNode }) {
     }, 1000);
   };
 
+  const validContent =
+    Array.isArray(children) && children[0]
+      ? children[0].props.children[0]
+      : null;
+  let data = null;
+
+  if (!validContent) {
+    data =
+      Array.isArray(children) && children[0]
+        ? children[0].props?.node.data?.meta
+        : null;
+  }
+
   return (
-    <pre>
+    <pre className="max-w-3xl break-all rounded-md bg-slate-900 p-2 text-white">
+      {/** should not need class max-w-3xl */}
       <div className="relative">
-        <div className="absolute right-2 top-2 text-white">
+        <div className="absolute right-1 top-1 text-white">
           <IconButton
             variant="tertiary"
             icon={confirmed ? ClipboardCheckIcon : ClipboardIcon}
             onClick={handleClick}
           />
         </div>
-        {children}
+        <div className="overflow-auto pt-8">
+          {validContent ? children : data || children}
+        </div>
       </div>
     </pre>
   );
@@ -62,16 +123,22 @@ function CodeBlock({
   children?: React.ReactNode;
 }): JSX.Element {
   const match = /language-(\w+)/.exec(className || "");
-  return !inline && match ? (
+  let language = match ? match[1] : "text";
+  const supportedLanguages = ["js", "ts", "tsx", "json", "text", "python"];
+  if (!supportedLanguages.includes(language)) {
+    language = "text";
+  }
+
+  return !inline && language ? (
     <SyntaxHighlighter
       className="rounded-md"
       style={a11yDark}
-      language={match[1]}
+      language={language}
       PreTag="div"
     >
       {String(children).replace(/\n$/, "")}
     </SyntaxHighlighter>
   ) : (
-    <code className="bg-action-50">{children}</code>
+    <code className="rounded-md bg-action-50 p-1">{children}</code>
   );
 }
