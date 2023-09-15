68dfb02731b2dd41288b043a4204101673ac9fd2
Stanislas Polu <spolu@proton.me>
2023-09-15 14:59:52 +0200

    Optimize new conversation creation (#1486)
    * Optimize new conversation creation
    
    * allow posting a new message at conversation creation

--- front/pages/w/[wId]/assistant/new.tsx
+++ front/pages/w/[wId]/assistant/new.tsx
@@ -5,22 +5,33 @@ import {
   PageHeader,
   RobotIcon,
 } from "@dust-tt/sparkle";
+import * as t from "io-ts";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
+import Conversation from "@app/components/assistant/conversation/Conversation";
+import { ConversationTitle } from "@app/components/assistant/conversation/ConversationTitle";
 import { FixedAssistantInputBar } from "@app/components/assistant/conversation/InputBar";
 import AppLayout from "@app/components/sparkle/AppLayout";
 import { subNavigationLab } from "@app/components/sparkle/navigation";
 import { Authenticator, getSession, getUserFromSession } from "@app/lib/auth";
-import { PostConversationsResponseBody } from "@app/pages/api/w/[wId]/assistant/conversations";
-import { MentionType } from "@app/types/assistant/conversation";
+import type {
+  PostConversationsRequestBodySchema,
+  PostConversationsResponseBody,
+} from "@app/pages/api/w/[wId]/assistant/conversations";
+import {
+  ConversationType,
+  MentionType,
+} from "@app/types/assistant/conversation";
 import { UserType, WorkspaceType } from "@app/types/user";
 
-const { GA_TRACKING_ID = "" } = process.env;
+const { URL = "", GA_TRACKING_ID = "" } = process.env;
 
 export const getServerSideProps: GetServerSideProps<{
   user: UserType;
   owner: WorkspaceType;
+  baseUrl: string;
   gaTrackingId: string;
 }> = async (context) => {
   const session = await getSession(context.req, context.res);
@@ -44,6 +55,7 @@ export const getServerSideProps: GetServerSideProps<{
     props: {
       user,
       owner,
+      baseUrl: URL,
       gaTrackingId: GA_TRACKING_ID,
     },
   };
@@ -60,21 +72,36 @@ export function AssistantHelper({ children }: { children: React.ReactNode }) {
 export default function AssistantNew({
   user,
   owner,
+  baseUrl,
   gaTrackingId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
 
+  const [conversation, setConversation] = useState<ConversationType | null>(
+    null
+  );
+
   const handleSubmit = async (input: string, mentions: MentionType[]) => {
-    // Create new conversation.
+    const body: t.TypeOf<typeof PostConversationsRequestBodySchema> = {
+      title: null,
+      visibility: "unlisted",
+      message: {
+        content: input,
+        context: {
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          profilePictureUrl: user.image,
+        },
+        mentions,
+      },
+    };
+
+    // Create new conversation and post the initial message at the same time.
     const cRes = await fetch(`/api/w/${owner.sId}/assistant/conversations`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        title: null,
-        visibility: "unlisted",
-      }),
+      body: JSON.stringify(body),
     });
 
     if (!cRes.ok) {
@@ -86,74 +113,74 @@ export default function AssistantNew({
     const conversation = ((await cRes.json()) as PostConversationsResponseBody)
       .conversation;
 
-    // Create a new user message.
-    const mRes = await fetch(
-      `/api/w/${owner.sId}/assistant/conversations/${conversation.sId}/messages`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          content: input,
-          context: {
-            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-            profilePictureUrl: user.image,
-          },
-          mentions,
-        }),
-      }
-    );
+    // We use this to clear the UI start rendering the conversation immediately to give an
+    // impression of instantaneity.
+    setConversation(conversation);
 
-    if (!mRes.ok) {
-      const data = await mRes.json();
-      window.alert(`Error creating message: ${data.error.message}`);
-      return;
-    }
-
-    await router.push(`/w/${owner.sId}/assistant/${conversation.sId}`);
+    // We start the push before creating the message to optimize for instantaneity as well.
+    void router.push(`/w/${owner.sId}/assistant/${conversation.sId}`);
   };
 
   return (
     <AppLayout
       user={user}
       owner={owner}
+      isWideMode={conversation ? true : false}
       gaTrackingId={gaTrackingId}
       topNavigationCurrent="lab"
       subNavigation={subNavigationLab({ owner, current: "assistant" })}
+      titleChildren={
+        conversation && (
+          <ConversationTitle
+            title={conversation.title || ""}
+            shareLink={`${baseUrl}/w/${owner.sId}/assistant/${conversation.sId}`}
+            // onDelete={() => {}}
+            onUpdateVisibility={() => {
+              return;
+            }}
+            visibility={"unlisted"}
+          />
+        )
+      }
     >
-      <PageHeader
-        title="Welcome to Assistant"
-        icon={ChatBubbleBottomCenterTextIcon}
-      />
-      <AssistantHelper>
-        <div className="mb-8 text-lg font-bold">
-          Get started with{" "}
-          <Logo className="inline-block w-14 pb-0.5 pl-1"></Logo>
-        </div>
-        <p className="my-4 text-sm text-element-800">
-          Lorem ispum dolor sit amet, consectetur adipiscing elit. You have
-          access to multiple assistants, each with their own set of skills.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <p className="my-4 text-sm text-element-800">
-          Assistants you have access to:{" "}
-          <span className="font-bold italic">@gpt3.5-turbo</span>, and{" "}
-          <span className="font-bold italic">@claude-instant</span>.
-        </p>
-        {["admin", "builder"].includes(owner.role) && (
-          <div className="pt-4 text-center">
-            <Button
-              variant={"primary"}
-              icon={RobotIcon}
-              label="Configure new Custom Assistants"
-              onClick={() => {
-                void router.push(`/w/${owner.sId}/builder/assistants`);
-              }}
-            />
-          </div>
-        )}
-      </AssistantHelper>
+      {!conversation ? (
+        <>
+          <PageHeader
+            title="Welcome to Assistant"
+            icon={ChatBubbleBottomCenterTextIcon}
+          />
+          <AssistantHelper>
+            <div className="mb-8 text-lg font-bold">
+              Get started with{" "}
+              <Logo className="inline-block w-14 pb-0.5 pl-1"></Logo>
+            </div>
+            <p className="my-4 text-sm text-element-800">
+              Lorem ispum dolor sit amet, consectetur adipiscing elit. You have
+              access to multiple assistants, each with their own set of skills.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className="my-4 text-sm text-element-800">
+              Assistants you have access to:{" "}
+              <span className="font-bold italic">@gpt3.5-turbo</span>, and{" "}
+              <span className="font-bold italic">@claude-instant</span>.
+            </p>
+            {["admin", "builder"].includes(owner.role) && (
+              <div className="pt-4 text-center">
+                <Button
+                  variant={"primary"}
+                  icon={RobotIcon}
+                  label="Configure new Custom Assistants"
+                  onClick={() => {
+                    void router.push(`/w/${owner.sId}/builder/assistants`);
+                  }}
+                />
+              </div>
+            )}
+          </AssistantHelper>
+        </>
+      ) : (
+        <Conversation owner={owner} conversationId={conversation.sId} />
+      )}
 
       <FixedAssistantInputBar owner={owner} onSubmit={handleSubmit} />
     </AppLayout>
