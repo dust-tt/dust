10d6ee3276ee145745c47728dd765a0034c33490
Aric Lasry <lasry.aric@gmail.com>
2023-09-18 14:52:52 +0200

    Clean up

--- front/lib/api/assistant/conversation.ts
+++ front/lib/api/assistant/conversation.ts
@@ -462,7 +462,7 @@ export async function* postUserMessage(
                 mention.configurationId
               );
               if (!configuration) {
-                throw new Error(`Configuration not found 1`);
+                throw new Error(`Configuration not found`);
               }
 
               await Mention.create(
@@ -857,7 +857,7 @@ export async function* editUserMessage(
                 mention.configurationId
               );
               if (!configuration) {
-                throw new Error(`Configuration not foundzz 2`);
+                throw new Error(`Configuration not found`);
               }
 
               await Mention.create(
