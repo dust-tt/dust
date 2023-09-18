36a6b768929fb1c3277b2e1a74488736b5427b9c
Stanislas Polu <spolu@proton.me>
2023-09-18 20:04:20 +0200

    Fix unused arguments and imports (#1550)

--- sparkle/src/stories/ContextItem.stories.tsx
+++ sparkle/src/stories/ContextItem.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta } from "@storybook/react";
 import React from "react";
 
 import { Drive, Github, Notion, Slack } from "@sparkle/logo/platforms";
