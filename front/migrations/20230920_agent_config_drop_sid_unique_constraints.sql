bfe606629bb8287490abea41dee6f50f81e0f298
Henry Fontanier <h.fontanier@gmail.com>
2023-09-20 13:44:00 +0200

    add migration to drop constraints

---
+++ front/migrations/20230920_agent_config_drop_sid_unique_constraints.sql
@@ -0,0 +1,17 @@
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key";
+
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key1";
+
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key2";
+
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key3";
+
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key4";
+
+alter table
+    "agent_configurations" drop constraint "agent_configurations_sId_key5";
