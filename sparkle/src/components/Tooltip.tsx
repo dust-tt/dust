6fe5f1604925d9aa4ed842b41dae8cf179e0e042
Stanislas Polu <spolu@proton.me>
2023-09-21 18:08:42 +0200

    Tooltip content (#1640)
    * Tooltip content
    
    * nit
    
    * contentChildren
    
    * nits

--- sparkle/src/components/Tooltip.tsx
+++ sparkle/src/components/Tooltip.tsx
@@ -4,11 +4,17 @@ import { classNames } from "@sparkle/lib/utils";
 
 export interface TooltipProps {
   children: React.ReactNode;
-  label: string;
+  label?: string;
   position?: "above" | "below";
+  contentChildren?: React.ReactNode;
 }
 
-export function Tooltip({ children, label, position = "above" }: TooltipProps) {
+export function Tooltip({
+  children,
+  label,
+  position = "above",
+  contentChildren,
+}: TooltipProps) {
   const [isHovered, setIsHovered] = useState(false);
   const [timerId, setTimerId] = useState<number | null>(null);
 
@@ -33,13 +39,15 @@ export function Tooltip({ children, label, position = "above" }: TooltipProps) {
   }, [timerId]);
 
   const baseClasses =
-    "s-absolute s-whitespace-nowrap s-z-10 s-px-3 s-py-2 s-text-sm s-rounded-xl s-border s-shadow-md s-transition-all s-duration-500 s-ease-out s-transform s-bg-structure-0 dark:s-bg-structure-0-dark s-text-element-700 dark:s-text-element-700-dark";
+    "s-absolute s-z-10 s-px-3 s-py-2 s-text-sm s-rounded-xl s-border s-shadow-md s-transition-all s-duration-500 s-ease-out s-transform s-bg-structure-0 dark:s-bg-structure-0-dark s-text-element-700 dark:s-text-element-700-dark";
   const hiddenClasses = "s-translate-y-2 s-opacity-0 s-pointer-events-none"; // Added s-pointer-events-none
   const visibleClasses = "-s-translate-y-0 s-opacity-100";
 
   const tooltipCenterClasses = "s-left-1/2 -s-translate-x-1/2";
   const tooltipPositionClasses =
     position === "above" ? "s-bottom-full s-mb-2" : "s-top-full s-mt-2";
+
+  const labelClasses = "s-whitespace-no-wrap";
 
   return (
     <div
@@ -57,7 +65,7 @@ export function Tooltip({ children, label, position = "above" }: TooltipProps) {
         )}
         onAnimationEnd={() => setIsHovered(false)}
       >
-        {label}
+        {contentChildren || <span className={labelClasses}>{label}</span>}
       </div>
     </div>
   );
--- sparkle/src/stories/Tooltip.stories.tsx
+++ sparkle/src/stories/Tooltip.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
 
 import { Tooltip } from "../index_with_tw_base";
 
@@ -25,3 +26,26 @@ export const TooltipBelow: Story = {
     position: "below",
   },
 };
+
+export const ToolTipWithContent = () => (
+  <div className="s-pl-32">
+    <Tooltip
+      contentChildren={
+        <div className="s-w-64">
+          <div className="s-flex s-flex-col s-gap-y-2 s-break-words">
+            <div className="s-flex s-font-semibold">
+              This is a more complex tooltip:
+            </div>
+            <div className="s-flex s-flex-wrap s-break-words">
+              It has some content that is long here and describes something
+              about the content associated with this.
+            </div>
+          </div>
+        </div>
+      }
+      position="below"
+    >
+      Hover me
+    </Tooltip>
+  </div>
+);
