47f93da3019a303971fff4bb7210da6dca1d3866
Philippe Rolet <pr@dust.tt>
2023-09-21 18:26:14 +0200

    Remove tooltips on mobile screens (#1641)
    Current tooltips break mobile display
    
    After 15mn exploration, no trivial fix in sight
    
    As per discussion with Ed, tooltips are virtually useless on mobile,
    so we might as well remove them, so that they only appear on desktop

--- sparkle/src/components/Tooltip.tsx
+++ sparkle/src/components/Tooltip.tsx
@@ -42,7 +42,7 @@ export function Tooltip({
     "s-absolute s-z-10 s-px-3 s-py-2 s-text-sm s-rounded-xl s-border s-shadow-md s-transition-all s-duration-500 s-ease-out s-transform s-bg-structure-0 dark:s-bg-structure-0-dark s-text-element-700 dark:s-text-element-700-dark";
   const hiddenClasses = "s-translate-y-2 s-opacity-0 s-pointer-events-none"; // Added s-pointer-events-none
   const visibleClasses = "-s-translate-y-0 s-opacity-100";
-
+  const hiddenOnMobileClasses = "s-hidden sm:s-block";
   const tooltipCenterClasses = "s-left-1/2 -s-translate-x-1/2";
   const tooltipPositionClasses =
     position === "above" ? "s-bottom-full s-mb-2" : "s-top-full s-mt-2";
@@ -60,6 +60,7 @@ export function Tooltip({
         className={classNames(
           `${isHovered ? visibleClasses : hiddenClasses}`,
           baseClasses,
+          hiddenOnMobileClasses,
           tooltipPositionClasses,
           tooltipCenterClasses
         )}
