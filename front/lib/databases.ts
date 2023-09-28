f1a1b358d52251b73e97a7bb4ea8c53add3d5300
Aric Lasry <lasry.aric@gmail.com>
2023-09-28 14:30:12 +0200

    Increase PGSQL connection pool for front (#1852)
    * Increase PGSQL connection pool for front
    
    * Update databases.ts

--- front/lib/databases.ts
+++ front/lib/databases.ts
@@ -3,6 +3,10 @@ import { Sequelize } from "sequelize";
 const { FRONT_DATABASE_URI, XP1_DATABASE_URI } = process.env;
 
 export const front_sequelize = new Sequelize(FRONT_DATABASE_URI as string, {
+  pool: {
+    // default is 5
+    max: 10,
+  },
   logging: false,
 });
 export const xp1_sequelize = new Sequelize(XP1_DATABASE_URI as string, {
