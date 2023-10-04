21212f39639250e2fefafc0536127b6d724892c7
Stanislas Polu <spolu@proton.me>
2023-10-04 16:11:00 +0200

    Update to 16k model for GPT-3.5

--- front/lib/assistant.ts
+++ front/lib/assistant.ts
@@ -28,7 +28,7 @@ export const GPT_4_MODEL_CONFIG = {
 
 export const GPT_3_5_TURBO_DEFAULT_MODEL_CONFIG = {
   providerId: "openai",
-  modelId: "gpt-3.5-turbo",
+  modelId: "gpt-3.5-turbo-16k",
   displayName: "GPT 3.5 Turbo",
   contextSize: 16384,
   largeModel: false,
