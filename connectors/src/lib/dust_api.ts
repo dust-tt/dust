e6f7490248d2ea80dcab1bce4d6d4b666dbb68b0
philipperolet <pr@dust.tt>
2023-10-05 11:43:51 +0200

    connectors types

--- connectors/src/lib/dust_api.ts
+++ connectors/src/lib/dust_api.ts
@@ -241,11 +241,11 @@ export type RetrievalDocumentType = {
   reference: string; // Short random string so that the model can refer to the document.
   timestamp: number;
   tags: string[];
-  score: number;
+  score: number | null;
   chunks: {
     text: string;
     offset: number;
-    score: number;
+    score: number | null;
   }[];
 };
 
