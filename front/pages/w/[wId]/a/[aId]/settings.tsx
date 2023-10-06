e0a35df3ad91412940235fd35bb9e07b463bc09c
Stanislas Polu <spolu@proton.me>
2023-10-06 21:38:00 +0200

    scrollbar-hide

--- front/pages/w/[wId]/a/[aId]/settings.tsx
+++ front/pages/w/[wId]/a/[aId]/settings.tsx
@@ -174,7 +174,7 @@ export default function SettingsView({
       hideSidebar
     >
       <div className="flex w-full flex-col">
-        <div className="mt-2 overflow-x-scroll">
+        <div className="mt-2 overflow-x-auto scrollbar-hide">
           <Tab tabs={subNavigationApp({ owner, app, current: "settings" })} />
         </div>
         <div className="mt-8 flex flex-1">
