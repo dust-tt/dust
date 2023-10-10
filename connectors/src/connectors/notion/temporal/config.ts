e34d7639323ffcc74adae06fc3710ea8f2f0d5ca
Henry Fontanier <h.fontanier@gmail.com>
2023-10-10 18:47:30 +0200

    example

--- connectors/src/connectors/notion/temporal/config.ts
+++ connectors/src/connectors/notion/temporal/config.ts
@@ -1,2 +1,18 @@
 export const WORKFLOW_VERSION = 24;
 export const QUEUE_NAME = `notion-queue-v${WORKFLOW_VERSION}`;
+
+type X = "A" | "B" | "C";
+
+const x: X = "A";
+
+if (x === "A") {
+  console.log("A");
+} else if (x === "B") {
+  console.log("B");
+} else if (x === "C") {
+  console.log("C");
+} else {
+  ((thing: never) => {
+    throw new Error(`Unexpected thing: ${thing}`);
+  })(x);
+}
