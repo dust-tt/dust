f86f9beba60f79898e3ad8b286fc14dcaf75e6f7
Henry Fontanier <h.fontanier@gmail.com>
2023-10-10 15:15:02 +0200

    add logs when errorring in retrieval (#2037)

--- front/lib/api/assistant/actions/retrieval.ts
+++ front/lib/api/assistant/actions/retrieval.ts
@@ -479,6 +479,14 @@ export async function* runRetrieval(
   );
 
   if (paramsRes.isErr()) {
+    logger.error(
+      {
+        workspaceId: owner.id,
+        conversationId: conversation.id,
+        error: paramsRes.error,
+      },
+      "Error generating retrieval parameters"
+    );
     yield {
       type: "retrieval_error",
       created: Date.now(),
@@ -594,6 +602,14 @@ export async function* runRetrieval(
   ]);
 
   if (res.isErr()) {
+    logger.error(
+      {
+        workspaceId: owner.id,
+        conversationId: conversation.id,
+        error: res.error,
+      },
+      "Error running retrieval"
+    );
     yield {
       type: "retrieval_error",
       created: Date.now(),
@@ -621,6 +637,14 @@ export async function* runRetrieval(
 
   for (const t of run.traces) {
     if (t[1][0][0].error) {
+      logger.error(
+        {
+          workspaceId: owner.id,
+          conversationId: conversation.id,
+          error: t[1][0][0].error,
+        },
+        "Error running retrieval"
+      );
       yield {
         type: "retrieval_error",
         created: Date.now(),
