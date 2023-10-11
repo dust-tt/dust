74e4d36b6251c655d09422f886766a59d1acb326
Henry Fontanier <h.fontanier@gmail.com>
2023-10-11 16:27:11 +0200

    add some space below the conversation (like chatGPT) (#2050)
    * Revert "Revert "add some space below the conversation (like chatGPT) (#2038)" (#2048)"
    
    This reverts commit 9a1226028869d8ae82cb41c479548892cad2a91c.
    
    * hide on mobile

--- front/pages/w/[wId]/assistant/[cId]/index.tsx
+++ front/pages/w/[wId]/assistant/[cId]/index.tsx
@@ -146,6 +146,7 @@ export default function AssistantConversation({
           conversationId={conversationId}
           onStickyMentionsChange={setStickyMentions}
         />
+        <div className="hidden h-48 lg:block" />
         <FixedAssistantInputBar
           owner={owner}
           onSubmit={handleSubmit}
