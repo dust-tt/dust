064bab89dd1bb4c396cb100173e6d97a4e2fc768
PopDaph <daphne.popin@gmail.com>
2023-10-13 12:50:19 +0200

    Remove unused

--- sparkle/src/stories/RadioButton.stories.tsx
+++ sparkle/src/stories/RadioButton.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import React from "react";
 
 import { RadioButton } from "@sparkle/components/RadioButton";
