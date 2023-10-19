f4629f4f742850919228f09c7dfe1596d0b5ee53
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-10-19 15:29:17 +0200

    Robot icon updat
    wq
    :w
    :wq

--- sparkle/src/icons/stroke/Robot.tsx
+++ sparkle/src/icons/stroke/Robot.tsx
@@ -11,8 +11,15 @@ const SvgRobot = (props: SVGProps<SVGSVGElement>) => (
   >
     <path
       fill="currentColor"
-      d="M13.5 2c0 .444-.193.843-.5 1.118V5h5a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V8a3 3 0 0 1 3-3h5V3.118A1.5 1.5 0 1 1 13.5 2ZM6 7a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H6Zm-4 3H0v6h2v-6Zm20 0h2v6h-2v-6ZM9 14.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm6 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"
+      d="M14 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM9 12.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM16.5 11a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM12 16c1.03 0 2.034-.111 3-.322V14H9v1.678c.966.21 1.97.322 3 .322Z"
     />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M18 5a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V8a3 3 0 0 1 3-3h12ZM6 7a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H6Z"
+      clipRule="evenodd"
+    />
+    <path fill="currentColor" d="M0 10h2v6H0v-6ZM22 10h2v6h-2v-6Z" />
   </svg>
 );
 export default SvgRobot;
