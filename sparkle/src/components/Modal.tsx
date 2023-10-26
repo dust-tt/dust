a896470ed603ecaccd968727344d80822325488d
Henry Fontanier <h.fontanier@gmail.com>
2023-10-26 14:34:29 +0200

    feat add modal right-side-wide

--- sparkle/src/components/Modal.tsx
+++ sparkle/src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import React, { Fragment } from "react";
 
-import { classNames } from "@sparkle/lib/utils";
+import { assertNever, classNames } from "@sparkle/lib/utils";
 
 import { BarHeader, BarHeaderButtonBarProps } from "./BarHeader";
 import { Button, ButtonProps } from "./Button";
@@ -17,7 +17,7 @@ interface ModalProps {
   isSaving?: boolean;
   savingLabel?: string;
   title?: string;
-  type?: "full-screen" | "right-side" | "default";
+  type?: "full-screen" | "right-side" | "default" | "right-side-wide";
 }
 
 export function Modal({
@@ -47,6 +47,83 @@ export function Modal({
         onClose: onClose,
       };
 
+  const justifyClass = (() => {
+    switch (type) {
+      case "right-side":
+      case "right-side-wide":
+        return "s-justify-end";
+
+      case "full-screen":
+      case "default":
+        return "s-justify-center";
+
+      default:
+        throw assertNever(type);
+    }
+  })();
+
+  const outerContainerClasses = (() => {
+    switch (type) {
+      case "right-side":
+      case "right-side-wide":
+      case "full-screen":
+        return "s-h-full s-p-0";
+
+      case "default":
+        return "s-min-h-full s-p-4";
+
+      default:
+        throw assertNever(type);
+    }
+  })();
+
+  const transitionEnterLeaveClasses = (() => {
+    switch (type) {
+      case "right-side":
+      case "right-side-wide":
+        return "s-translate-x-full";
+
+      case "full-screen":
+      case "default":
+        return "s-translate-y-4 sm:s-translate-y-0  sm:s-scale-95";
+
+      default:
+        throw assertNever(type);
+    }
+  })();
+
+  const panelClasses = (() => {
+    switch (type) {
+      case "right-side":
+      case "right-side-wide":
+        return "s-m-0 s-h-full s-max-h-full s-w-full s-max-w-full sm:s-w-[448px]";
+
+      case "full-screen":
+        return "s-m-0 s-h-full s-max-h-full s-w-full s-max-w-full";
+
+      case "default":
+        return "s-max-w-2xl s-rounded-lg s-shadow-xl lg:s-w-1/2";
+
+      default:
+        throw assertNever(type);
+    }
+  })();
+
+  const innerContainerClasses = (() => {
+    switch (type) {
+      case "right-side":
+      case "right-side-wide":
+      case "full-screen":
+        return "s-h-full s-overflow-y-auto";
+
+      case "default":
+        return "";
+
+      default:
+        throw assertNever(type);
+    }
+  })();
+
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog as="div" className="s-relative s-z-50" onClose={onClose}>
@@ -66,42 +143,23 @@ export function Modal({
           <div
             className={classNames(
               "s-flex s-items-center",
-              type === "right-side" ? "s-justify-end" : "s-justify-center",
-              type === "full-screen" || type === "right-side"
-                ? "s-h-full s-p-0"
-                : "s-min-h-full s-p-4"
+              justifyClass,
+              outerContainerClasses
             )}
           >
             <Transition.Child
               as={Fragment}
               enter="s-ease-out s-duration-300"
-              enterFrom={classNames(
-                "s-opacity-0",
-                type === "right-side"
-                  ? "s-translate-x-16"
-                  : "s-translate-y-4 sm:s-translate-y-0 sm:s-scale-95"
-              )}
+              enterFrom={classNames("s-opacity-0", transitionEnterLeaveClasses)}
               enterTo="s-opacity-100 s-translate-y-0 sm:s-scale-100"
               leave="s-ease-in s-duration-200"
               leaveFrom="s-opacity-100 s-translate-y-0 sm:s-scale-100"
-              leaveTo={classNames(
-                "s-opacity-0",
-                type === "right-side"
-                  ? "s-translate-x-full"
-                  : "s-translate-y-4 sm:s-translate-y-0  sm:s-scale-95"
-              )}
+              leaveTo={classNames("s-opacity-0", transitionEnterLeaveClasses)}
             >
               <Dialog.Panel
                 className={classNames(
                   "s-relative s-transform s-overflow-hidden s-bg-white s-px-3 s-transition-all sm:s-px-4",
-                  type === "full-screen" || type === "right-side"
-                    ? "s-m-0 s-h-full s-max-h-full"
-                    : "s-max-w-2xl s-rounded-lg s-shadow-xl lg:s-w-1/2",
-                  type === "full-screen"
-                    ? "s-w-full s-max-w-full"
-                    : type === "right-side"
-                    ? "s-w-full s-max-w-full sm:s-w-[448px]"
-                    : ""
+                  panelClasses
                 )}
               >
                 <BarHeader
@@ -110,11 +168,10 @@ export function Modal({
                   rightActions={<BarHeader.ButtonBar {...buttonBarProps} />}
                 />
                 <div
-                  className={`s-pb-6 s-pt-14 ${
-                    type === "full-screen" || type === "right-side"
-                      ? "s-h-full s-overflow-y-auto"
-                      : ""
-                  }`}
+                  className={classNames(
+                    "s-pb-6 s-pt-14",
+                    innerContainerClasses
+                  )}
                 >
                   {children}
                 </div>
--- sparkle/src/lib/utils.ts
+++ sparkle/src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export function assertNever(x: never): never {
+  throw new Error(`${x} is not of type never. This should never happen.`);
+}
