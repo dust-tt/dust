6dbac4206a714d66625eba31c0aab1a1fb8f5d08
Stanislas Polu <spolu@proton.me>
2023-11-08 16:38:40 +0100

    remove banner (#2443)

--- front/components/sparkle/AppLayout.tsx
+++ front/components/sparkle/AppLayout.tsx
@@ -364,7 +364,7 @@ export default function AppLayout({
             </div>
           </div>
 
-          {/** BEGIN Incident Banner */}
+          {/** BEGIN Incident Banner
           <div className="relative z-[80] flex w-full items-center justify-center bg-warning-300 py-2 text-element-900">
             <div className="">
               <span className="font-bold">OpenAI Outage:</span> OpenAI is
