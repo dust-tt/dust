e6c425b1d66850a2519689723e6c693cf591e93c
Stanislas Polu <spolu@proton.me>
2023-11-08 15:39:38 +0100

    emailing openAI down

--- front/mailing/20231108-openai_down.ts
+++ front/mailing/20231108-openai_down.ts
@@ -5,19 +5,15 @@ import { front_sequelize } from "@app/lib/databases";
 const { SENDGRID_API_KEY = "", LIVE = false } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-export const sendAPIUserEmail = async ({
-  user_email,
-}: {
-  user_email: string;
-}) => {
+export const sendAPIUserEmail = async ({ email }: { email: string }) => {
   const msg = {
-    to: user_email,
+    to: email,
     from: "spolu@dust.tt",
     subject: "[Dust] OpenAI Outage",
     html: `<p>Hi there—</p>
     <p>I'm Stan, a cofounder of Dust.</p>
-    <p>Since 2:54 UTC today, OpenAI is encountering a full outage of their APIs. They are investigating the issue. You can check their status</p>
-    <p>Most of our services are directly impacted. We apologize for the inconvenience. You can keep using assitants based on non-OpenAI models that do not rely on any retrieval from data sources.</p>
+    <p>Since 2:54 UTC today, OpenAI is encountering a full outage of their APIs. They are investigating the issue. You can check their status here: https://status.openai.com/</p>
+    <p>Most of our services are directly impacted, and we apologize for the inconvenience. You can keep using assitants based on non-OpenAI models that do not rely on any retrieval from data sources.</p>
     <p>We are closely monitoring the situation and will keep you posted as soon as the incident is resolved.</p>
     <p>-stan</p>
 `,
@@ -25,24 +21,17 @@ export const sendAPIUserEmail = async ({
 
   await sgMail.send(msg);
 
-  console.log("EMAIL SENT", user_email);
+  console.log("EMAIL SENT", email);
 };
 
 async function main() {
   const [rows] = await front_sequelize.query(
-    `select max(ws.name) ws_name, max(ws."sId") ws_sid, u.name user_name, u.email user_email, u.id user_id
-    from
-        agent_generation_configurations agc
-        inner join agent_configurations ac on ac."generationConfigurationId" = agc.id
-        inner join workspaces ws on ws.id = ac."workspaceId"
-        inner join memberships m on m."workspaceId" = ws.id
-        inner join users u on m."userId" = u.id
-    where
-        agc."modelId" ilike 'gpt-3%' or agc."modelId" ilike 'claude-instant%'
-        and ws.plan not ilike '%"largeModels":true%'
-        and m.role = 'admin'
-    group by u.id
-          `
+    `SELECT w.name, w."sId", u.name, u.email FROM subscriptions s
+INNER JOIN workspaces w ON s."workspaceId" = w.id
+INNER JOIN memberships m ON m."workspaceId" = w.id
+INNER JOIN users u ON m."userId" = u.id
+WHERE s.status = 'active'
+AND m.role = 'admin'`
   );
 
   console.log({ count: rows.length });
@@ -64,11 +53,11 @@ async function main() {
   }
 
   for (let i = 0; i < chunks.length; i++) {
-    const chunk = chunks[i] as { user_email: string }[];
+    const chunk = chunks[i] as { email: string }[];
     console.log("SENDING CHUNK", i, chunk.length);
     await Promise.all(
       chunk.map((row) => {
-        console.log("PREPARING EMAIL", row.user_email);
+        console.log("PREPARING EMAIL", row.email);
         if (LIVE && LIVE === "true") {
           return sendAPIUserEmail(row);
         } else {
