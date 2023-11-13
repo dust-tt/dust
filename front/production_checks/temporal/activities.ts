630a7350a0c2222ec3ba61b2225349c65e32d5a1
Aric Lasry <lasry.aric@gmail.com>
2023-11-13 14:38:14 +0100

    Adjusting comment for failed check to "Production check failed"

--- front/production_checks/temporal/activities.ts
+++ front/production_checks/temporal/activities.ts
@@ -30,7 +30,7 @@ async function runAllChecks(checks: Check[]) {
         logger.info({ reportPayload }, "Check succeeded");
       };
       const reportFailure = (reportPayload: unknown, message: string) => {
-        logger.error({ reportPayload, errorMessage: message }, "Check failed");
+        logger.error({ reportPayload, errorMessage: message }, "Production check failed");
       };
       Context.current().heartbeat({
         type: "start",
