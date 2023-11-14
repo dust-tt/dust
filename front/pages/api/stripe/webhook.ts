67d3525bdd1e5e8564dba221ffdf1fa76ed7a10b
philipperolet <pr@dust.tt>
2023-11-14 15:35:26 +0100

    Updated to handle reactivation of subscription after cancelation

--- front/pages/api/stripe/webhook.ts
+++ front/pages/api/stripe/webhook.ts
@@ -292,14 +292,17 @@ async function handler(
           if (!previousAttributes) break; // should not happen by definition of the subscription.updated event
 
           if (
-            // The subscription is canceled (but not yet ended)
+            // The subscription is canceled (but not yet ended) or reactivated
             stripeSubscription.status === "active" &&
-            "cancel_at_period_end" in previousAttributes &&
-            stripeSubscription.cancel_at_period_end
+            "cancel_at_period_end" in previousAttributes
           ) {
             // first update subscription endDate in our database
             const result = await Subscription.update(
-              { endDate: new Date(stripeSubscription.cancel_at!) },
+              {
+                endDate: stripeSubscription.cancel_at
+                  ? new Date(stripeSubscription.cancel_at)
+                  : null,
+              },
               { where: { stripeSubscriptionId: stripeSubscription.id } }
             );
             if (result[0] !== 1) {
@@ -312,7 +315,7 @@ async function handler(
                 },
               });
             }
-            // then email admins to let them know the implications of canceling the
+            // then email admins
             const adminEmails = await getAdminEmailsForSubscription(
               stripeSubscription.id
             );
@@ -328,7 +331,12 @@ async function handler(
             }
             // send email to admins
             for (const adminEmail of adminEmails)
-              await sendCancellationEmail(adminEmail["email"]);
+              await sendAdminEmail(
+                adminEmail["email"],
+                stripeSubscription.cancel_at_period_end
+                  ? cancellationMessage
+                  : reactivationMessage
+              );
           } else {
             logger.warn(
               { event },
@@ -404,30 +412,45 @@ async function getAdminEmailsForSubscription(stripeSubscriptionId: string) {
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-async function sendCancellationEmail(email: string) {
-  const message = {
-    to: email,
-    from: {
-      name: "Dust team",
-      email: "team@dust.tt",
-    },
-    subject: `[Dust] Your subscription has been canceled; important information`,
-    text: `<p>You have requested to cancel your subscription</p> 
-    <p>Your subscription will end at the end of the current billing period. You can reactivate your subscription at any time before then. If you do not reactivate your subscription, you will be switched back to our free plan at the end of the current billing period.</p>
-    <p>This will have the following consequences:</p>
-    <ul>
-    <li>all users except one will be removed from your workspace;</li>
-    <li>your connections will be deleted</li>
-    <li>the rest of your data (conversations, custom assistants, custom datasource) will still be accessible but with multiple limitations.</li>
-    <li>you will be subjected to the <a>restrictions of the free plan</a></li>
-    </ul>
-    <p>Complete details as to what will happen are available at <a href="https://dust.tt/pricing">subscription cancelling FAQ</a>.</p>
-    <p>If you have any questions, please contact us at team@dust.tt.</p>
-    <p>Best,</p>
-    <p>The Dust team</p>`,
-  };
+const cancellationMessage = {
+  from: {
+    name: "Dust team",
+    email: "team@dust.tt",
+  },
+  subject: `[Dust] Your subscription has been canceled; important information`,
+  text: `<p>You have requested to cancel your subscription</p> 
+  <p>Your subscription will end at the end of the current billing period. You can reactivate your subscription at any time before then. If you do not reactivate your subscription, you will be switched back to our free plan at the end of the current billing period.</p>
+  <p>This will have the following consequences:</p>
+  <ul>
+  <li>all users except one will be removed from your workspace;</li>
+  <li>your connections will be deleted</li>
+  <li>the rest of your data (conversations, custom assistants, custom datasource) will still be accessible but with multiple limitations.</li>
+  <li>you will be subjected to the <a>restrictions of the free plan</a></li>
+  </ul>
+  <p>Complete details as to what will happen are available at <a href="https://dust.tt/pricing">subscription cancelling FAQ</a>.</p>
+  <p>If you have any questions, please contact us at team@dust.tt.</p>
+  <p>Best,</p>
+  <p>The Dust team</p>`,
+};
+
+const reactivationMessage = {
+  from: {
+    name: "Dust team",
+    email: "team@dust.tt",
+  },
+  subject: `[Dust] Your subscription has been reactivated`,
+  text: `<p>You have requested to reactivate your subscription.</p> 
+  <p>Therefore, your subscription will not be canceled at the end of the billing period, no downgrade actions will take place, and you can continue using Dust as usual.</p>
+  <p>We really appreciate you renewing your trust in us.</p>
+  <p>If you have any questions, we'll gladly answer at team@dust.tt.</p>
+  <p>Best,</p>
+  <p>The Dust team</p>`,
+};
+
+async function sendAdminEmail(email: string, message: any) {
+  const msg = { ...message, to: email };
   try {
-    await sgMail.send(message);
+    await sgMail.send(msg);
   } catch (error) {
     logger.error(
       { error, email },
