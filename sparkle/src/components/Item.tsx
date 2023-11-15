b60a340060d248130a639aa331a22cfda11a38d5
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-11-15 10:57:15 +0100

    Removing old Item

--- sparkle/src/components/Item.tsx
+++ sparkle/src/components/Item.tsx
@@ -11,301 +11,25 @@ import { classNames } from "@sparkle/lib/utils";
 import { Avatar } from "./Avatar";
 import { Icon } from "./Icon";
 
-const sizeClasses = {
-  sm: "s-py-2 s-text-sm s-font-normal",
-  md: "s-py-3 s-text-sm s-font-semibold",
+const activeLabelStyleClasses =
+  "group-hover:s-text-action-500 group-active:s-text-action-700 dark:group-hover:s-text-action-600-dark dark:group-active:s-text-action-400-dark";
+
+const labelStyleClasses = {
+  item: "s-font-normal",
+  action: "s-font-semibold",
 };
 
-const baseClasses =
-  "s-group s-inline-flex s-transition-colors s-ease-out s-duration-400 s-box-border s-gap-x-2 s-select-none s-text-sm";
-
-const iconBaseClasses = "s-transition-colors s-ease-out s-duration-400";
-
-const iconClasses = {
-  default: {
-    base: "s-text-element-600",
-    hover: "group-hover:s-text-action-400",
-    dark: {
-      base: "dark:s-text-element-700-dark",
-      hover: "dark:group-hover:s-text-action-500-dark",
-    },
-  },
-  selected: {
-    base: "s-text-action-400",
-    hover: "",
-    dark: {
-      base: "dark:s-text-action-400-dark",
-      hover: "",
-    },
-  },
+const labelColorClasses = {
+  item: "s-text-element-600 dark:s-text-element-500-dark",
+  action: "s-text-element-900 dark:s-text-element-800-dark",
 };
 
-const chevronClasses = {
-  default: {
-    base: "s-text-element-600",
-    hover: "group-hover:s-text-action-400",
-    dark: {
-      base: "dark:s-text-element-600-dark",
-      hover: "dark:group-hover:s-text-action-500-dark",
-    },
-  },
-  selected: {
-    base: "s-text-action-300",
-    hover: "",
-    active: "",
-    dark: {
-      base: "dark:s-text-action-300-dark",
-      hover: "",
-    },
-    disabled: "",
-  },
-};
-
-const stateClasses = {
-  default: {
-    base: "s-text-element-700 s-cursor-pointer",
-    hover: "hover:s-text-element-800",
-    active: "active:s-text-element-900",
-    dark: {
-      base: "dark:s-text-element-700-dark",
-      hover: "dark:hover:s-text-element-800-dark",
-      active: "dark:active:s-text-element-900-dark",
-    },
-    disabled: "s-opacity-50",
-  },
-  selected: {
-    base: "s-text-action-500",
-    hover: "",
-    active: "",
-    dark: {
-      base: "dark:s-text-action-500-dark",
-      hover: "",
-      active: "",
-    },
-    disabled: "",
-  },
-};
-
-const stateClassesMD = {
-  default: {
-    base: "s-text-element-900 s-cursor-pointer",
-    hover: "hover:s-text-action-500",
-    active: "active:s-text-action-700",
-    dark: {
-      base: "dark:s-text-element-900-dark",
-      hover: "dark:hover:s-text-action-400",
-      active: "dark:active:s-text-action-600",
-    },
-    disabled: "s-opacity-50",
-  },
-  selected: {
-    base: "s-text-action-500",
-    hover: "",
-    active: "",
-    dark: {
-      base: "dark:s-text-action-500-dark",
-      hover: "",
-      active: "",
-    },
-    disabled: "",
-  },
+const spacingClasses = {
+  sm: "s-py-2 s-gap-x-2",
+  md: "s-py-2.5 s-gap-x-3",
 };
 
 type ItemProps = {
-  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
-  size?: "sm" | "md";
-  variant?: "default" | "dropdown";
-  selected?: boolean;
-  disabled?: boolean;
-  label: string;
-  icon?: ComponentType;
-  visual?: string | React.ReactNode;
-  className?: string;
-  href?: string;
-};
-
-export function Item({
-  onClick,
-  selected = false,
-  size = "sm",
-  disabled = false,
-  variant = "default",
-  label,
-  icon,
-  visual,
-  className = "",
-  href,
-}: ItemProps) {
-  const { components } = React.useContext(SparkleContext);
-
-  const currentStateClasses =
-    size === "md"
-      ? selected
-        ? stateClassesMD.selected
-        : stateClassesMD.default
-      : selected
-      ? stateClasses.selected
-      : stateClasses.default;
-  const currentIconClasses = selected
-    ? iconClasses.selected
-    : iconClasses.default;
-
-  const currentChevronClasses = selected
-    ? chevronClasses.selected
-    : chevronClasses.default;
-
-  const itemClasses = classNames(
-    baseClasses,
-    sizeClasses[size],
-    disabled ? "s-cursor-default" : currentStateClasses.base,
-    disabled ? "s-cursor-default" : currentStateClasses.dark?.base,
-    disabled ? currentStateClasses.disabled : "",
-    !selected && !disabled ? currentStateClasses.hover : "",
-    !selected && !disabled ? currentStateClasses.dark?.hover : "",
-    !selected && !disabled ? currentStateClasses.active : "",
-    !selected && !disabled ? currentStateClasses.dark?.active : "",
-    className
-  );
-
-  const finalIconClasses = classNames(
-    iconBaseClasses,
-    currentIconClasses.base,
-    !disabled ? currentIconClasses.hover : "", // Add condition here
-    currentIconClasses.dark.base,
-    !disabled ? currentIconClasses.dark.hover : "" // Add condition here
-  );
-
-  const finalCevronClasses = classNames(
-    iconBaseClasses,
-    "s-flex-shrink-0",
-    size === "sm" ? "s-text-transparent" : currentChevronClasses.base,
-    !disabled ? currentChevronClasses.hover : "", // Add condition here
-    currentChevronClasses.dark.base,
-    !disabled ? currentChevronClasses.dark.hover : "" // Add condition here
-  );
-
-  const Link: SparkleContextLinkType = href ? components.link : noHrefLink;
-
-  return (
-    <Link
-      className={itemClasses}
-      onClick={selected || disabled ? undefined : onClick}
-      aria-label={label}
-      href={href || "#"}
-    >
-      {icon && <Icon visual={icon} className={finalIconClasses} />}
-      {visual && <Avatar size="xs" visual={visual} />}
-      <span className="s-grow s-truncate">{label}</span>
-
-      {variant === "default" && (
-        <Icon visual={ChevronRight} className={finalCevronClasses} size="sm" />
-      )}
-    </Link>
-  );
-}
-
-interface EntryItemProps {
-  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
-  disabled?: boolean;
-  selected?: boolean;
-  label: string;
-  icon?: ComponentType;
-  className?: string;
-  href?: string;
-}
-
-Item.Entry = function (props: EntryItemProps) {
-  return <Item2 {...props} spacing="sm" style="item" hasAction={"hover"} />;
-};
-
-interface AvatarItemProps {
-  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
-  disabled?: boolean;
-  label: string;
-  description?: string;
-  visual?: string | React.ReactNode;
-  className?: string;
-  href?: string;
-}
-
-Item.Avatar = function ({ description, ...otherProps }: AvatarItemProps) {
-  return (
-    <Item2
-      {...otherProps}
-      style="action"
-      spacing={description ? "md" : "sm"}
-      description={description}
-    />
-  );
-};
-
-interface NavigationListItemProps {
-  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
-  selected?: boolean;
-  disabled?: boolean;
-  label: string;
-  description?: string;
-  icon?: ComponentType;
-  className?: string;
-  href?: string;
-}
-
-Item.Navigation = function (props: NavigationListItemProps) {
-  return <Item2 {...props} style="action" spacing="md" />;
-};
-
-interface DropdownListItemProps {
-  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
-  disabled?: boolean;
-  label: string;
-  description?: string;
-  visual?: string | React.ReactNode;
-  icon?: ComponentType;
-  className?: string;
-  href?: string;
-}
-
-Item.Dropdown = function (props: DropdownListItemProps) {
-  return <Item2 {...props} spacing="md" style="action" hasAction={false} />;
-};
-
-interface ListItemProps {
-  children: ReactNode;
-  className?: string;
-}
-
-interface ItemSectionHeaderProps {
-  label: string;
-  variant?: "primary" | "secondary";
-}
-
-Item.SectionHeader = function ({
-  label,
-  variant = "primary",
-}: ItemSectionHeaderProps) {
-  return (
-    <div
-      className={classNames(
-        variant === "primary"
-          ? "s-text-element-800 dark:s-text-element-800-dark"
-          : "s-text-element-600 dark:s-text-element-600-dark",
-        "s-pb-2 s-pt-6 s-text-xs s-font-medium s-uppercase"
-      )}
-    >
-      {label}
-    </div>
-  );
-};
-
-Item.List = function ({ children, className }: ListItemProps) {
-  return (
-    <div className={classNames(className ? className : "", "s-flex")}>
-      <div className={"s-flex s-w-full s-flex-col"}>{children}</div>
-    </div>
-  );
-};
-
-type Item2Props = {
   onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
   style: "action" | "item";
   spacing?: "sm" | "md";
@@ -322,7 +46,7 @@ type Item2Props = {
   href?: string;
 };
 
-export function Item2({
+export function Item({
   label,
   description,
   visual,
@@ -336,7 +60,7 @@ export function Item2({
   disabled = false,
   className = "",
   href,
-}: Item2Props) {
+}: ItemProps) {
   const { components } = React.useContext(SparkleContext);
 
   const Link: SparkleContextLinkType = href ? components.link : noHrefLink;
@@ -438,20 +162,103 @@ export function Item2({
   );
 }
 
-const activeLabelStyleClasses =
-  "group-hover:s-text-action-500 group-active:s-text-action-700 dark:group-hover:s-text-action-600-dark dark:group-active:s-text-action-400-dark";
+interface EntryItemProps {
+  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
+  disabled?: boolean;
+  selected?: boolean;
+  label: string;
+  icon?: ComponentType;
+  className?: string;
+  href?: string;
+}
 
-const labelStyleClasses = {
-  item: "s-font-normal",
-  action: "s-font-semibold",
+Item.Entry = function (props: EntryItemProps) {
+  return <Item {...props} spacing="sm" style="item" hasAction={"hover"} />;
 };
 
-const labelColorClasses = {
-  item: "s-text-element-600 dark:s-text-element-500-dark",
-  action: "s-text-element-900 dark:s-text-element-800-dark",
+interface AvatarItemProps {
+  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
+  disabled?: boolean;
+  label: string;
+  description?: string;
+  visual?: string | React.ReactNode;
+  className?: string;
+  href?: string;
+}
+
+Item.Avatar = function ({ description, ...otherProps }: AvatarItemProps) {
+  return (
+    <Item
+      {...otherProps}
+      style="action"
+      spacing={description ? "md" : "sm"}
+      description={description}
+    />
+  );
 };
 
-const spacingClasses = {
-  sm: "s-py-2 s-gap-x-2",
-  md: "s-py-2.5 s-gap-x-3",
+interface NavigationListItemProps {
+  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
+  selected?: boolean;
+  disabled?: boolean;
+  label: string;
+  description?: string;
+  icon?: ComponentType;
+  className?: string;
+  href?: string;
+}
+
+Item.Navigation = function (props: NavigationListItemProps) {
+  return <Item {...props} style="action" spacing="md" />;
+};
+
+interface DropdownListItemProps {
+  onClick?: (event: MouseEvent<HTMLAnchorElement>) => void;
+  disabled?: boolean;
+  label: string;
+  description?: string;
+  visual?: string | React.ReactNode;
+  icon?: ComponentType;
+  className?: string;
+  href?: string;
+}
+
+Item.Dropdown = function (props: DropdownListItemProps) {
+  return <Item {...props} spacing="md" style="action" hasAction={false} />;
+};
+
+interface ListItemProps {
+  children: ReactNode;
+  className?: string;
+}
+
+interface ItemSectionHeaderProps {
+  label: string;
+  variant?: "primary" | "secondary";
+}
+
+Item.SectionHeader = function ({
+  label,
+  variant = "primary",
+}: ItemSectionHeaderProps) {
+  return (
+    <div
+      className={classNames(
+        variant === "primary"
+          ? "s-text-element-800 dark:s-text-element-800-dark"
+          : "s-text-element-600 dark:s-text-element-600-dark",
+        "s-pb-2 s-pt-6 s-text-xs s-font-medium s-uppercase"
+      )}
+    >
+      {label}
+    </div>
+  );
+};
+
+Item.List = function ({ children, className }: ListItemProps) {
+  return (
+    <div className={classNames(className ? className : "", "s-flex")}>
+      <div className={"s-flex s-w-full s-flex-col"}>{children}</div>
+    </div>
+  );
 };
