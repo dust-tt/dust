606a4ddce5be22cbce31c2544166a02bbfc7888b
Henry Fontanier <h.fontanier@gmail.com>
2023-11-17 17:06:22 +0100

    fix: silence GH rate limiting

--- connectors/src/connectors/github/temporal/cast_known_errors.ts
+++ connectors/src/connectors/github/temporal/cast_known_errors.ts
@@ -1,9 +1,9 @@
+import { RequestError } from "@octokit/types";
 import {
   ActivityExecuteInput,
   ActivityInboundCallsInterceptor,
   Next,
 } from "@temporalio/worker";
-
 export class GithubCastKnownErrorsInterceptor
   implements ActivityInboundCallsInterceptor
 {
@@ -14,25 +14,15 @@ export class GithubCastKnownErrorsInterceptor
     try {
       return await next(input);
     } catch (err: unknown) {
-      const maybeGhError = err as {
-        status?: number;
-        name?: string;
-        type?: string;
-      };
+      const maybeGhError = err as RequestError;
 
-      if (
-        maybeGhError.status === 403 &&
-        maybeGhError.type === "RequestError" &&
-        maybeGhError.name === "HttpError"
-      ) {
+      if (maybeGhError.status === 403 && maybeGhError.name === "HttpError") {
         throw {
           __is_dust_error: true,
           message: "Github rate limited",
           type: "github_rate_limited",
         };
       }
-
-      throw err;
     }
   }
 }
