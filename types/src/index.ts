b1dbac8f9521b000ddcf0757515426965fa9c652
ARIC LASRY <aric@ARICs-MacBook-Pro.local>
2023-11-22 11:02:58 +0100

    Bumping

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('dev only output');
+    console.log('dev only outputt');
   }
   return a + b;
 };
