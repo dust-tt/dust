0d241553d4a0b994b666ffd7cf69bd477d3d42dd
ARIC LASRY <aric@ARICs-MacBook-Pro.local>
2023-11-22 14:33:05 +0100

    new p

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('dev only outputt');
+    console.log('dev only outputtt');
   }
   return a + b;
 };
