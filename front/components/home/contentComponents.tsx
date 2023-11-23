d7d5f3447945a41ef4443363b9915c984e8446b6
édouard wautier <edouardwautier@MacBook-Pro-de-edouard.local>
2023-11-23 16:12:06 +0100

    Using Hover on Homepage

--- front/components/home/contentComponents.tsx
+++ front/components/home/contentComponents.tsx
@@ -1,6 +1,8 @@
 import {
   CircleIcon,
+  Div3D,
   HexagonIcon,
+  Hover3D,
   Icon,
   RectangleIcon,
   SquareIcon,
@@ -260,19 +262,33 @@ export const ReactiveIcon = ({
     {
       className: classNames(
         singleChild.props.className,
-        "h-8 w-8 md:h-12 md:w-12 drop-shadow-[0_5px_5px_rgba(0,0,0,0.4)]"
+        "h-8 w-8 md:h-12 md:w-12"
       ),
     }
   );
   return (
-    <ReactiveImg
-      colorHEX={colorHEX}
-      className={classNames("w-fit", className)}
-      paddingCSS="p-4"
-      tooltipLabel={tooltipLabel}
-      isSmall
+    // <ReactiveImg
+    //   colorHEX={colorHEX}
+    //   className={classNames("w-fit", className)}
+    //   paddingCSS="p-4"
+    //   tooltipLabel={tooltipLabel}
+    //   isSmall
+    // >
+    //   {modifiedChild}
+    // </ReactiveImg>
+    <Hover3D
+      attack={0.1}
+      release={1}
+      className={classNames(
+        "rounded-2xl border p-3 shadow-xl",
+        "bg-gradient-to-b from-zinc-700/60 to-zinc-900/80",
+        "border-stone-700/40"
+      )}
+      depth={-10}
     >
-      {modifiedChild}
-    </ReactiveImg>
+      <Div3D depth={30} className="top-0">
+        {modifiedChild}
+      </Div3D>
+    </Hover3D>
   );
 };
