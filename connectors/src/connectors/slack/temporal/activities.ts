f0e1175c7ab2728b4625c14affd0c7163c4322a4
Stanislas Polu <spolu@proton.me>
2023-11-27 10:46:54 +0100

    slack: use display name or real name or username in conversations (#2675)
    * slack: use display name or real name or username in conversations
    
    * fix

--- connectors/src/connectors/slack/temporal/activities.ts
+++ connectors/src/connectors/slack/temporal/activities.ts
@@ -745,9 +745,15 @@ export async function getUserName(
 
   const info = await slackClient.users.info({ user: slackUserId });
 
-  if (info && info.user?.name) {
-    await cacheSet(getUserCacheKey(slackUserId, connectorId), info.user.name);
-    return info.user.name;
+  if (info && info.user) {
+    const displayName = info.user.profile?.display_name;
+    const realName = info.user.profile?.real_name;
+    const userName = displayName || realName || info.user.name;
+
+    if (userName) {
+      await cacheSet(getUserCacheKey(slackUserId, connectorId), userName);
+      return info.user.name;
+    }
   }
   return;
 }
