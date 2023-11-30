45037c76f663501451fb7dc0181727a5fdc2e0a1
Philippe Rolet <philipperolet@gmail.com>
2023-11-30 16:19:00 +0100

    wip

--- front/components/assistant/AssistantDetails.tsx
+++ front/components/assistant/AssistantDetails.tsx
@@ -7,12 +7,22 @@ import {
   isRetrievalConfiguration,
 } from "@app/types/assistant/actions/retrieval";
 import { AgentConfigurationType } from "@app/types/assistant/agent";
-import { Modal, Avatar, CloudArrowDownIcon } from "@dust-tt/sparkle";
+import {
+  Modal,
+  Avatar,
+  CloudArrowDownIcon,
+  CommandLineIcon,
+  Button,
+  TrashIcon,
+} from "@dust-tt/sparkle";
 import ReactMarkdown from "react-markdown";
 import { CONNECTOR_CONFIGURATIONS } from "@app/lib/connector_providers";
 import { ConnectorProvider } from "@app/lib/connectors_api";
 import { useApp } from "@app/lib/swr";
 import { WorkspaceType } from "@app/types/user";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { SendNotificationsContext } from "@app/components/sparkle/Notification";
 
 export function AssistantDetails({
   owner,
@@ -21,51 +31,68 @@ export function AssistantDetails({
   onClose,
 }: {
   owner: WorkspaceType;
-  assistant: AgentConfigurationType | null;
+  assistant: AgentConfigurationType;
   show: boolean;
   onClose: () => void;
 }) {
+  const DescriptionSection = () => (
+    <div className="flex flex-col gap-4 sm:flex-row">
+      <Avatar
+        visual={<img src={assistant.pictureUrl} alt="Assistant avatar" />}
+        size="md"
+      />
+      <div>{assistant.description}</div>
+    </div>
+  );
+
+  const InstructionsSection = () =>
+    assistant.generation?.prompt ? (
+      <div className="flex flex-col gap-2">
+        <div className="text-lg font-bold text-element-800">Instructions</div>
+        <ReactMarkdown className="max-h-64 overflow-y-auto">
+          {assistant.generation.prompt}
+        </ReactMarkdown>
+      </div>
+    ) : (
+      "This assistant has no instructions."
+    );
+
+  const ActionSection = () =>
+    assistant.action ? (
+      isDustAppRunConfiguration(assistant.action) ? (
+        <div className="flex flex-col gap-2">
+          <div className="text-lg font-bold text-element-800">Action</div>
+          <DustAppSection dustApp={assistant.action} owner={owner} />
+        </div>
+      ) : isRetrievalConfiguration(assistant.action) ? (
+        <div className="flex flex-col gap-2">
+          <div className="text-lg font-bold text-element-800">
+            Data source(s)
+          </div>
+          <DataSourcesSection
+            dataSourceConfigurations={assistant.action.dataSources}
+          />
+        </div>
+      ) : null
+    ) : null;
+
   return (
     <Modal
       isOpen={show}
-      title={`@${assistant?.name}`}
+      title={`@${assistant.name}`}
       onClose={onClose}
       hasChanged={false}
       variant="side-sm"
     >
       <div className="flex flex-col gap-5 p-6 text-sm text-element-700">
-        <div className="flex">buttons</div>
-        <div className="flex flex-col gap-4 sm:flex-row">
-          <Avatar
-            visual={<img src={assistant?.pictureUrl} alt="Assistant avatar" />}
-            size="md"
-          />
-          <div>{assistant?.description}</div>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <div className="text-lg font-bold text-element-800">Instructions</div>
-          <ReactMarkdown className="max-h-64 overflow-y-auto">
-            {assistant?.generation?.prompt || ""}
-          </ReactMarkdown>
-        </div>
-        {assistant?.action ? (
-          isDustAppRunConfiguration(assistant.action) ? (
-            <div className="flex flex-col gap-2">
-              <div className="text-lg font-bold text-element-800">Action</div>
-              <DustAppSection dustApp={assistant.action} owner={owner} />
-            </div>
-          ) : isRetrievalConfiguration(assistant.action) ? (
-            <div className="flex flex-col gap-2">
-              <div className="text-lg font-bold text-element-800">
-                Data source(s)
-              </div>
-              <DataSourcesSection
-                dataSourceConfigurations={assistant.action.dataSources}
-              />
-            </div>
-          ) : null
-        ) : null}
+        <ButtonsSection
+          owner={owner}
+          agentConfiguration={assistant}
+          detailsModalClose={onClose}
+        />
+        <DescriptionSection />
+        <InstructionsSection />
+        <ActionSection />
       </div>
     </Modal>
   );
@@ -80,6 +107,7 @@ function DataSourcesSection({
     ds.dataSourceId.startsWith("managed-")
       ? (ds.dataSourceId.slice(8) as ConnectorProvider)
       : undefined;
+
   const compareDatasourceNames = (
     a: DataSourceConfiguration,
     b: DataSourceConfiguration
@@ -97,6 +125,7 @@ function DataSourcesSection({
     }
     return a.dataSourceId > b.dataSourceId ? -1 : 1;
   };
+
   return (
     <div className="flex flex-col gap-1">
       {dataSourceConfigurations.sort(compareDatasourceNames).map((ds) => {
@@ -123,6 +152,7 @@ function DataSourcesSection({
     </div>
   );
 }
+
 function DustAppSection({
   owner,
   dustApp,
@@ -131,5 +161,133 @@ function DustAppSection({
   dustApp: DustAppRunConfigurationType;
 }) {
   const { app } = useApp({ workspaceId: owner.sId, appId: dustApp.appId });
-  return <div>{app ? app.name : ""}</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div>The following action is run before answering:</div>
+      <div className="flex items-center gap-2 capitalize">
+        <div>
+          <CommandLineIcon />
+        </div>
+        <div>{app ? app.name : ""}</div>
+      </div>
+    </div>
+  );
+}
+
+function ButtonsSection({
+  owner,
+  agentConfiguration,
+  detailsModalClose,
+}: {
+  owner: WorkspaceType;
+  agentConfiguration: AgentConfigurationType;
+  detailsModalClose: () => void;
+}) {
+  const [showDeletionModal, setShowDeletionModal] = useState<boolean>(false);
+  const canDelete =
+    (agentConfiguration.scope === "workspace" &&
+      ["builder", "admin"].includes(owner.role)) ||
+    (["published", "private"].includes(agentConfiguration.scope) &&
+      inMyList(agentConfiguration));
+  return (
+    <Button.List className="flex items-center justify-end gap-1">
+      {canDelete && (
+        <>
+          <DeletionModal
+            owner={owner}
+            agentConfiguration={agentConfiguration}
+            show={showDeletionModal}
+            onClose={() => setShowDeletionModal(false)}
+            detailsModalClose={detailsModalClose}
+          />
+          <Button
+            label={"Delete"}
+            icon={TrashIcon}
+            variant="secondaryWarning"
+            size="xs"
+            disabled={!["builder", "admin"].includes(owner.role)}
+            onClick={() => setShowDeletionModal(true)}
+          />
+        </>
+      )}
+    </Button.List>
+  );
+}
+
+function DeletionModal({
+  owner,
+  agentConfiguration,
+  show,
+  onClose,
+  detailsModalClose,
+}: {
+  owner: WorkspaceType;
+  agentConfiguration: AgentConfigurationType;
+  show: boolean;
+  onClose: () => void;
+  detailsModalClose: () => void;
+}) {
+  const router = useRouter();
+  const sendNotification = useContext(SendNotificationsContext);
+
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  return (
+    <Modal
+      isOpen={show}
+      title={`Delete @${agentConfiguration.name}`}
+      onClose={onClose}
+      hasChanged={false}
+      variant="dialogue"
+    >
+      <div className="flex flex-col gap-2 p-6 text-sm text-element-700">
+        <div>
+          Are you sure you want to delete this assistant? It will be removed for
+          all users and the action cannot be undone.
+          <br />
+          Consider just removing it from your list if you are not sure others
+          won't be affected.
+        </div>
+      </div>
+      <div className="flex flex-row justify-end gap-1">
+        <Button label="Cancel" variant="tertiary" onClick={onClose} />
+        <Button
+          label={isDeleting ? "Deleting..." : "Yes, Delete"}
+          variant="primaryWarning"
+          onClick={async () => {
+            setIsDeleting(true);
+            const res = await fetch(
+              `/api/w/${owner.sId}/assistant/agent_configurations/${agentConfiguration.sId}`,
+              {
+                method: "DELETE",
+              }
+            );
+            onClose();
+            detailsModalClose();
+            if (!res.ok) {
+              const data = await res.json();
+              sendNotification({
+                title: "Error deleting Assistant",
+                description: data.error.message,
+                type: "error",
+              });
+              return;
+            } else {
+              sendNotification({
+                title: "Assistant deleted",
+                type: "success",
+              });
+            }
+
+            setIsDeleting(false);
+          }}
+          disabled={isDeleting}
+        />
+      </div>
+    </Modal>
+  );
+}
+
+function inMyList(agentConfiguration: AgentConfigurationType): boolean {
+  // TODO IMPLEMENT
+  return true;
 }
