b2d6170d30dff8a8d7aa641da12540467e0f83fb
ARIC LASRY <aric@ARICs-MacBook-Pro.local>
2023-12-04 14:32:36 +0100

    npm run build

--- front/pages/api/poke/workspaces/[wId]/agent_configurations/[aId]/index.ts
+++ front/pages/api/poke/workspaces/[wId]/agent_configurations/[aId]/index.ts
@@ -1,3 +1,4 @@
+import { ReturnedAPIErrorType } from "@dust-tt/types";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import {
@@ -5,7 +6,6 @@ import {
   getAgentConfiguration,
 } from "@app/lib/api/assistant/configuration";
 import { Authenticator, getSession } from "@app/lib/auth";
-import { ReturnedAPIErrorType } from "@app/lib/error";
 import { apiError, withLogging } from "@app/logger/withlogging";
 
 export type DeleteAgentConfigurationResponseBody = {
