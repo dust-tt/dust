1989c4c857855df3537ac0fe7737ddc8c221f122
Philippe Rolet <philipperolet@gmail.com>
2023-12-04 10:06:49 +0100

    migration

---
+++ front/migrations/20231204_drop_chat_v0.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "chat_messages";
+DROP TABLE IF EXISTS "chat_retrieved_documents";
+DROP TABLE IF EXISTS "chat_sessions";
+DROP TABLE IF EXISTS "gens_templates";
+DROP TABLE IF EXISTS "member_agent_visibilities";
