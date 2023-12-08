167a17fce0f4ad578369c0b1db829660b3f44fbc
Aric Lasry <lasry.aric@gmail.com>
2023-12-08 13:29:58 +0100

    Fix front and connectors docker file

--- connectors/src/start.ts
+++ connectors/src/start.ts
@@ -29,3 +29,24 @@ runGithubWorker().catch((err) =>
 runGoogleWorker().catch((err) =>
   logger.error(errorFromAny(err), "Error running notion worker")
 );
+
+let startingMemoryUsage: any = null;
+let i = 0;
+
+setInterval(() => {
+  if (i === 3) {
+    startingMemoryUsage = process.memoryUsage();
+    console.log("starting defined", new Date());
+  }
+  i++;
+  if (startingMemoryUsage === null) {
+    console.log("starting not defined", new Date());
+    return;
+  }
+  const memoryUsage = process.memoryUsage();
+  Object.entries(memoryUsage).forEach(([key, value]) => {
+    const diff = value - startingMemoryUsage[key as keyof NodeJS.MemoryUsage];
+    console.log(`${key}: ${diff / (1024 * 1024)} megabytes`);
+  });
+  console.log("----------------", new Date());
+}, 5000);
