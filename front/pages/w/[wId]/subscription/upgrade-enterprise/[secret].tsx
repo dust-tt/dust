58025dd171deb99ebfc81c240f8d5af5884c705f
Stanislas Polu <spolu@dust.tt>
2023-12-11 12:14:00 +0100

    fix imports

--- front/pages/w/[wId]/subscription/upgrade-enterprise/[secret].tsx
+++ front/pages/w/[wId]/subscription/upgrade-enterprise/[secret].tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { GetServerSideProps } from "next";
 
 import { Authenticator, getSession } from "@app/lib/auth";
 import { Workspace } from "@app/lib/models";
