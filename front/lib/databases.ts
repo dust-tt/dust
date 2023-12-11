ba19fb739737f997b34a3ad36242d7b9f491d07d
Aric Lasry <lasry.aric@gmail.com>
2023-12-11 15:21:07 +0100

    Log long SQL connection acquisition time (#2824)

--- front/lib/databases.ts
+++ front/lib/databases.ts
@@ -1,6 +1,10 @@
 import { Sequelize } from "sequelize";
 
+import logger from "@app/logger/logger";
+
 const { FRONT_DATABASE_URI, XP1_DATABASE_URI } = process.env;
+
+const acquireAttempts = new WeakMap();
 
 export const front_sequelize = new Sequelize(FRONT_DATABASE_URI as string, {
   pool: {
@@ -8,6 +12,22 @@ export const front_sequelize = new Sequelize(FRONT_DATABASE_URI as string, {
     max: 10,
   },
   logging: false,
+  hooks: {
+    beforePoolAcquire: (options) => {
+      acquireAttempts.set(options, Date.now());
+    },
+    afterPoolAcquire: (connection, options) => {
+      const elapsedTime = Date.now() - acquireAttempts.get(options);
+      if (elapsedTime > 100) {
+        logger.info(
+          {
+            elapsedTime,
+          },
+          "Long sequelize connection acquisition detected"
+        );
+      }
+    },
+  },
 });
 export const xp1_sequelize = new Sequelize(XP1_DATABASE_URI as string, {
   logging: false,
