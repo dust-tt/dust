169bb00d274449503fb08204a17bfa92b6b3cd5c
Daphné Popin <daphne.popin@gmail.com>
2023-12-12 15:03:50 +0100

    isDevelopmentOrDustWorkspace true for Dust-app

--- front/lib/development.ts
+++ front/lib/development.ts
@@ -1,10 +1,16 @@
 import { WorkspaceType } from "@dust-tt/types";
 
 const PRODUCTION_DUST_WORKSPACE_ID = "0ec9852c2f";
+const PRODUCTION_DUST_APPS_WORKSPACE_ID = "78bda07b39";
+
 export function isDevelopment() {
   return process.env.NODE_ENV === "development";
 }
 
 export function isDevelopmentOrDustWorkspace(owner: WorkspaceType) {
-  return isDevelopment() || owner.sId === PRODUCTION_DUST_WORKSPACE_ID;
+  return (
+    isDevelopment() ||
+    owner.sId === PRODUCTION_DUST_WORKSPACE_ID ||
+    owner.sId === PRODUCTION_DUST_APPS_WORKSPACE_ID
+  );
 }
