c1308c48cd78faba3e80b8a0c469baa71b7f4efb
Henry Fontanier <h.fontanier@gmail.com>
2023-12-13 09:55:20 +0100

    allow more concurrent activity per notion worker (#2834)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- connectors/src/connectors/notion/temporal/worker.ts
+++ connectors/src/connectors/notion/temporal/worker.ts
@@ -16,7 +16,7 @@ export async function runNotionWorker() {
     taskQueue: QUEUE_NAME,
     connection,
     namespace,
-    maxConcurrentActivityTaskExecutions: 3,
+    maxConcurrentActivityTaskExecutions: 8,
     interceptors: {
       activityInbound: [
         (ctx: Context) => {
