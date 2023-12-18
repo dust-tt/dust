48cf1661595c7e049e371651db39f25ce1c3e0dd
filou <philipperolet@gmail.com>
2023-12-18 09:52:17 +0100

    clean 3

--- connectors/src/connectors/google_drive/temporal/activities.ts
+++ connectors/src/connectors/google_drive/temporal/activities.ts
@@ -22,6 +22,7 @@ import { drive_v3 } from "googleapis";
 import { OAuth2Client } from "googleapis-common";
 import { CreationAttributes, literal, Op } from "sequelize";
 
+import { registerWebhook } from "@connectors/connectors/google_drive/lib";
 import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
 import { dpdf2text } from "@connectors/lib/dpdf2text";
 import { ExternalOauthTokenError } from "@connectors/lib/error";
@@ -35,8 +36,6 @@ import {
 } from "@connectors/lib/models/google_drive";
 import { getConnectionFromNango } from "@connectors/lib/nango_helpers";
 import logger from "@connectors/logger/logger";
-
-import { registerWebhook } from "../lib";
 
 const FILES_SYNC_CONCURRENCY = 10;
 const FILES_GC_CONCURRENCY = 5;
