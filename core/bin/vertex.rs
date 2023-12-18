7f531e0d3b12b567264b9d8bf487f85f432d985e
Henry Fontanier <henry@dust.tt>
2023-12-18 12:28:32 +0100

    [WIP] ability to setup vertex ai

--- core/bin/vertex.rs
+++ core/bin/vertex.rs
@@ -1,20 +1,29 @@
-use dust::providers::google_vertex_ai::streamed_completion;
+use anyhow::anyhow;
+use dust::providers::google_vertex_ai::{get_access_token, streamed_completion};
 use hyper::Uri;
 
-const TOKEN: &str = "REDACTED";
 const URI: &str = "https://us-central1-aiplatform.googleapis.com/v1/projects/or1g1n-186209/locations/us-central1/publishers/google/models/gemini-pro:streamGenerateContent?alt=sse";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sa_json_path = match std::env::var("SERVICE_ACCOUNT") {
+        Ok(path) => path,
+        Err(_) => Err(anyhow!("SERVICE_ACCOUNT environment variable not set."))?,
+    };
+
+    let sa_json_contents = std::fs::read_to_string(sa_json_path)?;
+
+    let access_token = get_access_token(sa_json_contents.as_str()).await?;
+
     streamed_completion(
         URI.parse::<Uri>().unwrap(),
-        TOKEN.to_string(),
+        access_token,
         "Generate some very long text about how Mistral AI is superior to Gemini.",
         Some(8000),
         0.2,
         &vec![],
         0.8,
-        40,
+        None,
         None,
     )
     .await?;
