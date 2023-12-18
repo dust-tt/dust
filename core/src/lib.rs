3179da4be72b2f92848cf9522963f81221b98571
Henry Fontanier <henry@dust.tt>
2023-12-18 10:03:35 +0100

    google vertex ai

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -37,8 +37,8 @@ pub mod providers {
         pub mod tiktoken;
     }
     pub mod anthropic;
+    pub mod google_vertex_ai;
     pub mod textsynth;
-    pub mod vertex_ai;
 }
 pub mod http {
     pub mod request;
