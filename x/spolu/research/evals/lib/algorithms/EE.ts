f341a60a456d39c1bb26b298c1c487399fe498dc
Stanislas Polu <spolu@dust.tt>
2024-01-08 08:55:41 +0100

    more tweaks

--- x/spolu/research/evals/lib/algorithms/EE.ts
+++ x/spolu/research/evals/lib/algorithms/EE.ts
@@ -10,25 +10,30 @@ type Explanation = {
   answer: string;
   check: boolean;
   judgements: string[];
+  score?: number;
 };
 
 export class EE extends Algorithm {
   readonly N_SHOT = 8;
-  readonly POOL_SIZE = 64;
+  readonly POOL_SIZE = 32;
   readonly TEMPERATURE = 0.7;
   readonly JUDGEMENTS_DEPTH = 4;
-  readonly GENERATIONS = 3;
+  readonly GENERATIONS = 4;
   readonly MAX_CROSSOVERS = 3;
   readonly INNER_CONCURRENCY = 4;
-  readonly USE_INSIGHTS = false;
+
+  readonly GRADES = [
+    "GRADE_INCORRECT",
+    "GRADE_MEDIOCRE",
+    "GRADE_GOOD",
+    "GRADE_VERY_GOOD",
+  ];
 
   private generationResults: TestResult[][];
-  private insights: { [key: string]: string[] };
 
   constructor(dataset: Dataset, model: Model) {
     super(dataset, model);
     this.generationResults = [];
-    this.insights = {};
     console.log(
       "Predicted runs per problem:" +
         (this.POOL_SIZE +
@@ -108,12 +113,12 @@ export class EE extends Algorithm {
       content: `QUESTION: ${test.question}`,
     });
 
-    messages.forEach((m) => {
-      console.log(`+++++++++++++++++++++++++++++++`);
-      console.log(`[${m.role}]`);
-      console.log(`-------------------------------`);
-      console.log(`${m.content}`);
-    });
+    // messages.forEach((m) => {
+    //   console.log(`+++++++++++++++++++++++++++++++`);
+    //   console.log(`[${m.role}]`);
+    //   console.log(`-------------------------------`);
+    //   console.log(`${m.content}`);
+    // });
 
     const query: ChatQuery = {
       provider: this.model.provider,
@@ -176,12 +181,6 @@ export class EE extends Algorithm {
     generation: number;
     iteration: number;
   }) {
-    let currentInsights: string | null = null;
-    if (this.insights[test.id] && this.insights[test.id].length > 0) {
-      currentInsights =
-        this.insights[test.id][this.insights[test.id].length - 1];
-    }
-
     const messages: ChatMessage[] = [];
 
     let prompt = `<Instructions>\n`;
@@ -193,22 +192,20 @@ export class EE extends Algorithm {
     prompt += `As domain expert, be precise about what you think is good or bad in the proposed explanation.`;
     prompt += ` Focus on checking its correctness and propose ways to improve it.`;
     prompt += `\n\n`;
-    if (explanation.judgements.length === 0) {
-      prompt += `Produce a (200 words/symbols max, be as dense as possible) commentary/judgement of the explanation`;
-    } else {
-      prompt += `Judge (200 words/symbols max, be as dense as possible) the commentaries made by other experts on the explanation`;
-    }
-    prompt += ` proposed to answer the following question:`;
+    // if (explanation.judgements.length === 0) {
+    prompt += `Produce a commentary/judgement of the explanation`;
+    // } else {
+    //   prompt += `Judge (200 words/symbols max, be as dense as possible) the commentaries made by other experts on the explanation`;
+    // }
+    prompt += ` proposed to answer the following question (200 words/symbols max, be as dense as possible):`;
     prompt += `\n\n`;
     prompt += `<Question>\n`;
     prompt += `${test.question}`;
     prompt += `\n</Question>`;
-    if (this.USE_INSIGHTS && currentInsights) {
-      prompt += `\n\n`;
-      prompt += `Leverage the insights about the question accumulated from previous rounds of`;
-      prompt += ` explanations and judgements to avoid common pitfalls and reinforce convincing ideas.`;
-      prompt += `\n\n<Insights>\n${currentInsights}\n</Insights>`;
-    }
+    prompt += `\n\n`;
+    prompt += `Finish your commentary by a grading of the explanation, one of: `;
+    prompt += `${this.GRADES.join(", ")}. `;
+    prompt += ` Put extra care in detecting incorrect explanations to mark them as GRADE_INCORRECT.`;
     prompt += `\n</Instructions>`;
 
     messages.push({
@@ -231,12 +228,12 @@ export class EE extends Algorithm {
       content,
     });
 
-    messages.forEach((m) => {
-      console.log(`+++++++++++++++++++++++++++++++`);
-      console.log(`[${m.role}]`);
-      console.log(`-------------------------------`);
-      console.log(`${m.content}`);
-    });
+    // messages.forEach((m) => {
+    //   console.log(`+++++++++++++++++++++++++++++++`);
+    //   console.log(`[${m.role}]`);
+    //   console.log(`-------------------------------`);
+    //   console.log(`${m.content}`);
+    // });
 
     const query: ChatQuery = {
       provider: this.model.provider,
@@ -283,12 +280,6 @@ export class EE extends Algorithm {
       `EE-CROSSOVER-${test.id}-${generation}-${iteration}`
     );
 
-    let currentInsights: string | null = null;
-    if (this.insights[test.id] && this.insights[test.id].length > 0) {
-      currentInsights =
-        this.insights[test.id][this.insights[test.id].length - 1];
-    }
-
     // choose a random number between 2 and this.MAX_CROSSOVERS using rng
     const crossOvers = Math.floor(rng() * (this.MAX_CROSSOVERS - 2)) + 2;
 
@@ -311,7 +302,7 @@ export class EE extends Algorithm {
     prompt += `\n</Task>\n\n`;
     prompt += this.explanativePrompt();
     prompt += `\n\n`;
-    prompt += `Based on the following ${crossOvers} explanation proposal${
+    prompt += `Based on the following ${crossOvers} explanation${
       crossOvers === 1 ? "" : "s"
     }`;
     prompt += ` and associated commentaries/judgements made by domain experts,`;
@@ -320,12 +311,6 @@ export class EE extends Algorithm {
     prompt += `<Question>\n`;
     prompt += `${test.question}`;
     prompt += `\n</Question>`;
-    if (this.USE_INSIGHTS && currentInsights) {
-      prompt += `\n\n`;
-      prompt += `Take into account the insights about the question accumulated from previous`;
-      prompt += ` rounds of explanations and judgements to avoid common pitfalls and explore convincing ideas.`;
-      prompt += `\n\n<Insights>\n${currentInsights}\n</Insights>`;
-    }
     prompt += `\n</Instructions>`;
 
     messages.push({
@@ -354,12 +339,12 @@ export class EE extends Algorithm {
       content,
     });
 
-    messages.forEach((m) => {
-      console.log(`+++++++++++++++++++++++++++++++`);
-      console.log(`[${m.role}]`);
-      console.log(`-------------------------------`);
-      console.log(`${m.content}`);
-    });
+    // messages.forEach((m) => {
+    //   console.log(`+++++++++++++++++++++++++++++++`);
+    //   console.log(`[${m.role}]`);
+    //   console.log(`-------------------------------`);
+    //   console.log(`${m.content}`);
+    // });
 
     const query: ChatQuery = {
       provider: this.model.provider,
@@ -445,120 +430,6 @@ export class EE extends Algorithm {
     };
   }
 
-  async aggregateInsights({
-    test,
-    pool,
-    generation,
-  }: {
-    test: Test;
-    pool: Explanation[];
-    generation: number;
-  }): Promise<void> {
-    let currentInsights: string | null = null;
-
-    if (!this.insights[test.id]) {
-      this.insights[test.id] = [];
-    }
-    if (this.insights[test.id].length !== generation) {
-      throw new Error("Invalid insights length");
-    }
-    if (this.insights[test.id].length > 0) {
-      currentInsights =
-        this.insights[test.id][this.insights[test.id].length - 1];
-    }
-
-    const messages: ChatMessage[] = [];
-
-    let prompt = `<Instructions>\n`;
-    prompt += `<Task>\n`;
-    prompt += this.taskPrompt();
-    prompt += `\n</Task>\n\n`;
-    prompt += this.explanativePrompt();
-    prompt += `\n\n`;
-    prompt += `Insights about a question are a condensed (1000 words max) repository of learnings about a question accumulated`;
-    prompt += ` across rounds of explanation proposals and expert judgements about them.`;
-    prompt += ` Record spotted errors and good strategies in explored explanations so that they`;
-    prompt += ` can be presented to future experts working on the explanation of the question,`;
-    prompt += ` to ensure future explanations are correct and mistakes are not repeated.`;
-    prompt += `\n\n`;
-    if (currentInsights) {
-      prompt += `Update the current insights about the question based on the last round of explanations and judgements, record common errors, valid reasonings and convincing new ideas.`;
-    } else {
-      prompt += `Record insights about the question based on the last round of explanations and judgements.`;
-    }
-    prompt += `\n\n`;
-    prompt += `<Question>\n`;
-    prompt += `${test.question}`;
-    prompt += `\n</Question>`;
-    prompt += `\n</Instructions>`;
-
-    messages.push({
-      role: "system",
-      content: prompt,
-    });
-
-    let content = ``;
-    if (currentInsights) {
-      content += `<Insights>\n${currentInsights}\n</Insights>\n\n`;
-    }
-
-    for (let i = 0; i < pool.length; i++) {
-      if (i > 0) {
-        content += `\n\n`;
-      }
-      content += `EXPLANATION ${i}:\n\n${pool[i].explanation}`;
-      for (let j = 0; j < pool[i].judgements.length; j++) {
-        content += `\n\nEXPERT JUDGEMENT ${i} ${j}:\n\n${pool[i].judgements[j]}`;
-      }
-    }
-    content += `\n\n`;
-
-    if (currentInsights) {
-      content += `Propose new insights based on the current insights and the most important learnings from the explanations and judgements.`;
-    } else {
-      content += `Propose insights based on the most important learnings from the explanations and judgements.`;
-    }
-
-    messages.push({
-      role: "user",
-      content,
-    });
-
-    messages.forEach((m) => {
-      console.log(`+++++++++++++++++++++++++++++++`);
-      console.log(`[${m.role}]`);
-      console.log(`-------------------------------`);
-      console.log(`${m.content}`);
-    });
-
-    const query: ChatQuery = {
-      provider: this.model.provider,
-      model: this.model.model(),
-      messages,
-      temperature: this.TEMPERATURE,
-      maxTokens: 2048,
-    };
-
-    const c = await this.runCompletion(query);
-
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
-    console.log(`INSIGHTS test=${test.id} generation=${generation}`);
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
-    console.log(c.content);
-    console.log("<<<<<<<<<<<<<<<<<<<<<<<<<");
-    console.log("\n\n\n");
-
-    await this.storeCompletion({
-      test,
-      completion: c,
-      query,
-      check: false,
-    });
-    this.stats();
-
-    this.insights[test.id].push(c.content);
-  }
-
   async runOne({ test }: { test: Test }): Promise<TestResult> {
     const initQueue = new PQueue({
       concurrency: this.INNER_CONCURRENCY,
@@ -618,9 +489,46 @@ export class EE extends Algorithm {
         );
       }
 
-      // Insight aggregation.
-      if (this.USE_INSIGHTS) {
-        await this.aggregateInsights({ test, pool, generation });
+      const GRADE_TO_SCORES: { [grade: string]: number } = {
+        GRADE_INCORRECT: -2,
+        GRADE_MEDIOCRE: 0,
+        GRADE_GOOD: 1,
+        GRADE_VERY_GOOD: 2,
+      };
+
+      // Rating
+      for (const e of pool) {
+        let score = 0;
+        for (const j of e.judgements) {
+          let maxPos = -1;
+          let grade: string | null = null;
+          for (const g of this.GRADES) {
+            const pos = j.indexOf(g);
+            if (pos > maxPos) {
+              maxPos = pos;
+              grade = g;
+            }
+          }
+          if (grade != null) {
+            score += GRADE_TO_SCORES[grade];
+          } else {
+            console.log(`Grade not found ${test.id} ${generation} ${j}`);
+          }
+        }
+        e.score = score;
+      }
+
+      pool.sort((a, b) => {
+        if (a.score === undefined || b.score === undefined) {
+          throw new Error("Invalid score");
+        }
+        return b.score - a.score;
+      });
+
+      for (const e of pool) {
+        console.log(
+          `POOL ${test.id}: generation=${generation} score=${e.score} answer=${e.answer} check=${e.check}`
+        );
       }
 
       // Cross-over.
@@ -632,7 +540,13 @@ export class EE extends Algorithm {
         await Promise.all(
           pool.map((_, i) => {
             return crossOverQueue.add(() =>
-              this.crossOver({ test, pool, generation, iteration: i })
+              this.crossOver({
+                test,
+                // Select the best half.
+                pool: pool.slice(0, this.POOL_SIZE / 2),
+                generation,
+                iteration: i,
+              })
             );
           })
         )
