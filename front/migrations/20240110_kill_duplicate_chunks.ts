feb084c278b9e80dc1e8b9989846cc0a65a71003
Stanislas Polu <spolu@dust.tt>
2024-01-10 17:00:14 +0100

    migration to clean-up duplicate chunks

---
+++ front/migrations/20240110_kill_duplicate_chunks.ts
@@ -0,0 +1,85 @@
+import { QdrantClient } from "@qdrant/js-client-rest";
+
+const { QDRANT_API_KEY, QDRANT_URL } = process.env;
+
+const BUGGY_COLLECTION_NAMES: string[] = [];
+
+const client = new QdrantClient({
+  url: QDRANT_URL,
+  apiKey: QDRANT_API_KEY,
+});
+
+async function run() {
+  const result = await client.getCollections();
+  const collections = result.collections;
+
+  console.log(`Processing ${collections.length} collections.`);
+  let i = 0;
+  for (const c of collections) {
+    // console.log(c);
+    const chunkHashes: { [key: string]: string | number } = {};
+    const duplicates: (string | number)[] = [];
+
+    if (BUGGY_COLLECTION_NAMES.includes(c.name)) {
+      console.log(`SKIPPING ${c.name}`);
+      continue;
+    }
+    let offset:
+      | number
+      | undefined
+      | null
+      | string
+      | Record<string, unknown> = 0;
+    while (offset !== null && offset !== undefined) {
+      const res = await client.scroll(c.name, {
+        // with_payload: true,
+        limit: 1024,
+        offset,
+      });
+
+      res.points.forEach((p) => {
+        if (p.payload) {
+          const chunkHash = p.payload["chunk_hash"] as string;
+          if (!chunkHash || chunkHash.length === 0) {
+            console.log(`EMPTY ${p.payload["data_source_id"]} ${p.id}`);
+            return;
+          }
+          const pointId = p.id;
+          if (chunkHashes[chunkHash]) {
+            // console.log(
+            //   `DUPLICATE ${chunkHash} ${p.payload["data_source_id"]} ${pointId} ${chunkHashes[chunkHash]}`
+            // );
+            duplicates.push(pointId);
+          } else {
+            chunkHashes[chunkHash] = pointId;
+          }
+        }
+      });
+
+      offset = res.next_page_offset;
+    }
+
+    console.log(
+      `Found ${duplicates.length} duplicates and ${
+        Object.keys(chunkHashes).length
+      } unique chunks in ${c.name}.`
+    );
+
+    // if (duplicates.length > 0) {
+    //   await client.delete(c.name, { wait: true, points: duplicates });
+    //   console.log(
+    //     `DELETED ${duplicates.length} points from ${c.name} res=${r}`
+    //   );
+    // }
+
+    i++;
+    if (i % 32 === 0) {
+      console.log(`PROCESS [${i}/${collections.length}] sleeping 1s`);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+}
+
+void (async () => {
+  await run();
+})();
