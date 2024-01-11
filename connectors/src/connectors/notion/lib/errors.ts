0d77709b9de3b60c3bb8d419fbfc0dc38f82f73e
Flavien David <flavien@dust.tt>
2024-01-11 13:45:48 +0100

    Make NotionError typeguard stricter.

--- connectors/src/connectors/notion/lib/errors.ts
+++ connectors/src/connectors/notion/lib/errors.ts
@@ -1,11 +1,20 @@
 // This type is a simplified abstraction and
+
+import { APIErrorCode } from "@notionhq/client";
+const APIErrorCodeValues = Object.values(APIErrorCode);
+
 // does not fully represent the structure of errors returned by the Notion API.
 interface NotionError extends Error {
   body: unknown;
-  code: string;
+  code: APIErrorCode;
   status: number;
 }
 
-export function isNotionError(error: Error): error is NotionError {
-  return "code" in error;
+export function isNotionError(error: unknown): error is NotionError {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "code" in error &&
+    APIErrorCodeValues.includes((error as NotionError).code)
+  );
 }
