bfa4013f70fec9bf9dd37cb91dff4e70f33c6846
Flavien David <flavien@dust.tt>
2024-01-11 15:01:08 +0100

    :books:

--- connectors/src/connectors/notion/temporal/utils.ts
+++ connectors/src/connectors/notion/temporal/utils.ts
@@ -2,6 +2,7 @@ import { ModelId } from "@dust-tt/types";
 
 import { DataSourceInfo } from "@connectors/types/data_source_config";
 
+// Changes made here should be reflected in the production environment checks.
 export function getWorkflowId(dataSourceInfo: DataSourceInfo) {
   return `workflow-notion-${dataSourceInfo.workspaceId}-${dataSourceInfo.dataSourceName}`;
 }
