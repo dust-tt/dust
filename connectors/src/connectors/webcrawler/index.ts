44fdec9428a6afed88d38685444a4ceae6efcfce
Aric Lasry <lasry.aric@gmail.com>
2024-01-12 16:42:56 +0100

    [URL Connector] Better folders structure
    Better pages/folders reconciliation
    
    Clean up after self review
    
    Better naming
    
    Fix folders structure
    
    Clean up after self review
    
    Revert util file
    
    Set upsert concurrency to 4
    
    Using `_index` as page name for index pages
    
    URL Connector - UI for (select) public relase
    
    Pages that are also folders are now merged into an expendable file.
    
    REmove all UI files

--- connectors/src/connectors/webcrawler/index.ts
+++ connectors/src/connectors/webcrawler/index.ts
@@ -1,6 +1,9 @@
 import type { ConnectorResource, ModelId } from "@dust-tt/types";
 
-import { stableIdForUrl } from "@connectors/connectors/webcrawler/lib/utils";
+import {
+  normalizeFolderUrl,
+  stableIdForUrl,
+} from "@connectors/connectors/webcrawler/lib/utils";
 import { Connector, sequelize_conn } from "@connectors/lib/models";
 import {
   WebCrawlerConfiguration,
@@ -96,14 +99,6 @@ export async function retrieveWebcrawlerConnectorPermissions({
     parentUrl = parent.url;
   }
 
-  const folders = await WebCrawlerFolder.findAll({
-    where: {
-      connectorId: connector.id,
-      webcrawlerConfigurationId: webCrawlerConfig.id,
-      parentUrl: parentUrl,
-    },
-  });
-
   const pages = await WebCrawlerPage.findAll({
     where: {
       connectorId: connector.id,
@@ -112,8 +107,26 @@ export async function retrieveWebcrawlerConnectorPermissions({
     },
   });
 
+  const folders = await WebCrawlerFolder.findAll({
+    where: {
+      connectorId: connector.id,
+      webcrawlerConfigurationId: webCrawlerConfig.id,
+      parentUrl: parentUrl,
+    },
+  });
+
+  const normalizedPagesSet = new Set(
+    pages.map((p) => normalizeFolderUrl(p.url))
+  );
+  // List of folders that are also pages
+  const excludedFoldersSet = new Set(
+    folders.map((f) => f.url).filter((f) => normalizedPagesSet.has(f))
+  );
+
   return new Ok(
     folders
+      // We don't want to show folders that are also pages.
+      .filter((f) => !excludedFoldersSet.has(f.url))
       .map((folder): ConnectorResource => {
         return {
           provider: "webcrawler",
@@ -139,9 +152,18 @@ export async function retrieveWebcrawlerConnectorPermissions({
       })
       .concat(
         pages.map((page): ConnectorResource => {
+          const parsedUrl = new URL(page.url);
+          const isFileAndFolder = excludedFoldersSet.has(
+            normalizeFolderUrl(page.url)
+          );
           return {
             provider: "webcrawler",
-            internalId: page.documentId,
+            internalId: isFileAndFolder
+              ? stableIdForUrl({
+                  url: normalizeFolderUrl(page.url),
+                  ressourceType: "folder",
+                })
+              : page.documentId,
             parentInternalId: page.parentUrl
               ? stableIdForUrl({
                   url: page.parentUrl,
@@ -149,12 +171,12 @@ export async function retrieveWebcrawlerConnectorPermissions({
                 })
               : null,
             title:
-              new URL(page.url).pathname
+              parsedUrl.pathname
                 .split("/")
                 .filter((x) => x)
-                .pop() || page.url,
+                .pop() || parsedUrl.origin,
             sourceUrl: page.url,
-            expandable: false,
+            expandable: isFileAndFolder ? true : false,
             permission: "read",
             dustDocumentId: page.documentId,
             type: "file",
--- connectors/src/connectors/webcrawler/lib/utils.ts
+++ connectors/src/connectors/webcrawler/lib/utils.ts
@@ -12,3 +12,66 @@ export function stableIdForUrl({
 }) {
   return Buffer.from(blake3(`${ressourceType}-${url}`)).toString("hex");
 }
+
+export function getParentsForPage(url: string, pageInItsOwnFolder: boolean) {
+  const parents: string[] = [];
+  parents.push(stableIdForUrl({ url, ressourceType: "file" }));
+  if (pageInItsOwnFolder) {
+    parents.push(
+      stableIdForUrl({ url: normalizeFolderUrl(url), ressourceType: "folder" })
+    );
+  }
+  parents.push(
+    ...getAllFoldersForUrl(url).map((f) =>
+      stableIdForUrl({ url: f, ressourceType: "folder" })
+    )
+  );
+
+  return parents;
+}
+
+// Returns all parent folders for a given url
+// eg: https://example.com/a/b/c -> [https://example.com/a/b, https://example.com/a, https://example.com/]
+export function getAllFoldersForUrl(url: string) {
+  const parents: string[] = [];
+
+  let parent: string | null = null;
+  while ((parent = getFolderForUrl(url))) {
+    parents.push(parent);
+    url = parent;
+  }
+
+  return parents;
+}
+
+// Returns the parent folder for a given url
+// eg: https://example.com/foo/bar -> https://example.com/foo
+// eg: https://example.com/foo -> https://example.com/
+export function getFolderForUrl(url: string) {
+  const parsed = new URL(url);
+  const urlParts = parsed.pathname.split("/").filter((part) => part.length > 0);
+  if (parsed.pathname === "/") {
+    return null;
+  } else {
+    return normalizeFolderUrl(
+      `${parsed.origin}/${urlParts.slice(0, -1).join("/")}`
+    );
+  }
+}
+
+export function isTopFolder(url: string) {
+  return new URL(url).pathname === "/";
+}
+
+// Normalizes a url path by removing trailing slashes and empty path parts (eg: //)
+export function normalizeFolderUrl(url: string) {
+  const parsed = new URL(url);
+  return (
+    parsed.origin +
+    "/" +
+    parsed.pathname
+      .split("/")
+      .filter((x) => x)
+      .join("/")
+  );
+}
