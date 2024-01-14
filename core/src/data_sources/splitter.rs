64eaff9023f212b46eac0b8cbf254b0261c50192
Stanislas Polu <spolu@dust.tt>
2024-01-14 15:01:29 +0100

    core/connectors: Incident 20240112 (#3202)
    * bump tiktoken code
    
    * trim gdrive
    
    * fmt

--- core/src/data_sources/splitter.rs
+++ core/src/data_sources/splitter.rs
@@ -183,10 +183,21 @@ impl TokenizedSection {
             None => "",
         };
 
+        println!("Tokenizing section: {}", path);
+
         let (prefix, mut content) = try_join!(
             TokenizedText::from(embedder, section.prefix.as_ref()),
             TokenizedText::from(embedder, section.content.as_ref())
         )?;
+
+        match content.as_ref() {
+            Some(c) => {
+                println!("Tokenized content: tokens={}", c.tokens.len());
+            }
+            None => {
+                println!("Tokenized content: None");
+            }
+        };
 
         // Add the new prefix to the list of prefixes to be passed down children.
         match prefix.as_ref() {
@@ -1005,4 +1016,24 @@ mod tests {
             .await
             .unwrap();
     }
+
+    // #[tokio::test]
+    // async fn test_splitter_bug_20240112() {
+    //     let bstr = "\t\t\t\t\t\t\r\n";
+    //     let section = Section {
+    //         prefix: None,
+    //         content: bstr.repeat(8192).into(),
+    //         sections: vec![],
+    //     };
+
+    //     let provider_id = ProviderID::OpenAI;
+    //     let model_id = "text-embedding-ada-002";
+    //     let credentials = Credentials::from([("OPENAI_API_KEY".to_string(), "abc".to_string())]);
+
+    //     // Before the fix, this would fail (assertion failure in TokenizedSection.chunk).
+    //     splitter(SplitterID::BaseV0)
+    //         .split(credentials, provider_id, model_id, 256, section)
+    //         .await
+    //         .unwrap();
+    // }
 }
