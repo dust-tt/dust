97d74b3ba029754271454de42e1eddf92cb36de1
Flavien David <flavien@dust.tt>
2024-01-15 10:37:59 +0100

    Improve messaging + logic

--- front/dangerfile.ts
+++ front/dangerfile.ts
@@ -8,23 +8,21 @@ const modifiedModelFiles = danger.git.modified_files.filter((path) => {
 });
 
 if (modifiedModelFiles.length > 0) {
-  const migrationLabel = "migration-applied";
-  const hasMigrationLabel = danger.github.issue.labels.some(
-    (label) => label.name === migrationLabel
+  const migrationAckLabel = "migration-ack";
+  const hasMigrationAckLabel = danger.github.issue.labels.some(
+    (label) => label.name === migrationAckLabel
   );
 
-  if (!hasMigrationLabel) {
+  if (!hasMigrationAckLabel) {
     fail(
       "Files in `front/lib/models/` or `connectors/src/lib/models/` have been modified. " +
-        "A migration is required! Please ensure the PR has the `" +
-        migrationLabel +
-        "` label before merging."
+        `Please add the \`${migrationAckLabel}\` label to acknowledge that a migration will be needed once merged into 'main'.`
     );
   } else {
     warn(
       "Files in `front/lib/models/` or `connectors/src/lib/models/` have been modified and the PR has the `" +
-        migrationLabel +
-        "` label. Please double-check the migration is properly applied."
+        hasMigrationAckLabel +
+        "` label. Don't forget to run the migration from the `-edge` infrastructure."
     );
   }
 }
