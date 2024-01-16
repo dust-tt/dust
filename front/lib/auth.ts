2c81edf60ef057e22845565af14acdd98bb9a789
Stanislas Polu <spolu@dust.tt>
2024-01-16 18:19:25 +0100

    more

--- front/lib/auth.ts
+++ front/lib/auth.ts
@@ -19,11 +19,8 @@ import {
   User,
   Workspace,
 } from "@app/lib/models";
-import {
-  FREE_TEST_PLAN_DATA,
-  isUpgraded,
-  PlanAttributes,
-} from "@app/lib/plans/free_plans";
+import { FREE_TEST_PLAN_DATA, PlanAttributes } from "@app/lib/plans/free_plans";
+import { isUpgraded } from "@app/lib/plans/plan_codes";
 import { new_id } from "@app/lib/utils";
 import logger from "@app/logger/logger";
 import { authOptions } from "@app/pages/api/auth/[...nextauth]";
