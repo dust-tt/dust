d6b77be4afd1cb4ae828d953330442f1d3f3eff8
filou <philipperolet@gmail.com>
2024-01-17 11:41:13 +0100

    fixed + cleaner `renderPrefix`

--- connectors/src/lib/data_sources.ts
+++ connectors/src/lib/data_sources.ts
@@ -256,32 +256,53 @@ export async function renderPrefixSection(
       sections: [],
     };
   }
-  const tokens = (
-    await tokenize(prefix.substring(0, MAX_PREFIX_CHARS), dataSourceConfig)
-  ).map((token) => token[1]);
 
-  if (tokens.length <= MAX_PREFIX_TOKENS && prefix.length <= MAX_PREFIX_CHARS) {
+  // if too many chars in the prefix, we have to split AND check for tokens
+  if (prefix.length > MAX_PREFIX_CHARS) {
+    const tokens = (
+      await tokenize(prefix.substring(0, MAX_PREFIX_CHARS), dataSourceConfig)
+    ).map((token) => token[1]);
+
+    // if in truncated prefix, we don't have too many tokens, we can split
+    // directly via chars
+    if (tokens.length <= MAX_PREFIX_TOKENS) {
+      return {
+        prefix: prefix.substring(0, MAX_PREFIX_CHARS - 4) + "...\n", // account for the ellipsis
+        content: "..." + prefix.substring(MAX_PREFIX_CHARS - 4),
+        sections: [],
+      };
+    }
+
+    // otherwise, we split via tokens
+    const prefixTextLength = tokens
+      .slice(0, MAX_PREFIX_TOKENS)
+      .reduce((acc, t) => acc + t.length, 0);
     return {
-      prefix,
-      content: null,
-      sections: [],
-    };
-  } else if (
-    tokens.length <= MAX_PREFIX_TOKENS &&
-    prefix.length > MAX_PREFIX_CHARS
-  ) {
-    return {
-      prefix: prefix.substring(0, prefix.length - 4) + "...\n", // account for the ellipsis
-      content: `...${prefix.substring(prefix.length - 4)}`,
+      prefix: prefix.substring(0, prefixTextLength - 4) + "...\n", // account for the ellipsis
+      content: `...${prefix.substring(prefixTextLength - 4)}`,
       sections: [],
     };
   }
-  const prefixTextLength =
-    tokens.slice(0, MAX_PREFIX_TOKENS).reduce((acc, t) => acc + t.length, 0) -
-    4; // account for the ellipsis
+
+  // if not too many chars, we split only if too many tokens
+  const tokens = (await tokenize(prefix, dataSourceConfig)).map(
+    (token) => token[1]
+  );
+
+  if (tokens.length <= MAX_PREFIX_TOKENS) {
+    return {
+      prefix: prefix,
+      content: null,
+      sections: [],
+    };
+  }
+
+  const prefixTextLength = tokens
+    .slice(0, MAX_PREFIX_TOKENS)
+    .reduce((acc, t) => acc + t.length, 0);
   return {
-    prefix: prefix.substring(0, prefixTextLength) + "...\n",
-    content: `...${prefix.substring(prefixTextLength)}`,
+    prefix: prefix.substring(0, prefixTextLength - 4) + "...\n",
+    content: `...${prefix.substring(prefixTextLength - 4)}`,
     sections: [],
   };
 }
