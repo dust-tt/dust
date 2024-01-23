077295ac666fc004ec3cce8e74c3bd1f8170595d
édouard wautier <edouardwautier@Ed-Pro.local>
2024-01-23 16:35:55 +0100

    Fixing Dropdown with custom usage

--- front/components/assistant/AssistantPicker.tsx
+++ front/components/assistant/AssistantPicker.tsx
@@ -2,16 +2,15 @@ import {
   Button,
   DropdownMenu,
   Item,
+  ListIcon,
   PlusIcon,
   RobotIcon,
   Searchbar,
-  WrenchIcon,
 } from "@dust-tt/sparkle";
 import type {
   LightAgentConfigurationType,
   WorkspaceType,
 } from "@dust-tt/types";
-import { isBuilder } from "@dust-tt/types";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -22,7 +21,6 @@ export function AssistantPicker({
   assistants,
   onItemClick,
   pickerButton,
-  showBuilderButtons,
   size = "md",
 }: {
   owner: WorkspaceType;
@@ -53,59 +51,64 @@ export function AssistantPicker({
           />
         )}
       </div>
-      <DropdownMenu.Items origin="auto" width={240}>
-        {assistants.length > 7 && (
-          <div className="border-b border-structure-100 px-2 pb-2 pt-1">
-            <Searchbar
-              placeholder="Search"
-              name="input"
-              size="xs"
-              value={searchText}
-              onChange={setSearchText}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && searchedAssistants.length > 0) {
-                  onItemClick(searchedAssistants[0]);
-                  setSearchText("");
-                }
-              }}
-            />
-          </div>
-        )}
-        <div className="max-h-[22.5rem] overflow-y-auto [&>*]:w-full">
-          {searchedAssistants.map((c) => (
-            <Item.Avatar
-              key={`assistant-picker-${c.sId}`}
-              label={"@" + c.name}
-              visual={c.pictureUrl}
-              hasAction={false}
-              className="px-3"
-              onClick={() => {
-                onItemClick(c);
-                setSearchText("");
-              }}
-            />
-          ))}
-        </div>
-        {isBuilder(owner) && showBuilderButtons && (
-          <div className="flex flex-row justify-between border-t border-structure-100 px-3 pb-1 pt-2">
+      <DropdownMenu.Items
+        origin="auto"
+        width={240}
+        topBar={
+          <>
+            {assistants.length > 7 && (
+              <div className="flex flex-grow flex-row border-b border-structure-50 p-2">
+                <Searchbar
+                  placeholder="Search"
+                  name="input"
+                  size="xs"
+                  value={searchText}
+                  onChange={setSearchText}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && searchedAssistants.length > 0) {
+                      onItemClick(searchedAssistants[0]);
+                      setSearchText("");
+                    }
+                  }}
+                />
+              </div>
+            )}
+          </>
+        }
+        bottomBar={
+          <div className="flex border-t border-structure-50 p-2">
             <Link href={`/w/${owner.sId}/builder/assistants/new`}>
               <Button
                 label="Create"
                 size="xs"
-                variant="secondary"
+                variant="primary"
                 icon={PlusIcon}
               />
             </Link>
-            <Link href={`/w/${owner.sId}/builder/assistants`}>
+            <div className="s-flex-grow" />
+            <Link href={`/w/${owner.sId}/assistant/assistants`}>
               <Button
-                label="Manage"
+                label="My Assistant"
                 size="xs"
                 variant="tertiary"
-                icon={WrenchIcon}
+                icon={ListIcon}
               />
             </Link>
           </div>
-        )}
+        }
+      >
+        {searchedAssistants.map((c) => (
+          <Item.Avatar
+            key={`assistant-picker-${c.sId}`}
+            label={"@" + c.name}
+            visual={c.pictureUrl}
+            hasAction={false}
+            onClick={() => {
+              onItemClick(c);
+              setSearchText("");
+            }}
+          />
+        ))}
       </DropdownMenu.Items>
     </DropdownMenu>
   );
