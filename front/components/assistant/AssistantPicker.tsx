f4e814812a82652047c564294412be0f89c9d264
Alban Dumouilla <alban@selecteev.io>
2024-01-24 12:50:32 +0100

    Move ellipsis to action in storybook

--- front/components/assistant/AssistantPicker.tsx
+++ front/components/assistant/AssistantPicker.tsx
@@ -90,7 +90,7 @@ export function AssistantPicker({
             />
           ))}
         </div>
-        {isBuilder(owner) && showBuilderButtons && (
+        {showBuilderButtons && (
           <div className="flex flex-row justify-between border-t border-structure-100 px-3 pb-1 pt-2">
             <Link href={`/w/${owner.sId}/builder/assistants/new?flow=personal_assistants`}>
               <Button
