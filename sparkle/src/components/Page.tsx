271dca4320776cde88ef22380c09ec7cf7ade440
Flavien David <flavien@dust.tt>
2024-01-26 15:52:36 +0100

    :sparkles:

--- sparkle/src/components/Page.tsx
+++ sparkle/src/components/Page.tsx
@@ -16,7 +16,7 @@ Page.defaultProps = {
 
 export function Page({ children, variant }: PageProps) {
   const mainVariantClasses =
-    variant === "normal" ? "s-h-full s-py-16" : "s-h-full s-py-6";
+    variant === "normal" ? "s-h-full s-py-16" : "s-h-full s-py-4";
   const divVariantClassNames =
     variant === "normal" ? "s-gap-6 s-px-6" : "s-gap-4";
 
