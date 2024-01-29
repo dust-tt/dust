521f91bd8ad66ed9903ace31bd6c81133cd4f1d4
Henry Fontanier <h.fontanier@gmail.com>
2024-01-29 14:22:00 +0100

    fix: properly delete cache entries when cleaning up notion conn (#3478)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

---
+++ connectors/migrations/20240129_cleanup_orphaned_notion_cache.ts
@@ -0,0 +1,20 @@
+import { sequelize_conn } from "@connectors/lib/models";
+
+async function main() {
+  await sequelize_conn.query(`
+        DELETE FROM notion_connector_page_cache_entries WHERE "connectorId" IS NULL;
+        DELETE FROM notion_connector_block_cache_entries WHERE "connectorId" IS NULL;
+        DELETE FROM notion_connector_resources_to_check_cache_entries WHERE "connectorId" IS NULL;
+    `);
+}
+
+main()
+  .then(() => {
+    console.error("\x1b[32m%s\x1b[0m", `Done`);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error("\x1b[31m%s\x1b[0m", `Error: ${err.message}`);
+    console.log(err);
+    process.exit(1);
+  });
