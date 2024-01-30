26d911fc1d25a31407bcb60afaf0956e82ddf403
Daphné Popin <PopDaph@users.noreply.github.com>
2024-01-30 14:42:24 +0100

    Feature flag: Allow all in dev by default (#3506)

--- front/lib/api/feature_flags.ts
+++ front/lib/api/feature_flags.ts
@@ -1,11 +1,18 @@
 import type { WhitelistableFeature, WorkspaceType } from "@dust-tt/types";
+import { WHITELISTABLE_FEATURES } from "@dust-tt/types";
 
+import { isDevelopment } from "@app/lib/development";
 import { FeatureFlag } from "@app/lib/models/feature_flag";
+
+const { ACTIVATE_ALL_FEATURES_DEV = true } = process.env;
 
 export async function isFeatureEnabled(
   owner: WorkspaceType,
   feature: WhitelistableFeature
 ): Promise<boolean> {
+  if (ACTIVATE_ALL_FEATURES_DEV && isDevelopment()) {
+    return true;
+  }
   return !!(await FeatureFlag.count({
     where: {
       workspaceId: owner.id,
@@ -17,11 +24,13 @@ export async function isFeatureEnabled(
 export async function getFeatureFlags(
   owner: WorkspaceType
 ): Promise<WhitelistableFeature[]> {
+  if (ACTIVATE_ALL_FEATURES_DEV && isDevelopment()) {
+    return [...WHITELISTABLE_FEATURES];
+  }
   const flags = await FeatureFlag.findAll({
     where: {
       workspaceId: owner.id,
     },
   });
-
   return flags.map((flag) => flag.name);
 }
