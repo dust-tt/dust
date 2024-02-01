d04781a6fdf4022c63ff19a5494bf75cef4436b4
Flavien David <flavien@dust.tt>
2024-02-01 09:45:27 +0100

    Add Hoverable component

---
+++ sparkle/src/components/Hoverable.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ReactNode } from "react";
+
+import { classNames } from "@sparkle/lib/utils";
+
+interface HoverableProps {
+  children: ReactNode;
+  className: string;
+  onClick: () => void;
+}
+
+export function Hoverable({ children, className, onClick }: HoverableProps) {
+  return (
+    <span
+      className={classNames(
+        "s-cursor-pointer s-duration-300 hover:s-text-action-500 active:s-text-action-600",
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </span>
+  );
+}
