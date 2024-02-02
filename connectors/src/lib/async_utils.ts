6cb9d500b3cedf5f3cbc891c7629c46d024d556c
Flavien David <flavien.david74@gmail.com>
2024-02-02 07:31:52 +0100

    Use concurrentExecutor in GDrive permissions view (#3459)
    * Use concurrentExecutor in GDrive permissions view
    
    * :see_no_evil:
    
    * Use count instead of findOne

--- connectors/src/lib/async_utils.ts
+++ connectors/src/lib/async_utils.ts
@@ -1,18 +1,20 @@
 import PQueue from "p-queue";
 
-export async function concurrentExecutor<T>(
+export async function concurrentExecutor<T, V>(
   items: T[],
-  iterator: (item: T, idx: number) => Promise<void>,
+  iterator: (item: T, idx: number) => Promise<V>,
   { concurrency = 8 }: { concurrency: number }
 ) {
   const queue = new PQueue({ concurrency });
-
-  const promises = [];
+  const promises: Promise<V>[] = [];
 
   for (const [idx, item] of items.entries()) {
     // Queue each task. The queue manages concurrency.
     // Each task is wrapped in a promise to capture its completion.
-    promises.push(queue.add(async () => iterator(item, idx)));
+    const p = queue.add(async () => iterator(item, idx));
+
+    // Cast the promise to Promise<R> to fix the return type from P-Queue.
+    promises.push(p as Promise<V>);
   }
 
   // `Promise.all` will throw at the first rejection, but all tasks will continue to process.
