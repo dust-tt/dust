5287c2e5fdd6f542ed06fa8007f77424a299cbf1
Flavien David <flavien.david74@gmail.com>
2024-02-05 12:15:10 +0100

    Refactor AssistantPreview2 (#3580)
    * Refactor AssistantPreview2
    
    * :scissors:
    
    * :sparkles:
    
    * Bump sparkle
    
    * Set description's height to 48 px.

--- sparkle/src/components/AssistantPreview2.tsx
+++ sparkle/src/components/AssistantPreview2.tsx
@@ -6,15 +6,33 @@ import { classNames } from "@sparkle/lib/utils";
 
 type AssistantPreviewVariant = "item" | "list" | "gallery";
 
-interface AssistantPreviewProps {
+interface BaseAssistantPreviewProps {
   variant: AssistantPreviewVariant;
   description: string;
   title: string;
   pictureUrl: string;
-  actions?: React.ReactNode;
-  subtitle?: string;
+  subtitle: string;
   onClick?: () => void;
 }
+
+type ItemVariantAssistantPreviewProps = BaseAssistantPreviewProps & {
+  variant: "item";
+  actions: React.ReactNode;
+};
+
+type ListVariantAssistantPreviewProps = BaseAssistantPreviewProps & {
+  variant: "list";
+};
+
+type GalleryVariantAssistantPreviewProps = BaseAssistantPreviewProps & {
+  variant: "gallery";
+  renderActions?: (isHovered: boolean) => React.ReactNode;
+};
+
+type AssistantPreviewProps =
+  | ItemVariantAssistantPreviewProps
+  | ListVariantAssistantPreviewProps
+  | GalleryVariantAssistantPreviewProps;
 
 const titleClassNames = {
   base: "s-truncate s-font-medium s-text-element-900 s-w-full",
@@ -30,22 +48,176 @@ const subtitleClassNames = {
 };
 const descriptionClassNames = {
   base: "s-font-normal s-mb-1",
-  item: "s-text-xs s-text-element-700  s-pl-1",
+  item: "s-text-xs s-text-element-700 s-pl-1 s-line-clamp-3 s-h-12",
   list: "s-text-base s-text-element-800",
   gallery: "s-text-base s-text-element-800",
 };
 
-export function AssistantPreview2({
-  variant,
-  title,
-  subtitle,
-  pictureUrl,
-  description,
-  actions,
-  onClick,
-}: AssistantPreviewProps) {
+function renderVariantContent(
+  props: AssistantPreviewProps & { isHovered: boolean }
+) {
+  switch (props.variant) {
+    case "item":
+      return <ItemVariantContent {...props} />;
+    case "list":
+      return <ListVariantContent {...props} />;
+    case "gallery":
+      return <GalleryVariantContent {...props} />;
+    default:
+      return <></>;
+  }
+}
+
+const ItemVariantContent = (props: ItemVariantAssistantPreviewProps) => {
+  const { actions, description, title, pictureUrl, subtitle } = props;
+
+  return (
+    <>
+      <div className="s-flex s-gap-2">
+        <Avatar
+          visual={<img src={pictureUrl} alt={`Avatar of ${title}`} />}
+          size="sm"
+        />
+        <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-2">
+          <div className="s-flex s-w-full s-min-w-0 s-flex-row s-gap-3">
+            <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-0">
+              <div
+                className={classNames(
+                  titleClassNames["base"],
+                  titleClassNames.item
+                )}
+              >
+                @{title}
+              </div>
+              <div
+                className={classNames(
+                  subtitleClassNames["base"],
+                  subtitleClassNames.item
+                )}
+              >
+                By: {subtitle}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={classNames(
+          descriptionClassNames["base"],
+          descriptionClassNames.item
+        )}
+      >
+        {description}
+      </div>
+      {actions}
+    </>
+  );
+};
+
+const ListVariantContent = (props: ListVariantAssistantPreviewProps) => {
+  const { description, title, pictureUrl, subtitle } = props;
+
+  return (
+    <div className="s-flex s-gap-2">
+      <Avatar
+        visual={<img src={pictureUrl} alt={`Avatar of ${title}`} />}
+        size="md"
+      />
+      <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-2">
+        <div className="s-flex s-w-full s-min-w-0 s-flex-row s-gap-3">
+          <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-0">
+            <div
+              className={classNames(
+                titleClassNames["base"],
+                titleClassNames.list
+              )}
+            >
+              @{title}
+            </div>
+            <div
+              className={classNames(
+                subtitleClassNames["base"],
+                subtitleClassNames.list
+              )}
+            >
+              By: {subtitle}
+            </div>
+          </div>
+        </div>
+        <div
+          className={classNames(
+            descriptionClassNames["base"],
+            descriptionClassNames.list
+          )}
+        >
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const GalleryVariantContent = (
+  props: GalleryVariantAssistantPreviewProps & { isHovered: boolean }
+) => {
+  const { renderActions, description, title, pictureUrl, subtitle, isHovered } =
+    props;
+
+  return (
+    <div className="s-flex s-gap-2">
+      <Avatar
+        visual={<img src={pictureUrl} alt={`Avatar of ${title}`} />}
+        size="md"
+      />
+      <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-2">
+        <div className="s-flex s-w-full s-min-w-0 s-flex-row s-gap-3">
+          <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-0">
+            <div
+              className={classNames(
+                titleClassNames["base"],
+                titleClassNames.gallery
+              )}
+            >
+              @{title}
+            </div>
+            <div
+              className={classNames(
+                subtitleClassNames["base"],
+                subtitleClassNames.gallery
+              )}
+            >
+              By: {subtitle}
+            </div>
+          </div>
+          {isHovered && (
+            <Button
+              variant="primary"
+              size="sm"
+              label="Try"
+              labelVisible={false}
+              icon={PlayIcon}
+            />
+          )}
+        </div>
+        {renderActions && renderActions(isHovered)}
+        <div
+          className={classNames(
+            descriptionClassNames["base"],
+            descriptionClassNames.gallery
+          )}
+        >
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export function AssistantPreview2(props: AssistantPreviewProps) {
+  const { onClick, variant } = props;
   // State to manage the visibility of the play button
   const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
       className={classNames(
@@ -56,70 +228,7 @@ export function AssistantPreview2({
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="s-flex s-gap-2">
-        <Avatar
-          visual={<img src={pictureUrl} alt={`Avatar of ${title}`} />}
-          size={variant === "item" ? "sm" : "md"}
-        />
-        <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-2">
-          <div className="s-flex s-w-full s-min-w-0 s-flex-row s-gap-3">
-            <div className="s-flex s-w-full s-min-w-0 s-flex-col s-items-start s-gap-0">
-              <div
-                className={classNames(
-                  titleClassNames["base"],
-                  titleClassNames[variant]
-                )}
-              >
-                @{title}
-              </div>
-              {variant !== "item" && (
-                <div
-                  className={classNames(
-                    subtitleClassNames["base"],
-                    subtitleClassNames[variant]
-                  )}
-                >
-                  By: {subtitle}
-                </div>
-              )}
-              {variant === "item" && <div className="s-pt-1">{actions}</div>}
-            </div>
-            {variant === "gallery" &&
-              isHovered && ( // Conditional rendering based on isHovered
-                <Button
-                  variant="primary"
-                  size="sm"
-                  label="Try"
-                  labelVisible={false}
-                  icon={PlayIcon}
-                />
-              )}
-          </div>
-          {variant !== "item" && (
-            <>
-              {actions}
-              <div
-                className={classNames(
-                  descriptionClassNames["base"],
-                  descriptionClassNames[variant]
-                )}
-              >
-                {description}
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-      {variant === "item" && (
-        <div
-          className={classNames(
-            descriptionClassNames["base"],
-            descriptionClassNames[variant]
-          )}
-        >
-          {description}
-        </div>
-      )}
+      {renderVariantContent({ ...props, isHovered })}
     </div>
   );
 }
