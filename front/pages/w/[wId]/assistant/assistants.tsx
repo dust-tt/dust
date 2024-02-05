87c03db543019ab9a5dc3e1c0b737c28b8142118
filou <philipperolet@gmail.com>
2024-02-05 16:49:08 +0100

    [Ahuna] Delete button in menu in assistant detail view

--- front/pages/w/[wId]/assistant/assistants.tsx
+++ front/pages/w/[wId]/assistant/assistants.tsx
@@ -181,7 +181,7 @@ export default function PersonalAssistants({
           show={showDetails !== null}
           onClose={({ shouldMutateAgentConfigurations }) => {
             setShowDetails(null);
-            shouldMutateAgentConfigurations && void mutateAgentConfigurations();
+            void mutateAgentConfigurations();
           }}
         />
       )}
