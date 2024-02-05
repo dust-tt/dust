161e35c878427b0720f78e28a2b01605613cb241
Flavien David <flavien.david74@gmail.com>
2024-02-05 17:14:05 +0100

    Refactor assistant gallery (#3586)
    * Refactor assistant gallery
    
    * Bump sparkle
    
    * Address comments from review
    
    * Only allow add/remove list on shared

---
+++ front/components/assistant/AssistantListActions.tsx
@@ -0,0 +1,104 @@
+import {
+  Button,
+  Chip,
+  ListAddIcon,
+  ListIcon,
+  ListRemoveIcon,
+} from "@dust-tt/sparkle";
+import type {
+  AgentUserListStatus,
+  LightAgentConfigurationType,
+  WorkspaceType,
+} from "@dust-tt/types";
+import React, { useContext, useState } from "react";
+
+import { SendNotificationsContext } from "@app/components/sparkle/Notification";
+import { updateAgentUserListStatus } from "@app/lib/client/dust_api";
+import { classNames } from "@app/lib/utils";
+
+interface AssistantListActions {
+  agentConfiguration: LightAgentConfigurationType;
+  owner: WorkspaceType;
+  isParentHovered: boolean;
+}
+
+export default function AssistantListActions({
+  agentConfiguration,
+  isParentHovered,
+  owner,
+}: AssistantListActions) {
+  const { scope } = agentConfiguration;
+
+  const sendNotification = useContext(SendNotificationsContext);
+
+  // Use the function to set the initial state.
+  const [isAdded, setIsAdded] = useState(
+    () => agentConfiguration.userListStatus === "in-list"
+  );
+
+  if (scope !== "published") {
+    return null;
+  }
+
+  const updateList = async (listStatus: AgentUserListStatus) => {
+    const { success, errorMessage } = await updateAgentUserListStatus({
+      owner,
+      agentConfigurationId: agentConfiguration.sId,
+      listStatus,
+    });
+
+    if (success) {
+      setIsAdded(listStatus === "in-list");
+      sendNotification({
+        title: `Assistant sharing updated.`,
+        type: "success",
+      });
+    } else {
+      sendNotification({
+        title: `Error updating assistant sharing.`,
+        description: errorMessage,
+        type: "error",
+      });
+    }
+  };
+
+  return (
+    <div className="group">
+      {isAdded && (
+        <Chip
+          label="In my list"
+          icon={ListIcon}
+          className={isAdded ? "group-hover:hidden" : "hidden"}
+        />
+      )}
+      <div className={classNames("hidden", isAdded ? "group-hover:block" : "")}>
+        <Button
+          label={"Remove from my list"}
+          size="xs"
+          hasMagnifying={false}
+          variant="tertiary"
+          icon={ListRemoveIcon}
+          onClick={(e) => {
+            e.stopPropagation();
+            return updateList("not-in-list");
+          }}
+        />
+      </div>
+      <div
+        className={isParentHovered && !isAdded ? "group-hover:block" : "hidden"}
+      >
+        <Button
+          label={"Add to my list"}
+          size="xs"
+          hasMagnifying={false}
+          variant="tertiary"
+          icon={ListAddIcon}
+          onClick={(e) => {
+            e.stopPropagation();
+            return updateList("in-list");
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- front/components/assistant/GalleryAssistantPreviewContainer.tsx
+++ front/components/assistant/GalleryAssistantPreviewContainer.tsx
@@ -1,17 +1,15 @@
-import { AssistantPreview } from "@dust-tt/sparkle";
+import { AssistantPreview2 } from "@dust-tt/sparkle";
 import type {
-  AgentUserListStatus,
   LightAgentConfigurationType,
   PlanType,
   WorkspaceType,
 } from "@dust-tt/types";
-import { useContext, useEffect, useState } from "react";
+import type { SyntheticEvent } from "react";
 
-import type { NotificationType } from "@app/components/sparkle/Notification";
-import { SendNotificationsContext } from "@app/components/sparkle/Notification";
+import AssistantListActions from "@app/components/assistant/AssistantListActions";
+import { SharingChip } from "@app/components/assistant/Sharing";
 import { isLargeModel } from "@app/lib/assistant";
 import { isUpgraded } from "@app/lib/plans/plan_codes";
-import type { PostAgentListStatusRequestBody } from "@app/pages/api/w/[wId]/members/me/agent_list_status";
 
 type AssistantPreviewFlow = "personal" | "workspace";
 
@@ -27,172 +25,51 @@ interface GalleryAssistantPreviewContainerProps {
   ) => void;
 }
 
-const useAssistantUpdate = (
-  agentConfiguration: LightAgentConfigurationType,
-  owner: WorkspaceType,
-  sendNotification: (notification: NotificationType) => void,
-  onSuccess: (isAdded: boolean) => void,
-  onError: () => void
-) => {
-  const updateAssistant = async (
-    action: "added" | "removed",
-    flow: AssistantPreviewFlow,
-    listStatus: AgentUserListStatus,
-    scope: "workspace" | "published"
-  ) => {
-    const isAdding = action === "added";
-    const url =
-      flow === "workspace"
-        ? `/api/w/${owner.sId}/assistant/agent_configurations/${agentConfiguration.sId}/scope`
-        : `/api/w/${owner.sId}/members/me/agent_list_status`;
-
-    const fullAssistantRes = await fetch(
-      `/api/w/${owner.sId}/assistant/agent_configurations/${agentConfiguration.sId}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    if (!fullAssistantRes.ok) {
-      throw new Error(
-        (await fullAssistantRes.json()).error.message ?? "Error fetching"
-      );
-    }
-
-    const body:
-      | { scope: "workspace" | "published" }
-      | PostAgentListStatusRequestBody =
-      flow === "workspace"
-        ? {
-            scope,
-          }
-        : {
-            agentId: agentConfiguration.sId,
-            listStatus,
-          };
-
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      });
-
-      if (!response.ok) {
-        throw new Error((await response.json()).error.message);
-      }
-
-      sendNotification({
-        title: `Assistant ${isAdding ? "added to" : "removed from"} ${
-          flow === "workspace" ? "Workspace" : "your"
-        } list`,
-        type: "success",
-      });
-
-      onSuccess(isAdding);
-    } catch (error) {
-      sendNotification({
-        title: `Error ${isAdding ? "adding" : "removing"} Assistant`,
-        description: error instanceof Error ? error.message : String(error),
-        type: "error",
-      });
-      onError();
-    }
-  };
-
-  return updateAssistant;
-};
-
 export function GalleryAssistantPreviewContainer({
   agentConfiguration,
   flow,
   onShowDetails,
-  onUpdate,
   owner,
   plan,
   setTestModalAssistant,
 }: GalleryAssistantPreviewContainerProps) {
-  const [isUpdatingList, setIsUpdatingList] = useState(false);
-
-  // Function to determine if the assistant is added based on the flow and configuration.
-  const determineIfAdded = (
-    agentConfiguration: LightAgentConfigurationType,
-    currentFlow: AssistantPreviewFlow
-  ) => {
-    return currentFlow === "personal"
-      ? agentConfiguration.userListStatus === "in-list"
-      : agentConfiguration.scope === "workspace";
+  const handleTestClick = (e: SyntheticEvent) => {
+    e.stopPropagation();
+    setTestModalAssistant?.(agentConfiguration);
   };
-
-  // Use the function to set the initial state.
-  const [isAdded, setIsAdded] = useState(() =>
-    determineIfAdded(agentConfiguration, flow)
-  );
-
-  // Effect to update isAdded when agentConfiguration or flow changes.
-  useEffect(() => {
-    setIsAdded(determineIfAdded(agentConfiguration, flow));
-  }, [agentConfiguration, flow]);
-
-  const handleSuccess = (isAdded: boolean) => {
-    setIsAdded(isAdded);
-    setIsUpdatingList(false);
-    onUpdate();
-  };
-
-  const handleError = () => {
-    setIsUpdatingList(false);
-  };
-
-  const updateStatus = useAssistantUpdate(
-    agentConfiguration,
-    owner,
-    useContext(SendNotificationsContext),
-    handleSuccess,
-    handleError
-  );
-
-  const handleUpdate = async (action: "added" | "removed") => {
-    const isAdding = action === "added";
-    const newScope = isAdding ? "workspace" : "published";
-    const newListStatus = isAdding ? "in-list" : "not-in-list";
-
-    setIsUpdatingList(true);
-
-    return updateStatus(action, flow, newListStatus, newScope);
-  };
-
-  const handleTestClick = () => setTestModalAssistant?.(agentConfiguration);
 
   const { description, generation, lastAuthors, name, pictureUrl, scope } =
     agentConfiguration;
 
   const isGlobal = scope === "global";
-  const isAddedToWorkspace = flow === "workspace" && isAdded;
+  const isPersonalFlow = flow === "personal";
   const hasAccessToLargeModels = isUpgraded(plan);
   const eligibleForTesting =
     hasAccessToLargeModels || !isLargeModel(generation?.model);
-  const isTestable = !isGlobal && !isAdded && eligibleForTesting;
+  const isTestable = !isGlobal && eligibleForTesting;
   return (
-    <AssistantPreview
-      allowAddAction={!isGlobal}
-      allowRemoveAction={!isGlobal && !isAddedToWorkspace}
-      description={description}
-      isAdded={isAdded}
-      isUpdatingList={isUpdatingList}
-      isWorkspace={flow === "workspace"}
-      name={name}
+    <AssistantPreview2
+      title={name}
       pictureUrl={pictureUrl}
       subtitle={lastAuthors?.join(", ") ?? ""}
-      variant="lg"
-      onUpdate={handleUpdate}
-      onTestClick={isTestable ? handleTestClick : undefined}
-      onShowDetailsClick={onShowDetails}
+      description={description}
+      variant="gallery"
+      onClick={onShowDetails}
+      onPlayClick={isTestable ? handleTestClick : undefined}
+      renderActions={(isParentHovered) => {
+        return (
+          <div className="s-flex s-gap-2">
+            <SharingChip scope={scope} />
+            {isPersonalFlow && (
+              <AssistantListActions
+                agentConfiguration={agentConfiguration}
+                isParentHovered={isParentHovered}
+                owner={owner}
+              />
+            )}
+          </div>
+        );
+      }}
     />
   );
 }
