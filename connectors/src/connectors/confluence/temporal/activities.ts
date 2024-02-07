09dbbc8ae2382e6d6b0be40e7249a4cb8c92dab3
Flavien David <flavien@dust.tt>
2024-02-07 18:13:39 +0100

    Use local logger in Confluence

--- connectors/src/connectors/confluence/temporal/activities.ts
+++ connectors/src/connectors/confluence/temporal/activities.ts
@@ -388,7 +388,7 @@ export async function confluenceGetRootPageIdActivity({
   // TODO(2024-01-31 flav) Find a better way to deal with spaces
   // that have many root pages.
   if (rootPages.length > 1) {
-    logger.error("Found Confluence space with many root pages.", {
+    localLogger.error("Found Confluence space with many root pages.", {
       rootPagesCount: rootPages.length,
     });
   }
