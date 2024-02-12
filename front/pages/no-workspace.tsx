7dba752601014810dcda6b8583a19172731af0ea
Flavien David <flavien@dust.tt>
2024-02-12 12:59:43 +0100

    Revert "Handle existing workspace with verified domain in sign up flow (#3680)"
    This reverts commit 7897d6c9026a1028516103fb40c44a3b9e529bb7.

--- front/pages/no-workspace.tsx
+++ front/pages/no-workspace.tsx
@@ -5,60 +5,17 @@ import {
   LogoSquareColorLogo,
   Page,
 } from "@dust-tt/sparkle";
-import type { UserTypeWithWorkspaces } from "@dust-tt/types";
 import type { InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 
 import { getSession, getUserFromSession } from "@app/lib/auth";
-import { Membership, Workspace, WorkspaceHasDomain } from "@app/lib/models";
+import { Membership, Workspace } from "@app/lib/models";
 import logger from "@app/logger/logger";
 import { withGetServerSidePropsLogging } from "@app/logger/withlogging";
 
-// Fetch workspace details for scenarios where auto-join is disabled.
-async function fetchWorkspaceDetails(
-  user: UserTypeWithWorkspaces
-): Promise<WorkspaceHasDomain | null> {
-  const [, userEmailDomain] = user.email.split("@");
-  const workspaceWithVerifiedDomain = await WorkspaceHasDomain.findOne({
-    where: {
-      domain: userEmailDomain,
-    },
-    include: [
-      {
-        model: Workspace,
-        as: "workspace",
-        required: true,
-      },
-    ],
-  });
-
-  return workspaceWithVerifiedDomain;
-}
-
-// Fetch workspace details for users whose membership has been revoked.
-async function fetchRevokedWorkspace(
-  user: UserTypeWithWorkspaces
-): Promise<Workspace | null> {
-  const memberships = await Membership.findAll({
-    where: { userId: user.id },
-  });
-
-  if (!memberships.length) {
-    const message = "Unreachable: user has no memberships.";
-    logger.error({ userId: user.id, panic: true }, message);
-    throw new Error(message);
-  }
-
-  const revokedWorkspaceId = memberships[0].workspaceId;
-
-  return Workspace.findByPk(revokedWorkspaceId);
-}
-
 export const getServerSideProps = withGetServerSidePropsLogging<{
-  status: "auto-join-disabled" | "revoked";
+  revokedWorkspaceName?: string;
   userFirstName: string;
-  workspaceName: string;
-  workspaceVerifiedDomain: string | null;
 }>(async (context) => {
   const session = await getSession(context.req, context.res);
   const user = await getUserFromSession(session);
@@ -69,65 +26,48 @@ export const getServerSideProps = withGetServerSidePropsLogging<{
     };
   }
 
+  const memberships = await Membership.findAll({
+    where: { userId: user.id },
+  });
+
+  if (!memberships.length) {
+    const message = "Unreachable: user has no memberships";
+    logger.error({ userId: user.id, panic: true }, message);
+    throw new Error(message);
+  }
+
   if (user.workspaces.length) {
-    const message = "Unreachable: user already has a workspace.";
+    const message = "Unreachable: user already has a workspace";
     logger.error({ userId: user.id, panic: true }, message);
     return {
       notFound: true,
     };
   }
 
-  const flow =
-    context.query.flow && typeof context.query.flow === "string"
-      ? context.query.flow
-      : null;
-  let workspace: Workspace | null = null;
-  let workspaceVerifiedDomain: string | null = null;
-  let status: "auto-join-disabled" | "revoked";
+  const revokedWorkspaceId = memberships[0].workspaceId;
 
-  if (flow === "no-auto-join") {
-    status = "auto-join-disabled";
-    const workspaceHasDomain = await fetchWorkspaceDetails(user);
-    workspace = workspaceHasDomain?.workspace ?? null;
-    workspaceVerifiedDomain = workspaceHasDomain?.domain ?? null;
+  const workspace = await Workspace.findByPk(revokedWorkspaceId);
 
-    if (!workspace || !workspaceVerifiedDomain) {
-      logger.error(
-        { userId: user.id, panic: true },
-        "Unreachable: workspace not found."
-      );
-      throw new Error("Workspace not found.");
-    }
-  } else if (flow === "revoked") {
-    status = "revoked";
-    workspace = await fetchRevokedWorkspace(user);
-
-    if (!workspace) {
-      logger.error(
-        { userId: user.id, panic: true },
-        "Unreachable: workspace not found."
-      );
-      throw new Error("Workspace not found.");
-    }
-  } else {
-    throw new Error("No workspace found.");
+  if (!workspace) {
+    const message = "Unreachable: workspace not found";
+    logger.error(
+      { userId: user.id, workspaceId: revokedWorkspaceId, panic: true },
+      message
+    );
+    throw new Error(message);
   }
 
   return {
     props: {
-      status,
+      revokedWorkspaceName: workspace.name,
       userFirstName: user.firstName,
-      workspaceName: workspace.name,
-      workspaceVerifiedDomain,
     },
   };
 });
 
 export default function NoWorkspace({
-  status,
+  revokedWorkspaceName,
   userFirstName,
-  workspaceName,
-  workspaceVerifiedDomain,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
 
@@ -169,47 +109,26 @@ export default function NoWorkspace({
           </span>
         </div>
         <div>
-          {status === "auto-join-disabled" && (
-            <div className="flex flex-col gap-4">
-              <span className="text-lg font-semibold text-element-700">
-                {workspaceVerifiedDomain ?? workspaceName} already has a Dust
-                workspace.
-              </span>
-              <span className="text-md text-element-700">
-                To join the existing workspace of your company, <br />
-                <span className="font-semibold">
-                  please request an invitation from your colleagues,{" "}
-                </span>{" "}
-                then use the link provided in the invitation email to access the
-                workspace.
-              </span>
-              <span className="text-md text-element-700">
-                If you're looking to establish a new, separate workspace,
-                continue with the following step:
-              </span>
-            </div>
-          )}
-          {status === "revoked" && (
-            <div className="flex flex-col gap-4">
-              <span className="text-lg font-semibold text-element-700">
-                You no longer have access to {workspaceName}'s Dust workspace.
-              </span>
-              <span className="text-md text-element-700">
-                You may have been removed from the workspace or the workspace
-                may have reached its maximum number of users.
-                <br />
-                Please{" "}
-                <span className="font-semibold">
-                  contact the administrator in {workspaceName}
-                </span>{" "}
-                for more informations or to add you again.
-              </span>
-              <span className="text-md text-element-700">
-                If you're looking to establish a new, separate workspace,
-                continue with the following step:
-              </span>
-            </div>
-          )}
+          <div className="flex flex-col gap-4">
+            <span className="text-lg font-semibold text-element-700">
+              You no longer have access to {revokedWorkspaceName}'s Dust
+              workspace.
+            </span>
+            <span className="text-md text-element-700">
+              You may have been removed from the workspace or the workspace may
+              have reached its maximum number of users.
+              <br />
+              Please{" "}
+              <span className="font-semibold">
+                contact the administrator in {revokedWorkspaceName}
+              </span>{" "}
+              for more informations or to add you again.
+            </span>
+            <span className="text-md text-element-700">
+              If you're looking to establish a new, separate workspace, continue
+              with the following step:
+            </span>
+          </div>
         </div>
         <div className="flex flex-row justify-end">
           <Button
