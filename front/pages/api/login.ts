bb0bbe8b323f69fa394fbf5b52a25f7847a4341c
Flavien David <flavien@dust.tt>
2024-02-12 12:59:38 +0100

    Revert "Flav/sign up with invitation link (#3684)"
    This reverts commit 81939a13443e74e4422696169c0bd11da30a2505.

--- front/pages/api/login.ts
+++ front/pages/api/login.ts
@@ -192,12 +192,12 @@ async function handler(
     );
 
     // Redirect the user to a different screen if a workspace with
-    // a verified domain exists but auto join is not enabled and there is no membership invite.
+    // a verified domain exists but auto join is not enabled.
     const workspaceHasAutoJoin =
       workspaceWithVerifiedDomain?.domainAutoJoinEnabled === true;
     if (workspaceHasAutoJoin) {
       autoJoinWorkspaceWithDomain = workspaceWithVerifiedDomain.workspace;
-    } else if (workspaceWithVerifiedDomain && !membershipInvite) {
+    } else if (workspaceWithVerifiedDomain) {
       res.redirect("/no-workspace?flow=no-auto-join");
       return;
     }
