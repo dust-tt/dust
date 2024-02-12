20ab9928029f02dd01d3fbd605c03669707867f1
Henry Fontanier <h.fontanier@gmail.com>
2024-02-12 16:15:30 +0100

    feat: add ability to search datasources from poke (#3691)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- front/pages/api/w/[wId]/data_sources/[name]/search.ts
+++ front/pages/api/w/[wId]/data_sources/[name]/search.ts
@@ -40,7 +40,7 @@ const searchQuerySchema: JSONSchemaType<DatasourceSearchQuery> = {
   required: ["query", "top_k", "full_text"],
 };
 
-type DatasourceSearchResponseBody = {
+export type DatasourceSearchResponseBody = {
   documents: Array<DocumentType>;
 };
 
@@ -65,88 +65,9 @@ async function handler(
     });
   }
 
-  const dataSource = await getDataSource(auth, req.query.name as string);
-
-  if (!dataSource) {
-    return apiError(req, res, {
-      status_code: 404,
-      api_error: {
-        type: "data_source_not_found",
-        message: "The data source you requested was not found.",
-      },
-    });
-  }
-
   switch (req.method) {
     case "GET": {
-      // I could not find a way to make the query params be an array if there is only one tag.
-      if (req.query.tags_in && typeof req.query.tags_in === "string") {
-        req.query.tags_in = [req.query.tags_in];
-      }
-      if (req.query.tags_not && typeof req.query.tags_not === "string") {
-        req.query.tags_not = [req.query.tags_not];
-      }
-      const requestPayload = req.query;
-
-      // Dust managed credentials: all data sources.
-      const credentials = dustManagedCredentials();
-
-      const searchQueryRes = parse_payload(searchQuerySchema, requestPayload);
-
-      if (searchQueryRes.isErr()) {
-        const err = searchQueryRes.error;
-        return apiError(req, res, {
-          status_code: 400,
-          api_error: {
-            type: "invalid_request_error",
-            message: `Invalid body sent: ${err.message}`,
-          },
-        });
-      }
-      const searchQuery = searchQueryRes.value;
-
-      const coreAPI = new CoreAPI(logger);
-      const data = await coreAPI.searchDataSource(
-        dataSource.dustAPIProjectId,
-        dataSource.name,
-        {
-          query: searchQuery.query,
-          topK: searchQuery.top_k,
-          fullText: searchQuery.full_text,
-          target_document_tokens: searchQuery.target_document_tokens,
-          filter: {
-            tags: {
-              in: searchQuery.tags_in,
-              not: searchQuery.tags_not,
-            },
-            parents: {
-              in: searchQuery.parents_in,
-              not: searchQuery.parents_not,
-            },
-            timestamp: {
-              gt: searchQuery.timestamp_gt,
-              lt: searchQuery.timestamp_lt,
-            },
-          },
-          credentials: credentials,
-        }
-      );
-
-      if (data.isErr()) {
-        return apiError(req, res, {
-          status_code: 400,
-          api_error: {
-            type: "data_source_error",
-            message: "There was an error performing the data source search.",
-            data_source_error: data.error,
-          },
-        });
-      }
-
-      res.status(200).json({
-        documents: data.value.documents,
-      });
-      return;
+      return handleSearchDataSource({ req, res, auth });
     }
 
     default:
@@ -161,3 +82,94 @@ async function handler(
 }
 
 export default withLogging(handler);
+
+export async function handleSearchDataSource({
+  req,
+  res,
+  auth,
+}: {
+  req: NextApiRequest;
+  res: NextApiResponse<WithAPIErrorReponse<DatasourceSearchResponseBody>>;
+  auth: Authenticator;
+}) {
+  const dataSource = await getDataSource(auth, req.query.name as string);
+
+  if (!dataSource) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "data_source_not_found",
+        message: "The data source you requested was not found.",
+      },
+    });
+  }
+
+  // I could not find a way to make the query params be an array if there is only one tag.
+  if (req.query.tags_in && typeof req.query.tags_in === "string") {
+    req.query.tags_in = [req.query.tags_in];
+  }
+  if (req.query.tags_not && typeof req.query.tags_not === "string") {
+    req.query.tags_not = [req.query.tags_not];
+  }
+  const requestPayload = req.query;
+
+  // Dust managed credentials: all data sources.
+  const credentials = dustManagedCredentials();
+
+  const searchQueryRes = parse_payload(searchQuerySchema, requestPayload);
+
+  if (searchQueryRes.isErr()) {
+    const err = searchQueryRes.error;
+    return apiError(req, res, {
+      status_code: 400,
+      api_error: {
+        type: "invalid_request_error",
+        message: `Invalid body sent: ${err.message}`,
+      },
+    });
+  }
+  const searchQuery = searchQueryRes.value;
+
+  const coreAPI = new CoreAPI(logger);
+  const data = await coreAPI.searchDataSource(
+    dataSource.dustAPIProjectId,
+    dataSource.name,
+    {
+      query: searchQuery.query,
+      topK: searchQuery.top_k,
+      fullText: searchQuery.full_text,
+      target_document_tokens: searchQuery.target_document_tokens,
+      filter: {
+        tags: {
+          in: searchQuery.tags_in,
+          not: searchQuery.tags_not,
+        },
+        parents: {
+          in: searchQuery.parents_in,
+          not: searchQuery.parents_not,
+        },
+        timestamp: {
+          gt: searchQuery.timestamp_gt,
+          lt: searchQuery.timestamp_lt,
+        },
+      },
+      credentials: credentials,
+    }
+  );
+
+  if (data.isErr()) {
+    return apiError(req, res, {
+      status_code: 400,
+      api_error: {
+        type: "data_source_error",
+        message: "There was an error performing the data source search.",
+        data_source_error: data.error,
+      },
+    });
+  }
+
+  res.status(200).json({
+    documents: data.value.documents,
+  });
+  return;
+}
