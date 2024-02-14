8563cffde4ba8498ce16726c8d2f20aae1a61181
filou <philipperolet@gmail.com>
2024-02-14 14:40:13 +0100

    onSave management

--- sparkle/src/components/ElementModal.tsx
+++ sparkle/src/components/ElementModal.tsx
@@ -1,26 +1,32 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import { Modal, ModalProps } from "./Modal";
 
-type ElementModalProps<T> = Omit<ModalProps, "isOpen"> & {
+type ElementModalProps<T> = Omit<ModalProps, "isOpen" | "onSave"> & {
   openOnElement: T | null;
+  onSave?: (closeModalFn: () => void) => void;
 };
 
 export function ElementModal<T>({
   openOnElement,
+  onClose,
+  onSave,
   ...props
 }: ElementModalProps<T>) {
-  const { onClose } = props;
   const [isClosingTransition, setIsClosingTransition] = useState(false);
-  const realOnClose = () => {
+  const transitionOnClose = useCallback(() => {
     setIsClosingTransition(true);
     setTimeout(() => {
       onClose();
       setIsClosingTransition(false);
     }, 200);
-  };
-  props.onClose = realOnClose;
+  }, [onClose]);
   return (
-    <Modal isOpen={openOnElement !== null && !isClosingTransition} {...props} />
+    <Modal
+      isOpen={openOnElement !== null && !isClosingTransition}
+      onClose={transitionOnClose}
+      onSave={onSave ? () => onSave(transitionOnClose) : undefined}
+      {...props}
+    />
   );
 }
