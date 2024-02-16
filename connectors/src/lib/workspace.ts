71bf1e9789ad99fd54f922918ba650d402754c14
Flavien David <flavien@dust.tt>
2024-02-16 09:20:21 +0100

    Add some caching

---
+++ connectors/src/lib/workspace.ts
@@ -0,0 +1,42 @@
+import { cacheWithRedis, DustAPI } from "@dust-tt/types";
+
+import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
+import logger from "@connectors/logger/logger";
+import type { ConnectorResource } from "@connectors/resources/connector_resource";
+
+async function getEnabledFeatureFlags(
+  connector: ConnectorResource
+): Promise<string[]> {
+  const ds = dataSourceConfigFromConnector(connector);
+
+  // List the emails of all active members in the workspace.
+  const dustAPI = new DustAPI(
+    {
+      apiKey: ds.workspaceAPIKey,
+      workspaceId: ds.workspaceId,
+    },
+    logger,
+    { useLocalInDev: true }
+  );
+
+  const workspaceFeatureFlags = await dustAPI.getWorkspaceFeatureFlags();
+  if (workspaceFeatureFlags.isErr()) {
+    logger.error("Error getting enabled feature flags for workspace.", {
+      error: workspaceFeatureFlags.error,
+    });
+
+    throw new Error("Error getting enabled feature flags for workspace.");
+  }
+
+  return workspaceFeatureFlags.value;
+}
+
+export const getEnabledFeatureFlagsMemoized = cacheWithRedis(
+  getEnabledFeatureFlags,
+  (connector: ConnectorResource) => {
+    return `enabled-feature-flags-for-workspace-${connector.workspaceId}`;
+  },
+  // Caches data for 15 minutes to limit frequent API calls.
+  // Note: Updates (e.g., feature flags update) may take up to 15 minutes to be reflected.
+  15 * 10 * 1000
+);
