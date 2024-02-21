addaeb3be2b43cda08d57c6c47f504d3f0535400
filou <philipperolet@gmail.com>
2024-02-21 17:04:48 +0100

    Naming extraction

--- front/components/assistant_builder/AssistantBuilder.tsx
+++ front/components/assistant_builder/AssistantBuilder.tsx
@@ -275,7 +275,6 @@ export default function AssistantBuilder({
   >(null);
   const [timeFrameError, setTimeFrameError] = useState<string | null>(null);
 
-  const [isAvatarModalOpen, setIsAvatarModalOpen] = useState(false);
   const checkUsernameTimeout = React.useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
@@ -867,93 +866,6 @@ export default function AssistantBuilder({
     );
   }
 
-  function NamingScreen() {
-    return (
-      <>
-        <div className="flex w-full flex-col gap-4">
-          <div className="text-2xl font-bold text-element-900">Identity</div>
-          <div className="flex flex-row items-start gap-8">
-            <div className="flex flex-col gap-4">
-              <div className="text-lg font-bold text-element-900">Name</div>
-              <div className="flex-grow self-stretch text-sm font-normal text-element-700">
-                Choose a name reflecting the expertise, knowledge access or
-                function of your&nbsp;assistant. Mentioning the&nbsp;assistant
-                in a conversation, like <span className="italic">"@help"</span>{" "}
-                will prompt a&nbsp;response from&nbsp;them.
-              </div>
-              <div className="text-sm">
-                <Input
-                  placeholder="SalesAssistant, FrenchTranslator, SupportCenter…"
-                  value={builderState.handle}
-                  onChange={(value) => {
-                    setEdited(true);
-                    setBuilderState((state) => ({
-                      ...state,
-                      handle: value.trim(),
-                    }));
-                  }}
-                  error={assistantHandleError}
-                  name="assistantName"
-                  showErrorLabel
-                  className="text-sm"
-                />
-              </div>
-              <div className="text-lg font-bold text-element-900">
-                Description
-              </div>
-              <div className="flex-grow self-stretch text-sm font-normal text-element-700">
-                Add a short description that will help Dust and other workspace
-                members understand the&nbsp;assistant’s&nbsp;purpose.
-              </div>
-              <div className="text-sm">
-                <Input
-                  placeholder="Answer questions about sales, translate from English to French…"
-                  value={builderState.description}
-                  onChange={(value) => {
-                    setEdited(true);
-                    setBuilderState((state) => ({
-                      ...state,
-                      description: value,
-                    }));
-                  }}
-                  error={null} // TODO ?
-                  name="assistantDescription"
-                  className="text-sm"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col items-center space-y-2">
-              <Avatar
-                size="xl"
-                visual={<img src={builderState.avatarUrl || ""} />}
-              />
-              <Button
-                labelVisible={true}
-                label={"Change"}
-                variant="tertiary"
-                size="xs"
-                icon={PencilSquareIcon}
-                onClick={() => {
-                  setIsAvatarModalOpen(true);
-                }}
-              />
-            </div>
-          </div>
-        </div>
-        <SharingSection
-          owner={owner}
-          agentConfigurationId={agentConfigurationId}
-          initialScope={initialBuilderState?.scope ?? defaultScope}
-          newScope={builderState.scope}
-          setNewScope={(scope: Exclude<AgentConfigurationScope, "global">) => {
-            setEdited(scope !== initialBuilderState?.scope);
-            setBuilderState((state) => ({ ...state, scope }));
-          }}
-        />
-      </>
-    );
-  }
-
   return (
     <>
       <AssistantBuilderDataSourceModal
@@ -1023,20 +935,6 @@ export default function AssistantBuilder({
         }}
         tablesQueryConfiguration={builderState.tablesQueryConfiguration}
       />
-      <AvatarPicker
-        owner={owner}
-        isOpen={isAvatarModalOpen}
-        setOpen={setIsAvatarModalOpen}
-        onPick={(avatarUrl) => {
-          setEdited(true);
-          setBuilderState((state) => ({
-            ...state,
-            avatarUrl,
-          }));
-        }}
-        droidAvatarUrls={DROID_AVATAR_URLS}
-        spiritAvatarUrls={SPIRIT_AVATAR_URLS}
-      />
       <AppLayout
         subscription={subscription}
         hideSidebar
@@ -1077,7 +975,18 @@ export default function AssistantBuilder({
               case "actions":
                 return <ActionScreen />;
               case "naming":
-                return <NamingScreen />;
+                return (
+                  <NamingScreen
+                    owner={owner}
+                    builderState={builderState}
+                    setBuilderState={setBuilderState}
+                    setEdited={setEdited}
+                    agentConfigurationId={agentConfigurationId}
+                    initialBuilderState={initialBuilderState}
+                    defaultScope={defaultScope}
+                    assistantHandleError={assistantHandleError}
+                  />
+                );
               default:
                 assertNever(screen);
             }
@@ -1152,6 +1061,120 @@ function InstructionScreen({
         />
       </div>
     </div>
+  );
+}
+
+function NamingScreen({
+  owner,
+  builderState,
+  setBuilderState,
+  setEdited,
+  agentConfigurationId,
+  initialBuilderState,
+  defaultScope,
+  assistantHandleError,
+}: {
+  owner: WorkspaceType;
+  builderState: AssistantBuilderState;
+  setBuilderState: (
+    statefn: (state: AssistantBuilderState) => AssistantBuilderState
+  ) => void;
+  setEdited: (edited: boolean) => void;
+  agentConfigurationId: string | null;
+  initialBuilderState: AssistantBuilderInitialState | null;
+  defaultScope: AgentConfigurationScope;
+  assistantHandleError: string | null;
+}) {
+  const [isAvatarModalOpen, setIsAvatarModalOpen] = useState(false);
+
+  return (
+    <>
+      <AvatarPicker
+        owner={owner}
+        isOpen={isAvatarModalOpen}
+        setOpen={setIsAvatarModalOpen}
+        onPick={(avatarUrl) => {
+          setEdited(true);
+          setBuilderState((state) => ({
+            ...state,
+            avatarUrl,
+          }));
+        }}
+        droidAvatarUrls={DROID_AVATAR_URLS}
+        spiritAvatarUrls={SPIRIT_AVATAR_URLS}
+      />
+
+      <div className="flex w-full flex-col gap-4">
+        <div className="text-2xl font-bold text-element-900">Identity</div>
+        <div className="flex flex-row items-start gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="text-lg font-bold text-element-900">Name</div>
+            <div className="flex-grow self-stretch text-sm font-normal text-element-700">
+              Choose a name reflecting the expertise, knowledge access or
+              function of your&nbsp;assistant. Mentioning the&nbsp;assistant in
+              a conversation, like <span className="italic">"@help"</span> will
+              prompt a&nbsp;response from&nbsp;them.
+            </div>
+            <div className="text-sm">
+              <Input
+                placeholder="SalesAssistant, FrenchTranslator, SupportCenter…"
+                value={builderState.handle}
+                onChange={(value) => {
+                  setEdited(true);
+                  setBuilderState((state) => ({
+                    ...state,
+                    handle: value.trim(),
+                  }));
+                }}
+                error={assistantHandleError}
+                name="assistantName"
+                showErrorLabel
+                className="text-sm"
+              />
+            </div>
+            <div className="text-lg font-bold text-element-900">
+              Description
+            </div>
+            <div className="flex-grow self-stretch text-sm font-normal text-element-700">
+              Add a short description that will help Dust and other workspace
+              members understand the&nbsp;assistant’s&nbsp;purpose.
+            </div>
+            <div className="text-sm">
+              <Input
+                placeholder="Answer questions about sales, translate from English to French…"
+                value={builderState.description}
+                onChange={(value) => {
+                  setEdited(true);
+                  setBuilderState((state) => ({
+                    ...state,
+                    description: value,
+                  }));
+                }}
+                error={null} // TODO ?
+                name="assistantDescription"
+                className="text-sm"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <Avatar
+              size="xl"
+              visual={<img src={builderState.avatarUrl || ""} />}
+            />
+            <Button
+              labelVisible={true}
+              label={"Change"}
+              variant="tertiary"
+              size="xs"
+              icon={PencilSquareIcon}
+              onClick={() => {
+                setIsAvatarModalOpen(true);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
