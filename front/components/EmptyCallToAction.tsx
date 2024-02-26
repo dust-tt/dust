7b3b8211492e3a48eb077e5197d78a498dabcd30
Stanislas Polu <spolu@dust.tt>
2024-02-26 12:51:45 +0100

    nit

--- front/components/EmptyCallToAction.tsx
+++ front/components/EmptyCallToAction.tsx
@@ -24,7 +24,7 @@ export function EmptyCallToAction({
       size="sm"
       label={label}
       variant="primary"
-      icon={icon || undefined}
+      icon={icon}
       onClick={onClick}
     />
   );
