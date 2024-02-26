a97737fafdc328312fe3156ead32553ad8d72ee2
Flavien David <flavien@dust.tt>
2024-02-26 13:38:35 +0100

    Improve assert never log

--- types/src/shared/utils/assert_never.ts
+++ types/src/shared/utils/assert_never.ts
@@ -1,3 +1,7 @@
 export function assertNever(x: never): never {
-  throw new Error(`${x} is not of type never. This should never happen.`);
+  throw new Error(
+    `${
+      typeof x === "object" ? JSON.stringify(x, null, 2) : x
+    } is not of type never. This should never happen.`
+  );
 }
