31fcb7ce182c13788df99d63eb79a83880c79dd4
Flavien David <flavien.david74@gmail.com>
2024-02-26 13:48:49 +0100

    Improve assert never log (#3936)
    * Improve assert never log
    
    * :scissors:

--- types/src/shared/utils/assert_never.ts
+++ types/src/shared/utils/assert_never.ts
@@ -1,3 +1,7 @@
 export function assertNever(x: never): never {
-  throw new Error(`${x} is not of type never. This should never happen.`);
+  throw new Error(
+    `${
+      typeof x === "object" ? JSON.stringify(x) : x
+    } is not of type never. This should never happen.`
+  );
 }
