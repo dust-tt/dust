3a7a6bc1f99b55fd71e6f3efae0ff72baf3141d1
Aric Lasry <aric@dust.tt>
2024-02-27 14:03:23 +0100

    Migration to clean up dangling webcrawler connectors (#3975)
    * Migration to clean up dangling webcrawler connectors
    
    * Clean up after self review

---
+++ front/migrations/20240227_cleanup_dangling_webcrawler_connectors.ts
@@ -0,0 +1,45 @@
+import { ConnectorsAPI } from "@dust-tt/types";
+import { QueryTypes } from "sequelize";
+
+import { DataSource } from "@app/lib/models";
+import logger from "@app/logger/logger";
+import { getConnectorReplicaDbConnection } from "@app/production_checks/lib/utils";
+
+async function main() {
+  const { LIVE } = process.env;
+  const connectorsReplica = getConnectorReplicaDbConnection();
+  const connectors: { id: number }[] = await connectorsReplica.query(
+    "SELECT id FROM connectors WHERE type = 'webcrawler' ORDER BY id ASC",
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  console.log("Will check this number of connectors:", connectors.length);
+  for (const connector of connectors) {
+    const dataSource = await DataSource.findOne({
+      where: {
+        connectorId: connector.id.toString(),
+        connectorProvider: "webcrawler",
+      },
+    });
+    if (!dataSource) {
+      console.log(
+        "No data source found for connector, should delete connector",
+        connector
+      );
+      if (LIVE) {
+        const connectorsAPI = new ConnectorsAPI(logger);
+        const deleteRes = await connectorsAPI.deleteConnector(
+          connector.id.toString()
+        );
+        if (deleteRes.isErr()) {
+          throw deleteRes.error;
+        } else {
+          console.log("Deleted connector", connector);
+        }
+      }
+    }
+  }
+}
+
+main().catch(console.error);
