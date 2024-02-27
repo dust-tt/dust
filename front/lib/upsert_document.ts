7bc9b003018c7ccc8b876972b92ca2e11af0befd
Stanislas Polu <spolu@dust.tt>
2024-02-27 11:48:22 +0100

    Upsert Queue: activity implementation (#3969)
    * Upsert Queue: activity implementation
    
    * lint + fix logger

--- front/lib/upsert_document.ts
+++ front/lib/upsert_document.ts
@@ -1,20 +1,37 @@
-import type { Result } from "@dust-tt/types";
-import { Err, FrontDataSourceDocumentSection } from "@dust-tt/types";
+import type {
+  CoreAPIDocument,
+  CoreAPILightDocument,
+  DataSourceType,
+  Result,
+  UpsertContext,
+} from "@dust-tt/types";
+import {
+  Err,
+  FrontDataSourceDocumentSection,
+  sectionFullText,
+  UpsertContextSchema,
+} from "@dust-tt/types";
 import { Storage } from "@google-cloud/storage";
 import * as t from "io-ts";
 import { v4 as uuidv4 } from "uuid";
 
+import { getDocumentsPostUpsertHooksToRun } from "@app/documents_post_process_hooks/hooks";
+import { launchRunPostUpsertHooksWorkflow } from "@app/documents_post_process_hooks/temporal/client";
+import { Authenticator } from "@app/lib/auth";
 import { launchUpsertDocumentWorkflow } from "@app/upsert_queue/temporal/client";
 
-const EnqueueUpsertDocument = t.type({
-  projectId: t.string,
+const { DUST_UPSERT_QUEUE_BUCKET, SERVICE_ACCOUNT } = process.env;
+
+export const EnqueueUpsertDocument = t.type({
   workspaceId: t.string,
   dataSourceName: t.string,
   documentId: t.string,
   tags: t.union([t.array(t.string), t.null]),
   parents: t.union([t.array(t.string), t.null]),
   sourceUrl: t.union([t.string, t.null]),
+  timestamp: t.union([t.number, t.null]),
   section: FrontDataSourceDocumentSection,
+  upsertContext: t.union([UpsertContextSchema, t.null]),
 });
 
 export async function enqueueUpsertDocument({
@@ -22,7 +39,6 @@ export async function enqueueUpsertDocument({
 }: {
   upsertDocument: t.TypeOf<typeof EnqueueUpsertDocument>;
 }): Promise<Result<string, Error>> {
-  const { DUST_UPSERT_QUEUE_BUCKET, SERVICE_ACCOUNT } = process.env;
   if (!DUST_UPSERT_QUEUE_BUCKET) {
     throw new Error("DUST_UPSERT_QUEUE_BUCKET is not set");
   }
@@ -34,7 +50,9 @@ export async function enqueueUpsertDocument({
   try {
     const storage = new Storage({ keyFilename: SERVICE_ACCOUNT });
     const bucket = storage.bucket(DUST_UPSERT_QUEUE_BUCKET);
-    await bucket.file(uuid).save(JSON.stringify(upsertDocument));
+    await bucket.file(`${uuid}.json`).save(JSON.stringify(upsertDocument), {
+      contentType: "application/json",
+    });
 
     return await launchUpsertDocumentWorkflow({
       workspaceId: upsertDocument.workspaceId,
@@ -47,5 +65,50 @@ export async function enqueueUpsertDocument({
     } else {
       throw e;
     }
+  }
+}
+
+export async function runPostUpsertHooks({
+  workspaceId,
+  dataSource,
+  documentId,
+  section,
+  document,
+  sourceUrl,
+  upsertContext,
+}: {
+  workspaceId: string;
+  dataSource: DataSourceType;
+  documentId: string;
+  section: t.TypeOf<typeof FrontDataSourceDocumentSection>;
+  document: CoreAPILightDocument | CoreAPIDocument;
+  sourceUrl: string | null;
+  upsertContext?: UpsertContext;
+}) {
+  const fullText = sectionFullText(section);
+  const auth = await Authenticator.internalBuilderForWorkspace(workspaceId);
+
+  const postUpsertHooksToRun = await getDocumentsPostUpsertHooksToRun({
+    auth,
+    dataSourceName: dataSource.name,
+    documentId: documentId,
+    documentText: fullText,
+    documentHash: document.hash,
+    dataSourceConnectorProvider: dataSource.connectorProvider || null,
+    documentSourceUrl: sourceUrl || undefined,
+    upsertContext,
+  });
+
+  // TODO: parallel.
+  for (const { type: hookType, debounceMs } of postUpsertHooksToRun) {
+    await launchRunPostUpsertHooksWorkflow(
+      dataSource.name,
+      workspaceId,
+      documentId,
+      document.hash,
+      dataSource.connectorProvider || null,
+      hookType,
+      debounceMs
+    );
   }
 }
--- front/upsert_queue/temporal/activities.ts
+++ front/upsert_queue/temporal/activities.ts
@@ -1,7 +1,98 @@
+import { CoreAPI, dustManagedCredentials } from "@dust-tt/types";
+import { Storage } from "@google-cloud/storage";
+import { isLeft } from "fp-ts/lib/Either";
+import * as reporter from "io-ts-reporters";
+
+import { getDataSource } from "@app/lib/api/data_sources";
+import { Authenticator } from "@app/lib/auth";
+import {
+  EnqueueUpsertDocument,
+  runPostUpsertHooks,
+} from "@app/lib/upsert_document";
 import mainLogger from "@app/logger/logger";
 
-export async function upsertDocumentActivity(upsertQueueId: string) {
-  mainLogger.info({ upsertQueueId }, "Retrieving upsert queue item");
+const { DUST_UPSERT_QUEUE_BUCKET, SERVICE_ACCOUNT } = process.env;
 
-  // TODO: Implement upsert logic
+export async function upsertDocumentActivity(upsertQueueId: string) {
+  if (!DUST_UPSERT_QUEUE_BUCKET) {
+    throw new Error("DUST_UPSERT_QUEUE_BUCKET is not set");
+  }
+  if (!SERVICE_ACCOUNT) {
+    throw new Error("SERVICE_ACCOUNT is not set");
+  }
+
+  let logger = mainLogger.child({ upsertQueueId });
+  logger.info({}, "[UpsertQueue] Retrieving item");
+
+  const storage = new Storage({ keyFilename: SERVICE_ACCOUNT });
+  const bucket = storage.bucket(DUST_UPSERT_QUEUE_BUCKET);
+  const content = await bucket.file(`${upsertQueueId}.json`).download();
+
+  const upsertDocument = JSON.parse(content.toString());
+
+  const itemValidation = EnqueueUpsertDocument.decode(upsertDocument);
+  if (isLeft(itemValidation)) {
+    const pathError = reporter.formatValidationErrors(itemValidation.left);
+    throw new Error(`Invalid upsertQueue item: ${pathError}`);
+  }
+  const upsertQueueItem = itemValidation.right;
+
+  const auth = await Authenticator.internalBuilderForWorkspace(
+    upsertQueueItem.workspaceId
+  );
+
+  const dataSource = await getDataSource(auth, upsertQueueItem.dataSourceName);
+
+  if (!dataSource) {
+    throw new Error(`Data source ${upsertQueueItem.dataSourceName} not found`);
+  }
+
+  logger = logger.child({
+    workspaceId: upsertQueueItem.workspaceId,
+    dataSourceName: dataSource.name,
+    documentId: upsertQueueItem.documentId,
+  });
+
+  // Dust managed credentials: all data sources.
+  const credentials = dustManagedCredentials();
+
+  const coreAPI = new CoreAPI(logger);
+
+  // Create document with the Dust internal API.
+  const upsertRes = await coreAPI.upsertDataSourceDocument({
+    projectId: dataSource.dustAPIProjectId,
+    dataSourceName: dataSource.name,
+    documentId: upsertQueueItem.documentId,
+    tags: upsertQueueItem.tags || [],
+    parents: upsertQueueItem.parents || [],
+    sourceUrl: upsertQueueItem.sourceUrl,
+    timestamp: upsertQueueItem.timestamp,
+    section: upsertQueueItem.section,
+    credentials,
+    lightDocumentOutput: true,
+  });
+
+  if (upsertRes.isErr()) {
+    logger.error(
+      {
+        error: upsertRes.error,
+      },
+      "[UpsertQueue] Failed upsert"
+    );
+    throw new Error(`Upsert error: ${upsertRes.error}`);
+  }
+
+  logger.info({}, "[UpsertQueue] Successful upsert");
+
+  await runPostUpsertHooks({
+    workspaceId: upsertQueueItem.workspaceId,
+    dataSource,
+    documentId: upsertQueueItem.documentId,
+    section: upsertQueueItem.section,
+    document: upsertRes.value.document,
+    sourceUrl: upsertQueueItem.sourceUrl,
+    upsertContext: upsertQueueItem.upsertContext || undefined,
+  });
+
+  logger.info({}, "[UpsertQueue] Successful runPostUpsertHooks");
 }
