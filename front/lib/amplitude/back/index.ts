f911e91c7cf510d4fe246b82fbfaed806cd115b8
Aric Lasry <lasry.aric@gmail.com>
2024-03-04 19:25:17 +0100

    Commiting generated code and getClient() for Amplitude front and back

---
+++ front/lib/amplitude/back/index.ts
@@ -0,0 +1,22 @@
+import type { Ampli } from "@app/lib/amplitude/back/generated";
+import { ampli } from "@app/lib/amplitude/back/generated";
+
+let BACKEND_CLIENT: Ampli | null = null;
+
+const { AMPLITUDE_ENABLED } = process.env;
+
+export function getBackendClient() {
+  if (BACKEND_CLIENT) {
+    return BACKEND_CLIENT;
+  }
+
+  const disabled = !(AMPLITUDE_ENABLED === "true");
+  console.log("amplitude disabled?", disabled);
+  ampli.load({
+    environment: "dust",
+    disabled: disabled,
+  });
+  BACKEND_CLIENT = ampli;
+
+  return BACKEND_CLIENT;
+}
