31a53ecde7d803b4b24a80292c0faa971c4e8999
édouard wautier <edouardwautier@Ed-Pro.local>
2024-03-05 15:31:58 +0100

    Updating Citation

--- sparkle/src/stories/Citation.stories.tsx
+++ sparkle/src/stories/Citation.stories.tsx
@@ -12,11 +12,64 @@ export default meta;
 
 export const CitationsExample = () => (
   <div className="s-flex s-flex-col s-gap-8">
+    <div className="s-flex s-gap-2">
+      <Citation
+        title="With close action"
+        size="xs"
+        type="document"
+        index="3"
+        onClose={() => alert("Close action clicked")}
+        isBlinking={true}
+      />
+
+      <Citation
+        title="With avatarUrl"
+        size="xs"
+        type="document"
+        onClose={() => alert("Close action clicked")}
+        isBlinking={true}
+        avatarUrl="https://cdn.discordapp.com/attachments/995248824375316560/1143857310142316685/duncid_friendly_Scandinavian_droid_designed_by_Wes_Anderson_and_29eec588-b898-4e4a-9776-10c27790cbf9.png"
+      />
+    </div>
+    <div className="s-flex s-gap-2">
+      <Citation
+        title="Source: Thread on #general message from @ed"
+        size="xs"
+        type="slack"
+        index="1"
+        href="https://www.google.com"
+      />
+      <Citation
+        title="Title"
+        type="github"
+        size="xs"
+        index="2"
+        href="https://www.google.com"
+      />
+    </div>
     <Citation
       title="Source: Thread on #general"
       type="slack"
       href="https://www.google.com"
     />
+    <div className="s-flex s-gap-2">
+      <Citation
+        title="Title"
+        type="google_drive"
+        index="3"
+        href="https://www.google.com"
+        description="Write a 120 character description of the citation here to be displayed in the citation list."
+        isBlinking={true}
+      />
+      <Citation
+        title="Awesome article"
+        type="intercom"
+        index="3"
+        href="https://www.google.com"
+        description="Write a 120 character description of the citation here to be displayed in the citation list."
+        isBlinking={false}
+      />
+    </div>
     <div className="s-flex s-gap-2">
       <Citation
         title="With close action"
