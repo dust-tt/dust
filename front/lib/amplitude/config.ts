6171808a4c6a615e25348b54f36e47547565603b
Aric Lasry <lasry.aric@gmail.com>
2024-03-06 14:59:32 +0100

    Setting dust-prod public api key

--- front/lib/amplitude/config.ts
+++ front/lib/amplitude/config.ts
@@ -1,2 +1,2 @@
 // This is a public API key, which will endup in the user's browser anyway.
-export const AMPLITUDE_PUBLIC_API_KEY = "6ba33096c77a939358f9c21e12d73592";
+export const AMPLITUDE_PUBLIC_API_KEY = "940c526d7c7c91a38c267be75c958890";
