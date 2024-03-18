f73bfd9a90418d5546c4f4c58fb8d471366fad2a
Stanislas Polu <spolu@dust.tt>
2024-03-18 10:51:49 +0100

    Use dbConfig

--- front/lib/resources/storage/index.ts
+++ front/lib/resources/storage/index.ts
@@ -1,32 +1,34 @@
 import { Sequelize } from "sequelize";
 
+import { dbConfig } from "@app/lib/resources/storage/config";
 import logger from "@app/logger/logger";
-
-const { FRONT_DATABASE_URI } = process.env;
 
 const acquireAttempts = new WeakMap();
 
-export const frontSequelize = new Sequelize(FRONT_DATABASE_URI as string, {
-  pool: {
-    // default is 5
-    max: 50,
-  },
-  logging: false,
-  hooks: {
-    beforePoolAcquire: (options) => {
-      acquireAttempts.set(options, Date.now());
+export const frontSequelize = new Sequelize(
+  dbConfig.getRequiredFrontDatabaseURI(),
+  {
+    pool: {
+      // default is 5
+      max: 50,
     },
-    afterPoolAcquire: (connection, options) => {
-      const elapsedTime = Date.now() - acquireAttempts.get(options);
-      if (elapsedTime > 100) {
-        logger.info(
-          {
-            elapsedTime,
-            callStack: new Error().stack,
-          },
-          "Long sequelize connection acquisition detected"
-        );
-      }
+    logging: false,
+    hooks: {
+      beforePoolAcquire: (options) => {
+        acquireAttempts.set(options, Date.now());
+      },
+      afterPoolAcquire: (connection, options) => {
+        const elapsedTime = Date.now() - acquireAttempts.get(options);
+        if (elapsedTime > 100) {
+          logger.info(
+            {
+              elapsedTime,
+              callStack: new Error().stack,
+            },
+            "Long sequelize connection acquisition detected"
+          );
+        }
+      },
     },
-  },
-});
+  }
+);
