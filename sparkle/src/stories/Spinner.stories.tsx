c859fe2e51b011463683d36dbd40033a027a5de6
édouard wautier <edouardwautier@Ed-Pro.local>
2024-03-19 17:03:59 +0100

    rebasing

--- sparkle/src/stories/Spinner.stories.tsx
+++ sparkle/src/stories/Spinner.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
 
-import { Spinner } from "../index_with_tw_base";
+import { Spinner, Spinner2 } from "../index_with_tw_base";
 
 const meta = {
   title: "Primitives/Spinner",
@@ -9,6 +10,28 @@ const meta = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const SpinnerExample = () => {
+  return (
+    <div className="s-flex s-gap-4">
+      <div className="s-p-20">
+        <Spinner variant="success" />
+      </div>
+      <div className="s-p-20">
+        <Spinner2 variant="color" />
+      </div>
+      <div className="s-bg-slate-900 s-p-20">
+        <Spinner2 variant="light" />
+      </div>
+      <div className="s-bg-emerald-500 s-p-20">
+        <Spinner2 variant="light" />
+      </div>
+      <div className="s-p-20">
+        <Spinner2 variant="dark" />
+      </div>
+    </div>
+  );
+};
 
 export const BasicSpinner: Story = {
   args: {
