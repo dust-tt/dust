3ca7775df907e896f8256f5d37f681fac47d2b9f
filou <philipperolet@gmail.com>
2024-03-25 17:06:05 +0100

    remove useless sql migration

--- front/migrations/20240325_deprecate_columns_from_cf.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "url";
-ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "content";
-ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "textUrl";
