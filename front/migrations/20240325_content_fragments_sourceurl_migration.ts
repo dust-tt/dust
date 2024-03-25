7c2b74ed2700cf9e0c1ee91b04302816101f2e7a
Philippe Rolet <pr@dust.tt>
2024-03-25 19:26:34 +0100

    [Content Fragment] Migrate deprecated `sourceUrl` fields (#4448)
    * [Content Fragment] Migrate deprecated `sourceUrl` fields
    
    Legacy content fragment messages may have a sourceUrl with a private GCS link to extracted textual content, only accessible to Dust so non-downloadable. This PR resets those to a null sourceUrl, to enforce the invariant that sourceUrl is a user-accessible link to an original version of the content--which we do not have for old content fragment.
    
    * cleaning
    
    * logging

---
+++ front/migrations/20240325_content_fragments_sourceurl_migration.ts
@@ -0,0 +1,58 @@
+import { Op } from "sequelize";
+
+import { Message } from "@app/lib/models";
+import { ContentFragmentResource } from "@app/lib/resources/content_fragment_resource";
+import { ContentFragmentModel } from "@app/lib/resources/storage/models/content_fragment";
+
+const { LIVE } = process.env;
+
+async function main() {
+  // get all messages that are content fragments, and whose content fragment's sourceUrl field starts with
+  // "https://storage.googleapis.com/"
+  // by batches of 128
+
+  let messages = [];
+  do {
+    messages = await Message.findAll({
+      where: {
+        contentFragmentId: {
+          [Op.not]: null,
+        },
+      },
+      include: [
+        {
+          model: ContentFragmentModel,
+          as: "contentFragment",
+          where: {
+            sourceUrl: {
+              [Op.startsWith]: "https://storage.googleapis.com/",
+            },
+          },
+        },
+      ],
+      limit: 128,
+    });
+    // reset sourceUrl to null for those content fragments in parallel
+    if (LIVE) {
+      console.log(`Processing ${messages.length} messages`);
+      await Promise.all(
+        messages.map(async (message) => {
+          const cf = ContentFragmentResource.fromMessage(message);
+          await cf.update({
+            sourceUrl: null,
+          });
+        })
+      );
+    }
+  } while (messages.length > 0 && LIVE);
+}
+
+main()
+  .then(() => {
+    console.log("Done");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
