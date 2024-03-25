db71c222d8bba1cc5166e6166dc27fc049fb4d22
filou <philipperolet@gmail.com>
2024-03-25 16:26:08 +0100

    drop columns

---
+++ front/migrations/20240325_deprecate_columns_from_cf.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "url";
+ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "content";
+ALTER TABLE "content_fragments" DROP COLUMN IF EXISTS "textUrl";
