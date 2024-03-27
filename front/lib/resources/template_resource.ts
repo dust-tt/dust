4868375a9c544aeb70f16f0c193333c7aba24a45
Flavien David <flavien.david74@gmail.com>
2024-03-27 12:11:57 +0100

    Add endpoints for assistant templates (#4471)
    * Add endpoints for assistant templates
    
    * :sparkles:

--- front/lib/resources/template_resource.ts
+++ front/lib/resources/template_resource.ts
@@ -34,6 +34,38 @@ export class TemplateResource extends BaseResource<TemplateModel> {
     return new this(TemplateModel, template.get());
   }
 
+  // TODO(2024-03-27 flav) Move this to the `BaseResource`.
+  static async fetchByExternalId(
+    sId: string,
+    transaction?: Transaction
+  ): Promise<TemplateResource | null> {
+    const blob = await this.model.findOne({
+      where: {
+        sId,
+      },
+      transaction,
+    });
+    if (!blob) {
+      return null;
+    }
+
+    // Use `.get` to extract model attributes, omitting Sequelize instance metadata.
+    return new TemplateResource(this.model, blob.get());
+  }
+
+  static async listAllPublished() {
+    const blobs = await TemplateResource.model.findAll({
+      where: {
+        visibility: "published",
+      },
+    });
+
+    return blobs.map(
+      // Use `.get` to extract model attributes, omitting Sequelize instance metadata.
+      (b) => new TemplateResource(this.model, b.get())
+    );
+  }
+
   async delete(transaction?: Transaction): Promise<Result<undefined, Error>> {
     try {
       await this.model.destroy({
@@ -47,5 +79,36 @@ export class TemplateResource extends BaseResource<TemplateModel> {
     } catch (err) {
       return new Err(err as Error);
     }
+  }
+
+  isPublished() {
+    return this.visibility === "published";
+  }
+
+  toListJSON() {
+    return {
+      description: this.description,
+      name: this.name,
+      sId: this.sId,
+      tags: this.tags,
+    };
+  }
+
+  toJSON() {
+    return {
+      description: this.description,
+      helpActions: this.helpActions,
+      helpInstructions: this.helpInstructions,
+      name: this.name,
+      presetAction: this.presetAction,
+      presetDescription: this.presetDescription,
+      presetHandle: this.presetHandle,
+      presetInstructions: this.presetInstructions,
+      presetModelId: this.presetModelId,
+      presetProviderId: this.presetProviderId,
+      presetTemperature: this.presetTemperature,
+      sId: this.sId,
+      tags: this.tags,
+    };
   }
 }
---
+++ front/pages/api/w/[wId]/assistant/builder/templates/index.ts
@@ -0,0 +1,54 @@
+import type { WithAPIErrorReponse } from "@dust-tt/types";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { Authenticator, getSession } from "@app/lib/auth";
+import { TemplateResource } from "@app/lib/resources/template_resource";
+import { apiError, withLogging } from "@app/logger/withlogging";
+
+export type AssistantTemplate = ReturnType<TemplateResource["toListJSON"]>;
+
+export interface FetchAssistantTemplatesResponse {
+  templates: AssistantTemplate[];
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<WithAPIErrorReponse<FetchAssistantTemplatesResponse>>
+): Promise<void> {
+  const session = await getSession(req, res);
+  const auth = await Authenticator.fromSession(
+    session,
+    req.query.wId as string
+  );
+  const owner = auth.workspace();
+  if (!owner || !auth.isUser()) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "app_auth_error",
+        message:
+          "Workspace not found or user not authenticated to this workspace.",
+      },
+    });
+  }
+
+  switch (req.method) {
+    case "GET":
+      const templates = await TemplateResource.listAllPublished();
+
+      return res
+        .status(200)
+        .json({ templates: templates.map((t) => t.toListJSON()) });
+
+    default:
+      return apiError(req, res, {
+        status_code: 405,
+        api_error: {
+          type: "method_not_supported_error",
+          message: "The method passed is not supported, POST is expected.",
+        },
+      });
+  }
+}
+
+export default withLogging(handler);
