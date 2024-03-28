a60617d887604d423615c7faf8890b9e9e8ce5e1
Flavien David <flavien@dust.tt>
2024-03-28 11:43:06 +0100

    Support creating/updating templates in Poke

--- front/poke/swr.ts
+++ front/poke/swr.ts
@@ -3,6 +3,7 @@ import type { Fetcher } from "swr";
 import useSWR from "swr";
 
 import { fetcher } from "@app/lib/swr";
+import type { PokeFetchAssistantTemplateResponse } from "@app/pages/api/poke/templates/[tId]";
 import type { FetchAssistantTemplatesResponse } from "@app/pages/api/w/[wId]/assistant/builder/templates";
 
 export function usePokeAssistantTemplates() {
@@ -21,5 +22,26 @@ export function usePokeAssistantTemplates() {
     isAssistantTemplatesLoading: !error && !data,
     isAssistantTemplatesError: error,
     mutateAssistantTemplates: mutate,
+  };
+}
+
+export function usePokeAssistantTemplate({
+  templateId,
+}: {
+  templateId: string | null;
+}) {
+  const assistantTemplateFetcher: Fetcher<PokeFetchAssistantTemplateResponse> =
+    fetcher;
+
+  const { data, error, mutate } = useSWR(
+    templateId !== null ? `/api/poke/templates/${templateId}` : null,
+    assistantTemplateFetcher
+  );
+
+  return {
+    assistantTemplate: useMemo(() => (data ? data : null), [data]),
+    isAssistantTemplateLoading: !error && !data,
+    isAssistantTemplateError: error,
+    mutateAssistantTemplate: mutate,
   };
 }
