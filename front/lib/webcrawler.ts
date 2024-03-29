57d88b436f99ad33c51600d18efedef960c41522
Aric Lasry <aric@dust.tt>
2024-03-29 20:57:08 +0100

    [Websites] Allow naming a website (#4528)
    * [Websites] Allow naming a website
    
    * Disable Data Source name edition
    
    * Reviewing UI with @spolu

--- front/lib/webcrawler.ts
+++ front/lib/webcrawler.ts
@@ -1,8 +1,43 @@
 export function urlToDataSourceName(url: string) {
-  return url
-    .trim()
-    .replace(/https?:\/\//, "")
-    .replace(/\/$/, "")
-    .replace(/\//g, "-")
-    .replace(/\?/g, "-");
+  function sanitizeString(inputString: string): string {
+    // Regular expression to match characters that are NOT letters, numbers, '.', '_', or '-'
+    // ^ outside of character set negates the set, matching anything not in the set
+    const regex = /[^a-zA-Z0-9._-]/g;
+
+    // Replace characters that match the regex with '-'
+    const sanitizedString = inputString.replace(regex, "_");
+
+    return sanitizedString;
+  }
+
+  let name = "";
+  try {
+    const parsed = new URL(url);
+    if (parsed.pathname === "/") {
+      // no path after the hostname
+      name += parsed.hostname;
+    } else {
+      // We have a path after the hostname
+      name +=
+        parsed.hostname +
+        decodeURIComponent(parsed.pathname).replaceAll("/", "-");
+    }
+    name = name.substring(0, 60);
+    return sanitizeString(name);
+  } catch (e) {
+    // We failed to parse the URL, we are going to return an empty string
+    return "";
+  }
+}
+
+export function isUrlValid(url: string) {
+  try {
+    if (url.trim().length === 0) {
+      return false;
+    }
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
