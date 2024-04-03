219abb2ba3dd0b2fa58cfc0b2895fd940c39e1d2
Flavien David <flavien.david74@gmail.com>
2024-04-03 15:18:52 +0200

    Clamp notification content (#4548)
    * Clamp notification content
    
    * Bump to max 320px
    
    * :sparkles:

--- sparkle/src/components/Notification.tsx
+++ sparkle/src/components/Notification.tsx
@@ -28,7 +28,7 @@ export function Notification({
   return (
     <div
       className={classNames(
-        "s-pointer-events-auto s-flex s-max-w-[260px] s-flex-row s-items-center s-gap-2 s-rounded-xl s-border s-border-structure-100 s-bg-structure-0 s-p-4 s-shadow-xl",
+        "s-pointer-events-auto s-flex s-max-w-[400px] s-flex-row s-items-center s-gap-2 s-rounded-xl s-border s-border-structure-100 s-bg-structure-0 s-p-4 s-shadow-xl",
         className
       )}
     >
@@ -50,7 +50,7 @@ export function Notification({
         <div className="s-flex s-grow s-flex-row s-gap-6">
           <div
             className={classNames(
-              "s-text-md s-grow  s-font-semibold",
+              "s-text-md s-line-clamp-1 s-grow s-font-semibold",
               variant === "success"
                 ? "s-text-success-500"
                 : "s-text-warning-500"
@@ -66,7 +66,7 @@ export function Notification({
           />
         </div>
         {description && (
-          <div className="s-pr-2 s-text-sm s-font-normal s-text-element-700">
+          <div className="s-line-clamp-3 s-pr-2 s-text-sm s-font-normal s-text-element-700">
             {description}
           </div>
         )}
