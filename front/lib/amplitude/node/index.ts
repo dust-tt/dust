d2504fa456b338b05977244085dcaac8a8ce5821
Daphné Popin <daphne.popin@gmail.com>
2024-04-03 12:28:48 +0200

    Remove insert id

--- front/lib/amplitude/node/index.ts
+++ front/lib/amplitude/node/index.ts
@@ -213,7 +213,6 @@ export function trackDataSourceUpdated(
     { ...event, groups: { [GROUP_TYPE]: workspace.sId } },
     {
       time: Date.now(),
-      insert_id: `data_source_updated_${dataSource.id}_${Date.now()}`,
     }
   );
 }
