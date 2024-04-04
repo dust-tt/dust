c6ad5b1b5bbb9914e21ae389ec11c49f6992442f
Stanislas Polu <spolu@dust.tt>
2024-04-04 18:22:40 +0200

    dust-apps: more stability + observability (#4577)
    * dust-apps: more stability + observability
    
    * remove streaming retries

--- core/src/providers/anthropic.rs
+++ core/src/providers/anthropic.rs
@@ -541,11 +541,7 @@ impl AnthropicLLM {
                                     "Anthropic API Error: {}",
                                     event.error.to_string()
                                 ),
-                                retryable: Some(ModelErrorRetryOptions {
-                                    sleep: Duration::from_millis(500),
-                                    factor: 1,
-                                    retries: 1,
-                                }),
+                                retryable: None,
                             })?;
                             break 'stream;
                         }
@@ -557,7 +553,10 @@ impl AnthropicLLM {
                     }
                 },
                 Err(error) => {
-                    Err(anyhow!("Error streaming from Anthropic: {:?}", error))?;
+                    Err(anyhow!(
+                        "Error streaming tokens from Anthropic: {:?}",
+                        error
+                    ))?;
                     break 'stream;
                 }
             }
