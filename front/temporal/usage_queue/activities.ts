afe6b12a32e62047c36b04cdb5d7fea01e0e8af5
Flavien David <flavien@dust.tt>
2024-04-05 20:51:41 +0200

    :sparkles:

--- front/temporal/usage_queue/activities.ts
+++ front/temporal/usage_queue/activities.ts
@@ -1,4 +1,5 @@
-import { Subscription, Workspace } from "@app/lib/models";
+import { Plan, Subscription, Workspace } from "@app/lib/models";
+import { FREE_TEST_PLAN_CODE } from "@app/lib/plans/plan_codes";
 import { getStripeSubscription } from "@app/lib/plans/stripe";
 import { reportUsageForSubscriptionItems } from "@app/lib/plans/usage";
 import { renderLightWorkspaceType } from "@app/lib/workspace";
@@ -18,15 +19,25 @@ export async function recordUsageActivity(workspaceId: string) {
     where: {
       workspaceId: workspace.id,
     },
+    include: [Plan],
   });
 
   const logger = mainLogger.child({ workspaceId });
   logger.info({}, "[UsageQueue] Recording usage for worskpace.");
 
   if (!subscription) {
+    throw new Error(
+      "Cannot record usage of subscription: missing subscription."
+    );
+
+    return;
+  }
+
+  // Legacy free test plans don't have a Stripe subscription.
+  if (subscription.plan.code === FREE_TEST_PLAN_CODE) {
     logger.info(
-      {},
-      "[UsageQueue] No subscription found -- skipping reporting usage."
+      { subscription },
+      "[UsageQueue] Subscription is on free test plan -- skipping reporting usage."
     );
 
     return;
