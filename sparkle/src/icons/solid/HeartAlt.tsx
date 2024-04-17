f0ebd0c690a1b96fa985cd7162ec5263ae88f142
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-04-17 15:56:17 +0200

    Adding new Icons, updating Headre of ContextItem (#4738)

--- sparkle/src/icons/solid/HeartAlt.tsx
+++ sparkle/src/icons/solid/HeartAlt.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from "react";
 import * as React from "react";
-const SvgPencilSquare = (props: SVGProps<SVGSVGElement>) => (
+const SvgHeartAlt = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -9,11 +9,10 @@ const SvgPencilSquare = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <path fill="currentColor" d="M3 18.996h18v2H3v-2Z" />
     <path
       fill="currentColor"
-      d="m3 19 4 1.996 9.5-9.496-4-4L3 17v2ZM16.5 3.5l4 4-3 3-4-4 3-3Z"
+      d="M20.243 4.757a6 6 0 0 1 .236 8.236l-8.48 8.492-8.478-8.492a6 6 0 0 1 6.74-9.553L6.343 7.358l1.414 1.415L12 4.53l-.013-.014.014.013a5.998 5.998 0 0 1 8.242.228Z"
     />
   </svg>
 );
-export default SvgPencilSquare;
+export default SvgHeartAlt;
--- sparkle/src/icons/solid/MovingMail.tsx
+++ sparkle/src/icons/solid/MovingMail.tsx
@@ -9,13 +9,8 @@ const SvgMovingMail = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 24 24"
     {...props}
   >
-    <path
-      fill="currentColor"
-      fillRule="evenodd"
-      d="M22 3v18H2v-2h18V7.3l-8 7.2-10-9V3h20Z"
-      clipRule="evenodd"
-    />
-    <path fill="currentColor" d="M8 15v2H0v-2h8ZM5 10v2H0v-2h5Z" />
+    <path fill="currentColor" d="M2 6.76V20h20V6.76l-10 9.091-10-9.09Z" />
+    <path fill="currentColor" d="M22 4.058V4H2v.058l10 9.09 10-9.09Z" />
   </svg>
 );
 export default SvgMovingMail;
