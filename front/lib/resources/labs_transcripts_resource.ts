45f32915f11ae8ada118eb7eebe2d577faac9703
Flavien David <flavien.david74@gmail.com>
2024-04-18 16:34:55 +0200

    Add transcript resource (#4766)
    * Add transcript resource
    
    * Plumbing for blocking on completion

---
+++ front/lib/resources/labs_transcripts_resource.ts
@@ -0,0 +1,175 @@
+import type { ModelId, Result } from "@dust-tt/types";
+import type { LabsTranscriptsProviderType } from "@dust-tt/types";
+import { Err, Ok } from "@dust-tt/types";
+import type {
+  Attributes,
+  InferAttributes,
+  ModelStatic,
+  Transaction,
+} from "sequelize";
+import type { CreationAttributes } from "sequelize";
+
+import { BaseResource } from "@app/lib/resources/base_resource";
+import { LabsTranscriptsConfigurationModel } from "@app/lib/resources/storage/models/labs_transcripts";
+import { LabsTranscriptsHistoryModel } from "@app/lib/resources/storage/models/labs_transcripts";
+import type { ReadonlyAttributesType } from "@app/lib/resources/storage/types";
+
+// Attributes are marked as read-only to reflect the stateless nature of our Resource.
+// This design will be moved up to BaseResource once we transition away from Sequelize.
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface LabsTranscriptsConfigurationResource
+  extends ReadonlyAttributesType<LabsTranscriptsConfigurationModel> {}
+export class LabsTranscriptsConfigurationResource extends BaseResource<LabsTranscriptsConfigurationModel> {
+  static model: ModelStatic<LabsTranscriptsConfigurationModel> =
+    LabsTranscriptsConfigurationModel;
+
+  constructor(blob: Attributes<LabsTranscriptsConfigurationModel>) {
+    super(LabsTranscriptsConfigurationModel, blob);
+  }
+
+  static async makeNew(
+    blob: Omit<
+      CreationAttributes<LabsTranscriptsConfigurationModel>,
+      "isActive"
+    >
+  ): Promise<LabsTranscriptsConfigurationResource> {
+    const configuration = await LabsTranscriptsConfigurationModel.create({
+      ...blob,
+      isActive: false,
+    });
+
+    return new LabsTranscriptsConfigurationResource(configuration.get());
+  }
+
+  static async findByUserWorkspaceAndProvider({
+    userId,
+    workspaceId,
+    provider,
+  }: {
+    userId: ModelId;
+    workspaceId: ModelId;
+    provider: LabsTranscriptsProviderType;
+  }): Promise<LabsTranscriptsConfigurationResource | null> {
+    const configuration = await LabsTranscriptsConfigurationModel.findOne({
+      where: {
+        userId,
+        workspaceId,
+        provider,
+      },
+    });
+
+    return configuration
+      ? new LabsTranscriptsConfigurationResource(configuration.get())
+      : null;
+  }
+
+  private async update(
+    blob: Partial<Attributes<LabsTranscriptsConfigurationModel>>
+  ): Promise<[affectedCount: number]> {
+    return this.model.update(blob, {
+      where: {
+        id: this.id,
+      },
+    });
+  }
+
+  async setAgentConfigurationId({
+    agentConfigurationId,
+  }: {
+    agentConfigurationId: string | null;
+  }) {
+    if (this.agentConfigurationId === agentConfigurationId) {
+      return;
+    }
+
+    return this.update({ agentConfigurationId });
+  }
+
+  async setIsActive(isActive: boolean) {
+    if (this.isActive === isActive) {
+      return;
+    }
+
+    return this.update({ isActive });
+  }
+
+  async delete(transaction?: Transaction): Promise<Result<undefined, Error>> {
+    try {
+      await this.model.destroy({
+        where: {
+          id: this.id,
+        },
+        transaction,
+      });
+
+      await this.deleteHistory(transaction);
+
+      return new Ok(undefined);
+    } catch (err) {
+      return new Err(err as Error);
+    }
+  }
+
+  /**
+   * History
+   */
+
+  async recordHistory(
+    blob: Omit<CreationAttributes<LabsTranscriptsHistoryModel>, "id">
+  ): Promise<InferAttributes<LabsTranscriptsHistoryModel>> {
+    const history = await LabsTranscriptsHistoryModel.create(blob);
+
+    return history.get();
+  }
+
+  async fetchHistoryForFileId(
+    fileId: LabsTranscriptsHistoryModel["fileId"]
+  ): Promise<InferAttributes<LabsTranscriptsHistoryModel> | null> {
+    const history = await LabsTranscriptsHistoryModel.findOne({
+      where: {
+        fileId,
+      },
+    });
+
+    if (!history) {
+      return null;
+    }
+
+    return history.get();
+  }
+
+  async listHistory({
+    limit = 20,
+    sort = "DESC",
+  }: {
+    limit: number;
+    sort: "ASC" | "DESC";
+  }): Promise<InferAttributes<LabsTranscriptsHistoryModel>[]> {
+    const histories = await LabsTranscriptsHistoryModel.findAll({
+      where: {
+        configurationId: this.id,
+      },
+      limit,
+      order: [["createdAt", sort]],
+    });
+
+    return histories.map((history) => history.get());
+  }
+
+  private async deleteHistory(
+    transaction?: Transaction
+  ): Promise<Result<undefined, Error>> {
+    try {
+      await LabsTranscriptsHistoryModel.destroy({
+        where: {
+          id: this.id,
+        },
+        transaction,
+      });
+
+      return new Ok(undefined);
+    } catch (err) {
+      return new Err(err as Error);
+    }
+  }
+}
