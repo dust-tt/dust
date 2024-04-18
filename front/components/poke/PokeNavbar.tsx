5693d6e5ffc0d096a325d4d195a5e374a0c97ebc
Daphné Popin <PopDaph@users.noreply.github.com>
2024-04-18 10:20:13 +0200

    Poké header: Make Poké logo clickable (#4753)

--- front/components/poke/PokeNavbar.tsx
+++ front/components/poke/PokeNavbar.tsx
@@ -12,11 +12,10 @@ export const PokeNavbar: React.FC = () => (
     )}
   >
     <div className="flex items-center">
-      <Logo type="colored-grey" className="-mr-5 h-4 w-32 p-0" />
+      <Link href="/poke">
+        <Logo type="colored-grey" className="-mr-5 h-4 w-32 p-0" />
+      </Link>
       <div className="flex flex-row gap-4">
-        <Link href="/poke">
-          <div className="text-stucture-300 text-sm italic">Poké</div>
-        </Link>
         <Link href="/poke/plans">
           <div className="text-stucture-300 text-sm italic">Plans</div>
         </Link>
