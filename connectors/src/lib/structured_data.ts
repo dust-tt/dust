dfb58d122c73133248a419499868b5743793cc69
Henry Fontanier <henry@dust.tt>
2024-04-19 11:50:35 +0200

    r

--- connectors/src/lib/structured_data.ts
+++
@@ -1,6 +0,0 @@
-import { slugify } from "@dust-tt/types";
-
-export function makeStructuredDataTableName(name: string) {
-  // Keep a maximum of 32 characters of the name and slugify it.
-  return slugify(name.substring(0, 32));
-}
