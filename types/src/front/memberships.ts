48ad85f83ee695fdd5a9980a0f6042f9ccc028ea
Flavien David <flavien.david74@gmail.com>
2024-04-22 10:49:16 +0200

    Improve poke user table (#4792)
    * Improve poke user table
    
    * :see_no_evil:
    
    * :shirt:
    
    * :scissors:

--- types/src/front/memberships.ts
+++ types/src/front/memberships.ts
@@ -1,4 +1,4 @@
-const MEMBERSHIP_ROLE_TYPES = ["admin", "builder", "user"] as const;
+export const MEMBERSHIP_ROLE_TYPES = ["admin", "builder", "user"] as const;
 export type MembershipRoleType = (typeof MEMBERSHIP_ROLE_TYPES)[number];
 export function isMembershipRoleType(
   value: unknown
