f3684b0fbab5363b2089f41ad73f950cea151a56
Aric Lasry <aric@dust.tt>
2024-04-22 16:07:55 +0200

    [Gdrive] Fix webhook renewal when access to a shared drive is lost (#4797)
    * Revert "fix(google drive): put connector in error when drive is not found (#4770)"
    
    This reverts commit b6bce1dac541c7b7bc3d913c01936f5917b575af.
    
    * Fix: Gdrive webhook renwal for drive we lost access to.
    
    * logger
    
    * Fix typo

---
+++ connectors/migrations/20240422_fix_gdrive_errorType.ts
@@ -0,0 +1,64 @@
+import { getGoogleDriveObject } from "@connectors/connectors/google_drive/lib/google_drive_api";
+import { getAuthObject } from "@connectors/connectors/google_drive/temporal/utils";
+import logger from "@connectors/logger/logger";
+import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
+
+const { LIVE } = process.env;
+
+async function main() {
+  // Select all connectors with an associated githubconnectorstate that has codeSyncEnabled set to true
+
+  const connectors = await ConnectorModel.findAll({
+    where: {
+      type: "google_drive",
+      errorType: "oauth_token_revoked",
+    },
+  });
+
+  logger.info(
+    `Found ${connectors.length} connectors with errorType: oauth_token_revoked`
+  );
+
+  const toReEnable = [];
+  for (const connector of connectors) {
+    try {
+      const auth = await getAuthObject(connector.connectionId);
+      const gDriveObject = await getGoogleDriveObject(auth, "root");
+      logger.info(
+        {
+          connectorId: connector.id,
+          // gDriveObject being null still means we have a valid access token
+          fileName: gDriveObject?.name,
+        },
+        `Successfully fetched root folder for connector`
+      );
+      toReEnable.push(connector.id);
+    } catch (e) {
+      // no-op, this is expected
+    }
+  }
+
+  if (LIVE) {
+    for (const id of toReEnable) {
+      await ConnectorModel.update(
+        {
+          errorType: null,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+    }
+  }
+  logger.info(
+    {
+      toReEnable,
+    },
+    "Done!"
+  );
+}
+main()
+  .then(() => console.log("Done"))
+  .catch(console.error);
