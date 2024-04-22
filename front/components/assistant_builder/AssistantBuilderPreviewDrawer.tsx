882af7ad126c6ffc4e7b1e44306e10ebb99e6c64
Daphné Popin <PopDaph@users.noreply.github.com>
2024-04-22 11:17:40 +0200

    Rework Builder Layout for Templates (#4791)
    * Rework Builder Layout for Templates
    
    * Add auto scroll & fix margins
    
    * Apply Ed's feedabck

--- front/components/assistant_builder/AssistantBuilderPreviewDrawer.tsx
+++ front/components/assistant_builder/AssistantBuilderPreviewDrawer.tsx
@@ -1,17 +1,25 @@
 import {
   ChatBubbleBottomCenterTextIcon,
+  ContextItem,
+  LightbulbIcon,
+  MagicIcon,
   Markdown,
+  Page,
   Tab,
-  TemplateIcon,
 } from "@dust-tt/sparkle";
 import type { WorkspaceType } from "@dust-tt/types";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
+import ConversationViewer from "@app/components/assistant/conversation/ConversationViewer";
+import { GenerationContextProvider } from "@app/components/assistant/conversation/GenerationContextProvider";
+import { AssistantInputBar } from "@app/components/assistant/conversation/input_bar/InputBar";
+import { CONVERSATION_PARENT_SCROLL_DIV_ID } from "@app/components/assistant/conversation/lib";
 import {
-  TryAssistant,
   usePreviewAssistant,
+  useTryAssistantCore,
 } from "@app/components/assistant/TryAssistant";
 import type { AssistantBuilderState } from "@app/components/assistant_builder/types";
+import { useUser } from "@app/lib/swr";
 import { classNames } from "@app/lib/utils";
 import type { FetchAssistantTemplateResponse } from "@app/pages/api/w/[wId]/assistant/builder/templates/[tId]";
 
@@ -33,20 +41,20 @@ export default function AssistantBuilderPreviewDrawer({
   const previewDrawerTabs = useMemo(
     () => [
       {
+        label: "Template",
+        current: previewDrawerCurrentTab === "Template",
+        onClick: () => {
+          setPreviewDrawerCurrentTab("Template");
+        },
+        icon: MagicIcon,
+      },
+      {
         label: "Preview",
         current: previewDrawerCurrentTab === "Preview",
         onClick: () => {
           setPreviewDrawerCurrentTab("Preview");
         },
         icon: ChatBubbleBottomCenterTextIcon,
-      },
-      {
-        label: "Template",
-        current: previewDrawerCurrentTab === "Template",
-        onClick: () => {
-          setPreviewDrawerCurrentTab("Template");
-        },
-        icon: TemplateIcon,
       },
     ],
     [previewDrawerCurrentTab]
@@ -58,40 +66,109 @@ export default function AssistantBuilderPreviewDrawer({
     isFading,
   } = usePreviewAssistant({ owner, builderState });
 
+  const { user } = useUser();
+  const {
+    conversation,
+    setConversation,
+    stickyMentions,
+    setStickyMentions,
+    handleSubmit,
+  } = useTryAssistantCore({
+    owner,
+    user,
+    assistant: draftAssistant,
+  });
+
+  useEffect(() => {
+    setConversation(null);
+  }, [draftAssistant?.sId, setConversation]);
+
   return (
-    <div className="h-full pb-5">
-      {template ? (
-        <Tab
-          tabs={previewDrawerTabs}
-          variant="default"
-          className="hidden lg:flex"
-        />
-      ) : null}
-      {previewDrawerCurrentTab === "Preview" ? (
-        <div
-          className={classNames(
-            "flex h-full w-full overflow-hidden rounded-xl border border-structure-200 bg-structure-50 transition-all",
-            shouldAnimatePreviewDrawer &&
-              previewDrawerOpenedAt != null &&
-              // Only animate the reload if the drawer has been open for at least 1 second.
-              // This is to prevent the animation from triggering right after the drawer is opened.
-              Date.now() - previewDrawerOpenedAt > 1000
-              ? "animate-reload"
-              : ""
-          )}
-        >
-          <TryAssistant
-            owner={owner}
-            assistant={draftAssistant}
-            conversationFading={isFading}
+    <div className="flex h-full flex-col">
+      {template && (
+        <div className="shrink-0 bg-white pt-5">
+          <Tab
+            tabs={previewDrawerTabs}
+            variant="default"
+            className="hidden lg:flex"
           />
         </div>
-      ) : (
-        <Markdown
-          content={template?.helpInstructions ?? ""}
-          className="pr-8 pt-4"
-        />
       )}
+      <div
+        className={classNames(
+          template !== null
+            ? "grow-1 mb-5 h-full overflow-y-auto rounded-b-xl border-x border-b border-structure-200 bg-structure-50 pt-5"
+            : "grow-1 mb-5 mt-5 h-full overflow-y-auto rounded-xl border border-structure-200 bg-structure-50",
+          shouldAnimatePreviewDrawer &&
+            previewDrawerOpenedAt != null &&
+            // Only animate the reload if the drawer has been open for at least 1 second.
+            // This is to prevent the animation from triggering right after the drawer is opened.
+            Date.now() - previewDrawerOpenedAt > 1000
+            ? "animate-reload"
+            : ""
+        )}
+      >
+        {previewDrawerCurrentTab === "Preview" && user && draftAssistant && (
+          <div className="flex h-full w-full flex-1 flex-col justify-between">
+            <GenerationContextProvider>
+              <div
+                className="flex-grow overflow-y-auto overflow-x-hidden"
+                id={CONVERSATION_PARENT_SCROLL_DIV_ID.modal}
+              >
+                {conversation && (
+                  <ConversationViewer
+                    owner={owner}
+                    user={user}
+                    conversationId={conversation.sId}
+                    onStickyMentionsChange={setStickyMentions}
+                    isInModal
+                    hideReactions
+                    isFading={isFading}
+                    key={conversation.sId}
+                  />
+                )}
+              </div>
+              <div className="shrink-0">
+                <AssistantInputBar
+                  owner={owner}
+                  onSubmit={handleSubmit}
+                  stickyMentions={stickyMentions}
+                  conversationId={conversation?.sId || null}
+                  additionalAgentConfiguration={draftAssistant}
+                  hideQuickActions
+                  disableAutoFocus
+                  isFloating={false}
+                />
+              </div>
+            </GenerationContextProvider>
+          </div>
+        )}
+        {previewDrawerCurrentTab === "Template" && (
+          <div className="mb-72 flex flex-col gap-4 px-6">
+            <Page.Header icon={LightbulbIcon} title="Template's User manual" />
+            <Page.Separator />
+            <div id="instructions-help-container">
+              <ContextItem.SectionHeader
+                title='"Instructions" guide'
+                hasBorder={false}
+              />
+              <Markdown
+                content={template?.helpInstructions ?? ""}
+                className=""
+              />
+            </div>
+            <Page.Separator />
+
+            <div id="actions-help-container">
+              <ContextItem.SectionHeader
+                title='"Actions" guide'
+                hasBorder={false}
+              />
+              <Markdown content={template?.helpActions ?? ""} className="" />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
