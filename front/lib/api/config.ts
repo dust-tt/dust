2354a5895f867fde7e2c4f2d7c89345178ac8ff6
Alban Dumouilla <alban@dust.tt>
2024-05-02 16:29:43 +0200

    Remove getDeveloperSecretsSecret

--- front/lib/api/config.ts
+++ front/lib/api/config.ts
@@ -19,9 +19,6 @@ const config = {
   getDustInviteTokenSecret: (): string => {
     return EnvironmentConfig.getEnvVariable("DUST_INVITE_TOKEN_SECRET");
   },
-  getDeveloperSecretsSecret: (): string => {
-    return EnvironmentConfig.getEnvVariable("DUST_DEVELOPER_SECRETS_SECRET");
-  },
   getSendgridApiKey: (): string => {
     return EnvironmentConfig.getEnvVariable("SENDGRID_API_KEY");
   },
