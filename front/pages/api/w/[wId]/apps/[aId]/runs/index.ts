22959a6f2db14f01732eb9d0e67912e58fd51a47
Alban Dumouilla <alban@dust.tt>
2024-05-02 14:34:54 +0200

    Removing body.mode

--- front/pages/api/w/[wId]/apps/[aId]/runs/index.ts
+++ front/pages/api/w/[wId]/apps/[aId]/runs/index.ts
@@ -78,109 +78,95 @@ async function handler(
         }),
       ]);
 
-      switch (req.body.mode) {
-        // Run creation as part of the app design process (Specification pane).
-        case "design":
-          if (
-            !req.body ||
-            !(typeof req.body.config == "string") ||
-            !(typeof req.body.specification === "string")
-          ) {
-            return apiError(req, res, {
-              status_code: 400,
-              api_error: {
-                type: "invalid_request_error",
-                message:
-                  "The request body is invalid, expects { config: string, specificationHash: string }.",
-              },
-            });
-          }
-
-          const datasets = await coreAPI.getDatasets({
-            projectId: app.dustAPIProjectId,
-          });
-          if (datasets.isErr()) {
-            return apiError(req, res, {
-              status_code: 500,
-              api_error: {
-                type: "internal_server_error",
-                message: "Datasets retrieval failed.",
-                app_error: datasets.error,
-              },
-            });
-          }
-
-          const latestDatasets: { [key: string]: string } = {};
-          for (const d in datasets.value.datasets) {
-            latestDatasets[d] = datasets.value.datasets[d][0].hash;
-          }
-
-          const config = JSON.parse(req.body.config);
-          const inputConfigEntry: any = Object.values(config).find(
-            (configValue: any) => configValue.type == "input"
-          );
-          const inputDataset = inputConfigEntry
-            ? inputConfigEntry.dataset
-            : null;
-
-          const dustRun = await coreAPI.createRun({
-            projectId: app.dustAPIProjectId,
-            runAsWorkspaceId: owner.sId,
-            runType: "local",
-            specification: dumpSpecification(
-              JSON.parse(req.body.specification),
-              latestDatasets
-            ),
-            datasetId: inputDataset,
-            config: { blocks: config },
-            credentials: credentialsFromProviders(providers),
-          });
-
-          if (dustRun.isErr()) {
-            return apiError(req, res, {
-              status_code: 400,
-              api_error: {
-                type: "run_error",
-                message: "Run creation failed.",
-                run_error: dustRun.error,
-              },
-            });
-          }
-
-          await Promise.all([
-            Run.create({
-              dustRunId: dustRun.value.run.run_id,
-              appId: app.id,
-              runType: "local",
-              workspaceId: owner.id,
-            }),
-            App.update(
-              {
-                savedSpecification: req.body.specification,
-                savedConfig: req.body.config,
-                savedRun: dustRun.value.run.run_id,
-              },
-              {
-                where: {
-                  id: app.id,
-                },
-              }
-            ),
-          ]);
-
-          res.status(200).json({ run: dustRun.value.run });
-          return;
-
-        default:
-          return apiError(req, res, {
-            status_code: 400,
-            api_error: {
-              type: "invalid_request_error",
-              message:
-                "The request body is invalid, expects `mode` to be one of `design` or `execute` .",
-            },
-          });
+      if (
+        !req.body ||
+        !(typeof req.body.config == "string") ||
+        !(typeof req.body.specification === "string")
+      ) {
+        return apiError(req, res, {
+          status_code: 400,
+          api_error: {
+            type: "invalid_request_error",
+            message:
+              "The request body is invalid, expects { config: string, specificationHash: string }.",
+          },
+        });
       }
+
+      const datasets = await coreAPI.getDatasets({
+        projectId: app.dustAPIProjectId,
+      });
+      if (datasets.isErr()) {
+        return apiError(req, res, {
+          status_code: 500,
+          api_error: {
+            type: "internal_server_error",
+            message: "Datasets retrieval failed.",
+            app_error: datasets.error,
+          },
+        });
+      }
+
+      const latestDatasets: { [key: string]: string } = {};
+      for (const d in datasets.value.datasets) {
+        latestDatasets[d] = datasets.value.datasets[d][0].hash;
+      }
+
+      const config = JSON.parse(req.body.config);
+      const inputConfigEntry: any = Object.values(config).find(
+        (configValue: any) => configValue.type == "input"
+      );
+      const inputDataset = inputConfigEntry
+        ? inputConfigEntry.dataset
+        : null;
+
+      const dustRun = await coreAPI.createRun({
+        projectId: app.dustAPIProjectId,
+        runAsWorkspaceId: owner.sId,
+        runType: "local",
+        specification: dumpSpecification(
+          JSON.parse(req.body.specification),
+          latestDatasets
+        ),
+        datasetId: inputDataset,
+        config: { blocks: config },
+        credentials: credentialsFromProviders(providers),
+      });
+
+      if (dustRun.isErr()) {
+        return apiError(req, res, {
+          status_code: 400,
+          api_error: {
+            type: "run_error",
+            message: "Run creation failed.",
+            run_error: dustRun.error,
+          },
+        });
+      }
+
+      await Promise.all([
+        Run.create({
+          dustRunId: dustRun.value.run.run_id,
+          appId: app.id,
+          runType: "local",
+          workspaceId: owner.id,
+        }),
+        App.update(
+          {
+            savedSpecification: req.body.specification,
+            savedConfig: req.body.config,
+            savedRun: dustRun.value.run.run_id,
+          },
+          {
+            where: {
+              id: app.id,
+            },
+          }
+        ),
+      ]);
+
+      res.status(200).json({ run: dustRun.value.run });
+      return;
 
     case "GET":
       if (req.query.wIdTarget) {
