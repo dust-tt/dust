1f7e0f2a2b2b916629d129d5ced14c88e4965fde
Alban Dumouilla <alban@dust.tt>
2024-05-06 10:09:52 +0200

    Move dustappSecrets db queries to lib/api (#4980)

---
+++ front/lib/api/dust_app_secrets.ts
@@ -0,0 +1,52 @@
+import type { DustAppSecretType } from "@dust-tt/types";
+import { decrypt, redactString } from "@dust-tt/types";
+
+import type { Authenticator } from "@app/lib/auth";
+import { DustAppSecret } from "@app/lib/models/workspace";
+
+export async function getDustAppSecrets(
+  auth: Authenticator
+): Promise<DustAppSecretType[]> {
+  const owner = auth.workspace();
+  if (!owner) {
+    return [];
+  }
+
+  const secrets = await DustAppSecret.findAll({
+    where: {
+      workspaceId: owner.id,
+    },
+    order: [["name", "DESC"]],
+  });
+
+  return secrets.map((s) => {
+    const clearSecret = decrypt(s.hash, owner.sId);
+    return {
+      name: s.name,
+      value: redactString(clearSecret, 1),
+    };
+  });
+}
+
+export async function getDustAppSecret(
+  auth: Authenticator,
+  name: string
+): Promise<DustAppSecret | null> {
+  const owner = auth.workspace();
+  if (!owner) {
+    return null;
+  }
+
+  const secret = await DustAppSecret.findOne({
+    where: {
+      name: name,
+      workspaceId: owner.id,
+    },
+  });
+
+  if (!secret) {
+    return null;
+  }
+
+  return secret;
+}
