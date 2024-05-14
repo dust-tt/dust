1171065d1c6e7fba77cf85f893153f40ead451a2
Flavien David <flavien.david74@gmail.com>
2024-05-14 18:53:41 +0200

    Add script to replay upsert queue document tasks (#5053)
    * Add script to replay upsert queue document tasks
    
    * :sparkles:
    
    * Fixes
    
    * :sparkles:
    
    * :book:
    
    * :see_no_evil:
    
    * :shirt:

---
+++ front/scripts/replay-upsert-document-queue-tasks.ts
@@ -0,0 +1,86 @@
+import { WorkflowNotFoundError } from "@temporalio/common";
+import { parse } from "csv-parse";
+import fs from "fs";
+import path from "path";
+
+import { getTemporalClient } from "@app/lib/temporal";
+import type { Logger } from "@app/logger/logger";
+import { makeScript } from "@app/scripts/helpers";
+import { launchUpsertDocumentWorkflow } from "@app/temporal/upsert_queue/client";
+
+async function terminateWorkflow(workflowId: string, logger: Logger) {
+  const client = await getTemporalClient();
+  try {
+    const workflowHandle = client.workflow.getHandle(workflowId);
+    await workflowHandle.terminate();
+    logger.info({ workflowId }, "Workflow successfully terminated.");
+    return true;
+  } catch (e) {
+    if (e instanceof WorkflowNotFoundError) {
+      logger.info({ workflowId }, "Workflow not found -- skipping.");
+    } else {
+      throw e;
+    }
+  }
+  return false;
+}
+
+makeScript(
+  {
+    filePath: {
+      type: "string",
+      description: "The path to the file",
+    },
+  },
+  async ({ execute, filePath }, logger) => {
+    const csvPath = path.resolve(filePath);
+
+    if (!fs.existsSync(path.resolve(csvPath))) {
+      logger.info({ csvPath }, "File not found.");
+      return;
+    }
+
+    const parseTasks = parse({ columns: true, delimiter: "," });
+
+    fs.createReadStream(filePath).pipe(parseTasks);
+
+    for await (const task of parseTasks) {
+      // Remove the double quotes if they exist.
+      for (const key in task) {
+        if (typeof task[key] === "string") {
+          task[key] = task[key].replace(/"/g, "");
+        }
+      }
+
+      const { workspaceId, dataSourceName, upsertQueueId, enqueueTimestamp } =
+        task;
+
+      const workflowId = `upsert-queue-document-${workspaceId}-${dataSourceName}-${upsertQueueId}`;
+
+      logger.info(
+        { workspaceId, workflowId },
+        "Processing upsert document task."
+      );
+
+      if (!execute) {
+        continue;
+      }
+
+      // First, we terminate the current workflow.
+      await terminateWorkflow(workflowId, logger);
+
+      // The, we restart the workflow.
+      await launchUpsertDocumentWorkflow({
+        workspaceId,
+        dataSourceName,
+        upsertQueueId,
+        enqueueTimestamp,
+      });
+
+      logger.info(
+        { workspaceId, workflowId },
+        "Successfully processed upsert document task."
+      );
+    }
+  }
+);
