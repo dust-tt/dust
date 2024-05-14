c8d93fd4412b867e163117f8dc6a1016b8c89ac3
Stanislas Polu <spolu@dust.tt>
2024-05-14 13:01:20 +0200

    connectors prettier fix

--- connectors/src/api/webhooks/webhook_slack.ts
+++ connectors/src/api/webhooks/webhook_slack.ts
@@ -155,9 +155,8 @@ const _webhookSlackAPIHandler = async (
         status_code: 400,
       });
     }
-    const slackConfigurations = await SlackConfigurationResource.listForTeamId(
-      teamId
-    );
+    const slackConfigurations =
+      await SlackConfigurationResource.listForTeamId(teamId);
     if (slackConfigurations.length === 0) {
       return apiError(req, res, {
         api_error: {
@@ -203,9 +202,8 @@ const _webhookSlackAPIHandler = async (
             // Ignore message_changed events in private messages
             return res.status(200).send();
           }
-          const slackConfig = await SlackConfigurationResource.fetchByActiveBot(
-            teamId
-          );
+          const slackConfig =
+            await SlackConfigurationResource.fetchByActiveBot(teamId);
           if (!slackConfig) {
             return apiError(req, res, {
               api_error: {
