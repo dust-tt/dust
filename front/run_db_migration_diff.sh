63deab83e3fa3f429c1b6459793ad95a0fac2e38
Flavien David <flavien@dust.tt>
2024-05-15 22:14:26 +0200

    Improve db migration diff script

--- front/run_db_migration_diff.sh
+++ front/run_db_migration_diff.sh
@@ -2,31 +2,36 @@
 
 # Stash any uncommitted changes.
 echo "Stashing uncommitted changes..."
-git stash push -m "Temp stash for running diff"
+git stash push -m "Temp stash for running diff" --quiet
 
 # Get the current branch name.
 original_branch=$(git symbolic-ref --short HEAD)
 echo "Original branch: $original_branch"
 
-# Checkout main branch and run command.
-echo "Checking out main branch..."
-git checkout main
-echo "Running command on main branch..."
+# Checkout main branch and reset DB state to main.
+echo "Checking out main branch and resetting DB state..."
+git checkout main --quiet
+echo "First run: Resetting DB to main branch state..."
 NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > main_output.txt
 
-# Checkout original branch and run command.
-echo "Checking out $original_branch branch..."
-git checkout $original_branch
-echo "Running command on $original_branch branch..."
-NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > current_output.txt
+# Run the command a second time to capture the stable production state.
+echo "Second run: Capturing stable production state..."
+NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > main_output.txt
+
 
 # Pop the stash.
 echo "Restoring original changes..."
-git stash pop
+git stash pop --quiet
+
+# Checkout original branch and run command.
+echo "Checking out $original_branch branch..."
+git checkout $original_branch --quiet
+echo "Running command on $original_branch branch..."
+NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > current_output.txt
 
 # Run diff.
 echo "Running diff..."
-diff --unified=0 main_output.txt current_output.txt
+diff --unified=0 --color=always main_output.txt current_output.txt
 
 # Clean up the output files.
 echo "Cleaning up temporary files..."
