5d23bdaa0957aa3dc71214f9a359d21e21df5fa2
Flavien David <flavien@dust.tt>
2024-05-16 12:09:01 +0200

    Improve script

--- front/run_db_migration_diff.sh
+++ front/run_db_migration_diff.sh
@@ -6,6 +6,9 @@ if [ "$NODE_ENV" == "production" ]; then
   exit 1
 fi
 
+# Get current date in YYYYMMDD format
+current_date=$(date +%Y%m%d)
+
 # Stash any uncommitted changes.
 echo "Stashing uncommitted changes..."
 git stash push -m "Temp stash for running diff" --quiet
@@ -46,14 +49,15 @@ diff --unified=0 --color=always main_output.txt current_output.txt
 echo "Running diff and extracting SQL statements..."
 diff --unified=0 main_output.txt current_output.txt | awk '/^\+[^+]/ {print substr($0, 2)}' > diff_output.txt
 
-# # Find the last migration version
-# last_version=$(ls ./migrations/db/ | grep -oP 'migration_\K\d+' | sort -nr | head -n1)
-# next_version=$((last_version + 1))
-# formatted_next_version=$(printf "%02d" $next_version)
+# Find the last migration version.
+last_version=$(ls ./migrations/db | grep -oE 'migration_[0-9]{8}_([0-9]+).sql' | grep -oE '([0-9]+)\.sql$' | sed s/\.sql// | sort -n | tail -n1)
+# 10# ensures the number is interpreted as base-10, preventing errors with leading zeros.
+next_version=$(printf "%02d" $((10#$last_version + 1)))
+echo "Creating SQL migration $next_version."
 
-# # Save the latest changes to a new migration file
-# mv diff_output.txt "./migrations/db/migration_$formatted_next_version.sql"
+# Save the latest changes to a new migration file
+mv diff_output.txt "./migrations/db/migration_${current_date}_${next_version}.sql"
 
-# # Clean up the output files.
-# echo "Cleaning up temporary files..."
-# # rm main_output.txt current_output.txt
+# Clean up the output files.
+echo "Cleaning up temporary files..."
+rm main_output.txt current_output.txt
