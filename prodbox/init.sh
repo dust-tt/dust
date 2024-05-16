2f9b1455a052fee49a6f4f9e6b1e4e9a5f6735c0
Aric Lasry <aric@dust.tt>
2024-05-16 10:55:30 +0200

    Fix prodbox initialization (#5085)
    * Fix prodbox initialization
    
    * Adjusting init script path.
    
    * Slimmer dockerfile for testing faster
    
    * Try again

---
+++ prodbox/init.sh
@@ -0,0 +1,18 @@
+# This script is run when the prodbox container is started.
+# Some commands can only be run once the container is deployed, because they rely on some files
+# that are not available at build time.
+
+# only allow to pull via fast-forward
+git config pull.ff only
+
+# Setting up the ssh key to pull from Github
+# we need to copy the key from the mounted volume because ssh only accept keys
+# that are not readable by others and we can't chmod on the mounted volume.
+mkdir -p ~/.ssh
+cp /etc/github-deploykey-deploybox/github-deploykey-deploybox ~/.ssh/github-deploykey-deploybox
+chmod 600 ~/.ssh/github-deploykey-deploybox
+
+# Only allow to pull via fast-forward
+git config pull.ff only
+
+bash
