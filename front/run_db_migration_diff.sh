88112210d8ba66b386814381ade1157a83c07ec4
Flavien David <flavien.david74@gmail.com>
2024-05-16 12:36:02 +0200

    Generate SQL statements from migrations (#5089)
    * Only pop the stash if branch was stale
    
    * Prevent running the script on production environment
    
    * Block init_db in production
    
    * Avoid slack notification on development
    
    * Extract SQL statement
    
    * Improve script
    
    * Attempt fake update on Plan model
    
    * Revert "Attempt fake update on Plan model"
    
    This reverts commit a8be6e367cb2cc62c4c9c3a61ae1a3acf55ff528.
    
    * :sparkles:
    
    * Remove date from file name

--- front/run_db_migration_diff.sh
+++ front/run_db_migration_diff.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Ensure NODE_ENV is not set to production.
+if [ "$NODE_ENV" == "production" ]; then
+  echo "Error: NODE_ENV is set to production. Aborting script."
+  exit 1
+fi
+
+# Get current date in a human-readable format (e.g., May 28, 2024)
+current_date=$(date +"%b %d, %Y")
+
 # Stash any uncommitted changes.
 echo "Stashing uncommitted changes..."
 git stash push -m "Temp stash for running diff" --quiet
@@ -18,10 +27,13 @@ NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > main_
 echo "Second run: Capturing stable production state..."
 NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > main_output.txt
 
-
-# Pop the stash.
-echo "Restoring original changes..."
-git stash pop --quiet
+# Determine if there were any stashed changes.
+stash_list=$(git stash list)
+if [[ $stash_list == *"Temp stash for running diff"* ]]; then
+  # Pop the stash if it exists.
+  echo "Restoring original changes..."
+  git stash pop --quiet
+fi
 
 # Checkout original branch and run command.
 echo "Checking out $original_branch branch..."
@@ -33,7 +45,20 @@ NODE_ENV=development DB_LOGGING_ENABLED=true ./admin/init_db.sh --unsafe > curre
 echo "Running diff..."
 diff --unified=0 --color=always main_output.txt current_output.txt
 
+# Run diff and extract only SQL statements.
+echo "Running diff and extracting SQL statements..."
+echo "-- Migration created on $current_date" > diff_output.txt
+diff --unified=0 main_output.txt current_output.txt | awk '/^\+[^+]/ {print substr($0, 2)}' >> diff_output.txt
+
+# Find the last migration version.
+last_version=$(ls ./migrations/db | grep -oE 'migration_([0-9]+).sql' | grep -oE '([0-9]+)\.sql$' | sed s/\.sql// | sort -n | tail -n1)
+# 10# ensures the number is interpreted as base-10, preventing errors with leading zeros.
+next_version=$(printf "%02d" $((10#$last_version + 1)))
+echo "Creating SQL migration $next_version."
+
+# Save the latest changes to a new migration file.
+mv diff_output.txt "./migrations/db/migration_${next_version}.sql"
+
 # Clean up the output files.
 echo "Cleaning up temporary files..."
 rm main_output.txt current_output.txt
-
