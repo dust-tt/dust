3ab93583d7fd794b7e77227f848e64e8648a8fe3
Flavien David <flavien.david74@gmail.com>
2024-05-17 15:25:11 +0200

    Flav/add missing foreign key indexes (#5119)
    * Add missing foreign key indexes.
    
    * Add migration
    
    * Add  concurrently: true
    
    * Update migration

---
+++ front/migrations/db/migration_05.sql
@@ -0,0 +1,8 @@
+-- Migration created on May 17, 2024
+CREATE INDEX CONCURRENTLY "conversation_participants_conversation_id" ON "conversation_participants" ("conversationId")
+CREATE INDEX CONCURRENTLY "messages_agent_message_id" ON "messages" ("agentMessageId")
+CREATE INDEX CONCURRENTLY "messages_user_message_id" ON "messages" ("userMessageId")
+CREATE INDEX CONCURRENTLY "messages_content_fragment_id" ON "messages" ("contentFragmentId")
+CREATE INDEX CONCURRENTLY "messages_parent_id" ON "messages" ("parentId")
+CREATE INDEX CONCURRENTLY "message_reactions_user_id" ON "message_reactions" ("userId")
+CREATE INDEX CONCURRENTLY "mentions_user_id" ON "mentions" ("userId")
