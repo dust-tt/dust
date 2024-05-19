4ba3d3c2790507f77286d702649a3872cd2a7854
Flavien David <flavien.david74@gmail.com>
2024-05-19 22:25:06 +0200

    Revert "Randomize Notion search page_size on 504s. (#5134)" (#5136)
    This reverts commit fb92fcd7ff2182f00f0b6953ee233d99fe52279d.

--- connectors/src/connectors/notion/lib/notion_api.ts
+++ connectors/src/connectors/notion/lib/notion_api.ts
@@ -105,13 +105,8 @@ export async function getPagesAndDatabasesEditedSince(
   let resultsPage: SearchResponse | null = null;
 
   let tries = 0;
-  let page_size = 90;
   while (tries < retry.retries) {
-    const tryLogger = localLogger.child({
-      tries,
-      maxTries: retry.retries,
-      page_size,
-    });
+    const tryLogger = localLogger.child({ tries, maxTries: retry.retries });
     tryLogger.info("Fetching result page from Notion API.");
     try {
       resultsPage = await wrapNotionAPITokenErrors(async () => {
@@ -123,7 +118,7 @@ export async function getPagesAndDatabasesEditedSince(
               }
             : undefined,
           start_cursor: cursor || undefined,
-          page_size,
+          page_size: 90,
         });
       });
       tryLogger.info(
@@ -138,13 +133,6 @@ export async function getPagesAndDatabasesEditedSince(
       tries += 1;
       if (tries >= retry.retries) {
         throw e;
-      }
-
-      // Notion API sometimes returns 504 errors.
-      // In such cases, randomize the page_size before retrying.
-      if (APIResponseError.isAPIResponseError(e) && e.status === 504) {
-        // Generates a random number between 80 and 100.
-        page_size = Math.floor(Math.random() * (100 - 80 + 1) + 80);
       }
 
       const sleepTime = 500 * retry.backoffFactor ** tries;
