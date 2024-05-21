5c5b8b2933dd22b223b02f8a8015db15d43e4f8f
Aric Lasry <lasry.aric@gmail.com>
2024-05-21 13:09:42 +0200

    s/~//root/

--- prodbox/init.sh
+++ prodbox/init.sh
@@ -14,9 +14,9 @@ git config pull.ff only
 # Setting up the ssh key to pull from Github
 # we need to copy the key from the mounted volume because ssh only accept keys
 # that are not readable by others and we can't chmod on the mounted volume.
-mkdir -p ~/.ssh
-cp /etc/github-deploykey-deploybox/github-deploykey-deploybox ~/.ssh/github-deploykey-deploybox
-chmod 600 ~/.ssh/github-deploykey-deploybox
+mkdir -p /root/.ssh
+cp /etc/github-deploykey-deploybox/github-deploykey-deploybox /root/.ssh/github-deploykey-deploybox
+chmod 600 /root/.ssh/github-deploykey-deploybox
 
 # Only allow to pull via fast-forward
 git config pull.ff only
