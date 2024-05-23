5f627be0d2b6d5e71d624aaa47efe9276ecaeb8c
Flavien David <flavien.david74@gmail.com>
2024-05-23 16:56:39 +0200

    Revert "Use Qdrant cluster-0 as default clusters for all new data sources. (#…" (#5248)
    This reverts commit 28fb47b6b9abd1fdde7ab63de7041ae40059109c.

--- types/src/core/data_source.ts
+++ types/src/core/data_source.ts
@@ -6,7 +6,7 @@ export type QdrantCluster =
   | "cluster-0";
 
 export const DEFAULT_FREE_QDRANT_CLUSTER: QdrantCluster = "cluster-0";
-export const DEFAULT_PAID_QDRANT_CLUSTER: QdrantCluster = "cluster-0";
+export const DEFAULT_PAID_QDRANT_CLUSTER: QdrantCluster = "dedicated-2";
 
 export type CoreAPIDataSourceConfig = {
   provider_id: string;
