1ec6ec63a51abda07da68138fc236ffa48e364fb
Daphné Popin <PopDaph@users.noreply.github.com>
2024-05-23 15:45:29 +0200

    Intercom fix logs to understand daily small burst (#5242)

--- connectors/src/connectors/intercom/lib/intercom_api.ts
+++ connectors/src/connectors/intercom/lib/intercom_api.ts
@@ -91,7 +91,7 @@ async function queryIntercomAPI({
     return response;
   } catch (e) {
     logger.info(
-      { path, response: rawResponse.text, status: rawResponse.status },
+      { path, response: text, status: rawResponse.status },
       "Failed to parse Intercom JSON response."
     );
 
