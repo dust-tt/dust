e5a156a967f33647b0dfb5e16acb8e04eec573bc
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-05-24 10:50:22 +0200

    Website fixes (#5237)
    * Fixing Caroussel
    
    * Button size in caroussel fixing
    
    * Make Button Label truncatable

--- front/components/home/content/Product/BlogSection.tsx
+++ front/components/home/content/Product/BlogSection.tsx
@@ -34,18 +34,17 @@ export function BlogSection() {
         </P>
       </div>
       <div className="col-span-12 flex flex-col items-center gap-4">
-        <Carousel className="w-full" isLooping={true}>
-          <div className="flex w-full flex-row justify-center gap-4">
+        <Carousel className="w-full">
+          <div className="flex w-full flex-row gap-4 md:justify-center">
             <CarouselPrevious label="previous" />
             <CarouselNext label="next" />
           </div>
           <CarouselContent>
-            <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+            <CarouselItem className="basis-full md:basis-1/2 md:px-6 lg:basis-1/3">
               <BlogBlock
                 title="Navigating Growth and Innovation with November Five’s Dario Prskalo"
                 content="Discover how November Five leverages AI with Dust to enhance efficiency and maintain a human touch in their digital solutions."
                 href="https://blog.dust.tt/november-five-ai-transformation-dust/"
-                className="m-6 lg:m-4 xl:m-6"
               >
                 <img
                   src="https://blog.dust.tt/content/images/size/w2000/2024/04/DSCF6552-1.jpeg"
@@ -53,12 +52,11 @@ export function BlogSection() {
                 />
               </BlogBlock>
             </CarouselItem>
-            <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+            <CarouselItem className="basis-full px-6 md:basis-1/2 lg:basis-1/3">
               <BlogBlock
                 title="How Eléonore improved the efficiency of Pennylane’s Care team thanks to Dust"
                 content="Discover how Pennylane leveraged Dust’s specialized virtual assistants to improve efficiency and optimize workflows."
                 href="https://blog.dust.tt/pennylane-dust-customer-support-journey/"
-                className="m-6 lg:m-4 xl:m-6"
               >
                 <img
                   src="https://blog.dust.tt/content/images/size/w2000/2024/04/Ele-onore-MOTTE--1--1.jpg"
@@ -66,12 +64,11 @@ export function BlogSection() {
                 />
               </BlogBlock>
             </CarouselItem>
-            <CarouselItem className="basis-full md:basis-1/2 lg:basis-1/3">
+            <CarouselItem className="basis-full px-6 md:basis-1/2 lg:basis-1/3">
               <BlogBlock
                 title="Integrating AI for Enhanced Workflows at Alan"
                 content="Discover how Alan revolutionizes healthcare and enhances workflows using AI. See how @code-help and Dust streamline developer tasks."
                 href="https://blog.dust.tt/integrating-ai-workflows-alan/"
-                className="m-6 lg:m-4 xl:m-6"
               >
                 <img
                   src="https://blog.dust.tt/content/images/size/w2000/2024/03/cover-vincent.png"
