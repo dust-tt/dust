82c8735faca69ebd9fca618d7a0fe6ce5ee0da0a
Henry Fontanier <h.fontanier@gmail.com>
2024-05-29 15:52:17 +0200

    enh(multi-actions): render non-parallel tool calls properly (#5336)
    * enh(multi-actions): render non-parallel tool calls properly
    
    * remove the if
    
    * panic
    
    ---------
    
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- front/lib/api/assistant/generation.ts
+++ front/lib/api/assistant/generation.ts
@@ -3,6 +3,8 @@ import type {
   AgentMessageType,
   ContentFragmentMessageTypeModel,
   ConversationType,
+  FunctionCallType,
+  FunctionMessageTypeModel,
   GenerationCancelEvent,
   GenerationErrorEvent,
   GenerationSuccessEvent,
@@ -274,39 +276,53 @@ export async function renderConversationForModelMultiActions({
 
   // Render loop.
   // Render all messages and all actions.
-  for (let i = conversation.content.length - 1; i >= 0; i--) {
-    const versions = conversation.content[i];
+  for (const versions of conversation.content) {
     const m = versions[versions.length - 1];
 
     if (isAgentMessageType(m)) {
-      if (m.content) {
-        messages.unshift({
-          role: "assistant" as const,
-          name: m.configuration.name,
-          content: m.content,
+      const actions = removeNulls(m.actions);
+
+      // This array is 2D, because we can have multiple calls per agent message (parallel calls).
+      const steps = [] as Array<
+        Array<{ call: FunctionCallType; result: FunctionMessageTypeModel }>
+      >;
+
+      for (const action of actions) {
+        const stepIndex = action.step;
+        steps[stepIndex] = steps[stepIndex] || [];
+        steps[stepIndex].push({
+          call: action.renderForFunctionCall(),
+          result: action.renderForMultiActionsModel(),
         });
       }
 
-      const actions = removeNulls(m.actions);
-      const function_calls = [];
-      const function_messages = [];
-
-      for (const action of actions) {
-        if (isBaseActionClass(action)) {
-          function_messages.unshift(action.renderForMultiActionsModel());
-          function_calls.unshift(action.renderForFunctionCall());
-        } else {
-          assertNever(action);
+      for (const step of steps) {
+        if (!step?.length) {
+          logger.error(
+            {
+              workspaceId: conversation.owner.sId,
+              conversationId: conversation.sId,
+              agentMessageId: m.sId,
+              panic: true,
+            },
+            "Unexpected state, agent message step with no actions"
+          );
+          continue;
+        }
+        messages.push({
+          role: "assistant",
+          function_calls: step.map((s) => s.call),
+        });
+        for (const { result } of step) {
+          messages.push(result);
         }
       }
 
-      if (function_messages.length > 0) {
-        messages.unshift(...function_messages);
-      }
-      if (function_calls.length > 0) {
-        messages.unshift({
-          role: "assistant",
-          function_calls,
+      if (m.content) {
+        messages.push({
+          role: "assistant" as const,
+          name: m.configuration.name,
+          content: m.content,
         });
       }
     } else if (isUserMessageType(m)) {
@@ -317,7 +333,7 @@ export async function renderConversationForModelMultiActions({
           return `@${name}`;
         }
       );
-      messages.unshift({
+      messages.push({
         role: "user" as const,
         name: m.context.fullName || m.context.username,
         content,
@@ -329,7 +345,7 @@ export async function renderConversationForModelMultiActions({
           conversationId: conversation.sId,
           messageId: m.sId,
         });
-        messages.unshift({
+        messages.push({
           role: "content_fragment",
           name: `inject_${m.contentType}`,
           // The closing </attachment> tag will be added in the merging loop because we might
