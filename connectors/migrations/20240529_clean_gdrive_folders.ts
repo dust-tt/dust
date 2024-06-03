ebf9614a667991e49cb5e7b2f398229af26d05b2
Philippe Rolet <pr@dust.tt>
2024-06-03 09:34:28 +0200

    [Google Drive] Backfill missing rows from GoogleDriveFiles (#5401)
    * [Google Drive] Backfill missing rows from GoogleDriveFiles
    
    Description
    ---
    Fixes issue #5100 (in some cases, root folders could be incorrectly removed from
    google drive files, see details in last comments of issue)
    
    This PR is the last step to fix, following PR #5356 and re-revert of
    PR #5333
    
    Risk
    ---
    None big enough. Maybe unselecting a folder because it's temporarily
    unreachable during migration but the risk is very low and the blast
    too; and maybe backfilling a folder that shouldn't be, but similarly
    no reason why and blast quite low
    
    Can be considered no risk.
    
    Note: about ~500 folders are about to be backfilled as per issue.
    List of folders has been saved
    
    Deploy
    ---
    Run migration from prodbox
    
    * clean

---
+++ connectors/migrations/20240529_clean_gdrive_folders.ts
@@ -0,0 +1,93 @@
+import { QueryTypes } from "sequelize";
+
+import { getGoogleDriveObject } from "@connectors/connectors/google_drive/lib/google_drive_api";
+import {
+  getAuthObject,
+  getDocumentId,
+} from "@connectors/connectors/google_drive/temporal/utils";
+import {
+  GoogleDriveFiles,
+  GoogleDriveFolders,
+} from "@connectors/lib/models/google_drive";
+import logger from "@connectors/logger/logger";
+import { ConnectorResource } from "@connectors/resources/connector_resource";
+import { sequelizeConnection } from "@connectors/resources/storage";
+
+const { LIVE } = process.env;
+
+async function main() {
+  // get all folders that have no row in google_drive_files such that the
+  // folder's folderId is the same as the file's driveFileId
+  // not easy to do in sequelize, so direct query
+  const query = `
+  SELECT "folders".*
+  FROM "google_drive_folders" AS folders
+  LEFT JOIN "google_drive_files" AS files
+  ON folders."folderId" = files."driveFileId"
+  WHERE files."driveFileId" IS NULL
+  ORDER BY folders."updatedAt" DESC;
+`;
+
+  const results = await sequelizeConnection.query(query, {
+    type: QueryTypes.SELECT,
+  });
+
+  // Map the results to GoogleDriveFolders instances
+  const unusedFolders = results.map((result) =>
+    // @ts-expect-error typescript cannot correctly infer result's type
+    GoogleDriveFolders.build(result, { isNewRecord: false })
+  );
+
+  logger.info(`Found ${unusedFolders.length} unused folders`);
+
+  // loop across folders, check if we can get the google drive object from
+  // google API if we can't, delete the folder. Otherwise, backfill the
+  // google_drive_files row
+  for (const folder of unusedFolders) {
+    const { connectorId, folderId } = folder;
+    const connector = await ConnectorResource.fetchById(connectorId);
+
+    if (!connector) {
+      logger.info(
+        { connectorId, folderId },
+        `Connector not found, deleting folder (live: ${LIVE})`
+      );
+      if (LIVE) {
+        await GoogleDriveFolders.destroy({ where: { connectorId, folderId } });
+      }
+      continue;
+    }
+
+    const authCredentials = await getAuthObject(connector.connectionId);
+    const file = await getGoogleDriveObject(authCredentials, folderId);
+
+    if (!file) {
+      logger.info(
+        { connectorId, folderId },
+        `Folder not found on google, deleting folder (live: ${LIVE})`
+      );
+      if (LIVE) {
+        await GoogleDriveFolders.destroy({ where: { connectorId, folderId } });
+      }
+      continue;
+    }
+
+    logger.info(
+      { connectorId, folderId },
+      `Folder found on google, backfilling google_drive_files (live: ${LIVE})`
+    );
+    if (LIVE) {
+      await GoogleDriveFiles.create({
+        connectorId,
+        driveFileId: folderId,
+        name: file.name,
+        mimeType: file.mimeType,
+        dustFileId: getDocumentId(folderId),
+      });
+    }
+  }
+}
+
+main()
+  .then(() => console.log("Done"))
+  .catch(console.error);
