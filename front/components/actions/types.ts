ae9624991a9f7e992f644b30291096c6cf9e94f6
Flavien David <flavien@dust.tt>
2024-06-07 11:52:27 +0200

    Add new UI for dust app run details

--- front/components/actions/types.ts
+++ front/components/actions/types.ts
@@ -1,5 +1,6 @@
 import type { AgentActionType } from "@dust-tt/types";
 
+import { DustAppRunActionDetails } from "@app/components/actions/dust_app_run/DustAppActionDetails";
 import { RetrievalActionDetails } from "@app/components/actions/retrieval/RetrievalAction";
 
 export interface ActionDetailsComponentBaseProps<
@@ -21,7 +22,10 @@ type ActionSpecifications = {
 };
 
 const actionsSpecification: ActionSpecifications = {
-  dust_app_run_action: { runningLabel: "Running App" },
+  dust_app_run_action: {
+    runningLabel: "Running App",
+    detailsComponent: DustAppRunActionDetails,
+  },
   process_action: { runningLabel: "Gathering latest data" },
   retrieval_action: {
     runningLabel: "Searching data",
