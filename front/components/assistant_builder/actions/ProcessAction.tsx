4125eb1f0e892cd751d22a62852a35beeb009d60
Stanislas Polu <spolu@dust.tt>
2024-06-07 12:13:07 +0200

    tweak instructions

--- front/components/assistant_builder/actions/ProcessAction.tsx
+++ front/components/assistant_builder/actions/ProcessAction.tsx
@@ -333,7 +333,7 @@ export function ActionProcess({
     setEdited(true);
     let fullInstructions = `${instructions}`;
     if (description) {
-      fullInstructions += `\n${description}`;
+      fullInstructions += `\n\nAction description:\n${description}`;
     }
     if (instructions !== null) {
       setIsGeneratingSchema(true);
