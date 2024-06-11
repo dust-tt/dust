f650216358f3969a1a5748a47e5c66bed037498d
Aric Lasry <aric@dust.tt>
2024-06-11 16:41:52 +0200

    Dev setup - init script. (#5541)
    * Add script to init dev setup.
    
    * ADding comment.

---
+++ init_dev_container.sh
@@ -0,0 +1,12 @@
+## This is the init script used to initialize the development environment.
+
+## Initializing PostgresSQL databases
+psql "postgres://dev:dev@localhost:5432/" -c "CREATE DATABASE dust_api;";
+psql "postgres://dev:dev@localhost:5432/" -c "CREATE DATABASE dust_databases_store;";
+psql "postgres://dev:dev@localhost:5432/" -c "CREATE DATABASE dust_front;";
+psql "postgres://dev:dev@localhost:5432/" -c "CREATE DATABASE dust_connectors;";
+
+## Initilizing Qdrant collections
+cd core/
+cargo run --bin qdrant_create_collection -- --cluster cluster-0 --provider openai --model text-embedding-3-large-1536
+cd -
