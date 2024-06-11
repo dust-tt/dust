31c3a030900a05dbe713fa08020d38cad534b3a5
Henry Fontanier <henry@dust.tt>
2024-06-11 14:46:34 +0200

    sql

--- front/migrations/db/migration_19.sql
+++ front/migrations/db/migration_19.sql
@@ -1,2 +1,7 @@
 -- Migration created on Jun 11, 2024
-ALTER TABLE "public"."agent_configurations" ADD COLUMN "templateId" INTEGER REFERENCES "templates" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE
+    "public"."agent_configurations"
+ADD
+    COLUMN "templateId" INTEGER REFERENCES "templates" ("id") ON DELETE
+SET
+    NULL ON UPDATE CASCADE;
