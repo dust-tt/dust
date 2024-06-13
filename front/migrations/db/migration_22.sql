8e0f004cc88079065c95dea92ba26900ffdaee78
Flavien David <flavien@dust.tt>
2024-06-13 18:51:57 +0200

    :see_no_evil:

--- front/migrations/db/migration_22.sql
+++ front/migrations/db/migration_22.sql
@@ -1,2 +1,2 @@
 -- Migration created on Jun 13, 2024
-CREATE INDEX "runs_workspace_id_created_at" ON "runs" ("workspaceId", "createdAt");
+CREATE INDEX CONCURRENTLY "runs_workspace_id_created_at" ON "runs" ("workspaceId", "createdAt");
