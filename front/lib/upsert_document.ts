6dbde10ce9466d2c2e55ad08ae62426bc338b55b
filou <philipperolet@gmail.com>
2024-06-18 14:59:41 +0200

    clean

--- front/lib/upsert_document.ts
+++ front/lib/upsert_document.ts
@@ -21,10 +21,8 @@ import { getDocumentsPostUpsertHooksToRun } from "@app/lib/documents_post_proces
 import logger from "@app/logger/logger";
 import { statsDClient } from "@app/logger/withlogging";
 import { launchRunPostUpsertHooksWorkflow } from "@app/temporal/documents_post_process_hooks/client";
-import {
-  launchUpsertDocumentWorkflow,
-  launchUpsertTableWorkflow,
-} from "@app/temporal/upsert_queue/client";
+import type { launchUpsertTableWorkflow } from "@app/temporal/upsert_queue/client";
+import { launchUpsertDocumentWorkflow } from "@app/temporal/upsert_queue/client";
 
 const { DUST_UPSERT_QUEUE_BUCKET, SERVICE_ACCOUNT } = process.env;
 
