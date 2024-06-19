3f646ef2ccf6bf7b2a629d04e9e242b8b5efc07e
Aric Lasry <lasry.aric@gmail.com>
2024-06-19 17:37:00 +0200

    Remove webhook code.

--- connectors/src/connectors/google_drive/temporal/client.ts
+++ connectors/src/connectors/google_drive/temporal/client.ts
@@ -15,8 +15,6 @@ import {
   googleDriveGarbageCollectorWorkflow,
   googleDriveGarbageCollectorWorkflowId,
   googleDriveIncrementalSync,
-  googleDriveRenewWebhooks,
-  googleDriveRenewWebhooksWorkflowId,
 } from "./workflows";
 const logger = mainLogger.child({ provider: "google" });
 
@@ -114,46 +112,6 @@ export async function launchGoogleDriveIncrementalSyncWorkflow(
     logger.error(
       {
         workspaceId: dataSourceConfig.workspaceId,
-        workflowId,
-        error: e,
-      },
-      `Failed starting workflow.`
-    );
-    return new Err(e as Error);
-  }
-}
-
-export async function launchGoogleDriveRenewWebhooksWorkflow(): Promise<
-  Result<string, Error>
-> {
-  const client = await getTemporalClient();
-
-  const workflowId = googleDriveRenewWebhooksWorkflowId();
-  try {
-    const handle = client.workflow.getHandle(workflowId);
-    await handle.terminate();
-  } catch (e) {
-    if (!(e instanceof WorkflowNotFoundError)) {
-      throw e;
-    }
-  }
-  try {
-    await client.workflow.start(googleDriveRenewWebhooks, {
-      args: [],
-      taskQueue: QUEUE_NAME,
-      workflowId: workflowId,
-      cronSchedule: "*/30 * * * *", // every hour, on the hour
-    });
-    logger.info(
-      {
-        workflowId,
-      },
-      `Started workflow.`
-    );
-    return new Ok(workflowId);
-  } catch (e) {
-    logger.error(
-      {
         workflowId,
         error: e,
       },
