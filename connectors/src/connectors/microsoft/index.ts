2232605e571c57978f08148cc8b5dfa5c051941d
Thomas Draier <thomas@dust.tt>
2024-06-20 09:21:44 +0200

    Implement oauth and graph api

--- connectors/src/connectors/microsoft/index.ts
+++ connectors/src/connectors/microsoft/index.ts
@@ -7,18 +7,43 @@ import type {
   Result,
 } from "@dust-tt/types";
 import { Ok } from "@dust-tt/types";
+import { Client } from "@microsoft/microsoft-graph-client";
 
 import type { ConnectorPermissionRetriever } from "@connectors/connectors/interface";
+import { microsoftConfig } from "@connectors/connectors/microsoft/lib/config";
+import {
+  getSites,
+  getTeams,
+} from "@connectors/connectors/microsoft/lib/graph_api";
 import { launchMicrosoftFullSyncWorkflow } from "@connectors/connectors/microsoft/temporal/client";
+import { getAccessTokenFromNango } from "@connectors/lib/nango_helpers";
 import { syncSucceeded } from "@connectors/lib/sync_status";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
 import type { DataSourceConfig } from "@connectors/types/data_source_config";
 
+async function getClient(connectionId: NangoConnectionId) {
+  const nangoConnectionId = connectionId;
+
+  const msAccessToken = await getAccessTokenFromNango({
+    connectionId: nangoConnectionId,
+    integrationId: microsoftConfig.getRequiredNangoMicrosoftConnectorId(),
+    useCache: false,
+  });
+
+  return Client.init({
+    authProvider: (done) => done(null, msAccessToken),
+  });
+}
+
 export async function createMicrosoftConnector(
   dataSourceConfig: DataSourceConfig,
   connectionId: NangoConnectionId
-): Promise<Result<string, Error>> {
-  console.log("createMicrosoftConnector", dataSourceConfig, connectionId);
+) {
+  const client = await getClient(connectionId);
+  await getSites(client);
+  await getTeams(client);
+
+  //   const webhook = await registerWebhook(client, `drives/${drives[0].id}/root`);
 
   const connector = await ConnectorResource.makeNew(
     "microsoft",
