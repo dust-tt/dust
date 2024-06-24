18ddd927ea1325d5249446a2d2c96df27f973bab
Aric Lasry <lasry.aric@gmail.com>
2024-06-24 12:05:58 +0200

    Revert "Revert "Increase connectors Sequelize connection pool (#5808)" (#5811)"
    This reverts commit 3a6f127492cdb6512bede818adfb2c9a0ec1febc.

--- connectors/src/resources/storage/index.ts
+++ connectors/src/resources/storage/index.ts
@@ -1,10 +1,36 @@
+import { isDevelopment } from "@dust-tt/types";
 import { Sequelize } from "sequelize";
 
+import logger from "@connectors/logger/logger";
 import { dbConfig } from "@connectors/resources/storage/config";
 
-export const sequelizeConnection = new Sequelize(
-  dbConfig.getRequiredDatabaseURI(),
-  {
-    logging: false,
-  }
-);
+export const sequelizeConnection = new Sequelize({
+  logging: false,
+});
+
+const acquireAttempts = new WeakMap();
+
+new Sequelize(dbConfig.getRequiredDatabaseURI(), {
+  pool: {
+    // Default is 5.
+    max: isDevelopment() ? 5 : 10,
+  },
+  logging: false,
+  hooks: {
+    beforePoolAcquire: (options) => {
+      acquireAttempts.set(options, Date.now());
+    },
+    afterPoolAcquire: (connection, options) => {
+      const elapsedTime = Date.now() - acquireAttempts.get(options);
+      if (elapsedTime > 100) {
+        logger.info(
+          {
+            elapsedTime,
+            callStack: new Error().stack,
+          },
+          "Long sequelize connection acquisition detected"
+        );
+      }
+    },
+  },
+});
