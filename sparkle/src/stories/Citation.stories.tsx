5dd8f448c32dcf89ad4b7f0114539dbb35d9ee64
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-06-25 13:15:58 +0200

    Sparkle citation img (#5372)
    * Adding images to citation
    
    * Polishing style
    
    * State
    
    * Mor polish
    
    * Bump Sparkle version
    
    ---------
    
    Co-authored-by: Flavien David <flavien@dust.tt>

--- sparkle/src/stories/Citation.stories.tsx
+++ sparkle/src/stories/Citation.stories.tsx
@@ -24,13 +24,29 @@ export const CitationsExample = () => (
         isBlinking={true}
       />
       <Citation
-        title="With avatarUrl"
+        title="With imgSrc"
         size="xs"
         type="document"
         onClose={() => alert("Close action clicked")}
         isBlinking={true}
-        avatarUrl="https://cdn.discordapp.com/attachments/995248824375316560/1143857310142316685/duncid_friendly_Scandinavian_droid_designed_by_Wes_Anderson_and_29eec588-b898-4e4a-9776-10c27790cbf9.png"
+        avatarSrc="https://dust.tt/static/droidavatar/Droid_Black_4.jpg"
       />
+      <Citation
+        title="With imgSrc"
+        size="xs"
+        type="image"
+        onClose={() => alert("Close action clicked")}
+        isBlinking={true}
+        imgSrc="https://dust.tt/static/droidavatar/Droid_Black_4.jpg"
+      />
+      <div>
+        <Citation
+          title=""
+          size="xs"
+          type="image"
+          imgSrc="https://dust.tt/static/droidavatar/Droid_Black_4.jpg"
+        />
+      </div>
     </div>
     <h3>In Grid</h3>
     <div className="s-w-full">
@@ -131,12 +147,12 @@ export const CitationsExample = () => (
       />
 
       <Citation
-        title="With avatarUrl"
+        title="With imgSrc"
         type="document"
         onClose={() => alert("Close action clicked")}
         description="Write a 120 character description of the citation here to be displayed in the citation list."
         isBlinking={true}
-        avatarUrl="https://cdn.discordapp.com/attachments/995248824375316560/1143857310142316685/duncid_friendly_Scandinavian_droid_designed_by_Wes_Anderson_and_29eec588-b898-4e4a-9776-10c27790cbf9.png"
+        imgSrc="https://cdn.discordapp.com/attachments/995248824375316560/1143857310142316685/duncid_friendly_Scandinavian_droid_designed_by_Wes_Anderson_and_29eec588-b898-4e4a-9776-10c27790cbf9.png"
       />
     </div>
     <div className="s-flex s-gap-2">
