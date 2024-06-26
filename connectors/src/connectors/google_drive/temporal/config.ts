7b5720f6d444c28f04b8101ee94232247700ebee
Aric Lasry <aric@dust.tt>
2024-06-26 10:56:38 +0200

    Rename Gdrive full sync queue. (#5874)

--- connectors/src/connectors/google_drive/temporal/config.ts
+++ connectors/src/connectors/google_drive/temporal/config.ts
@@ -1,3 +1,3 @@
 export const WORKFLOW_VERSION = 5;
-export const GDRIVE_FULL_SYNC_QUEUE_NAME = `google-queue-v${WORKFLOW_VERSION}`;
+export const GDRIVE_FULL_SYNC_QUEUE_NAME = `google-queue-fullsync-v${WORKFLOW_VERSION}`;
 export const GDRIVE_INCREMENTAL_SYNC_QUEUE_NAME = `google-queue-incremental-v${WORKFLOW_VERSION}`;
