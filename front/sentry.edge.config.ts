ce9ce3b21153e725a19c7d25b5b09467e4bac517
Flavien David <flavien.david74@gmail.com>
2024-06-28 08:05:09 +0200

    Revert "Revert "Flav/add sentry in front (#5918)" (#5920)"
    This reverts commit 41785aeb6beb6c3aa867ac6c2733a1efb941e717.

---
+++ front/sentry.edge.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: "https://9d71076535e796b7601c88ed49f23374@o4507503820079105.ingest.de.sentry.io/4507503822635088",
+
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
