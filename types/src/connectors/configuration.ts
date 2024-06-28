b0ce1f103751f74e61dfdd7e4f8727f2858bfaa7
Stanislas Polu <spolu@dust.tt>
2024-06-28 13:39:50 +0200

    comment

--- types/src/connectors/configuration.ts
+++ types/src/connectors/configuration.ts
@@ -5,6 +5,8 @@ export type ConnectorConfiguration = WebCrawlerConfigurationType | null;
 export type ConnectorConfigurations = {
   webcrawler: WebCrawlerConfigurationType;
   notion: null;
+  // Slack technically has a configuration as per file `src/connectors/slack.ts` but we don't set it
+  // here because we don't expect a configuration at connector creation time for now.
   slack: null;
   google_drive: null;
   github: null;
