c4444887afdd2a3ced25c93a6d50297f5143c961
Alban Dumouilla <alban@dust.tt>
2024-07-03 11:30:32 +0200

    lint

--- front/lib/development.ts
+++ front/lib/development.ts
@@ -5,7 +5,9 @@ import { EnvironmentConfig } from "@dust-tt/types";
 export function isDevelopmentOrDustWorkspace(owner: WorkspaceType) {
   return (
     isDevelopment() ||
-    owner.sId === EnvironmentConfig.getEnvVariable("PRODUCTION_DUST_WORKSPACE_ID") ||
-    owner.sId === EnvironmentConfig.getEnvVariable("PRODUCTION_DUST_APPS_WORKSPACE_ID")
+    owner.sId ===
+      EnvironmentConfig.getEnvVariable("PRODUCTION_DUST_WORKSPACE_ID") ||
+    owner.sId ===
+      EnvironmentConfig.getEnvVariable("PRODUCTION_DUST_APPS_WORKSPACE_ID")
   );
 }
