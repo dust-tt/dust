01d50ad46eb7e608e7e80a855cb2783c1dde6343
Flavien David <flavien@dust.tt>
2024-07-04 09:57:00 +0200

    Add resource public ids

---
+++ front/lib/resources/public_ids.ts
@@ -0,0 +1,86 @@
+import type { ModelId, Result } from "@dust-tt/types";
+import { Err, Ok } from "@dust-tt/types";
+import Sqids from "sqids";
+
+import logger from "@app/logger/logger";
+
+const RESOURCE_PUBLIC_ID_MIN_LENGTH = 10;
+
+const sqids = new Sqids({
+  minLength: 10,
+});
+
+// Start with a shard key to avoid collisions with other IDs.
+const SHARD_KEY = 1;
+
+const RESOURCES_PREFIX = {
+  file: "fil",
+};
+
+const ALL_RESOURCES_PREFIXES = Object.values(RESOURCES_PREFIX);
+
+type ResourceNameType = keyof typeof RESOURCES_PREFIX;
+
+export function makePublicId(
+  resourceName: ResourceNameType,
+  {
+    id,
+    workspaceId,
+  }: {
+    id: ModelId;
+    workspaceId: ModelId;
+  }
+): string {
+  const idsToEncode = [
+    // First encode shard key.
+    SHARD_KEY,
+    // Then encode the workspace Id.
+    workspaceId,
+    // Finally encode the resource Id.
+    id,
+  ];
+
+  return `${RESOURCES_PREFIX[resourceName]}_${sqids.encode(idsToEncode)}`;
+}
+
+function getIdsFromPublicId(
+  publicId: string
+): Result<[number, ModelId, ModelId], Error> {
+  const [resourcePrefix, publicIdWithoutPrefix] = publicId.split("_");
+
+  if (!ALL_RESOURCES_PREFIXES.includes(resourcePrefix)) {
+    return new Err(new Error("Invalid resource prefix in public Id"));
+  }
+
+  if (publicIdWithoutPrefix.length < RESOURCE_PUBLIC_ID_MIN_LENGTH) {
+    return new Err(new Error("Invalid public Id length"));
+  }
+
+  try {
+    const ids = sqids.decode(publicId);
+
+    if (ids.length !== 3) {
+      return new Err(new Error("Invalid decoded public Id length"));
+    }
+
+    return new Ok([ids[1], ids[2], ids[3]]);
+  } catch (error) {
+    return new Err(
+      error instanceof Error ? error : new Error("Failed to decode public Id")
+    );
+  }
+}
+
+export function getResourceIdFromPublicId(publicId: string): ModelId | null {
+  const publicIdsRes = getIdsFromPublicId(publicId);
+  if (publicIdsRes.isErr()) {
+    logger.error(
+      { publicId, error: publicIdsRes.error },
+      "Failed to get IDs from public Id"
+    );
+
+    return null;
+  }
+
+  return publicIdsRes.value[2];
+}
