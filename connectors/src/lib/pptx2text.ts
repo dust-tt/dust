80ffd40ed92d1f627f02481248941eac9b606b14
Aric Lasry <lasry.aric@gmail.com>
2024-07-04 16:19:23 +0200

    Adding tracing around pptx2text

--- connectors/src/lib/pptx2text.ts
+++ connectors/src/lib/pptx2text.ts
@@ -1,3 +1,4 @@
+import tracer from "dd-trace";
 import JSZip from "jszip";
 import turndown from "turndown";
 
@@ -7,51 +8,65 @@ type PPTXDocument = {
   }[];
 };
 
-export async function PPTX2Text(fileBuffer: Buffer): Promise<PPTXDocument> {
-  const zip = new JSZip();
-  await zip.loadAsync(fileBuffer);
+export async function PPTX2Text(
+  fileBuffer: Buffer,
+  filename?: string
+): Promise<PPTXDocument> {
+  return tracer.trace(
+    `PPTX2Text`,
+    {
+      resource: `PPTX2Text`,
+    },
+    async (span) => {
+      span?.setTag("fileBufferLength", fileBuffer.length);
+      span?.setTag("filename", filename);
 
-  const document: PPTXDocument = {
-    pages: [],
-  };
+      const zip = new JSZip();
+      await zip.loadAsync(fileBuffer);
 
-  let slideIndex = 1;
-  let slideFile: JSZip.JSZipObject | null = null;
+      const document: PPTXDocument = {
+        pages: [],
+      };
 
-  while ((slideFile = zip.file(`ppt/slides/slide${slideIndex}.xml`))) {
-    slideIndex++;
+      let slideIndex = 1;
+      let slideFile: JSZip.JSZipObject | null = null;
 
-    if (!slideFile) {
-      break;
+      while ((slideFile = zip.file(`ppt/slides/slide${slideIndex}.xml`))) {
+        slideIndex++;
+
+        if (!slideFile) {
+          break;
+        }
+
+        const slideXmlStr = await slideFile.async("text");
+        let text: string | null = null;
+        try {
+          text = new turndown()
+            // Add a space character between each paragraph.
+            .addRule("paragraph_spacing", {
+              filter: (node) => {
+                if (node.nodeName === "A:T") {
+                  return true;
+                }
+                return false;
+              },
+              replacement: function (content) {
+                return content + " ";
+              },
+            })
+            .turndown(slideXmlStr);
+        } catch (e) {
+          console.error(`Failed to extract text from slide ${slideIndex}`);
+        }
+
+        if (text) {
+          document.pages.push({
+            content: text,
+          });
+        }
+      }
+
+      return document;
     }
-
-    const slideXmlStr = await slideFile.async("text");
-    let text: string | null = null;
-    try {
-      text = new turndown()
-        // Add a space character between each paragraph.
-        .addRule("paragraph_spacing", {
-          filter: (node) => {
-            if (node.nodeName === "A:T") {
-              return true;
-            }
-            return false;
-          },
-          replacement: function (content) {
-            return content + " ";
-          },
-        })
-        .turndown(slideXmlStr);
-    } catch (e) {
-      console.error(`Failed to extract text from slide ${slideIndex}`);
-    }
-
-    if (text) {
-      document.pages.push({
-        content: text,
-      });
-    }
-  }
-
-  return document;
+  );
 }
