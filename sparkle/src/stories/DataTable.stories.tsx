c8f05ba096294e8bb6a975e9a7c27a466011bcdb
Thomas Draier <thomas@dust.tt>
2024-07-04 09:40:12 +0200

    Clean index

--- sparkle/src/stories/DataTable.stories.tsx
+++ sparkle/src/stories/DataTable.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta } from "@storybook/react";
 import React from "react";
 
-import { Button, DataTable } from "../_index";
+import { Button, DataTable } from "../index";
 
 const meta = {
   title: "Components/DataTable",
