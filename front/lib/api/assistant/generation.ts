8ef42a2d6d1e48b47cfb87d118725ff13b85e94a
Henry Fontanier <henry@dust.tt>
2024-07-04 14:52:31 +0200

    simplify logic

--- front/lib/api/assistant/generation.ts
+++ front/lib/api/assistant/generation.ts
@@ -143,22 +143,18 @@ export async function renderConversationForModelMultiActions({
             continue;
           }
 
-          if (step.actions.length) {
-            messages.push({
-              role: "assistant",
-              function_calls: step.actions.map((s) => s.call),
-              content: step.contents.join("\n"),
-            } satisfies AssistantFunctionCallMessageTypeModel);
+          messages.push({
+            role: "assistant",
+            function_calls: step.actions.length
+              ? step.actions.map((s) => s.call)
+              : undefined,
+            content: step.contents.join("\n"),
+          } as typeof step.actions.length extends 0
+            ? AssistantContentMessageTypeModel
+            : AssistantFunctionCallMessageTypeModel);
 
-            for (const { result } of step.actions) {
-              messages.push(result);
-            }
-          } else if (step.contents.length) {
-            messages.push({
-              role: "assistant",
-              name: m.configuration.name,
-              content: step.contents.join("\n"),
-            } satisfies AssistantContentMessageTypeModel);
+          for (const { result } of step.actions) {
+            messages.push(result);
           }
         }
       }
