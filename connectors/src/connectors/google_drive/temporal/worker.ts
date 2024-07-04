dfd6ae0751b49a33389242d6ab451bd2ca6bcb3e
Henry Fontanier <h.fontanier@gmail.com>
2024-07-04 14:57:27 +0200

    fix: set max cached workflows to the default value for gdrive workers (#6063)
    * fix: set max cached workflows to the right value for gdrive workers
    
    * remove the setting
    
    ---------
    
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- connectors/src/connectors/google_drive/temporal/worker.ts
+++ connectors/src/connectors/google_drive/temporal/worker.ts
@@ -4,10 +4,7 @@ import { Worker } from "@temporalio/worker";
 import * as activities from "@connectors/connectors/google_drive/temporal/activities";
 import { GoogleDriveCastKnownErrorsInterceptor } from "@connectors/connectors/google_drive/temporal/cast_known_errors";
 import * as sync_status from "@connectors/lib/sync_status";
-import {
-  getTemporalWorkerConnection,
-  TEMPORAL_MAXED_CACHED_WORKFLOWS,
-} from "@connectors/lib/temporal";
+import { getTemporalWorkerConnection } from "@connectors/lib/temporal";
 import { ActivityInboundLogInterceptor } from "@connectors/lib/temporal_monitoring";
 import logger from "@connectors/logger/logger";
 
@@ -24,7 +21,6 @@ export async function runGoogleWorkers() {
     taskQueue: GDRIVE_FULL_SYNC_QUEUE_NAME,
     maxConcurrentActivityTaskExecutions: 10,
     connection,
-    maxCachedWorkflows: TEMPORAL_MAXED_CACHED_WORKFLOWS,
     reuseV8Context: true,
     namespace,
     interceptors: {
@@ -43,7 +39,6 @@ export async function runGoogleWorkers() {
     taskQueue: GDRIVE_INCREMENTAL_SYNC_QUEUE_NAME,
     maxConcurrentActivityTaskExecutions: 15,
     connection,
-    maxCachedWorkflows: TEMPORAL_MAXED_CACHED_WORKFLOWS,
     reuseV8Context: true,
     namespace,
     interceptors: {
