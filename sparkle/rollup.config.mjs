c984e55ab254804642dbbbb167ea86ea04de79a8
Thomas Draier <thomas@dust.tt>
2024-07-05 13:33:37 +0200

    Refactor packaging, compile tsc & tailwind

--- sparkle/rollup.config.mjs
+++ sparkle/rollup.config.mjs
@@ -1,7 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
 import autoprefixer from "autoprefixer";
 import fs from "fs";
 import path from "path";
@@ -10,23 +9,13 @@ import postcss from "rollup-plugin-postcss";
 import tailwindcss from "tailwindcss";
 import { fileURLToPath } from "url";
 
-let tsPluginOptions = {
-  tsconfig: "./tsconfig.json",
-  outputToFilesystem: true,
-};
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const pkg = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")
 );
 
-let entryFile = "src/index.ts";
-tsPluginOptions.exclude = "src/index_with_tw_base.ts";
-if (process.env.INCLUDE_TW_BASE) {
-  entryFile = "src/index_with_tw_base.ts";
-  tsPluginOptions.exclude = "src/index.ts";
-}
+let entryFile = "dist/esm/index.js";
 
 const config = {
   input: entryFile,
@@ -37,17 +26,11 @@ const config = {
       sourcemap: true,
       name: "react-lib",
     },
-    {
-      file: pkg.module,
-      format: "esm",
-      sourcemap: true,
-    },
   ],
   plugins: [
     external(),
     resolve(),
     commonjs(),
-    typescript(tsPluginOptions),
     postcss({
       plugins: [
         tailwindcss({
