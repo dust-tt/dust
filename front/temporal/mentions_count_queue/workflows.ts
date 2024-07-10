e02b55d56450595fdcd1fc09db68e5446cde93d2
filou <philipperolet@gmail.com>
2024-07-10 14:03:29 +0200

    clean

--- front/temporal/mentions_count_queue/workflows.ts
+++ front/temporal/mentions_count_queue/workflows.ts
@@ -2,12 +2,10 @@ import { proxyActivities } from "@temporalio/workflow";
 
 import type * as activities from "@app/temporal/mentions_count_queue/activities";
 
-const { mentionsCountActivity: mentionCountActivity } = proxyActivities<
-  typeof activities
->({
+const { mentionsCountActivity } = proxyActivities<typeof activities>({
   startToCloseTimeout: "2 minutes",
 });
 
 export async function mentionsCountWorkflow(workspaceId: string) {
-  await mentionCountActivity(workspaceId);
+  await mentionsCountActivity(workspaceId);
 }
