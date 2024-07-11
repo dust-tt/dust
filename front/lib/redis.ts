4827fca8c809f722a6b83d2674d47a2d90b9f19c
Flavien David <flavien.david74@gmail.com>
2024-07-11 18:37:11 +0200

    End message stream once message generation is done. (#6181)

--- front/lib/redis.ts
+++ front/lib/redis.ts
@@ -1,11 +1,12 @@
+import type { RedisClientType } from "redis";
 import { createClient } from "redis";
 
-export async function redisClient() {
+export async function redisClient(): Promise<RedisClientType> {
   const { REDIS_URI } = process.env;
   if (!REDIS_URI) {
     throw new Error("REDIS_URI is not defined");
   }
-  const client = createClient({
+  const client: RedisClientType = createClient({
     url: REDIS_URI,
   });
   client.on("error", (err) => console.log("Redis Client Error", err));
@@ -16,7 +17,7 @@ export async function redisClient() {
 }
 
 export async function safeRedisClient<T>(
-  fn: (client: Awaited<ReturnType<typeof redisClient>>) => PromiseLike<T>
+  fn: (client: RedisClientType) => PromiseLike<T>
 ): Promise<T> {
   const client = await redisClient();
   try {
