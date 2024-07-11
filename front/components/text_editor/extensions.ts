22d634d808265f1848c5d89404c1b22a90112332
Flavien David <flavien@dust.tt>
2024-07-11 22:22:33 +0200

    Better handle paragraph in inputBar

--- front/components/text_editor/extensions.ts
+++ front/components/text_editor/extensions.ts
@@ -3,7 +3,8 @@ import Paragraph from "@tiptap/extension-paragraph";
 export const ParagraphExtension = Paragraph.extend({
   addKeyboardShortcuts() {
     return {
-      "Shift-Enter": () => this.editor.commands.enter(),
+      "Shift-Enter": () => this.editor.commands.splitBlock(),
+      Enter: () => false,
     };
   },
 });
