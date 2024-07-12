670a50c49e4f993650e2c314d5ce1ce9e2bf5cce
Alban Dumouilla <alban@dust.tt>
2024-07-12 09:02:43 +0200

    Add documentation-ack to danger check for public api changes

--- front/dangerfile.ts
+++ front/dangerfile.ts
@@ -1,12 +1,15 @@
 import { danger, fail, warn } from "danger";
 
+const migrationAckLabel = "migration-ack";
+const documentationAckLabel = "documentation-ack";
+
 function failMigrationAck() {
   fail(
     "Files in `**/models/` have been modified. " +
       `Addition and deletion should be in 2 separate PRs:\n` +
       ` 1. Addition: migrate and deploy\n` +
       ` 2. Deletion: deploy and migrate\n\n` +
-      `Please add the \`migration-ack\` label to acknowledge ` +
+      `Please add the \`${migrationAckLabel}\` label to acknowledge ` +
       `that a migration will be needed once merged into 'main'.`
   );
 }
@@ -15,12 +18,41 @@ function warnMigrationAck(migrationAckLabel: string) {
   warn(
     "Files in `**/lib/models/` have been modified and the PR has the `" +
       migrationAckLabel +
-      "` label. Don't forget to run the migration from the `-edge` infrastructure."
+      "` label. Don't forget to run the migration from prodbox."
+  );
+}
+
+function checkDocumentationLabel() {
+  const hasDocumentationAckLabel = danger.github.issue.labels.some(
+    (label) => label.name === documentationAckLabel
+  );
+
+  if (!hasDocumentationAckLabel) {
+    failDocumentationAck();
+  } else {
+    warnDocumentationAck(documentationAckLabel);
+  }
+}
+
+function failDocumentationAck() {
+  fail(
+    "Files in `**/api/v1/` have been modified. " +
+      `Please add the \`${documentationAckLabel}\` label to acknowlegde that if anything changes 
+      in a public endpoint, you need to edit the JSDoc comment 
+      above the handler definition and/or the swagger_schemas.ts file and regenerate the documentation using \`npm run docs\``
+  );
+}
+
+function warnDocumentationAck(documentationAckLabel: string) {
+  warn(
+    "Files in `**/api/v1/` have been modified and the PR has the `" +
+      documentationAckLabel +
+      "` label. \n" +
+      "Don't forget to run `npm run docs` and use the `Deploy OpenAPI Docs` Github action to update https://docs.dust.tt/reference."
   );
 }
 
 function checkMigrationLabel() {
-  const migrationAckLabel = "migration-ack";
   const hasMigrationAckLabel = danger.github.issue.labels.some(
     (label) => label.name === migrationAckLabel
   );
@@ -46,7 +78,7 @@ function checkDeployPlanSection() {
   }
 }
 
-function checkModifiedModelFiles() {
+function checkModifiedFiles() {
   const modifiedModelFiles = danger.git.modified_files.filter((path) => {
     return (
       path.startsWith("front/lib/models/") ||
@@ -60,6 +92,14 @@ function checkModifiedModelFiles() {
     checkMigrationLabel();
     checkDeployPlanSection();
   }
+
+  const modifiedPublicApiFiles = danger.git.modified_files.filter((path) => {
+    return path.startsWith("front/pages/api/v1/");
+  });
+
+  if (modifiedPublicApiFiles.length > 0) {
+    checkDocumentationLabel();
+  }
 }
 
-checkModifiedModelFiles();
+checkModifiedFiles();
