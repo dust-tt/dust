222f8cf2e4ab05977f11ecb447e1bac6784d9da5
Henry Fontanier <henry@dust.tt>
2024-07-15 12:37:39 +0200

    tokenize batch endpoint

--- core/bin/dust_api.rs
+++ core/bin/dust_api.rs
@@ -2442,6 +2442,56 @@ async fn tokenize(Json(payload): Json<TokenizePayload>) -> (StatusCode, Json<API
     }
 }
 
+#[derive(serde::Deserialize)]
+struct TokenizeBatchPayload {
+    texts: Vec<String>,
+    provider_id: ProviderID,
+    model_id: String,
+    credentials: Option<run::Credentials>,
+}
+
+async fn tokenize_batch(
+    Json(payload): Json<TokenizeBatchPayload>,
+) -> (StatusCode, Json<APIResponse>) {
+    let mut llm = provider(payload.provider_id).llm(payload.model_id);
+
+    // If we received credentials we initialize the llm with them.
+    match payload.credentials {
+        Some(c) => {
+            match llm.initialize(c.clone()).await {
+                Err(e) => {
+                    return error_response(
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        "internal_server_error",
+                        "Failed to initialize LLM",
+                        Some(e),
+                    );
+                }
+                Ok(()) => (),
+            };
+        }
+        None => (),
+    }
+
+    match llm.tokenize(payload.texts).await {
+        Err(e) => error_response(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "internal_server_error",
+            "Failed to tokenize text",
+            Some(e),
+        ),
+        Ok(res) => (
+            StatusCode::OK,
+            Json(APIResponse {
+                error: None,
+                response: Some(json!({
+                    "tokens": res,
+                })),
+            }),
+        ),
+    }
+}
+
 fn main() {
     JSExecutor::init();
 
@@ -2625,6 +2675,7 @@ fn main() {
         .route("/sqlite_workers", delete(sqlite_workers_delete))
         // Misc
         .route("/tokenize", post(tokenize))
+        .route("/tokenize/batch", post(tokenize_batch))
 
         // Extensions
         .layer(DefaultBodyLimit::disable())
