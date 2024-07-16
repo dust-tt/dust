7d0cc1f26c0db6a7cb2d2974f597146b79530521
Daphné Popin <PopDaph@users.noreply.github.com>
2024-07-16 11:49:51 +0200

    Sparkle: Hoverable primary (#6236)

--- sparkle/src/components/Hoverable.tsx
+++ sparkle/src/components/Hoverable.tsx
@@ -7,19 +7,25 @@ interface HoverableProps {
   children: ReactNode;
   className?: string;
   onClick: (e: SyntheticEvent) => void;
+  variant?: "primary" | "invisible";
 }
 
 export function Hoverable({
   children,
   className = "",
   onClick,
+  variant = "invisible",
 }: HoverableProps) {
+  const baseClasses = "s-cursor-pointer s-duration-300";
+
+  const variantClasses = {
+    invisible: "hover:s-text-action-500 active:s-text-action-600",
+    primary: "s-font-bold s-text-blue-500 hover:active:s-text-action-600",
+  };
+
   return (
     <span
-      className={classNames(
-        "s-cursor-pointer s-duration-300 hover:s-text-action-500 active:s-text-action-600",
-        className
-      )}
+      className={classNames(baseClasses, variantClasses[variant], className)}
       onClick={onClick}
     >
       {children}
