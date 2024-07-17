5faee928c85ea46508c881a23f6ee3129bbd50f8
Henry Fontanier <henry@dust.tt>
2024-07-17 18:51:35 +0200

    enh: clients authenticate with oauth API

--- connectors/src/lib/api/config.ts
+++ connectors/src/lib/api/config.ts
@@ -1,9 +1,10 @@
 import { EnvironmentConfig } from "@dust-tt/types";
 
 export const apiConfig = {
-  getOAuthAPIConfig: (): { url: string } => {
+  getOAuthAPIConfig: (): { url: string; apiKey: string | null } => {
     return {
       url: EnvironmentConfig.getEnvVariable("OAUTH_API"),
+      apiKey: EnvironmentConfig.getOptionalEnvVariable("OAUTH_API_KEY") ?? null,
     };
   },
   getDustAPIConfig: (): { url: string; nodeEnv: string } => {
