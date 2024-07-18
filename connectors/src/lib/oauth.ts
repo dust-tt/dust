85a36e55e86d70bab7b72a80659a94cc168ed6e9
Stanislas Polu <spolu@dust.tt>
2024-07-18 17:55:59 +0200

    nit

--- connectors/src/lib/oauth.ts
+++ connectors/src/lib/oauth.ts
@@ -1,5 +1,5 @@
 // This function is used to discreminate between a new OAuth connection and an old Nango/Github
-// connection. It is ussed to support dual-use while migrating and should be unused by a connector
+// connection. It is used to support dual-use while migrating and should be unused by a connector
 // once fully migrated
 export function isDualUseOAuthConnectionId(connectionId: string): boolean {
   // TODO(spolu): make sure this function is removed once fully migrated.
