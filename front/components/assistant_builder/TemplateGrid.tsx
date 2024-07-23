87fb8b29c1d92e54cda4eb641635cab7e6c8c24c
Daphné Popin <PopDaph@users.noreply.github.com>
2024-07-23 14:19:55 +0200

    Fix navigation issues on /builder/assistants/create modal. (#6349)
    * Fix useNavigation lock blocked on modal
    
    * Fix navigation of templates in modal
    
    * Fix opening modal push templateId in url
    
    * Apply feedback

--- front/components/assistant_builder/TemplateGrid.tsx
+++ front/components/assistant_builder/TemplateGrid.tsx
@@ -1,39 +1,31 @@
 import { AssistantPreview } from "@dust-tt/sparkle";
-import { useRouter } from "next/router";
 
 import type { AssistantTemplateListType } from "@app/pages/api/w/[wId]/assistant/builder/templates";
 
 interface TemplateGridProps {
   templates: AssistantTemplateListType[];
+  openTemplateModal: (templateId: string) => void;
 }
 
-export function TemplateGrid({ templates }: TemplateGridProps) {
-  const router = useRouter();
-
-  const makeTemplateModalHref = (templateId: string) => {
-    return {
-      pathname: router.pathname,
-      query: {
-        ...router.query,
-        templateId,
-      },
-    };
-  };
-
-  const items = templates.map((t) => (
-    <AssistantPreview
-      key={t.sId}
-      title={t.handle}
-      pictureUrl={t.pictureUrl}
-      description={t.description ?? ""}
-      variant="list"
-      onClick={() => router.push(makeTemplateModalHref(t.sId))}
-    />
-  ));
-
-  if (items.length === 0) {
+export function TemplateGrid({
+  templates,
+  openTemplateModal,
+}: TemplateGridProps) {
+  if (!templates.length) {
     return null;
   }
-
-  return <div className="grid grid-cols-2 gap-2">{items}</div>;
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {templates.map((t) => (
+        <AssistantPreview
+          key={t.sId}
+          title={t.handle}
+          pictureUrl={t.pictureUrl}
+          description={t.description ?? ""}
+          variant="list"
+          onClick={() => openTemplateModal(t.sId)}
+        />
+      ))}
+    </div>
+  );
 }
