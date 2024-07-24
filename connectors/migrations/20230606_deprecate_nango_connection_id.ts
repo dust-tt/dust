d6468c6ae40c8ee798904227043ca4fb22f8710d
Stanislas Polu <spolu@dust.tt>
2024-07-25 01:20:00 +0200

    comment deprecated migration

--- connectors/migrations/20230606_deprecate_nango_connection_id.ts
+++ connectors/migrations/20230606_deprecate_nango_connection_id.ts
@@ -1,30 +1,30 @@
-import { Op } from "sequelize";
-
-import { sequelizeConnection } from "@connectors/resources/storage";
-import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
-
-async function main() {
-  await ConnectorModel.update(
-    {
-      connectionId: sequelizeConnection.col("nangoConnectionId"),
-    },
-    {
-      // @ts-expect-error `connectionId` has been made non-nullable
-      where: {
-        connectionId: {
-          [Op.eq]: null,
-        },
-      },
-    }
-  );
-}
-
-main()
-  .then(() => {
-    console.log("Done");
-    process.exit(0);
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+// import { Op } from "sequelize";
+// 
+// import { sequelizeConnection } from "@connectors/resources/storage";
+// import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
+// 
+// async function main() {
+//   await ConnectorModel.update(
+//     {
+//       connectionId: sequelizeConnection.col("nangoConnectionId"),
+//     },
+//     {
+//       // @ts-expect-error `connectionId` has been made non-nullable
+//       where: {
+//         connectionId: {
+//           [Op.eq]: null,
+//         },
+//       },
+//     }
+//   );
+// }
+// 
+// main()
+//   .then(() => {
+//     console.log("Done");
+//     process.exit(0);
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   });
