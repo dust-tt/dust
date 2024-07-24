92e5a16b2c515341baaafb4124b863a9c44d8e83
Stanislas Polu <spolu@dust.tt>
2024-07-25 01:03:50 +0200

    core: fix version endpoints SQL query (#6497)

--- core/src/stores/postgres.rs
+++ core/src/stores/postgres.rs
@@ -1525,7 +1525,7 @@ impl Store for PostgresStore {
         params.push(&data_source_row_id);
         where_clauses.push("document_id = $2".to_string());
         params.push(&document_id);
-        where_clauses.push("created <= $3'".to_string());
+        where_clauses.push("created <= $3".to_string());
         params.push(&latest_hash_created);
 
         let (filter_clauses, filter_params, p_idx) =
