130f75e112e92522289e8a4279d4faa6a7afb06d
Flavien David <flavien@dust.tt>
2024-07-25 19:04:57 +0200

    Add CSP on viz

--- viz/next.config.mjs
+++ viz/next.config.mjs
@@ -11,6 +11,12 @@ const nextConfig = {
             key: "Access-Control-Allow-Origin",
             value: isDev ? "http://localhost:3000" : "https://dust.tt",
           },
+          {
+            key: "Content-Security-Policy",
+            value: isDev
+              ? "frame-ancestors 'self' http://localhost:3000;"
+              : "frame-ancestors 'self' https://dust.tt;",
+          },
         ],
       },
       // Allow CORS for static files.
