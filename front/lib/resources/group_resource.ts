81c12a29698aff752a534325a760f148b7a769e3
Daphné Popin <PopDaph@users.noreply.github.com>
2024-07-26 16:11:19 +0200

    Groups auth (#6536)
    * Groups auth
    
    * Apply feedback

--- front/lib/resources/group_resource.ts
+++ front/lib/resources/group_resource.ts
@@ -1,4 +1,10 @@
-import type { LightWorkspaceType, ModelId, Result } from "@dust-tt/types";
+import type {
+  GroupType,
+  LightWorkspaceType,
+  ModelId,
+  Result,
+  UserType,
+} from "@dust-tt/types";
 import { Err, Ok } from "@dust-tt/types";
 import type {
   Attributes,
@@ -10,6 +16,7 @@ import { Op } from "sequelize";
 
 import type { Authenticator } from "@app/lib/auth";
 import { BaseResource } from "@app/lib/resources/base_resource";
+import type { KeyResource } from "@app/lib/resources/key_resource";
 import { MembershipResource } from "@app/lib/resources/membership_resource";
 import { GroupMembershipModel } from "@app/lib/resources/storage/models/group_memberships";
 import { GroupModel } from "@app/lib/resources/storage/models/groups";
@@ -116,53 +123,49 @@ export class GroupResource extends BaseResource<GroupModel> {
     return new this(this.model, blob.get());
   }
 
-  static async fetchWorkspaceGroups(
-    auth: Authenticator,
-    transaction?: Transaction
+  static async superAdminFetchWorkspaceGroups(
+    userResource: UserResource,
+    workspaceId: ModelId
   ): Promise<GroupResource[]> {
-    const owner = auth.getNonNullableWorkspace();
+    if (!userResource.isDustSuperUser) {
+      throw new Error("User is not a super admin.");
+    }
 
     const groups = await this.model.findAll({
       where: {
-        workspaceId: owner.id,
+        workspaceId,
       },
-      transaction,
     });
 
     return groups.map((group) => new this(GroupModel, group.get()));
   }
 
-  static async fetchWorkspaceSystemGroup(
-    auth: Authenticator,
-    transaction?: Transaction
-  ): Promise<GroupResource> {
-    const owner = auth.getNonNullableWorkspace();
+  static async fetchWorkspaceGroupsFromKey(
+    key: KeyResource
+  ): Promise<GroupResource[]> {
+    // TODO(GROUPS_INFRA): we need to pull the groups associated with the key once that's built.
     const group = await this.model.findOne({
       where: {
-        workspaceId: owner.id,
-        type: "system",
+        workspaceId: key.workspaceId,
+        type: key.isSystem ? "system" : "global",
       },
-      transaction,
     });
 
     if (!group) {
-      throw new Error("System group not found.");
+      throw new Error("Group for key not found.");
     }
 
-    return new this(GroupModel, group.get());
+    return [new this(GroupModel, group.get())];
   }
 
-  static async fetchWorkspaceGlobalGroup(
-    auth: Authenticator,
-    transaction?: Transaction
+  static async internalFetchWorkspaceGlobalGroup(
+    workspaceId: ModelId
   ): Promise<GroupResource> {
-    const owner = auth.getNonNullableWorkspace();
     const group = await this.model.findOne({
       where: {
-        workspaceId: owner.id,
+        workspaceId,
         type: "global",
       },
-      transaction,
     });
 
     if (!group) {
@@ -172,10 +175,41 @@ export class GroupResource extends BaseResource<GroupModel> {
     return new this(GroupModel, group.get());
   }
 
+  static async fetchWorkspaceGlobalGroup(
+    auth: Authenticator
+  ): Promise<GroupResource> {
+    const owner = auth.getNonNullableWorkspace();
+    const group = await this.model.findOne({
+      where: {
+        workspaceId: owner.id,
+        type: "global",
+      },
+    });
+
+    if (!group) {
+      throw new Error("Global group not found.");
+    }
+
+    return new this(GroupModel, group.get());
+  }
+
+  static async fetchWorkspaceGroups(
+    auth: Authenticator
+  ): Promise<GroupResource[]> {
+    const owner = auth.getNonNullableWorkspace();
+
+    const groups = await this.model.findAll({
+      where: {
+        workspaceId: owner.id,
+      },
+    });
+
+    return groups.map((group) => new this(GroupModel, group.get()));
+  }
+
   static async fetchWorkspaceGroup(
     auth: Authenticator,
-    groupId: string,
-    transaction?: Transaction
+    groupId: string
   ): Promise<GroupResource | null> {
     const owner = auth.getNonNullableWorkspace();
     const groupModelId = getResourceIdFromSId(groupId);
@@ -189,7 +223,6 @@ export class GroupResource extends BaseResource<GroupModel> {
         workspaceId: owner.id,
         id: groupModelId,
       },
-      transaction,
     });
 
     if (!group) {
@@ -199,9 +232,59 @@ export class GroupResource extends BaseResource<GroupModel> {
     return new this(GroupModel, group.get());
   }
 
+  static async fetchActiveGroupsOfUserInWorkspace({
+    user,
+    workspace,
+  }: {
+    user: UserResource;
+    workspace: LightWorkspaceType;
+  }): Promise<GroupResource[]> {
+    // First we need to check if the user is a member of the workspace.
+    const workspaceMembership =
+      await MembershipResource.getActiveMembershipOfUserInWorkspace({
+        user,
+        workspace,
+      });
+    if (!workspaceMembership) {
+      return [];
+    }
+
+    // If yes, we can fetch the groups the user is a member of.
+    // First the global group which has no db entries and is always present.
+    const globalGroup = await this.model.findOne({
+      where: {
+        workspaceId: workspace.id,
+        type: "global",
+      },
+    });
+
+    if (!globalGroup) {
+      throw new Error("Global group not found.");
+    }
+
+    const regularGroups = await GroupModel.findAll({
+      include: [
+        {
+          model: GroupMembershipModel,
+          where: {
+            userId: user.id,
+            workspaceId: workspace.id,
+            startAt: { [Op.lte]: new Date() },
+            [Op.or]: [{ endAt: null }, { endAt: { [Op.gt]: new Date() } }],
+          },
+          required: true,
+        },
+      ],
+    });
+
+    const groups = [globalGroup, ...regularGroups];
+
+    return groups.map((group) => new this(GroupModel, group.get()));
+  }
+
   async addMember(
     auth: Authenticator,
-    userId: string,
+    user: UserType,
     transaction?: Transaction
   ): Promise<
     Result<
@@ -217,13 +300,13 @@ export class GroupResource extends BaseResource<GroupModel> {
   > {
     // Checking that the user is a member of the workspace.
     const owner = auth.getNonNullableWorkspace();
-    const user = await UserResource.fetchById(userId);
-    if (!user) {
+    const userResource = await UserResource.fetchById(user.sId);
+    if (!userResource) {
       return new Err({ type: "user_not_found" });
     }
     const workspaceMembership =
       await MembershipResource.getActiveMembershipOfUserInWorkspace({
-        user,
+        user: userResource,
         workspace: owner,
         transaction,
       });
@@ -267,7 +350,7 @@ export class GroupResource extends BaseResource<GroupModel> {
     return new Ok(undefined);
   }
 
-  toJSON() {
+  toJSON(): GroupType {
     return {
       id: this.id,
       name: this.name,
