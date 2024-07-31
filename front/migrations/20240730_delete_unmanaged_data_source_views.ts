7c1d0b1be1035dbe3671e5609cd95d349e21abe0
Flavien David <flavien@dust.tt>
2024-07-31 14:19:42 +0200

    Add migration to delete un managed data source views

---
+++ front/migrations/20240730_delete_unmanaged_data_source_views.ts
@@ -0,0 +1,64 @@
+import type { LightWorkspaceType } from "@dust-tt/types";
+import assert from "assert";
+
+import { Authenticator } from "@app/lib/auth";
+import { DataSourceResource } from "@app/lib/resources/data_source_resource";
+import { DataSourceViewResource } from "@app/lib/resources/data_source_view_resource";
+import type { Logger } from "@app/logger/logger";
+import { makeScript, runOnAllWorkspaces } from "@app/scripts/helpers";
+
+async function deleteUnmanagedDataSourceViewsForWorkspace(
+  workspace: LightWorkspaceType,
+  logger: Logger,
+  execute: boolean
+) {
+  const auth = await Authenticator.internalAdminForWorkspace(workspace.sId);
+
+  // List workspace data sources.
+  const dataSources = await DataSourceResource.listByWorkspace(auth);
+
+  // List workspace data source views.
+  const dataSourceViews = await DataSourceViewResource.listByWorkspace(auth);
+
+  logger.info(
+    `Found ${dataSourceViews.length} data source views for workspace(${workspace.sId}).`
+  );
+
+  const viewsToDelete: DataSourceViewResource[] = [];
+  for (const dataSourceView of dataSourceViews) {
+    const ds = dataSources.find((ds) => ds.id === dataSourceView.dataSourceId);
+    assert(ds, `Data source ${dataSourceView.dataSourceId} not found.`);
+
+    // If data source is not managed, delete the view.
+    if (!ds.isManaged()) {
+      viewsToDelete.push(dataSourceView);
+    }
+  }
+
+  logger.info(
+    `About to delete ${viewsToDelete.length} unmanaged data source views for workspace(${workspace.sId}).`
+  );
+
+  if (!execute) {
+    return;
+  }
+
+  for (const view of viewsToDelete) {
+    await view.delete(auth);
+    logger.info(`Deleted view for data source ${view.dataSourceId}.`);
+  }
+
+  logger.info(
+    `Deleted all unmanaged data source views for workspace(${workspace.sId}).`
+  );
+}
+
+makeScript({}, async ({ execute }, logger) => {
+  return runOnAllWorkspaces(async (workspace) => {
+    await deleteUnmanagedDataSourceViewsForWorkspace(
+      workspace,
+      logger,
+      execute
+    );
+  });
+});
