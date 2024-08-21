3e7fabf3162093de1d844918c20ee00f7916eac8
Thomas Draier <thomas@dust.tt>
2024-08-21 10:19:15 +0200

    Skip existing datasets points

--- front/admin/copy_apps.sh
+++ front/admin/copy_apps.sh
@@ -4,6 +4,9 @@ DIR=$(dirname $0)
 function escaped_columns_list {
     echo $* | sed -E 's/ /,/g'| sed -E 's/([a-zA-Z_]+)/\\"\1\\"/g'
 }
+function escaped_values_list {
+    echo $* | sed -E 's/ /,/g'| sed -E 's/([a-zA-Z_]+)/\\\\'\1\\\\'/g'
+}
 function columns_list {
     echo $* | sed -E 's/ /,/g'| sed -E 's/([a-zA-Z_]+)/"\1"/g'
 }
@@ -28,17 +31,20 @@ function import {
     table_name=${2}
     cols_to_import=${3}
     cols_to_update=${4}
+    on_conflict=${5}
+    additional_where=${6}
 
     eval uri='$'${database_uri}_DATABASE_URI
     echo -n "Preparing ${table_name}... "
-    psql ${uri} -c "drop table if exists __copy; create table __copy as (select * from ${table_name} limit 0);"
-    echo -n "Importing ${table_name}... "
-    psql ${uri} -c "COPY __copy ($(columns_list ${cols_to_import})) from stdin;" < /tmp/dust-apps/${database_uri}_${table_name}.csv
-    echo -n "Updating existing ${table_name}... "
-    psql ${uri} -c "update ${table_name} set $(updates_clause $cols_to_update) from __copy where ${table_name}.id = __copy.id;"
-    echo -n "Inserting new ${table_name}..."
-    psql ${uri} -c "insert into ${table_name} ($(columns_list ${cols_to_import})) (select $(copy_clause ${cols_to_import}) from __copy left join ${table_name} using(id) where ${table_name} is null);"
-    echo -n "Cleaning up ${table_name}... "
+    psql ${uri} -c "drop table if exists __copy" > /dev/null 2>&1
+    psql ${uri} -c "create table __copy as (select * from ${table_name} limit 0)" | tr -d '\n'
+    echo -n "... Importing ${table_name}... "
+    psql ${uri} -c "COPY __copy ($(columns_list ${cols_to_import})) from stdin;" < /tmp/dust-apps/${database_uri}_${table_name}.csv | tr -d '\n'
+    echo -n "... Updating existing ${table_name}... "
+    psql ${uri} -c "update ${table_name} set $(updates_clause $cols_to_update) from __copy where ${table_name}.id = __copy.id;" | tr -d '\n'
+    echo -n "... Inserting new ${table_name}..."
+    psql ${uri} -c "insert into ${table_name} ($(columns_list ${cols_to_import})) (select $(copy_clause ${cols_to_import}) from __copy left join ${table_name} using(id) where ${table_name} is null ${additional_where}) ${on_conflict};" | tr -d '\n'
+    echo -n "... Cleaning up ${table_name}... "
     psql ${uri} -c "drop table if exists __copy;"
 }
 
@@ -51,6 +57,13 @@ fi
 mkdir -p /tmp/dust-apps
 
 cd ${DIR}/..
+
+if [ "$1" == "--cleanup" ]
+then
+    psql ${FRONT_DATABASE_URI} -c "delete from apps where id>1000;"
+    psql ${CORE_DATABASE_URI} -c "delete from specifications where id>10000;"
+    psql ${CORE_DATABASE_URI} -c "delete from datasets_joins where id>10000; delete from datasets_points where id>10000; delete from datasets where id>10000;"
+fi
 
 if [ "$1" != "--force" ]
 then
@@ -80,37 +93,42 @@ set -e
 echo "Fetching prodbox pod..."
 PRODBOX_POD_NAME=$(kubectl get pods |grep prodbox |cut -d \  -f1)
 
-# ---- apps
+# ---- front
+
 fetch FRONT apps "id createdAt updatedAt sId name description visibility savedSpecification savedConfig savedRun dustAPIProjectId workspaceId" "\\\"workspaceId\\\"=5069"
+project_ids=$(cut -f 11 /tmp/dust-apps/FRONT_apps.csv |paste -sd "," -)
+fetch FRONT datasets "id createdAt updatedAt name description schema appId workspaceId" "\\\"workspaceId\\\"=5069"
+
+
+# ---- apps
 cat /tmp/dust-apps/FRONT_apps.csv | cut -f1-11 | sed -E "s/^(.*)$/\1\t${DUST_APPS_SYNC_WORKSPACE_ID}/g" > /tmp/dust-apps/FRONT_apps_transformed.csv
 mv /tmp/dust-apps/FRONT_apps_transformed.csv /tmp/dust-apps/FRONT_apps.csv
 import FRONT apps "id createdAt updatedAt sId name description visibility savedSpecification savedConfig savedRun dustAPIProjectId workspaceId" "updatedAt name description visibility savedSpecification savedConfig savedRun dustAPIProjectId"
 
 # ---- datasets
-fetch FRONT datasets "id createdAt updatedAt name description schema appId workspaceId" "\\\"workspaceId\\\"=5069"
 cat /tmp/dust-apps/FRONT_datasets.csv | cut -f1-7 | sed -E "s/^(.*)$/\1\t${DUST_APPS_SYNC_WORKSPACE_ID}/g" > /tmp/dust-apps/FRONT_datasets_transformed.csv
 mv /tmp/dust-apps/FRONT_datasets_transformed.csv /tmp/dust-apps/FRONT_datasets.csv
 import FRONT datasets "id createdAt updatedAt name description schema appId workspaceId" "updatedAt name description schema"
 
-project_ids=$(cut -f 11 /tmp/dust-apps/FRONT_apps.csv |paste -sd "," -)
+# ---- core
 
-# ---- projects
 fetch CORE projects "id" "\\\"id\\\" in (${project_ids})"
-import CORE projects "id" "id"
-
-# ---- specifications
 fetch CORE specifications "id project created hash specification" "\\\"project\\\" in (${project_ids})"
-import CORE specifications "id project created hash specification" "hash specification"
-
-# ---- datasets
 fetch CORE datasets "id project created dataset_id hash" "\\\"project\\\" in (${project_ids})"
 dataset_ids=$(cut -f 1 /tmp/dust-apps/CORE_datasets.csv |paste -sd "," -)
 fetch CORE datasets_joins "id dataset point point_idx" "\\\"dataset\\\" in (${dataset_ids})"
 dataset_points_ids=$(cut -f 3 /tmp/dust-apps/CORE_datasets_joins.csv |paste -sd "," -)
 fetch CORE datasets_points "id hash json" "\\\"id\\\" in (${dataset_points_ids})"
 
+# ---- projects
+import CORE projects "id" "id"
+
+# ---- specifications
+import CORE specifications "id project created hash specification" "hash specification"
+
+# ---- datasets
 import CORE datasets "id project created dataset_id hash" "hash"
-import CORE datasets_points "id hash json" "hash json"
-import CORE datasets_joins "id dataset point point_idx" "point point_idx" 
+import CORE datasets_points "id hash json" "hash json" "on conflict(hash) do nothing"
+import CORE datasets_joins "id dataset point point_idx" "point point_idx" "" "and __copy.point in (select id from datasets_points)"
 
 rm -R /tmp/dust-apps
