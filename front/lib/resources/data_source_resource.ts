0161b124426aa5d629f6c17e7be586e2775a0cd1
Stanislas Polu <spolu@dust.tt>
2024-08-22 17:08:29 +0200

    Remove updateDataSourceEditedBy (#6868)
    * remove updateDataSourceEditedBy
    
    * remove auth check

--- front/lib/resources/data_source_resource.ts
+++ front/lib/resources/data_source_resource.ts
@@ -3,6 +3,7 @@ import type {
   DataSourceType,
   ModelId,
   Result,
+  UserType,
 } from "@dust-tt/types";
 import { Err, formatUserFullName, Ok } from "@dust-tt/types";
 import type {
@@ -224,6 +225,13 @@ export class DataSourceResource extends ResourceWithVault<DataSource> {
     // Update the current instance with the new values to avoid stale data
     Object.assign(this, affectedRows[0].get());
     return [affectedCount];
+  }
+
+  async setEditedBy(user: UserType) {
+    await this.update({
+      editedByUserId: user.id,
+      editedAt: new Date(),
+    });
   }
 
   private makeEditedBy(
