f00fa5742e6d013651a3ce9feda186ead3b28f30
Aric Lasry <lasry.aric@gmail.com>
2024-08-26 17:02:03 +0200

    Allow specifying the group for whitelisted Slack workflows

---
+++ front/migrations/20240826_backfill_slack_whitelisted_bots.ts
@@ -0,0 +1,62 @@
+import { EnvironmentConfig } from "@dust-tt/types";
+import { QueryTypes, Sequelize } from "sequelize";
+
+import { Authenticator } from "@app/lib/auth";
+import { Workspace } from "@app/lib/models/workspace";
+import { GroupResource } from "@app/lib/resources/group_resource";
+import { makeSId } from "@app/lib/resources/string_ids";
+import { makeScript } from "@app/scripts/helpers";
+
+makeScript({}, async ({ execute }, logger) => {
+  const connectorDB = new Sequelize(
+    EnvironmentConfig.getEnvVariable("CONNECTORS_DATABASE_URI")
+  );
+  const bots: { id: number; workspaceid: string }[] = await connectorDB.query(
+    `SELECT swb.id as id, c."workspaceId" as workspaceid FROM slack_bot_whitelist swb INNER JOIN connectors c ON swb."connectorId" = c.id WHERE c.type = 'slack'`,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  for (const bot of bots) {
+    const auth = await Authenticator.internalAdminForWorkspace(bot.workspaceid);
+    const groupRes = await GroupResource.fetchWorkspaceGlobalGroup(auth);
+    if (groupRes.isErr()) {
+      throw new Error(
+        `Failed to fetch global group for workspace ${bot.workspaceid}`
+      );
+    }
+    const workspace = await Workspace.findOne({
+      where: { sId: bot.workspaceid },
+    });
+    if (!workspace) {
+      throw new Error(`Workspace not found for workspaceId ${bot.workspaceid}`);
+    }
+
+    const groupId = makeSId("group", {
+      id: groupRes.value.id,
+      workspaceId: workspace.id,
+    });
+
+    logger.info(
+      {
+        botId: bot.id,
+        workspaceId: bot.workspaceid,
+        groupId: groupId,
+      },
+      "Updating slack_bot_whitelist with groupId"
+    );
+
+    if (execute) {
+      await connectorDB.query(
+        `UPDATE slack_bot_whitelist SET "groupIds" = array_appen("groupIds", :groupId) WHERE id = :id`,
+        {
+          replacements: {
+            groupId: groupId,
+            id: bot.id,
+          },
+        }
+      );
+    }
+  }
+});
