601c14c1efb462d8230193d860691eea06808704
Daphné Popin <PopDaph@users.noreply.github.com>
2024-08-26 16:53:51 +0200

    Create new website in a vault (#6872)
    * Create new website in a vault
    
    * Add new routes for delete datasources in vault

--- front/pages/api/w/[wId]/vaults/[vId]/data_sources/managed.ts
+++ front/pages/api/w/[wId]/vaults/[vId]/data_sources/managed.ts
@@ -51,6 +51,7 @@ export function getConnectorProviderCodec(): t.Mixed {
 const PostManagedDataSourceRequestBodySchema = t.type({
   provider: getConnectorProviderCodec(),
   connectionId: t.union([t.string, t.undefined]),
+  name: t.union([t.string, t.undefined]),
   configuration: ConnectorConfigurationTypeSchema,
 });
 
@@ -105,7 +106,7 @@ async function handler(
         });
       }
 
-      const { connectionId, provider } = bodyValidation.right;
+      const { connectionId, provider, name } = bodyValidation.right;
 
       // Checking that the provider is allowed for the workspace plan
       const isDataSourceAllowedInPlan = isConnectorProviderAllowedForPlan(
@@ -156,7 +157,7 @@ async function handler(
           },
         });
       }
-      const dataSourceName = getDefaultDataSourceName(provider, suffix);
+      const dataSourceName = name ?? getDefaultDataSourceName(provider, suffix);
       let dataSourceDescription = getDefaultDataSourceDescription(
         provider,
         suffix
