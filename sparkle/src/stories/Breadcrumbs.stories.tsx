095e611923b3f57c6fe19be3f7cd642b5deec57c
filou <philipperolet@gmail.com>
2024-08-27 18:31:19 +0200

    [Sparkle] Super breadcrumbs
    Description
    ---
    Part of task #6906
    
    Super breadcrumbs:
    - Shows up to 5 elements;
    - if more than 5 elements, shows the 2 first & 2 last elements, ellipsis in the middle;
    - clic on ellipsis shows dropdown
    - cuts middle elements to 15 chars, last element to 30 chars
    
    Risk
    ---
    na
    
    Deploy
    ---
    deploy front

--- sparkle/src/stories/Breadcrumbs.stories.tsx
+++ sparkle/src/stories/Breadcrumbs.stories.tsx
@@ -1,7 +1,12 @@
 import type { Meta } from "@storybook/react";
 import React from "react";
 
-import { Breadcrumbs, CompanyIcon, HomeIcon } from "../index_with_tw_base";
+import {
+  Breadcrumbs,
+  CompanyIcon,
+  FolderIcon,
+  HomeIcon,
+} from "../index_with_tw_base";
 
 const meta = {
   title: "Components/Breadcrumbs",
@@ -20,12 +25,55 @@ export const BreadcrumbsExample = () => {
     { label: "Home", href: "#", icon: HomeIcon },
     { label: "Vaults", href: "#", icon: CompanyIcon },
     { label: "My Vault", href: "#" },
+    { label: "loooong name in the end, like very very long long" },
+  ];
+
+  const items3 = [
+    { label: "Home", href: "#", icon: HomeIcon },
+    {
+      label: "Middle long name, oh very looong folder name in the middle",
+      href: "#",
+      icon: CompanyIcon,
+    },
+    { label: "My Vault", href: "#" },
+    { label: "Data Sources" },
+    { label: "Folder1", href: "#", icon: FolderIcon },
+    { label: "With ellipsis", href: "#" },
+  ];
+
+  const items4 = [
+    { label: "Home", href: "#", icon: HomeIcon },
+    {
+      label: "Middle long name, oh very looong folder name in the middle",
+      href: "#",
+      icon: CompanyIcon,
+    },
+    { label: "My Vault", href: "#" },
+    { label: "Data Sources" },
+    { label: "Folder1", href: "#", icon: FolderIcon },
+    { label: "Folder2", href: "#", icon: FolderIcon },
+    { label: "Folder3", href: "#", icon: FolderIcon },
+    { label: "With ellipsis", href: "#" },
+  ];
+
+  const items5 = [
+    { label: "Home", href: "#", icon: HomeIcon },
+    {
+      label: "Long, oh very looong folder name in the middle",
+      href: "#",
+      icon: CompanyIcon,
+    },
+    { label: "My Vault", href: "#" },
     { label: "Data Sources" },
   ];
+
   return (
-    <div className="s-flex s-flex-col s-gap-4">
+    <div className="s-flex s-flex-col s-gap-4 s-pb-8">
       <Breadcrumbs items={items1} />
       <Breadcrumbs items={items2} />
+      <Breadcrumbs items={items4} />
+      <Breadcrumbs items={items3} />
+      <Breadcrumbs items={items5} />
     </div>
   );
 };
