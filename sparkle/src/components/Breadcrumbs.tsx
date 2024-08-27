0fd92286d756bf06a172b529e884ddfa099e6c13
Philippe Rolet <pr@dust.tt>
2024-08-27 15:30:18 +0200

    [Sparkle] Breadcrumbs component (#6934)
    * [Sparkle] Breadcrumbs component
    
    Description
    ---
    Moves the Breadcrumbs component to sparkle as part of #6906
    
    Risk
    ---
    na - component is added but not used & the former is not deleted (this
    comes in a follow-up PR)
    
    Deploy
    ---
    publish sparkle
    
    * clean
    
    * next tentative
    
    * next tentative plock
    
    * remove next after all
    
    * lint

---
+++ sparkle/src/components/Breadcrumbs.tsx
@@ -0,0 +1,56 @@
+import type { ComponentType } from "react";
+import React from "react";
+
+import { SparkleContextLinkType } from "@sparkle/context";
+import { ChevronRightIcon, Icon, SparkleContext } from "@sparkle/index";
+
+type BreadcrumbProps = {
+  items: {
+    label: string;
+    icon?: ComponentType<{ className?: string }>;
+    href?: string;
+  }[];
+};
+
+export function Breadcrumbs({ items }: BreadcrumbProps) {
+  const { components } = React.useContext(SparkleContext);
+
+  const Link: SparkleContextLinkType = components.link;
+
+  return (
+    <div className="gap-2 s-flex s-flex-row s-items-center">
+      {items.map((item, index) => (
+        <div key={index} className="s-flex s-flex-row s-items-center s-gap-1">
+          <Icon visual={item.icon} className="s-text-brand" />
+          <div>
+            {item.href ? (
+              <Link
+                href={item.href}
+                className={
+                  index === items.length - 1
+                    ? "s-text-element-900"
+                    : "s-text-element-700"
+                }
+              >
+                {item.label}
+              </Link>
+            ) : (
+              <span
+                className={
+                  index === items.length - 1
+                    ? "s-text-element-900"
+                    : "s-text-element-700"
+                }
+              >
+                {item.label}
+              </span>
+            )}
+          </div>
+          {index === items.length - 1 ? null : (
+            <ChevronRightIcon className="s-text-element-500" />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
