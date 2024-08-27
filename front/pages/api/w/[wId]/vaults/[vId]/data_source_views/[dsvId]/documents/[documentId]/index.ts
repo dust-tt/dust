cf6d092c4f0298c494e6d967483508e7fb437220
Flavien David <flavien.david74@gmail.com>
2024-08-27 09:20:59 +0200

    Apply view filter when fetch document content (#6920)
    * Apply view filter when fetch document content
    
    * :see_no_evil:

--- front/pages/api/w/[wId]/vaults/[vId]/data_source_views/[dsvId]/documents/[documentId]/index.ts
+++ front/pages/api/w/[wId]/vaults/[vId]/data_source_views/[dsvId]/documents/[documentId]/index.ts
@@ -61,9 +61,17 @@ async function handler(
   switch (req.method) {
     case "GET":
       const document = await coreAPI.getDataSourceDocument({
-        projectId: dataSourceView.dataSource.dustAPIProjectId,
         dataSourceName: dataSourceView.dataSource.name,
         documentId,
+        projectId: dataSourceView.dataSource.dustAPIProjectId,
+        viewFilter: {
+          parents: {
+            in: dataSourceView.parentsIn,
+            not: null,
+          },
+          tags: null,
+          timestamp: null,
+        },
       });
 
       if (document.isErr()) {
