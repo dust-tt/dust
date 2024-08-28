533b2ccfd7967e29a2e5877f07fcd0aabc0275a0
Stanislas Polu <spolu@dust.tt>
2024-08-28 10:46:39 +0200

    final version

--- front/lib/production_checks/checks/check_connectors_last_sync_success.ts
+++ front/lib/production_checks/checks/check_connectors_last_sync_success.ts
@@ -1,3 +1,5 @@
+import type { ConnectorProvider } from "@dust-tt/types";
+import { isWebhookBasedProvider } from "@dust-tt/types";
 import { QueryTypes } from "sequelize";
 
 import type { CheckFunction } from "@app/lib/production_checks/types";
@@ -5,36 +7,52 @@ import { getConnectorReplicaDbConnection } from "@app/lib/production_checks/util
 
 interface ConnectorBlob {
   id: number;
-  type: string;
+  type: ConnectorProvider;
   createdAt: Date;
   dataSourceName: string;
   workspaceId: string;
   pausedAt: Date | null;
   lastSyncSuccessfulTime: Date | null;
+  lastSyncStartTime: Date | null;
 }
 
 const connectorsReplica = getConnectorReplicaDbConnection();
 
 async function listAllConnectors() {
   const connectors: ConnectorBlob[] = await connectorsReplica.query(
-    `SELECT id, "dataSourceName", "workspaceId", "pausedAt", "lastSyncSuccessfulTime", "createdAt", "type" FROM connectors WHERE "errorType" IS NULL`,
+    `SELECT id, "dataSourceName", "workspaceId", "pausedAt", "lastSyncSuccessfulTime", "lastSyncStartTime", "createdAt", "type" FROM connectors WHERE "errorType" IS NULL AND "pausedAt" IS NULL AND "type" <> 'webcrawler'`,
     {
       type: QueryTypes.SELECT,
     }
   );
-
   return connectors;
 }
 
-function isLastSyncSuccessFullLessThanAWeek(connector: ConnectorBlob) {
-  const lastSuccessOrStart =
-    connector.lastSyncSuccessfulTime || connector.createdAt;
+function isLastSyncSuccessfullOrStartLessFresh(connector: ConnectorBlob) {
+  const oneWeek = 7 * 24 * 60 * 60 * 1000;
 
-  // if older than a week
-  if (Date.now() - lastSuccessOrStart.getTime() > 7 * 24 * 60 * 60 * 1000) {
-    return false;
+  // If we have a lastSyncSuccessfulTime and it's less than a week old then we're good.
+  if (
+    connector.lastSyncSuccessfulTime &&
+    Date.now() - connector.lastSyncSuccessfulTime.getTime() < oneWeek
+  ) {
+    return true;
   }
-  return true;
+
+  // If the last sync started less than a week ago, we're good.
+  if (
+    connector.lastSyncStartTime &&
+    Date.now() - connector.lastSyncStartTime.getTime() < oneWeek
+  ) {
+    return true;
+  }
+
+  // If the connector was created less than a week ago, we're good.
+  if (Date.now() - connector.createdAt.getTime() < oneWeek) {
+    return true;
+  }
+
+  return false;
 }
 
 export const checkConnectorsLastSyncSuccess: CheckFunction = async (
@@ -45,11 +63,15 @@ export const checkConnectorsLastSyncSuccess: CheckFunction = async (
   heartbeat
 ) => {
   const stalledLastSyncConnectors: any[] = [];
-  const connectors = await listAllConnectors();
+  const connectors = (await listAllConnectors()).filter(
+    (connector) =>
+      // Ignore webhook-based connectors and webcrawlers
+      !isWebhookBasedProvider(connector.type) && connector.type !== "webcrawler"
+  );
   heartbeat();
 
   for (const connector of connectors) {
-    const isFresh = isLastSyncSuccessFullLessThanAWeek(connector);
+    const isFresh = isLastSyncSuccessfullOrStartLessFresh(connector);
     if (!isFresh) {
       stalledLastSyncConnectors.push({
         provider: connector.type,
