0dfba117dd9bcdda7352cd1a82f84b423293ac62
Aric Lasry <lasry.aric@gmail.com>
2024-08-29 11:30:54 +0200

    Corretly read the x-dust-group-ids header

--- front/lib/http_api/group_header.ts
+++ front/lib/http_api/group_header.ts
@@ -3,7 +3,7 @@ import { DustGroupIdsHeader } from "@dust-tt/types";
 export function getGroupIdsFromHeaders(
   headers: Record<string, string | string[] | undefined>
 ): string[] | undefined {
-  const groupIds = headers[DustGroupIdsHeader];
+  const groupIds = headers[DustGroupIdsHeader.toLowerCase()];
   if (typeof groupIds === "string") {
     return groupIds.split(",").map((id) => id.trim());
   } else {
