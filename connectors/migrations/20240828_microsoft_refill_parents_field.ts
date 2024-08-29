c6b267fa13108f24969a43b2018640e59cf82e93
Philippe Rolet <pr@dust.tt>
2024-08-29 15:07:11 +0200

    [Microsoft] Improve parents migration (#7001)
    Description
    ---
    Migration from # was too aggressive on sql & qdrant. We decrease
    concurrency, paginate, log cleaner, and do connector by connector
    
    Risk
    ---
    na
    
    Deploy
    ---
    connectors

--- connectors/migrations/20240828_microsoft_refill_parents_field.ts
+++ connectors/migrations/20240828_microsoft_refill_parents_field.ts
@@ -5,70 +5,101 @@ import { getParents } from "@connectors/connectors/microsoft/temporal/file";
 import { concurrentExecutor } from "@connectors/lib/async_utils";
 import { updateDocumentParentsField } from "@connectors/lib/data_sources";
 import { MicrosoftNodeModel } from "@connectors/lib/models/microsoft";
-import logger from "@connectors/logger/logger";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
 
-makeScript({}, async ({ execute }) => {
-  const connectors = await ConnectorResource.listByType("microsoft", {});
-  for (const connector of connectors) {
-    logger.info({ connector: connector.toJSON() }, "Processing connector");
-    await updateParentsFieldForConnector(connector, execute);
+makeScript(
+  {
+    connectorId: { type: "number", demandOption: false },
+  },
+  async ({ execute, connectorId }) => {
+    if (connectorId) {
+      const connector = await ConnectorResource.fetchById(connectorId);
+      if (!connector) {
+        throw new Error(`Connector with id ${connectorId} not found`);
+      }
+      await updateParentsFieldForConnector(connector, execute);
+      return;
+    }
+
+    const connectors = await ConnectorResource.listByType("microsoft", {});
+    console.log(`Found ${connectors.length} Microsoft connectors, pick one`);
+    for (const connector of connectors) {
+      console.log(
+        `Workspace ${connector.workspaceId}:  connector ${connector.id}`
+      );
+    }
   }
-});
+);
 
 async function updateParentsFieldForConnector(
   connector: ConnectorResource,
   execute = false
 ) {
-  // get all nodes (not using resource here since 1-shot clean migration)
-  const nodes = await MicrosoftNodeModel.findAll({
-    where: {
-      connectorId: connector.id,
-      nodeType: "file",
-      // exclude spreadsheets and csv files (their parents were correct)
-      mimeType: {
-        [Op.notIn]: [
-          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-          "application/vnd.ms-excel",
-          "text/csv",
-        ],
-      },
-    },
-  });
+  let idCursor = 0;
 
-  const startSyncTs = new Date().getTime();
-  const res = await concurrentExecutor(
-    nodes,
-    async (node) => {
-      const parents = await getParents({
+  const pageSize = 512;
+
+  let nodes: MicrosoftNodeModel[] = [];
+  do {
+    nodes = await MicrosoftNodeModel.findAll({
+      where: {
         connectorId: connector.id,
-        internalId: node.internalId,
-        startSyncTs,
-      });
-
-      if (!execute) {
-        return true;
-      }
-
-      return updateDocumentParentsField({
-        dataSourceConfig: {
-          dataSourceName: connector.dataSourceName,
-          workspaceId: connector.workspaceId,
-          workspaceAPIKey: connector.workspaceAPIKey,
+        nodeType: "file",
+        // exclude spreadsheets and csv files (their parents were correct)
+        mimeType: {
+          [Op.notIn]: [
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.ms-excel",
+            "text/csv",
+          ],
         },
-        documentId: node.internalId,
-        parents,
-      });
-    },
-    { concurrency: 64 }
-  );
+        id: {
+          [Op.gte]: idCursor,
+        },
+      },
+      limit: pageSize,
+      order: [["id", "ASC"]],
+    });
 
-  logger.info(
-    {
-      connectorId: connector.id,
-      updated: res.filter((r) => r).length,
-      total: res.length,
-    },
-    "Updated parents field for connector"
+    const startSyncTs = new Date().getTime();
+    const res = await concurrentExecutor(
+      nodes,
+      async (node) => {
+        const parents = await getParents({
+          connectorId: connector.id,
+          internalId: node.internalId,
+          startSyncTs,
+        });
+
+        if (!execute) {
+          return true;
+        }
+
+        return updateDocumentParentsField({
+          dataSourceConfig: {
+            dataSourceName: connector.dataSourceName,
+            workspaceId: connector.workspaceId,
+            workspaceAPIKey: connector.workspaceAPIKey,
+          },
+          documentId: node.internalId,
+          parents,
+        });
+      },
+      { concurrency: 8 }
+    );
+
+    if (nodes.length > 0) {
+      const lastNode = nodes[nodes.length - 1];
+      if (!lastNode) {
+        throw new Error("Last node is undefined");
+      }
+      idCursor = lastNode.id + 1;
+    }
+
+    console.log(`Processed ${res.length} nodes, new cursor ${idCursor}`);
+  } while (nodes.length > 0);
+
+  console.log(
+    `Finished processing connector ${connector.id} (workspace ${connector.workspaceId})`
   );
 }
