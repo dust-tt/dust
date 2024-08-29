e55fe5a0ca3e08c7a17057757b1d0a44d662ae6d
Philippe Rolet <pr@dust.tt>
2024-08-29 11:47:10 +0200

    [Microsoft] Migrate parents in core to correct order (#6976)
    * [Microsoft] Migrate parents in core to correct order
    
    Description
    ---
    Fixes #6975
    
    Remove the reverse making parents in the wrong order, & add the migration script
    
    Risks
    ---
    Radius: microsoft connectors
    Risk: messing up with exisiting accounts, but there are only 4; worst case we can full resync
    
    Deploy plan
    ---
    - pause all microsoft connectors
    - deploy connectors
    - run migration
    - restart connectors
    
    * clean
    
    * fix migration
    
    * increase concurrency

---
+++ connectors/migrations/20240828_microsoft_refill_parents_field.ts
@@ -0,0 +1,74 @@
+import { makeScript } from "scripts/helpers";
+import { Op } from "sequelize";
+
+import { getParents } from "@connectors/connectors/microsoft/temporal/file";
+import { concurrentExecutor } from "@connectors/lib/async_utils";
+import { updateDocumentParentsField } from "@connectors/lib/data_sources";
+import { MicrosoftNodeModel } from "@connectors/lib/models/microsoft";
+import logger from "@connectors/logger/logger";
+import { ConnectorResource } from "@connectors/resources/connector_resource";
+
+makeScript({}, async ({ execute }) => {
+  const connectors = await ConnectorResource.listByType("microsoft", {});
+  for (const connector of connectors) {
+    logger.info({ connector: connector.toJSON() }, "Processing connector");
+    await updateParentsFieldForConnector(connector, execute);
+  }
+});
+
+async function updateParentsFieldForConnector(
+  connector: ConnectorResource,
+  execute = false
+) {
+  // get all nodes (not using resource here since 1-shot clean migration)
+  const nodes = await MicrosoftNodeModel.findAll({
+    where: {
+      connectorId: connector.id,
+      nodeType: "file",
+      // exclude spreadsheets and csv files (their parents were correct)
+      mimeType: {
+        [Op.notIn]: [
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          "application/vnd.ms-excel",
+          "text/csv",
+        ],
+      },
+    },
+  });
+
+  const startSyncTs = new Date().getTime();
+  const res = await concurrentExecutor(
+    nodes,
+    async (node) => {
+      const parents = await getParents({
+        connectorId: connector.id,
+        internalId: node.internalId,
+        startSyncTs,
+      });
+
+      if (!execute) {
+        return true;
+      }
+
+      return updateDocumentParentsField({
+        dataSourceConfig: {
+          dataSourceName: connector.dataSourceName,
+          workspaceId: connector.workspaceId,
+          workspaceAPIKey: connector.workspaceAPIKey,
+        },
+        documentId: node.internalId,
+        parents,
+      });
+    },
+    { concurrency: 64 }
+  );
+
+  logger.info(
+    {
+      connectorId: connector.id,
+      updated: res.filter((r) => r).length,
+      total: res.length,
+    },
+    "Updated parents field for connector"
+  );
+}
