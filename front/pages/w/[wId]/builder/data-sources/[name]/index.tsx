0e90411931b59fb43cce18e52dd987415707f42f
Stanislas Polu <spolu@dust.tt>
2024-08-30 15:20:59 +0200

    fix: permission update message on error

--- front/pages/w/[wId]/builder/data-sources/[name]/index.tsx
+++ front/pages/w/[wId]/builder/data-sources/[name]/index.tsx
@@ -1064,6 +1064,21 @@ function ManagedDataSourceView({
     };
   };
 
+  const {
+    displayEditionModal,
+    displayManageConnectionButton,
+    addDataWithConnection,
+    displayAddDataButton,
+    displayWebcrawlerSettingsButton,
+    guideLink,
+    postPermissionsUpdateMessage,
+  } = getRenderingConfigForConnectorProvider(connectorProvider);
+
+  const [
+    postPermissionsUpdateDialogIsOpen,
+    setPostPermissionsUpdateDialogIsOpen,
+  ] = useState(false);
+
   const handleUpdatePermissions = async () => {
     if (!connector) {
       console.error("No connector");
@@ -1095,23 +1110,12 @@ function ManagedDataSourceView({
         title: "Failed to update the permissions of the Data Source",
         description: updateRes.error,
       });
+      return;
     }
+
+    // If the update permission was successful show the post permission update dialog.
+    postPermissionsUpdateMessage && setPostPermissionsUpdateDialogIsOpen(true);
   };
-
-  const {
-    displayEditionModal,
-    displayManageConnectionButton,
-    addDataWithConnection,
-    displayAddDataButton,
-    displayWebcrawlerSettingsButton,
-    guideLink,
-    postPermissionsUpdateMessage,
-  } = getRenderingConfigForConnectorProvider(connectorProvider);
-
-  const [
-    postPermissionsUpdateDialogIsOpen,
-    setPostPermissionsUpdateDialogIsOpen,
-  ] = useState(false);
 
   return (
     <>
@@ -1293,10 +1297,7 @@ function ManagedDataSourceView({
           owner={owner}
           user={user}
           onEditPermissionsClick={() => {
-            void handleUpdatePermissions().then(() => {
-              postPermissionsUpdateMessage &&
-                setPostPermissionsUpdateDialogIsOpen(true);
-            });
+            void handleUpdatePermissions();
           }}
           dustClientFacingUrl={dustClientFacingUrl}
         />
