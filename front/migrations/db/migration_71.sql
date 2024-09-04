4f7f4269834726bead2093475399a69011cc5c9a
Thomas Draier <thomas@dust.tt>
2024-09-04 12:13:09 +0200

    Add migration

---
+++ front/migrations/db/migration_71.sql
@@ -0,0 +1,9 @@
+-- Migration created on Aug 30, 2024
+UPDATE "public"."data_source_views"
+SET
+    "editedByUserId" = "data_sources"."editedByUserId"
+FROM "public"."data_sources"
+WHERE
+    "data_sources"."id" = "data_source_views"."dataSourceId"
+    AND "data_source_views"."editedByUserId" IS NULL
+    AND "data_sources"."editedByUserId" IS NOT NULL
