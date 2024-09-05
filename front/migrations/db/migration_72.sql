c364af8b607f9b10ba45c9bce4b649d157dabbc1
Sebastien Flory <sflory@gmail.com>
2024-09-05 14:20:49 +0200

    Fix: wrap migration with backfill script

--- front/migrations/db/migration_72.sql
+++ front/migrations/db/migration_72.sql
@@ -1,5 +1,29 @@
--- Migration created on Sep 04, 2024
-ALTER TABLE "keys"
-ALTER COLUMN "groupId"
-SET
-    NOT NULL;
+-- -- This migration is dependant on a backfill script
+-- -- The backfill script is: front/migrations/20240731_backfill_keys.ts
+-- -- run psql with --set=backfilled=1 argument if you have rune the script.
+
+CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
+RETURNS VARCHAR AS $$
+BEGIN
+    IF NOT backfilled THEN
+        RAISE NOTICE 'The backfill script: front/migrations/20240731_backfill_keys.ts is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.';
+    END IF;
+
+        -- Migration created on Sep 04, 2024
+    ALTER TABLE "keys"
+    ALTER COLUMN "groupId"
+    SET
+        NOT NULL;
+
+    RETURN 'success';
+END;
+$$ LANGUAGE plpgsql;
+
+\if :{?backfilled}
+   SELECT perform_migration(:'backfilled'::boolean);
+\else
+    \echo '!! Migration was NOT applied !!'
+    \echo 'The backfill script: front/migrations/20240731_backfill_keys.ts is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.'
+\endif
+
+DROP FUNCTION perform_migration(boolean);
