4a766e6dd3b830f947ba0d5fad83bf800bc3e6d2
Stanislas Polu <spolu@dust.tt>
2024-09-06 12:16:34 +0200

    db migration

---
+++ front/migrations/db/migration_75.sql
@@ -0,0 +1,26 @@
+-- -- This migration is dependant on a backfill script
+-- -- The backfill script is: 20240906_backfill_apps_vault_ids.ts
+-- -- run psql with --set=backfilled=1 argument if you have rune the script.
+
+CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
+RETURNS VARCHAR AS $$
+BEGIN
+    IF NOT backfilled THEN
+        RAISE NOTICE 'The backfill script: 20240906_backfill_apps_vault_ids.ts is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.';
+    END IF;
+
+-- Migration created on Sep 06, 2024
+ALTER TABLE "public"."apps" ADD COLUMN "vaultId" INTEGER REFERENCES "vaults" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+    RETURN 'success';
+END;
+$$ LANGUAGE plpgsql;
+
+\if :{?backfilled}
+   SELECT perform_migration(:'backfilled'::boolean);
+\else
+    \echo '!! Migration was NOT applied !!'
+    \echo 'The backfill script: 20240906_backfill_apps_vault_ids.ts is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.'
+\endif
+
+DROP FUNCTION perform_migration(boolean);
