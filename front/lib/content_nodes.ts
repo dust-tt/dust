3f29760f60115cceb3a9a582df22949caedccf34
Flavien David <flavien@dust.tt>
2024-09-06 21:19:21 +0200

    Fix Document icons

--- front/lib/content_nodes.ts
+++ front/lib/content_nodes.ts
@@ -1,13 +1,22 @@
 import {
   ChatBubbleLeftRightIcon,
-  DocumentTextIcon,
+  DocumentIcon,
+  DocumentPileIcon,
   FolderIcon,
   Square3Stack3DIcon,
 } from "@dust-tt/sparkle";
-import type { ContentNode, LightContentNode } from "@dust-tt/types";
+import type { ContentNode } from "@dust-tt/types";
 import { assertNever } from "@dust-tt/types";
 
-export function getVisualForContentNode(node: ContentNode | LightContentNode) {
+function getVisualForFileContentNode(node: ContentNode & { type: "file" }) {
+  if (node.expandable) {
+    return DocumentPileIcon;
+  }
+
+  return DocumentIcon;
+}
+
+export function getVisualForContentNode(node: ContentNode) {
   switch (node.type) {
     case "channel":
       return ChatBubbleLeftRightIcon;
@@ -16,7 +25,9 @@ export function getVisualForContentNode(node: ContentNode | LightContentNode) {
       return Square3Stack3DIcon;
 
     case "file":
-      return DocumentTextIcon;
+      return getVisualForFileContentNode(
+        node as ContentNode & { type: "file" }
+      );
 
     case "folder":
       return FolderIcon;
