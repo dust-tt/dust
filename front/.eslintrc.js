b5cf947f47284692f2f407164ecfb91ea0fb0b44
Stanislas Polu <spolu@dust.tt>
2024-09-09 17:49:34 +0200

    error on cycles

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   plugins: ["import", "simple-import-sort"],
   rules: {
-    "import/no-cycle": "warn",
+    "import/no-cycle": "error",
     /**
     "@typescript-eslint/naming-convention": [
       "error",
