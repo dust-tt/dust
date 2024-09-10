ea58fd8d0ec755ea8d27a7077ca00458e9118592
Flavien David <flavien@dust.tt>
2024-09-10 16:04:08 +0200

    Add migration to add `dataSourceViewId` on `retrieval_documents`.

---
+++ front/migrations/db/migration_77.sql
@@ -0,0 +1,3 @@
+-- Migration created on Sep 09, 2024
+ALTER TABLE "public"."retrieval_documents"
+ADD COLUMN "dataSourceViewId" INTEGER REFERENCES "data_source_views" ("id") ON DELETE SET NULL ON UPDATE CASCADE;
