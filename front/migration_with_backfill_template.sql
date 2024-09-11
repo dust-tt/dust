90dbcaef2158a37b9332eadbc146f7c40ed68724
Flavien David <flavien@dust.tt>
2024-09-11 15:20:46 +0200

    Add migration + fix template

--- front/migration_with_backfill_template.sql
+++ front/migration_with_backfill_template.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
 RETURNS VARCHAR AS $$
 BEGIN
     IF NOT backfilled THEN
-        RAISE NOTICE 'The backfill script: BACKFILL_SCRIPT_NAME is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.';
+        RAISE NOTICE 'The backfill script: BACKFILL_SCRIPT_NAME is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.';
     END IF;
 
     MIGRATION_STATEMENTS
@@ -19,7 +19,7 @@ $$ LANGUAGE plpgsql;
    SELECT perform_migration(:'backfilled'::boolean);
 \else
     \echo '!! Migration was NOT applied !!'
-    \echo 'The backfill script: BACKFILL_SCRIPT_NAME is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.'
+    \echo 'The backfill script: BACKFILL_SCRIPT_NAME is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.'
 \endif
 
 DROP FUNCTION perform_migration(boolean);
