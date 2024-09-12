d3bb910051d7fa8998b6bd01f3772010a754bc17
Aric Lasry <aric@dust.tt>
2024-09-12 16:40:42 +0200

    Change the user operation logs sent on signup. (#7341)

--- front/pages/api/user/index.ts
+++ front/pages/api/user/index.ts
@@ -2,6 +2,7 @@ import type {
   UserTypeWithWorkspaces,
   WithAPIErrorResponse,
 } from "@dust-tt/types";
+import { sendUserOperationMessage } from "@dust-tt/types";
 import { isLeft } from "fp-ts/lib/Either";
 import * as t from "io-ts";
 import * as reporter from "io-ts-reporters";
@@ -81,6 +82,18 @@ async function handler(
             type: "user_not_found",
             message: "The user was not found.",
           },
+        });
+      }
+      if (user.workspaces[0]?.role === "admin") {
+        sendUserOperationMessage({
+          message: `workspace_sid: ${user.workspaces[0]?.sId}; email: [${user.email}]; User Name [${user.firstName} ${user.lastName}].`,
+          logger,
+          channel: "C075LJ6PUFQ",
+        }).catch((err) => {
+          logger.error(
+            { error: err },
+            "Failed to send user operation message to Slack."
+          );
         });
       }
 
