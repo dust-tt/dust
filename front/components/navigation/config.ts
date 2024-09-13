6bdf692f559c53d36f2cfd48aaaa1fc92a5475af
Thomas Draier <thomas@dust.tt>
2024-09-13 17:26:09 +0200

    Hide tab

--- front/components/navigation/config.ts
+++ front/components/navigation/config.ts
@@ -122,7 +122,7 @@ export const getTopNavigationTabs = (owner: WorkspaceType) => {
       ].includes(currentRoute),
   });
 
-  if (isBuilder(owner)) {
+  if (isBuilder(owner) && !owner.flags.includes("data_vaults_feature")) {
     nav.push({
       id: "assistants",
       label: "Build",
