043b4fd3eadf0b832b7ec4ef1830b19feed55d15
Philippe Rolet <pr@dust.tt>
2024-09-13 09:03:06 +0200

    [Poke] Add 'revoke invitation' button in invitations view (#7352)
    Description
    ---
    We can invite people from poke, we should be able to uninvite them too
    
    Risks
    ---
    No real risk, blast limited to poke invitation management
    
    Deploy
    ---
    front

--- front/components/poke/invitations/table.tsx
+++ front/components/poke/invitations/table.tsx
@@ -1,16 +1,45 @@
-import type { MembershipInvitationType } from "@dust-tt/types";
+import type { MembershipInvitationType, WorkspaceType } from "@dust-tt/types";
+import { useRouter } from "next/router";
 
 import { PokeDataTable } from "@app/components/poke/shadcn/ui/data_table";
 
 import { makeColumnsForInvitations } from "./columns";
 
 interface InvitationsDataTableProps {
+  owner: WorkspaceType;
   invitations: MembershipInvitationType[];
 }
 
 export function InvitationsDataTable({
+  owner,
   invitations,
 }: InvitationsDataTableProps) {
+  const router = useRouter();
+  async function onRevokeInvitation(email: string): Promise<void> {
+    if (!window.confirm(`Are you sure you want to revoke ${email}?`)) {
+      return;
+    }
+
+    try {
+      const r = await fetch(`/api/poke/workspaces/${owner.sId}/invitations`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
+      if (!r.ok) {
+        throw new Error(`Failed to revoke invitation: ${r.statusText}`);
+      }
+      router.reload();
+    } catch (e) {
+      console.error(e);
+      window.alert("An error occurred while revoking the invitation.");
+    }
+  }
+
   return (
     <>
       <div className="border-material-200 my-4 flex w-full flex-col rounded-lg border p-4">
@@ -18,7 +47,7 @@ export function InvitationsDataTable({
           <h2 className="text-md mb-4 font-bold">Pending Invitations:</h2>
         </div>
         <PokeDataTable
-          columns={makeColumnsForInvitations()}
+          columns={makeColumnsForInvitations(onRevokeInvitation)}
           data={invitations}
           defaultFilterColumn="inviteEmail"
         />
