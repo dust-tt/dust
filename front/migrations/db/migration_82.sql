00448a43a1edc035021d9e0e745bffd044f95742
Flavien David <flavien@dust.tt>
2024-09-16 20:48:32 -0700

    :see_no_evil:

--- front/migrations/db/migration_82.sql
+++ front/migrations/db/migration_82.sql
@@ -2,6 +2,10 @@
 ALTER TABLE "public"."agent_tables_query_configuration_tables"
 ADD COLUMN "dataSourceId" INTEGER REFERENCES "data_sources" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
+ALTER TABLE "public"."agent_tables_query_configuration_tables"
+ALTER COLUMN "dataSourceIdNew"
+DROP NOT NULL;
+
 UPDATE "public"."agent_tables_query_configuration_tables"
 SET
   "dataSourceId" = "dataSourceIdNew";
