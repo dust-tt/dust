335cb4096ed05f048b1cfa291698c5336dcb10b1
Thomas Draier <thomas@dust.tt>
2024-09-17 10:44:17 +0200

    open selected item

--- front/components/vaults/VaultSideBarMenu.tsx
+++ front/components/vaults/VaultSideBarMenu.tsx
@@ -259,7 +259,8 @@ const SystemVaultItem = ({
   const router = useRouter();
 
   const itemPath = `/w/${owner.sId}/vaults/${vault.sId}/categories/${category}`;
-  const isAncestorToCurrentPage = router.asPath.startsWith(itemPath + "/");
+  const isAncestorToCurrentPage =
+    router.asPath.startsWith(itemPath + "/") || router.asPath === itemPath;
 
   // Unfold the item if it's an ancestor of the current page.
   const [isExpanded, setIsExpanded] = useState(false);
@@ -336,7 +337,8 @@ const VaultMenuItem = ({
   const router = useRouter();
 
   const vaultPath = `/w/${owner.sId}/vaults/${vault.sId}`;
-  const isAncestorToCurrentPage = router.asPath.startsWith(vaultPath + "/");
+  const isAncestorToCurrentPage =
+    router.asPath.startsWith(vaultPath + "/") || router.asPath === vaultPath;
 
   // Unfold the vault if it's an ancestor of the current page.
   const [isExpanded, setIsExpanded] = useState(false);
@@ -471,9 +473,9 @@ const VaultDataSourceViewSubMenu = ({
   const router = useRouter();
 
   const vaultCategoryPath = `/w/${owner.sId}/vaults/${vault.sId}/categories/${category}`;
-  const isAncestorToCurrentPage = router.asPath.includes(
-    vaultCategoryPath + "/"
-  );
+  const isAncestorToCurrentPage =
+    router.asPath.startsWith(vaultCategoryPath + "/") ||
+    router.asPath === vaultCategoryPath;
 
   // Unfold the vault's category if it's an ancestor of the current page.
   const [isExpanded, setIsExpanded] = useState(false);
@@ -563,9 +565,9 @@ const VaultAppSubMenu = ({
   const router = useRouter();
 
   const vaultCategoryPath = `/w/${owner.sId}/vaults/${vault.sId}/categories/${category}`;
-  const isAncestorToCurrentPage = router.asPath.includes(
-    vaultCategoryPath + "/"
-  );
+  const isAncestorToCurrentPage =
+    router.asPath.startsWith(vaultCategoryPath + "/") ||
+    router.asPath === vaultCategoryPath;
 
   // Unfold the vault's category if it's an ancestor of the current page.
   const [isExpanded, setIsExpanded] = useState(false);
