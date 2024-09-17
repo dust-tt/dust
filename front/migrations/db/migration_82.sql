3cf4dbbf614caf06d2857e58698eb8ca1d996864
Flavien David <flavien@dust.tt>
2024-09-16 20:40:39 -0700

    :scissors:

--- front/migrations/db/migration_82.sql
+++ front/migrations/db/migration_82.sql
@@ -1,6 +1,6 @@
 -- Migration created on Sep 16, 2024
 ALTER TABLE "public"."agent_tables_query_configuration_tables"
-ADD COLUMN "dataSourceId" INTEGER NOT NULL REFERENCES "data_sources" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ADD COLUMN "dataSourceId" INTEGER REFERENCES "data_sources" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 UPDATE "public"."agent_tables_query_configuration_tables"
 SET
