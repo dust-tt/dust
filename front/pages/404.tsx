5374ab8966b4d50fec3398ba005a316ff742a4cb
Alban Dumouilla <alban@dust.tt>
2024-09-20 09:41:23 +0200

    Adding attempted URL in 404 (#7557)

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -1,7 +1,17 @@
 import { Button, LogoutIcon } from "@dust-tt/sparkle";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Custom404() {
+  const [currentURL, setCurrentURL] = useState("");
+
+  useEffect(() => {
+    // Only set the URL on the client-side
+    if (typeof window !== "undefined") {
+      setCurrentURL(window.location.href);
+    }
+  }, []);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col gap-3 text-center">
@@ -12,9 +22,14 @@ export default function Custom404() {
           <p className="text-xl font-bold leading-7 text-slate-900">
             404: Page not found
           </p>
-          <p className="max-w-48 text-sm font-normal leading-tight text-slate-500">
+          <p className="text-sm font-normal leading-tight text-slate-500">
             Looks like this page took an unscheduled coffee break.
           </p>
+          {currentURL && (
+            <p className="mt-2 text-xs text-slate-400">
+              Attempted URL: {currentURL}
+            </p>
+          )}
         </div>
         <Link href="/">
           <Button
