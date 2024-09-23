ec3b42afb27556589479b7a44cd1ecf3b7d0d6f4
Flavien David <flavien@dust.tt>
2024-09-23 11:03:38 +0200

    Backfill `editedByUserId`

--- front/migrations/db/migration_86.sql
+++ front/migrations/db/migration_86.sql
@@ -14,6 +14,14 @@ UPDATE "data_source_views"
 SET "editedAt" = "updatedAt"
 WHERE "editedAt" IS NULL;
 
+UPDATE "data_source_views"
+SET
+    "editedByUserId" = "data_sources"."editedByUserId"
+FROM "data_sources"
+WHERE
+    "data_sources"."id" = "data_source_views"."dataSourceId"
+    AND "data_source_views"."editedByUserId" IS NULL;
+
 ALTER TABLE "data_source_views" ALTER COLUMN "editedAt" SET NOT NULL;
 ALTER TABLE "data_source_views" ALTER COLUMN "editedAt" DROP DEFAULT;
 ALTER TABLE "data_source_views"
