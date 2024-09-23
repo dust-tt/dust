6dada77c670285d72495bee7031981d0c5a3cf67
Flavien David <flavien@dust.tt>
2024-09-23 11:00:26 +0200

    Add migration

--- front/migrations/db/migration_86.sql
+++ front/migrations/db/migration_86.sql
@@ -1,29 +1,22 @@
 -- -- This migration is dependant on a backfill script
--- -- The backfill script is: 20240912_backfill_editedbyuser_id
+-- -- The backfill script is: <insert_script>
 -- -- run psql with --set=backfilled=1 argument if you have rune the script.
 
 CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
 RETURNS VARCHAR AS $$
 BEGIN
     IF NOT backfilled THEN
-        RAISE NOTICE 'The backfill script: 20240912_backfill_editedbyuser_id is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.';
+        RAISE NOTICE 'The backfill script: <insert_script> is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.';
     END IF;
 
 -- Migration created on Sep 23, 2024
-UPDATE "data_sources"
+UPDATE "data_source_views"
 SET "editedAt" = "updatedAt"
 WHERE "editedAt" IS NULL;
 
-ALTER TABLE "data_sources"
-ALTER COLUMN "editedAt"
-SET
-  NOT NULL;
-
-ALTER TABLE "data_sources"
-ALTER COLUMN "editedAt"
-DROP DEFAULT;
-
-ALTER TABLE "data_sources"
+ALTER TABLE "data_source_views" ALTER COLUMN "editedAt" SET NOT NULL;
+ALTER TABLE "data_source_views" ALTER COLUMN "editedAt" DROP DEFAULT;
+ALTER TABLE "data_source_views"
 ALTER COLUMN "editedByUserId"
 SET
   NOT NULL;
@@ -36,7 +29,7 @@ $$ LANGUAGE plpgsql;
    SELECT perform_migration(:'backfilled'::boolean);
 \else
     \echo '!! Migration was NOT applied !!'
-    \echo 'The backfill script: 20240912_backfill_editedbyuser_id is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.'
+    \echo 'The backfill script: <insert_script> is required before applying this migation. If you already did it, run psql with --set=backfilled=1 argument.'
 \endif
 
 DROP FUNCTION perform_migration(boolean);
