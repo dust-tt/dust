156129e47caedb55c07629d0388cd82573569663
Henry Fontanier <h.fontanier@gmail.com>
2024-09-24 15:36:38 +0200

    feat: limit snowflake query result size (#7651)
    * feat: limit snowflake query result size
    
    * handle timeouts
    
    ---------
    
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- core/src/databases/database.rs
+++ core/src/databases/database.rs
@@ -35,6 +35,8 @@ pub enum QueryDatabaseError {
     GenericError(#[from] anyhow::Error),
     #[error("Too many result rows")]
     TooManyResultRows,
+    #[error("Result is too large: {0}")]
+    ResultTooLarge(String),
     #[error("Query execution error: {0}")]
     ExecutionError(String),
 }
