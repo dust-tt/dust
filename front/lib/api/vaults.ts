781d293752b067640577a7f38d59b666db29587a
Thomas Draier <thomas@dust.tt>
2024-09-24 15:37:07 +0200

    Fix vault deletion

--- front/lib/api/vaults.ts
+++ front/lib/api/vaults.ts
@@ -2,6 +2,7 @@ import type { DataSourceWithAgentsUsageType } from "@dust-tt/types";
 import { uniq } from "lodash";
 
 import type { Authenticator } from "@app/lib/auth";
+import { DataSourceResource } from "@app/lib/resources/data_source_resource";
 import { DataSourceViewResource } from "@app/lib/resources/data_source_view_resource";
 import { frontSequelize } from "@app/lib/resources/storage";
 import type { VaultResource } from "@app/lib/resources/vault_resource";
@@ -28,10 +29,21 @@ export const deleteVault = async (
       usages.push(usage.value);
     }
   }
+
+  const dataSources = await DataSourceResource.listByVault(auth, vault);
+  for (const ds of dataSources) {
+    const usage = await ds.getUsagesByAgents(auth);
+    if (usage.isErr()) {
+      throw usage.error;
+    } else if (usage.value.count > 0) {
+      usages.push(usage.value);
+    }
+  }
+
   if (usages.length > 0) {
     const agentNames = uniq(usages.map((u) => u.agentNames).flat());
     throw new Error(
-      `Cannot delete vault with data source views in use by assistant(s): ${agentNames.join(", ")}.`
+      `Cannot delete vault with data source in use by assistant(s): ${agentNames.join(", ")}.`
     );
   }
 
@@ -39,6 +51,13 @@ export const deleteVault = async (
     // delete all data source views
     for (const view of dataSourceViews) {
       const res = await view.delete(auth, t);
+      if (res.isErr()) {
+        throw res.error;
+      }
+    }
+
+    for (const ds of dataSources) {
+      const res = await ds.delete(auth, t);
       if (res.isErr()) {
         throw res.error;
       }
