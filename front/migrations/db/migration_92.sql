859f5ee533df161902aca534d78b939da5b7eaba
Flavien David <flavien@dust.tt>
2024-09-25 17:10:15 +0200

    Migration

--- front/migrations/db/migration_92.sql
+++ front/migrations/db/migration_92.sql
@@ -1,2 +1,10 @@
 -- Migration created on Sep 25, 2024
-ALTER TABLE "public"."data_sources" ADD COLUMN "deletedAt" TIMESTAMP WITH TIME ZONE;
+ALTER TABLE "public"."data_sources"
+ADD COLUMN "deletedAt" TIMESTAMP
+WITH
+  TIME ZONE;
+
+ALTER TABLE "public"."data_source_views"
+ADD COLUMN "deletedAt" TIMESTAMP
+WITH
+  TIME ZONE;
