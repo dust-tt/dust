dc6dbc1ab5a44e38641d6aea18f42ebfc7f00b78
Stanislas Polu <spolu@dust.tt>
2024-09-25 15:51:44 +0200

    Use dataSource.sId in Vaults/DataSourceView POST/PATCH routes (#7685)
    * post data_source_view uses dataSourceId
    
    * clean-up

--- types/src/front/api_handlers/public/vaults.ts
+++ types/src/front/api_handlers/public/vaults.ts
@@ -3,14 +3,12 @@ import * as t from "io-ts";
 import { ContentNodeType } from "../../lib/connectors_api";
 
 export const ContentSchema = t.type({
-  // TODO(DATASOURCE_SID): rename to `dataSourceId`
-  dataSource: t.string,
+  dataSourceId: t.string,
   parentsIn: t.union([t.array(t.string), t.null]),
 });
 
 export const PostDataSourceViewSchema = t.type({
-  // TODO(DATASOURCE_SID): rename to `dataSourceId`
-  name: t.string,
+  dataSourceId: t.string,
   parentsIn: t.union([t.array(t.string), t.null]),
 });
 
