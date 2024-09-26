9faa941b38d0e5c1d946e8e1a1d60f96421e46bf
Flavien David <flavien@dust.tt>
2024-09-26 09:36:43 +0200

    Migration

--- front/migrations/db/migration_92.sql
+++ front/migrations/db/migration_92.sql
@@ -4,7 +4,16 @@ ADD COLUMN "deletedAt" TIMESTAMP
 WITH
   TIME ZONE;
 
+CREATE UNIQUE INDEX CONCURRENTLY "data_sources_workspace_id_name_deleted_at" ON "data_sources" ("workspaceId", "name", "deletedAt");
+
 ALTER TABLE "public"."data_source_views"
 ADD COLUMN "deletedAt" TIMESTAMP
 WITH
   TIME ZONE;
+
+CREATE UNIQUE INDEX CONCURRENTLY "data_source_views_workspace_id_data_source_id_vault_id_deleted_at" ON "data_source_views" (
+  "workspaceId",
+  "dataSourceId",
+  "vaultId",
+  "deletedAt"
+);
