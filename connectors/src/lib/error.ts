d3ba78fcfae6c2035c7bc00fef024c6aae398f76
filou <philipperolet@gmail.com>
2024-09-27 14:46:06 +0200

    [Microsoft] Move Application disabled error to oauth
    Description
    ---
    This kind of error should be handled in oauth.
    
    Risks
    ---
    na
    
    Deploy
    ---
    - core
    - connectors

--- connectors/src/lib/error.ts
+++ connectors/src/lib/error.ts
@@ -1,10 +1,6 @@
 // JS cannot give you any guarantee about the shape of an error you `catch`
 
-import type {
-  APIError,
-  ConnectorProvider,
-  OAuthAPIError,
-} from "@dust-tt/types";
+import type { APIError, ConnectorProvider } from "@dust-tt/types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function errorFromAny(e: any): Error {
@@ -74,17 +70,6 @@ export class ExternalOAuthTokenError extends Error {
 
 export function isNotFoundError(err: unknown): err is NotFoundError {
   return err instanceof HTTPError && err.statusCode === 404;
-}
-
-export function isMicrosoftApplicationDisabledError(
-  error: OAuthAPIError,
-  provider: string
-): boolean {
-  return (
-    error.code === "provider_access_token_refresh_error" &&
-    provider === "microsoft" &&
-    /Application.*is disabled/.test(error.message)
-  );
 }
 
 // Error for invalid rows when upserting table
