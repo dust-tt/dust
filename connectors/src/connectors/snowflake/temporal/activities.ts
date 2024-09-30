d0095340b66ac40a105e3e9e222f2f2f5beed29d
Henry Fontanier <h.fontanier@gmail.com>
2024-09-30 09:59:26 +0200

    feat(snowflake connector): temporal scaffolding (#7741)
    * feat(snowflake connector): temporal scaffolding
    
    * add signal setup
    
    ---------
    
    Co-authored-by: Henry Fontanier <henry@dust.tt>

---
+++ connectors/src/connectors/snowflake/temporal/activities.ts
@@ -0,0 +1,7 @@
+export async function syncSnowflakeConnection(connectorId: number) {
+  // TODO(SNOWFLAKE)
+  // do SHOW TABLES on the Snowflake connection
+  // look at DBs / Schemas / Tables with explicit read permissions
+  // garbage collect / add missing resources as needed
+  void connectorId;
+}
