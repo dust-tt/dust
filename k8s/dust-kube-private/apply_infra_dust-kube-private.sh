b2df6fa17cac8f15e1515c7bfb81a1f6658ef68f
Sebastien Flory <seb@dust.tt>
2024-09-30 13:34:17 +0200

    Add private cluster with socks-proxy in addition to existing public cluster (#7751)
    * Add private cluster with socks-proxy in addition to existing public cluster.
    
    * Review fdbk

---
+++ k8s/dust-kube-private/apply_infra_dust-kube-private.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../utils.sh"
+
+# By convention, the name of the folder enclosing this script is the cluster name
+CLUSTER_NAME=$(basename $(dirname "$0"))
+
+check_context $CLUSTER_NAME
+
+install_datadog_agent
+
+echo "-----------------------------------"
+echo "Applying configmaps"
+echo "-----------------------------------"
+
+kubectl apply -f "$(dirname "$0")/configmaps/socks-proxy-configmap.yaml"
+
+echo "-----------------------------------"
+echo "Applying deployments"
+echo "-----------------------------------"
+
+apply_deployment socks-proxy-deployment
+
+echo "-----------------------------------"
+echo "Applying network policies"
+echo "-----------------------------------"
+
+# kubectl apply -f "$(dirname "$0")/network-policies/allow-same-namespace.yaml"
+# kubectl apply -f "$(dirname "$0")/network-policies/default-deny-ingress.yaml"
