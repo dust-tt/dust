c9eea876d831d6d65c75d93f1af0943e7555bfb3
Jules Belveze <32683010+JulesBelveze@users.noreply.github.com>
2024-10-01 14:17:10 +0200

    Revert "Revert "[front] - enh(DataSourceViewSelector): disable root node chec…"
    This reverts commit 3930ce443e7e45cff64be3601c2b7e87f80ba4cc.

--- front/pages/poke/[wId]/vaults/[vId]/data_source_views/[dsvId]/index.tsx
+++ front/pages/poke/[wId]/vaults/[vId]/data_source_views/[dsvId]/index.tsx
@@ -55,6 +55,7 @@ export default function DataSourceViewPage({
           setSelectionConfigurations={() => {}}
           useContentNodes={usePokeDataSourceViewContentNodes}
           viewType="documents"
+          isRootSelectable={true}
         />
       </div>
     </div>
