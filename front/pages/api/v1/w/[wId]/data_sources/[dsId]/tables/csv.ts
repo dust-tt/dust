d89b5d26a24b3f2bf307d45ef2d98bb004c6029b
Stanislas Polu <spolu@dust.tt>
2024-10-01 16:46:30 +0200

    fix size limits

--- front/pages/api/v1/w/[wId]/data_sources/[dsId]/tables/csv.ts
+++ front/pages/api/v1/w/[wId]/data_sources/[dsId]/tables/csv.ts
@@ -1,5 +1,13 @@
 import handler from "@app/pages/api/v1/w/[wId]/vaults/[vId]/data_sources/[dsId]/tables/csv";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb",
+    },
+  },
+};
+
 /**
  * @ignoreswagger
  * Legacy endpoint. Still relied on by connectors.
