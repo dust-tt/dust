a4a407cade57a4dea5ab143dd76dd6c252823635
Stanislas Polu <spolu@dust.tt>
2024-10-01 16:51:48 +0200

    fix size limits (#7799)

--- front/pages/api/v1/w/[wId]/data_sources/[dsId]/documents/[documentId]/index.ts
+++ front/pages/api/v1/w/[wId]/data_sources/[dsId]/documents/[documentId]/index.ts
@@ -1,5 +1,13 @@
 import handler from "@app/pages/api/v1/w/[wId]/vaults/[vId]/data_sources/[dsId]/documents/[documentId]/index";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "8mb",
+    },
+  },
+};
+
 /**
  * @ignoreswagger
  * Legacy endpoint. Still relied on by connectors.
