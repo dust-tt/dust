71b452486e0f28158392def00c45e9d8d26ef757
Flavien David <flavien@dust.tt>
2024-10-02 12:18:21 +0200

    Re-queue to new queue

--- front/temporal/upsert_queue/activities.ts
+++ front/temporal/upsert_queue/activities.ts
@@ -11,10 +11,12 @@ import type { WorkflowError } from "@app/lib/temporal_monitoring";
 import {
   EnqueueUpsertDocument,
   EnqueueUpsertTable,
+  enqueueUpsertTable,
   runPostUpsertHooks,
 } from "@app/lib/upsert_queue";
 import mainLogger from "@app/logger/logger";
 import { statsDClient } from "@app/logger/withlogging";
+import { launchUpsertTableWorkflow } from "@app/temporal/upsert_tables/client";
 
 const { DUST_UPSERT_QUEUE_BUCKET, SERVICE_ACCOUNT } = process.env;
 
@@ -189,101 +191,12 @@ export async function upsertTableActivity(
     tableId: upsertQueueItem.tableId,
   });
 
-  const auth = await Authenticator.internalAdminForWorkspace(
-    upsertQueueItem.workspaceId
-  );
-
-  const owner = auth.workspace();
-  if (!owner) {
-    logger.error(
-      {
-        delaySinceEnqueueMs: Date.now() - enqueueTimestamp,
-      },
-      "[UpsertQueue] Giving up: Workspace not found"
-    );
-    return;
-  }
-
-  const dataSource = await DataSourceResource.fetchById(
-    auth,
-    upsertQueueItem.dataSourceId
-  );
-  if (!dataSource) {
-    // If the data source was not found, we simply give up and remove the item from the queue as it
-    // means that the data source was deleted.
-    logger.info(
-      {
-        delaySinceEnqueueMs: Date.now() - enqueueTimestamp,
-      },
-      "[UpsertQueue] Giving up: DataSource not found"
-    );
-    return;
-  }
-
-  const statsDTags = [
-    `data_source_name:${dataSource.name}`,
-    `workspace_id:${upsertQueueItem.workspaceId}`,
-  ];
-
-  const upsertTimestamp = Date.now();
-
-  const tableRes = await upsertTableFromCsv({
-    auth,
-    dataSource,
-    tableName: upsertQueueItem.tableName,
-    tableDescription: upsertQueueItem.tableDescription,
-    tableId: upsertQueueItem.tableId,
-    tableTimestamp: upsertQueueItem.tableTimestamp ?? null,
-    tableTags: upsertQueueItem.tableTags || [],
-    tableParents: upsertQueueItem.tableParents || [],
-    csv: upsertQueueItem.csv,
-    truncate: upsertQueueItem.truncate,
-    useAppForHeaderDetection: upsertQueueItem.useAppForHeaderDetection ?? false,
+  await launchUpsertTableWorkflow({
+    workspaceId: upsertQueueItem.workspaceId,
+    dataSourceId: upsertQueueItem.dataSourceId,
+    upsertQueueId,
+    enqueueTimestamp,
   });
 
-  if (tableRes.isErr()) {
-    logger.error(
-      {
-        error: tableRes.error,
-        latencyMs: Date.now() - upsertTimestamp,
-        delaySinceEnqueueMs: Date.now() - enqueueTimestamp,
-        csvSize: upsertQueueItem.csv?.length || 0,
-      },
-      "[UpsertQueue] Failed table upsert"
-    );
-    statsDClient.increment("upsert_queue_table_error.count", 1, statsDTags);
-    statsDClient.distribution(
-      "upsert_queue_upsert_table_error.duration.distribution",
-      Date.now() - upsertTimestamp,
-      []
-    );
-
-    const error: WorkflowError = {
-      __is_dust_error: true,
-      message: `Upsert error: ${JSON.stringify(tableRes.error)}`,
-      type: "upsert_queue_upsert_table_error",
-    };
-
-    throw error;
-  }
-
-  logger.info(
-    {
-      latencyMs: Date.now() - upsertTimestamp,
-      delaySinceEnqueueMs: Date.now() - enqueueTimestamp,
-      csvSize: upsertQueueItem.csv?.length || 0,
-    },
-    "[UpsertQueue] Successful table upsert"
-  );
-  statsDClient.increment("upsert_queue_table_success.count", 1, statsDTags);
-  statsDClient.distribution(
-    "upsert_queue_upsert_table_success.duration.distribution",
-    Date.now() - upsertTimestamp,
-    []
-  );
-  statsDClient.distribution(
-    "upsert_queue_table.duration.distribution",
-    Date.now() - enqueueTimestamp,
-    []
-  );
+  logger.info({}, "[UpsertQueue] Re-enqueue in new queue");
 }
