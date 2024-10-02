e06e105f13fd88c395e8eb257123b4a7c6becad1
Thomas Draier <thomas@dust.tt>
2024-10-02 10:49:55 +0200

    [front] Add log for upserted table size (#7810)

--- front/lib/api/tables.ts
+++ front/lib/api/tables.ts
@@ -244,6 +244,7 @@ export async function upsertTableFromCsv({
   }
 
   if (csvRows) {
+    const now = performance.now();
     const rowsRes = await coreAPI.upsertTableRows({
       projectId: dataSource.dustAPIProjectId,
       dataSourceId: dataSource.dustAPIDataSourceId,
@@ -251,6 +252,18 @@ export async function upsertTableFromCsv({
       rows: csvRows,
       truncate,
     });
+
+    logger.info(
+      {
+        durationMs: performance.now() - now,
+        csvRowsLength: csvRows.length,
+        csvColsLength: Object.keys(csvRows[0]?.value)?.length,
+        workspaceId: owner.id,
+        tableId,
+        tableName,
+      },
+      "Upserting table rows"
+    );
 
     if (rowsRes.isErr()) {
       const errorDetails = {
