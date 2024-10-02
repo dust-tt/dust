ded7151d5090875c90811a31365b3b6faeb39fe3
Flavien David <flavien.david74@gmail.com>
2024-10-02 13:47:23 +0200

    Clean up + fix upsert table (#7822)

--- front/temporal/upsert_queue/workflows.ts
+++ front/temporal/upsert_queue/workflows.ts
@@ -6,21 +6,9 @@ const { upsertDocumentActivity } = proxyActivities<typeof activities>({
   startToCloseTimeout: "10 minutes",
 });
 
-const { upsertTableActivity } = proxyActivities<typeof activities>({
-  startToCloseTimeout: "20 minutes",
-});
-
 export async function upsertDocumentWorkflow(
   upsertQueueId: string,
   enqueueTimestamp: number
 ) {
   await upsertDocumentActivity(upsertQueueId, enqueueTimestamp);
-}
-
-// TODO(2024-10-02 flav) Removed once all the upsert tables have been processed from this queue.
-export async function upsertTableWorkflow(
-  upsertQueueId: string,
-  enqueueTimestamp: number
-) {
-  await upsertTableActivity(upsertQueueId, enqueueTimestamp);
 }
