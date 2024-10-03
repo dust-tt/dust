98977b57f8a7458dbf64820159cffdfe2c7f9aa7
Flavien David <flavien.david74@gmail.com>
2024-10-03 11:46:03 +0200

    Fix Assistant Builder selection (#7856)
    * Fix Assistant Builder selection
    
    * :book:
    
    * :sparkles:

--- front/components/data_source_view/DataSourceViewSelector.tsx
+++ front/components/data_source_view/DataSourceViewSelector.tsx
@@ -411,18 +411,28 @@ export function DataSourceViewSelector({
                 checked: checkedStatus,
                 onChange: () => {
                   setSelectionConfigurations((prevState) => {
-                    const prevSelectionConfiguration =
-                      prevState[dataSourceView.sId] ??
+                    const { sId } = dataSourceView;
+                    const defaultConfig =
                       defaultSelectionConfiguration(dataSourceView);
-                    const udpatedConfig = {
-                      ...prevSelectionConfiguration,
+                    const prevConfig = prevState[sId] || defaultConfig;
+
+                    const updatedConfig = {
+                      ...prevConfig,
                       selectedResources: [],
-                      isSelectAll: checkedStatus !== "checked",
+                      isSelectAll: checkedStatus === "unchecked",
                     };
+
+                    // If nothing is selected and selectAll is false, remove the entry from the state.
+                    if (
+                      !updatedConfig.selectedResources.length &&
+                      !updatedConfig.isSelectAll
+                    ) {
+                      return _.omit(prevState, sId);
+                    }
 
                     return {
                       ...prevState,
-                      [dataSourceView.sId]: udpatedConfig,
+                      [sId]: updatedConfig,
                     };
                   });
                 },
