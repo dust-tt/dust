9e4117a036eaf1d1f6f799946b62fe0435bbb18c
Stanislas Polu <spolu@dust.tt>
2024-10-04 16:28:09 +0200

    Move documentation of Apps under vaults path (#7898)
    * Update apps swagger
    
    * swagger update

--- front/pages/api/v1/w/[wId]/apps/[aId]/runs/[runId]/index.ts
+++ front/pages/api/v1/w/[wId]/apps/[aId]/runs/[runId]/index.ts
@@ -7,44 +7,7 @@ export const config = {
 };
 
 /**
- * @swagger
- * /api/v1/w/{wId}/apps/{aId}/runs/{runId}:
- *   get:
- *     summary: Get an app run
- *     description: Retrieve a run for an app in the workspace identified by {wId}.
- *     tags:
- *       - Apps
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: wId
- *         required: true
- *         description: Unique string identifier for the workspace
- *         schema:
- *           type: string
- *       - in: path
- *         name: aId
- *         required: true
- *         description: ID of the app
- *         schema:
- *           type: string
- *       - in: path
- *         name: runId
- *         required: true
- *         description: ID of the run
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: The run
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 run:
- *                   $ref: '#/components/schemas/Run'
+ * @ignoreswagger
+ * Legacy endpoint.
  */
-
 export default handler;
--- front/pages/api/v1/w/[wId]/vaults/[vId]/apps/[aId]/runs/[runId]/index.ts
+++ front/pages/api/v1/w/[wId]/vaults/[vId]/apps/[aId]/runs/[runId]/index.ts
@@ -24,6 +24,53 @@ export type GetRunResponseBody = {
   run: RunType;
 };
 
+/**
+ * @swagger
+ * /api/v1/w/{wId}/vaults/{vId}/apps/{aId}/runs/{runId}:
+ *   get:
+ *     summary: Get an app run
+ *     description: Retrieve a run for an app in the vault identified by {vId}.
+ *     tags:
+ *       - Apps
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: wId
+ *         required: true
+ *         description: Unique string identifier for the workspace
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: vId
+ *         required: true
+ *         description: ID of the vault
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: aId
+ *         required: true
+ *         description: ID of the app
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: runId
+ *         required: true
+ *         description: ID of the run
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The run
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 run:
+ *                   $ref: '#/components/schemas/Run'
+ */
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<WithAPIErrorResponse<GetRunResponseBody>>,
