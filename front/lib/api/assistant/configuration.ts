a24b6d3951c26e92ef33bba6e77da910ca2932ba
Stanislas Polu <spolu@dust.tt>
2024-10-08 05:56:33 +0200

    nit

--- front/lib/api/assistant/configuration.ts
+++ front/lib/api/assistant/configuration.ts
@@ -591,11 +591,13 @@ export async function getAgentConfigurations<V extends "light" | "full">({
     throw new Error("Archived view is for dust superusers only.");
   }
 
-  if (agentsGetView === "list" && !user) {
-    throw new Error("`list` view is specific to a user.");
-  }
-  if (agentsGetView === "assistants-search" && !user) {
-    throw new Error("`assistant-search` view is specific to a user.");
+  if (
+    (agentsGetView === "list" || agentsGetView === "assistants-search") &&
+    !user
+  ) {
+    throw new Error(
+      "`list` or `assistants-search` view is specific to a user."
+    );
   }
 
   const applySortAndLimit = makeApplySortAndLimit(sort, limit);
