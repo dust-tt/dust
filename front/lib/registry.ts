ec5105f0453bca963e45d3421cc6a14288d3022a
Henry Fontanier <henry@dust.tt>
2024-10-08 13:43:46 +0200

    enh: add thinking param again

--- front/lib/registry.ts
+++ front/lib/registry.ts
@@ -158,7 +158,7 @@ export const DustProdActionRegistry = createActionRegistry({
       workspaceId: PRODUCTION_DUST_APPS_WORKSPACE_ID,
       appId: "b4f205e453",
       appHash:
-        "0dce3a86731ec21d70a7a92b81bf66aca7bca5aa7ff26137fb7b4962a870ef35",
+        "2f595021c892daff7ec6b208602ebedb2cd6ad01680d59515a2405496d5af369",
       appVaultId: PRODUCTION_DUST_APPS_VAULT_ID,
     },
     config: {
