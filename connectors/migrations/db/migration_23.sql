cc94e1ae8bda2b252c3293c70e07291fdec404e8
Flavien David <flavien@dust.tt>
2024-10-09 13:56:17 +0200

    :scissors:

--- connectors/migrations/db/migration_23.sql
+++ connectors/migrations/db/migration_23.sql
@@ -1,11 +1,3 @@
 -- Migration created on Oct 09, 2024
 ALTER TABLE "notion_connector_block_cache_entries"
-ALTER COLUMN "childDatabaseTitle"
-DROP NOT NULL;
-
-ALTER TABLE "notion_connector_block_cache_entries"
-ALTER COLUMN "childDatabaseTitle"
-DROP DEFAULT;
-
-ALTER TABLE "notion_connector_block_cache_entries"
 ALTER COLUMN "childDatabaseTitle" TYPE TEXT;
