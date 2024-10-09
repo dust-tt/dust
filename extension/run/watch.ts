df94f6da012202aa1a6b5bb66212940a21cc0720
Daphné Popin <daphne.popin@gmail.com>
2024-10-09 12:20:16 +0200

    Extension Env var

--- extension/run/watch.ts
+++ extension/run/watch.ts
@@ -1,10 +1,8 @@
 import webpack from "webpack";
-import { getEnv } from "../config/env";
 import { getConfig } from "../config/webpack";
 
 async function main() {
-  const environment = getEnv();
-  const config = await getConfig({ shouldBuild: "none" });
+  const config = await getConfig({ env: "development", shouldBuild: "none" });
   const compiler = webpack(config);
   compiler.watch({ ignored: /node_modules/ }, async (err, res) => {
     if (err) {
@@ -13,9 +11,7 @@ async function main() {
     if (res?.hasErrors) {
       console.error(res.compilation.errors);
     }
-    console.log(
-      `[Dust Extension][${environment}] Webpack successfully compiled.`
-    );
+    console.log(`[Dust Extension][development] Webpack successfully compiled.`);
   });
 }
 
