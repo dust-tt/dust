5e064724fc43436dd0c384cdfbf69944f1f0f091
Thomas Draier <thomas@dust.tt>
2024-10-09 14:30:11 +0200

    Add lint config

--- extension/app/background.ts
+++ extension/app/background.ts
@@ -1,3 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+  void chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
--- extension/app/main.tsx
+++ extension/app/main.tsx
@@ -7,6 +7,7 @@ import "./src/css/components.css";
 
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { routes } from "./src/routes";
 
 const router = createBrowserRouter(routes);
