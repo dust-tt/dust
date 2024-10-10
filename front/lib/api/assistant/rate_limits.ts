4d61d2f274a1a93958ed873f6958d9999a6fc787
Flavien David <flavien@dust.tt>
2024-10-10 10:53:07 +0200

    :sparkles:

--- front/lib/api/assistant/rate_limits.ts
+++ front/lib/api/assistant/rate_limits.ts
@@ -2,8 +2,8 @@ import type {
   LightWorkspaceType,
   MaxMessagesTimeframeType,
 } from "@dust-tt/types";
+import { expireRateLimiterKey } from "@dust-tt/types";
 
-import { runOnRedis } from "@app/lib/api/redis";
 import type { Authenticator } from "@app/lib/auth";
 
 export const makeMessageRateLimitKeyForWorkspace = (
@@ -20,17 +20,17 @@ export const makeAgentMentionsRateLimitKeyForWorkspace = (
 };
 
 export async function resetWorkspaceRateLimit(auth: Authenticator) {
-  await runOnRedis(async (redis) => {
-    const workspace = auth.getNonNullableWorkspace();
-    const plan = auth.getNonNullablePlan();
+  const workspace = auth.getNonNullableWorkspace();
+  const plan = auth.getNonNullablePlan();
 
-    await redis.expire(makeMessageRateLimitKeyForWorkspace(workspace), 0);
-    await redis.expire(
-      makeAgentMentionsRateLimitKeyForWorkspace(
-        workspace,
-        plan.limits.assistant.maxMessagesTimeframe
-      ),
-      0
-    );
+  await expireRateLimiterKey({
+    key: makeMessageRateLimitKeyForWorkspace(workspace),
+  });
+
+  await expireRateLimiterKey({
+    key: makeAgentMentionsRateLimitKeyForWorkspace(
+      workspace,
+      plan.limits.assistant.maxMessagesTimeframe
+    ),
   });
 }
