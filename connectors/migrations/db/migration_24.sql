a20f2708cf62bbe593c2145d2a53799d1c334991
Flavien David <flavien@dust.tt>
2024-10-11 11:40:44 +0200

    Add migration

--- connectors/migrations/db/migration_24.sql
+++ connectors/migrations/db/migration_24.sql
@@ -1,2 +1,4 @@
 -- Migration created on Oct 11, 2024
 CREATE UNIQUE INDEX "webcrawler_pages_connector_id_document_id" ON "webcrawler_pages" ("connectorId", "documentId");
+
+CREATE UNIQUE INDEX "webcrawler_folders_connector_id_internal_id" ON "webcrawler_folders" ("connectorId", "internalId");
