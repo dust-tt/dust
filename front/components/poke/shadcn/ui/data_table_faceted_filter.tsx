ce1b00e98d7ed83a9f8b669f94806c54e8e8f484
Philippe Rolet <pr@dust.tt>
2024-10-11 22:44:31 +0200

    Fix: linting errors from #8010 (#8022)

--- front/components/poke/shadcn/ui/data_table_faceted_filter.tsx
+++ front/components/poke/shadcn/ui/data_table_faceted_filter.tsx
@@ -115,7 +115,7 @@ export function PokeDataTableFacetedFilter<TData, TValue>({
                       <CheckIcon className={cn("h-4 w-4")} />
                     </div>
                     {option.icon && (
-                      <option.icon className="text-muted-foreground mr-2 h-4 w-4" />
+                      <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
                     )}
                     <span>{option.label}</span>
                     {facets?.get(option.value) && (
