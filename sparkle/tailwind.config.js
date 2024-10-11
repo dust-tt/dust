e3229aa5b730896496c3be6fde96d6aad74ffcc3
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-10-11 14:50:49 +0200

    bumping

--- sparkle/tailwind.config.js
+++ sparkle/tailwind.config.js
@@ -167,7 +167,7 @@ module.exports = {
       colors: {
         separator: {
           DEFAULT: colors.slate[200],
-          dark: colors.slate[800]
+          dark: colors.slate[800],
         },
         border: {
           DEFAULT: colors.slate[100],
@@ -179,15 +179,15 @@ module.exports = {
         },
         background: {
           DEFAULT: colors.white,
-          dark: colors.slate[950]
+          dark: colors.slate[950],
         },
         foreground: {
           DEFAULT: colors.slate[950],
-          dark: colors.white
+          dark: colors.white,
         },
         muted: {
           DEFAULT: colors.slate[50],
-          dark: colors.slate[900]
+          dark: colors.slate[900],
         },
         "muted-foreground": {
           DEFAULT: colors.slate[500],
@@ -211,17 +211,17 @@ module.exports = {
           50: { DEFAULT: colors.blue[50], dark: colors.blue[950] },
         },
         primary: {
-          950: { DEFAULT: colors.slate[950], dark: colors.slate[50] },
-          900: { DEFAULT: colors.slate[900], dark: colors.slate[100] },
-          800: { DEFAULT: colors.slate[800], dark: colors.slate[200] },
-          700: { DEFAULT: colors.slate[700], dark: colors.slate[300] },
-          600: { DEFAULT: colors.slate[600], dark: colors.slate[400] },
-          500: { DEFAULT: colors.slate[500], dark: colors.slate[500] },
-          400: { DEFAULT: colors.slate[400], dark: colors.slate[600] },
-          300: { DEFAULT: colors.slate[300], dark: colors.slate[700] },
-          200: { DEFAULT: colors.slate[200], dark: colors.slate[800] },
-          100: { DEFAULT: colors.slate[100], dark: colors.slate[900] },
-          50: { DEFAULT: colors.slate[50], dark: colors.slate[950] },
+          950: { DEFAULT: colors.blue[950], dark: colors.blue[50] },
+          900: { DEFAULT: colors.blue[900], dark: colors.blue[100] },
+          800: { DEFAULT: colors.blue[800], dark: colors.blue[200] },
+          700: { DEFAULT: colors.blue[700], dark: colors.blue[300] },
+          600: { DEFAULT: colors.blue[600], dark: colors.blue[400] },
+          500: { DEFAULT: colors.blue[500], dark: colors.blue[500] },
+          400: { DEFAULT: colors.blue[400], dark: colors.blue[600] },
+          300: { DEFAULT: colors.blue[300], dark: colors.blue[700] },
+          200: { DEFAULT: colors.blue[200], dark: colors.blue[800] },
+          100: { DEFAULT: colors.blue[100], dark: colors.blue[900] },
+          50: { DEFAULT: colors.blue[50], dark: colors.blue[950] },
         },
         action: {
           950: { DEFAULT: colors.blue[950], dark: colors.blue[50] },
