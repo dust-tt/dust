531a7d71d289bbc82c726c0a769da91a99c1fa0d
Aubin <aubin@dust.tt>
2024-10-14 13:52:35 +0200

    mergefix

--- front/components/assistant/AssistantBrowser.tsx
+++ front/components/assistant/AssistantBrowser.tsx
@@ -158,7 +158,6 @@ export function AssistantBrowser({
                   </div>
                 </Link>
               }
-              triggerIsButton
             />
             {isBuilder && (
               <Tooltip
