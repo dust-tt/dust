401e165d96fcc81119ef7c12f4ad777fe0abdb37
Aubin <aubin@dust.tt>
2024-10-21 18:19:22 +0200

    feat: add data model for the help center part (Brand, Category, Article)

--- connectors/src/admin/db.ts
+++ connectors/src/admin/db.ts
@@ -61,7 +61,7 @@ import {
   WebCrawlerFolder,
   WebCrawlerPage,
 } from "@connectors/lib/models/webcrawler";
-import { ZendeskConfiguration } from "@connectors/lib/models/zendesk";
+import { ZendeskArticle,ZendeskBrand, ZendeskConfiguration } from "@connectors/lib/models/zendesk";
 import logger from "@connectors/logger/logger";
 import { sequelizeConnection } from "@connectors/resources/storage";
 import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
@@ -115,6 +115,8 @@ async function main(): Promise<void> {
   await RemoteSchemaModel.sync({ alter: true });
   await RemoteTableModel.sync({ alter: true });
   await ZendeskConfiguration.sync({ alter: true });
+  await ZendeskBrand.sync({ alter: true });
+  await ZendeskArticle.sync({ alter: true });
 
   // enable the `unaccent` extension
   await sequelizeConnection.query("CREATE EXTENSION IF NOT EXISTS unaccent;");
