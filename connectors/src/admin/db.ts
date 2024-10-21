791194468447be2db8a4ef37491fce72d263e97a
Aubin <aubin@dust.tt>
2024-10-21 18:39:53 +0200

    fix: add the model for ZendeskCategory to db.ts

--- connectors/src/admin/db.ts
+++ connectors/src/admin/db.ts
@@ -61,7 +61,7 @@ import {
   WebCrawlerFolder,
   WebCrawlerPage,
 } from "@connectors/lib/models/webcrawler";
-import { ZendeskArticle,ZendeskBrand, ZendeskConfiguration } from "@connectors/lib/models/zendesk";
+import { ZendeskArticle, ZendeskBrand, ZendeskCategory, ZendeskConfiguration } from "@connectors/lib/models/zendesk";
 import logger from "@connectors/logger/logger";
 import { sequelizeConnection } from "@connectors/resources/storage";
 import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
@@ -116,6 +116,7 @@ async function main(): Promise<void> {
   await RemoteTableModel.sync({ alter: true });
   await ZendeskConfiguration.sync({ alter: true });
   await ZendeskBrand.sync({ alter: true });
+  await ZendeskCategory.sync({ alter: true });
   await ZendeskArticle.sync({ alter: true });
 
   // enable the `unaccent` extension
