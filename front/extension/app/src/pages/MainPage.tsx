26229b6bba531aac51ddef8d42db99b997917903
Daphné Popin <daphne.popin@gmail.com>
2024-10-21 18:26:55 +0200

    Fix flow when reopening the sidepanel

--- front/extension/app/src/pages/MainPage.tsx
+++ front/extension/app/src/pages/MainPage.tsx
@@ -1,13 +1,24 @@
 import { GenerationContextProvider } from "@app/components/assistant/conversation/GenerationContextProvider";
 import { FixedAssistantInputBar } from "@app/components/assistant/conversation/input_bar/InputBar";
 import { useAuth } from "@app/extension/app/src/context/AuthProvider";
-import { Page } from "@dust-tt/sparkle";
+import { Page, Spinner } from "@dust-tt/sparkle";
 import type { WorkspaceType } from "@dust-tt/types";
 import { useNavigate } from "react-router-dom";
 
 export const MainPage = () => {
   const navigate = useNavigate();
-  const { isAuthenticated, user } = useAuth();
+  const { isLoading, isAuthenticated, user } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full w-full">
+        <div className="flex h-full w-full items-center justify-center">
+          <Spinner />
+        </div>
+      </div>
+    );
+  }
+
   if (!isAuthenticated || !user) {
     navigate("/login");
     return;
