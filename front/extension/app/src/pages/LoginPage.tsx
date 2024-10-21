a11437162c6c2ee2a0c9290621859f32c482e3c3
Daphné Popin <PopDaph@users.noreply.github.com>
2024-10-21 14:32:31 +0200

    Ext: End of login flow: call /me, save user & pick workspace (#8115)
    * Ext: End of login flow: call /me, save user & pick workspace
    
    * Apply feedback

--- front/extension/app/src/pages/LoginPage.tsx
+++ front/extension/app/src/pages/LoginPage.tsx
@@ -1,28 +1,53 @@
-import { Button, LoginIcon } from "@dust-tt/sparkle";
+import { Button, DropdownMenu, LoginIcon } from "@dust-tt/sparkle";
 import { useAuth } from "@extension/context/AuthProvider";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
-  const { handleLogin, isAuthenticated, isLoading } = useAuth();
+  const {
+    user,
+    isAuthenticated,
+    isUserSetup,
+    handleLogin,
+    handleSelectWorkspace,
+    isLoading,
+  } = useAuth();
 
   useEffect(() => {
-    if (isAuthenticated) {
+    if (isAuthenticated && isUserSetup) {
       navigate("/");
     }
-  }, [isAuthenticated, navigate]);
+  }, [navigate, user, isAuthenticated, isUserSetup]);
 
   return (
     <div className="flex h-screen flex-col gap-2 p-4">
       <div className="flex h-full w-full items-center justify-center">
-        <Button
-          icon={LoginIcon}
-          variant="primary"
-          label="Sign in"
-          onClick={handleLogin}
-          disabled={isLoading}
-        />
+        {!isAuthenticated && (
+          <Button
+            icon={LoginIcon}
+            variant="primary"
+            label="Sign in"
+            onClick={handleLogin}
+            disabled={isLoading}
+          />
+        )}
+        {isAuthenticated && !isUserSetup && user?.workspaces.length && (
+          <DropdownMenu className="flex">
+            <DropdownMenu.Button label="Select workspace" />
+            <DropdownMenu.Items>
+              {user.workspaces.map((w) => {
+                return (
+                  <DropdownMenu.Item
+                    key={w.sId}
+                    onClick={() => handleSelectWorkspace(w.sId)}
+                    label={w.name}
+                  />
+                );
+              })}
+            </DropdownMenu.Items>
+          </DropdownMenu>
+        )}
       </div>
     </div>
   );
