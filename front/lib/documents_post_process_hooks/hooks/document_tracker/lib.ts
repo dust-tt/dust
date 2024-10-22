8c826cf963f8cdb58623d7557a6278e9b1710176
Henry Fontanier <h.fontanier@gmail.com>
2024-10-22 10:06:05 +0200

    fix(doc tracker revival): use vaults (#8143)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- front/lib/documents_post_process_hooks/hooks/document_tracker/lib.ts
+++ front/lib/documents_post_process_hooks/hooks/document_tracker/lib.ts
@@ -1,43 +1,21 @@
-import { DustAPI } from "@dust-tt/client";
-import type { WorkspaceType } from "@dust-tt/types";
-
-import config from "@app/lib/api/config";
-import { prodAPICredentialsForOwner } from "@app/lib/auth";
+import type { Authenticator } from "@app/lib/auth";
 import { TRACKABLE_CONNECTOR_TYPES } from "@app/lib/documents_post_process_hooks/hooks/document_tracker/consts";
-import logger from "@app/logger/logger";
+import { DataSourceViewResource } from "@app/lib/resources/data_source_view_resource";
+import { VaultResource } from "@app/lib/resources/vault_resource";
 
-export async function getTrackableDataSources(owner: WorkspaceType): Promise<
-  {
-    workspace_id: string;
-    data_source_id: string;
-  }[]
-> {
-  const prodCredentials = await prodAPICredentialsForOwner(owner);
-
-  const prodAPI = new DustAPI(
-    config.getDustAPIConfig(),
-    prodCredentials,
-    logger
-  );
-
-  // Fetch data sources
-  const dsRes = await prodAPI.getDataSources(prodAPI.workspaceId());
-  if (dsRes.isErr()) {
-    throw dsRes.error;
-  }
-  const dataSources = dsRes.value;
+export async function getTrackableDataSourceViews(
+  auth: Authenticator
+): Promise<DataSourceViewResource[]> {
+  const globalVault = await VaultResource.fetchWorkspaceGlobalVault(auth);
+  // TODO(DOC_TRACKER):
+  const views = await DataSourceViewResource.listByVault(auth, globalVault);
 
   // Filter data sources to only include trackable ones
-  const trackableDataSources = dataSources.filter(
-    (ds) =>
-      ds.connectorProvider &&
-      TRACKABLE_CONNECTOR_TYPES.includes(ds.connectorProvider)
+  const trackableViews = views.filter(
+    (view) =>
+      view.dataSource.connectorProvider &&
+      TRACKABLE_CONNECTOR_TYPES.includes(view.dataSource.connectorProvider)
   );
 
-  return trackableDataSources.map((ds) => ({
-    workspace_id: prodAPI.workspaceId(),
-    // TODO(GROUPS_INFRA): this should pull the data source views from the global vaults (we need an
-    // API for that).
-    data_source_id: ds.sId,
-  }));
+  return trackableViews;
 }
