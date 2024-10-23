848acaf582473042204bc3df88027e8eedc5765e
Aubin <aubin@dust.tt>
2024-10-23 13:32:09 +0200

    feat: rework the content node tree for the tickets

--- connectors/src/connectors/zendesk/lib/ticket_permissions.ts
+++ connectors/src/connectors/zendesk/lib/ticket_permissions.ts
@@ -6,12 +6,9 @@ import type {
 } from "@dust-tt/types";
 
 import {
-  getBrandInternalId,
+  getBrandIdFromInternalId,
   getTicketsInternalId,
 } from "@connectors/connectors/zendesk/lib/id_conversions";
-import { getZendeskAccessToken } from "@connectors/connectors/zendesk/lib/zendesk_access_token";
-import { createZendeskClient } from "@connectors/connectors/zendesk/lib/zendesk_api";
-import { ZendeskBrand } from "@connectors/lib/models/zendesk";
 import logger from "@connectors/logger/logger";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
 
@@ -33,70 +30,26 @@ export async function retrieveZendeskTicketPermissions({
 
   const isRootLevel = !parentInternalId;
 
-  // At the root level, we show two nodes: Help Center and Tickets.
-  if (isRootLevel) {
-    const ticketsNode: ContentNode = {
-      provider: connector.type,
-      internalId: getTicketsInternalId(connectorId),
-      parentInternalId: null,
-      type: "database",
-      title: "Tickets",
-      sourceUrl: null,
-      expandable: true,
-      permission: "none",
-      dustDocumentId: null,
-      lastUpdatedAt: null,
-    };
-
-    return [ticketsNode];
-  }
-
-  const token = await getZendeskAccessToken(connector.connectionId);
-  const zendeskApiClient = createZendeskClient({ token });
-
-  const isReadPermissionsOnly = filterPermission === "read";
-  let nodes: ContentNode[] = [];
-
-  // If the parent is the Tickets node, we retrieve the list of Brands.
-  // If isReadPermissionsOnly = true, we retrieve the list of Brands from the DB that have permission == "read"
-  // If isReadPermissionsOnly = false, we retrieve the list of Brands from Zendesk
-  if (parentInternalId === getTicketsInternalId(connectorId)) {
-    if (isReadPermissionsOnly) {
-      const brandsInDatabase = await ZendeskBrand.findAll({
-        where: {
-          connectorId: connectorId,
-          permission: "read",
-        },
-      });
-      nodes = brandsInDatabase.map((brand) => ({
+  // There is no ticket at the root level, only the brands.
+  if (!isRootLevel) {
+    const brandId = getBrandIdFromInternalId(connectorId, parentInternalId);
+    // If the parent is a Brand, we return a single node for its help center.
+    if (brandId) {
+      const ticketsNode: ContentNode = {
         provider: connector.type,
-        internalId: getBrandInternalId(connectorId, brand.brandId),
-        parentInternalId: getTicketsInternalId(connectorId),
-        type: "folder",
-        title: brand.name,
-        sourceUrl: brand.url,
-        expandable: false,
-        permission: brand.permission,
-        dustDocumentId: null,
-        lastUpdatedAt: brand.updatedAt.getTime(),
-      }));
-    } else {
-      const { result: brands } = await zendeskApiClient.brand.list();
-      nodes = brands.map((brand) => ({
-        provider: connector.type,
-        internalId: getBrandInternalId(connectorId, brand.id),
-        parentInternalId: getTicketsInternalId(connectorId),
-        type: "folder",
-        title: brand.name || "Brand",
-        sourceUrl: brand.brand_url,
-        expandable: false,
+        internalId: getTicketsInternalId(connectorId),
+        parentInternalId: parentInternalId,
+        type: "database",
+        title: "Tickets",
+        sourceUrl: null,
+        expandable: true,
         permission: "none",
         dustDocumentId: null,
         lastUpdatedAt: null,
-      }));
+      };
+
+      return [ticketsNode];
     }
   }
-
-  nodes.sort((a, b) => a.title.localeCompare(b.title));
-  return nodes;
+  return [];
 }
