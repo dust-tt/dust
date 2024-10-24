c50913a4f64130862af9b3b58f858c5370f61bac
Aubin <aubin@dust.tt>
2024-10-24 15:28:29 +0200

    remove the catch-all try catch block

--- connectors/src/connectors/zendesk/index.ts
+++ connectors/src/connectors/zendesk/index.ts
@@ -170,119 +170,108 @@ export class ZendeskConnectorManager extends BaseConnectorManager<null> {
     const toBeSignaledTicketsIds = new Set<number>();
     const toBeSignaledHelpCenterIds = new Set<number>();
     const toBeSignaledCategoryIds = new Set<number>();
-    try {
-      for (const [id, permission] of Object.entries(permissions)) {
-        if (permission !== "none" && permission !== "read") {
-          return new Err(
-            new Error(
-              `Invalid permission ${permission} for connector ${this.connectorId}`
-            )
-          );
-        }
-
-        const brandId = getBrandIdFromInternalId(this.connectorId, id);
-        const brandHelpCenterId = getBrandIdFromHelpCenterId(
-          this.connectorId,
-          id
+    for (const [id, permission] of Object.entries(permissions)) {
+      if (permission !== "none" && permission !== "read") {
+        return new Err(
+          new Error(
+            `Invalid permission ${permission} for connector ${this.connectorId}`
+          )
         );
-        const brandTicketsId = getBrandIdFromTicketsId(this.connectorId, id);
-        const categoryId = getCategoryIdFromInternalId(this.connectorId, id);
+      }
 
-        if (brandId) {
-          toBeSignaledBrandIds.add(brandId);
-          if (permission === "none") {
-            await revokeSyncZendeskBrand({
-              connectorId: this.connectorId,
-              brandId,
-            });
+      const brandId = getBrandIdFromInternalId(this.connectorId, id);
+      const brandHelpCenterId = getBrandIdFromHelpCenterId(
+        this.connectorId,
+        id
+      );
+      const brandTicketsId = getBrandIdFromTicketsId(this.connectorId, id);
+      const categoryId = getCategoryIdFromInternalId(this.connectorId, id);
+
+      if (brandId) {
+        toBeSignaledBrandIds.add(brandId);
+        if (permission === "none") {
+          await revokeSyncZendeskBrand({
+            connectorId: this.connectorId,
+            brandId,
+          });
+        }
+        if (permission === "read") {
+          await allowSyncZendeskBrand({
+            subdomain,
+            connectorId: this.connectorId,
+            connectionId,
+            brandId,
+          });
+        }
+      } else if (brandHelpCenterId) {
+        if (permission === "none") {
+          const revokedCollection = await revokeSyncZendeskHelpCenter({
+            connectorId: this.connectorId,
+            brandId: brandHelpCenterId,
+          });
+          if (revokedCollection) {
+            toBeSignaledHelpCenterIds.add(revokedCollection.brandId);
           }
-          if (permission === "read") {
-            await allowSyncZendeskBrand({
-              subdomain,
-              connectorId: this.connectorId,
-              connectionId,
-              brandId,
-            });
+        }
+        if (permission === "read") {
+          const newBrand = await allowSyncZendeskHelpCenter({
+            connectorId: this.connectorId,
+            connectionId,
+            brandId: brandHelpCenterId,
+            subdomain,
+          });
+          if (newBrand) {
+            toBeSignaledHelpCenterIds.add(newBrand.brandId);
           }
-        } else if (brandHelpCenterId) {
-          if (permission === "none") {
-            const revokedCollection = await revokeSyncZendeskHelpCenter({
-              connectorId: this.connectorId,
-              brandId: brandHelpCenterId,
-            });
-            if (revokedCollection) {
-              toBeSignaledHelpCenterIds.add(revokedCollection.brandId);
-            }
+        }
+      } else if (brandTicketsId) {
+        if (permission === "none") {
+          const revokedCollection = await revokeSyncZendeskTickets({
+            connectorId: this.connectorId,
+            brandId: brandTicketsId,
+          });
+          if (revokedCollection) {
+            toBeSignaledTicketsIds.add(revokedCollection.brandId);
           }
-          if (permission === "read") {
-            const newBrand = await allowSyncZendeskHelpCenter({
-              connectorId: this.connectorId,
-              connectionId,
-              brandId: brandHelpCenterId,
-              subdomain,
-            });
-            if (newBrand) {
-              toBeSignaledHelpCenterIds.add(newBrand.brandId);
-            }
+        }
+        if (permission === "read") {
+          const newBrand = await allowSyncZendeskTickets({
+            connectorId: this.connectorId,
+            connectionId,
+            brandId: brandTicketsId,
+            subdomain,
+          });
+          if (newBrand) {
+            toBeSignaledTicketsIds.add(newBrand.brandId);
           }
-        } else if (brandTicketsId) {
-          if (permission === "none") {
-            const revokedCollection = await revokeSyncZendeskTickets({
-              connectorId: this.connectorId,
-              brandId: brandTicketsId,
-            });
-            if (revokedCollection) {
-              toBeSignaledTicketsIds.add(revokedCollection.brandId);
-            }
+        }
+      } else if (categoryId) {
+        if (permission === "none") {
+          const revokedCategory = await revokeSyncZendeskCategory({
+            connectorId: this.connectorId,
+            categoryId,
+          });
+          if (revokedCategory) {
+            toBeSignaledCategoryIds.add(revokedCategory.categoryId);
           }
-          if (permission === "read") {
-            const newBrand = await allowSyncZendeskTickets({
-              connectorId: this.connectorId,
-              connectionId,
-              brandId: brandTicketsId,
-              subdomain,
-            });
-            if (newBrand) {
-              toBeSignaledTicketsIds.add(newBrand.brandId);
-            }
-          }
-        } else if (categoryId) {
-          if (permission === "none") {
-            const revokedCategory = await revokeSyncZendeskCategory({
-              connectorId: this.connectorId,
-              categoryId,
-            });
-            if (revokedCategory) {
-              toBeSignaledCategoryIds.add(revokedCategory.categoryId);
-            }
-          }
-          if (permission === "read") {
-            const newCategory = await allowSyncZendeskCategory({
-              subdomain,
-              connectorId: this.connectorId,
-              connectionId,
-              categoryId,
-            });
-            if (newCategory) {
-              toBeSignaledCategoryIds.add(newCategory.categoryId);
-            }
+        }
+        if (permission === "read") {
+          const newCategory = await allowSyncZendeskCategory({
+            subdomain,
+            connectorId: this.connectorId,
+            connectionId,
+            categoryId,
+          });
+          if (newCategory) {
+            toBeSignaledCategoryIds.add(newCategory.categoryId);
           }
         }
       }
-
-      /// Launch a sync workflow here
-
-      return new Ok(undefined);
-    } catch (e) {
-      logger.error(
-        {
-          connectorId: this.connectorId,
-          error: e,
-        },
-        "Error setting connector permissions."
-      );
-      return new Err(new Error("Error setting permissions"));
     }
+
+    /// Launch a sync workflow here
+
+    return new Ok(undefined);
   }
 
   async retrieveBatchContentNodes({
