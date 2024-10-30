752204d9b88ae9575b3ee7543b2ece49ebf50c40
Henry Fontanier <henry@dust.tt>
2024-10-30 13:11:40 +0100

    fix: backfill parents for notion orphaned resources

---
+++ connectors/migrations/20241030_fix_notion_parents.ts
@@ -0,0 +1,127 @@
+import { makeScript } from "scripts/helpers";
+import { Op } from "sequelize";
+
+import { getParents } from "@connectors/connectors/notion/lib/parents";
+import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
+import { concurrentExecutor } from "@connectors/lib/async_utils";
+import {
+  updateDocumentParentsField,
+  updateTableParentsField,
+} from "@connectors/lib/data_sources";
+import { NotionDatabase, NotionPage } from "@connectors/lib/models/notion";
+import { ConnectorResource } from "@connectors/resources/connector_resource";
+
+async function updateParentsFieldForConnector(
+  connector: ConnectorResource,
+  execute = false
+) {
+  let pagesIdCursor = 0;
+  let databasesIdCursor = 0;
+
+  const pageSize = 512;
+  let nodes: (NotionPage | NotionDatabase)[] = [];
+  do {
+    const pages = await NotionPage.findAll({
+      where: {
+        connectorId: connector.id,
+        parentId: "unknown",
+        id: {
+          [Op.gte]: pagesIdCursor,
+        },
+      },
+      limit: pageSize,
+      order: [["id", "ASC"]],
+    });
+    const databases = await NotionDatabase.findAll({
+      where: {
+        connectorId: connector.id,
+        parentId: "unknown",
+        id: {
+          [Op.gte]: databasesIdCursor,
+        },
+      },
+      limit: pageSize,
+      order: [["id", "ASC"]],
+    });
+
+    if (pages.length > 0) {
+      const newCursor = pages[pages.length - 1]?.id;
+      if (!newCursor) {
+        throw new Error("Last page is undefined");
+      }
+      pagesIdCursor = newCursor;
+    }
+    if (databases.length > 0) {
+      const newCursor = databases[databases.length - 1]?.id;
+      if (!newCursor) {
+        throw new Error("Last database is undefined");
+      }
+      databasesIdCursor = newCursor;
+    }
+
+    nodes = [...pages, ...databases];
+    const res = await concurrentExecutor(
+      nodes,
+      async (node) => {
+        const parents = await getParents(
+          connector.id,
+          "notionPageId" in node ? node.notionPageId : node.notionDatabaseId,
+          [],
+          undefined
+        );
+
+        let documentId: string | null = null;
+        let tableId: string | null = null;
+
+        if ("notionPageId" in node) {
+          // its a page
+          documentId = `notion-${node.notionPageId}`;
+        } else {
+          tableId = `notion-${node.notionDatabaseId}`;
+          documentId = `notion-database-${node.notionDatabaseId}`;
+        }
+
+        if (documentId) {
+          if (execute) {
+            await updateDocumentParentsField({
+              dataSourceConfig: dataSourceConfigFromConnector(connector),
+              documentId,
+              parents,
+            });
+          }
+        }
+
+        if (tableId) {
+          if (execute) {
+            await updateTableParentsField({
+              dataSourceConfig: dataSourceConfigFromConnector(connector),
+              tableId,
+              parents,
+            });
+          }
+        }
+      },
+      { concurrency: 8 }
+    );
+
+    console.log(
+      `Processed ${res.length} nodes, (pages cursor: ${pagesIdCursor}, databases cursor: ${databasesIdCursor})`
+    );
+  } while (nodes.length > 0);
+
+  console.log(
+    `Finished processing connector ${connector.id} (workspace ${connector.workspaceId})`
+  );
+}
+
+makeScript({}, async ({ execute }) => {
+  const connectors = await ConnectorResource.listByType("notion", {});
+
+  console.log(`Found ${connectors.length} Notion connectors`);
+  for (const connector of connectors) {
+    console.log(
+      `Processing connector ${connector.id} (workspace ${connector.workspaceId})`
+    );
+    await updateParentsFieldForConnector(connector, execute);
+  }
+});
