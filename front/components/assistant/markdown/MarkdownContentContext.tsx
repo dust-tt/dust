e058d2bea14c0b17669d03365cd01edf351d6862
Thomas Draier <thomas@dust.tt>
2024-10-30 10:55:44 +0100

    [front] Use separate dark/light context per block (#8327)

--- front/components/assistant/markdown/MarkdownContentContext.tsx
+++ front/components/assistant/markdown/MarkdownContentContext.tsx
@@ -4,12 +4,8 @@ export const MarkdownContentContext = React.createContext<{
   content: string;
   isStreaming: boolean;
   isLastMessage: boolean;
-  setIsDarkMode: (v: boolean) => void;
-  isDarkMode: boolean;
 }>({
   content: "",
   isStreaming: false,
   isLastMessage: false,
-  setIsDarkMode: () => {},
-  isDarkMode: false,
 });
