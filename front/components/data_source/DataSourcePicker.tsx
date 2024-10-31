756a77887cee1a350352da061fab67c6ec9d0948
Flavien David <flavien@dust.tt>
2024-10-31 09:00:41 +0100

    :pencil:

--- front/components/data_source/DataSourcePicker.tsx
+++ front/components/data_source/DataSourcePicker.tsx
@@ -138,7 +138,7 @@ export default function DataSourcePicker({
                 <Button variant="outline" label="Select DataSource" isSelect />
               ) : (
                 <Link
-                  href={`/w/${owner.sId}/data-sources/vaults`}
+                  href={`/w/${owner.sId}/data-sources/spaces`}
                   className={classNames(
                     readOnly ? "text-gray-300" : "text-gray-700"
                   )}
