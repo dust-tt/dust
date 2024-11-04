a380e5bcab3d23547250298194405f8cf0660046
Flavien David <flavien@dust.tt>
2024-11-04 12:13:47 +0100

    Add migration

---
+++ front/migrations/db/migration_110.sql
@@ -0,0 +1,3 @@
+-- Migration created on Nov 04, 2024
+ALTER TABLE "public"."agent_configurations" ADD COLUMN "requestedGroupIds" INTEGER[][] NOT NULL DEFAULT ARRAY[]::INTEGER[][];
+ALTER TABLE "public"."conversations" ADD COLUMN "requestedGroupIds" INTEGER[] NOT NULL DEFAULT ARRAY[]::INTEGER[];
