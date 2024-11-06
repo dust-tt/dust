36d48cfb484c6e1809abf57ae68c85432b09888e
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-11-06 11:24:02 +0100

    Updating CardButton

--- sparkle/src/components/CardButton.tsx
+++ sparkle/src/components/CardButton.tsx
@@ -7,32 +7,32 @@ import {
   SparkleContext,
   SparkleContextLinkType,
 } from "@sparkle/context";
+import { cn } from "@sparkle/lib/utils";
 
-const CARD_BUTTON_VARIANTS = ["primary", "secondary", "tertiary"] as const;
-
-type CardButtonVariantType = (typeof CARD_BUTTON_VARIANTS)[number];
-
-const CARD_BUTTON_SIZES = ["sm", "md", "lg"] as const;
-
-type CardButtonSizeType = (typeof CARD_BUTTON_SIZES)[number];
-
-const variantClasses: Record<CardButtonVariantType, string> = {
-  primary:
-    "s-bg-structure-50 s-border s-border-structure-200 hover:s-bg-white hover:s-border-structure-100 active:s-bg-structure-100 active:s-border-structure-200 s-cursor-pointer",
-  secondary:
-    "s-bg-structure-0 s-border s-border-structure-100 hover:s-bg-structure-50 hover:s-border-structure-200 active:s-bg-structure-100 active:s-border-structure-300 s-cursor-pointer",
-  tertiary:
-    "s-border-structure-100/0 s-border s-border-structure-0 hover:s-bg-structure-50 hover:s-border-structure-100 active:s-bg-structure-100 active:s-border-structure-200 s-cursor-pointer",
-};
-
-const sizeVariants: Record<CardButtonSizeType, string> = {
-  sm: "s-p-3 s-rounded-xl",
-  md: "s-p-4 s-rounded-2xl",
-  lg: "s-p-5 s-rounded-3xl",
-};
+const cardButtonVariants = cva(
+  "s-flex s-text-left s-group s-transition s-duration-200 s-border s-overflow-hidden s-text-foreground s-cursor-pointer hover:s-border-primary-100 hover:s-bg-primary-100 active:s-bg-primary-200 disabled:s-text-primary-muted disabled:s-border-structure-100 disabled:s-pointer-events-none",
+  {
+    variants: {
+      variant: {
+        primary: "s-bg-structure-50 s-border-border-dark/0",
+        secondary: "s-bg-background s-border-border-dark",
+        tertiary: "s-bg-background s-border-border-dark/0",
+      },
+      size: {
+        sm: "s-p-3 s-rounded-xl",
+        md: "s-p-4 s-rounded-2xl",
+        lg: "s-p-5 s-rounded-3xl",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  }
+);
 
 interface CardButtonProps
-  extends React.HTMLAttributes<HTMLDivElement>,
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof cardButtonVariants> {
   children: ReactNode;
   href?: string | UrlObject;
@@ -42,24 +42,10 @@ interface CardButtonProps
   shallow?: boolean;
 }
 
-const cardButtonVariants = cva(
-  "s-flex s-group s-transition s-duration-200 s-overflow-hidden",
-  {
-    variants: {
-      variant: variantClasses,
-      size: sizeVariants,
-    },
-    defaultVariants: {
-      variant: "primary",
-      size: "md",
-    },
-  }
-);
-
 export function CardButton({
   children,
-  variant,
-  size,
+  variant = "primary",
+  size = "md",
   className,
   onClick,
   onMouseEnter,
@@ -69,15 +55,21 @@ export function CardButton({
   rel = "",
   replace,
   shallow,
+  ...props
 }: CardButtonProps) {
   const { components } = React.useContext(SparkleContext);
   const Link: SparkleContextLinkType = href ? components.link : noHrefLink;
+
+  const cardButtonClassNames = cn(
+    cardButtonVariants({ variant, size }),
+    className
+  );
 
   if (href) {
     return (
       <Link
         href={href}
-        className={cardButtonVariants({ variant, size, className })}
+        className={cardButtonClassNames}
         replace={replace}
         shallow={shallow}
         target={target}
@@ -89,13 +81,14 @@ export function CardButton({
   }
 
   return (
-    <div
-      className={cardButtonVariants({ variant, size, className })}
+    <button
+      className={cardButtonClassNames}
       onClick={onClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      {...props}
     >
       {children}
-    </div>
+    </button>
   );
 }
