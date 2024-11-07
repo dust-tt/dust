917e94c68bc4bf640a706faa35bb2d6bdc5b0c27
JulesBelveze <jules.belveze@hotmail.fr>
2024-11-07 09:43:25 +0100

    [front] - feature: update @dust-tt/sparkle to version 0.2.298-rc-2
     - Upgrade @dust-tt/sparkle to include the latest features and fixes in the release candidate
     - Ensure package-lock.json matches the new @dust-tt/sparkle dependency version
    
    [front] - refactor: use self-closing tag for Button in subscribe page
    
     - Simplify Button component usage by using self-closing tag when no children are present

--- front/pages/w/[wId]/subscribe.tsx
+++ front/pages/w/[wId]/subscribe.tsx
@@ -197,7 +197,7 @@ export default function Subscribe({
                   onClick={() => {
                     void handleSubscribePlan(billingPeriod);
                   }}
-                ></Button>
+                />
               </Page.Vertical>
               <Page.Horizontal sizing="grow">
                 <ProPlansTable
