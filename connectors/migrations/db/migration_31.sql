87f66dd063ff6b5da9325aa21fba7c1a44cb04c8
Alban Dumouilla <alban@dust.tt>
2024-11-08 11:23:43 +0100

    remove bigint

--- connectors/migrations/db/migration_31.sql
+++ connectors/migrations/db/migration_31.sql
@@ -1,8 +1,8 @@
 -- Migration created on Nov 07, 2024
-ALTER TABLE "zendesk_tickets" ALTER COLUMN "brandId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "brandId" DROP DEFAULT;ALTER TABLE "zendesk_tickets" ALTER COLUMN "brandId" TYPE BIGINT;
-ALTER TABLE "zendesk_tickets" ALTER COLUMN "groupId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "groupId" DROP DEFAULT;ALTER TABLE "zendesk_tickets" ALTER COLUMN "groupId" TYPE BIGINT;
-ALTER TABLE "zendesk_tickets" ALTER COLUMN "assigneeId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "assigneeId" DROP DEFAULT;ALTER TABLE "zendesk_tickets" ALTER COLUMN "assigneeId" TYPE BIGINT;
-ALTER TABLE "zendesk_tickets" ALTER COLUMN "organizationId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "organizationId" DROP DEFAULT;ALTER TABLE "zendesk_tickets" ALTER COLUMN "organizationId" TYPE BIGINT;
+ALTER TABLE "zendesk_tickets" ALTER COLUMN "brandId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "brandId" DROP DEFAULT;
+ALTER TABLE "zendesk_tickets" ALTER COLUMN "groupId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "groupId" DROP DEFAULT;
+ALTER TABLE "zendesk_tickets" ALTER COLUMN "assigneeId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "assigneeId" DROP DEFAULT;
+ALTER TABLE "zendesk_tickets" ALTER COLUMN "organizationId" DROP NOT NULL;ALTER TABLE "zendesk_tickets" ALTER COLUMN "organizationId" DROP DEFAULT;
 ALTER TABLE "public"."zendesk_tickets" RENAME COLUMN "name" TO "subject";
 ALTER TABLE "public"."zendesk_tickets" DROP COLUMN "satisfactionScore";
 ALTER TABLE "public"."zendesk_tickets" DROP COLUMN "satisfactionComment";
