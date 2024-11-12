d7878489778621bf126b8ef74507eecbcb7d1f97
Flavien David <flavien@dust.tt>
2024-11-12 16:21:50 +0100

    Add Spaces table in Poke

---
+++ front/components/poke/spaces/columns.tsx
@@ -0,0 +1,110 @@
+import { IconButton } from "@dust-tt/sparkle";
+import type { SpaceType, WorkspaceType } from "@dust-tt/types";
+import { ArrowsUpDownIcon } from "@heroicons/react/20/solid";
+import type { ColumnDef } from "@tanstack/react-table";
+
+import PokeLink from "@app/components/poke/shadcn/ui/link";
+import { formatTimestampToFriendlyDate } from "@app/lib/utils";
+
+export function makeColumnsForSpaces(
+  owner: WorkspaceType
+): ColumnDef<SpaceType>[] {
+  return [
+    {
+      accessorKey: "sId",
+      cell: ({ row }) => {
+        const sId: string = row.getValue("sId");
+
+        return (
+          <PokeLink href={`/poke/${owner.sId}/spaces/${sId}`}>{sId}</PokeLink>
+        );
+      },
+      header: ({ column }) => {
+        return (
+          <div className="flex space-x-2">
+            <p>sId</p>
+            <IconButton
+              variant="outline"
+              icon={ArrowsUpDownIcon}
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <div className="flex space-x-2">
+            <p>Name</p>
+            <IconButton
+              variant="outline"
+              icon={ArrowsUpDownIcon}
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "kind",
+      header: ({ column }) => {
+        return (
+          <div className="flex space-x-2">
+            <p>Kind</p>
+            <IconButton
+              variant="outline"
+              icon={ArrowsUpDownIcon}
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "isRestricted",
+      cell: ({ row }) => {
+        const isRestricted: boolean = row.getValue("isRestricted");
+
+        return isRestricted ? "Yes" : "No";
+      },
+      header: ({ column }) => {
+        return (
+          <div className="flex space-x-2">
+            <p>Is Restricted</p>
+            <IconButton
+              variant="outline"
+              icon={ArrowsUpDownIcon}
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            />
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "createdAt",
+      cell: ({ row }) => {
+        const createdAt: number = row.getValue("createdAt");
+
+        return formatTimestampToFriendlyDate(createdAt);
+      },
+    },
+    {
+      accessorKey: "updatedAt",
+      cell: ({ row }) => {
+        const updatedAt: number = row.getValue("updatedAt");
+
+        return formatTimestampToFriendlyDate(updatedAt);
+      },
+    },
+  ];
+}
--- types/src/front/space.ts
+++ types/src/front/space.ts
@@ -2,9 +2,11 @@ const SPACE_KINDS = ["regular", "global", "system", "public"] as const;
 export type SpaceKind = (typeof SPACE_KINDS)[number];
 
 export type SpaceType = {
-  name: string;
-  sId: string;
-  kind: SpaceKind;
+  createdAt: number;
   groupIds: string[];
   isRestricted: boolean;
+  kind: SpaceKind;
+  name: string;
+  sId: string;
+  updatedAt: number;
 };
