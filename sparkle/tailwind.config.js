d8d427e6a25d5bdbedc110e35a9a7855527de158
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-11-12 20:54:35 +0100

    Page

--- sparkle/tailwind.config.js
+++ sparkle/tailwind.config.js
@@ -252,7 +252,7 @@ module.exports = {
         },
         primary: {
           DEFAULT: colors.slate[800],
-          light: { DEFAULT: colors.slate[700], dark: colors.slate[300] },
+          light: { DEFAULT: colors.slate[600], dark: colors.slate[300] },
           dark: { DEFAULT: colors.slate[950], dark: colors.slate[50] },
           muted: { DEFAULT: colors.slate[400], dark: colors.slate[600] },
           950: { DEFAULT: colors.slate[950], dark: colors.slate[50] },
