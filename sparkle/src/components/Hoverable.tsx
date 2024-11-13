1ade87e90867e41108987c1c43eb961ed67de231
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-11-13 15:59:22 +0100

    Making Hoverable href compatible

--- sparkle/src/components/Hoverable.tsx
+++ sparkle/src/components/Hoverable.tsx
@@ -1,7 +1,8 @@
+import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import React, { SyntheticEvent } from "react";
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
+import { LinkWrapper, LinkWrapperProps } from "@sparkle/components/LinkWrapper";
 import { cn } from "@sparkle/lib/utils";
 
 const hoverableVariants = cva(
@@ -22,24 +23,67 @@ const hoverableVariants = cva(
   }
 );
 
-interface HoverableProps extends VariantProps<typeof hoverableVariants> {
-  children: ReactNode;
-  className?: string;
-  onClick: (e: SyntheticEvent) => void;
+export const HOVERABLE_VARIANTS = [
+  "invisible",
+  "primary",
+  "highlight",
+] as const;
+export type HoverableVariantType = (typeof HOVERABLE_VARIANTS)[number];
+
+interface MetaHoverableProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof hoverableVariants> {
+  asChild?: boolean;
 }
 
-export function Hoverable({
-  children,
-  className,
-  onClick,
-  variant,
-}: HoverableProps) {
-  return (
-    <span
-      className={cn(hoverableVariants({ variant }), className)}
-      onClick={onClick}
-    >
-      {children}
-    </span>
-  );
+const MetaHoverable = React.forwardRef<HTMLElement, MetaHoverableProps>(
+  ({ className, variant, asChild = false, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : "span";
+    return (
+      <Comp
+        className={cn(variant && hoverableVariants({ variant }), className)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
+MetaHoverable.displayName = "MetaHoverable";
+
+export interface HoverableProps
+  extends Omit<MetaHoverableProps, "children">,
+    Omit<LinkWrapperProps, "children" | "className"> {
+  children: ReactNode;
+  href?: string;
+  target?: string;
+  rel?: string;
 }
+
+const Hoverable = React.forwardRef<HTMLElement, HoverableProps>(
+  ({ href, target, rel, children, variant, className, ...props }, ref) => {
+    const innerElement = (
+      <MetaHoverable
+        ref={ref}
+        variant={variant}
+        className={className}
+        {...props}
+      >
+        {children}
+      </MetaHoverable>
+    );
+
+    return href ? (
+      <LinkWrapper href={href} target={target} rel={rel}>
+        {innerElement}
+      </LinkWrapper>
+    ) : (
+      innerElement
+    );
+  }
+);
+
+Hoverable.displayName = "Hoverable";
+
+export { Hoverable };
