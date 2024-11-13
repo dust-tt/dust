a1aae53eaf6734616bbed83f79111e0cb31ccf80
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2024-11-13 11:27:57 +0100

    [connectors] Refactor the interaction with the Zendesk API (#8603)
    * refactor: move the retry mechanism to a separate function
    
    * refactor: move _getZendeskConnectorOrRaise and _getZendeskCategoryOrRaise to utils
    
    * refactor: use fetchWithRetries when fetching tickets
    
    * fix: use the brand subdomain to fetch tickets instead of the base one
    
    * docs: fix the description of a function
    
    * docs: add a small description for the ticket sync activity
    
    * fix: fix a grammatical error
    
    * refactor: add a generic function runWithPagination
    
    * refactor: rename runWithPagination into a Zendesk-specific name
    
    * refactor: simplify the workflows
    
    * refactor: rename fetchWithRetries

--- connectors/src/connectors/zendesk/lib/zendesk_api.ts
+++ connectors/src/connectors/zendesk/lib/zendesk_api.ts
@@ -95,6 +95,61 @@ async function handleZendeskRateLimit(response: Response): Promise<boolean> {
 }
 
 /**
+ * Runs a GET request to the Zendesk API with a maximum number of retries before throwing.
+ */
+async function fetchFromZendeskWithRetries({
+  url,
+  accessToken,
+}: {
+  url: string;
+  accessToken: string;
+}) {
+  const runFetch = async () =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+  let rawResponse = await runFetch();
+
+  let retryCount = 0;
+  while (await handleZendeskRateLimit(rawResponse)) {
+    rawResponse = await runFetch();
+    retryCount++;
+    if (retryCount >= ZENDESK_RATE_LIMIT_MAX_RETRIES) {
+      logger.info(
+        { response: rawResponse },
+        `[Zendesk] Rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES}, aborting.`
+      );
+      throw new Error(
+        `Zendesk rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES} times, aborting.`
+      );
+    }
+  }
+  const text = await rawResponse.text();
+  const response = JSON.parse(text);
+
+  if (!rawResponse.ok) {
+    if (response.type === "error.list" && response.errors?.length) {
+      const error = response.errors[0];
+      if (error.code === "unauthorized") {
+        throw new ExternalOAuthTokenError();
+      }
+      if (error.code === "not_found") {
+        return null;
+      }
+    }
+    logger.error({ response }, "[Zendesk] Zendesk API error");
+    throw new Error(`Zendesk API error: ${text}`);
+  }
+
+  return response;
+}
+
+/**
  * Fetches a batch of articles in a category from the Zendesk API.
  */
 export async function fetchZendeskArticlesInCategory({
@@ -117,56 +172,16 @@ export async function fetchZendeskArticlesInCategory({
     pageSize <= 100,
     `pageSize must be at most 100 (current value: ${pageSize})` // https://developer.zendesk.com/api-reference/introduction/pagination
   );
-  const runFetch = async () =>
-    fetch(
+
+  const response = await fetchFromZendeskWithRetries({
+    url:
       `https://${subdomain}.zendesk.com/api/v2/help_center/categories/${categoryId}/articles?page[size]=${pageSize}` +
-        (cursor ? `&page[after]=${cursor}` : ""),
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-  let rawResponse = await runFetch();
-
-  let retryCount = 0;
-  while (await handleZendeskRateLimit(rawResponse)) {
-    rawResponse = await runFetch();
-    retryCount++;
-    if (retryCount >= ZENDESK_RATE_LIMIT_MAX_RETRIES) {
-      logger.info(
-        { response: rawResponse },
-        `[Zendesk] Rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES}, aborting.`
-      );
-      throw new Error(
-        `Zendesk rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES} times, aborting.`
-      );
-    }
-  }
-
-  const text = await rawResponse.text();
-  const response = JSON.parse(text);
-
-  if (!rawResponse.ok) {
-    if (
-      response.type === "error.list" &&
-      response.errors &&
-      response.errors.length > 0
-    ) {
-      const error = response.errors[0];
-      if (error.code === "unauthorized") {
-        throw new ExternalOAuthTokenError();
-      }
-      if (error.code === "not_found") {
-        return { articles: [], meta: { has_more: false, after_cursor: "" } };
-      }
-    }
-  }
-
-  return response;
+      (cursor ? `&page[after]=${cursor}` : ""),
+    accessToken,
+  });
+  return (
+    response || { articles: [], meta: { has_more: false, after_cursor: "" } }
+  );
 }
 
 export async function fetchZendeskTicketsInBrand({
@@ -188,61 +203,20 @@ export async function fetchZendeskTicketsInBrand({
     `pageSize must be at most 100 (current value: ${pageSize})`
   );
 
-  const runFetch = async () =>
-    fetch(
+  const response = await fetchFromZendeskWithRetries({
+    url:
       `https://${brandSubdomain}.zendesk.com/api/v2/tickets?page[size]=${pageSize}` +
-        (cursor ? `&page[after]=${cursor}` : ""),
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
+      (cursor ? `&page[after]=${cursor}` : ""),
+    accessToken,
+  });
+
+  return response
+    ? {
+        tickets: response.tickets || [],
+        meta: {
+          has_more: !!response.meta?.has_more,
+          after_cursor: response.meta?.after_cursor || "",
         },
       }
-    );
-
-  let rawResponse = await runFetch();
-
-  let retryCount = 0;
-  while (await handleZendeskRateLimit(rawResponse)) {
-    rawResponse = await runFetch();
-    retryCount++;
-    if (retryCount >= ZENDESK_RATE_LIMIT_MAX_RETRIES) {
-      logger.info(
-        { response: rawResponse },
-        `[Zendesk] Rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES}, aborting.`
-      );
-      throw new Error(
-        `Zendesk rate limit hit more than ${ZENDESK_RATE_LIMIT_MAX_RETRIES} times, aborting.`
-      );
-    }
-  }
-
-  const text = await rawResponse.text();
-  const response = JSON.parse(text);
-
-  if (!rawResponse.ok) {
-    if (
-      response.type === "error.list" &&
-      response.errors &&
-      response.errors.length > 0
-    ) {
-      const error = response.errors[0];
-      if (error.code === "unauthorized") {
-        throw new ExternalOAuthTokenError();
-      }
-      if (error.code === "not_found") {
-        return { tickets: [], meta: { has_more: false, after_cursor: "" } };
-      }
-    }
-    throw new Error(`Zendesk API error: ${text}`);
-  }
-
-  return {
-    tickets: response.tickets || [],
-    meta: {
-      has_more: !!response.meta?.has_more,
-      after_cursor: response.meta?.after_cursor || "",
-    },
-  };
+    : { tickets: [], meta: { has_more: false, after_cursor: "" } };
 }
