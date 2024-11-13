e6256fc7df661578300720d5f1bc77cafaa7976a
Stanislas Polu <spolu@dust.tt>
2024-11-13 13:30:04 +0100

    nits

--- core/bin/init_db.rs
+++ core/bin/init_db.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use dust::{
     databases_store::store::PostgresDatabasesStore, oauth::store::PostgresOAuthStore,
     stores::postgres::PostgresStore,
