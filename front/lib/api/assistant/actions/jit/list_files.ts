ee51e661ad63d4b4b6eddb1741428abb97569ab0
Stanislas Polu <spolu@dust.tt>
2024-11-14 16:57:07 +0100

    simplify

--- front/lib/api/assistant/actions/jit/list_files.ts
+++ front/lib/api/assistant/actions/jit/list_files.ts
@@ -1,13 +1,12 @@
 import type {
+  AgentMessageType,
+  ConversationType,
   FunctionCallType,
   FunctionMessageTypeModel,
   JITFileType,
-  JITListFilesConfigurationType,
-  JITListFilesSuccessEvent,
+  JITListFilesActionType,
   ModelId,
 } from "@dust-tt/types";
-import type { AgentActionSpecification } from "@dust-tt/types";
-import type { Result } from "@dust-tt/types";
 import {
   BaseAction,
   getTablesQueryResultsFileTitle,
@@ -15,11 +14,6 @@ import {
   isContentFragmentType,
   isTablesQueryActionType,
 } from "@dust-tt/types";
-import { Ok } from "@dust-tt/types";
-
-import type { BaseActionRunParams } from "@app/lib/api/assistant/actions/types";
-import { BaseActionConfigurationServerRunner } from "@app/lib/api/assistant/actions/types";
-import type { Authenticator } from "@app/lib/auth";
 
 interface JITListFilesActionBlob {
   agentMessageId: ModelId;
@@ -57,8 +51,8 @@ export class JITListFilesAction extends BaseAction {
 
   renderForMultiActionsModel(): FunctionMessageTypeModel {
     let content = "CONVERSATION FILES:\n";
-    for (const file of this.files) {
-      content += `${file}\n`;
+    for (const f of this.files) {
+      content += `<file id="${f.fileId}" name="${f.title}" type="${f.contentType}" />\n`;
     }
 
     return {
@@ -70,75 +64,44 @@ export class JITListFilesAction extends BaseAction {
   }
 }
 
-/**
- * Params generation.
- */
+export function makeJITListFilesAction(
+  step: number,
+  agentMessage: AgentMessageType,
+  conversation: ConversationType
+): JITListFilesActionType {
+  const jitFiles: JITFileType[] = [];
 
-export class JITListFileConfigurationServerRunner extends BaseActionConfigurationServerRunner<JITListFilesConfigurationType> {
-  // Generates the action specification for generation of rawInputs passed to `run`.
-  async buildSpecification(
-    _auth: Authenticator,
-    { name, description }: { name: string; description: string | null }
-  ): Promise<Result<AgentActionSpecification, Error>> {
-    return new Ok({
-      name,
-      description:
-        description ||
-        "Retrieve the list of files attached to the conversation",
-      inputs: [],
-    });
-  }
-
-  async *run(
-    _auth: Authenticator,
-    {
-      agentConfiguration,
-      conversation,
-      agentMessage,
-      functionCallId,
-      step,
-    }: BaseActionRunParams
-  ): AsyncGenerator<JITListFilesSuccessEvent | void> {
-    const jitFiles: JITFileType[] = [];
-
-    for (const m of conversation.content.flat(1)) {
-      if (isContentFragmentType(m)) {
-        if (m.fileId) {
-          jitFiles.push({
-            fileId: m.fileId,
-            title: m.title,
-            contentType: m.contentType,
-          });
-        }
-      } else if (isAgentMessageType(m)) {
-        for (const a of m.actions) {
-          if (isTablesQueryActionType(a)) {
-            if (a.resultsFileId && a.resultsFileSnippet) {
-              jitFiles.push({
-                fileId: a.resultsFileId,
-                contentType: "text/csv",
-                title: getTablesQueryResultsFileTitle({ output: a.output }),
-              });
-            }
+  for (const m of conversation.content.flat(1)) {
+    if (isContentFragmentType(m)) {
+      if (m.fileId) {
+        jitFiles.push({
+          fileId: m.fileId,
+          title: m.title,
+          contentType: m.contentType,
+        });
+      }
+    } else if (isAgentMessageType(m)) {
+      for (const a of m.actions) {
+        if (isTablesQueryActionType(a)) {
+          if (a.resultsFileId && a.resultsFileSnippet) {
+            jitFiles.push({
+              fileId: a.resultsFileId,
+              contentType: "text/csv",
+              title: getTablesQueryResultsFileTitle({ output: a.output }),
+            });
           }
         }
       }
     }
-
-    yield {
-      type: "jit_list_files_success",
-      created: Date.now(),
-      configurationId: agentConfiguration.sId,
-      messageId: agentMessage.sId,
-      action: new JITListFilesAction({
-        functionCallId,
-        functionCallName: "list_conversation_files",
-        files: jitFiles,
-        agentMessageId: agentMessage.agentMessageId,
-        step: step,
-      }),
-    };
   }
+
+  return new JITListFilesAction({
+    functionCallId: "call_" + Math.random().toString(36).substring(7),
+    functionCallName: "list_conversation_files",
+    files: jitFiles,
+    agentMessageId: agentMessage.agentMessageId,
+    step: step,
+  });
 }
 
 /**
