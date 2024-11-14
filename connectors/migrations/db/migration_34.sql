134fbf35dacf1f5a5859db1232bd5fb0017b4680
Aubin <aubin@dust.tt>
2024-11-14 15:39:03 +0100

    feat: add the migration file

--- connectors/migrations/db/migration_34.sql
+++ connectors/migrations/db/migration_34.sql
@@ -1,2 +1,3 @@
 -- Migration created on Nov 14, 2024
-ALTER TABLE "public"."zendesk_configurations" ADD COLUMN "lastSuccessfulSyncStartTs" TIMESTAMP WITH TIME ZONE;
+ALTER TABLE "public"."zendesk_categories" ALTER COLUMN "description" TYPE TEXT;
+ALTER TABLE "public"."zendesk_tickets" ALTER COLUMN "subject" TYPE TEXT;
