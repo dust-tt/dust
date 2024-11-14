56ad0fb32f62d37fb17b1c8824f36545ccc44f56
Aubin <aubin@dust.tt>
2024-11-14 15:42:11 +0100

    feat: add the migration file

--- connectors/migrations/db/migration_34.sql
+++ connectors/migrations/db/migration_34.sql
@@ -1,3 +1,14 @@
 -- Migration created on Nov 14, 2024
+CREATE TABLE IF NOT EXISTS "zendesk_workspaces"
+(
+    "id"                        SERIAL,
+    "createdAt"                 TIMESTAMP WITH TIME ZONE NOT NULL,
+    "updatedAt"                 TIMESTAMP WITH TIME ZONE NOT NULL,
+    "lastSuccessfulSyncStartTs" TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    "connectorId"               INTEGER                  NOT NULL REFERENCES "connectors" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY ("id")
+);
+CREATE UNIQUE INDEX "zendesk_workspaces_connector_id" ON "zendesk_workspaces" ("connectorId");
+
 ALTER TABLE "public"."zendesk_categories" ALTER COLUMN "description" TYPE TEXT;
 ALTER TABLE "public"."zendesk_tickets" ALTER COLUMN "subject" TYPE TEXT;
