04b4d7b85a408d73ff2a200ae1f8837c797f6689
Aubin <aubin@dust.tt>
2024-11-15 17:16:23 +0100

    fix: set a default value for the ticketCreatedAt column

--- connectors/migrations/db/migration_36.sql
+++ connectors/migrations/db/migration_36.sql
@@ -1,3 +1,4 @@
 -- Migration created on Nov 15, 2024
-ALTER TABLE "public"."zendesk_tickets" ADD COLUMN "ticketCreatedAt" TIMESTAMP WITH TIME ZONE;
-ALTER TABLE "public"."zendesk_tickets" ALTER COLUMN "ticketCreatedAt" SET NOT NULL; -- empty table at time of migration
+ALTER TABLE public.zendesk_tickets ADD COLUMN "ticketCreatedAt" TIMESTAMP WITH TIME ZONE;
+UPDATE public.zendesk_tickets SET "ticketCreatedAt" = "createdAt";
+ALTER TABLE public.zendesk_tickets ALTER COLUMN "ticketCreatedAt" SET NOT NULL;
