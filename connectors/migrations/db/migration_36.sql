cd876000935a7a51a884f4469a9548a1df921796
Aubin <aubin@dust.tt>
2024-11-18 08:44:11 +0100

    refactor: merge both migration scripts into one

--- connectors/migrations/db/migration_36.sql
+++
@@ -1,4 +0,0 @@
--- Migration created on Nov 15, 2024
-ALTER TABLE public.zendesk_tickets ADD COLUMN "ticketCreatedAt" TIMESTAMP WITH TIME ZONE;
-UPDATE public.zendesk_tickets SET "ticketCreatedAt" = "createdAt";
-ALTER TABLE public.zendesk_tickets ALTER COLUMN "ticketCreatedAt" SET NOT NULL;
