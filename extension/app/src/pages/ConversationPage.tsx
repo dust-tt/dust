e93eaf7348471168b4cd2f01929c42eb26677ab9
Daphné Popin <PopDaph@users.noreply.github.com>
2024-11-19 11:16:45 +0100

    Extension: Rework nav headers (#8734)

--- extension/app/src/pages/ConversationPage.tsx
+++ extension/app/src/pages/ConversationPage.tsx
@@ -1,7 +1,11 @@
-import { BarHeader, Button, ExternalLinkIcon } from "@dust-tt/sparkle";
+import {
+  BarHeader,
+  Button,
+  ChevronLeftIcon,
+  ExternalLinkIcon,
+} from "@dust-tt/sparkle";
 import type { ProtectedRouteChildrenProps } from "@extension/components/auth/ProtectedRoute";
 import { ConversationContainer } from "@extension/components/conversation/ConversationContainer";
-import { ConversationsListButton } from "@extension/components/conversation/ConversationsListButton";
 import { FileDropProvider } from "@extension/components/conversation/FileUploaderContext";
 import { usePublicConversation } from "@extension/components/conversation/usePublicConversation";
 import { useNavigate, useParams } from "react-router-dom";
@@ -27,18 +31,22 @@ export const ConversationPage = ({
       <BarHeader
         title={conversation?.title || "Conversation"}
         tooltip={conversation?.title || "Conversation"}
+        leftActions={
+          <Button
+            icon={ChevronLeftIcon}
+            variant="ghost"
+            onClick={() => navigate(-1)}
+            size="md"
+          />
+        }
         rightActions={
           <div className="flex flex-row items-right">
-            <ConversationsListButton />
             <Button
               icon={ExternalLinkIcon}
               variant="ghost"
               href={`${process.env.DUST_DOMAIN}/w/${workspace.sId}/assistant/${conversationId}`}
               target="_blank"
-            />
-            <BarHeader.ButtonBar
-              variant="close"
-              onClose={() => navigate("/")}
+              size="md"
             />
           </div>
         }
