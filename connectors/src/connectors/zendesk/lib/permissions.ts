f632a80a11d84fa4512f3e7a699fb0874fde1108
Aubin <aubin@dust.tt>
2024-11-20 19:12:35 +0100

    fix: make the tickets expandable in some cases

--- connectors/src/connectors/zendesk/lib/permissions.ts
+++ connectors/src/connectors/zendesk/lib/permissions.ts
@@ -47,7 +47,7 @@ export async function retrieveAllSelectedNodes(
   const ticketNodes: ContentNode[] = brands
     .filter((brand) => brand.ticketsPermission === "read")
     .map((brand) => ({
-      ...brand.getTicketsContentNode(connectorId),
+      ...brand.getTicketsContentNode(connectorId, { expandable: true }),
       title: `${brand.name} - Tickets`, // adding the name of the brand since this will be named "Tickets" otherwise
     }));
 
