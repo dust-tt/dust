b89e5bdc0e9bf2b41967e5642771068b611255c3
Aubin <aubin@dust.tt>
2024-11-21 13:11:58 +0100

    :memo:

--- connectors/src/connectors/zendesk/temporal/config.ts
+++ connectors/src/connectors/zendesk/temporal/config.ts
@@ -3,4 +3,5 @@ export const QUEUE_NAME = `zendesk-queue-v${WORKFLOW_VERSION}`;
 export const GARBAGE_COLLECT_QUEUE_NAME = `zendesk-gc-queue-v${WORKFLOW_VERSION}`;
 
 // Batch size used when fetching from Zendesk API or from the database
+// 100 is the maximum value allowed for most endpoints in Zendesk: https://developer.zendesk.com/api-reference/introduction/pagination/
 export const ZENDESK_BATCH_SIZE = 100;
