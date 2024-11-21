11cd7d90ecd9cfc3ec1458cd5172e777198dc2d5
Aubin <aubin@dust.tt>
2024-11-21 14:27:21 +0100

    refactor: remove a duplicate function

--- connectors/src/connectors/zendesk/temporal/gc_activities.ts
+++ connectors/src/connectors/zendesk/temporal/gc_activities.ts
@@ -24,42 +24,6 @@ import {
   ZendeskConfigurationResource,
   ZendeskTicketResource,
 } from "@connectors/resources/zendesk_resources";
-import type { DataSourceConfig } from "@connectors/types/data_source_config";
-
-/**
- * Deletes all the data stored in the db and in the data source relative to a category (articles).
- */
-export async function deleteCategory({
-  connectorId,
-  categoryId,
-  dataSourceConfig,
-}: {
-  connectorId: number;
-  categoryId: number;
-  dataSourceConfig: DataSourceConfig;
-}) {
-  /// deleting the articles in the data source
-  const articles = await ZendeskArticleResource.fetchByCategoryId({
-    connectorId,
-    categoryId,
-  });
-  await concurrentExecutor(
-    articles,
-    (article) =>
-      deleteFromDataSource(
-        dataSourceConfig,
-        getArticleInternalId({ connectorId, articleId: article.articleId })
-      ),
-    { concurrency: 10 }
-  );
-  /// deleting the articles stored in the db
-  await ZendeskArticleResource.deleteByCategoryId({
-    connectorId,
-    categoryId,
-  });
-  // deleting the category stored in the db
-  await ZendeskCategoryResource.deleteByCategoryId({ connectorId, categoryId });
-}
 
 /**
  * Looks for empty Help Centers (no category with read permissions) and removes their permissions.
