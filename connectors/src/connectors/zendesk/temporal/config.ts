ad0acff770dda0e32fc9387c148b68760a45ff55
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2024-11-21 15:33:50 +0100

    [connectors] Refactor a few Zendesk activities return values (#8807)
    * :recycle: turn simple boolean into objects to name the values
    
    * bump the workflow version
    
    * fix: allow syncing back tickets

--- connectors/src/connectors/zendesk/temporal/config.ts
+++ connectors/src/connectors/zendesk/temporal/config.ts
@@ -1,4 +1,4 @@
-export const WORKFLOW_VERSION = 8;
+export const WORKFLOW_VERSION = 9;
 export const QUEUE_NAME = `zendesk-queue-v${WORKFLOW_VERSION}`;
 export const GARBAGE_COLLECT_QUEUE_NAME = `zendesk-gc-queue-v${WORKFLOW_VERSION}`;
 
