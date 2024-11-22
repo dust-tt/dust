c90ba7b079960209b96a69ea73695e3331849e7f
Aubin <aubin@dust.tt>
2024-11-22 16:23:56 +0100

    :triangular_flag_on_post: update the migration script without the drop

--- connectors/migrations/db/migration_37.sql
+++ connectors/migrations/db/migration_37.sql
@@ -4,7 +4,4 @@ ALTER TABLE "zendesk_categories" ALTER COLUMN "name" TYPE TEXT;
 ALTER TABLE "zendesk_categories" ALTER COLUMN "url" TYPE TEXT;
 ALTER TABLE "zendesk_articles" ALTER COLUMN "name" TYPE TEXT;
 ALTER TABLE "zendesk_articles" ALTER COLUMN "url" TYPE TEXT;
-ALTER TABLE "public"."zendesk_tickets" DROP COLUMN "groupId";
-ALTER TABLE "public"."zendesk_tickets" DROP COLUMN "assigneeId";
-ALTER TABLE "public"."zendesk_tickets" DROP COLUMN "organizationId";
 ALTER TABLE "zendesk_tickets" ALTER COLUMN "url" TYPE TEXT;
