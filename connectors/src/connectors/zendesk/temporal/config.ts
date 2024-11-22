e7cd3100d5d89242b178a6286e5b335869513e67
Aubin <aubin@dust.tt>
2024-11-22 10:29:21 +0100

    :wrench: lower the batch size of data fetched from Zendesk

--- connectors/src/connectors/zendesk/temporal/config.ts
+++ connectors/src/connectors/zendesk/temporal/config.ts
@@ -4,4 +4,4 @@ export const GARBAGE_COLLECT_QUEUE_NAME = `zendesk-gc-queue-v${WORKFLOW_VERSION}
 
 // Batch size used when fetching from Zendesk API or from the database
 // 100 is the maximum value allowed for most endpoints in Zendesk: https://developer.zendesk.com/api-reference/introduction/pagination/
-export const ZENDESK_BATCH_SIZE = 100;
+export const ZENDESK_BATCH_SIZE = 30;
