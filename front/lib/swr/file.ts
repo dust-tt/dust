8f3a32e4997ee0f26c230da86af32e683cfdc25a
Lucas <lucas@Dusts-MacBook-Pro.local>
2024-11-22 18:18:16 +0100

    Applied PR comments

--- front/lib/swr/file.ts
+++ front/lib/swr/file.ts
@@ -16,7 +16,7 @@ export function useFileProcessedContent(
     mutate,
   } = useSWRWithDefaults(
     `/api/w/${owner.sId}/files/${fileId}?action=view&version=processed`,
-    // Stream fetcher -> don;t try to parse the stream
+    // Stream fetcher -> don't try to parse the stream
     // Wait for initial response to trigger swr error handling
     async (...args) => {
       const response = await fetch(...args);
@@ -30,7 +30,7 @@ export function useFileProcessedContent(
 
   return {
     // Do not extract text from the response -> Allows streaming on client side
-    content: () => (response ? response : null),
+    content: () => response ?? null,
     isContentLoading: config?.disabled ? false : !error && !response,
     isContentError: config?.disabled ? false : error,
     mutateFileProcessedContent: mutate,
