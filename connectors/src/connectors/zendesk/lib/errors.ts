5082873be0d5b0a61771e267ad9e626ef315615e
Aubin <aubin@dust.tt>
2024-11-26 16:43:17 +0100

    add a few error types and checks

--- connectors/src/connectors/zendesk/lib/errors.ts
+++ connectors/src/connectors/zendesk/lib/errors.ts
@@ -7,6 +7,11 @@ interface NodeZendeskError extends Error {
   result: string | null;
 }
 
+interface ZendeskApiError extends Error {
+  status: number;
+  description: string | null;
+}
+
 export function isNodeZendeskForbiddenError(
   err: unknown
 ): err is NodeZendeskError {
@@ -15,5 +20,28 @@ export function isNodeZendeskForbiddenError(
     err !== null &&
     "statusCode" in err &&
     err.statusCode === 403
+  );
+}
+
+export function isZendeskExpiredCursorError(
+  err: unknown
+): err is ZendeskApiError {
+  return (
+    typeof err === "object" &&
+    err !== null &&
+    "status" in err &&
+    err.status === 422 &&
+    "description" in err &&
+    typeof err.description === "string" &&
+    err.description.includes("Invalid search: cursor has expired")
+  );
+}
+
+export function isZendeskEpipeError(err: unknown): err is NodeZendeskError {
+  return (
+    typeof err === "object" &&
+    err !== null &&
+    "code" in err &&
+    err.code === "EPIPE"
   );
 }
