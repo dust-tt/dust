e4addef3c1faa583033c315ba0bbd1c03cdde3c2
Henry Fontanier <h.fontanier@gmail.com>
2024-11-26 16:06:32 +0100

    enh(zendesk): reduce batch size and add heartbeat (#8910)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- connectors/src/connectors/zendesk/temporal/config.ts
+++ connectors/src/connectors/zendesk/temporal/config.ts
@@ -3,4 +3,4 @@ export const QUEUE_NAME = `zendesk-queue-v${WORKFLOW_VERSION}`;
 
 // Batch size used when fetching from Zendesk API or from the database
 // 100 is the maximum value allowed for most endpoints in Zendesk: https://developer.zendesk.com/api-reference/introduction/pagination/
-export const ZENDESK_BATCH_SIZE = 50;
+export const ZENDESK_BATCH_SIZE = 30;
