f0685e5f033bebca275f681878eb8e2040d8ec06
Alban Dumouilla <alban@dust.tt>
2024-11-27 12:22:06 +0100

    Add SET NULL on users FK

--- front/migrations/db/migration_122.sql
+++ front/migrations/db/migration_122.sql
@@ -5,3 +5,11 @@ ALTER TABLE "agent_message_feedbacks" ADD COLUMN "agentConfigurationVersion" INT
 
 ALTER TABLE "agent_message_feedbacks" ALTER COLUMN "workspaceId" SET NOT NULL;
 ALTER TABLE "agent_message_feedbacks"  ADD FOREIGN KEY ("workspaceId") REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "agent_message_feedbacks" 
+DROP CONSTRAINT "agent_message_feedbacks_userId_fkey",
+ADD CONSTRAINT "agent_message_feedbacks_userId_fkey" 
+    FOREIGN KEY ("userId") 
+    REFERENCES "users"("id") 
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE;
