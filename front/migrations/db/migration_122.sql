3b484aa3d2402212a4fd7234853d25ee850aa00e
Alban Dumouilla <alban@dust.tt>
2024-11-27 10:25:44 +0100

    Add workspaceId FK

--- front/migrations/db/migration_122.sql
+++ front/migrations/db/migration_122.sql
@@ -1,4 +1,7 @@
--- First drop the column
+-- First drop the column,
 ALTER TABLE "agent_message_feedbacks" DROP COLUMN "agentConfigurationId";
 ALTER TABLE "agent_message_feedbacks" ADD COLUMN "agentConfigurationId" VARCHAR(255);
 ALTER TABLE "agent_message_feedbacks" ADD COLUMN "agentConfigurationVersion" INTEGER;
+
+ALTER TABLE "agent_message_feedbacks" ALTER COLUMN "workspaceId" SET NOT NULL;
+ALTER TABLE "agent_message_feedbacks"  ADD FOREIGN KEY ("workspaceId") REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
