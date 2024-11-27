15fbb9668b3ec323d791739f1f693b7e998c3a0a
Daphné Popin <daphne.popin@gmail.com>
2024-11-27 10:22:34 +0100

    Fix padding input bar

--- extension/app/src/components/input_bar/InputBarContainer.tsx
+++ extension/app/src/components/input_bar/InputBarContainer.tsx
@@ -69,7 +69,7 @@ export const InputBarContainer = ({
 
   const contentEditableClasses = classNames(
     "inline-block w-full",
-    "border-0 pr-1 pl-2 sm:pl-0 outline-none ring-0 focus:border-0 focus:outline-none focus:ring-0 py-3.5",
+    "border-0 pr-1 pl-2 sm:pl-0 outline-none ring-0 focus:border-0 focus:outline-none focus:ring-0",
     "whitespace-pre-wrap font-normal"
   );
 
