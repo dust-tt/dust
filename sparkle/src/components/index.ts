38a7ee24ee0feee57828fea63b539128ce53fac2
pinotalexandre <hialexpinot@gmail.com>
2024-11-28 11:27:18 +0100

    fix issue

--- sparkle/src/components/index.ts
+++ sparkle/src/components/index.ts
@@ -66,7 +66,7 @@ export { Label } from "./Label";
 export type { LinkWrapperProps } from "./LinkWrapper";
 export { LinkWrapper } from "./LinkWrapper";
 export * from "./markdown";
-export { MetricCard } from './MetricCard';
+export * from './MetricCard';
 export { Modal } from "./Modal";
 export {
   NavigationList,
