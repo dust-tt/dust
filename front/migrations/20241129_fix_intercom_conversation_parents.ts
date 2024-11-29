9d1d9d50d28fd0b87bb461c502564ce87e7b9d68
Aubin <aubin@dust.tt>
2024-11-29 14:15:48 +0100

    use replacements in queries of the backfill script

--- front/migrations/20241129_fix_intercom_conversation_parents.ts
+++ front/migrations/20241129_fix_intercom_conversation_parents.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { QueryTypes, Sequelize } from "sequelize";
 
 import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
 import { makeScript } from "@app/scripts/helpers";
@@ -17,30 +17,43 @@ makeScript({}, async ({ execute }, logger) => {
   for (const ds of frontIntercomDataSources) {
     logger.info(`Processing data source ${ds.id}`);
 
-    const queryWhere = `WHERE document_id <> ALL(parents)
-    AND EXISTS (
-      SELECT 1 FROM data_sources
-      WHERE data_sources.id = data_sources_documents.data_source
-      AND data_sources.data_source_id = ${ds.dustAPIDataSourceId}
-      AND data_sources.project = ${ds.dustAPIProjectId}
-    )`;
+    const queryWhere = `
+      WHERE document_id <> ALL(parents)
+      AND EXISTS (
+        SELECT 1 FROM data_sources
+        WHERE data_sources.id = data_sources_documents.data_source
+        AND data_sources.data_source_id = :dataSourceId
+        AND data_sources.project = :projectId
+      )`;
 
     if (execute) {
       const query = `
-    UPDATE data_sources_documents 
-    SET parents = array_prepend(document_id, parents) 
-    ${queryWhere};`;
-      console.log(`Running the following query on core: ${query}`);
+          UPDATE data_sources_documents
+          SET parents = array_prepend(document_id, parents)
+              ${queryWhere};`;
 
-      await coreSequelize.query(query);
+      console.log(`Running the following query on core: ${query}`);
+      await coreSequelize.query(query, {
+        replacements: {
+          dataSourceId: ds.dustAPIDataSourceId,
+          projectId: ds.dustAPIProjectId,
+        },
+      });
     } else {
       const query = `
-    SELECT document_id, parents
-    FROM data_sources_documents 
-    ${queryWhere};`;
-      console.log(`Running the following query on core: ${query}`);
+          SELECT document_id, parents
+          FROM data_sources_documents
+                   ${queryWhere};`;
 
-      const [results] = await coreSequelize.query(query);
+      console.log(`Running the following query on core: ${query}`);
+      const results = await coreSequelize.query(query, {
+        replacements: {
+          dataSourceId: ds.dustAPIDataSourceId,
+          projectId: ds.dustAPIProjectId,
+        },
+        type: QueryTypes.SELECT,
+      });
+
       console.log(`Would update ${results.length} documents`);
       console.log("Sample of affected rows:", results.slice(0, 5));
     }
