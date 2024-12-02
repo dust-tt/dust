5b1b6941077f55fec97755626b13557b4de72808
Stanislas Polu <spolu@dust.tt>
2024-12-02 17:54:07 +0100

    fix migration

--- front/migrations/db/migration_123.sql
+++ front/migrations/db/migration_123.sql
@@ -1,3 +1,3 @@
 -- Migration created on Dec 02, 2024
-ALTER TABLE "data_sources" ALTER COLUMN "editedByUserId" SET NOT NULL;
-ALTER TABLE "data_sources_views" ALTER COLUMN "editedByUserId" SET NOT NULL;
+ALTER TABLE "data_sources" ALTER COLUMN "editedByUserId" DROP NOT NULL;
+ALTER TABLE "data_source_views" ALTER COLUMN "editedByUserId" DROP NOT NULL;
