f099f607f6215d2d244d1ada8e7544aa81adb2f8
Thomas Draier <thomas@dust.tt>
2024-12-02 11:47:24 +0100

    keep searchfilter as is

--- core/src/search_filter.rs
+++ core/src/search_filter.rs
@@ -71,7 +71,7 @@ where
 pub struct SearchFilter {
     pub tags: Option<TagsFilter>,
     pub parents: Option<ParentsFilter>,
-    #[serde(default, deserialize_with = "deserialize_timestamp_filter")]
+    #[serde(deserialize_with = "deserialize_timestamp_filter")]
     pub timestamp: Option<TimestampFilter>,
 }
 
