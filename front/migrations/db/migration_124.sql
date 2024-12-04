f7a15f0325f9ccc3a9ab7caf698ded392352a6dd
JulesBelveze <jules.belveze@hotmail.fr>
2024-12-04 15:18:40 +0100

    [front/db] - fix: adjust foreign key constraint on retrieval_documents
     - Updated the foreign key constraint name to use the correct case sensitivity
     - Modified retrievalActionId column to allow null values and set related records to null on deletion of foreign reference

--- front/migrations/db/migration_124.sql
+++ front/migrations/db/migration_124.sql
@@ -1,11 +1,11 @@
 -- Drop existing foreign key constraint
 ALTER TABLE retrieval_documents
-DROP CONSTRAINT retrieval_documents_retrievalactionid_fkey;
+DROP CONSTRAINT "retrieval_documents_retrievalActionid_fkey";
 
 -- Allow null and add new foreign key with SET NULL
 ALTER TABLE retrieval_documents
 ALTER COLUMN "retrievalActionId" DROP NOT NULL,
-ADD CONSTRAINT retrieval_documents_retrievalActionId_fkey
+ADD CONSTRAINT "retrieval_documents_retrievalActionid_fkey"
   FOREIGN KEY ("retrievalActionId")
   REFERENCES agent_retrieval_actions(id)
   ON DELETE SET NULL;
