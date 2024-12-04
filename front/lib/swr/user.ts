da9dd1800e1cc87d68611ba4025385f1954adf71
Lucas <lucas@dust.tt>
2024-12-04 18:23:42 +0100

    Skeleton is here, need to bring the configurations history endpoint

--- front/lib/swr/user.ts
+++ front/lib/swr/user.ts
@@ -2,6 +2,7 @@ import type { Fetcher } from "swr";
 
 import { fetcher, useSWRWithDefaults } from "@app/lib/swr/swr";
 import type { GetUserResponseBody } from "@app/pages/api/user";
+import type { GetUserDetailsResponseBody } from "@app/pages/api/user/[uId]/details";
 import type { GetUserMetadataResponseBody } from "@app/pages/api/user/metadata/[key]";
 
 export function useUser() {
@@ -28,5 +29,19 @@ export function useUserMetadata(key: string) {
     isMetadataLoading: !error && !data,
     isMetadataError: error,
     mutateMetadata: mutate,
+  };
+}
+
+export function useUserDetails(userId: number) {
+  const userFetcher: Fetcher<GetUserDetailsResponseBody> = fetcher;
+  const { data, error } = useSWRWithDefaults(
+    `/api/user/${userId}/details`,
+    userFetcher
+  );
+
+  return {
+    userDetails: data,
+    isUserDetailsLoading: !error && !data,
+    isUserDetailsError: error,
   };
 }
