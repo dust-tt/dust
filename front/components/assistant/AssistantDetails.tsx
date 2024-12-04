153f771f4099fed6385b54af8df628b1002deafd
Lucas <lucas@dust.tt>
2024-12-04 18:23:42 +0100

    Skeleton is here, need to bring the configurations history endpoint

--- front/components/assistant/AssistantDetails.tsx
+++ front/components/assistant/AssistantDetails.tsx
@@ -12,7 +12,11 @@ import {
   TabsList,
   TabsTrigger,
 } from "@dust-tt/sparkle";
-import type { AgentConfigurationScope, WorkspaceType } from "@dust-tt/types";
+import type {
+  AgentConfigurationScope,
+  LightAgentConfigurationType,
+  WorkspaceType,
+} from "@dust-tt/types";
 import { useCallback, useState } from "react";
 
 import { AssistantDetailsButtonBar } from "@app/components/assistant/AssistantDetailsButtonBar";
@@ -20,11 +24,14 @@ import { AssistantActionsSection } from "@app/components/assistant/details/Assis
 import { AssistantUsageSection } from "@app/components/assistant/details/AssistantUsageSection";
 import { ReadOnlyTextArea } from "@app/components/assistant/ReadOnlyTextArea";
 import { SharingDropdown } from "@app/components/assistant_builder/Sharing";
+import type { AgentMessageFeedbackType } from "@app/lib/api/assistant/feedback";
 import {
   useAgentConfiguration,
   useAgentConfigurationFeedbacks,
+  useAgentConfigurations,
   useUpdateAgentScope,
 } from "@app/lib/swr/assistants";
+import { useUserDetails } from "@app/lib/swr/user";
 import { classNames, timeAgoFrom } from "@app/lib/utils";
 
 type AssistantDetailsProps = {
@@ -164,63 +171,51 @@ export function AssistantDetails({
       isAgentConfigurationFeedbacksLoading,
     } = useAgentConfigurationFeedbacks({
       workspaceId: owner.sId,
-      agentConfigurationId: assistantId,
+      agentConfigurationId: assistantId ?? "",
     });
 
-    return isAgentConfigurationFeedbacksLoading ? (
+    // TODO This doesn't return old versions. replace by useAgentConversationHistory from
+    // https://github.com/dust-tt/dust/blob/1725-prompt-versioning/front/components/assistant_builder/InstructionScreen.tsx#L166
+    const { agentConfigurations, isAgentConfigurationsLoading } =
+      useAgentConfigurations({
+        workspaceId: owner.sId,
+        agentsGetView: { agentIds: [assistantId || ""], allVersions: true },
+        disabled: !assistantId,
+      });
+
+    return isAgentConfigurationFeedbacksLoading ||
+      isAgentConfigurationsLoading ? (
       <Spinner />
     ) : (
       <div>
         {!agentConfigurationFeedbacks ||
-        agentConfigurationFeedbacks.length === 0 ? (
+        agentConfigurationFeedbacks.length === 0 ||
+        !assistantId ? (
           <div>No feedbacks.</div>
         ) : (
           <div className="mt-3">
-            <Page.H variant="h6">
-              Latest version ({agentConfiguration.version})
-            </Page.H>
+            <ConfigVersionHeader
+              agentConfiguration={agentConfiguration}
+              agentConfigurationVersion={agentConfiguration.version}
+              isLatestVersion={true}
+            />
             {agentConfigurationFeedbacks.map((feedback, index) => (
               <div key={feedback.id}>
                 {index > 0 &&
                   feedback.agentConfigurationVersion !==
                     agentConfigurationFeedbacks[index - 1]
                       .agentConfigurationVersion && (
-                    <Page.H variant="h6">
-                      Version {feedback.agentConfigurationVersion}
-                    </Page.H>
-                  )}
-                <ContentMessage variant="slate" className="my-2">
-                  <div className="justify-content-around mb-3 flex items-center gap-2">
-                    <div className="flex w-full items-center gap-2">
-                      <Avatar size="xs" name="Eleanor Wright" /> Eleanor Wright
-                    </div>
-                    <div className="flex-shrink-0 text-xs text-muted-foreground">
-                      {timeAgoFrom(
-                        feedback.createdAt instanceof Date
-                          ? feedback.createdAt.getTime()
-                          : new Date(feedback.createdAt).getTime(),
-                        {
-                          useLongFormat: true,
-                        }
-                      )}{" "}
-                      ago
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <div className="flex-grow">{feedback.content}</div>
-                    <div className="flex-shrink-0">
-                      {feedback.thumbDirection === "up" ? (
-                        <button className="rounded bg-sky-200 p-2">
-                          <HandThumbUpIcon />
-                        </button>
-                      ) : (
-                        <button className="rounded bg-warning-200 p-2">
-                          <HandThumbDownIcon />
-                        </button>
+                    <ConfigVersionHeader
+                      agentConfiguration={agentConfigurations.find(
+                        (c) => c.version === feedback.agentConfigurationVersion
                       )}
-                    </div>
-                  </div>
-                </ContentMessage>
+                      agentConfigurationVersion={
+                        feedback.agentConfigurationVersion
+                      }
+                      isLatestVersion={false}
+                    />
+                  )}
+                <FeedbackCard feedback={feedback} />
               </div>
             ))}
           </div>
@@ -242,5 +237,69 @@ export function AssistantDetails({
         <TabsSection />
       </div>
     </ElementModal>
+  );
+}
+
+function ConfigVersionHeader({
+  agentConfigurationVersion,
+  agentConfiguration,
+  isLatestVersion,
+}: {
+  agentConfigurationVersion: number;
+  agentConfiguration: LightAgentConfigurationType | undefined;
+  isLatestVersion: boolean;
+}) {
+  return (
+    <div className="flex items-center gap-2">
+      <Page.H variant="h6">
+        {agentConfigurationVersion} : {agentConfiguration?.versionCreatedAt} (
+        {isLatestVersion ? "Latest" : ""})
+      </Page.H>
+    </div>
+  );
+}
+
+function FeedbackCard({ feedback }: { feedback: AgentMessageFeedbackType }) {
+  const { userDetails } = useUserDetails(feedback.userId);
+  console.log(userDetails);
+
+  return (
+    <ContentMessage variant="slate" className="my-2">
+      <div className="justify-content-around mb-3 flex items-center gap-2">
+        <div className="flex w-full items-center gap-2">
+          <Avatar
+            size="xs"
+            visual={userDetails?.image}
+            name={userDetails?.firstName || "?"}
+          />
+          {userDetails?.firstName} {userDetails?.lastName}
+        </div>
+        <div className="flex-shrink-0 text-xs text-muted-foreground">
+          {timeAgoFrom(
+            feedback.createdAt instanceof Date
+              ? feedback.createdAt.getTime()
+              : new Date(feedback.createdAt).getTime(),
+            {
+              useLongFormat: true,
+            }
+          )}{" "}
+          ago
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="flex-grow">{feedback.content}</div>
+        <div className="flex-shrink-0">
+          {feedback.thumbDirection === "up" ? (
+            <button className="rounded bg-sky-200 p-2">
+              <HandThumbUpIcon />
+            </button>
+          ) : (
+            <button className="rounded bg-warning-200 p-2">
+              <HandThumbDownIcon />
+            </button>
+          )}
+        </div>
+      </div>
+    </ContentMessage>
   );
 }
