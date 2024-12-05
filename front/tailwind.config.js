67a9c5ae6ad23881cb9464ecc1d284ae8c140cec
Jules Belveze <32683010+JulesBelveze@users.noreply.github.com>
2024-12-05 20:51:38 +0100

    [front] - fix(AddToSpaceDialog): make content scrollable (#9169)
    * [front/components/spaces] - feature: extend AddToSpaceDialog with scrollable dropdown
    
     - Implemented a scrollable area for the space selection dropdown menu
     - Added repeated space items to simulate a longer list for testing the scrollbar functionality
    
    * [front] - refactor: adjust dropdown menu styles for consistency
    
    - Use Tailwind CSS predefined classes for min-width and max-height to maintain style consistency
    - Enhance maintainability by referencing Tailwind's spacing theme for minWidth configuration in Tailwind settings
    
    * fix: lint/format
    
    * [front] - fix: remove unnecessary min-width from icons
    
     - Min-width property on execution status and information icons is removed for consistent styling

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -33,6 +33,9 @@ module.exports = {
       zIndex: {
         60: "60",
       },
+      minWidth: (theme) => ({
+        ...theme("spacing"),
+      }),
       minHeight: (theme) => ({
         ...theme("spacing"),
       }),
