15dcb227c2ad6edde00707288e8aa052b17381fe
JulesBelveze <jules.belveze@hotmail.fr>
2024-12-06 17:42:33 +0100

    fix: lint/format

--- front/lib/webcrawler.ts
+++ front/lib/webcrawler.ts
@@ -31,7 +31,7 @@ export function urlToDataSourceName(url: string) {
 }
 
 const urlValidationRegex = new RegExp(
-    "^(https?:\\/\\/)?" + // validate protocol
+  "^(https?:\\/\\/)?" + // validate protocol
     "((([a-z\\d@]([a-z\\d@-]*[a-z\\d@])*)\\.)+[a-z]{2,}|" + // validate domain name with @ allowed
     "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
     "(\\:\\d+)?(\\/[-a-z\\d%@_.~+]*)*" + // validate port and path
