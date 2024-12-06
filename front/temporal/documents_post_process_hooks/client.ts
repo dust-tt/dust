16ed7168dbdd584d9d8e5b0cd8df931ca1688ec5
Henry Fontanier <henry@dust.tt>
2024-12-06 16:53:16 +0100

    chore: drain docs post process hooks queue

--- front/temporal/documents_post_process_hooks/client.ts
+++ front/temporal/documents_post_process_hooks/client.ts
@@ -1,14 +1,6 @@
 import type { ConnectorProvider } from "@dust-tt/types";
 
 import type { DocumentsPostProcessHookType } from "@app/lib/documents_post_process_hooks/hooks";
-import { getTemporalClient } from "@app/lib/temporal";
-import { newUpsertSignal } from "@app/temporal/documents_post_process_hooks/signals";
-import {
-  runPostDeleteHoosWorkflow,
-  runPostUpsertHooksWorkflow,
-} from "@app/temporal/documents_post_process_hooks/workflows";
-
-const QUEUE_NAME = "post-upsert-hooks-queue"; // TODO: rename to post-process-hooks-queue
 
 export async function launchRunPostUpsertHooksWorkflow(
   workspaceId: string,
@@ -19,23 +11,30 @@ export async function launchRunPostUpsertHooksWorkflow(
   hookType: DocumentsPostProcessHookType,
   debounceMs: number
 ) {
-  const client = await getTemporalClient();
+  void workspaceId;
+  void dataSourceId;
+  void documentId;
+  void documentHash;
+  void dataSourceConnectorProvider;
+  void hookType;
+  void debounceMs;
 
-  await client.workflow.signalWithStart(runPostUpsertHooksWorkflow, {
-    args: [
-      workspaceId,
-      dataSourceId,
-      documentId,
-      documentHash,
-      dataSourceConnectorProvider,
-      hookType,
-      debounceMs,
-    ],
-    taskQueue: QUEUE_NAME,
-    workflowId: `workflow-run-post-upsert-hooks-${hookType}-${workspaceId}-${dataSourceId}-${documentId}`,
-    signal: newUpsertSignal,
-    signalArgs: undefined,
-  });
+  // const client = await getTemporalClient();
+  // await client.workflow.signalWithStart(runPostUpsertHooksWorkflow, {
+  //   args: [
+  //     workspaceId,
+  //     dataSourceId,
+  //     documentId,
+  //     documentHash,
+  //     dataSourceConnectorProvider,
+  //     hookType,
+  //     debounceMs,
+  //   ],
+  //   taskQueue: QUEUE_NAME,
+  //   workflowId: `workflow-run-post-upsert-hooks-${hookType}-${workspaceId}-${dataSourceId}-${documentId}`,
+  //   signal: newUpsertSignal,
+  //   signalArgs: undefined,
+  // });
 }
 
 export async function launchRunPostDeleteHooksWorkflow(
@@ -45,17 +44,23 @@ export async function launchRunPostDeleteHooksWorkflow(
   dataSourceConnectorProvider: ConnectorProvider | null,
   hookType: DocumentsPostProcessHookType
 ) {
-  const client = await getTemporalClient();
+  void workspaceId;
+  void dataSourceId;
+  void documentId;
+  void dataSourceConnectorProvider;
+  void hookType;
 
-  await client.workflow.start(runPostDeleteHoosWorkflow, {
-    args: [
-      dataSourceId,
-      workspaceId,
-      documentId,
-      dataSourceConnectorProvider,
-      hookType,
-    ],
-    taskQueue: QUEUE_NAME,
-    workflowId: `workflow-run-post-delete-hooks-${hookType}-${workspaceId}-${dataSourceId}-${documentId}`,
-  });
+  // const client = await getTemporalClient();
+
+  // await client.workflow.start(runPostDeleteHoosWorkflow, {
+  //   args: [
+  //     dataSourceId,
+  //     workspaceId,
+  //     documentId,
+  //     dataSourceConnectorProvider,
+  //     hookType,
+  //   ],
+  //   taskQueue: QUEUE_NAME,
+  //   workflowId: `workflow-run-post-delete-hooks-${hookType}-${workspaceId}-${dataSourceId}-${documentId}`,
+  // });
 }
