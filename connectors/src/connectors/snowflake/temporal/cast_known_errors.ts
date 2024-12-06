58da8f11162e73b8cf6883db1585fe7ab3b6153b
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2024-12-06 14:36:42 +0100

    fix(snowflake) - fix snowflake interceptor (#9190)
    * fix: fix snowflake interceptor once again
    
    * fix: fix interface SnowflakeExpiredPasswordError

--- connectors/src/connectors/snowflake/temporal/cast_known_errors.ts
+++ connectors/src/connectors/snowflake/temporal/cast_known_errors.ts
@@ -8,17 +8,23 @@ import { ExternalOAuthTokenError } from "@connectors/lib/error";
 
 interface SnowflakeExpiredPasswordError extends Error {
   code: number;
-  name: string;
+  name: "OperationFailedError";
+  data: {
+    nextAction: "PWD_CHANGE";
+  };
 }
 
 function isSnowflakeExpiredPasswordError(
   err: unknown
 ): err is SnowflakeExpiredPasswordError {
   return (
-    typeof err === "object" &&
-    err !== null &&
-    "code" in err &&
-    err.code === 390106 // this is the magic code number for an expired password error, the message says "Specified password has expired.  Password must be changed using the Snowflake web console."
+    err instanceof Error &&
+    err.name === "OperationFailedError" &&
+    "data" in err &&
+    typeof err.data === "object" &&
+    err.data !== null &&
+    "nextAction" in err.data &&
+    err.data.nextAction === "PWD_CHANGE"
   );
 }
 
