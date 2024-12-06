563528bb49ab9721122bb3d2f7d34cc06b7a3bd5
Aubin <aubin@dust.tt>
2024-12-06 15:08:06 +0100

    feat: add a connectorsIn option

--- connectors/migrations/20241205_check_confluence_modified_pages.ts
+++ connectors/migrations/20241205_check_confluence_modified_pages.ts
@@ -19,22 +19,35 @@ makeScript(
       default: 60 * 60 * 1000,
       description: "Size of the time window in ms.",
     },
-    connectorsToSkip: {
+    connectorsOut: {
       type: "array",
       demandOption: false,
       default: [],
       description: "IDs of the connectors to skip.",
     },
+    connectorsIn: {
+      type: "array",
+      demandOption: false,
+      default: [],
+      description: "IDs of the connectors to include.",
+    },
   },
-  async ({ timeWindowMs, connectorsToSkip }) => {
+  async ({ timeWindowMs, connectorsOut, connectorsIn }) => {
     const connectors = await ConnectorResource.listByType("confluence", {});
-    const skippedConnectorIdsAsStrings = connectorsToSkip.map(
+    const skippedConnectorIdsAsStrings = connectorsOut.map(
       (id) => id.toString() // we can actually get numbers from the CLI
     );
+    const connectorsToInclude = connectorsIn.map((id) => id.toString());
 
     const startDate = new Date(Date.now() - timeWindowMs);
 
     for (const connector of connectors) {
+      if (
+        connectorsToInclude.length > 0 ||
+        !connectorsToInclude.includes(connector.id.toString())
+      ) {
+        continue;
+      }
       if (skippedConnectorIdsAsStrings.includes(connector.id.toString())) {
         console.log(`-- Skipping connector ${connector.id}`);
         continue;
@@ -81,7 +94,7 @@ makeScript(
             (page) => new Date(page.version.createdAt) >= startDate
           );
           console.log(
-            `${recentlyModifiedPages.length} pages out of ${allPages.length} modified in the last hour for space ${spaceId}`
+            `${recentlyModifiedPages.length} pages modified in the last ${Math.floor(timeWindowMs / 1000)} s for space ${spaceId}`
           );
           connectorCount += recentlyModifiedPages.length;
         }
