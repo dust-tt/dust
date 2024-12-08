6b50d30acd96782ce7c1d831e94237350da8d867
filou <philipperolet@gmail.com>
2024-12-08 18:34:50 +0100

    cleaned script

--- core/bin/elasticsearch/create_index.rs
+++ core/bin/elasticsearch/create_index.rs
@@ -1,46 +1,67 @@
 use std::collections::HashMap;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use elasticsearch::auth::Credentials;
 use elasticsearch::http::transport::{SingleNodeConnectionPool, TransportBuilder};
 use elasticsearch::indices::{IndicesCreateParts, IndicesExistsParts};
 use elasticsearch::Elasticsearch;
+use http::StatusCode;
 use url::Url;
+
+#[derive(Parser, Debug, Clone, ValueEnum)]
+enum Region {
+    Local,
+    #[clap(name = "us-central-1")]
+    UsCentral1,
+}
+
+impl std::fmt::Display for Region {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Region::Local => write!(f, "local"),
+            Region::UsCentral1 => write!(f, "us-central-1"),
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(help = "The index name (without the version)")]
+    #[arg(long, help = "The index name (without the version)")]
     index_name: String,
 
-    #[arg(help = "The version of the index")]
-    version: String,
+    #[arg(long, help = "The version of the index")]
+    index_version: u32,
+
+    #[arg(long, help = "The region of the index (local or us-central-1)")]
+    #[arg(value_enum)]
+    region: Region,
 }
 /*
- * Create an index in Elasticsearch
+ * Create an index in Elasticsearch for core
  *
  * Usage:
- * cargo run --bin create_index -- --index_name <index_name> --version <version>
+ * cargo run --bin create_index -- --index-name <index_name> --index-version <version> --region <region>
  *
- * Look for index settings and mappings in migrations/elasticsearch/indices/[index_name]_[version].settings.[region].json
+ * Look for index settings and mappings in src/search_stores/indices/[index_name]_[version].settings.[region].json
  * Create the index with the given settings and mappings at [index_name]_[version], and set the alias to [index_name]
  */
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // parse args and env vars
     let args = Args::parse();
     let index_alias = args.index_name;
-    let version = args.version;
-
+    let index_version = args.index_version;
+    let region = args.region;
     let url = std::env::var("ELASTICSEARCH_URL").expect("ELASTICSEARCH_URL must be set");
 
     let username =
         std::env::var("ELASTICSEARCH_USERNAME").expect("ELASTICSEARCH_USERNAME must be set");
     let password =
         std::env::var("ELASTICSEARCH_PASSWORD").expect("ELASTICSEARCH_PASSWORD must be set");
-    let region = std::env::var("DUST_REGION").expect("DUST_REGION must be set");
 
+    // create ES client
     let credentials = Credentials::Basic(username, password);
-
     let u = Url::parse(&url)?;
     let conn_pool = SingleNodeConnectionPool::new(u);
     let mut transport_builder = TransportBuilder::new(conn_pool);
@@ -49,44 +70,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = Elasticsearch::new(transport);
 
-    // get index settings and mappings, parse them as json
-    let settings_path = format!(
-        "bin/migrations/elasticsearch/indices/{}_{}.settings.{}.json",
-        index_alias, version, region
-    );
-    let mappings_path = format!(
-        "bin/migrations/elasticsearch/indices/{}_{}.mappings.json",
-        index_alias, version
+    let index_fullname = format!(
+        "{}.core.{}_{}",
+        region.to_string().to_lowercase(),
+        index_alias,
+        index_version
     );
 
-    let settings = std::fs::read_to_string(settings_path)?;
-    let mappings = std::fs::read_to_string(mappings_path)?;
-
-    let settings: HashMap<String, serde_json::Value> = serde_json::from_str(&settings)?;
-    let mappings: HashMap<String, serde_json::Value> = serde_json::from_str(&mappings)?;
-
-    let body = serde_json::json!({
-        "settings": settings,
-        "mappings": mappings,
-        /*"aliases": {
-            index_alias.clone(): {}
-        }*/
-    });
-
-    let index_fullname = format!("{}_{}", index_alias.clone(), version);
-    println!("{:?}", serde_json::to_string(&body)?);
-
-    // check if index exists
+    // do not create index if it already exists
     let response = client
         .indices()
         .exists(IndicesExistsParts::Index(&[index_fullname.as_str()]))
         .send()
         .await?;
 
-    if response.status_code() == 200 {
-        println!("Index already exists");
-        return Ok(());
+    if response.status_code() == StatusCode::OK {
+        return Err(anyhow::anyhow!("Index already exists").into());
     }
+
+    // get index settings and mappings, parse them as json
+    let settings_path = format!(
+        "src/search_stores/indices/{}_{}.settings.{}.json",
+        index_alias,
+        index_version,
+        region.to_string().to_lowercase()
+    );
+    let mappings_path = format!(
+        "src/search_stores/indices/{}_{}.mappings.json",
+        index_alias, index_version
+    );
+
+    // catch errors, provide the error message
+    let settings_raw = std::fs::read_to_string(&settings_path).expect(&format!(
+        "Failed to read settings file at {}: {:?}",
+        settings_path,
+        std::io::Error::last_os_error()
+    ));
+    let mappings_raw = std::fs::read_to_string(&mappings_path).expect(&format!(
+        "Failed to read mappings file at {}: {:?}",
+        mappings_path,
+        std::io::Error::last_os_error()
+    ));
+    let settings: HashMap<String, serde_json::Value> =
+        serde_json::from_str(&settings_raw).expect(&format!(
+            "Failed to parse settings file at {settings_path}: {:?}",
+            std::io::Error::last_os_error()
+        ));
+    let mappings: HashMap<String, serde_json::Value> =
+        serde_json::from_str(&mappings_raw).expect(&format!(
+            "Failed to parse mappings file at {mappings_path}: {:?}",
+            std::io::Error::last_os_error()
+        ));
+
+    let body = serde_json::json!({
+        "settings": settings,
+        "mappings": mappings,
+        "aliases": {
+            index_alias.clone(): {}
+        }
+    });
 
     // create index with settings, mappings and alias
     let response = client
@@ -96,9 +138,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
-    println!("{:?}", response);
-    // get the body
-    let body = response.json::<serde_json::Value>().await?;
-    println!("{:?}", body);
-    Ok(())
+    match response.status_code() {
+        StatusCode::OK => {
+            println!("Index created: {}", index_fullname);
+            Ok(())
+        }
+        _ => {
+            let body = response.json::<serde_json::Value>().await?;
+            eprintln!("{:?}", body);
+            Err(anyhow::anyhow!("Failed to create index").into())
+        }
+    }
 }
