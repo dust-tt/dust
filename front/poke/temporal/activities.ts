4535ddee9c01f74c3f7bbacf008e5267da1c39bd
Aubin <aubin@dust.tt>
2024-12-10 20:12:28 +0100

    add a hard delete on memberships to fix stuck workflows

--- front/poke/temporal/activities.ts
+++ front/poke/temporal/activities.ts
@@ -606,6 +606,7 @@ export async function deleteWorkspaceActivity({
     });
     await FileResource.deleteAllForWorkspace(workspace, t);
     await RunResource.deleteAllForWorkspace(workspace, t);
+    await MembershipResource.deleteAllForWorkspace(workspace, t);
     await AgentUserRelation.destroy({
       where: { workspaceId: workspace.id },
       transaction: t,
