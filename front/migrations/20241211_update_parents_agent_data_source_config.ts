cb833d8529a0a3cc4c4a4739d5ba9cc07adc5539
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2024-12-11 17:01:34 +0100

    Update migration on `agent_data_source_configurations` to first double and then cleanup (#9289)
    * rename the file
    
    * feat: add up and down passes (up: double, down: /2)
    
    * :recycle: uniformize action names with the migrator script
    
    * rollback the name change since this breaks the diff
    
    * fix the transformer
    
    * add a unique filtering

--- front/migrations/20241211_update_parents_agent_data_source_config.ts
+++ front/migrations/20241211_update_parents_agent_data_source_config.ts
@@ -7,7 +7,16 @@ import { AgentDataSourceConfiguration } from "@app/lib/models/assistant/actions/
 import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
 import { makeScript } from "@app/scripts/helpers";
 
-type ProviderMigrator = (parents: string[]) => string[];
+type MigratorAction = "transform" | "clean";
+
+const isMigratorAction = (action: string): action is MigratorAction => {
+  return ["transform", "clean"].includes(action);
+};
+
+type ProviderMigrator = {
+  transformer: (parents: string[]) => string[];
+  cleaner: (parents: string[]) => string[];
+};
 
 const AGENT_CONFIGURATION_BATCH_SIZE = 100;
 const UPDATE_CONCURRENCY = 10;
@@ -56,92 +65,124 @@ const migrators: Record<ConnectorProvider, ProviderMigrator | null> = {
   snowflake: null,
   webcrawler: null,
   zendesk: null, // no migration needed!
-  confluence: (parents) => parents.map(getUpdatedConfluenceId),
+  confluence: {
+    transformer: (parents) => [
+      ...new Set([...parents, ...parents.map(getUpdatedConfluenceId)]),
+    ],
+    cleaner: (parents) =>
+      parents.filter(
+        (parent) =>
+          !parent.startsWith(ConfluenceOldIdPrefix.Page) &&
+          !parent.startsWith(ConfluenceOldIdPrefix.Space)
+      ),
+  },
   intercom: null, // no migration needed!
 };
 
-makeScript({}, async ({ execute }, logger) => {
-  let lastSeenId = 0;
-
-  for (;;) {
-    const configurations = await AgentDataSourceConfiguration.findAll({
-      limit: AGENT_CONFIGURATION_BATCH_SIZE,
-      where: { id: { [Op.gt]: lastSeenId } },
-      order: [["id", "ASC"]],
-      raw: true,
-      include: [
-        {
-          model: DataSourceModel,
-          as: "dataSource",
-          attributes: [],
-          required: true,
-        },
-      ],
-    });
-
-    if (configurations.length === 0) {
-      break;
-    }
-    lastSeenId = configurations[configurations.length - 1].id;
-
-    try {
-      await concurrentExecutor(
-        configurations,
-        async (configuration) => {
-          assert(
-            configuration.dataSource.connectorProvider,
-            "connectorProvider is required"
-          );
-          const migrator =
-            migrators[configuration.dataSource.connectorProvider];
-          if (!migrator) {
-            return; // no migration needed
-          }
-
-          const { parentsIn, parentsNotIn } = configuration;
-          const newParentsIn = parentsIn && migrator(parentsIn);
-          const newParentsNotIn = parentsNotIn && migrator(parentsNotIn);
-
-          if (execute) {
-            await configuration.update({
-              parentsIn: newParentsIn,
-              parentsNotIn: newParentsNotIn,
-            });
-            logger.info(
-              {
-                configurationId: configuration.id,
-                connectorProvider: configuration.dataSource.connectorProvider,
-                fromParentsIn: parentsIn,
-                toParentsIn: newParentsIn,
-                fromParentsNotIn: parentsNotIn,
-                toParentsNotIn: newParentsNotIn,
-              },
-              `LIVE`
-            );
-          } else {
-            logger.info(
-              {
-                configurationId: configuration.id,
-                connectorProvider: configuration.dataSource.connectorProvider,
-                fromParentsIn: parentsIn,
-                toParentsIn: newParentsIn,
-                fromParentsNotIn: parentsNotIn,
-                toParentsNotIn: newParentsNotIn,
-              },
-              `DRY`
-            );
-          }
-        },
-        { concurrency: UPDATE_CONCURRENCY }
+makeScript(
+  {
+    action: {
+      type: "string",
+    },
+  },
+  async ({ execute, action }, logger) => {
+    if (!isMigratorAction(action)) {
+      console.error(
+        `Invalid action ${action}, supported actions are "transform" and "clean"`
       );
-      logger.info(`Data processed up to id ${lastSeenId}`);
-    } catch (e) {
-      logger.error({ error: e, lastSeenId }, `ERROR`);
-      throw e;
+      return;
     }
-  }
 
-  logger.info(
-    `Finished migrating parents for agent data source configurations.`
-  );
-});
+    let lastSeenId = 0;
+
+    for (;;) {
+      const configurations = await AgentDataSourceConfiguration.findAll({
+        limit: AGENT_CONFIGURATION_BATCH_SIZE,
+        where: { id: { [Op.gt]: lastSeenId } },
+        order: [["id", "ASC"]],
+        raw: true,
+        include: [
+          {
+            model: DataSourceModel,
+            as: "dataSource",
+            attributes: [],
+            required: true,
+          },
+        ],
+      });
+
+      if (configurations.length === 0) {
+        break;
+      }
+      lastSeenId = configurations[configurations.length - 1].id;
+
+      try {
+        await concurrentExecutor(
+          configurations,
+          async (configuration) => {
+            assert(
+              configuration.dataSource.connectorProvider,
+              "connectorProvider is required"
+            );
+            const migrator =
+              migrators[configuration.dataSource.connectorProvider];
+            if (!migrator) {
+              return; // no migration needed
+            }
+
+            const { parentsIn, parentsNotIn } = configuration;
+            const newParentsIn =
+              parentsIn &&
+              (action === "transform"
+                ? migrator.transformer
+                : migrator.cleaner)(parentsIn);
+            const newParentsNotIn =
+              parentsNotIn &&
+              (action === "clean" ? migrator.transformer : migrator.cleaner)(
+                parentsNotIn
+              );
+
+            if (execute) {
+              await configuration.update({
+                parentsIn: newParentsIn,
+                parentsNotIn: newParentsNotIn,
+              });
+              logger.info(
+                {
+                  configurationId: configuration.id,
+                  connectorProvider: configuration.dataSource.connectorProvider,
+                  fromParentsIn: parentsIn,
+                  toParentsIn: newParentsIn,
+                  fromParentsNotIn: parentsNotIn,
+                  toParentsNotIn: newParentsNotIn,
+                },
+                `LIVE`
+              );
+            } else {
+              logger.info(
+                {
+                  configurationId: configuration.id,
+                  connectorProvider: configuration.dataSource.connectorProvider,
+                  fromParentsIn: parentsIn,
+                  toParentsIn: newParentsIn,
+                  fromParentsNotIn: parentsNotIn,
+                  toParentsNotIn: newParentsNotIn,
+                },
+                `DRY`
+              );
+            }
+          },
+          { concurrency: UPDATE_CONCURRENCY }
+        );
+        logger.info(`Data processed up to id ${lastSeenId}`);
+      } catch (e) {
+        logger.error({ error: e, lastSeenId }, `ERROR`);
+        throw e;
+      }
+    }
+
+    logger.info(
+      `Finished migrating parents for agent data source configurations.`
+    );
+  }
+);
