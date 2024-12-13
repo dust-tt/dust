798b98e2f055f608122de4f5cc9670e3df7ca784
Thomas Draier <thomas@dust.tt>
2024-12-13 15:00:40 +0100

    lint

--- front/pages/api/v1/w/[wId]/spaces/[spaceId]/data_sources/[dsId]/documents/index.ts
+++ front/pages/api/v1/w/[wId]/spaces/[spaceId]/data_sources/[dsId]/documents/index.ts
@@ -10,7 +10,6 @@ import { DataSourceResource } from "@app/lib/resources/data_source_resource";
 import { SpaceResource } from "@app/lib/resources/space_resource";
 import logger from "@app/logger/logger";
 import { apiError } from "@app/logger/withlogging";
-import doc from "@app/pages/api/doc";
 
 /**
  * @swagger
