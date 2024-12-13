964e010706f7717ba44fc02830f5be8bbd093f2a
Lucas <lucas@dust.tt>
2024-12-13 15:31:27 +0100

    2nd layer of auth in prompt history endpoint

--- front/pages/api/w/[wId]/assistant/agent_configurations/[aId]/history/index.ts
+++ front/pages/api/w/[wId]/assistant/agent_configurations/[aId]/history/index.ts
@@ -7,7 +7,10 @@ import { isLeft } from "fp-ts/lib/Either";
 import * as reporter from "io-ts-reporters";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { getAgentConfigurations } from "@app/lib/api/assistant/configuration";
+import {
+  getAgentConfiguration,
+  getAgentConfigurations,
+} from "@app/lib/api/assistant/configuration";
 import { withSessionAuthenticationForWorkspace } from "@app/lib/api/auth_wrappers";
 import type { Authenticator } from "@app/lib/auth";
 import { apiError } from "@app/logger/withlogging";
@@ -23,6 +26,36 @@ async function handler(
   >,
   auth: Authenticator
 ): Promise<void> {
+  if (
+    !req.query.aId ||
+    typeof req.query.aId !== "string" ||
+    req.query.aId.length === 0
+  ) {
+    return apiError(req, res, {
+      status_code: 400,
+      api_error: {
+        type: "invalid_request_error",
+        message: "Invalid Assistant ID provided.",
+      },
+    });
+  }
+
+  // Check that user has access to this assistant
+  const assistant = await getAgentConfiguration(auth, req.query.aId as string);
+  if (
+    !assistant ||
+    (assistant.scope === "private" &&
+      assistant.versionAuthorId !== auth.user()?.id)
+  ) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "agent_configuration_not_found",
+        message: "The Assistant you're trying to access was not found.",
+      },
+    });
+  }
+
   switch (req.method) {
     case "GET":
       // extract the limit from the query parameters
