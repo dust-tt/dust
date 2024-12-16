de5ab7bf65b70ebdfb47a5c7b4f6353ce5d0fd17
Henry Fontanier <h.fontanier@gmail.com>
2024-12-16 16:53:11 +0100

    fix(parents migrator): increase doc batch size to 10k (#9422)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- front/migrations/20241211_parents_migrator.ts
+++ front/migrations/20241211_parents_migrator.ts
@@ -34,7 +34,7 @@ type ProviderMigrator = {
   ) => { parents: string[]; parentId: string | null };
 };
 
-const QUERY_BATCH_SIZE = 2048;
+const QUERY_BATCH_SIZE = 10_000;
 const DOCUMENT_CONCURRENCY = 16;
 const TABLE_CONCURRENCY = 16;
 
