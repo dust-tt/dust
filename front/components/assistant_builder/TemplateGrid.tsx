952fd93cd98cbf4879b45e8e84863e5b5d45e3dd
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2024-12-17 14:15:21 +0100

    Fix

--- front/components/assistant_builder/TemplateGrid.tsx
+++ front/components/assistant_builder/TemplateGrid.tsx
@@ -1,4 +1,4 @@
-import { AssistantCard, CardGrid } from "@dust-tt/sparkle";
+import { LargeAssistantCard } from "@dust-tt/sparkle";
 
 import type { AssistantTemplateListType } from "@app/pages/api/w/[wId]/assistant/builder/templates";
 
@@ -15,9 +15,9 @@ export function TemplateGrid({
     return null;
   }
   return (
-    <CardGrid>
+    <div className="grid grid-cols-2 gap-2">
       {templates.map((t) => (
-        <AssistantCard
+        <LargeAssistantCard
           key={t.sId}
           title={t.handle}
           pictureUrl={t.pictureUrl}
@@ -25,6 +25,6 @@ export function TemplateGrid({
           onClick={() => openTemplateModal(t.sId)}
         />
       ))}
-    </CardGrid>
+    </div>
   );
 }
