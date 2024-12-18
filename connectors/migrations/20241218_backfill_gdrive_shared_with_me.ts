f0fd61c4d0e2874ae65dd55838e8e1050cdce720
Aubin <aubin@dust.tt>
2024-12-18 16:27:54 +0100

    add concurrency on the script

--- connectors/migrations/20241218_backfill_gdrive_shared_with_me.ts
+++ connectors/migrations/20241218_backfill_gdrive_shared_with_me.ts
@@ -2,31 +2,38 @@ import { makeScript } from "scripts/helpers";
 
 import { getSharedWithMeFolderId } from "@connectors/connectors/google_drive/lib/hierarchy";
 import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
+import { concurrentExecutor } from "@connectors/lib/async_utils";
 import { upsertDataSourceFolder } from "@connectors/lib/data_sources";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
 
 makeScript({}, async ({ execute }, logger) => {
   const connectors = await ConnectorResource.listByType("google_drive", {});
 
-  for (const connector of connectors) {
-    // this is a strict copy-paste of upsertSharedWithMeFolder, I don't want to export it for a migration script and want the folderId for logging purposes
-    const dataSourceConfig = dataSourceConfigFromConnector(connector);
-    const folderId = getSharedWithMeFolderId(connector);
+  await concurrentExecutor(
+    connectors,
+    async (connector) => {
+      // this is a strict copy-paste of upsertSharedWithMeFolder, I don't want to export it for a migration script and want the folderId for logging purposes
+      const dataSourceConfig = dataSourceConfigFromConnector(connector);
+      const folderId = getSharedWithMeFolderId(connector);
 
-    if (execute) {
-      await upsertDataSourceFolder({
-        dataSourceConfig,
-        folderId,
-        parents: [folderId],
-        parentId: null,
-        title: "Shared with me",
-        mimeType: "application/vnd.dust.googledrive.folder",
-      });
-      logger.info(`Upserted folder ${folderId} for connector ${connector.id}`);
-    } else {
-      logger.info(
-        `Would upsert folder ${folderId} for connector ${connector.id}`
-      );
-    }
-  }
+      if (execute) {
+        await upsertDataSourceFolder({
+          dataSourceConfig,
+          folderId,
+          parents: [folderId],
+          parentId: null,
+          title: "Shared with me",
+          mimeType: "application/vnd.dust.googledrive.folder",
+        });
+        logger.info(
+          `Upserted folder ${folderId} for connector ${connector.id}`
+        );
+      } else {
+        logger.info(
+          `Would upsert folder ${folderId} for connector ${connector.id}`
+        );
+      }
+    },
+    { concurrency: 10 }
+  );
 });
