fd1061c161424aacdae178583bcb1feed02a3a78
JulesBelveze <jules.belveze@hotmail.fr>
2024-12-18 09:07:10 +0100

    [front] - feature: extend lookup API to handle workspace queries
     - Implement a base lookup response structure for the lookup API endpoint
     - Add resource type handling to differentiate user and workspace lookups
     - Provide separate processing logic for user and workspace lookup paths

--- front/pages/api/lookup.ts
+++ front/pages/api/lookup.ts
@@ -8,15 +8,20 @@ import { getBearerToken } from "@app/lib/auth";
 import { getPendingMembershipInvitationWithWorkspaceForEmail } from "@app/lib/iam/invitations";
 import { fetchUserFromSession } from "@app/lib/iam/users";
 import { findWorkspaceWithVerifiedDomain } from "@app/lib/iam/workspaces";
+import { Workspace } from "@app/lib/models/workspace";
 import { apiError, withLogging } from "@app/logger/withlogging";
 
-
-export type UserLookupResponseBody = {
+export type BaseLookupResponse = {
   isNew: boolean;
+};
+
+export type UserLookupResponse = BaseLookupResponse & {
   hasInvite: boolean;
   hasAutoJoinWorkspace: boolean;
   workspaceId?: string;
 };
+
+export type WorkspaceLookupResponse = BaseLookupResponse;
 
 const ExternalUserCodec = t.type({
   email: t.string,
@@ -29,13 +34,26 @@ const ExternalUserCodec = t.type({
   picture: t.union([t.string, t.undefined]),
 });
 
-const LookupRequestBodySchema = t.type({
+export type LookupResponseBody = UserLookupResponse | WorkspaceLookupResponse;
+
+const UserLookupSchema = t.type({
+  resource: t.literal("user"),
   user: ExternalUserCodec,
 });
 
+const WorkspaceLookupSchema = t.type({
+  resource: t.literal("workspace"),
+  workspace: t.string,
+});
+
+const LookupRequestBodySchema = t.union([
+  UserLookupSchema,
+  WorkspaceLookupSchema,
+]);
+
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<WithAPIErrorResponse<UserLookupResponseBody>>
+  res: NextApiResponse<WithAPIErrorResponse<LookupResponseBody>>
 ): Promise<void> {
   if (req.method !== "POST") {
     return apiError(req, res, {
@@ -68,8 +86,8 @@ async function handler(
     });
   }
 
-  const queryValidation = LookupRequestBodySchema.decode(req.body);
-  if (isLeft(queryValidation)) {
+  const bodyValidation = LookupRequestBodySchema.decode(req.body);
+  if (isLeft(bodyValidation)) {
     return apiError(req, res, {
       status_code: 404,
       api_error: {
@@ -78,34 +96,61 @@ async function handler(
       },
     });
   }
-  const session = queryValidation.right;
 
+  const body = bodyValidation.right;
+
+  let response: LookupResponseBody | null = null;
+  switch (body.resource) {
+    case "user":
+      {
+        response = await handleLookupUser(body);
+      }
+      break;
+    case "workspace": {
+      response = await handleLookupWorkspace(body);
+    }
+  }
+  res.status(200).json(response);
+  return;
+}
+
+async function handleLookupUser(
+  body: t.TypeOf<typeof UserLookupSchema>
+): Promise<UserLookupResponse> {
   // Check if user exists
-  const user = await fetchUserFromSession(session);
+  const user = await fetchUserFromSession(body);
 
   // Check for pending invitations
   const pendingInvite =
-    await getPendingMembershipInvitationWithWorkspaceForEmail(
-      session.user.email
-    );
+    await getPendingMembershipInvitationWithWorkspaceForEmail(body.user.email);
 
   // Check for workspace with verified domain
   const workspaceWithVerifiedDomain =
-    await findWorkspaceWithVerifiedDomain(session);
+    await findWorkspaceWithVerifiedDomain(body);
 
   // Check auto-join
   const canAutoJoin =
     workspaceWithVerifiedDomain?.domainAutoJoinEnabled ?? false;
 
-  res.status(200).json({
+  return {
     isNew: !!user,
     hasInvite: !!pendingInvite,
     hasAutoJoinWorkspace: canAutoJoin,
     workspaceId:
       pendingInvite?.workspace.sId ||
       (canAutoJoin ? workspaceWithVerifiedDomain?.workspace?.sId : undefined),
+  };
+}
+
+async function handleLookupWorkspace(
+  body: t.TypeOf<typeof WorkspaceLookupSchema>
+): Promise<WorkspaceLookupResponse> {
+  const workspace = await Workspace.findOne({
+    where: { sId: body.workspace },
   });
-  return;
+  return {
+    isNew: !!workspace,
+  };
 }
 
 export default withLogging(handler);
