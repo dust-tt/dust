f32fb380fe2d4e846eb18b0f37099c42349d5282
Aubin <aubin@dust.tt>
2024-12-19 15:31:15 +0100

    add a TODO

--- connectors/src/connectors/github/temporal/config.ts
+++ connectors/src/connectors/github/temporal/config.ts
@@ -1,3 +1,4 @@
+// TODO(2024-12-19 aubin): remove these once we only have long-running workflows in the old queue and have moved them to the new one
 export const OLD_WORKFLOW_VERSION = 2;
 export const OLD_QUEUE_NAME = `github-queue-v${OLD_WORKFLOW_VERSION}`;
 
