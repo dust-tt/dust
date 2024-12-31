57220c136b1276f570fe8468b8e99830652fe76a
thib-martin <168569391+thib-martin@users.noreply.github.com>
2024-12-31 13:15:05 +0100

    fixing bug

--- front/components/poke/workspace/table.tsx
+++ front/components/poke/workspace/table.tsx
@@ -46,9 +46,8 @@ export function WorkspaceInfoTable({
                        href={`https://metabase.dust.tt/dashboard/34-snowflake-workspace-health?end_date=2030-12-31&start_date=2024-01-01&tab=30-executive-summary&workspace_size_difference_margin=0.2&workspacesid=${owner.sId}`}
                        target="_blank"
                        className="text-xs text-action-400"
-                  > 
+                  >
                     Metabase
-                    }
                   </Link>
               </PokeTableCell>
             </PokeTableRow>
