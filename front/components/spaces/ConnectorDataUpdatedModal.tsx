be7ff3e836dd7c6dfeefaa60a0a651db47fe46c6
JulesBelveze <jules.belveze@hotmail.fr>
2025-01-02 15:57:13 +0100

    [front/components/spaces] - refactor: replace Modal with Sheet in ConnectorDataUpdatedModal
     - Transition to using Sheet component to handle the data sync progress interface instead of Modal
     - Prevent interactions outside Sheet when data sync is in progress to maintain focus on the process
     - Improve structure and readability of the component by grouping related elements within SheetContainer and SheetContent components

--- front/components/spaces/ConnectorDataUpdatedModal.tsx
+++ front/components/spaces/ConnectorDataUpdatedModal.tsx
@@ -3,8 +3,12 @@ import {
   ContentMessage,
   Hoverable,
   Icon,
-  Modal,
   Page,
+  Sheet,
+  SheetContainer,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
   SparklesIcon,
 } from "@dust-tt/sparkle";
 import type { ConnectorProvider, LightWorkspaceType } from "@dust-tt/types";
@@ -28,70 +32,79 @@ export const ConnectorDataUpdatedModal = ({
     REMOTE_DATABASE_CONNECTOR_PROVIDERS.includes(connectorProvider);
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={() => {}}
-      onSave={() => {}}
-      isSaving={false}
-      hasChanged={false}
-      variant="side-sm"
-      alertModal
-    >
-      <Page variant="modal">
-        <Page.Vertical sizing="grow">
-          <div className="flex flex-col gap-2">
-            <div className="p-1 text-xl font-bold">
-              <Icon visual={SparklesIcon} className="text-brand" size="lg" />
-              <div>Data sync in progress...</div>
-            </div>
-          </div>
+    <Sheet open={isOpen}>
+      <SheetContent
+        size="lg"
+        onInteractOutside={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <SheetHeader hideButton>
+          <SheetTitle />
+        </SheetHeader>
+        <SheetContainer>
+          <Page variant="modal">
+            <Page.Vertical sizing="grow">
+              <div className="flex flex-col gap-2">
+                <div className="p-1 text-xl font-bold">
+                  <Icon
+                    visual={SparklesIcon}
+                    className="text-brand"
+                    size="lg"
+                  />
+                  <div>Data sync in progress...</div>
+                </div>
+              </div>
 
-          <ContentMessage variant="slate">
-            <div className="flex flex-col gap-2">
+              <ContentMessage variant="slate">
+                <div className="flex flex-col gap-2">
+                  <p>
+                    Once synchronized, {isRemoteDbProvider ? "tables" : "data"}{" "}
+                    will appear under <em>"Connection Admin"</em>.
+                  </p>
+                  <p className="font-bold">
+                    Add {isRemoteDbProvider ? "tables" : "data"} to{" "}
+                    <Link
+                      className="cursor-pointer font-bold text-action-500"
+                      href={`/w/${owner.sId}/spaces`}
+                    >
+                      <Hoverable onClick={() => {}}>Company Data</Hoverable>
+                    </Link>{" "}
+                    for team-wide access or to a specific space to manage
+                    access.
+                  </p>
+                </div>
+              </ContentMessage>
+              <div className="w-full pt-4">
+                <div className="relative w-full overflow-hidden rounded-lg pb-[56.20%]">
+                  <iframe
+                    src="https://fast.wistia.net/embed/iframe/9vf0b2rv5f?seo=true&videoFoam=false"
+                    title="Data Management"
+                    allow="autoplay; fullscreen"
+                    frameBorder="0"
+                    className="absolute left-0 top-0 h-full w-full rounded-lg"
+                  ></iframe>
+                </div>
+              </div>
               <p>
-                Once synchronized, {isRemoteDbProvider ? "tables" : "data"} will
-                appear under <em>"Connection Admin"</em>.
-              </p>
-              <p className="font-bold">
-                Add {isRemoteDbProvider ? "tables" : "data"} to{" "}
-                <Link
+                See{" "}
+                <Hoverable
                   className="cursor-pointer font-bold text-action-500"
-                  href={`/w/${owner.sId}/spaces`}
+                  onClick={() => {
+                    window.open("https://docs.dust.tt/docs/data", "_blank");
+                  }}
                 >
-                  <Hoverable onClick={() => {}}>Company Data</Hoverable>
-                </Link>{" "}
-                for team-wide access or to a specific space to manage access.
+                  documentation
+                </Hoverable>{" "}
+                for more information.
               </p>
-            </div>
-          </ContentMessage>
-          <div className="w-full pt-4">
-            <div className="relative w-full overflow-hidden rounded-lg pb-[56.20%]">
-              <iframe
-                src="https://fast.wistia.net/embed/iframe/9vf0b2rv5f?seo=true&videoFoam=false"
-                title="Data Management"
-                allow="autoplay; fullscreen"
-                frameBorder="0"
-                className="absolute left-0 top-0 h-full w-full rounded-lg"
-              ></iframe>
-            </div>
-          </div>
-          <p>
-            See{" "}
-            <Hoverable
-              className="cursor-pointer font-bold text-action-500"
-              onClick={() => {
-                window.open("https://docs.dust.tt/docs/data", "_blank");
-              }}
-            >
-              documentation
-            </Hoverable>{" "}
-            for more information.
-          </p>
-          <div className="flex w-full justify-end">
-            <Button label="Ok" onClick={() => onClose()} />
-          </div>
-        </Page.Vertical>
-      </Page>
-    </Modal>
+              <div className="flex w-full justify-end">
+                <Button label="Ok" onClick={() => onClose()} />
+              </div>
+            </Page.Vertical>
+          </Page>
+        </SheetContainer>
+      </SheetContent>
+    </Sheet>
   );
 };
