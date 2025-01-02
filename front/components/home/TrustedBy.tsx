bddaf8dcd7990e577be74988a0a1ca404c9ec9e1
pinotalexandre <hialexpinot@gmail.com>
2025-01-02 15:05:46 +0100

    using tailwind grid

--- front/components/home/TrustedBy.tsx
+++ front/components/home/TrustedBy.tsx
@@ -5,73 +5,70 @@ import { classNames } from "@app/lib/utils";
 
 export default function TrustedBy() {
   return (
-    <>
-      <div
-        className={classNames(
-          "col-span-12 flex flex-col items-center py-8",
-          "lg:col-span-12 lg:col-start-1",
-          "xl:col-span-10 xl:col-start-2"
-        )}
-      >
-        <H3 className="w-full text-center text-white">
-          Trusted by 1000+ organizations
-        </H3>
-        <div className="mt-8 flex w-full flex-col items-center gap-y-8">
-          {/* First row - 5 items */}
-          <div className="flex w-full justify-center gap-x-24">
-            {/* First 5 logos */}
-            <Image
-              alt="alan"
-              src="/static/landing/logos/alan.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="watershed"
-              src="/static/landing/logos/watershed.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="qonto"
-              src="/static/landing/logos/qonto.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="pennylane"
-              src="/static/landing/logos/pennylane.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="payfit"
-              src="/static/landing/logos/payfit.png"
-              width={250}
-              height={100}
-            />
-          </div>
-          {/* Second row - 4 items */}
-          <div className="flex w-full justify-center gap-x-24">
-            {/* Last 4 logos */}
-            <Image
-              alt="malt"
-              src="/static/landing/logos/malt.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="hivebrite"
-              src="/static/landing/logos/hivebrite.png"
-              width={250}
-              height={100}
-            />
-            <Image
-              alt="blueground"
-              src="/static/landing/logos/blueground.png"
-              width={250}
-              height={100}
-            />
+    <div
+      className={classNames(
+        "col-span-12 flex flex-col items-center py-4",
+        "lg:col-span-12 lg:col-start-1",
+        "xl:col-span-10 xl:col-start-2"
+      )}
+    >
+      <H3 className="w-full text-center text-white">
+        Trusted by 1000+ organizations
+      </H3>
+
+      <div className="mx-auto mt-8 w-full max-w-[1600px] px-8">
+        <div className="grid grid-cols-1 place-items-center gap-8 md:grid-cols-2 lg:grid-cols-4">
+          {" "}
+          {/* Reduced from gap-16 to gap-8 */}
+          <Image
+            alt="alan"
+            src="/static/landing/logos/alan.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="watershed"
+            src="/static/landing/logos/watershed.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="qonto"
+            src="/static/landing/logos/qonto.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="pennylane"
+            src="/static/landing/logos/pennylane.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="payfit"
+            src="/static/landing/logos/payfit.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="malt"
+            src="/static/landing/logos/malt.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="hivebrite"
+            src="/static/landing/logos/hivebrite.png"
+            width={250}
+            height={100}
+          />
+          <Image
+            alt="blueground"
+            src="/static/landing/logos/blueground.png"
+            width={250}
+            height={100}
+          />
+          <div className="col-span-1 md:col-span-2 lg:col-span-2 lg:col-start-2">
             <Image
               alt="clay"
               src="/static/landing/logos/clay.png"
@@ -81,6 +78,6 @@ export default function TrustedBy() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
