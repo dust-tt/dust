356bd0db1a3253b2eba09ad916360f0739301f48
pinotalexandre <hialexpinot@gmail.com>
2025-01-02 14:22:19 +0100

    adding clay logo

--- front/components/home/TrustedBy.tsx
+++ front/components/home/TrustedBy.tsx
@@ -16,62 +16,69 @@ export default function TrustedBy() {
         <H3 className="w-full text-center text-white">
           Trusted by 1000+ organizations
         </H3>
-        <div
-          className={classNames(
-            "mt-8 w-full",
-            "grid grid-cols-2 place-items-center gap-1",
-            "sm:grid-cols-3 sm:gap-0",
-            "md:grid-cols-4 md:gap-0"
-          )}
-        >
-          <Image
-            alt="alan"
-            src="/static/landing/logos/alan.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="watershed"
-            src="/static/landing/logos/watershed.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="qonto"
-            src="/static/landing/logos/qonto.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="pennylane"
-            src="/static/landing/logos/pennylane.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="payfit"
-            src="/static/landing/logos/payfit.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="malt"
-            src="/static/landing/logos/malt.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="hivebrite"
-            src="/static/landing/logos/hivebrite.png"
-            width={250}
-            height={100}
-          />
-          <Image
-            alt="blueground"
-            src="/static/landing/logos/blueground.png"
-            width={250}
-            height={100}
-          />
+        <div className="mt-8 flex w-full flex-col items-center gap-y-8">
+          {/* First row - 5 items */}
+          <div className="flex w-full justify-center gap-x-24">
+            {/* First 5 logos */}
+            <Image
+              alt="alan"
+              src="/static/landing/logos/alan.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="watershed"
+              src="/static/landing/logos/watershed.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="qonto"
+              src="/static/landing/logos/qonto.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="pennylane"
+              src="/static/landing/logos/pennylane.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="payfit"
+              src="/static/landing/logos/payfit.png"
+              width={250}
+              height={100}
+            />
+          </div>
+          {/* Second row - 4 items */}
+          <div className="flex w-full justify-center gap-x-24">
+            {/* Last 4 logos */}
+            <Image
+              alt="malt"
+              src="/static/landing/logos/malt.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="hivebrite"
+              src="/static/landing/logos/hivebrite.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="blueground"
+              src="/static/landing/logos/blueground.png"
+              width={250}
+              height={100}
+            />
+            <Image
+              alt="clay"
+              src="/static/landing/logos/clay.png"
+              width={250}
+              height={100}
+            />
+          </div>
         </div>
       </div>
     </>
