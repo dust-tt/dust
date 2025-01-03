adcc1a084e30dbb84c0a7b8a5431cf13012478e1
Daphné Popin <daphne.popin@gmail.com>
2025-01-03 16:37:49 +0100

    Doc Tracker: Remove old models

--- front/lib/models/doc_tracker.ts
+++ front/lib/models/doc_tracker.ts
@@ -8,7 +8,6 @@ import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
 import { DataSourceViewModel } from "@app/lib/resources/storage/models/data_source_view";
 import { SpaceModel } from "@app/lib/resources/storage/models/spaces";
 import { UserModel } from "@app/lib/resources/storage/models/user";
-import { BaseModel } from "@app/lib/resources/storage/wrappers";
 import { SoftDeletableModel } from "@app/lib/resources/storage/wrappers";
 
 export class TrackerConfigurationModel extends SoftDeletableModel<TrackerConfigurationModel> {
@@ -300,105 +299,4 @@ DataSourceModel.hasMany(TrackerGenerationModel, {
 TrackerGenerationModel.belongsTo(DataSourceModel, {
   foreignKey: { allowNull: false },
   as: "dataSource",
-});
-
-// TODO(DOC_TRACKER) Delete models below this line
-// They will be replaced by the new models
-
-export class TrackedDocument extends BaseModel<TrackedDocument> {
-  declare createdAt: CreationOptional<Date>;
-  declare updatedAt: CreationOptional<Date>;
-
-  declare documentId: string;
-  declare trackingEnabledAt: Date | null;
-
-  declare userId: ForeignKey<UserModel["id"]>;
-  declare dataSourceId: ForeignKey<DataSourceModel["id"]>;
-}
-
-TrackedDocument.init(
-  {
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    documentId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    trackingEnabledAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    modelName: "tracked_document",
-    sequelize: frontSequelize,
-    indexes: [
-      { fields: ["userId", "dataSourceId", "documentId"], unique: true },
-      { fields: ["dataSourceId"] },
-    ],
-  }
-);
-
-DataSourceModel.hasMany(TrackedDocument, {
-  foreignKey: { allowNull: false },
-  onDelete: "CASCADE",
-});
-UserModel.hasMany(TrackedDocument, {
-  foreignKey: { allowNull: false },
-  onDelete: "CASCADE",
-});
-
-export class DocumentTrackerChangeSuggestion extends BaseModel<DocumentTrackerChangeSuggestion> {
-  declare createdAt: CreationOptional<Date>;
-  declare updatedAt: CreationOptional<Date>;
-
-  declare suggestion: string;
-  declare reason: string | null;
-  declare status: "pending" | "done" | "rejected";
-
-  declare trackedDocumentId: ForeignKey<TrackedDocument["id"]>;
-  declare sourceDataSourceId: ForeignKey<DataSourceModel["id"]>;
-  declare sourceDocumentId: string;
-}
-
-DocumentTrackerChangeSuggestion.init(
-  {
-    createdAt: { type: DataTypes.DATE, allowNull: false },
-    updatedAt: { type: DataTypes.DATE, allowNull: false },
-    suggestion: { type: DataTypes.TEXT, allowNull: false },
-    suggestionTitle: { type: DataTypes.TEXT, allowNull: true },
-    reason: { type: DataTypes.TEXT, allowNull: true },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "pending",
-    },
-    sourceDocumentId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    modelName: "document_tracker_change_suggestion",
-    sequelize: frontSequelize,
-    indexes: [{ fields: ["trackedDocumentId"] }],
-  }
-);
-
-TrackedDocument.hasMany(DocumentTrackerChangeSuggestion, {
-  foreignKey: { allowNull: false },
-  onDelete: "CASCADE",
-});
-DataSourceModel.hasMany(DocumentTrackerChangeSuggestion, {
-  foreignKey: { allowNull: false, name: "sourceDataSourceId" },
-  onDelete: "RESTRICT",
 });
