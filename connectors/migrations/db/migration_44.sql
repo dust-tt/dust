a84bce5f60aef14042e0460383e05ddcd7ffb003
Aubin <aubin@dust.tt>
2025-01-06 11:08:26 +0100

    remove an obsolete drop index in the migration script

--- connectors/migrations/db/migration_44.sql
+++ connectors/migrations/db/migration_44.sql
@@ -1,7 +1,6 @@
 -- Migration created on Jan 02, 2025
 CREATE UNIQUE INDEX IF NOT EXISTS "zendesk_brands_connector_brand_idx" ON "zendesk_brands" ("connectorId", "brandId");
 DROP INDEX IF EXISTS "zendesk_connector_brand_idx";
-DROP INDEX IF EXISTS "zendesk_brands_brand_idx";
 DROP INDEX IF EXISTS "zendesk_brands_brand_id";
 
 CREATE UNIQUE INDEX IF NOT EXISTS "zendesk_categories_connector_brand_category_idx" ON "zendesk_categories" ("connectorId", "brandId", "categoryId");
