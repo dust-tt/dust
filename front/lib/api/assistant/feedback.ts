855c90c6c06528e5ca67a11d0eb28b1bc32e7cf6
Lucas Massemin <lucasmassemin@gmail.com>
2025-01-07 15:09:52 +0100

    Update front/lib/api/assistant/feedback.ts
    Co-authored-by: Flavien David <flavien.david74@gmail.com>

--- front/lib/api/assistant/feedback.ts
+++ front/lib/api/assistant/feedback.ts
@@ -58,8 +58,8 @@ export async function getConversationFeedbacksForUser(
       conversation
     );
 
-  const feedbacks = feedbacksRes.map((feedback) => {
-    return feedback.toJSON() as AgentMessageFeedbackType;
+  const feedbacks: AgentMessageFeedbackType[] = feedbacksRes.map((feedback) => {
+    return feedback.toJSON();
   });
 
   return new Ok(feedbacks);
