4c371b7260323acd8712482c3c12137e0634895d
Lucas Massemin <lucasmassemin@gmail.com>
2025-01-07 15:08:17 +0100

    Update front/lib/workspace_usage.ts
    Co-authored-by: Flavien David <flavien.david74@gmail.com>

--- front/lib/workspace_usage.ts
+++ front/lib/workspace_usage.ts
@@ -451,7 +451,7 @@ export async function getFeedbacksUsageData(
     return "No data available for the selected period.";
   }
 
-  const feedbacksWithMinimalFields = feedbacks.map((feedback) => {
+  const feedbacksWithMinimalFields: FeedbackQueryResult[] = feedbacks.map((feedback) => {
     const jsonFeedback = feedback.toJSON();
     return {
       id: jsonFeedback.id,
@@ -463,7 +463,7 @@ export async function getFeedbacksUsageData(
       thumb: jsonFeedback.thumbDirection,
       content: jsonFeedback.content?.replace(/\r?\n/g, "\\n") || null,
     };
-  }) as FeedbackQueryResult[];
+  });
   return generateCsvFromQueryResult(feedbacksWithMinimalFields);
 }
 
