ddeff204ec07524ad11cf7a3bcfd4dcee8b0873f
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-01-07 14:36:15 +0100

    Fix

--- front/components/FairUsageModal.tsx
+++ front/components/FairUsageModal.tsx
@@ -33,7 +33,7 @@ export function FairUsageModal({ isOpened, onClose }: FairUsageModalProps) {
     >
       <div className="py-8">
         <Icon visual={AttachmentIcon} size="lg" className="text-emerald-500" />
-        <Markdown content={FAIR_USE_CONTENT} forcedTextSize="sm" />
+        <Markdown content={FAIR_USE_CONTENT} forcedTextSize="text-sm" />
       </div>
     </Modal>
   );
