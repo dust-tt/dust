2053d6bf38b286ee8ddb5b76810e0d9fc719cb1b
Flavien David <flavien@dust.tt>
2025-01-08 23:34:17 +0100

    Use distinct service account for GitHub build invoker and actual build runtime

--- k8s/cloud-build.sh
+++ k8s/cloud-build.sh
@@ -54,7 +54,7 @@ fi
 cd "$WORKING_DIR"
 
 # Prepare the build command
-BUILD_CMD=(gcloud builds submit --quiet --config "${SCRIPT_DIR}/cloudbuild.yaml" --service-account="projects/${PROJECT_ID}/serviceAccounts/cloudbuild-publisher@${PROJECT_ID}.iam.gserviceaccount.com")
+BUILD_CMD=(gcloud builds submit --quiet --config "${SCRIPT_DIR}/cloudbuild.yaml" --service-account="projects/${PROJECT_ID}/serviceAccounts/cloudbuild-runtime@${PROJECT_ID}.iam.gserviceaccount.com")
 
 if [ -n "$GCLOUD_IGNORE_FILE" ]; then
     BUILD_CMD+=(--ignore-file="$GCLOUD_IGNORE_FILE")
