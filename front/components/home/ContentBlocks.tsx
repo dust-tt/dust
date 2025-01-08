d95807a76df873a84cb1b49d923cd4ee9d7ac0a2
Amelie Deltombe <ameliedeltombe@Amelies-MacBook-Air.local>
2025-01-08 14:21:36 +0100

    linting

--- front/components/home/ContentBlocks.tsx
+++ front/components/home/ContentBlocks.tsx
@@ -164,24 +164,24 @@ export const HeaderContentBlock = ({
         </P>
       )}
       {hasCTA && (
-         <div className="flex gap-4">
-         <Link href="/home/pricing" shallow={true}>
-           <Button
-             variant="highlight"
-             size="md"
-             label="Get started"
-             icon={RocketIcon}
-           />
-         </Link>
-           <Button
-             variant="outline"
-             size="md"
-             label="Talk to sales"
-             icon={UserArrowIcon}
-             href="https://forms.gle/dGaQ1AZuDCbXY1ft9"
-             target='_blank'
-           />
-       </div>
+        <div className="flex gap-4">
+        <Link href="/home/pricing" shallow={true}>
+          <Button
+            variant="highlight"
+            size="md"
+            label="Get started"
+            icon={RocketIcon}
+          />
+        </Link>
+          <Button
+            variant="outline"
+            size="md"
+            label="Talk to sales"
+            icon={UserArrowIcon}
+            href="https://forms.gle/dGaQ1AZuDCbXY1ft9"
+            target='_blank'
+          />
+        </div>
       )}
     </div>
   </Grid>
