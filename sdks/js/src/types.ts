e6bcbc90de46556bb73eb48b9ed05775a3f5f446
Stanislas Polu <spolu@dust.tt>
2025-01-09 16:11:58 +0100

    fix loging (#9867)

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -339,7 +339,6 @@ export interface LoggerInterface {
   info: (args: Record<string, unknown>, message: string) => void;
   trace: (args: Record<string, unknown>, message: string) => void;
   warn: (args: Record<string, unknown>, message: string) => void;
-  child: (args: Record<string, unknown>) => LoggerInterface;
 }
 
 const DataSourceViewCategoriesSchema = FlexibleEnumSchema<
