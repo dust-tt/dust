4722772930cd9ef1f96e8ff63ae379bb2ab67e6e
Sebastien Flory <seb@dust.tt>
2025-01-10 17:12:36 +0100

    Upg: cleaner lock implementation (#9901)

--- front/lib/wake_lock.ts
+++ front/lib/wake_lock.ts
@@ -1,13 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
 
-export async function wakeLock<T>(
-  autoCallback: () => Promise<T>,
-  lockName?: string
-): Promise<T> {
+export async function wakeLock<T>(autoCallback: () => Promise<T>): Promise<T> {
   if (!global.wakeLocks) {
     global.wakeLocks = new Set();
   }
-  lockName ??= uuidv4();
+  const lockName = uuidv4();
   global.wakeLocks.add(lockName);
   try {
     const r = await autoCallback();
@@ -17,11 +14,6 @@ export async function wakeLock<T>(
   }
 }
 
-// If a lockName is provided, checks if that lock is free, otherwise checks if all locks are free
-export function wakeLockIsFree(lockName?: string): boolean {
-  if (lockName) {
-    return !global.wakeLocks || !global.wakeLocks.has(lockName);
-  }
-
+export function wakeLockIsFree(): boolean {
   return !global.wakeLocks || global.wakeLocks.size === 0;
 }
