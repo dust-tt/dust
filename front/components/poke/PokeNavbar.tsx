4b9e334391ef438b0445b053d524768eb6fa56c3
Flavien David <flavien@dust.tt>
2025-01-15 09:30:48 +0100

    Fix Poke command bar

--- front/components/poke/PokeNavbar.tsx
+++ front/components/poke/PokeNavbar.tsx
@@ -39,10 +39,8 @@ export const PokeNavbar: React.FC = () => (
 export default PokeNavbar;
 
 export function PokeSearchCommand() {
-  const router = useRouter();
   const [open, setOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [isNavigating, setIsNavigating] = useState(false);
 
   const { isError, isLoading, results } = usePokeSearch({
     // Disable search until the user has typed at least 2 characters.
@@ -50,33 +48,17 @@ export function PokeSearchCommand() {
     search: searchTerm,
   });
 
-  const navigateTo = useCallback(
-    async (link: string) => {
-      setIsNavigating(true);
-      await router.push(link);
-      setIsNavigating(false);
-    },
-    [router]
-  );
-
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
         setOpen((open) => !open);
-      } else if (
-        open &&
-        results.length > 0 &&
-        e.key === "Enter" &&
-        results[0].link
-      ) {
-        e.preventDefault();
-        void navigateTo(results[0].link);
       }
     };
     document.addEventListener("keydown", down);
+
     return () => document.removeEventListener("keydown", down);
-  }, [navigateTo, open, results]);
+  }, [open, results]);
 
   return (
     <>
@@ -89,47 +71,39 @@ export function PokeSearchCommand() {
         className="bg-structure-50 sm:max-w-[600px]"
         shouldFilter={false}
       >
-        {isNavigating ? (
-          <div className="m-4 flex items-center justify-center">
-            <Spinner size="md" />
-          </div>
-        ) : (
-          <>
-            <PokeCommandInput
-              placeholder="Type a command or search..."
-              onValueChange={(value) => setSearchTerm(value)}
-              className="border-none focus:outline-none focus:ring-0"
-            />
-            <PokeCommandList>
-              {isLoading && <div className="p-4 text-sm">Searching...</div>}
-              {searchTerm &&
-                searchTerm.length >= 2 &&
-                !isError &&
-                !isLoading &&
-                results.length === 0 && (
-                  <div className="p-4 text-sm">No results found.</div>
-                )}
-              {isError && (
-                <div className="p-4 text-sm">Something went wrong.</div>
-              )}
-              {searchTerm.length < 2 && (
-                <div className="p-4 text-sm">
-                  Enter at least 2 characters...
-                </div>
-              )}
+        <PokeCommandInput
+          placeholder="Type a command or search..."
+          onValueChange={(value) => setSearchTerm(value)}
+          className="border-none focus:outline-none focus:ring-0"
+        />
+        <PokeCommandList>
+          {isLoading && <div className="p-4 text-sm">Searching...</div>}
+          {searchTerm &&
+            searchTerm.length >= 2 &&
+            !isError &&
+            !isLoading &&
+            results.length === 0 && (
+              <div className="p-4 text-sm">No results found.</div>
+            )}
+          {isError && <div className="p-4 text-sm">Something went wrong.</div>}
+          {searchTerm.length < 2 && (
+            <div className="p-4 text-sm">Enter at least 2 characters...</div>
+          )}
 
-              {results.map(({ id, link, name }) => (
-                <PokeCommandItem
-                  key={id}
-                  value={name}
-                  onClick={() => (link ? navigateTo(link) : null)}
-                >
+          {results.map(({ id, link, name }) =>
+            link ? (
+              <Link href={link} key={id}>
+                <PokeCommandItem value={name}>
                   {name} (id: {id})
                 </PokeCommandItem>
-              ))}
-            </PokeCommandList>
-          </>
-        )}
+              </Link>
+            ) : (
+              <PokeCommandItem key={id} value={name}>
+                {name} (id: {id})
+              </PokeCommandItem>
+            )
+          )}
+        </PokeCommandList>
       </PokeCommandDialog>
     </>
   );
