49b76b349c785cdbdb8114cf2a815d9b843c2b56
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-01-16 19:24:52 +0100

    Speed up `sourceUrl` backfill scripts by leveraging indexes (#10039)
    * remove the pages backfill and leverage an index in the update
    
    * remove the documents from the notion backfill
    
    * leverage the (node_id, data_source) index on core db
    
    * reverse the order of the WHERE since the index is (data_source, node_id) and not (node_id, data_source)
    
    * :wastebasket:

--- front/migrations/20250116_backfill_notion_source_url.ts
+++ front/migrations/20250116_backfill_notion_source_url.ts
@@ -7,78 +7,37 @@ import {
 } from "@app/lib/production_checks/utils";
 import type Logger from "@app/logger/logger";
 import { makeScript } from "@app/scripts/helpers";
+import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
 
 const BATCH_SIZE = 1024;
 
-async function updateNodes(
-  coreSequelize: Sequelize,
-  nodeIds: string[],
-  urls: string[]
-) {
-  await coreSequelize.query(
-    `UPDATE data_sources_nodes
-     SET source_url = urls.url
-     FROM (SELECT unnest(ARRAY [:nodeIds]::text[]) as node_id,
-                  unnest(ARRAY [:urls]::text[])    as url) urls
-     WHERE data_sources_nodes.node_id = urls.node_id;`,
-    { replacements: { urls, nodeIds } }
-  );
-}
-
-async function backfillPages(
-  coreSequelize: Sequelize,
-  connectorsSequelize: Sequelize,
-  execute: boolean,
-  logger: typeof Logger
-) {
-  logger.info("Processing pages");
-
-  let lastId = 0;
-  let rows: { id: number; notionPageId: string; notionUrl: string }[] = [];
-
-  do {
-    rows = await connectorsSequelize.query(
-      `SELECT id, "notionPageId", "notionUrl"
-       FROM notion_pages
-       WHERE id > :lastId
-       ORDER BY id
-       LIMIT :batchSize;`,
-      {
-        replacements: { batchSize: BATCH_SIZE, lastId },
-        type: QueryTypes.SELECT,
-      }
-    );
-
-    const urls = rows.map((row) => row.notionUrl);
-    // taken from connectors/migrations/20241030_fix_notion_parents.ts
-    const nodeIds = rows.map((row) => `notion-${row.notionPageId}`);
-
-    if (rows.length === 0) {
-      break;
-    }
-
-    if (execute) {
-      await updateNodes(coreSequelize, nodeIds, urls);
-      logger.info(
-        `Updated ${rows.length} pages from id ${rows[0].id} to id ${rows[rows.length - 1].id}.`
-      );
-    } else {
-      logger.info(
-        `Would update ${rows.length} pages from id ${rows[0].id} to id ${rows[rows.length - 1].id}.`
-      );
-    }
-
-    lastId = rows[rows.length - 1].id;
-  } while (rows.length === BATCH_SIZE);
-}
-
 async function backfillDatabases(
+  frontDataSource: DataSourceModel,
   coreSequelize: Sequelize,
   connectorsSequelize: Sequelize,
   execute: boolean,
   logger: typeof Logger
 ) {
   logger.info("Processing databases");
+
+  const coreDataSourceIds: { id: number }[] = await coreSequelize.query(
+    `SELECT id
+     FROM data_sources
+     WHERE project = :projectId
+       AND data_source_id = :dataSourceId;`,
+    {
+      replacements: {
+        dataSourceId: frontDataSource.dustAPIDataSourceId,
+        projectId: frontDataSource.dustAPIProjectId,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+  const coreDataSourceId = coreDataSourceIds[0].id;
+  if (!coreDataSourceId) {
+    logger.error("No core data source found for the given front data source.");
+    return;
+  }
 
   let lastId = 0;
   let rows: {
@@ -102,12 +61,8 @@ async function backfillDatabases(
     );
 
     // taken from connectors/migrations/20241030_fix_notion_parents.ts
-    // for each database, we upsert documents with an id starting in `notion-database-` +
+    // for each database, we upsert documents (not backfilled here) with an id starting in `notion-database-` +
     // if structuredDataEnabled we also upsert a table with an id starting in `notion-`
-    const documentUrls = rows.map((row) => row.notionUrl);
-    const documentNodeIds = rows.map(
-      (row) => `notion-database-${row.notionDatabaseId}`
-    );
     const tableRows = rows.filter(
       (row) => row.structuredDataUpsertedTs !== null
     );
@@ -120,20 +75,23 @@ async function backfillDatabases(
       break;
     }
 
-    if (execute) {
-      await updateNodes(coreSequelize, documentNodeIds, documentUrls);
-      logger.info(
-        `Updated ${rows.length} databases (documents) from id ${rows[0].id} to id ${rows[rows.length - 1].id}.`
-      );
-    } else {
-      logger.info(
-        `Would update ${rows.length} databases (documents) from id ${rows[0].id} to id ${rows[rows.length - 1].id}.`
-      );
-    }
-
     if (tableRows.length > 0) {
       if (execute) {
-        await updateNodes(coreSequelize, tableNodeIds, tableUrls);
+        await coreSequelize.query(
+          `UPDATE data_sources_nodes
+           SET source_url = urls.url
+           FROM (SELECT unnest(ARRAY [:nodeIds]::text[]) as node_id,
+                        unnest(ARRAY [:urls]::text[])    as url) urls
+           WHERE data_sources_nodes.data_source = :dataSourceId
+             AND data_sources_nodes.node_id = urls.node_id;`,
+          {
+            replacements: {
+              urls: tableUrls,
+              nodeIds: tableNodeIds,
+              dataSourceId: coreDataSourceId,
+            },
+          }
+        );
         logger.info(
           `Updated ${tableRows.length} databases (tables) from id ${tableRows[0].id} to id ${tableRows[tableRows.length - 1].id}.`
         );
@@ -148,10 +106,39 @@ async function backfillDatabases(
   } while (rows.length === BATCH_SIZE);
 }
 
+async function backfillDataSource(
+  frontDataSource: DataSourceModel,
+  coreSequelize: Sequelize,
+  connectorsSequelize: Sequelize,
+  execute: boolean,
+  logger: typeof Logger
+) {
+  logger.info("Processing data source");
+
+  await backfillDatabases(
+    frontDataSource,
+    coreSequelize,
+    connectorsSequelize,
+    execute,
+    logger
+  );
+}
+
 makeScript({}, async ({ execute }, logger) => {
   const coreSequelize = getCorePrimaryDbConnection();
   const connectorsSequelize = getConnectorsReplicaDbConnection();
 
-  await backfillPages(coreSequelize, connectorsSequelize, execute, logger);
-  await backfillDatabases(coreSequelize, connectorsSequelize, execute, logger);
+  const frontDataSources = await DataSourceModel.findAll({
+    where: { connectorProvider: "notion" },
+  });
+  logger.info(`Found ${frontDataSources.length} Notion data sources`);
+  for (const frontDataSource of frontDataSources) {
+    await backfillDataSource(
+      frontDataSource,
+      coreSequelize,
+      connectorsSequelize,
+      execute,
+      logger
+    );
+  }
 });
