22d0d36fd52d6d0fea643f75ecd70c5855b90ee9
Aubin <aubin@dust.tt>
2025-01-16 19:20:12 +0100

    add aliases

--- core/src/stores/migrations/20250116_nodes_backfill_documents_source_urls.sql
+++ core/src/stores/migrations/20250116_nodes_backfill_documents_source_urls.sql
@@ -1,5 +1,5 @@
-UPDATE data_sources_nodes
-SET source_url = data_sources_documents.source_url
-FROM data_sources_documents
-WHERE data_sources_nodes.document = data_sources_documents.id
-  AND data_sources_nodes.document IS NOT NULL;
+UPDATE data_sources_nodes dsn
+SET source_url = dsd.source_url
+FROM data_sources_documents dsd
+WHERE dsn.document = dsd.id
+  AND dsn.document IS NOT NULL;
