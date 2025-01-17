919209514691d88ff55f97d0d0cae30399635022
JulesBelveze <jules.belveze@hotmail.fr>
2025-01-17 12:47:07 +0100

    [front] - refactor: update mobile sidebar implementation
     - Replaced custom sidebar implementation with Sheet components from @dust-tt/sparkle library
     - Removed Dialog and Transition.Child components and their associated classes for sidebar handling
     - Added new SheetHeader, SheetTitle, and Button components for a streamlined mobile sidebar design
     - Updated package dependencies to include the latest version of @dust-tt/sparkle

--- front/components/navigation/Navigation.tsx
+++ front/components/navigation/Navigation.tsx
@@ -1,9 +1,15 @@
-import { XMarkIcon } from "@dust-tt/sparkle";
+import {
+  Button,
+  MenuIcon,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@dust-tt/sparkle";
 import type { SubscriptionType, WorkspaceType } from "@dust-tt/types";
-import { Dialog, Transition } from "@headlessui/react";
-import { Bars3Icon } from "@heroicons/react/20/solid";
+import { Transition } from "@headlessui/react";
 import React, { Fragment, useContext } from "react";
-
 import type { SidebarNavigation } from "@app/components/navigation/config";
 import {
   NavigationSidebar,
@@ -40,80 +46,32 @@ export function Navigation({
   return (
     <div className="flex shrink-0 overflow-x-hidden">
       {/* Mobile sidebar */}
-      <div className="fixed left-0 top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 lg:hidden lg:px-6">
-        <button
-          type="button"
-          className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-          onClick={() => setSidebarOpen(true)}
+      <Sheet open={sidebarOpen} onOpenChange={setSidebarOpen}>
+        <div className="fixed left-0 top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 lg:hidden lg:px-6">
+          <SheetTrigger asChild>
+            <Button
+              variant="ghost"
+              icon={MenuIcon}
+              onClick={() => setSidebarOpen(true)}
+            />
+          </SheetTrigger>
+        </div>
+        <SheetContent
+          side="left"
+          className="flex w-full max-w-xs flex-1 border-r border-slate-800 p-0"
         >
-          <span className="sr-only">Open sidebar</span>
-          <Bars3Icon className="h-5 w-5" aria-hidden="true" />
-        </button>
-      </div>
-      <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-50 lg:hidden"
-          onClose={setSidebarOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+          <SheetHeader>
+            <SheetTitle className="hidden" />
+          </SheetHeader>
+          <NavigationSidebar
+            subscription={subscription}
+            owner={owner}
+            subNavigation={subNavigation}
           >
-            <div className="fixed inset-0 bg-gray-900/80" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <NavigationSidebar
-                  subscription={subscription}
-                  owner={owner}
-                  subNavigation={subNavigation}
-                >
-                  {navChildren && navChildren}
-                </NavigationSidebar>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
+            {navChildren && navChildren}
+          </NavigationSidebar>
+        </SheetContent>
+      </Sheet>
 
       {/*Desktop sidebar*/}
       <Transition
@@ -136,6 +94,7 @@ export function Navigation({
           </NavigationSidebar>
         </div>
       </Transition>
+
       <div
         className={classNames(
           "fixed z-40 hidden lg:top-1/2 lg:flex",
