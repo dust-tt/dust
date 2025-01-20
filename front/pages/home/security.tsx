a4e422717fb1c7fab11bdcd583b14c9988515e94
thib-martin <168569391+thib-martin@users.noreply.github.com>
2025-01-20 11:51:14 +0100

    fixing secu page

--- front/pages/home/security.tsx
+++ front/pages/home/security.tsx
@@ -1,12 +1,8 @@
-import { Div3D, Hover3D } from "@dust-tt/sparkle";
+import { ArrowRightIcon, Button, Div3D, Hover3D } from "@dust-tt/sparkle";
 import type { ReactElement } from "react";
 import React from "react";
 
-import {
-  HeaderContentBlock,
-  ImgBlock,
-} from "@app/components/home/ContentBlocks";
-import { A, Grid, P, Strong } from "@app/components/home/ContentComponents";
+import { A, Grid, H1, P, Strong } from "@app/components/home/ContentComponents";
 import type { LandingLayoutProps } from "@app/components/home/LandingLayout";
 import LandingLayout from "@app/components/home/LandingLayout";
 import {
@@ -23,136 +19,246 @@ export async function getServerSideProps() {
   };
 }
 
+const MainVisualModels = () => (
+  <Hover3D depth={-20} perspective={1000} className={classNames("relative")}>
+    <Div3D depth={-20}>
+      <img src="/static/landing/selection/selection1.png" />
+    </Div3D>
+    <Div3D depth={20} className="absolute top-0">
+      <img src="/static/landing/selection/selection2.png" />
+    </Div3D>
+    <Div3D depth={40} className="absolute top-0">
+      <img src="/static/landing/selection/selection3.png" />
+    </Div3D>
+    <Div3D depth={70} className="absolute top-0">
+      <img src="/static/landing/selection/selection4.png" />
+    </Div3D>
+  </Hover3D>
+);
+
+const MainVisualData = () => (
+  <Hover3D depth={-20} perspective={1000} className={classNames("relative")}>
+    <Div3D depth={-20}>
+      <img src="/static/landing/selection/selection1.png" />
+    </Div3D>
+    <Div3D depth={20} className="absolute top-0">
+      <img src="/static/landing/selection/selection2.png" />
+    </Div3D>
+    <Div3D depth={40} className="absolute top-0">
+      <img src="/static/landing/selection/selection3.png" />
+    </Div3D>
+    <Div3D depth={70} className="absolute top-0">
+      <img src="/static/landing/selection/selection4.png" />
+    </Div3D>
+  </Hover3D>
+);
+
+const MainVisualUsers = () => (
+  <Hover3D depth={-20} perspective={1000} className={classNames("relative")}>
+    <Div3D depth={-20}>
+      <img src="/static/landing/member/member1.png" />
+    </Div3D>
+    <Div3D depth={20} className="absolute top-0">
+      <img src="/static/landing/member/member2.png" />
+    </Div3D>
+    <Div3D depth={40} className="absolute top-0">
+      <img src="/static/landing/member/member3.png" />
+    </Div3D>
+    <Div3D depth={70} className="absolute top-0">
+      <img src="/static/landing/member/member4.png" />
+    </Div3D>
+  </Hover3D>
+);
+
 export default function Security() {
   return (
     <>
-      <HeaderContentBlock
-        title={<>Enterprise-grade Security</>}
-        from="from-yellow-200"
-        to="to-amber-400"
-        uptitle="Designed for enterprises"
-        hasCTA={false}
-        subtitle={
-          <>
-            We've made security our core focus from day&nbsp;one to safeguard
-            your&nbsp;company&nbsp;data and workspace&nbsp;privacy.
+      {" "}
+      <div className="container flex w-full flex-col gap-16 px-6 pb-12 md:gap-32">
+        <Grid className="md:gap-y-4">
+          <div
+            className={classNames(
+              "sm:pt-18 flex flex-col justify-end gap-12 pt-12 lg:pt-36",
+              "col-span-12",
+              "sm:col-span-12",
+              "lg:col-span-12",
+              "xl:col-span-12",
+              "2xl:col-span-12"
+            )}
+          >
+            <P size="lg" className="text-muted-foreground">
+              {<>Designed for enterprises</>}
+            </P>
+            <H1 from="from-yellow-200" to="to-amber-400">
+              {<>Enterprise-grade Security</>}
+            </H1>
+            <P size="lg" className="pb-6 text-slate-50">
+              We've made security our core focus from day&nbsp;one to safeguard
+              your&nbsp;company&nbsp;data and workspace&nbsp;privacy. <br></br>
+              <Strong>GDPR Compliant & SOC2 Type II Certified.</Strong>
+            </P>
             <div className="flex gap-6 py-8">
               <img src="/static/landing/security/gdpr.svg" className="h-28" />
               <img src="/static/landing/security/soc2.svg" className="h-28" />
             </div>
-            <Strong>GDPR Compliant & Soc2 type II certified</Strong>
-            <br />
-            Learn more about security at&nbsp;Dust in&nbsp;our{" "}
-            <A
+            <div className="flex gap-4">
+              <Button
+                href="https://app.vanta.com/dust.tt/trust/f3ytzxpay31bwsiyuqjto"
+                variant="highlight"
+                size="md"
+                icon={ArrowRightIcon}
+                label="Visit our Trust Center"
+                target="_blank"
+              />
+            </div>
+          </div>
+        </Grid>
+        <Grid className="md:gap-y-4">
+          <div className="col-span-12 flex flex-col justify-center py-4 sm:max-w-[100%] md:max-w-[90%]">
+            <H1 from="from-yellow-200" to="to-amber-400">
+              Ingest data on your terms
+            </H1>
+            <P size="lg" className="pb-6 text-slate-50">
+              Select the data you wish to ingest, stored in your preferred
+              region, under the highest security standards.
+            </P>
+          </div>
+          <div
+            className={classNames(
+              "col-span-12",
+              "grid grid-cols-1 gap-12 px-6",
+              "sm:grid-cols-2 sm:gap-6 sm:pr-0",
+              "lg:col-span-10 lg:col-start-2",
+              "xl:col-span-12 xl:grid-cols-4"
+            )}
+          >
+            <div className="hidden lg:block">
+              <MainVisualData />
+            </div>
+            <P size="md" dotCSS="text-amber-300" shape="triangle">
+              <Strong>Granular Data Selection</Strong>
+              <br />
+              Fully control which data Dust ingests from each source.
+            </P>
+            <P size="md" dotCSS="text-red-400" shape="rectangle">
+              <Strong>End-to-End Encryption</Strong>
+              <br />
+              Data is encrypted with AES-256 at rest, TLS in transit.
+            </P>
+            <P size="md" dotCSS="text-sky-400" shape="circle">
+              <Strong>Regional Hosting</Strong>
+              <br />
+              Host in the EU or US to meet your regulatory needs.
+            </P>
+          </div>
+        </Grid>
+
+        <Grid className="md:gap-y-4">
+          <div className="col-span-12 flex flex-col justify-center py-4 sm:max-w-[100%] md:max-w-[90%]">
+            <H1 from="from-yellow-200" to="to-amber-400">
+              Select trusted models, keep data protected
+            </H1>
+            <P size="lg" className="pb-6 text-slate-50">
+              Eliminate risks of shadow IT by leveraging our provider
+              agreements, ensuring your data is never repurposed for model
+              training.
+            </P>
+          </div>
+          <div
+            className={classNames(
+              "col-span-12",
+              "grid grid-cols-1 gap-12 px-6",
+              "sm:grid-cols-2 sm:gap-6 sm:pr-0",
+              "lg:col-span-10 lg:col-start-2",
+              "xl:col-span-12 xl:grid-cols-4"
+            )}
+          >
+            <div className="hidden lg:block">
+              <MainVisualModels />
+            </div>
+            <P size="md" dotCSS="text-amber-300" shape="triangle">
+              <Strong>No Model Training</Strong>
+              <br />
+              Your data is never used to train models.
+            </P>
+            <P size="md" dotCSS="text-red-400" shape="rectangle">
+              <Strong>Zero Data Retention</Strong>
+              <br />
+              No data is stored by third-party model providers.
+            </P>
+            <P size="md" dotCSS="text-sky-400" shape="circle">
+              <Strong>Flexible Providers</Strong>
+              <br />
+              Pick the providers and embedding models you want.
+            </P>
+          </div>
+        </Grid>
+
+        <Grid className="md:gap-y-4">
+          <div className="col-span-12 flex flex-col justify-center py-4 sm:max-w-[100%] md:max-w-[90%]">
+            <H1 from="from-yellow-200" to="to-amber-400">
+              Maintain rigorous access control at all levels
+            </H1>
+            <P size="lg" className="pb-6 text-slate-50">
+              Tailor Dust's features to each user according to specified access
+              rights.
+            </P>
+          </div>
+          <div
+            className={classNames(
+              "col-span-12",
+              "grid grid-cols-1 gap-12 px-6",
+              "sm:grid-cols-2 sm:gap-6 sm:pr-0",
+              "lg:col-span-10 lg:col-start-2",
+              "xl:col-span-12 xl:grid-cols-4"
+            )}
+          >
+            <div className="hidden lg:block">
+              <MainVisualUsers />
+            </div>
+            <P size="md" dotCSS="text-amber-300" shape="triangle">
+              <Strong>Single Sign-On (SSO)</Strong>
+              <br />
+              Use SSO to manage user access across the workspace.
+            </P>
+            <P size="md" dotCSS="text-red-400" shape="rectangle">
+              <Strong>Role-Based Access</Strong>
+              <br />
+              Assign user, builder, or admin roles to control permissions.
+            </P>
+            <P size="md" dotCSS="text-sky-400" shape="circle">
+              <Strong>Private Spaces</Strong>
+              <br />
+              Use private spaces for sensitive data, restricting access by role.
+            </P>
+          </div>
+        </Grid>
+        <div
+          className={classNames(
+            "col-span-12 flex flex-col items-center",
+            "lg:col-span-12 lg:col-start-1",
+            "xl:col-span-10 xl:col-start-2"
+          )}
+        >
+          {" "}
+          <div className="mt-4 flex justify-center gap-4">
+            <Button
+              href="/home/contact"
+              variant="outline"
+              size="md"
+              label="Contact us"
+            />{" "}
+            <Button
               href="https://app.vanta.com/dust.tt/trust/f3ytzxpay31bwsiyuqjto"
+              variant="highlight"
+              size="md"
+              icon={ArrowRightIcon}
+              label="Visit our Trust Center"
               target="_blank"
-            >
-              Trust&nbsp;Center
-            </A>
-            .
-          </>
-        }
-      />
-      <Grid>
-        <div
-          className={classNames(
-            "col-span-12",
-            "grid grid-cols-1 gap-12 px-6",
-            "sm:grid-cols-2 sm:gap-6 sm:pr-0",
-            "lg:col-span-10 lg:col-start-2",
-            "xl:col-span-12 xl:grid-cols-4"
-          )}
-        >
-          <P size="md" dotCSS="text-amber-300" shape="triangle">
-            <Strong>Encryption at rest</Strong>
-            <br />
-            Stored data encrypted with&nbsp;AES-256.
-          </P>
-          <P size="md" dotCSS="text-red-400" shape="rectangle">
-            <Strong>Encryption in transit</Strong>
-            <br />
-            Encrypted with TLS 1.2 or&nbsp;greater.
-          </P>
-          <P size="md" dotCSS="text-sky-400" shape="circle">
-            <Strong>Data segregation</Strong>
-            <br />
-            By workspace and&nbsp;companies; Services are&nbsp;isolated.
-          </P>
-          <P size="md" dotCSS="text-emerald-400" shape="hexagon">
-            <Strong>No training</Strong>
-            <br />
-            Customer prompts or&nbsp;data are not&nbsp;used for
-            training&nbsp;models.
-          </P>
+            />
+          </div>
         </div>
-      </Grid>
-      <Grid>
-        <div
-          className={classNames(
-            "col-span-12 grid grid-cols-1 gap-8",
-            "sm:grid-cols-2",
-            "lg:col-span-10 lg:col-start-2",
-            "2xl:col-span-8 2xl:col-start-3"
-          )}
-        >
-          <ImgBlock
-            title={<>Full granularity in data&nbsp;selection.</>}
-            content={
-              <>
-                For each Data&nbsp;Source, granularlly select what&nbsp;you want
-                shared with&nbsp;Dust.
-              </>
-            }
-          >
-            <Hover3D
-              depth={-20}
-              perspective={1000}
-              className={classNames("relative")}
-            >
-              <Div3D depth={-20}>
-                <img src="/static/landing/selection/selection1.png" />
-              </Div3D>
-              <Div3D depth={20} className="absolute top-0">
-                <img src="/static/landing/selection/selection2.png" />
-              </Div3D>
-              <Div3D depth={40} className="absolute top-0">
-                <img src="/static/landing/selection/selection3.png" />
-              </Div3D>
-              <Div3D depth={70} className="absolute top-0">
-                <img src="/static/landing/selection/selection4.png" />
-              </Div3D>
-            </Hover3D>
-          </ImgBlock>
-          <ImgBlock
-            title={<>Manage workspace invitations&nbsp;seamlessly.</>}
-            content={
-              <>
-                Control your workspace with Single Sign-On (SSO) and easy
-                batch&nbsp;invites.
-              </>
-            }
-          >
-            <Hover3D
-              depth={-20}
-              perspective={1000}
-              className={classNames("relative")}
-            >
-              <Div3D depth={-20}>
-                <img src="/static/landing/member/member1.png" />
-              </Div3D>
-              <Div3D depth={20} className="absolute top-0">
-                <img src="/static/landing/member/member2.png" />
-              </Div3D>
-              <Div3D depth={40} className="absolute top-0">
-                <img src="/static/landing/member/member3.png" />
-              </Div3D>
-              <Div3D depth={70} className="absolute top-0">
-                <img src="/static/landing/member/member4.png" />
-              </Div3D>
-            </Hover3D>
-          </ImgBlock>
-        </div>
-      </Grid>
+      </div>
     </>
   );
 }
