433814b771e78210f4740fc4a3dac7f9ee586c69
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-01-20 16:09:53 +0100

    updating Switch

--- sparkle/src/components/Switch.tsx
+++ sparkle/src/components/Switch.tsx
@@ -9,9 +9,9 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "data-[state=checked]:s-bg-highlight-light data-[state=unchecked]:s-bg-primary-muted",
-      "focus-visible:s-ring-ring focus-visible:s-outline-none focus-visible:s-ring-2 focus-visible:s-ring-offset-2 focus-visible:s-ring-offset-background",
-      "s-peer s-inline-flex s-h-6 s-w-11 s-shrink-0 s-cursor-pointer s-items-center s-rounded-full s-border-2 s-border-transparent s-transition-colors",
+      "data-[state=checked]:s-bg-primary data-[state=unchecked]:s-bg-primary-300",
+      "focus-visible:s-outline-none focus-visible:s-ring-2 focus-visible:s-ring-ring focus-visible:s-ring-offset-2 focus-visible:s-ring-offset-background",
+      "s-peer s-inline-flex s-h-6 s-w-10 s-shrink-0 s-cursor-pointer s-items-center s-rounded-full s-border-2 s-border-transparent s-transition-colors",
       "disabled:s-cursor-not-allowed disabled:s-opacity-50",
       className
     )}
@@ -20,7 +20,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "s-pointer-events-none s-block s-h-5 s-w-5 s-rounded-full s-bg-background s-shadow-lg s-ring-0 s-transition-transform data-[state=checked]:s-translate-x-5 data-[state=unchecked]:s-translate-x-0"
+        "s-pointer-events-none s-block s-h-5 s-w-5 s-rounded-full s-bg-background s-shadow-md s-ring-0 s-transition-transform data-[state=checked]:s-translate-x-4 data-[state=unchecked]:s-translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
