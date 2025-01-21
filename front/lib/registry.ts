7b33d061d07a8383daf5c3118c7456f70963331d
Thomas Draier <thomas@dust.tt>
2025-01-21 12:11:19 +0100

    test

--- front/lib/registry.ts
+++ front/lib/registry.ts
@@ -19,7 +19,7 @@ export const BaseDustProdActionRegistry = {
     app: {
       appId: "0e9889c787",
       appHash:
-        "4e896f08ef6c2c69c97610c861cd444e3d34c839eab44f9b4fd7dd1d166c40a2",
+        "5e896f08ef6c2c69c97610c861cd444e3d34c839eab44f9b4fd7dd1d166c40a2",
     },
     config: {
       MODEL: {
