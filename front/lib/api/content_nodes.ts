dfc8d6bc30b18b78085b72db440cd1ee08a64396
Aubin <aubin@dust.tt>
2025-01-22 10:58:53 +0100

    remove an obsolete case in getContentNodeMetadata

--- front/lib/api/content_nodes.ts
+++ front/lib/api/content_nodes.ts
@@ -185,8 +185,6 @@ export function getContentNodeMetadata(
       return { type: "database" };
     case MIME_TYPES.WEBCRAWLER.FOLDER:
       return { type: "folder" };
-    case MIME_TYPES.ZENDESK.BRAND:
-      return { type: "folder" };
     case MIME_TYPES.ZENDESK.HELP_CENTER:
       return { type: "folder" };
     case MIME_TYPES.ZENDESK.CATEGORY:
