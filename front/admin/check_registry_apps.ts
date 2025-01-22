71f3348ec8acde3b1e75326b878d33360a6a7dc0
Thomas Draier <thomas@dust.tt>
2025-01-22 12:01:10 +0100

    Use makeScript

--- front/admin/check_registry_apps.ts
+++ front/admin/check_registry_apps.ts
@@ -1,27 +1,26 @@
 import { DustAPI } from "@dust-tt/client";
-import parseArgs from "minimist";
 
 import config from "@app/lib/api/config";
 import { BaseDustProdActionRegistry } from "@app/lib/registry";
 import logger from "@app/logger/logger";
+import { makeScript } from "@app/scripts/helpers";
 
-async function main() {
-  const argv = parseArgs(process.argv.slice(2));
-
-  if (!argv.url) {
-    throw new Error("Missing --url argument");
-  }
-  if (!argv.wId) {
-    throw new Error("Missing --wId argument");
-  }
-  if (!argv.spaceId) {
-    throw new Error("Missing --spaceId argument");
-  }
+async function main({
+  url,
+  wId,
+  spaceId,
+  apiKey,
+}: {
+  url: string;
+  wId: string;
+  spaceId: string;
+  apiKey: string;
+}) {
   const api = new DustAPI(
     config.getDustAPIConfig(),
-    { apiKey: argv.apiKey, workspaceId: argv.wId },
+    { apiKey, workspaceId: wId },
     logger,
-    argv.url
+    url
   );
 
   const actions = Object.values(BaseDustProdActionRegistry);
@@ -33,7 +32,7 @@ async function main() {
         appHash: action.app.appHash,
       })),
     },
-    argv.spaceId
+    spaceId
   );
   if (res.isErr()) {
     throw new Error(res.error.message);
@@ -47,11 +46,16 @@ async function main() {
   console.log("All apps are deployed");
 }
 
-main()
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+makeScript(
+  {
+    url: { type: "string", required: true },
+    wId: { type: "string", required: true },
+    spaceId: { type: "string", required: true },
+    apiKey: { type: "string", required: true },
+  },
+  async ({ url, wId, spaceId, apiKey, execute }) => {
+    if (execute) {
+      await main({ url, wId, spaceId, apiKey });
+    }
+  }
+);
