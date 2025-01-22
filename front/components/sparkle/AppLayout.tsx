cbbd2616dceb04c4b57c4056c6594a55dfe7d4d1
Théo Vanneufville <theo@dust.tt>
2025-01-22 17:41:59 +0100

    Update AppLayout.tsx

--- front/components/sparkle/AppLayout.tsx
+++ front/components/sparkle/AppLayout.tsx
@@ -181,7 +181,7 @@ export default function AppLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MDWPND4G');
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_TRACKING_ID}');
           `,
         }}
       />
