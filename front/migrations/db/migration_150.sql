16330a4e96e24cc2c871d7d9a85469bb8961146c
Flavien David <flavien@dust.tt>
2025-01-23 13:36:22 +0100

    ➡️

--- front/migrations/db/migration_150.sql
+++
@@ -1,8 +0,0 @@
--- Migration created on Jan 23, 2025
-ALTER TABLE "public"."conversation_participants" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."user_messages" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."agent_messages" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."messages" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."message_reactions" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."mentions" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "public"."content_fragments" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
