e4f4bb153b1e99f7388042dc82f965f9b889662f
Thomas Draier <thomas@dust.tt>
2025-01-23 13:53:43 +0100

    unused

--- front/lib/api/regions/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest } from "next";
-
-import type { RegionType } from "@app/lib/api/regions/config";
-
-export function getRegionFromRequest(req: NextApiRequest): RegionType {
-  const host = req.headers.host || "";
-
-  return host.startsWith("eu.") ? "europe-west1" : "us-central1";
-}
