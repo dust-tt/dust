162f88e62b5aec4033a1cdc3819cd268c67c9c37
JulesBelveze <jules.belveze@hotmail.fr>
2025-01-23 16:35:28 +0100

    [front/migrations] - feature: make workspaceId nullable before data backfill
     - Remove the NOT NULL constraint from workspaceId in retrieval_documents and retrieval_document_chunks tables to allow backfilling data
     - Introduce a new migration to enforce the NOT NULL constraint on workspaceId after backfilling is completed
    
    [front/migrations] - feature: add pre-migration check for backfill script execution
    
     - Create a function perform_migration to check if data has been backfilled before applying the NOT NULL constraint on workspaceId
     - Conditionally apply the migration to change workspaceId to NOT NULL based on the backfilled flag set via psql argument

---
+++ front/migrations/db/migration_154.sql
@@ -0,0 +1,26 @@
+-- The backfill script is: 20250123_backfill_retrieval
+-- run psql with --set=backfilled=1 argument if you have rune the script.
+
+CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
+RETURNS VARCHAR AS $$
+BEGIN
+    IF NOT backfilled THEN
+        RAISE NOTICE 'The backfill script: 20250123_backfill_retrieval.ts is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.';
+    END IF;
+
+-- Migration created on Jan 23, 2025
+ALTER TABLE "public"."retrieval_documents" ALTER COLUMN "workspaceId" SET NOT NULL;
+ALTER TABLE "public"."retrieval_document_chunks" ALTER COLUMN "workspaceId" SET NOT NULL;
+
+    RETURN 'success';
+END;
+$$ LANGUAGE plpgsql;
+
+\if :{?backfilled}
+   SELECT perform_migration(:'backfilled'::boolean);
+\else
+    \echo '!! Migration was NOT applied !!'
+    \echo 'The backfill script: 20250123_backfill_retrieval is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.'
+\endif
+
+DROP FUNCTION perform_migration(boolean);
