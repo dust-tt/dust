3fdad2684647b21c2f685c852e59f5c6bd7cd031
Aubin <aubin@dust.tt>
2025-01-23 15:53:32 +0100

    replace another call using node-zendesk with our wrapper

--- connectors/src/connectors/zendesk/temporal/activities.ts
+++ connectors/src/connectors/zendesk/temporal/activities.ts
@@ -16,6 +16,7 @@ import {
   fetchZendeskArticlesInCategory,
   fetchZendeskBrand,
   fetchZendeskCategoriesInBrand,
+  fetchZendeskCategory,
   fetchZendeskManyUsers,
   fetchZendeskTicketComments,
   fetchZendeskTickets,
@@ -410,17 +411,22 @@ export async function syncZendeskCategoryActivity({
     return { shouldSyncArticles: false, helpCenterIsAllowed: null };
   }
 
-  const zendeskApiClient = createZendeskClient(
-    await getZendeskSubdomainAndAccessToken(connector.connectionId)
+  const { accessToken, subdomain } = await getZendeskSubdomainAndAccessToken(
+    connector.connectionId
   );
-  await changeZendeskClientSubdomain(zendeskApiClient, {
+  const brandSubdomain = await getZendeskBrandSubdomain({
     connectorId,
+    accessToken,
+    subdomain,
     brandId,
   });
 
   // if the category is not on Zendesk anymore, we remove its permissions
-  const { result: fetchedCategory } =
-    await zendeskApiClient.helpcenter.categories.show(categoryId);
+  const fetchedCategory = await fetchZendeskCategory({
+    accessToken,
+    brandSubdomain,
+    categoryId,
+  });
   if (!fetchedCategory) {
     await categoryInDb.revokePermissions();
     return { shouldSyncArticles: false, helpCenterIsAllowed: null };
