942f6da1140ea9ec78d54e38b29d4da624b811ff
Flavien David <flavien@dust.tt>
2025-01-24 12:00:52 +0100

    🙈

--- front/migrations/db/migration_158.sql
+++ front/migrations/db/migration_158.sql
@@ -1,10 +1,10 @@
--- The backfill script is: 20250123_backfill_retrieval
+-- The backfill script is: 20250124_backfill_run_usages
 -- run psql with --set=backfilled=1 argument if you have rune the script.
 CREATE OR REPLACE FUNCTION perform_migration(backfilled boolean DEFAULT false)
 RETURNS VARCHAR AS $$
 BEGIN
     IF NOT backfilled THEN
-        RAISE NOTICE 'The backfill script: 20250123_backfill_retrieval.ts is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.';
+        RAISE NOTICE 'The backfill script: 20250124_backfill_run_usages.ts is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.';
     END IF;
 
 -- Migration created on Jan 24, 2025
@@ -18,7 +18,7 @@ $$ LANGUAGE plpgsql;
    SELECT perform_migration(:'backfilled'::boolean);
 \else
     \echo '!! Migration was NOT applied !!'
-    \echo 'The backfill script: 20250123_backfill_retrieval is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.'
+    \echo 'The backfill script: 20250124_backfill_run_usages is required before applying this migration. If you already did it, run psql with --set=backfilled=1 argument.'
 \endif
 
 DROP FUNCTION perform_migration(boolean);
