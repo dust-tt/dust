fb4f27f4262b731c5b59434439ac7b1f9385703b
filou <philipperolet@gmail.com>
2025-01-29 10:55:56 +0100

    good

--- connectors/migrations/20250128_github_backfill_folders_bis.ts
+++ connectors/migrations/20250128_github_backfill_folders_bis.ts
@@ -1,73 +1,95 @@
-import {
-  getDiscussionsInternalId,
-  getIssuesInternalId,
-  getRepositoryInternalId,
-  matchGithubInternalIdType,
-} from "@connectors/connectors/github/lib/utils";
-import { GithubDiscussion, GithubIssue } from "@connectors/lib/models/github";
-import { ConnectorResource } from "@connectors/resources/connector_resource";
-import { ModelId } from "@dust-tt/types";
-import { Logger } from "pino";
+import type { ModelId } from "@dust-tt/types";
+import { concurrentExecutor } from "@dust-tt/types";
+import type { Logger } from "pino";
 import { makeScript } from "scripts/helpers";
-import { Sequelize } from "sequelize";
+
+import {
+  githubGetReposResultPageActivity,
+  githubUpsertDiscussionsFolderActivity,
+  githubUpsertIssuesFolderActivity,
+  githubUpsertRepositoryFolderActivity,
+} from "@connectors/connectors/github/temporal/activities";
+import { ConnectorResource } from "@connectors/resources/connector_resource";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 makeScript({}, async ({ execute }, logger) => {
   const connectors = await ConnectorResource.listByType("github", {});
 
   for (const connector of connectors) {
-    await backfillMissingFolders(connector, logger);
+    await backfillMissingFolders(connector, logger, execute);
   }
 });
 
 async function backfillMissingFolders(
   connector: ConnectorResource,
-  parentLogger: Logger
+  parentLogger: Logger,
+  execute: boolean
 ) {
   const connectorId = connector.id;
-  const logger = parentLogger.child({ connectorId });
-  const repoIds = await getRepositoryIds(connectorId, logger);
-  const internalIdsToCheck = repoIds
-    .map((repoId) => [
-      getRepositoryInternalId(repoId),
-      getDiscussionsInternalId(repoId),
-      getIssuesInternalId(repoId),
-    ])
-    .flat();
-  const idsNotInCore = await getFoldersNotInCore(internalIdsToCheck, logger);
-  for (const id of idsNotInCore) {
-    const { type, repoId } = matchGithubInternalIdType(id);
-    switch (type) {
-      case "REPO_FULL":
-        break;
-      case "REPO_ISSUES":
-        break;
-      case "REPO_DISCUSSIONS":
-        break;
-      default:
-        throw new Error(`Unexpected type: ${type}`);
-    }
-  }
+  const logger = parentLogger.child({ connectorId, execute });
+  logger.info("Backfilling folders");
+  const repos = await getRepositories(connectorId, logger);
+  await concurrentExecutor(
+    repos,
+    async (repo) => {
+      const repoId = repo.id;
+      const repoName = repo.name;
+      const repoLogin = repo.login;
+      logger.info(
+        { repoId, repoName, repoLogin },
+        "Upserting repository folders"
+      );
+      if (execute) {
+        await githubUpsertRepositoryFolderActivity({
+          connectorId,
+          repoId,
+          repoName,
+          repoLogin,
+        });
+        await githubUpsertIssuesFolderActivity({
+          connectorId,
+          repoId,
+          repoLogin,
+          repoName,
+        });
+        await githubUpsertDiscussionsFolderActivity({
+          connectorId,
+          repoId,
+          repoLogin,
+          repoName,
+        });
+      }
+    },
+    { concurrency: 4 }
+  );
+  logger.info("Finished backfilling folders");
 }
 
-async function getRepositoryIds(connectorId: ModelId, logger: Logger) {
-  const discussionRepos = await GithubDiscussion.findAll({
-    where: { connectorId: connectorId },
-    attributes: [[Sequelize.fn("DISTINCT", Sequelize.col("repoId")), "repoId"]],
-  });
-  const issuesRepos = await GithubIssue.findAll({
-    where: { connectorId: connectorId },
-    attributes: [[Sequelize.fn("DISTINCT", Sequelize.col("repoId")), "repoId"]],
-  });
-  const repositoryIds = [
-    ...new Set([
-      ...discussionRepos.map((repository) => repository.repoId),
-      ...issuesRepos.map((repository) => repository.repoId),
-    ]),
-  ];
-  logger.info(
-    { repositoryIds },
-    `Found ${repositoryIds.length} repository ids`
-  );
-  return repositoryIds;
+async function getRepositories(connectorId: ModelId) {
+  let pageNumber = 1; // 1-indexed
+  const repos: {
+    id: number;
+    name: string;
+    login: string;
+  }[] = [];
+  for (;;) {
+    const resultsPage = await githubGetReposResultPageActivity(
+      connectorId,
+      pageNumber,
+      { syncCodeOnly: "false" }
+    );
+    if (!resultsPage.length) {
+      break;
+    }
+    pageNumber += 1;
+
+    for (const repo of resultsPage) {
+      repos.push({
+        id: repo.id,
+        name: repo.name,
+        login: repo.login,
+      });
+    }
+  }
+  return repos;
 }
