6f7105655bd59d5100bdfb1e9ac11a0c3a0d91c3
Stanislas Polu <spolu@dust.tt>
2025-01-29 15:29:41 +0100

    cont'd

--- front/lib/api/assistant/configuration.ts
+++ front/lib/api/assistant/configuration.ts
@@ -35,6 +35,7 @@ import { Op, Sequelize, UniqueConstraintError } from "sequelize";
 
 import {
   DEFAULT_BROWSE_ACTION_NAME,
+  DEFAULT_GITHUB_CREATE_ISSUE_ACTION_NAME,
   DEFAULT_GITHUB_GET_PULL_REQUEST_ACTION_NAME,
   DEFAULT_PROCESS_ACTION_NAME,
   DEFAULT_REASONING_ACTION_NAME,
@@ -1194,6 +1195,24 @@ export async function createAgentActionConfiguration(
         sId: githubConfig.sId,
         type: "github_get_pull_request_configuration",
         name: action.name || DEFAULT_GITHUB_GET_PULL_REQUEST_ACTION_NAME,
+        description: action.description,
+      });
+    }
+    case "github_create_issue_configuration": {
+      const githubConfig = await AgentGithubConfiguration.create({
+        sId: generateRandomModelSId(),
+        agentConfigurationId: agentConfiguration.id,
+        actionType: "github_create_issue_action",
+        name: action.name,
+        description: action.description,
+        workspaceId: owner.id,
+      });
+
+      return new Ok({
+        id: githubConfig.id,
+        sId: githubConfig.sId,
+        type: "github_create_issue_configuration",
+        name: action.name || DEFAULT_GITHUB_CREATE_ISSUE_ACTION_NAME,
         description: action.description,
       });
     }
