3e2bfe97c4a09ed6e343efb235bd7b1945122c79
Flavien David <flavien@dust.tt>
2025-01-31 11:12:47 +0100

    Use proper env variables for relocation worker.

--- front/temporal/relocation/temporal.ts
+++ front/temporal/relocation/temporal.ts
@@ -3,13 +3,16 @@ import { NativeConnection } from "@temporalio/worker";
 import {
   getConnectionOptions,
   getTemporalClientForNamespace,
+  temporalWorkspaceToEnvVar,
 } from "@app/lib/temporal";
 
-export async function getTemporalWorkerConnection(): Promise<{
+export async function getTemporalRelocationWorkerConnection(): Promise<{
   connection: NativeConnection;
   namespace: string | undefined;
 }> {
-  const connectionOptions = await getConnectionOptions("relocation");
+  const connectionOptions = await getConnectionOptions(
+    temporalWorkspaceToEnvVar["relocation"]
+  );
   const connection = await NativeConnection.connect(connectionOptions);
   return { connection, namespace: process.env.TEMPORAL_RELOCATION_NAMESPACE };
 }
