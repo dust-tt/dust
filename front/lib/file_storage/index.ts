ccd8623be563b72c1bbd23e2feecf278c3300db7
Flavien David <flavien.david74@gmail.com>
2025-01-31 10:37:33 +0100

    Add support for pod annotation service account. (#10415)

--- front/lib/file_storage/index.ts
+++ front/lib/file_storage/index.ts
@@ -9,13 +9,20 @@ import { isGCSNotFoundError } from "@app/lib/file_storage/types";
 
 const DEFAULT_SIGNED_URL_EXPIRATION_DELAY_MS = 5 * 60 * 1000; // 5 minutes.
 
+interface FileStorageOptions {
+  useServiceAccount?: boolean;
+}
+
 export class FileStorage {
   private readonly bucket: Bucket;
   private readonly storage: Storage;
 
-  constructor(bucketKey: string) {
+  constructor(
+    bucketKey: string,
+    { useServiceAccount }: FileStorageOptions = { useServiceAccount: true }
+  ) {
     this.storage = new Storage({
-      keyFilename: config.getServiceAccount(),
+      keyFilename: useServiceAccount ? config.getServiceAccount() : undefined,
     });
 
     this.bucket = this.storage.bucket(bucketKey);
@@ -126,17 +133,18 @@ export class FileStorage {
 
 const bucketInstances = new Map();
 
-export const getBucketInstance: (bucketConfig: string) => FileStorage = (
-  bucketConfig: string
-) => {
+export const getBucketInstance: (
+  bucketConfig: string,
+  options?: FileStorageOptions
+) => FileStorage = (bucketConfig, options) => {
   if (!bucketInstances.has(bucketConfig)) {
-    bucketInstances.set(bucketConfig, new FileStorage(bucketConfig));
+    bucketInstances.set(bucketConfig, new FileStorage(bucketConfig, options));
   }
   return bucketInstances.get(bucketConfig);
 };
 
-export const getPrivateUploadBucket = () =>
-  getBucketInstance(config.getGcsPrivateUploadsBucket());
+export const getPrivateUploadBucket = (options?: FileStorageOptions) =>
+  getBucketInstance(config.getGcsPrivateUploadsBucket(), options);
 
-export const getPublicUploadBucket = () =>
-  getBucketInstance(config.getGcsPublicUploadBucket());
+export const getPublicUploadBucket = (options?: FileStorageOptions) =>
+  getBucketInstance(config.getGcsPublicUploadBucket(), options);
