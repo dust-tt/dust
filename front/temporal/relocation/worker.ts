f96cb1849d8117a59799a07e6d567a4fedefb13e
Flavien David <flavien@dust.tt>
2025-01-31 10:19:51 +0100

    Add support for pod annotation service account.

--- front/temporal/relocation/worker.ts
+++ front/temporal/relocation/worker.ts
@@ -5,12 +5,12 @@ import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 import { config } from "@app/lib/api/regions/config";
 import { ActivityInboundLogInterceptor } from "@app/lib/temporal_monitoring";
 import logger from "@app/logger/logger";
-import * as frontDestinationActivities from "@app/temporal/relocation/activities/destination_region/front";
-import * as frontSourceActivities from "@app/temporal/relocation/activities/source_region/front";
 import * as connectorsDestinationActivities from "@app/temporal/relocation/activities/destination_region/connectors/sql";
+import * as coreDestinationActivities from "@app/temporal/relocation/activities/destination_region/core";
+import * as frontDestinationActivities from "@app/temporal/relocation/activities/destination_region/front";
 import * as connectorsSourceActivities from "@app/temporal/relocation/activities/source_region/connectors/sql";
 import * as coreSourceActivities from "@app/temporal/relocation/activities/source_region/core";
-import * as coreDestinationActivities from "@app/temporal/relocation/activities/destination_region/core";
+import * as frontSourceActivities from "@app/temporal/relocation/activities/source_region/front";
 import { RELOCATION_QUEUES_PER_REGION } from "@app/temporal/relocation/config";
 import { getTemporalWorkerConnection } from "@app/temporal/relocation/temporal";
 
