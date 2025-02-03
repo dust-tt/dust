d201d57fd1cb9885320445d82aab9e4e018d80f7
Stanislas Polu <spolu@dust.tt>
2025-02-03 16:39:06 +0100

    fix

--- front/lib/api/files/upsert.ts
+++ front/lib/api/files/upsert.ts
@@ -218,6 +218,7 @@ const upsertDocumentToDatasource: ProcessingFunction = async ({
   }
   const { title: upsertTitle, ...restArgs } = upsertArgs ?? {};
   const upsertDocumentRes = await upsertDocument({
+    // Beware, most values here are default values that are overrided by the ...restArgs below.
     document_id: documentId,
     source_url: sourceUrl,
     text: content,
@@ -257,16 +258,14 @@ const upsertTableToDatasource: ProcessingFunction = async ({
   if (upsertArgs && "tableId" in upsertArgs) {
     tableId = upsertArgs.tableId ?? tableId;
   }
-  let description = "Table uploaded from file";
-  if (upsertArgs && "description" in upsertArgs && !upsertArgs.description) {
-    description = upsertArgs.description;
-  }
   const { title: upsertTitle, ...restArgs } = upsertArgs ?? {};
 
   const upsertTableRes = await upsertTable({
+    // Beware, most values here are default values that are overrided by the ...restArgs below,
+    // including description.
     tableId,
     name: slugify(file.fileName),
-    description,
+    description: "Table uploaded from file",
     truncate: true,
     csv: content,
     tags: [`title:${file.fileName}`, `fileId:${file.sId}`],
@@ -306,17 +305,7 @@ type ProcessingFunction = ({
   file: FileResource;
   content: string;
   dataSource: DataSourceResource;
-  upsertArgs?:
-    | Pick<UpsertDocumentArgs, "document_id" | "title" | "tags">
-    | Pick<
-        UpsertTableArgs,
-        | "name"
-        | "title"
-        | "description"
-        | "tableId"
-        | "tags"
-        | "useAppForHeaderDetection"
-      >;
+  upsertArgs?: UpsertDocumentArgs | UpsertTableArgs;
 }) => Promise<Result<undefined, Error>>;
 
 const getProcessingFunction = ({
@@ -432,17 +421,7 @@ export async function processAndUpsertToDataSource(
   }: {
     file: FileResource;
     optionalContent?: string;
-    upsertArgs?:
-      | Pick<UpsertDocumentArgs, "document_id" | "title" | "tags">
-      | Pick<
-          UpsertTableArgs,
-          | "name"
-          | "title"
-          | "description"
-          | "tableId"
-          | "tags"
-          | "useAppForHeaderDetection"
-        >;
+    upsertArgs?: UpsertDocumentArgs | UpsertTableArgs;
   }
 ): Promise<
   Result<
