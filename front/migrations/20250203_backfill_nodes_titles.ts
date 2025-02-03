ef8b8c8b6ab782b931993b4d823299fba5cd685c
Aubin <aubin@dust.tt>
2025-02-03 15:12:39 +0100

    add backfill script

---
+++ front/migrations/20250203_backfill_nodes_titles.ts
@@ -0,0 +1,247 @@
+import { concurrentExecutor } from "@dust-tt/types";
+import type { Sequelize } from "sequelize";
+import { Op, QueryTypes } from "sequelize";
+
+import { getCorePrimaryDbConnection } from "@app/lib/production_checks/utils";
+import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
+import type Logger from "@app/logger/logger";
+import { makeScript } from "@app/scripts/helpers";
+
+const DATASOURCE_BATCH_SIZE = 256;
+const SELECT_BATCH_SIZE = 256;
+
+const DATASOURCE_CONCURRENCY = 4;
+
+type Node = {
+  node_id: string;
+  tags_array: Record<string, string>;
+  timestamp: number;
+  title: string;
+};
+
+async function getCoreDataSourceId(
+  frontDataSource: DataSourceModel,
+  coreSequelize: Sequelize
+): Promise<number | null> {
+  const { dustAPIProjectId, dustAPIDataSourceId } = frontDataSource;
+  const coreDataSource: any = (
+    await coreSequelize.query(
+      `SELECT id
+       FROM data_sources
+       WHERE project = :dustAPIProjectId
+         AND data_source_id = :dustAPIDataSourceId
+       LIMIT 1`,
+      {
+        replacements: { dustAPIProjectId, dustAPIDataSourceId },
+        type: QueryTypes.SELECT,
+      }
+    )
+  )[0];
+  return coreDataSource?.id || null;
+}
+
+function logInconsistencies(nodes: Node[], logger: typeof Logger) {
+  const diff = Object.fromEntries(
+    nodes
+      .filter(
+        (n) =>
+          n.title !== n.tags_array.title.split(":")[0] &&
+          n.title !== n.tags_array.title
+      )
+      .map((n) => [
+        n.node_id,
+        { tagTitle: n.tags_array.title, nodeTitle: n.title },
+      ])
+  );
+  logger.info({ diff }, "Title inconsistencies.");
+}
+
+async function processNodes({
+  allNodes,
+  coreDataSourceId,
+  coreSequelize,
+}: {
+  allNodes: Node[];
+  coreDataSourceId: number;
+  coreSequelize: Sequelize;
+}) {
+  const nodes = allNodes.filter(
+    (n) => n.title === n.tags_array.title.split(":")[0]
+  );
+  if (nodes.length === 0) {
+    return;
+  }
+  // Replacing the titles with the ones in the tags.
+  const titles = nodes.map((n) => n.tags_array.title);
+
+  await coreSequelize.query(
+    `UPDATE data_sources_nodes dsn
+     SET title = unnested.title
+     FROM (
+              SELECT UNNEST(ARRAY [:nodeIds]::text[]) AS node_id,
+                     UNNEST(ARRAY [:titles]::text[])  AS title
+          ) unnested
+     WHERE dsn.data_source = :coreDataSourceId
+       AND dsn.node_id = unnested.node_id;`,
+    {
+      replacements: {
+        nodeIds: nodes.map((n) => n.node_id),
+        titles,
+        coreDataSourceId,
+      },
+    }
+  );
+}
+
+async function migrateDocuments({
+  coreDataSourceId,
+  coreSequelize,
+  execute,
+  logger,
+}: {
+  coreDataSourceId: number;
+  coreSequelize: Sequelize;
+  execute: boolean;
+  logger: typeof Logger;
+}) {
+  let nextId = "";
+  let nodes: Node[];
+
+  do {
+    nodes = (await coreSequelize.query(
+      `SELECT dsn.node_id, dsn.timestamp, dsn.title, dsd.tags_array
+       FROM data_sources_nodes dsn
+            JOIN data_sources_documents dsd ON dsd.id = dsn.document
+       WHERE dsn.data_source = :coreDataSourceId
+         AND dsn.node_id > :nextId
+       LIMIT :batchSize`,
+      {
+        replacements: {
+          coreDataSourceId,
+          batchSize: SELECT_BATCH_SIZE,
+          nextId,
+        },
+        type: QueryTypes.SELECT,
+      }
+    )) as Node[];
+
+    logger.info(`Found ${nodes.length} documents to process.`);
+    logInconsistencies(nodes, logger);
+
+    if (execute) {
+      await processNodes({
+        allNodes: nodes,
+        coreSequelize,
+        coreDataSourceId,
+      });
+    }
+    nextId = nodes[nodes.length - 1]?.node_id;
+  } while (nodes.length === SELECT_BATCH_SIZE);
+}
+
+async function migrateTables({
+  coreDataSourceId,
+  coreSequelize,
+  execute,
+  logger,
+}: {
+  coreDataSourceId: number;
+  coreSequelize: Sequelize;
+  execute: boolean;
+  logger: typeof Logger;
+}) {
+  let nextId = "";
+  let nodes: Node[];
+
+  do {
+    nodes = (await coreSequelize.query(
+      `SELECT dsn.node_id, dsn.timestamp, dsn.title, t.tags_array
+       FROM data_sources_nodes dsn
+            JOIN tables t ON t.id = dsn.table
+       WHERE dsn.data_source = :coreDataSourceId
+         AND dsn.node_id > :nextId
+       LIMIT :batchSize`,
+      {
+        replacements: {
+          coreDataSourceId,
+          batchSize: SELECT_BATCH_SIZE,
+          nextId,
+        },
+        type: QueryTypes.SELECT,
+      }
+    )) as Node[];
+
+    logger.info(`Found ${nodes.length} tables to process.`);
+    logInconsistencies(nodes, logger);
+
+    if (execute) {
+      await processNodes({
+        allNodes: nodes,
+        coreSequelize,
+        coreDataSourceId,
+      });
+    }
+    nextId = nodes[nodes.length - 1]?.node_id;
+  } while (nodes.length === SELECT_BATCH_SIZE);
+}
+
+async function migrateDataSource(
+  frontDataSource: DataSourceModel,
+  coreSequelize: Sequelize,
+  execute: boolean,
+  parentLogger: typeof Logger
+) {
+  const logger = parentLogger.child({
+    project: frontDataSource.dustAPIProjectId,
+    dataSourceId: frontDataSource.dustAPIDataSourceId,
+  });
+  logger.info("MIGRATE");
+
+  const coreDataSourceId = await getCoreDataSourceId(
+    frontDataSource,
+    coreSequelize
+  );
+  if (!coreDataSourceId) {
+    logger.error("No core datasource found.");
+    return;
+  }
+
+  await migrateDocuments({ coreDataSourceId, coreSequelize, execute, logger });
+  await migrateTables({ coreDataSourceId, coreSequelize, execute, logger });
+}
+
+async function migrateDataSources(
+  nextDataSourceId: number,
+  coreSequelize: Sequelize,
+  execute: boolean,
+  logger: typeof Logger
+) {
+  let startId = nextDataSourceId;
+  let staticDataSources;
+  do {
+    staticDataSources = await DataSourceModel.findAll({
+      where: { connectorProvider: null, id: { [Op.gt]: startId } },
+      limit: DATASOURCE_BATCH_SIZE,
+      order: [["id", "ASC"]],
+    });
+
+    await concurrentExecutor(
+      staticDataSources,
+      async (dataSource) => {
+        await migrateDataSource(dataSource, coreSequelize, execute, logger);
+      },
+      { concurrency: DATASOURCE_CONCURRENCY }
+    );
+    if (staticDataSources.length > 0) {
+      startId = staticDataSources[staticDataSources.length - 1].id;
+    }
+  } while (staticDataSources.length === DATASOURCE_BATCH_SIZE); // early exit on the first incomplete batch
+}
+
+makeScript(
+  { nextDataSourceId: { type: "number", default: 0 } },
+  async ({ nextDataSourceId, execute }, logger) => {
+    const coreSequelize = getCorePrimaryDbConnection();
+    await migrateDataSources(nextDataSourceId, coreSequelize, execute, logger);
+  }
+);
