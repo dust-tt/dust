040c4aadae3be2845cbc86c95d9e5853f95029a6
Flavien David <flavien@dust.tt>
2025-02-03 13:51:21 +0100

    Augment maintenance page.

--- front/pages/maintenance.tsx
+++ front/pages/maintenance.tsx
@@ -3,72 +3,103 @@ import { isString } from "@dust-tt/types";
 import type { InferGetServerSidePropsType } from "next";
 import Link from "next/link";
 
+import type { RegionInfo } from "@app/lib/api/regions/config";
+import { config } from "@app/lib/api/regions/config";
 import { makeGetServerSidePropsRequirementsWrapper } from "@app/lib/iam/session";
 
 export const getServerSideProps = makeGetServerSidePropsRequirementsWrapper({
   requireUserPrivilege: "none",
 })<{
   code: string;
+  otherRegion: RegionInfo;
 }>(async (context) => {
   return {
     props: {
       code: isString(context.query.code) ? context.query.code : "",
+      otherRegion: config.getOtherRegionInfo(),
     },
   };
 });
 
 const defaultErrorMessageClassName = "text-base font-normal text-slate-100";
 
-function getErrorMessage(code: string) {
+interface MaintenancePageInfo {
+  title: string;
+  message: React.ReactNode;
+  buttonLabel: string;
+  buttonUrl: string;
+}
+
+function getMaintenancePageInfo(
+  code: string,
+  otherRegion: RegionInfo
+): MaintenancePageInfo {
   switch (code) {
     case "relocation":
-      return (
-        <>
-          <Page.Header
-            title={
-              <span className="text-white">Service Relocation in Progress</span>
-            }
-          />
+      return {
+        title: "Service Relocation in Progress",
+        message: (
+          <>
+            <p className={defaultErrorMessageClassName}>
+              Your account is currently being relocated to a new region. During
+              this planned migration, you won't be able to access our
+              application. This temporary interruption ensures a smooth
+              transition of your organization's data.
+            </p>
+            <h4 className="text-xl font-bold text-white">What's happening?</h4>
+            <p className={defaultErrorMessageClassName}>
+              As discussed with your team, we're moving your account to a
+              different regional infrastructure. All your data, settings, and
+              configurations will remain exactly as they were. We'll notify your
+              team once the relocation is complete and your access is restored.
+            </p>
+          </>
+        ),
+        buttonLabel: "Back to homepage",
+        buttonUrl: "/",
+      };
+
+    case "relocation-done":
+      return {
+        title: "Service Relocation Complete",
+        message: (
           <p className={defaultErrorMessageClassName}>
-            Your account is currently being relocated to a new region. During
-            this planned migration, you won't be able to access our application.
-            This temporary interruption ensures a smooth transition of your
-            organization's data.
+            Your account has been successfully relocated to a new region. You
+            can now access our application.
           </p>
-          <h4 className="text-xl font-bold text-white">What's happening?</h4>
-          <p className={defaultErrorMessageClassName}>
-            As discussed with your team, we're moving your account to a
-            different regional infrastructure. All your data, settings, and
-            configurations will remain exactly as they were. We'll notify your
-            team once the relocation is complete and your access is restored.
-          </p>
-        </>
-      );
+        ),
+        buttonLabel: `Continue to ${otherRegion.name}`,
+        buttonUrl: `${otherRegion.url}/`,
+      };
+
     default:
-      return (
-        <>
-          <Page.Header
-            title={<span className="text-white">Under Maintenance</span>}
-          />
-          <p className={defaultErrorMessageClassName}>
-            We're currently performing maintenance on this workspace.
-            <br />
-            Please check back in a few minutes.
-          </p>
-          <p className="text-sm font-normal italic text-slate-300">
-            If this persists for an extended period,
-            <br />
-            please contact us at support@dust.tt
-          </p>{" "}
-        </>
-      );
+      return {
+        title: "Under Maintenance",
+        message: (
+          <>
+            <p className={defaultErrorMessageClassName}>
+              We're currently performing maintenance on this workspace.
+              <br />
+              Please check back in a few minutes.
+            </p>
+            <p className="text-sm font-normal italic text-slate-300">
+              If this persists for an extended period,
+              <br />
+              please contact us at support@dust.tt
+            </p>
+          </>
+        ),
+        buttonLabel: "Back to homepage",
+        buttonUrl: "/",
+      };
   }
 }
 
 export default function MaintenancePage({
   code,
+  otherRegion,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const errorMessage = getErrorMessage(code);
+  const maintenancePageInfo = getMaintenancePageInfo(code, otherRegion);
 
   return (
     <>
@@ -78,10 +109,21 @@ export default function MaintenancePage({
           <div className="flex flex-col items-center gap-6 text-center">
             <Icon visual={LogoSquareColorLogo} size="lg" />
             <div className="mx-20 flex flex-col items-center gap-6">
-              {errorMessage}
+              <Page.Header
+                title={
+                  <span className="text-white">
+                    {maintenancePageInfo.title}
+                  </span>
+                }
+              />
+              {maintenancePageInfo.message}
             </div>
-            <Link href="/">
-              <Button variant="primary" label="Back to homepage" size="sm" />
+            <Link href={maintenancePageInfo.buttonUrl}>
+              <Button
+                variant="outline"
+                label={maintenancePageInfo.buttonLabel}
+                size="sm"
+              />
             </Link>
           </div>
         </div>
