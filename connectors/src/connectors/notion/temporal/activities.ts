5df0cdb22e97d3951c17ced96c2feaa2cbe19b85
Aubin <aubin@dust.tt>
2025-02-05 17:36:31 +0100

    add parent info to the log

--- connectors/src/connectors/notion/temporal/activities.ts
+++ connectors/src/connectors/notion/temporal/activities.ts
@@ -2006,8 +2006,9 @@ export async function renderAndUpsertPageFromCache({
 
     const parentIds = parentPageOrDbIds.map((id) => `notion-${id}`);
     if (parentIds.length === 1) {
+      const page = await getNotionPageFromConnectorsDb(connectorId, pageId);
       localLogger.warn(
-        { parentIds },
+        { parentIds, parentType: page?.parentType, parentId: page?.parentId },
         "notionRenderAndUpsertPageFromCache: Page has no parent."
       );
     }
