8beba6408f177c36655069c4fc0cf7d4d2c63e14
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-02-10 11:38:10 +0100

    add exclusion rule due to escaping (#10652)

--- front/lib/api/content_nodes.ts
+++ front/lib/api/content_nodes.ts
@@ -283,6 +283,11 @@ export function computeNodesDiff({
         ) {
           return false;
         }
+
+        // Ignore diff in the sourceUrl due to URI encoding.
+        if (key === "sourceUrl" && coreValue === encodeURI(value)) {
+          return false;
+        }
         // Special case for expandable: if the core node is not expandable and the connectors one is, it means
         // that the difference comes from the fact that the node has no children: we omit from the log.
         if (key === "expandable" && value === true && coreValue === false) {
