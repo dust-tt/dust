5f1508dd614f57eaa1944606290799b115e1f7d6
JulesBelveze <jules.belveze@hotmail.fr>
2025-02-11 11:00:51 +0100

    [front] - refactor: reorganize SpaceWebsiteModal import in edit space component
     - Moved the SpaceWebsiteModal import to a more appropriate location within the components directory structure
     - Cleaned up unnecessary import destructuring for consistency and readability

--- front/components/spaces/EditSpaceStaticDatasourcesViews.tsx
+++ front/components/spaces/EditSpaceStaticDatasourcesViews.tsx
@@ -1,13 +1,9 @@
 import { Button, PlusIcon, Tooltip } from "@dust-tt/sparkle";
-import type {
-  DataSourceViewType,
-  SpaceType,
-  WorkspaceType,
-} from "@dust-tt/types";
+import type { DataSourceViewType, SpaceType, WorkspaceType } from "@dust-tt/types";
 
 import SpaceFolderModal from "@app/components/spaces/SpaceFolderModal";
-import SpaceWebsiteModal from "@app/components/spaces/SpaceWebsiteModal";
 import { useKillSwitches } from "@app/lib/swr/kill";
+import SpaceWebsiteModal from "@app/components/spaces/websites/SpaceWebsiteModal";
 
 interface EditSpaceStaticDatasourcesViewsProps {
   canWriteInSpace: boolean;
