72fb485a204dae23d3a70e68d21970a169551ba2
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2025-02-11 09:34:42 +0100

    Update GitHub issue title to distinguish issues and PRs

--- connectors/src/connectors/github/temporal/activities.ts
+++ connectors/src/connectors/github/temporal/activities.ts
@@ -149,7 +149,7 @@ async function renderIssue(
 
   const content = await renderDocumentTitleAndContent({
     dataSourceConfig,
-    title: `Issue #${issue.number} [${repoName}]: ${issue.title}`,
+    title: `${issue.isPullRequest ? "Pull Request" : "Issue"} #${issue.number} [${repoName}]: ${issue.title}`,
     createdAt: issue.createdAt || issue.updatedAt,
     updatedAt: issue.updatedAt,
     author: renderGithubUser(issue.creator),
