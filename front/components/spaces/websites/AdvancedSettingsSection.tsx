b93ccf00f38edc95112494f7c740a85dcbd4b222
JulesBelveze <jules.belveze@hotmail.fr>
2025-02-11 10:17:38 +0100

    [front/components/spaces/websites] - feature: add advanced settings to website form
     - Implement an AdvancedSettingsSection for custom web crawler headers in website configuration
     - Enhance SpaceWebsiteForm with new fields for extra web crawler settings and validation logic
    
    [front/components/spaces/websites] - feature: introduce SpaceWebsiteForm layout and logic
    
     - Create a new form component, SpaceWebsiteForm, for managing website crawler configurations
     - Add input fields, dropdowns and radio buttons for crawler settings such as entry point URL and indexing settings

---
+++ front/components/spaces/websites/AdvancedSettingsSection.tsx
@@ -0,0 +1,88 @@
+import { Button, Collapsible, Input, Label, XMarkIcon } from "@dust-tt/sparkle";
+import type { WebsiteFormState } from "@dust-tt/types";
+import { WebCrawlerHeaderRedactedValue } from "@dust-tt/types";
+
+export function AdvancedSettingsSection({
+  state,
+  dispatch,
+}: {
+  state: WebsiteFormState;
+  dispatch: any;
+}) {
+  return (
+    <Collapsible>
+      <Collapsible.Button label="Advanced settings" variant="secondary" />
+      <Collapsible.Panel>
+        <div className="flex w-full flex-col gap-3">
+          <Label>Custom Headers</Label>
+          <p>Add custom request headers for the web crawler.</p>
+          <div className="flex flex-col gap-4">
+            {state.headers.map((header, index) => (
+              <div key={index} className="flex gap-2">
+                <div className="flex grow flex-col gap-1 px-1">
+                  <Input
+                    placeholder="Header Name"
+                    value={header.key}
+                    name="headerName"
+                    onChange={(e) => {
+                      const newHeaders = [...state.headers];
+                      newHeaders[index].key = e.target.value;
+                      dispatch({
+                        type: "SET_FIELD",
+                        field: "headers",
+                        value: newHeaders,
+                      });
+                    }}
+                    disabled={header.value === WebCrawlerHeaderRedactedValue}
+                    className="grow"
+                  />
+                  <Input
+                    name="headerValue"
+                    placeholder="Header Value"
+                    value={header.value}
+                    onChange={(e) => {
+                      const newHeaders = [...state.headers];
+                      newHeaders[index].value = e.target.value;
+                      dispatch({
+                        type: "SET_FIELD",
+                        field: "headers",
+                        value: newHeaders,
+                      });
+                    }}
+                    disabled={header.value === WebCrawlerHeaderRedactedValue}
+                    className="flex-1"
+                  />
+                </div>
+                <Button
+                  variant="outline"
+                  icon={XMarkIcon}
+                  onClick={() => {
+                    const newHeaders = state.headers.filter(
+                      (_, i) => i !== index
+                    );
+                    dispatch({
+                      type: "SET_FIELD",
+                      field: "headers",
+                      value: newHeaders,
+                    });
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+          <Button
+            variant="outline"
+            label="Add Header"
+            onClick={() => {
+              dispatch({
+                type: "SET_FIELD",
+                field: "headers",
+                value: [...state.headers, { key: "", value: "" }],
+              });
+            }}
+          />
+        </div>
+      </Collapsible.Panel>
+    </Collapsible>
+  );
+}
