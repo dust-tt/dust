30e44f985179fd0024ebf8387b6a532f527b58ec
JulesBelveze <jules.belveze@hotmail.fr>
2025-02-11 15:25:11 +0100

    [front] - refactor: replace Modal with Sheet component in ActionsScreen
     - Changed the UI modal for adding a new action to use the Sheet component instead of the Modal component
     - Added Sheet specific components like SheetHeader, SheetContent, SheetFooter, and SheetTitle for structuring the Sheet modal
     - Implemented a new save handler onModalSave to manage saving actions within the Sheet modal
     - Adjusted DropdownMenu to disable modal behavior in AddAction component

--- front/components/assistant_builder/ActionsScreen.tsx
+++ front/components/assistant_builder/ActionsScreen.tsx
@@ -18,11 +18,16 @@ import {
   Icon,
   InformationCircleIcon,
   Input,
-  Modal,
   MoreIcon,
   Page,
   PlusIcon,
   Popover,
+  Sheet,
+  SheetContainer,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
   TextArea,
   XMarkIcon,
 } from "@dust-tt/sparkle";
@@ -88,6 +93,7 @@ import {
   isDefaultActionName,
 } from "@app/components/assistant_builder/types";
 import { ACTION_SPECIFICATIONS } from "@app/lib/api/assistant/actions/utils";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 const DATA_SOURCES_ACTION_CATEGORIES = [
   "RETRIEVAL_SEARCH",
@@ -601,71 +607,92 @@ function NewActionModal({
     }, 500);
   };
 
+  const onModalSave = () => {
+    if (
+      newAction &&
+      !titleError &&
+      descriptionValid &&
+      !hasActionError(newAction)
+    ) {
+      newAction.name = newAction.name.trim();
+      newAction.description = newAction.description.trim();
+      onSave(newAction);
+      onCloseLocal();
+    } else {
+      if (titleError) {
+        setShowInvalidActionNameError(titleError);
+      }
+      if (!descriptionValid) {
+        setShowInvalidActionDescError("Description cannot be empty.");
+      }
+      if (newAction) {
+        setShowInvalidActionError(hasActionError(newAction));
+      }
+    }
+  };
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onCloseLocal}
-      hasChanged={true}
-      variant="side-md"
-      title=" "
-      onSave={() => {
-        if (
-          newAction &&
-          !titleError &&
-          descriptionValid &&
-          !hasActionError(newAction)
-        ) {
-          newAction.name = newAction.name.trim();
-          newAction.description = newAction.description.trim();
-          onSave(newAction);
+    <Sheet
+      open={isOpen}
+      onOpenChange={(open) => {
+        if (!open) {
           onCloseLocal();
-        } else {
-          if (titleError) {
-            setShowInvalidActionNameError(titleError);
-          }
-          if (!descriptionValid) {
-            setShowInvalidActionDescError("Description cannot be empty.");
-          }
-          if (newAction) {
-            setShowInvalidActionError(hasActionError(newAction));
-          }
         }
       }}
     >
-      <div className="w-full pt-8">
-        <div className="flex flex-col gap-4">
-          {newAction && (
-            <ActionEditor
-              action={newAction}
-              spacesUsedInActions={spacesUsedInActions}
-              updateAction={({
-                actionName,
-                actionDescription,
-                getNewActionConfig,
-              }) => {
-                setNewAction({
-                  ...newAction,
-                  configuration: getNewActionConfig(
-                    newAction.configuration
-                  ) as any,
-                  description: actionDescription,
-                  name: actionName,
-                });
-                setShowInvalidActionError(null);
-              }}
-              owner={owner}
-              setEdited={setEdited}
-              builderState={builderState}
-              showInvalidActionNameError={showInvalidActionNameError}
-              showInvalidActionDescError={showInvalidActionDescError}
-              showInvalidActionError={showInvalidActionError}
-              setShowInvalidActionNameError={setShowInvalidActionNameError}
-              setShowInvalidActionDescError={setShowInvalidActionDescError}
-            />
-          )}
-        </div>
-      </div>
-    </Modal>
+      <SheetContent size="xl">
+        <VisuallyHidden>
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+          </SheetHeader>
+        </VisuallyHidden>
+
+        <SheetContainer>
+          <div className="w-full pt-8">
+            {newAction && (
+              <ActionEditor
+                action={newAction}
+                spacesUsedInActions={spacesUsedInActions}
+                updateAction={({
+                  actionName,
+                  actionDescription,
+                  getNewActionConfig,
+                }) => {
+                  setNewAction({
+                    ...newAction,
+                    configuration: getNewActionConfig(
+                      newAction.configuration
+                    ) as any,
+                    description: actionDescription,
+                    name: actionName,
+                  });
+                  setShowInvalidActionError(null);
+                }}
+                owner={owner}
+                setEdited={setEdited}
+                builderState={builderState}
+                showInvalidActionNameError={showInvalidActionNameError}
+                showInvalidActionDescError={showInvalidActionDescError}
+                showInvalidActionError={showInvalidActionError}
+                setShowInvalidActionNameError={setShowInvalidActionNameError}
+                setShowInvalidActionDescError={setShowInvalidActionDescError}
+              />
+            )}
+          </div>
+        </SheetContainer>
+        <SheetFooter
+          leftButtonProps={{
+            label: "Cancel",
+            variant: "outline",
+            onClick: onCloseLocal,
+          }}
+          rightButtonProps={{
+            label: "Send",
+            onClick: onModalSave,
+          }}
+        />
+      </SheetContent>
+    </Sheet>
   );
 }
 
@@ -1146,7 +1173,7 @@ interface AddActionProps {
 
 function AddAction({ onAddAction }: AddActionProps) {
   return (
-    <DropdownMenu>
+    <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <Button
           variant="primary"
