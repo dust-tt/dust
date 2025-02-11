48bedbfe882ab3131cf7648e5433ace598800464
Thomas Draier <thomas@dust.tt>
2025-02-11 15:21:35 +0100

    [extension] Add domain in env vars. Remove env from gitignore (#10700)

--- extension/app/src/lib/config.ts
+++ extension/app/src/lib/config.ts
@@ -5,5 +5,7 @@ export const AUTH0_AUDIENCE = `https://${AUTH0_CLIENT_DOMAIN}/api/v2/`;
 export const AUTH0_PROFILE_ROUTE = `https://${AUTH0_CLIENT_DOMAIN}/userinfo`;
 export const DUST_API_AUDIENCE = process.env.DUST_API_AUDIENCE ?? "";
 export const AUTH0_CLAIM_NAMESPACE = process.env.AUTH0_CLAIM_NAMESPACE ?? "";
+export const DUST_US_URL = process.env.DUST_US_URL ?? "";
+export const DUST_EU_URL = process.env.DUST_EU_URL ?? "";
 export const DEFAULT_DUST_API_DOMAIN =
   process.env.DEFAULT_DUST_API_DOMAIN ?? "";
