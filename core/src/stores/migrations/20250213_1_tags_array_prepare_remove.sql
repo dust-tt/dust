6c81f93d7c35ebc1efaf1983d80bc49902107183
Thomas Draier <thomas@dust.tt>
2025-02-13 10:49:25 +0100

    fix

--- core/src/stores/migrations/20250213_1_tags_array_prepare_remove.sql
+++ core/src/stores/migrations/20250213_1_tags_array_prepare_remove.sql
@@ -1,4 +1,7 @@
 ALTER TABLE
-    data_sources_nodes
-ADD COLUMN IF NOT EXISTS
-    tags_array text[] NOT NULL DEFAULT array[]::text[];
+    "data_sources_documents"
+ALTER COLUMN tags_array DROP NOT NULL;
+
+ALTER TABLE
+    "tables"
+ALTER COLUMN tags_array DROP NOT NULL;
