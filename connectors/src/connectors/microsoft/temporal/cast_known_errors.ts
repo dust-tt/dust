8e84bce912d93c101aa7c5380cf44f293f64512b
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-02-13 10:48:19 +0100

    enh(Microsoft) - recast "invalid_grant" errors into `ExternalOauthTokenError` (#10729)
    * cast AADSTS50173 invalid grant errors into ExternalOauthError
    
    * add a todo

--- connectors/src/connectors/microsoft/temporal/cast_known_errors.ts
+++ connectors/src/connectors/microsoft/temporal/cast_known_errors.ts
@@ -4,6 +4,29 @@ import type {
   Next,
 } from "@temporalio/worker";
 
+import { ExternalOAuthTokenError } from "@connectors/lib/error";
+
+interface MicrosoftAuthError extends Error {
+  error: string;
+  error_description: string;
+}
+
+// The SDK does not expose an error class that is rich enough for our use.
+// We'll use this function as a temporary solution for identifying an identified type of error.
+export function isMicrosoftSignInError(
+  err: unknown
+): err is MicrosoftAuthError {
+  return (
+    typeof err === "object" &&
+    err !== null &&
+    "error" in err &&
+    err.error === "invalid_grant" &&
+    "error_description" in err &&
+    typeof err.error_description === "string" &&
+    err.error_description.startsWith("AADSTS50173")
+  );
+}
+
 export class MicrosoftCastKnownErrorsInterceptor
   implements ActivityInboundCallsInterceptor
 {
@@ -14,10 +37,10 @@ export class MicrosoftCastKnownErrorsInterceptor
     try {
       return await next(input);
     } catch (err: unknown) {
-      // no error to intercept and cast as of yet
-      // but it will come very soon (below is to pleas the linter)
-      if (err instanceof Error) {
-        throw err;
+      // See https://learn.microsoft.com/en-us/answers/questions/1339560/sign-in-error-code-50173
+      // TODO(2025-02-12): add an error type for Microsoft client errors and catch them at strategic locations (e.g. API call to instantiate a client)
+      if (isMicrosoftSignInError(err)) {
+        throw new ExternalOAuthTokenError(err);
       }
       throw err;
     }
