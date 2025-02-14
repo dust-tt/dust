8e25306d9c44f0cce3b9426b47cbd2817b44f14d
Sebastien Flory <seb@dust.tt>
2025-02-14 11:05:06 +0100

    Make BigQuery connector available to all (#10791)

--- types/src/shared/feature_flags.ts
+++ types/src/shared/feature_flags.ts
@@ -12,13 +12,11 @@ export const WHITELISTABLE_FEATURES = [
   "deepseek_feature",
   "google_ai_studio_experimental_models_feature",
   "index_private_slack_channel",
-  "conversations_jit_actions",
   "disable_run_logs",
   "labs_trackers",
   "show_debug_tools",
   "labs_github_actions",
   "deepseek_r1_global_agent_feature",
-  "bigquery_feature",
   "tags_filters",
   "salesforce_feature",
 ] as const;
