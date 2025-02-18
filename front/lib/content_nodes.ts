60c4a8367fa068c7d324a03f84a665c820d1c21c
Philippe Rolet <pr@dust.tt>
2025-02-18 12:12:18 +0100

    [Node Core] Front cleaning 2 (#10871)
    * connector permissions modal relies on content nodes
    
    * remove former getVisualForCN

--- front/lib/content_nodes.ts
+++ front/lib/content_nodes.ts
@@ -42,38 +42,7 @@ function getVisualForFileContentNode(node: ContentNode & { type: "file" }) {
   return DocumentIcon;
 }
 
-// TODO(nodes-core) clean this up to always rely on the mime type.
-export function getVisualForContentNode(node: ContentNode, useMimeType = true) {
-  if (useMimeType) {
-    return getVisualForContentNodeBasedOnMimeType(node);
-  } else {
-    return getVisualForContentNodeBasedOnType(node);
-  }
-}
-
-function getVisualForContentNodeBasedOnType(node: ContentNode) {
-  switch (node.type) {
-    case "database":
-      return Square3Stack3DIcon;
-
-    case "file":
-      return getVisualForFileContentNode(
-        node as ContentNode & { type: "file" }
-      );
-
-    case "folder":
-      return FolderIcon;
-
-    default:
-      assertNever(node.type);
-  }
-}
-
-function getVisualForContentNodeBasedOnMimeType(node: ContentNode) {
-  if (!node.mimeType) {
-    // Hotfix to allow using the connNodes param.
-    return getVisualForContentNodeBasedOnType(node);
-  }
+export function getVisualForContentNode(node: ContentNode) {
   if (CHANNEL_MIME_TYPES.includes(node.mimeType)) {
     if (node.providerVisibility === "private") {
       return LockIcon;
