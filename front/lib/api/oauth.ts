a3472ec0ea6e79bb78320202afab9397074860dc
Sebastien Flory <sflory@gmail.com>
2025-02-19 09:11:32 +0100

    Re-enable google drive with less scopes

--- front/lib/api/oauth.ts
+++ front/lib/api/oauth.ts
@@ -81,7 +81,6 @@ const PROVIDER_STRATEGIES: Record<
           : [
               "https://www.googleapis.com/auth/drive.metadata.readonly",
               "https://www.googleapis.com/auth/drive.readonly",
-              "https://www.googleapis.com/auth/drive.labels.readonly",
             ];
       const qs = querystring.stringify({
         response_type: "code",
