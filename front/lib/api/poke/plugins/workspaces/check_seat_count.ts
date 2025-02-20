2f0ac3356105b2e04b11ac3cc7118f660bca8f0d
Aubin <aubin@dust.tt>
2025-02-20 13:06:17 +0100

    remove redundant+inaccurate check

--- front/lib/api/poke/plugins/workspaces/check_seat_count.ts
+++ front/lib/api/poke/plugins/workspaces/check_seat_count.ts
@@ -1,8 +1,7 @@
-import { Err, Ok } from "@dust-tt/types";
+import { Ok } from "@dust-tt/types";
 
 import { createPlugin } from "@app/lib/api/poke/types";
 import { checkSeatCountForWorkspace } from "@app/lib/api/workspace";
-import { isSeatBased } from "@app/lib/plans/plan_codes";
 import { launchUpdateUsageWorkflow } from "@app/temporal/usage_queue/client";
 
 export const checkSeatCountPlugin = createPlugin(
@@ -23,20 +22,16 @@ export const checkSeatCountPlugin = createPlugin(
     const workspace = auth.getNonNullableWorkspace();
 
     if (updateQuantity) {
-      const plan = auth.subscription()?.plan;
-      if (plan && isSeatBased(plan)) {
-        const result = await launchUpdateUsageWorkflow({
-          workspaceId: workspace.sId,
-        });
-        if (result.isErr()) {
-          return result;
-        }
-        return new Ok({
-          display: "text",
-          value: "Usage report workflow launched.",
-        });
+      const result = await launchUpdateUsageWorkflow({
+        workspaceId: workspace.sId,
+      });
+      if (result.isErr()) {
+        return result;
       }
-      return new Err(new Error("Plan does not enforce seat-based billing."));
+      return new Ok({
+        display: "text",
+        value: "Usage report workflow launched.",
+      });
     } else {
       const res = await checkSeatCountForWorkspace(workspace);
       if (res.isErr()) {
