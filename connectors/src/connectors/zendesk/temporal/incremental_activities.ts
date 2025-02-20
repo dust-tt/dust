112ae0c62284eada3e398259ed43758c235c5252
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-02-20 16:00:37 +0100

    fix call to /me with a brandSubdomain instead of a subdomain (#10974)

--- connectors/src/connectors/zendesk/temporal/incremental_activities.ts
+++ connectors/src/connectors/zendesk/temporal/incremental_activities.ts
@@ -109,6 +109,7 @@ export async function syncZendeskArticleUpdateBatchActivity({
   });
 
   const { articles, hasMore, endTime } = await fetchRecentlyUpdatedArticles({
+    subdomain,
     brandSubdomain,
     accessToken,
     startTime,
