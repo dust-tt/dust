d2f8115a59132d49c973cd5d5dc59b484caa7972
Daphné Popin <daphne.popin@gmail.com>
2025-02-21 17:45:34 +0100

    Salesforce temporal workflow

--- connectors/src/start_worker.ts
+++ connectors/src/start_worker.ts
@@ -6,6 +6,7 @@ import { hideBin } from "yargs/helpers";
 import { runBigQueryWorker } from "@connectors/connectors/bigquery/temporal/worker";
 import { runConfluenceWorker } from "@connectors/connectors/confluence/temporal/worker";
 import { runMicrosoftWorker } from "@connectors/connectors/microsoft/temporal/worker";
+import { runSalesforceWorker } from "@connectors/connectors/salesforce/temporal/worker";
 import { runSnowflakeWorker } from "@connectors/connectors/snowflake/temporal/worker";
 import { runWebCrawlerWorker } from "@connectors/connectors/webcrawler/temporal/worker";
 
@@ -38,8 +39,7 @@ const workerFunctions: Record<WorkerType, () => Promise<void>> = {
   snowflake: runSnowflakeWorker,
   zendesk: runZendeskWorkers,
   bigquery: runBigQueryWorker,
-  // TODO(salesforce): implement this
-  salesforce: () => Promise.resolve(),
+  salesforce: runSalesforceWorker,
 };
 
 const ALL_WORKERS = Object.keys(workerFunctions) as WorkerType[];
