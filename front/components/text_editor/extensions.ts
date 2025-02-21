cbf8776680870fe849973e47e7b6827132e24007
Flavien David <flavien@dust.tt>
2025-02-21 14:44:38 +0100

    Add proper support for Markdown in InputBar

--- front/components/text_editor/extensions.ts
+++ front/components/text_editor/extensions.ts
@@ -3,7 +3,22 @@ import Paragraph from "@tiptap/extension-paragraph";
 export const ParagraphExtension = Paragraph.extend({
   addKeyboardShortcuts() {
     return {
-      "Shift-Enter": () => this.editor.commands.splitBlock(),
+      Enter: () => false,
+
+      "Shift-Enter": () => {
+        // Chain is what Tiptap does by default for Enter:
+        // - newlineInCode: insert line breaks in code blocks
+        // - splitListItem: if in a list item, create new list item
+        // - liftEmptyBlock: if empty block (like empty quote), exit it
+        // - splitBlock: fallback -> normal line break
+        return this.editor.commands.first(({ commands }) => [
+          () => commands.newlineInCode(),
+          () => commands.splitListItem("listItem"),
+          () => commands.createParagraphNear(),
+          () => commands.liftEmptyBlock(),
+          () => commands.splitBlock(),
+        ]);
+      },
     };
   },
 });
