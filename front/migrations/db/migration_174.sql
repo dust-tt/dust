aa9cbdf446557c86130e918a7addf883b8f688a4
Thomas Draier <thomas@dust.tt>
2025-02-26 22:35:23 +0100

    update migration

--- front/migrations/db/migration_174.sql
+++ front/migrations/db/migration_174.sql
@@ -1,5 +1,7 @@
 -- Migration created on Feb 18, 2025
-ALTER TABLE "public"."conversations" ADD COLUMN "currentThreadVersion" INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE "public"."messages" ADD COLUMN "nextVersionMessageId" BIGINT;
-ALTER TABLE "public"."messages" ADD COLUMN "previousVersionMessageId" BIGINT;
+ALTER TABLE "public"."conversations" ADD COLUMN "lastThreadVersion" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "public"."messages" ADD COLUMN "nextThreadVersion" INTEGER;
+ALTER TABLE "public"."messages" ADD COLUMN "previousThreadVersion" INTEGER;
 ALTER TABLE "public"."messages" ADD COLUMN "threadVersions" INTEGER[] DEFAULT ARRAY[0]::INTEGER[];
+CREATE UNIQUE INDEX CONCURRENTLY "messages_conversation_id_rank_version_parent_id" ON "messages" ("conversationId", "rank", "version", "parentId");
+DROP INDEX IF EXISTS "messages_conversation_id_rank_version";
