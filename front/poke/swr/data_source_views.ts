d655d58305fc7db83e88fbbfc6be8417fb734477
Aubin <aubin@dust.tt>
2025-02-26 15:24:32 +0100

    remove unused hook `usePokeDataSourceViewContentNodesWithInfiniteScroll`

--- front/poke/swr/data_source_views.ts
+++ front/poke/swr/data_source_views.ts
@@ -7,13 +7,7 @@ import { useMemo } from "react";
 import type { Fetcher, KeyedMutator } from "swr";
 
 import type { CursorPaginationParams } from "@app/lib/api/pagination";
-import {
-  appendPaginationParams,
-  fetcher,
-  fetcherWithBody,
-  useSWRInfiniteWithDefaults,
-  useSWRWithDefaults,
-} from "@app/lib/swr/swr";
+import { fetcher, fetcherWithBody, useSWRWithDefaults } from "@app/lib/swr/swr";
 import type { PokeListDataSourceViews } from "@app/pages/api/poke/workspaces/[wId]/data_source_views";
 import type { PokeGetDataSourceViewContentNodes } from "@app/pages/api/poke/workspaces/[wId]/spaces/[spaceId]/data_source_views/[dsvId]/content-nodes";
 import type { PokeConditionalFetchProps } from "@app/poke/swr/types";
@@ -121,94 +115,5 @@ export function usePokeDataSourceViewContentNodes({
     totalNodesCount: data ? data.total : 0,
     totalNodesCountIsAccurate: data ? data.totalIsAccurate : true,
     nextPageCursor: data?.nextPageCursor || null,
-  };
-}
-
-interface DataSourceViewContentNodesWithInfiniteScrollProps {
-  dataSourceView?: DataSourceViewType;
-  internalIds?: string[];
-  owner: LightWorkspaceType;
-  pageSize?: number;
-  parentId?: string;
-  viewType?: ContentNodesViewType;
-}
-
-export function usePokeDataSourceViewContentNodesWithInfiniteScroll({
-  owner,
-  dataSourceView,
-  internalIds,
-  parentId,
-  pageSize = 50,
-  viewType,
-}: DataSourceViewContentNodesWithInfiniteScrollProps): {
-  isNodesError: boolean;
-  isNodesLoading: boolean;
-  isNodesValidating: boolean;
-  nodes: PokeGetDataSourceViewContentNodes["nodes"];
-  totalNodesCount: number;
-  hasMore: boolean;
-  nextPage: () => Promise<void>;
-} {
-  const url =
-    dataSourceView && viewType
-      ? `/api/poke/workspaces/${owner.sId}/spaces/${dataSourceView.spaceId}/data_source_views/${dataSourceView.sId}/content-nodes`
-      : null;
-
-  const body = {
-    internalIds,
-    parentId,
-    viewType,
-  };
-
-  const fetcher: Fetcher<
-    PokeGetDataSourceViewContentNodes,
-    [string, object, string]
-  > = fetcherWithBody;
-
-  const { data, error, setSize, size, isValidating } =
-    useSWRInfiniteWithDefaults(
-      (index) => {
-        if (!url) {
-          // No URL, return an empty array to skip the fetch
-          return null;
-        }
-
-        // Append the pagination params to the URL
-        const params = new URLSearchParams();
-        appendPaginationParams(params, {
-          pageIndex: index,
-          pageSize,
-        });
-
-        return JSON.stringify([url + "?" + params.toString(), body]);
-      },
-      async (fetchKey) => {
-        if (!fetchKey) {
-          return undefined;
-        }
-
-        // Get the URL and body from the fetchKey
-        const params = JSON.parse(fetchKey);
-
-        return fetcher(params);
-      },
-      {
-        revalidateFirstPage: false,
-      }
-    );
-
-  return {
-    isNodesError: !!error,
-    isNodesLoading: !error && !data,
-    isNodesValidating: isValidating,
-    nodes: useMemo(
-      () => (data ? data.flatMap((d) => (d ? d.nodes : [])) : []),
-      [data]
-    ),
-    totalNodesCount: data?.[0] ? data[0].total : 0,
-    hasMore: size * pageSize < (data?.[0] ? data[0].total : 0),
-    nextPage: async () => {
-      await setSize((size) => size + 1);
-    },
   };
 }
