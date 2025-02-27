f9b18a31658353842468da1e473eaf8ee9671fa1
Alexandre Pinot <32997243+pinotalexandre@users.noreply.github.com>
2025-02-27 17:04:36 +0100

    fixing background not displayed in button (#11107)

--- sparkle/src/components/Counter.tsx
+++ sparkle/src/components/Counter.tsx
@@ -54,6 +54,34 @@ const counterVariants = cva(
         variant: ["ghost", "ghost-secondary"],
         className: "s-text-primary dark:s-text-primary-night",
       },
+      {
+        isInButton: true,
+        variant: "primary",
+        className:
+          "s-bg-primary-600 dark:s-bg-primary-400 s-text-white dark:s-text-primary-900 s-font-semibold",
+      },
+      {
+        isInButton: true,
+        variant: "highlight",
+        className: "s-bg-highlight-400 s-text-white s-font-semibold",
+      },
+      {
+        isInButton: true,
+        variant: "warning",
+        className: "s-bg-warning-400 s-text-white s-font-semibold",
+      },
+      {
+        isInButton: true,
+        variant: "outline",
+        className:
+          "s-bg-slate-200 dark:s-bg-slate-700 s-text-primary-700 dark:s-text-primary-300 s-font-semibold",
+      },
+      {
+        isInButton: true,
+        variant: ["ghost", "ghost-secondary"],
+        className:
+          "s-bg-slate-200 dark:s-bg-slate-700 s-text-primary-700 dark:s-text-primary-300 s-font-semibold",
+      },
     ],
     defaultVariants: {
       size: "sm",
