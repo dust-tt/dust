91943d200996945b614fd2bf22c4d537bfa59ba0
Flavien David <flavien@dust.tt>
2025-02-28 13:30:55 +0100

    Fix relocations

--- core/bin/core_api.rs
+++ core/bin/core_api.rs
@@ -1484,6 +1484,7 @@ async fn data_sources_retrieve(
                             "data_source_id": ds.data_source_id(),
                             "data_source_internal_id": ds.internal_id(),
                             "config": ds.config(),
+                            "name": ds.name(),
                         },
                     })),
                 }),
