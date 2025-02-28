0b4937bde81fec3d5113d72d1e0ee8f470e8b3ff
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-02-28 20:02:52 +0100

    First shot

--- sparkle/src/stories/ColorPalette.stories.tsx
+++ sparkle/src/stories/ColorPalette.stories.tsx
@@ -16,7 +16,6 @@ const semanticColorFamilies = [
   "highlight",
   "success",
   "warning",
-  "info",
 ] as const;
 
 // Shades to display for each color
@@ -44,7 +43,7 @@ const ColorSwatch = ({
   <div className="s-flex s-flex-col s-gap-2">
     <div
       className={cn(
-        "s-h-16 s-w-24 s-rounded-lg s-border s-border-border dark:s-border-border-night",
+        "s-h-16 s-w-24 s-rounded-lg s-border s-border-border",
         colorClass
       )}
       style={{
@@ -97,73 +96,6 @@ export const SemanticColorPalette = () => {
                 <ColorSwatch
                   colorClass={`s-bg-${family}-${shade} dark:s-bg-${family}-${shade}-night`}
                   label={`${family}-${shade}`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const brandColorFamilies = [
-  {
-    name: "green",
-    shades: [
-      { name: "hunter-green", shade: 600 },
-      { name: "tea-green", shade: 200 },
-      { name: "support-green", shade: 50 },
-    ],
-  },
-  {
-    name: "blue",
-    shades: [
-      { name: "electric-blue", shade: 500 },
-      { name: "sky-blue", shade: 200 },
-      { name: "support-blue", shade: 50 },
-    ],
-  },
-  {
-    name: "rose",
-    shades: [
-      { name: "red-rose", shade: 500 },
-      { name: "pink-rose", shade: 200 },
-      { name: "support-rose", shade: 50 },
-    ],
-  },
-  {
-    name: "golden",
-    shades: [
-      { name: "orange-golden", shade: 500 },
-      { name: "sunshine-golden", shade: 200 },
-      { name: "support-golden", shade: 50 },
-    ],
-  },
-  {
-    name: "gray",
-    shades: [
-      { name: "dark-gray", shade: 700 },
-      { name: "light-gray", shade: 200 },
-      { name: "support-gray", shade: 50 },
-    ],
-  },
-];
-
-export const BrandColorPalette = () => {
-  return (
-    <div className="s-flex s-flex-col s-gap-8">
-      {brandColorFamilies.map((family) => (
-        <div key={family.name} className="s-flex s-flex-col s-gap-4">
-          <h3 className="s-text-lg s-font-semibold s-capitalize">
-            {family.name}
-          </h3>
-          <div className="s-flex s-flex-wrap s-gap-4">
-            {family.shades.map((shade) => (
-              <div key={shade.name}>
-                <ColorSwatch
-                  colorClass={`s-bg-${family.name}-${shade.shade} dark:s-bg-${family.name}-${shade.shade}-night`}
-                  label={shade.name}
                 />
               </div>
             ))}
