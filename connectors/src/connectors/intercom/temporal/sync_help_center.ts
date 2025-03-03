65a5e743212bcfef94314d425344d68e42f2f792
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-03-03 15:09:37 +0100

    fix(Intercom) - Prevent articles with empty content from being skipped (#11167)
    * prevent Intercom articles with empty content from being skipped
    
    * update comment and add fallback article content
    
    * remove redundant field in the logger

--- connectors/src/connectors/intercom/temporal/sync_help_center.ts
+++ connectors/src/connectors/intercom/temporal/sync_help_center.ts
@@ -378,70 +378,72 @@ export async function upsertArticle({
       ? ` - ${parentCollection.description}`
       : "";
 
-  const articleContentInMarkdown =
+  let articleContentInMarkdown =
     typeof article.body === "string"
       ? turndownService.turndown(article.body)
       : "";
 
+  if (!articleContentInMarkdown) {
+    logger.warn(
+      { ...loggerArgs, articleId: article.id },
+      "[Intercom] Article has no content."
+    );
+    // We still sync articles that have no content to have the node appear.
+    articleContentInMarkdown = "Article without content.";
+  }
+
   // append the collection description at the beginning of the article
   const markdown = `CATEGORY: ${categoryContent}\n\n${articleContentInMarkdown}`;
 
-  if (articleContentInMarkdown) {
-    const createdAtDate = new Date(article.created_at * 1000);
-    const updatedAtDate = new Date(article.updated_at * 1000);
+  const createdAtDate = new Date(article.created_at * 1000);
+  const updatedAtDate = new Date(article.updated_at * 1000);
 
-    const renderedMarkdown = await renderMarkdownSection(
-      dataSourceConfig,
-      markdown
-    );
-    const renderedPage = await renderDocumentTitleAndContent({
-      dataSourceConfig,
-      title: article.title,
-      content: renderedMarkdown,
-      createdAt: createdAtDate,
-      updatedAt: updatedAtDate,
-    });
+  const renderedMarkdown = await renderMarkdownSection(
+    dataSourceConfig,
+    markdown
+  );
+  const renderedPage = await renderDocumentTitleAndContent({
+    dataSourceConfig,
+    title: article.title,
+    content: renderedMarkdown,
+    createdAt: createdAtDate,
+    updatedAt: updatedAtDate,
+  });
 
-    const documentId = getHelpCenterArticleInternalId(connectorId, article.id);
+  const documentId = getHelpCenterArticleInternalId(connectorId, article.id);
 
-    const parents = await getParentIdsForArticle({
-      documentId,
-      connectorId,
-      parentCollectionId,
-      helpCenterId,
-    });
+  const parents = await getParentIdsForArticle({
+    documentId,
+    connectorId,
+    parentCollectionId,
+    helpCenterId,
+  });
 
-    await upsertDataSourceDocument({
-      dataSourceConfig,
-      documentId,
-      documentContent: renderedPage,
-      documentUrl: articleUrl,
-      timestampMs: updatedAtDate.getTime(),
-      tags: [
-        `title:${article.title}`,
-        `createdAt:${createdAtDate.getTime()}`,
-        `updatedAt:${updatedAtDate.getTime()}`,
-      ],
-      parents,
-      parentId: parents[1],
-      loggerArgs: {
-        ...loggerArgs,
-        articleId: article.id,
-      },
-      upsertContext: {
-        sync_type: "batch",
-      },
-      title: article.title,
-      mimeType: MIME_TYPES.INTERCOM.ARTICLE,
-      async: true,
-    });
-    await articleOnDb.update({
-      lastUpsertedTs: new Date(currentSyncMs),
-    });
-  } else {
-    logger.warn(
-      { ...loggerArgs, connectorId, articleId: article.id },
-      "[Intercom] Article has no content. Skipping sync."
-    );
-  }
+  await upsertDataSourceDocument({
+    dataSourceConfig,
+    documentId,
+    documentContent: renderedPage,
+    documentUrl: articleUrl,
+    timestampMs: updatedAtDate.getTime(),
+    tags: [
+      `title:${article.title}`,
+      `createdAt:${createdAtDate.getTime()}`,
+      `updatedAt:${updatedAtDate.getTime()}`,
+    ],
+    parents,
+    parentId: parents[1],
+    loggerArgs: {
+      ...loggerArgs,
+      articleId: article.id,
+    },
+    upsertContext: {
+      sync_type: "batch",
+    },
+    title: article.title,
+    mimeType: MIME_TYPES.INTERCOM.ARTICLE,
+    async: true,
+  });
+  await articleOnDb.update({
+    lastUpsertedTs: new Date(currentSyncMs),
+  });
 }
