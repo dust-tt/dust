e322a0ad839b6d8b19d9ab705a14691641e41144
Aubin <aubin@dust.tt>
2025-03-04 22:03:35 +0100

    add resync signal

--- connectors/src/connectors/gong/temporal/workflows.ts
+++ connectors/src/connectors/gong/temporal/workflows.ts
@@ -1,7 +1,9 @@
 import type { ModelId } from "@dust-tt/types";
-import { setHandler } from "@temporalio/workflow";
+import { defineSignal, setHandler } from "@temporalio/workflow";
 
-import { resyncSignal } from "@connectors/connectors/salesforce/temporal/signals";
+export interface ResyncSignal {}
+
+export const resyncSignal = defineSignal<ResyncSignal[]>("resyncSignal");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function gongSyncWorkflow(_: { connectorId: ModelId }) {
