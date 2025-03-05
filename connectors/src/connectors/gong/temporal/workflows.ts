48b2b0bd2007977d258b2e87e7b987307684a929
Aubin <aubin@dust.tt>
2025-03-05 14:01:23 +0100

    remove workflow signal

--- connectors/src/connectors/gong/temporal/workflows.ts
+++ connectors/src/connectors/gong/temporal/workflows.ts
@@ -1,20 +1,4 @@
 import type { ModelId } from "@dust-tt/types";
-import { defineSignal, setHandler } from "@temporalio/workflow";
-
-export interface ResyncSignal {}
-
-export const resyncSignal = defineSignal<ResyncSignal[]>("resyncSignal");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function gongSyncWorkflow(_: { connectorId: ModelId }) {
-  let signaled = false;
-
-  setHandler(resyncSignal, () => {
-    signaled = true;
-  });
-
-  do {
-    signaled = false;
-    // TODO(2025-03-04) - Implement this.
-  } while (signaled);
-}
+export async function gongSyncWorkflow(_: { connectorId: ModelId }) {}
