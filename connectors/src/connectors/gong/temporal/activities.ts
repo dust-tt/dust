a2e463ecd358c8dffc1b097ae80849925baaae8a
Aubin <aubin@dust.tt>
2025-03-05 13:37:13 +0100

    remove unnecessary activities

--- connectors/src/connectors/gong/temporal/activities.ts
+++ connectors/src/connectors/gong/temporal/activities.ts
@@ -5,13 +5,7 @@ import {
   GongClient,
 } from "@connectors/connectors/gong/lib/gong_api";
 import { syncStarted, syncSucceeded } from "@connectors/lib/sync_status";
-import mainLogger from "@connectors/logger/logger";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
-import { GongConfigurationResource } from "@connectors/resources/gong_resources";
-
-const logger = mainLogger.child({
-  provider: "gong",
-});
 
 async function getGongClient(connectorId: ModelId) {
   const connector = await ConnectorResource.fetchById(connectorId);
@@ -48,36 +42,6 @@ export async function gongSaveSyncSuccessActivity(connectorId: ModelId) {
   if (res.isErr()) {
     throw res.error;
   }
-}
-
-export async function gongLoadTimestampCursorActivity(
-  connectorId: ModelId
-): Promise<{ cursor: Date | null }> {
-  const connector = await ConnectorResource.fetchById(connectorId);
-  if (!connector) {
-    throw new Error("[Gong] Connector not found.");
-  }
-  const configuration =
-    await GongConfigurationResource.fetchByConnector(connector);
-
-  return { cursor: configuration?.timestampCursor ?? null };
-}
-
-export async function gongSaveTimestampCursorActivity(
-  connectorId: ModelId,
-  currentSyncDateMs: number
-) {
-  const connector = await ConnectorResource.fetchById(connectorId);
-  if (!connector) {
-    throw new Error("[Gong] Connector not found.");
-  }
-
-  const configuration =
-    await GongConfigurationResource.fetchByConnector(connector);
-  if (!configuration) {
-    throw new Error("[Gong] Configuration not found.");
-  }
-  await configuration.update({ timestampCursor: currentSyncDateMs });
 }
 
 export async function getGongTranscriptsActivity(
