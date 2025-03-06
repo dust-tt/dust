2432d128bef62cba2956d9fe6dc32638e2f1c02e
Aubin <aubin@dust.tt>
2025-03-06 16:39:51 +0100

    fix

--- connectors/migrations/db/migration_62.sql
+++ connectors/migrations/db/migration_62.sql
@@ -1,3 +1,2 @@
 ALTER TABLE "public"."gong_configurations" ADD COLUMN "retentionPeriodDays" INTEGER;
-ALTER TABLE "gong_configurations" ALTER COLUMN "lastSyncTimestamp" SET NOT NULL;
 ALTER TABLE "public"."gong_transcripts" ADD COLUMN "callDate" BIGINT;
