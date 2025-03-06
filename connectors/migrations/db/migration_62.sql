252afa3e7fa5aca1448442618d51b035ace7c4b5
Aubin <aubin@dust.tt>
2025-03-06 14:59:55 +0100

    merge both sql migration scripts

--- connectors/migrations/db/migration_62.sql
+++ connectors/migrations/db/migration_62.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "public"."gong_configurations" ADD COLUMN "retentionPeriodDays" INTEGER;
 UPDATE "public"."gong_configurations" SET "retentionPeriodDays" = 180;
 ALTER TABLE "public"."gong_configurations" ALTER COLUMN "retentionPeriodDays" SET NOT NULL;
+ALTER TABLE "gong_configurations" ALTER COLUMN "lastSyncTimestamp" SET NOT NULL;
--- connectors/migrations/db/migration_63.sql
+++
@@ -1,2 +0,0 @@
--- Migration created on Mar 06, 2025
-ALTER TABLE "gong_configurations" ALTER COLUMN "lastSyncTimestamp" SET NOT NULL;
