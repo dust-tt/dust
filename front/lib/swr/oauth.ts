7d1dd3fdf5923813ee38ab09bbdffa7db06c6744
Thomas Draier <thomas@dust.tt>
2025-03-13 18:38:08 +0100

    [front] Use api route for finalize (#11356)
    * Use api route for finalize
    
    * improved flow
    
    * improved flow
    
    * lint
    
    * comments
    
    * safety
    
    * remove casts
    
    * Move fetch to hook

---
+++ front/lib/swr/oauth.ts
@@ -0,0 +1,21 @@
+import type { OAuthProvider } from "@dust-tt/types";
+
+export const useFinalize = () => {
+  const doFinalize = async (
+    provider: OAuthProvider,
+    queryParams: Record<string, string | string[] | undefined>
+  ) => {
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (Array.isArray(value)) {
+        value.forEach((v) => params.append(key, v));
+      } else if (value !== undefined) {
+        params.append(key, value);
+      }
+    }
+
+    return fetch(`/api/oauth/${provider}/finalize?${params.toString()}`);
+  };
+
+  return doFinalize;
+};
