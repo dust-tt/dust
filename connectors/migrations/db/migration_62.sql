ed3003bab7a3e8be64ce14f54694b9b8adde7c27
Aubin <aubin@dust.tt>
2025-03-14 14:23:04 +0100

    add the column lastGarbageCollectTimestamp to the db migration

--- connectors/migrations/db/migration_62.sql
+++ connectors/migrations/db/migration_62.sql
@@ -1,2 +1,4 @@
+-- Migration created on Mar 7, 2025
+ALTER TABLE "public"."gong_configurations" ADD COLUMN "lastGarbageCollectTimestamp" BIGINT;
 ALTER TABLE "public"."gong_configurations" ADD COLUMN "retentionPeriodDays" INTEGER;
 ALTER TABLE "public"."gong_transcripts" ADD COLUMN "callDate" BIGINT;
