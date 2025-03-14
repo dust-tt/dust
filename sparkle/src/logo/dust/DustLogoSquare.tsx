b7d1c6224f4c31f5cfdcc65f2eb17c1905a1824e
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-03-14 14:44:02 +0100

    New Branding - Sparkle (#11362)
    * First shot
    
    * adjustments
    
    * more tweaks and adjustments
    
    * more tweaks
    
    * resetting package lock
    
    * fixing eslint
    
    * Prettier fix
    
    * sparkle bump
    
    * adjust colors
    
    * adjust colors
    
    * new sparkle
    
    * more tweaking
    
    * more tweaking
    
    * reset package lock from main
    
    * tweak
    
    * updating sparkle
    
    * tweaking
    
    * Removing usage of brand
    
    * Tweaks
    
    * New sparkle
    
    * Website initial changes
    
    * Updating Logo Assets
    
    * Updating logos
    
    * Updating colors
    
    * Updating assets
    
    * Bumping Sparkle
    
    * fix
    
    * Spinner and CollapseBar
    
    * Updated Sparkle
    
    * Rebasing
    
    * Updating Sparkle
    
    * Fixing linting
    
    * More fixes
    
    * Story book minor change
    
    * Sparkle bump
    
    * Few more tweaks
    
    * new caroussel
    
    * ScrollingHeader color fix + carousel nit
    
    * visual nit
    
    * deleting gradient
    
    * color nit
    
    * Bumping Sparkle
    
    * Bumping Sparkle
    
    * Bumping Sparkle
    
    * Reset front/ directory to main
    
    * rebase
    
    * Removing front changes
    
    * Removing extension changes
    
    * Version
    
    * cva and color fix
    
    ---------
    
    Co-authored-by: pinotalexandre <hialexpinot@gmail.com>

--- sparkle/src/logo/dust/DustLogoSquare.tsx
+++ sparkle/src/logo/dust/DustLogoSquare.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from "react";
 import * as React from "react";
-const SvgLogoSquareColor = (props: SVGProps<SVGSVGElement>) => (
+const SvgDustLogoSquare = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -9,7 +9,7 @@ const SvgLogoSquareColor = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 48 48"
     {...props}
   >
-    <path fill="#FCD34D" d="M36 36H24v12h12V36Z" />
+    <path fill="#FFAA0D" d="M36 24H24v24h12V24Z" />
     <path
       fill="#3B82F6"
       fillRule="evenodd"
@@ -17,21 +17,21 @@ const SvgLogoSquareColor = (props: SVGProps<SVGSVGElement>) => (
       clipRule="evenodd"
     />
     <path
-      fill="#7DD3FC"
+      fill="#9FDBFF"
       fillRule="evenodd"
       d="M0 48V36h12a6 6 0 0 1 0 12H0Z"
       clipRule="evenodd"
     />
     <path
-      fill="#6EE7B7"
+      fill="#E2F78C"
       d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Z"
     />
     <path
-      fill="#F9A8D4"
+      fill="#FFC3DF"
       d="M36 24c6.627 0 12-5.373 12-12S42.627 0 36 0 24 5.373 24 12s5.373 12 12 12Z"
     />
-    <path fill="#10B981" d="M12 0H0v24h12V0Z" />
-    <path fill="#F87171" d="M48 0H24v12h24V0Z" />
+    <path fill="#418B5C" d="M12 0H0v24h12V0Z" />
+    <path fill="#E14322" d="M48 0H24v12h24V0Z" />
   </svg>
 );
-export default SvgLogoSquareColor;
+export default SvgDustLogoSquare;
--- sparkle/src/logo/dust/DustLogoSquareLayer2.tsx
+++ sparkle/src/logo/dust/DustLogoSquareLayer2.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from "react";
 import * as React from "react";
-const SvgLogoSquareColorLayer2 = (props: SVGProps<SVGSVGElement>) => (
+const SvgDustLogoSquareLayer2 = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -15,8 +15,8 @@ const SvgLogoSquareColorLayer2 = (props: SVGProps<SVGSVGElement>) => (
       d="M12 36a6 6 0 0 1 0-12h36v12H12Z"
       clipRule="evenodd"
     />
-    <path fill="#10B981" d="M12 0H0v24h12V0Z" />
-    <path fill="#F87171" d="M48 0H24v12h24V0Z" />
+    <path fill="#418B5C" d="M12 0H0v24h12V0Z" />
+    <path fill="#E14322" d="M48 0H24v12h24V0Z" />
   </svg>
 );
-export default SvgLogoSquareColorLayer2;
+export default SvgDustLogoSquareLayer2;
