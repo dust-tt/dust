9562d4511042b31e03c44d12ad76ec8166ba57c2
Jules Belveze <32683010+JulesBelveze@users.noreply.github.com>
2025-03-17 16:50:38 +0100

    [sparkle] - chore(Chip): add "white" variant (#11414)
    * [sparkle] - feature: add white variant to AnimatedText and Chip components
    
     - Included 'white' in the list of color variants for both AnimatedText and Chip components
     - Added corresponding styles for the 'white' variant to handle light and dark themes
    
    * [sparkle] - feature: bump package version to 0.2.441
    
     - Update `@dust-tt/sparkle` package version to include the latest changes and improvements
     - Ensure consistency in package locks and package definition with the new version number

--- sparkle/src/components/AnimatedText.tsx
+++ sparkle/src/components/AnimatedText.tsx
@@ -1,6 +1,5 @@
 import { cva, VariantProps } from "class-variance-authority";
-import { ReactNode } from "react";
-import React from "react";
+import React, { ReactNode } from "react";
 
 import { cn } from "@sparkle/lib/utils";
 
@@ -15,6 +14,7 @@ const ANIMATED_TEXT_VARIANTS = [
   "sky",
   "pink",
   "red",
+  "white",
 ] as const;
 
 type AnimatedTextVariantType = (typeof ANIMATED_TEXT_VARIANTS)[number];
@@ -35,6 +35,10 @@ const animatedVariants: Record<AnimatedTextVariantType, string> = {
   amber: cn(
     "s-from-amber-800 s-via-amber-950 s-via-50% s-to-amber-800",
     "dark:s-from-amber-800-night dark:s-via-amber-950-night dark:s-via-50% dark:s-to-amber-800-night"
+  ),
+  white: cn(
+    "s-from-slate-600 s-via-slate-950 s-via-50% s-to-slate-600",
+    "dark:s-from-slate-600-night dark:s-via-slate-950-night dark:s-via-50% dark:s-to-slate-600-night"
   ),
   slate: cn(
     "s-from-slate-600 s-via-slate-950 s-via-50% s-to-slate-600",
@@ -80,6 +84,7 @@ const animatedTextVariants: Record<AnimatedTextVariantType, string> = {
   emerald: "s-text-emerald-800 dark:s-text-emerald-800-night",
   amber: "s-text-amber-800 dark:s-text-amber-800-night",
   slate: "s-text-muted-foreground dark:s-text-muted-foreground-night",
+  white: "s-text-muted-foreground dark:s-text-muted-foreground-night",
   purple: "s-text-purple-800 dark:s-text-purple-800-night",
   warning: "s-text-warning-800 dark:s-text-warning-800-night",
   sky: "s-text-sky-800 dark:s-text-sky-800-night",
