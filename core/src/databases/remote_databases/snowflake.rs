6507d71de70a891ddb9c3a7f6f3a21f64f784cf9
Thomas Draier <thomas@dust.tt>
2025-03-17 17:28:42 +0100

    missing files

--- core/src/databases/remote_databases/snowflake.rs
+++ core/src/databases/remote_databases/snowflake.rs
@@ -68,7 +68,7 @@ impl TryFrom<SnowflakeSchemaColumn> for TableSchemaColumn {
             name: col.name,
             value_type: col_type,
             possible_values: None,
-            filterable: true,
+            filterable: None,
         })
     }
 }
