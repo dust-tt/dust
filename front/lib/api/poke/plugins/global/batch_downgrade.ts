94b6912446357db8d43720d9c874a4f3db44954f
Frank Aloia <frank.e.aloia@gmail.com>
2025-03-17 13:11:09 -0400

    Merge main with subscription refactor changes

--- front/lib/api/poke/plugins/global/batch_downgrade.ts
+++ front/lib/api/poke/plugins/global/batch_downgrade.ts
@@ -2,7 +2,7 @@ import { Op } from "sequelize";
 
 import { createPlugin } from "@app/lib/api/poke/types";
 import { Workspace } from "@app/lib/models/workspace";
-import { internalSubscribeWorkspaceToFreeNoPlan } from "@app/lib/plans/subscription";
+import { SubscriptionResource } from "@app/lib/resources/subscription_resource";
 import { launchScheduleWorkspaceScrubWorkflow } from "@app/temporal/scrub_workspace/client";
 import { Err, Ok } from "@app/types";
 
@@ -50,7 +50,7 @@ export const batchDowngradePlugin = createPlugin({
     }
 
     for (const workspace of workspaces) {
-      await internalSubscribeWorkspaceToFreeNoPlan({
+      await SubscriptionResource.internalSubscribeWorkspaceToFreeNoPlan({
         workspaceId: workspace.sId,
       });
 
