139505fb3137a4115c527ef151a0957b0c215b0b
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-03-18 12:15:26 +0100

    Cleaning Status Banner

--- sparkle/src/stories/Playground.stories.tsx
+++ sparkle/src/stories/Playground.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { Button, FlexSplitButton } from "@sparkle/components";
-import { ArrowUpIcon, ChevronDownIcon } from "@sparkle/icons";
+import { cn } from "@sparkle/lib";
 
 export default {
   title: "Playground/Demo",
@@ -9,41 +8,17 @@ export default {
 
 export const Demo = () => {
   return (
-    <div className="s-flex s-h-full s-w-full s-flex-col s-gap-2">
-      <div className="s-flex s-gap-3">
-        <FlexSplitButton
-          label="Send"
-          variant={"highlight"}
-          icon={ArrowUpIcon}
-          splitAction={
-            <Button size="mini" variant={"highlight"} icon={ChevronDownIcon} />
-          }
-        />
-        <FlexSplitButton
-          label="Send"
-          variant={"primary"}
-          icon={ArrowUpIcon}
-          splitAction={
-            <Button size="mini" variant={"primary"} icon={ChevronDownIcon} />
-          }
-        />
-        <FlexSplitButton
-          label="Send"
-          variant={"outline"}
-          icon={ArrowUpIcon}
-          splitAction={
-            <Button size="mini" variant={"outline"} icon={ChevronDownIcon} />
-          }
-        />
-        <FlexSplitButton
-          label="Send"
-          variant={"ghost"}
-          icon={ArrowUpIcon}
-          splitAction={
-            <Button size="mini" variant={"ghost"} icon={ChevronDownIcon} />
-          }
-        />
-      </div>
+    <div
+      className={cn(
+        "s-space-y-2 s-border-y s-px-3 s-py-3 s-text-xs",
+        "s-border-info-200 dark:s-border-info-200-night",
+        "s-bg-info-100 dark:s-bg-info-100-night",
+        "s-text-info-900 dark:s-text-info-900-night"
+      )}
+    >
+      <div className="s-font-bold">Title</div>
+      <div className="s-font-normal">Description</div>
+      <div>Check our page for updates.</div>
     </div>
   );
 };
