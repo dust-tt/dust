94f54df7990f522c60c386976f8d5e5f6d6405a9
Alban Dumouilla <alban@dust.tt>
2025-03-18 11:31:00 +0100

    Fix migrations conflicts

--- front/migrations/db/migration_182.sql
+++ front/migrations/db/migration_182.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Migration created on Mar 12, 2025
 ALTER TABLE "public"."labs_transcripts_configurations" ADD COLUMN "useConnectorConnection" BOOLEAN NOT NULL DEFAULT false;
 
@@ -9,12 +8,3 @@ WHERE "provider" = 'gong';
 UPDATE "public"."labs_transcripts_configurations" 
 SET "isDefaultWorkspaceConfiguration" = true 
 WHERE "isDefaultFullStorage" = true;
-=======
-DROP TABLE agent_github_configurations;
-
-DROP TABLE agent_github_create_issue_actions;
-
-DROP TABLE agent_github_get_pull_request_actions;
-
-DROP TABLE platform_actions_configurations;
->>>>>>> main
