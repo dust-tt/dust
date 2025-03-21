57c1e964b74730037b33ac595b3e95b30777aeb9
Flavien David <flavien.david74@gmail.com>
2025-03-21 17:30:13 +0100

    Fix Firebase Configuration for Front Extension (#11519)
    * Fix Firebase Configuration for Front Extension
    
    * Improve Front Extension Layout

--- extension/platforms/front/components/FrontCaptureActions.tsx
+++ extension/platforms/front/components/FrontCaptureActions.tsx
@@ -7,22 +7,40 @@ export function FrontCaptureActions({
   isLoading,
 }: CaptureActionsProps) {
   return (
-    <div>
-      <Button
-        icon={ChatBubbleBottomCenterPlusIcon}
-        label="Include conversation"
-        tooltip="Add conversation content"
-        variant="outline"
-        className={isBlinking ? "animate-[bgblink_200ms_3]" : ""}
-        size="sm"
-        onClick={async () => {
-          await fileUploaderService.uploadContentTab({
-            includeContent: true,
-            includeCapture: false,
-          });
-        }}
-        disabled={isLoading}
-      />
-    </div>
+    <>
+      <div className="block sm:hidden">
+        <Button
+          icon={ChatBubbleBottomCenterPlusIcon}
+          tooltip="Add conversation content"
+          variant="outline"
+          className={isBlinking ? "animate-[bgblink_200ms_3]" : ""}
+          size="sm"
+          onClick={async () => {
+            await fileUploaderService.uploadContentTab({
+              includeContent: true,
+              includeCapture: false,
+            });
+          }}
+          disabled={isLoading}
+        />
+      </div>
+      <div className="hidden sm:block">
+        <Button
+          icon={ChatBubbleBottomCenterPlusIcon}
+          label="Include conversation"
+          tooltip="Add conversation content"
+          variant="outline"
+          size="sm"
+          className={isBlinking ? "animate-[bgblink_200ms_3]" : ""}
+          onClick={async () => {
+            await fileUploaderService.uploadContentTab({
+              includeContent: true,
+              includeCapture: false,
+            });
+          }}
+          disabled={isLoading}
+        />
+      </div>
+    </>
   );
 }
