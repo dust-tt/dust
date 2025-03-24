94ac9404c528008f7cdc22fa83c0acf275501dd3
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-03-24 16:48:41 +0100

    Geist front (#11531)
    * Using Geist
    
    * Rebase
    
    * Adding Italic
    
    * Prettier
    
    * Bump Sparkle
    
    * Replace objektiv
    
    * Advanced font settings
    
    * Tweaking Sparkle fonts
    
    * Adding new text styles
    
    * Adding new text styles
    
    * Bumping sparkle
    
    * Security policy
    
    * Security policy
    
    * Security policy
    
    * Letter Spacing
    
    * version patch
    
    * Storybook fix
    
    * front changes
    
    * Cleaning sparkle
    
    * Deploying Sparkle
    
    * Final verision of Sparkle

--- front/components/poke/shadcn/ui/data_table_faceted_filter.tsx
+++ front/components/poke/shadcn/ui/data_table_faceted_filter.tsx
@@ -117,7 +117,7 @@ export function PokeDataTableFacetedFilter<TData, TValue>({
                     )}
                     <span>{option.label}</span>
                     {facets?.get(option.value) && (
-                      <span className="font-mono ml-auto flex h-4 w-4 items-center justify-center text-xs">
+                      <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
                         {facets.get(option.value)}
                       </span>
                     )}
