95f9ec1eb67e7fb462c118f7abdb2511f38c6145
Aubin <aubin@dust.tt>
2025-03-25 20:02:21 +0100

    mergefix

--- front/lib/actions/mcp_internal_actions/input_schemas.ts
+++ front/lib/actions/mcp_internal_actions/input_schemas.ts
@@ -1,9 +1,9 @@
-import { MIME_TYPES } from "@dust-tt/client";
+import { INTERNAL_MIME_TYPES } from "@dust-tt/client";
 import { z } from "zod";
 
 export const DataSourceConfigurationInputSchema = z.object({
   uri: z.literal(
     "data_source_configuration://dust/w/{wId}/data_source_configurations/{dscId}"
   ),
-  mimeType: z.literal(MIME_TYPES.CONFIGURATION.DATA_SOURCE),
+  mimeType: z.literal(INTERNAL_MIME_TYPES.CONFIGURATION.DATA_SOURCE),
 });
