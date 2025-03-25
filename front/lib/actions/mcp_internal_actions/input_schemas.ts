ff48a9f8938c5891fb71cf27916dbd0a3d0a9493
Aubin <aubin@dust.tt>
2025-03-25 17:23:22 +0100

    unnest the schema

--- front/lib/actions/mcp_internal_actions/input_schemas.ts
+++ front/lib/actions/mcp_internal_actions/input_schemas.ts
@@ -2,11 +2,8 @@ import { MIME_TYPES } from "@dust-tt/client";
 import { z } from "zod";
 
 export const DataSourceConfigurationInputSchema = z.object({
-  type: z.literal("object"),
-  inputSchema: z.object({
-    uri: z.literal(
-      "data_source_configuration://dust/w/{wId}/data_source_configurations/{dscId}"
-    ),
-    mimeType: z.literal(MIME_TYPES.DATA_SOURCE_CONFIGURATION),
-  }),
+  uri: z.literal(
+    "data_source_configuration://dust/w/{wId}/data_source_configurations/{dscId}"
+  ),
+  mimeType: z.literal(MIME_TYPES.DATA_SOURCE_CONFIGURATION),
 });
