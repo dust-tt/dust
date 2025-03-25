3c51b262e7b2772e49673a4cb2e399b0ae4404e2
Stanislas Polu <spolu@dust.tt>
2025-03-25 14:06:55 +0100

    better migration

--- front/migrations/db/migration_189.sql
+++ front/migrations/db/migration_189.sql
@@ -1,3 +1,12 @@
 -- Migration created on Mar 25, 2025
 ALTER TABLE "public"."content_fragments" ADD COLUMN "nodeType" VARCHAR(255);
-UPDATE "public"."content_fragments" SET "nodeType" = 'document' WHERE "nodeId" IS NOT NULL;
+UPDATE "public"."content_fragments"
+SET "nodeType" = CASE
+  WHEN "contentType" IN (
+    'application/vnd.google-apps.spreadsheet',
+    'text/csv',
+    'application/vnd.dust.notion.database'
+  ) THEN 'table'
+  ELSE 'document'
+END
+WHERE "nodeId" IS NOT NULL;
