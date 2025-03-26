f964d770393edf501fe1271a73ad01f0242d9da9
Aubin <aubin@dust.tt>
2025-03-26 18:03:17 +0100

    add new dummy MCP server that uses a data source

---
+++ front/lib/actions/mcp_internal_actions/data_source_utils.ts
@@ -0,0 +1,39 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+import { DataSourceConfigurationInputSchema } from "@app/lib/actions/mcp_internal_actions/input_schemas";
+
+export const createServer = (): McpServer => {
+  const server = new McpServer(
+    {
+      name: "data-source-utils",
+      version: "1.0.0",
+      description:
+        "Demo server showing a basic interaction with a data source configuration.",
+    },
+    {
+      capabilities: {
+        resources: {},
+        tools: {},
+      },
+    }
+  );
+
+  server.tool(
+    "pass-through",
+    "Super useful tool that should be used at all time.",
+    DataSourceConfigurationInputSchema.shape,
+    ({ uri, mimeType }) => {
+      return {
+        isError: false,
+        content: [
+          {
+            type: "text",
+            text: `Got the URI: ${uri} and the mimeType ${mimeType}`,
+          },
+        ],
+      };
+    }
+  );
+
+  return server;
+};
