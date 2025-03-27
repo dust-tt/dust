1b406a799fa5fde5f8f46957bbedb6081ded5ab2
Aubin <aubin@dust.tt>
2025-03-27 18:42:21 +0100

    update the hook to get the metadata

--- front/lib/swr/mcp.ts
+++ front/lib/swr/mcp.ts
@@ -2,24 +2,24 @@ import { useMemo } from "react";
 import type { Fetcher } from "swr";
 
 import { fetcher, useSWRWithDefaults } from "@app/lib/swr/swr";
-import type { GetMCPServerToolsResponseBody } from "@app/pages/api/w/[wId]/mcp/[serverId]/tools";
+import type { GetMCPServerMetadataResponseBody } from "@app/pages/api/w/[wId]/mcp/[serverId]";
 import type { LightWorkspaceType } from "@app/types";
 
-export function useInternalMcpServerTools({
+export function useInternalMcpServerMetadata({
   owner,
   serverId,
 }: {
   owner: LightWorkspaceType;
   serverId: string | null;
 }) {
-  const configFetcher: Fetcher<GetMCPServerToolsResponseBody> = fetcher;
+  const configFetcher: Fetcher<GetMCPServerMetadataResponseBody> = fetcher;
 
   const { data, error, mutate } = useSWRWithDefaults(
-    serverId ? `/api/w/${owner.sId}/mcp/${serverId}/tools` : null,
+    serverId ? `/api/w/${owner.sId}/mcp/${serverId}` : null,
     configFetcher
   );
 
-  const tools = useMemo(() => (data ? data.tools : null), [data]);
+  const tools = useMemo(() => (data ? data.metadata : null), [data]);
 
   return {
     tools,
