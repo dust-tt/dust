74fb2407fa2f4bf3b789cc0b89fbddcaca2cb1bd
Aubin <aubin@dust.tt>
2025-03-27 18:42:10 +0100

    update the endpoint to return the metadata

--- front/pages/api/w/[wId]/mcp/[serverId]/index.ts
+++ front/pages/api/w/[wId]/mcp/[serverId]/index.ts
@@ -1,26 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { validateInternalMCPServerId } from "@app/lib/actions/mcp";
-import type { ToolType } from "@app/lib/actions/mcp_actions";
-import {
-  listMCPServerTools,
-  normalizeInputSchemaProperties,
-} from "@app/lib/actions/mcp_actions";
+import type { MCPServerMetadata } from "@app/lib/actions/mcp_actions";
+import { getMCPServerMetadataLocally } from "@app/lib/actions/mcp_actions";
 import { withSessionAuthenticationForWorkspace } from "@app/lib/api/auth_wrappers";
 import type { Authenticator } from "@app/lib/auth";
 import { apiError } from "@app/logger/withlogging";
 import type { WithAPIErrorResponse } from "@app/types";
 
-// TODO(mcp): Share this type with InputSchemaType.
-export type GetMCPServerToolsResponseBody = {
-  tools: (Pick<ToolType, "name" | "description"> & {
-    inputSchema: { [key: string]: string | number | boolean | object };
-  })[];
+export type GetMCPServerMetadataResponseBody = {
+  metadata: MCPServerMetadata;
 };
 
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<WithAPIErrorResponse<GetMCPServerToolsResponseBody>>,
+  res: NextApiResponse<WithAPIErrorResponse<GetMCPServerMetadataResponseBody>>,
   auth: Authenticator
 ): Promise<void> {
   const { serverId } = req.query;
@@ -48,15 +42,6 @@ async function handler(
 
   switch (req.method) {
     case "GET":
-      if (!serverId) {
-        return apiError(req, res, {
-          status_code: 400,
-          api_error: {
-            type: "invalid_mcp_server_id",
-            message: "MCP server id is required.",
-          },
-        });
-      }
       if (!validateInternalMCPServerId(serverId)) {
         return apiError(req, res, {
           status_code: 400,
@@ -67,17 +52,12 @@ async function handler(
         });
       }
 
-      const tools = await listMCPServerTools({
+      const metadata = await getMCPServerMetadataLocally({
         serverType: "internal",
         internalMCPServerId: serverId,
       });
 
-      return res.status(200).json({
-        tools: tools.map((tool) => ({
-          ...tool,
-          inputSchema: normalizeInputSchemaProperties(tool.inputSchema),
-        })),
-      });
+      return res.status(200).json({ metadata });
     default:
       return apiError(req, res, {
         status_code: 405,
