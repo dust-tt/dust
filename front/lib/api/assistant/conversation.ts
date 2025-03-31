dust-tt/dust

commit 300782e3b999e4683b1f4a411a085ecd1dc87396

fix: remove include (#10880)
Co-authored-by: Henry Fontanier <henry@dust.tt>

front/lib/api/assistant/conversation.ts
@@ -46,6 +46,7 @@ import {
   removeNulls,
 } from "@dust-tt/types";
 import { isEqual, sortBy } from "lodash";
+import _ from "lodash";
 import type { Transaction } from "sequelize";
 import { Op } from "sequelize";
 
@@ -239,56 +240,52 @@ export async function getUserConversations(
   }
 
   const participations = await ConversationParticipant.findAll({
-    attributes: ["userId", "updatedAt"],
+    attributes: ["userId", "updatedAt", "conversationId"],
     where: {
       userId: user.id,
       action: "posted",
     },
-    include: [
-      {
-        model: Conversation,
-        as: "conversation",
-        required: true,
-      },
-    ],
     order: [["updatedAt", "DESC"]],
   });
 
-  const conversations = participations.reduce<ConversationWithoutContentType[]>(
-    (acc, p) => {
-      if (!p.conversation) {
-        logger.error("Participation without conversation");
-        return acc;
-      }
-      if (
-        p.conversation.workspaceId !== owner.id ||
-        (p.conversation.visibility === "deleted" && !includeDeleted) ||
-        (p.conversation.visibility === "test" && !includeTest)
-      ) {
-        return acc;
-      }
-
-      const conversation: ConversationWithoutContentType = {
-        id: p.conversation.id,
-        created: p.conversation.createdAt.getTime(),
-        updated: p.updatedAt.getTime(),
-        sId: p.conversation.sId,
+  const conversations = (
+    await Conversation.findAll({
+      where: {
+        id: { [Op.in]: _.uniq(participations.map((p) => p.conversationId)) },
+        workspaceId: owner.id,
+        ...(includeDeleted ? {} : { visibility: { [Op.ne]: "deleted" } }),
+        ...(includeTest ? {} : { visibility: { [Op.ne]: "test" } }),
+      },
+    })
+  ).map(
+    (c) =>
+      ({
+        id: c.id,
+        created: c.createdAt.getTime(),
+        updated: c.updatedAt.getTime(),
+        sId: c.sId,
         owner,
-        title: p.conversation.title,
-        visibility: p.conversation.visibility,
-        requestedGroupIds: getConversationRequestedGroupIdsFromModel(
-          owner,
-          p.conversation
-        ),
+        title: c.title,
+        visibility: c.visibility,
+        requestedGroupIds: getConversationRequestedGroupIdsFromModel(owner, c),
         // TODO(2025-01-15) `groupId` clean-up. Remove once Chrome extension uses optional.
         groupIds: [],
-      };
-
-      return [...acc, conversation];
-    },
-    []
+      }) satisfies ConversationWithoutContentType
   );
-  return conversations;
+
+  const conversationById = _.keyBy(conversations, "id");
+
+  return removeNulls(
+    participations.map((p) => {
+      const conv: ConversationWithoutContentType | null =
+        conversationById[p.conversationId];
+      if (!conv) {
+        logger.error("Participation without conversation");
+        return null;
+      }
+      return conv;
+    })
+  );
 }
 
 async function createOrUpdateParticipation({

commit a64f16ff5bbedc01bdd873767100f3505d805eae

[Conversations sort] Fix: broken sort
Fixes https://github.com/dust-tt/tasks/issues/2441

Description
---
Fix regression introduced in https://github.com/dust-tt/dust/pull/10880 sorting conversations by convo.updatedAt rather than participant.updatedAt. See related issue: sorting by participant.updatedAt allows for better sorting.

Risks
---
standard

Deploy
---
front

front/lib/api/assistant/conversation.ts
@@ -265,6 +265,18 @@ export async function getUserConversations(
     includedConversationVisibilities.push("test");
   }
 
+  const participationsByConversationId = _.groupBy(
+    participations,
+    "conversationId"
+  );
+  const conversationUpdated = (c: Conversation) => {
+    const participations = participationsByConversationId[c.id];
+    if (!participations) {
+      return undefined;
+    }
+    return _.sortBy(participations, "updatedAt", "desc")[0].updatedAt.getTime();
+  };
+
   const conversations = (
     await Conversation.findAll({
       where: {
@@ -278,7 +290,7 @@ export async function getUserConversations(
       ({
         id: c.id,
         created: c.createdAt.getTime(),
-        updated: c.updatedAt.getTime(),
+        updated: conversationUpdated(c),
         sId: c.sId,
         owner,
         title: c.title,