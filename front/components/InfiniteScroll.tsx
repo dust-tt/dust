b27acd6ff867c52d5da97106e20aa2c0e0b9bfd2
ykmsd <yuka@dust.tt>
2025-03-31 10:25:57 +0200

    fix format

--- front/components/InfiniteScroll.tsx
+++ front/components/InfiniteScroll.tsx
@@ -11,7 +11,7 @@ export type InfiniteScrollProps = {
 
 /**
  * The sentinel div has 1px height becase when you zoom out the container can have a fractional pixel
- * and if it happens browsers will round it to determine the maximum scrollable position and you will not reach out 
+ * and if it happens browsers will round it to determine the maximum scrollable position and you will not reach out
  * the bottom of the container if the element height is 0px.
  */
 export const InfiniteScroll = ({
