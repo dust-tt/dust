740b672e0f9652fb6b217da624e737a58c93b4ec
Stanislas Polu <spolu@dust.tt>
2025-04-03 12:26:03 +0200

    add channel to sync

--- connectors/src/connectors/slack/lib/cli.ts
+++ connectors/src/connectors/slack/lib/cli.ts
@@ -1,4 +1,7 @@
-import { updateSlackChannelInConnectorsDb } from "@connectors/connectors/slack/lib/channels";
+import {
+  joinChannel,
+  updateSlackChannelInConnectorsDb,
+} from "@connectors/connectors/slack/lib/channels";
 import {
   getSlackChannelSourceUrl,
   slackChannelInternalIdFromSlackChannelId,
@@ -283,6 +286,66 @@ export const slack = async ({
         sourceUrl: getSlackChannelSourceUrl(args.channelId, slackConfiguration),
         providerVisibility: channel.private ? "private" : "public",
       });
+      return { success: true };
+    }
+
+    case "add-channel-to-sync": {
+      if (!args.wId) {
+        throw new Error("Missing --wId argument");
+      }
+      if (!args.channelId) {
+        throw new Error("Missing --channelId argument");
+      }
+      const connector = await ConnectorModel.findOne({
+        where: { workspaceId: `${args.wId}`, type: "slack" },
+      });
+      if (!connector) {
+        throw new Error(`Could not find connector for workspace ${args.wId}`);
+      }
+
+      const remoteChannel = await getChannel(connector.id, args.channelId);
+      if (!remoteChannel.name) {
+        throw new Error(
+          `Could not find channel name for channel ${args.channelId}`
+        );
+      }
+
+      const joinRes = await joinChannel(connector.id, args.channelId);
+      if (joinRes.isErr()) {
+        throw new Error(
+          `Could not join channel ${args.channelId}: ${joinRes.error}`
+        );
+      }
+
+      // const dataSourceConfig = dataSourceConfigFromConnector(connector);
+
+      const slackConfiguration =
+        await SlackConfigurationResource.fetchByConnectorId(connector.id);
+      if (!slackConfiguration) {
+        throw new Error(
+          `Could not find Slack configuration for connector ${connector.id}`
+        );
+      }
+
+      const channel = await updateSlackChannelInConnectorsDb({
+        slackChannelId: args.channelId,
+        slackChannelName: remoteChannel.name,
+        connectorId: connector.id,
+        createIfNotExistsWithParams: {
+          permission: "read_write",
+          private: !!remoteChannel.is_private,
+        },
+      });
+
+      const workflowRes = await launchSlackSyncWorkflow(connector.id, null, [
+        channel.slackId,
+      ]);
+      if (workflowRes.isErr()) {
+        throw new Error(
+          `Could not launch workflow for channel ${args.channelId}: ${workflowRes.error}`
+        );
+      }
+
       return { success: true };
     }
 
