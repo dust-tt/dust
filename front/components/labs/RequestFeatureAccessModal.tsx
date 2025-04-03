9f07e3f0597801fe68092af99a1b315002477d33
Alban Dumouilla <alban@dust.tt>
2025-04-03 17:11:18 +0200

    Move text under canRequestAccess

--- front/components/labs/RequestFeatureAccessModal.tsx
+++ front/components/labs/RequestFeatureAccessModal.tsx
@@ -71,18 +71,20 @@ export function RequestFeatureAccessModal({
         <SheetContainer>
           <div className="flex flex-col gap-4 p-4">
             <div className="flex flex-col gap-2">
-              <div className="flex flex-col gap-2">
-                <p className="text-element-700 mb-2 text-sm">
-                  {`This feature is currently in beta. If you'd like to request access, please fill out the form below.`}
-                </p>
-              </div>
               {canRequestAccess ? (
-                <TextArea
-                  placeholder={`Please tell us why you'd like to access ${featureName}`}
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  className="mb-2"
-                />
+                <>
+                  <div className="flex flex-col gap-2">
+                    <p className="text-element-700 mb-2 text-sm">
+                      {`This feature is currently in beta. If you'd like to request access, please fill out the form below.`}
+                    </p>
+                  </div>
+                  <TextArea
+                    placeholder={`Please tell us why you'd like to access ${featureName}`}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="mb-2"
+                  />
+                </>
               ) : (
                 <p className="text-element-700 mb-2 text-sm">
                   {`You don't have permission to request access to this feature. Please ask a Dust administrator to make the request.`}
