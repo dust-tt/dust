5525ad420867397ecc77570c655afbd1f00bde65
Aubin <aubin@dust.tt>
2025-04-04 16:08:46 +0200

    lint the migration file

--- front/migrations/db/migration_201.sql
+++ front/migrations/db/migration_201.sql
@@ -1,3 +1,5 @@
 -- Migration created on Apr 04, 2025
-ALTER TABLE "public"."agent_tables_query_configuration_tables" ADD COLUMN "mcpServerConfigurationId" BIGINT REFERENCES "agent_mcp_server_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "agent_tables_query_configuration_tables"  ALTER COLUMN "tablesQueryConfigurationId" DROP NOT NULL;
+ALTER TABLE "public"."agent_tables_query_configuration_tables"
+  ADD COLUMN "mcpServerConfigurationId" BIGINT REFERENCES "agent_mcp_server_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "agent_tables_query_configuration_tables"
+  ALTER COLUMN "tablesQueryConfigurationId" DROP NOT NULL;
