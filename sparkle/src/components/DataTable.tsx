3a341a920a3fa5dd0df5e7066f2d752a0fafcc0e
Jules Belveze <32683010+JulesBelveze@users.noreply.github.com>
2025-04-07 17:46:07 +0200

    [sparkle] - feat(DataTable): selectable rows (#11803)
    * [sparkle] - feature: implement row selection feature in DataTable component
    
     - Add RowSelectionState and handler for managing row selection state changes
     - Introduce enableRowSelection prop to conditionally enable row selection functionality
     - Create createSelectionColumn function to generate a column for selection checkboxes
     - Adjust DataTable component to handle new row selection and enable/disable logic
    
    * [sparkle] - feature: add row selection functionality to DataTable component
    
     - Implement row selection feature with the inclusion of createSelectionColumn in DataTable stories
     - Introduce id property in data model to support enableRowSelection
     - Enhance DataTableWithRowSelectionExample with new state management and UI elements to display selection state
    
    * [sparkle] - refactor: update DataTable component selection logic
    
     - Modified the `enableRowSelection` prop to work with `Row` type instead of raw data type for consistency
     - Refactored the `createSelectionColumn` function to streamline the checkbox state handling
     - Enhanced the DataTable row selection story to demonstrate the new selection logic with simpler configuration
    
    * [sparkle] - feature: highlight the selected row in DataTable
    
     - Added a `data-selected` property to DataRow to visually indicate selected rows
     - Modified Row component to apply a background highlight on selected rows
     - Ensured the selection checkbox aligns properly by wrapping it within a flex container
     - Updated the selection column's metadata to reflect the new alignment changes
    
    * [sparkle] - feature: implement row selection in DataTable component
    
     - Added row selection state management to enable selection features in the data table
     - Integrated row selection logic within the React Table hook to accommodate selection state changes
     - Enhanced DataTable component to accept row selection props for state management and conditional rendering
     - Updated DataTable stories to showcase row selection functionality with sample state and adjusted columns
     - Ensured selected state is reflected in the DOM by binding data-selected attribute in the row rendering logic
    
    * [sparkle] - feature: enable row selection in ScrollableDataTable
    
     - Introduced a new prop to control row selection functionality within the data table component
    
    * [sparkle] - feature: increment package version to 0.2.455
    
     - Update the sparkle package to the new version 0.2.455 in both package-lock.json and package.json files.

--- sparkle/src/components/DataTable.tsx
+++ sparkle/src/components/DataTable.tsx
@@ -8,6 +8,8 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   PaginationState,
+  Row,
+  RowSelectionState,
   type SortingState,
   Updater,
   useReactTable,
@@ -18,6 +20,7 @@ import React, { ReactNode, useEffect, useRef, useState } from "react";
 import {
   Avatar,
   Button,
+  Checkbox,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
@@ -95,6 +98,9 @@ interface DataTableProps<TData extends TBaseData> {
   setSorting?: (sorting: SortingState) => void;
   isServerSideSorting?: boolean;
   disablePaginationNumbers?: boolean;
+  rowSelection?: RowSelectionState;
+  setRowSelection?: (rowSelection: RowSelectionState) => void;
+  enableRowSelection?: boolean | ((row: Row<TData>) => boolean);
 }
 
 export function DataTable<TData extends TBaseData>({
@@ -113,6 +119,9 @@ export function DataTable<TData extends TBaseData>({
   setSorting,
   isServerSideSorting = false,
   disablePaginationNumbers = false,
+  rowSelection,
+  setRowSelection,
+  enableRowSelection = false,
 }: DataTableProps<TData>) {
   const windowSize = useWindowSize();
 
@@ -140,6 +149,15 @@ export function DataTable<TData extends TBaseData>({
         }
       : undefined;
 
+  const onRowSelectionChange =
+    rowSelection && setRowSelection
+      ? (updater: Updater<RowSelectionState>) => {
+          const newValue =
+            typeof updater === "function" ? updater(rowSelection) : updater;
+          setRowSelection(newValue);
+        }
+      : undefined;
+
   const table = useReactTable({
     data,
     columns,
@@ -157,17 +175,20 @@ export function DataTable<TData extends TBaseData>({
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: pagination ? getPaginationRowModel() : undefined,
     onColumnFiltersChange: setColumnFilters,
+    onRowSelectionChange,
     state: {
       columnFilters,
       ...(isServerSideSorting && {
         sorting,
       }),
       pagination,
+      rowSelection,
     },
     initialState: {
       sorting,
     },
     onPaginationChange,
+    enableRowSelection,
   });
 
   useEffect(() => {
@@ -235,6 +256,7 @@ export function DataTable<TData extends TBaseData>({
               widthClassName={widthClassName}
               key={row.id}
               onClick={row.original.onClick}
+              data-selected={row.getIsSelected()}
             >
               {row.getVisibleCells().map((cell) => {
                 const breakpoint = columnsBreakpoints[cell.column.id];
@@ -291,6 +313,9 @@ export function ScrollableDataTable<TData extends TBaseData>({
   maxHeight = "s-h-100",
   onLoadMore,
   isLoading = false,
+  rowSelection,
+  setRowSelection,
+  enableRowSelection,
 }: ScrollableDataTableProps<TData>) {
   const windowSize = useWindowSize();
   const tableContainerRef = useRef<HTMLDivElement>(null);
@@ -316,12 +341,26 @@ export function ScrollableDataTable<TData extends TBaseData>({
     };
   }, []);
 
+  const onRowSelectionChange =
+    rowSelection && setRowSelection
+      ? (updater: Updater<RowSelectionState>) => {
+          const newValue =
+            typeof updater === "function" ? updater(rowSelection) : updater;
+          setRowSelection(newValue);
+        }
+      : undefined;
+
   const table = useReactTable({
     data,
     columns,
     rowCount: totalRowCount,
     getCoreRowModel: getCoreRowModel(),
     enableColumnResizing: true,
+    onRowSelectionChange,
+    enableRowSelection,
+    state: {
+      rowSelection,
+    },
   });
 
   useEffect(() => {
@@ -466,6 +505,7 @@ export function ScrollableDataTable<TData extends TBaseData>({
                     widthClassName={widthClassName}
                     onClick={row.original.onClick}
                     className="s-absolute s-w-full"
+                    data-selected={row.getIsSelected()}
                     style={{
                       transform: `translateY(${virtualRow.start}px)`,
                     }}
@@ -616,6 +656,7 @@ interface RowProps extends React.HTMLAttributes<HTMLTableRowElement> {
   children: ReactNode;
   onClick?: () => void;
   widthClassName: string;
+  "data-selected"?: boolean;
 }
 
 DataTable.Row = function Row({
@@ -633,6 +674,8 @@ DataTable.Row = function Row({
         onClick
           ? "s-cursor-pointer hover:s-bg-muted dark:hover:s-bg-muted-night"
           : "",
+        props["data-selected"] &&
+          "s-bg-highlight-50 dark:s-bg-highlight-900/10",
         widthClassName,
         className
       )}
@@ -1013,3 +1056,47 @@ DataTable.Caption = function Caption({
     </caption>
   );
 };
+
+export function createSelectionColumn<TData>(): ColumnDef<TData> {
+  return {
+    id: "select",
+    enableSorting: false,
+    enableHiding: false,
+    header: ({ table }) => (
+      <Checkbox
+        size="xs"
+        checked={
+          table.getIsAllRowsSelected()
+            ? true
+            : table.getIsSomeRowsSelected()
+              ? "partial"
+              : false
+        }
+        onCheckedChange={(state) => {
+          if (state === "indeterminate") {
+            return;
+          }
+          table.toggleAllRowsSelected(state);
+        }}
+      />
+    ),
+    cell: ({ row }) => (
+      <div className="s-flex s-h-full s-w-full s-items-center">
+        <Checkbox
+          size="xs"
+          checked={row.getIsSelected()}
+          disabled={!row.getCanSelect()}
+          onCheckedChange={(state) => {
+            if (state === "indeterminate") {
+              return;
+            }
+            row.toggleSelected(state);
+          }}
+        />
+      </div>
+    ),
+    meta: {
+      className: "s-w-10",
+    },
+  };
+}
