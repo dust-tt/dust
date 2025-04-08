bf4c14d50998eab3183a0dc4b334b21a89f0d74d
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-04-08 15:29:37 +0200

    fix migration file

--- front/migrations/db/migration_205.sql
+++ front/migrations/db/migration_205.sql
@@ -3,9 +3,11 @@ CREATE TABLE IF NOT EXISTS "child_agent_configurations"
 (
   "createdAt"                TIMESTAMP WITH TIME ZONE NOT NULL,
   "updatedAt"                TIMESTAMP WITH TIME ZONE NOT NULL,
+  "agentConfigurationId"     VARCHAR(255)             NOT NULL,
   "workspaceId"              BIGINT                   NOT NULL REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
   "id"                       BIGSERIAL,
   "mcpServerConfigurationId" BIGINT                   NOT NULL REFERENCES "agent_mcp_server_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
   PRIMARY KEY ("id")
 );
+
 CREATE INDEX "child_agent_configurations_mcp_server_configuration_id" ON "child_agent_configurations" ("mcpServerConfigurationId");
