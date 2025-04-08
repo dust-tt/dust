7c23c90c92f74412d81e3fe930b84b7cb89997fd
Maxime Cattet <m@root.bar>
2025-04-08 18:06:58 +0200

    [front] docs(mcp): Add more comments and logger for the try catch

--- front/pages/api/w/[wId]/spaces/[spaceId]/mcp_views/not_activated.ts
+++ front/pages/api/w/[wId]/spaces/[spaceId]/mcp_views/not_activated.ts
@@ -8,6 +8,7 @@ import { withResourceFetchingFromRoute } from "@app/lib/api/resource_wrappers";
 import type { Authenticator } from "@app/lib/auth";
 import { MCPServerViewResource } from "@app/lib/resources/mcp_server_view_resource";
 import type { SpaceResource } from "@app/lib/resources/space_resource";
+import logger from "@app/logger/logger";
 import { apiError } from "@app/logger/withlogging";
 import type { WithAPIErrorResponse } from "@app/types";
 
@@ -54,10 +55,20 @@ async function handler(
     success: true,
     serverViews: removeNulls(
       serverViews.map((s) => {
-        // TODO: update
+        // WARN: Probably due to Intenal MCP Server view pointing to `internalMCPServerId` that doesn't exists anymore.
+        // Need to think about migration of mcp_server_view when we're updating internal mcp servers.
         try {
           return s.toJSON();
         } catch (err) {
+          logger.error(
+            {
+              serverViewId: s.sId,
+              workspaceId: s.workspaceId,
+              spaceId: space.sId,
+              userId: auth.getNonNullableUser().id,
+            },
+            "couldn't toJSON() a mcp_server_view"
+          );
           return null;
         }
       })
