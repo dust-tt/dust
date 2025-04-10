7edcb3ab366f83429e7313b116a8579ff22db2be
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-04-10 09:50:23 +0200

    fix typos in test scripts

--- front/admin/test.sh
+++ front/admin/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Check that we have TEST_FRONT_DATABASE_URI set
 if [ -z "$TEST_FRONT_DATABASE_URI" ]; then
@@ -12,6 +12,7 @@ fi
 # Init the test db
 echo "Synching test db schemas..."
 NODE_ENV=TEST FRONT_DATABASE_URI=$TEST_FRONT_DATABASE_URI npx tsx "$SCRIPT_DIR/db.ts"
+echo "Syncing test db schemas..."
 
 # Start the tests
 FRONT_DATABASE_URI=$TEST_FRONT_DATABASE_URI npm run test
