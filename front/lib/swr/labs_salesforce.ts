210546fd02fa82a3716290dbd64d5c42d587f1da
Thomas Draier <thomas@dust.tt>
2025-04-11 10:11:57 +0200

    renamed

--- front/lib/swr/labs_salesforce.ts
+++
@@ -1,7 +0,0 @@
-import { useSendNotification } from "@dust-tt/sparkle";
-import { useMemo } from "react";
-
-import { fetcher, useSWRWithDefaults } from "@app/lib/swr/swr";
-import { getPKCEConfig } from "@app/lib/utils/pkce";
-import type { DataSourceType, LightWorkspaceType } from "@app/types";
-import { isOAuthProvider, setupOAuthConnection } from "@app/types";
