707d9fb8495ea36846f2647dcbca16ee8a8bae75
Thomas Draier <thomas@dust.tt>
2025-04-11 14:20:09 +0200

    [sparkle] Action icons (#11924)
    * Change icon names
    
    * Add list export to simplify usage
    
    * lint
    
    * naming

--- sparkle/build_icons.sh
+++ sparkle/build_icons.sh
@@ -6,7 +6,7 @@ rm -rf src/logo/platforms
 rm -rf src/logo/dust
 
 npx @svgr/cli --no-prettier --index-template svgr-icon-template.js --out-dir src/icons/app/ src/icons/src/app
-npx @svgr/cli --no-prettier --index-template svgr-icon-template.js --out-dir src/icons/actions/ src/icons/src/actions
+npx @svgr/cli --no-prettier --index-template svgr-actions-icon-template.js --out-dir src/icons/actions/ src/icons/src/actions
 npx @svgr/cli --no-prettier --index-template svgr-platform-template.js --out-dir src/logo/platforms/ src/logo/src/platforms/
 npx @svgr/cli --no-prettier --index-template svgr-logo-template.js --out-dir src/logo/dust/ src/logo/src/dust/
 
