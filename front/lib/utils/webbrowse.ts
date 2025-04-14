e2009edbfb6d1bd0049a74028774ab55acb1711f
Maxime Cattet <m@root.bar>
2025-04-14 15:54:43 +0200

    [front] chore(mcp/webtools): Remove extra try catch

--- front/lib/utils/webbrowse.ts
+++ front/lib/utils/webbrowse.ts
@@ -38,58 +38,45 @@ export const browseUrl = async (
 
   webLogger.debug({ url }, "will scrape");
 
-  try {
-    const res = await fetch(
-      `${BROWSERLESS_BASE_URL}/scrape?token=${credentials.BROWSERLESS_API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control": "no-cache",
+  const res = await fetch(
+    `${BROWSERLESS_BASE_URL}/scrape?token=${credentials.BROWSERLESS_API_KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+      body: JSON.stringify({
+        url,
+        gotoOptions: {
+          timeout: 5000,
         },
-        body: JSON.stringify({
-          url,
-          gotoOptions: {
-            timeout: 5000,
+        elements: [
+          {
+            selector: "body",
           },
-          elements: [
-            {
-              selector: "body",
-            },
-          ],
-        }),
-      }
-    );
-
-    if (!res.ok) {
-      webLogger.error(
-        { status: res.status },
-        "bad request BrowserlessAPIError"
-      );
-      return new Err(new Error("Bad request scraping url"));
+        ],
+      }),
     }
+  );
 
-    try {
-      const json = await res.json();
-
-      webLogger.debug({ json }, "results");
-
-      return new Ok({
-        data: json.data as Record<string, any>,
-        response: {
-          code: res.headers.get("x-response-code"),
-          status: res.headers.get("x-response-status"),
-          url: res.headers.get("x-response-url"),
-          ip: res.headers.get("x-response-ip"),
-          port: res.headers.get("x-response-port"),
-        },
-      });
-    } catch (err) {
-      webLogger.error({ err }, "error parsing browserless response");
-      return new Err(err as Error);
-    }
-  } catch (err) {
-    webLogger.error({ err }, "error making BrowserlessAPI request");
-    return new Err(err as Error);
+  if (!res.ok) {
+    webLogger.error({ status: res.status }, "bad request BrowserlessAPIError");
+    return new Err(new Error("Bad request scraping url"));
   }
+
+  const json = await res.json();
+
+  webLogger.debug({ json }, "results");
+
+  return new Ok({
+    data: json.data as Record<string, any>,
+    response: {
+      code: res.headers.get("x-response-code"),
+      status: res.headers.get("x-response-status"),
+      url: res.headers.get("x-response-url"),
+      ip: res.headers.get("x-response-ip"),
+      port: res.headers.get("x-response-port"),
+    },
+  });
 };
