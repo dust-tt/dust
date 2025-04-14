5c99db164793b32dfc9f5561b8f2a7bce2658f8c
Maxime Cattet <m@root.bar>
2025-04-14 17:02:01 +0200

    Feat/mcp webtools (#11960)
    * fix(mcp/mime): Use regex to make a replace all
    
    Just using replace with a string replace the first found occurence of
    the string. This was making the function and the typins different. As we
    cannot use replaceAll, using the regex do the same
    
    * feat(mcp/webtools): scaffolding for webtools mcp server
    
    * [front] feat(mcp/webtools): add webtools websearch
    
    * [front] chore: rename utils webtools to just websearch
    
    * [front] feat(mcp/webtools): Add webbrowse capabilities
    
    * [front] chore(mcp/webtools): Remove extra try catch
    
    * [front] feat(agents): Remove dust-app web capabilities from selection
    
    * [front] docs(mcp/webtools): fix doc
    
    * Revert "[front] feat(agents): Remove dust-app web capabilities from selection"
    
    This reverts commit 585c81006b35836584861191a232f053a08b922d.
    
    * [front] feat(mcp/webtools): Update according to review
    
    - Make web_search_browse default but behind dev flag
    - Update tools and inputs description based on current dust-app web
    navigation
    - remove debug logs

---
+++ front/lib/actions/mcp_internal_actions/servers/webtools.ts
@@ -0,0 +1,101 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
+import { z } from "zod";
+
+import type { MCPServerDefinitionType } from "@app/lib/api/mcp";
+import { concurrentExecutor } from "@app/lib/utils/async_utils";
+import { browseUrl } from "@app/lib/utils/webbrowse";
+import { webSearch } from "@app/lib/utils/websearch";
+import type { OAuthProvider } from "@app/types";
+
+export const provider: OAuthProvider = "google_drive" as const;
+export const serverInfo: MCPServerDefinitionType = {
+  name: "web_search_&_browse",
+  version: "1.0.0",
+  description:
+    "Agent can search (Google) and retrieve information from specific websites.",
+  visual: "command",
+  authorization: {
+    provider,
+    use_case: "connection" as const,
+  },
+};
+
+const createServer = (): McpServer => {
+  const server = new McpServer(serverInfo);
+
+  server.tool(
+    "websearch",
+    "A google search tool",
+    {
+      query: z
+        .string()
+        .describe(
+          "The query used to perform the google search. If requested by the user, use the google syntax `site:` to restrict the the search to a particular website or domain."
+        ),
+    },
+    async ({ query }) => {
+      const websearchRes = await webSearch({ provider: "serpapi", query });
+
+      if (websearchRes.isErr()) {
+        return {
+          isError: false,
+          content: [
+            {
+              type: "text",
+              text: `Failed to search: ${websearchRes.error.message}`,
+            },
+          ],
+        };
+      }
+
+      return {
+        isError: false,
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(websearchRes.value),
+          },
+        ],
+      };
+    }
+  );
+
+  server.tool(
+    "webbrowser",
+    "A tool to browse website",
+    {
+      urls: z.string().array().describe("List of urls to browse"),
+    },
+    async ({ urls }) => {
+      const content: CallToolResult["content"] = await concurrentExecutor(
+        urls,
+        async (url) => {
+          const res = await browseUrl(url);
+
+          if (res.isErr()) {
+            return {
+              type: "text",
+              text: `Failed to fetch url "${url}": ${res.error.message}`,
+            };
+          }
+
+          return {
+            type: "text",
+            text: JSON.stringify(res.value),
+          };
+        },
+        { concurrency: 4 }
+      );
+
+      return {
+        isError: false,
+        content,
+      };
+    }
+  );
+
+  return server;
+};
+
+export default createServer;
