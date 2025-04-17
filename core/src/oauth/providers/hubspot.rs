6f437da84958b7a1f58856abaeddf0334650d674
Daphné Popin <daphne.popin@gmail.com>
2025-04-17 16:52:45 +0200

    Fix need to handle token refresh

--- core/src/oauth/providers/hubspot.rs
+++ core/src/oauth/providers/hubspot.rs
@@ -1,6 +1,7 @@
 use crate::oauth::{
     connection::{
         Connection, ConnectionProvider, FinalizeResult, Provider, ProviderError, RefreshResult,
+        PROVIDER_TIMEOUT_SECONDS,
     },
     credential::Credential,
     providers::utils::execute_request,
@@ -8,6 +9,7 @@ use crate::oauth::{
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use lazy_static::lazy_static;
+use serde_json::json;
 use std::env;
 
 lazy_static! {
@@ -55,6 +57,10 @@ impl Provider for HubspotConnectionProvider {
             .await
             .map_err(|e| self.handle_provider_request_error(e))?;
 
+        let expires_in = result["expires_in"]
+            .as_u64()
+            .ok_or_else(|| anyhow!("Missing expires_in in response"))?;
+
         Ok(FinalizeResult {
             redirect_uri: redirect_uri.to_string(),
             code: code.to_string(),
@@ -62,7 +68,9 @@ impl Provider for HubspotConnectionProvider {
                 .as_str()
                 .ok_or_else(|| anyhow!("Missing access_token in response"))?
                 .to_string(),
-            access_token_expiry: None, // HubSpot tokens don't expire
+            access_token_expiry: Some(
+                crate::utils::now() + (expires_in - PROVIDER_TIMEOUT_SECONDS) * 1000,
+            ),
             refresh_token: result["refresh_token"].as_str().map(|s| s.to_string()),
             raw_json: result,
         })
@@ -94,12 +102,18 @@ impl Provider for HubspotConnectionProvider {
             .await
             .map_err(|e| self.handle_provider_request_error(e))?;
 
+        let expires_in = result["expires_in"]
+            .as_u64()
+            .ok_or_else(|| anyhow!("Missing expires_in in response"))?;
+
         Ok(RefreshResult {
             access_token: result["access_token"]
                 .as_str()
                 .ok_or_else(|| anyhow!("Missing access_token in response"))?
                 .to_string(),
-            access_token_expiry: None, // HubSpot tokens don't expire
+            access_token_expiry: Some(
+                crate::utils::now() + (expires_in - PROVIDER_TIMEOUT_SECONDS) * 1000,
+            ),
             refresh_token: result["refresh_token"].as_str().map(|s| s.to_string()),
             raw_json: result,
         })
