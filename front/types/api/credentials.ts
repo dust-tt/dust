4f4440d56674f283659403f296ea75e8d1f4bbfc
Henry Fontanier <h.fontanier@gmail.com>
2025-04-22 17:57:03 +0200

    Revert "Revert "feat: switch to firecrawl for web browsing (#12176)" (#12178)" (#12179)
    This reverts commit 166fe1cefe1a3db26d8fb5a34a85c762cfa78cb0.

--- front/types/api/credentials.ts
+++ front/types/api/credentials.ts
@@ -13,6 +13,7 @@ const {
   DUST_MANAGED_TOGETHERAI_API_KEY = "",
   DUST_MANAGED_DEEPSEEK_API_KEY = "",
   DUST_MANAGED_FIREWORKS_API_KEY = "",
+  DUST_MANAGED_FIRECRAWL_API_KEY = "",
 } = process.env;
 
 export const credentialsFromProviders = (
@@ -70,6 +71,9 @@ export const credentialsFromProviders = (
       case "fireworks":
         credentials["FIREWORKS_API_KEY"] = config.api_key;
         break;
+      case "firecrawl":
+        credentials["FIRECRAWL_API_KEY"] = config.api_key;
+        break;
     }
   });
   return credentials;
@@ -89,5 +93,6 @@ export const dustManagedCredentials = (): CredentialsType => {
     TOGETHERAI_API_KEY: DUST_MANAGED_TOGETHERAI_API_KEY,
     DEEPSEEK_API_KEY: DUST_MANAGED_DEEPSEEK_API_KEY,
     FIREWORKS_API_KEY: DUST_MANAGED_FIREWORKS_API_KEY,
+    FIRECRAWL_API_KEY: DUST_MANAGED_FIRECRAWL_API_KEY,
   };
 };
