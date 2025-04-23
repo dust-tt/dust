b64996ac84b21fd9a92167479986fe797e5d5991
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-04-23 14:42:11 +0200

    Label has a muted prop

--- sparkle/src/components/Label.tsx
+++ sparkle/src/components/Label.tsx
@@ -3,22 +3,31 @@ import * as React from "react";
 
 import { cn } from "@sparkle/lib/utils";
 
-const labelVariants = cn(
-  "s-heading-sm",
-  "s-text-foreground dark:s-text-foreground-night",
-  "peer-disabled:s-cursor-not-allowed peer-disabled:s-opacity-70"
-);
+interface LabelProps
+  extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> {
+  isMuted?: boolean;
+}
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants, className)}
-    {...props}
-  />
-));
+  LabelProps
+>(({ className, isMuted = false, ...props }, ref) => {
+  const labelVariants = cn(
+    "s-heading-sm",
+    isMuted
+      ? "s-text-muted-foreground dark:s-text-muted-foreground-night"
+      : "s-text-foreground dark:s-text-foreground-night",
+    "peer-disabled:s-cursor-not-allowed peer-disabled:s-opacity-70"
+  );
+
+  return (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(labelVariants, className)}
+      {...props}
+    />
+  );
+});
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
