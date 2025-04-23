8ca4ba787709dea4dc3e52d219165289cb648e23
Henry Fontanier <h.fontanier@gmail.com>
2025-04-23 15:24:32 +0200

    fix: fix migration column names (#12219)
    Co-authored-by: Henry Fontanier <henry@dust.tt>

--- connectors/migrations/db/migration_66.sql
+++ connectors/migrations/db/migration_66.sql
@@ -3,5 +3,5 @@
 ALTER TABLE "remote_tables" 
   ALTER COLUMN "internalId" TYPE TEXT,
   ALTER COLUMN "name" TYPE TEXT,
-  ALTER COLUMN "database_name" TYPE TEXT, 
-  ALTER COLUMN "schema_name" TYPE TEXT;
+  ALTER COLUMN "databaseName" TYPE TEXT, 
+  ALTER COLUMN "schemaName" TYPE TEXT;
