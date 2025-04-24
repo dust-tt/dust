c4d457701605bbb0906ed63f6206cc18dedb8752
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-04-25 01:29:29 +0200

    rename migration script to avoid conflict

--- front/migrations/db/migration_219.sql
+++
@@ -1,7 +0,0 @@
--- Migration created on Apr 25, 2025
-ALTER TABLE "public"."agent_reasoning_configurations"
-ADD COLUMN "mcpServerConfigurationId" BIGINT REFERENCES "agent_mcp_server_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE "agent_reasoning_configurations"
-ALTER COLUMN "agentConfigurationId"
-DROP NOT NULL;
