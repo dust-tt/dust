3acd4dbaa95bc379b2de98bbc1186cd7b1143e28
Flavien David <flavien@dust.tt>
2025-04-24 12:01:29 +0200

    ✨

--- front/lib/plans/renderers.ts
+++ front/lib/plans/renderers.ts
@@ -35,7 +35,7 @@ export function renderPlanFromModel({
         },
       },
       capabilities: {
-        image: {
+        images: {
           maxImagesPerWeek: plan.maxImagesPerWeek,
         },
       },
