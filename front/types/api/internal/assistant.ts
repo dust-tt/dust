33eb70eb0df0c62e808c57b4923c2cb958343335
Maxime Cattet <m@root.bar>
2025-04-28 17:57:54 +0200

    [front] feat(ab): Add tags suggestions

--- front/types/api/internal/assistant.ts
+++ front/types/api/internal/assistant.ts
@@ -206,6 +206,15 @@ export const InternalPostBuilderSuggestionsRequestBodySchema = t.union([
     type: t.literal("description"),
     inputs: t.type({ instructions: t.string, name: t.string }),
   }),
+  t.type({
+    type: t.literal("tags"),
+    inputs: t.type({
+      instructions: t.string,
+      description: t.string,
+      isAdmin: t.boolean,
+      tags: t.array(t.string),
+    }),
+  }),
 ]);
 
 export type BuilderSuggestionsRequestType = t.TypeOf<
