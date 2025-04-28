d16176eb9478d1b3b12e23a62c3a0d9cad3e2bd8
ibharatsaini <sainib601@gmail.com>
2025-04-28 17:59:45 +0530

    fixed old secrets being exposed while updating

--- front/pages/w/[wId]/developers/dev-secrets.tsx
+++ front/pages/w/[wId]/developers/dev-secrets.tsx
@@ -196,6 +196,8 @@ export default function SecretsPage({
               message="Secret values are encrypted and stored securely in our database."
               name="Secret value"
               placeholder="Type the secret value"
+              type="password"
+              autocomplete="off"
               value={newDustAppSecret.value}
               onChange={(e) =>
                 setNewDustAppSecret({
