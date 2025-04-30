c10332b46594112db03e340cde2594e2c52fe65f
frankaloia <frank.e.aloia@gmail.com>
2025-04-30 15:55:42 -0400

    Add toggle to block Slack agents with access to restricted space (#12386)

--- front/lib/api/poke/plugins/data_sources/index.ts
+++ front/lib/api/poke/plugins/data_sources/index.ts
@@ -5,3 +5,4 @@ export * from "./notion_unstuck_syncing_nodes";
 export * from "./notion_update_orphaned_resources_parents";
 export * from "./notion_url_sync";
 export * from "./operations";
+export * from "./toggle_restricted_space_agent_slack_access";
