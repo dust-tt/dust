a7f3b7ba2148c797b353435119c3480bf3545d60
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-05-13 14:16:04 +0200

    remove a comment

--- front/pages/poke/[wId]/spaces/[spaceId]/mcp_server_views/[svId]/index.tsx
+++ front/pages/poke/[wId]/spaces/[spaceId]/mcp_server_views/[svId]/index.tsx
@@ -46,7 +46,6 @@ export const getServerSideProps = withSuperUserAuthRequirements<{
     };
   }
 
-  // Convert to PokeMCPServerViewType
   const pokeMCPServerView = await mcpServerViewToPokeJSON(mcpServerView);
 
   return {
