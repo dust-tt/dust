041df1b5716debaead7ddaa99c0652e6a42882ff
Maxime Cattet <m@root.bar>
2025-05-13 15:35:25 +0200

    fix table name

--- front/migrations/db/migration_243.sql
+++ front/migrations/db/migration_243.sql
@@ -1,2 +1,2 @@
 -- Migration created on May 13, 2025
-CREATE INDEX CONCURRENTLY "agent_conversation_include_file_action_workspace_id_agent_message_id" ON "agent_conversation_include_file_action" ("workspaceId", "agentMessageId");
+CREATE INDEX CONCURRENTLY "agent_conversation_include_file_action_workspace_id_agent_message_id" ON "agent_conversation_include_file_actions" ("workspaceId", "agentMessageId");
