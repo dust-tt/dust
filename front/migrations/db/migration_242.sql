b6a582322284eed20bb039cc4712dbe0ac3c7516
Maxime Cattet <m@root.bar>
2025-05-13 15:32:40 +0200

    fix table name

--- front/migrations/db/migration_242.sql
+++ front/migrations/db/migration_242.sql
@@ -1,2 +1,2 @@
 -- Migraiton created on May 13, 2025
-CREATE INDEX CONCURRENTLY "agent_tables_query_action_workspace_id_agent_message_id" ON "agent_tables_query_action" ("workspaceId", "agentMessageId")
+CREATE INDEX CONCURRENTLY "agent_tables_query_action_workspace_id_agent_message_id" ON "agent_tables_query_actions" ("workspaceId", "agentMessageId")
