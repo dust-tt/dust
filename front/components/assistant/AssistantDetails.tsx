a0fd7508c9b1d0a1f94aa50e1a0a2830409b1190
Thomas Draier <thomas@dust.tt>
2025-05-14 17:37:52 +0200

    test

--- front/components/assistant/AssistantDetails.tsx
+++ front/components/assistant/AssistantDetails.tsx
@@ -491,7 +491,7 @@ export function AssistantDetails({
                 <SheetTitle />
               </VisuallyHidden>
               <DescriptionSection />
-              {!showEditorsTabs && !showPerformanceTabs ? (
+              {showEditorsTabs || showPerformanceTabs ? (
                 <Tabs value={selectedTab}>
                   <TabsList border={false}>
                     <TabsTrigger
