3a671a15f8db1b5ddc7175c2b96ffdf1d9cc7154
Maxime Cattet <m@root.bar>
2025-05-14 16:33:36 +0200

    [front] chore(content_fragment): Add workspaceId filter

--- front/lib/actions/types/index.ts
+++ front/lib/actions/types/index.ts
@@ -64,13 +64,16 @@ export abstract class BaseAction {
   }
 
   abstract renderForFunctionCall(): FunctionCallType;
-  abstract renderForMultiActionsModel({
-    conversation,
-    model,
-  }: {
-    conversation: ConversationType;
-    model: ModelConfigurationType;
-  }): Promise<FunctionMessageTypeModel>;
+  abstract renderForMultiActionsModel(
+    auth: Authenticator,
+    {
+      conversation,
+      model,
+    }: {
+      conversation: ConversationType;
+      model: ModelConfigurationType;
+    }
+  ): Promise<FunctionMessageTypeModel>;
 }
 
 export type ExtractActionBlob<T extends BaseAction> = Pick<
