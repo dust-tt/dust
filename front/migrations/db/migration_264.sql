87f0a079a17d35fd08fd4b480afe2dfbe5083d93
Thomas Draier <thomas@dust.tt>
2025-05-15 13:06:21 +0200

    migration

--- front/migrations/db/migration_264.sql
+++ front/migrations/db/migration_264.sql
@@ -1,2 +1,2 @@
--- Migration created on May 14, 2025
-ALTER TABLE "public"."tags" ADD COLUMN "reserved" BOOLEAN NOT NULL DEFAULT false;
+-- Migration created on May 15, 2025
+ALTER TABLE "public"."tags" ADD COLUMN "kind" VARCHAR(255) DEFAULT 'standard';
