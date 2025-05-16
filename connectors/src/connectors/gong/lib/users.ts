e815395f8363f546d8f5429377c337f65b90d037
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-05-16 16:33:04 +0200

    format users.ts

--- connectors/src/connectors/gong/lib/users.ts
+++ connectors/src/connectors/gong/lib/users.ts
@@ -3,7 +3,10 @@ import { difference } from "lodash";
 import type { GongAPIUser } from "@connectors/connectors/gong/lib/gong_api";
 import { getGongClient } from "@connectors/connectors/gong/lib/utils";
 import type { ConnectorResource } from "@connectors/resources/connector_resource";
-import type { GongConfigurationResource, GongUserBlob } from "@connectors/resources/gong_resources";
+import type {
+  GongConfigurationResource,
+  GongUserBlob,
+} from "@connectors/resources/gong_resources";
 import { GongUserResource } from "@connectors/resources/gong_resources";
 import { concurrentExecutor } from "@connectors/types";
 
