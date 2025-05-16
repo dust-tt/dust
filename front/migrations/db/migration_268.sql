61e4112c0f215e163642c67ff57887b2765f8c3f
Maxime Cattet <m@root.bar>
2025-05-16 16:51:21 +0200

    rename migration

--- front/migrations/db/migration_268.sql
+++ front/migrations/db/migration_268.sql
@@ -1,2 +1,3 @@
 -- Migration created on May 14, 2025
-ALTER TABLE "public"."agent_messages" ADD COLUMN "skipToolsValidation" BOOLEAN NOT NULL DEFAULT false;
+CREATE INDEX CONCURRENTLY "labs_transcripts_configurations_workspace_id_provider" ON "labs_transcripts_configurations" ("workspaceId", "provider");
+CREATE INDEX CONCURRENTLY "labs_transcripts_configurations_workspace_id_user_id" ON "labs_transcripts_configurations" ("workspaceId", "userId");
