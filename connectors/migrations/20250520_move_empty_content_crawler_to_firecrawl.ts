0b1627b7f8529308394a203e763963f016c43727
Maxime Cattet <m@root.bar>
2025-05-20 15:45:27 +0200

    [connectors] chore(webcrawler): Add migration to move webcrawler custom crawler to firecrawl

---
+++ connectors/migrations/20250520_move_empty_content_crawler_to_firecrawl.ts
@@ -0,0 +1,58 @@
+/**
+ * Move webcrawler_configurations that have their connector errorType = webcrawling_error_empty_content to customCrawler = firecrawl
+ * They mustn't be part of a enterprise workspace
+ */
+import fs from "fs";
+import { makeScript } from "scripts/helpers";
+import { Op } from "sequelize";
+
+import { WebCrawlerConfigurationModel } from "@connectors/lib/models/webcrawler";
+import { ConnectorModel } from "@connectors/resources/storage/models/connector_model";
+
+makeScript(
+  {
+    skippedWorkspaceFile: { type: "string", required: true },
+  },
+  async ({ execute, skippedWorkspaceFile }, logger) => {
+    if (!fs.existsSync(skippedWorkspaceFile)) {
+      logger.error(`File "${skippedWorkspaceFile} doesn't exists`);
+      return;
+    }
+
+    const workspaceFileContent = fs.readFileSync(skippedWorkspaceFile, "utf-8");
+    const workspaceIds = workspaceFileContent.split("\n");
+    logger.info(`${workspaceIds.length} workspaces will be skipped`);
+
+    const webcrawlerConfigs = await WebCrawlerConfigurationModel.findAll({
+      include: [
+        {
+          model: ConnectorModel,
+          as: "connectors",
+          where: {
+            type: "webcrawler",
+            errorType: "webcrawling_error_empty_content",
+            workspaceId: {
+              [Op.notIn]: workspaceIds,
+            },
+          },
+        },
+      ],
+    });
+
+    logger.info(
+      `Found ${webcrawlerConfigs.length} webcrawler configuration that are webcrawler, in empty error state and not in the given workspaces`
+    );
+
+    if (execute) {
+      logger.info("Will execute");
+      for (const crawler of webcrawlerConfigs) {
+        await crawler.update("customCrawler", "firecrawl");
+      }
+      logger.info(
+        `Set "customCrawler" to "firecrawl" for ${webcrawlerConfigs.length} webcrawler configurations`
+      );
+    } else {
+      logger.info("Not executing update");
+    }
+  }
+);
