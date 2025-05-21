1ce1ebb2b0cfbad4857828a8d877e667acacb5c9
Maxime Cattet <m@root.bar>
2025-05-21 11:27:31 +0200

    [front] feat(invitation): Move invitation code to MembershipInvitationResource

--- front/lib/resources/membership_invitation_resource.ts
+++ front/lib/resources/membership_invitation_resource.ts
@@ -1,18 +1,21 @@
-// Attributes are marked as read-only to reflect the stateless nature of our Resource.
-// This design will be moved up to BaseResource once we transition away from Sequelize.
-
+import { verify } from "jsonwebtoken";
 import type { Attributes, Transaction } from "sequelize";
 
+import config from "@app/lib/api/config";
 import type { Authenticator } from "@app/lib/auth";
+import { AuthFlowError } from "@app/lib/iam/errors";
 import { MembershipInvitationModel } from "@app/lib/models/membership_invitation";
 import { Workspace } from "@app/lib/models/workspace";
 import { BaseResource } from "@app/lib/resources/base_resource";
 import type { ReadonlyAttributesType } from "@app/lib/resources/storage/types";
 import type { ModelStaticWorkspaceAware } from "@app/lib/resources/storage/wrappers/workspace_models";
-import type { Result, WorkspaceType } from "@app/types";
+import type { UserResource } from "@app/lib/resources/user_resource";
+import logger from "@app/logger/logger";
+import type { Result } from "@app/types";
+import { Err, Ok } from "@app/types";
 
-import { renderLightWorkspaceType } from "../workspace";
-
+// Attributes are marked as read-only to reflect the stateless nature of our Resource.
+// This design will be moved up to BaseResource once we transition away from Sequelize.
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unsafe-declaration-merging
 export interface MembershipInvitationResource
   extends ReadonlyAttributesType<MembershipInvitationModel> {}
@@ -21,7 +24,10 @@ export class MembershipInvitationResource extends BaseResource<MembershipInvitat
   static model: ModelStaticWorkspaceAware<MembershipInvitationModel> =
     MembershipInvitationModel;
 
-  readonly workspace: WorkspaceType;
+  static logger = logger.child({
+    module: MembershipInvitationResource.constructor.name,
+  });
+  readonly workspace: Workspace;
 
   constructor(
     model: ModelStaticWorkspaceAware<MembershipInvitationModel>,
@@ -29,7 +35,7 @@ export class MembershipInvitationResource extends BaseResource<MembershipInvitat
     { workspace }: { workspace: Workspace }
   ) {
     super(MembershipInvitationModel, blob);
-    this.workspace = renderLightWorkspaceType({ workspace });
+    this.workspace = workspace;
   }
 
   static async getPendingForEmail(
@@ -48,6 +54,89 @@ export class MembershipInvitationResource extends BaseResource<MembershipInvitat
           workspace: pendingInvitation.workspace,
         })
       : null;
+  }
+
+  static async getPendingForEmailAndWorkspace(
+    email: string,
+    workspaceId: number
+  ): Promise<MembershipInvitationResource | null> {
+    const invitation = await MembershipInvitationModel.findOne({
+      where: {
+        inviteEmail: email,
+        workspaceId,
+        status: "pending",
+      },
+      include: [Workspace],
+    });
+
+    return invitation
+      ? new MembershipInvitationResource(this.model, invitation.get(), {
+          workspace: invitation.workspace,
+        })
+      : null;
+  }
+
+  static async getPendingForToken(
+    inviteToken: string | string[] | undefined
+  ): Promise<Result<MembershipInvitationResource | null, AuthFlowError>> {
+    if (inviteToken && typeof inviteToken === "string") {
+      let decodedToken: { membershipInvitationId: number } | null = null;
+      try {
+        decodedToken = verify(
+          inviteToken,
+          config.getDustInviteTokenSecret()
+        ) as {
+          membershipInvitationId: number;
+        };
+      } catch (e) {
+        // Log the error and continue as we test `deodedToken` is not null below.
+        this.logger.error(
+          {
+            error: e,
+          },
+          "Error while verifying invite token"
+        );
+      }
+      if (!decodedToken) {
+        return new Err(
+          new AuthFlowError(
+            "invalid_invitation_token",
+            "The invite token is invalid, please ask your admin to resend an invitation."
+          )
+        );
+      }
+
+      const membershipInvite = await MembershipInvitationModel.findOne({
+        where: {
+          id: decodedToken.membershipInvitationId,
+          status: "pending",
+        },
+        include: [Workspace],
+      });
+      if (!membershipInvite) {
+        return new Err(
+          new AuthFlowError(
+            "invalid_invitation_token",
+            "The invite token is invalid, please ask your admin to resend an invitation."
+          )
+        );
+      }
+
+      return new Ok(
+        new MembershipInvitationResource(this.model, membershipInvite.get(), {
+          workspace: membershipInvite.workspace,
+        })
+      );
+    }
+
+    return new Ok(null);
+  }
+
+  async markAsConsumed(user: UserResource) {
+    return this.update({
+      status: "consumed",
+      invitedUserId: user.id,
+    });
   }
 
   delete(
