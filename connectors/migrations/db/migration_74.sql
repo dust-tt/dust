bbc728eedfb6cab5c70f5c32a21d656dd1c28a3f
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-05-21 12:11:49 +0200

    update migration

--- connectors/migrations/db/migration_74.sql
+++ connectors/migrations/db/migration_74.sql
@@ -1,2 +1,3 @@
 -- Migration created on May 21, 2025
 ALTER TABLE "public"."gong_configurations" ADD COLUMN "trackersEnabled" BOOLEAN NOT NULL DEFAULT false;
+UPDATE "public"."gong_configurations" SET "trackersEnabled" = "smartTrackersEnabled";
