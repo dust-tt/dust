695f179c46a403cd7fb3093951d15de43423db36
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-05-21 14:45:00 +0200

    use a rename instead of a create and drop

--- connectors/migrations/db/migration_74.sql
+++ connectors/migrations/db/migration_74.sql
@@ -1,3 +1,2 @@
 -- Migration created on May 21, 2025
-ALTER TABLE "public"."gong_configurations" ADD COLUMN "trackersEnabled" BOOLEAN NOT NULL DEFAULT false;
-UPDATE "public"."gong_configurations" SET "trackersEnabled" = "smartTrackersEnabled";
+ALTER TABLE "public"."gong_configurations" RENAME COLUMN "smartTrackersEnabled" TO "trackersEnabled";
--- connectors/migrations/db/migration_75.sql
+++
@@ -1,2 +0,0 @@
--- Migration created on May 21, 2025
-ALTER TABLE "public"."gong_configurations" DROP COLUMN "smartTrackersEnabled";
