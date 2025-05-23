5a4f03b8ccc6308a7fefb760a2e8b188e5c19923
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-05-23 17:13:31 +0200

    add migration script

---
+++ front/migrations/db/migration_workos_3.sql
@@ -0,0 +1,4 @@
+-- Migration created on May 23, 2025
+ALTER TABLE "public"."groups" ADD COLUMN IF NOT EXISTS "workOSGroupId" VARCHAR(255);
+ALTER TABLE "public"."groups" DROP COLUMN IF EXISTS "directoryId";
+CREATE UNIQUE INDEX "groups_workspace_id_work_o_s_group_id" ON "groups" ("workspaceId", "workOSGroupId");
