55d445eaedf91ddbc5fbf995e9773c1d68911839
Joshua Gisiger <joshuagisiger142@gmail.com>
2025-06-02 16:37:50 +0200

    feat(job-type-tracking): define new swr hook for patchUser

--- front/lib/swr/user.ts
+++ front/lib/swr/user.ts
@@ -1,8 +1,17 @@
 import type { Fetcher } from "swr";
 
-import { fetcher, useSWRWithDefaults } from "@app/lib/swr/swr";
-import type { GetUserResponseBody } from "@app/pages/api/user";
+import {
+  fetcher,
+  getErrorFromResponse,
+  useSWRWithDefaults,
+} from "@app/lib/swr/swr";
+import type {
+  GetUserResponseBody,
+  PostUserMetadataResponseBody,
+} from "@app/pages/api/user";
 import type { GetUserMetadataResponseBody } from "@app/pages/api/user/metadata/[key]";
+import { JobType } from "@app/types/job_type";
+import { useSendNotification } from "@dust-tt/sparkle";
 
 export function useUser() {
   const userFetcher: Fetcher<GetUserResponseBody> = fetcher;
@@ -41,4 +50,49 @@ export function useDeleteMetadata(prefix: string) {
   };
 
   return { deleteMetadata };
+}
+
+export function usePatchUser() {
+  const { mutateUser } = useUser();
+  const sendNotification = useSendNotification();
+
+  const patchUser = async (
+    firstName: string,
+    lastName: string,
+    jobType?: JobType
+  ) => {
+    const res = await fetch("/api/user", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        jobType,
+      }),
+    });
+
+    if (res.ok) {
+      sendNotification({
+        type: "success",
+        title: "Updated User",
+        description: `Successfully updated user ${firstName} ${lastName}.`,
+      });
+      await mutateUser();
+
+      return res.json();
+    } else {
+      const errorData = await getErrorFromResponse(res);
+      sendNotification({
+        type: "error",
+        title: "Error updating user",
+        description: `Error: ${errorData.message}`,
+      });
+
+      return null;
+    }
+  };
+
+  return { patchUser };
 }
