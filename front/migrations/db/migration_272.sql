33c52420e9b96e5ff5eb38129e38afe1a7b37f6f
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-02 15:36:01 +0200

    merge migration scripts

---
+++ front/migrations/db/migration_272.sql
@@ -0,0 +1,14 @@
+-- Migration created on Jun 2, 2025
+
+-- Migration created on May 22, 2025
+ALTER TABLE "public"."workspaces" ADD COLUMN "workOSOrganizationId" VARCHAR(255);
+CREATE UNIQUE INDEX "workspaces_work_o_s_organization_id" ON "workspaces" ("workOSOrganizationId");
+
+-- Migration created on May 23, 2025
+ALTER TABLE "public"."groups" ADD COLUMN IF NOT EXISTS "workOSGroupId" VARCHAR(255);
+CREATE UNIQUE INDEX "groups_workspace_id_work_o_s_group_id" ON "groups" ("workspaceId", "workOSGroupId");
+
+ALTER TABLE "public"."users" ADD COLUMN "workOSId" VARCHAR(255);
+
+-- Migration created on May 28, 2025
+CREATE UNIQUE INDEX CONCURRENTLY "users_work_o_s_id" ON "users" ("workOSId");
