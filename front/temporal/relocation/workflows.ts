7a03fe3d4edbd5347b6cfbdd26f480ca10d93604
Maxime Cattet <m@root.bar>
2025-06-03 11:59:22 +0200

    skip some apps during relocation

--- front/temporal/relocation/workflows.ts
+++ front/temporal/relocation/workflows.ts
@@ -20,6 +20,7 @@ import type {
 } from "@app/temporal/relocation/activities/types";
 import { RELOCATION_QUEUES_PER_REGION } from "@app/temporal/relocation/config";
 import type { ModelId } from "@app/types";
+import logger from "@app/logger/logger";
 
 const CHUNK_SIZE = 5000;
 const TEMPORAL_WORKFLOW_MAX_HISTORY_LENGTH = 10_000;
@@ -794,6 +795,14 @@ export async function workspaceRelocateAppsWorkflow({
     currentId = lastId;
 
     for (const dustAPIProjectId of dustAPIProjectIds) {
+      if (["11877", "11704", "18538"].includes(dustAPIProjectId)) {
+        logger.info(
+          { dustAPIProjectId, workspaceId },
+          "skipping apps relocation"
+        );
+        continue;
+      }
+
       const { dataPath } = await sourceRegionActivities.getApp({
         dustAPIProjectId,
         workspaceId,
