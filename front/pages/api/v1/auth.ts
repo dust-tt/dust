4035f181332f1fdf2f2b25d1249d596d7008c044
Thomas Draier <thomas@dust.tt>
2025-06-03 11:34:50 +0200

    Add endpoint to check auth

---
+++ front/pages/api/v1/auth.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { apiError } from "@app/logger/withlogging";
+import type { WithAPIErrorResponse } from "@app/types";
+
+export type MeResponseType = {
+  auth: "auth0" | "workos";
+};
+
+/**
+ * @ignoreswagger
+ * undocumented.
+ * TODO(workos) Remove the endpoint once migrated.
+ */
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<WithAPIErrorResponse<MeResponseType>>
+): Promise<void> {
+  switch (req.method) {
+    case "GET":
+      return res.status(200).json({ auth: "auth0" });
+
+    default:
+      return apiError(req, res, {
+        status_code: 405,
+        api_error: {
+          type: "method_not_supported_error",
+          message: "The method passed is not supported, GET is expected.",
+        },
+      });
+  }
+}
+
+export default handler;
