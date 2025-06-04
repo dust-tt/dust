f6ca007503794c29614a5ae737b900e7e5b9ed22
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-04 07:49:57 +0200

    rename sId resource name

--- front/lib/actions/mcp.ts
+++ front/lib/actions/mcp.ts
@@ -363,7 +363,7 @@ export class MCPActionType extends BaseAction {
     id: ModelId;
     workspaceId: ModelId;
   }): string {
-    return makeSId("action", {
+    return makeSId("mcp_action", {
       id,
       workspaceId,
     });
