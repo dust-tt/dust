1e6ee8a74bad485a246384c6601abf4703f7e977
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-04 22:56:43 +0200

    add system group deletion

--- front/poke/temporal/activities.ts
+++ front/poke/temporal/activities.ts
@@ -735,6 +735,13 @@ export async function deleteWorkspaceActivity({
     },
   });
 
+  const systemGroup = await GroupResource.internalFetchWorkspaceSystemGroup(
+    workspace.id
+  );
+  if (systemGroup) {
+    await systemGroup.delete(auth);
+  }
+
   hardDeleteLogger.info({ workspaceId }, "Deleting Workspace");
 
   await Workspace.destroy({
