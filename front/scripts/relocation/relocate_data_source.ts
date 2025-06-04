d37d82d6563ec7c4adc35fb4d0eb5bade4d8876f
Maxime Cattet <m@root.bar>
2025-06-04 10:21:25 +0200

    add workspace check

--- front/scripts/relocation/relocate_data_source.ts
+++ front/scripts/relocation/relocate_data_source.ts
@@ -6,6 +6,7 @@ import {
   SUPPORTED_REGIONS,
 } from "@app/lib/api/regions/config";
 import { Authenticator } from "@app/lib/auth";
+import { Workspace } from "@app/lib/models/workspace";
 import { DataSourceModel } from "@app/lib/resources/storage/models/data_source";
 import { makeScript } from "@app/scripts/helpers";
 import { RELOCATION_QUEUES_PER_REGION } from "@app/temporal/relocation/config";
@@ -62,16 +63,22 @@ makeScript(
       "Must run from source region"
     );
 
-    const dataSource = await DataSourceModel.findOne({
-      attributes: ["id", "dustAPIDataSourceId", "dustAPIProjectId"],
-      where: {
-        id: dataSourceId,
-      },
-      order: [["id", "ASC"]],
+    const dataSource = await DataSourceModel.findByPk(dataSourceId, {
+      include: [
+        {
+          model: Workspace,
+          required: true,
+        },
+      ],
     });
 
     if (!dataSource) {
       logger.error("DataSource not found");
+      return;
+    }
+
+    if (dataSource.workspace.sId !== workspaceId) {
+      logger.error("DataSource is not part of the workspaceId you gave");
       return;
     }
 
