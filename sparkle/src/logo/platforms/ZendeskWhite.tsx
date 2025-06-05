78397b8700da05f8fe6819be0a14455928c04c71
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2025-06-05 11:15:45 +0200

    521

--- sparkle/src/logo/platforms/ZendeskWhite.tsx
+++ sparkle/src/logo/platforms/ZendeskWhite.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from "react";
 import * as React from "react";
-const SvgZendesk = (props: SVGProps<SVGSVGElement>) => (
+const SvgZendeskWhite = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -10,9 +10,9 @@ const SvgZendesk = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill="#000"
+      fill="#fff"
       d="M13 4v12L23 4H13ZM6 9a5 5 0 0 1-5-5h10a5 5 0 0 1-5 5ZM18 15a5 5 0 0 1 5 5H13a5 5 0 0 1 5-5ZM11 8v12H1L11 8Z"
     />
   </svg>
 );
-export default SvgZendesk;
+export default SvgZendeskWhite;
