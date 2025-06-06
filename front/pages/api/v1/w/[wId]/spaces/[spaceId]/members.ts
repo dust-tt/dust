806f3cf6612567f41107bc8c662d149e0ecbb452
Joshua Gisiger <joshuagisiger142@gmail.com>
2025-06-06 16:28:46 +0200

    feat(partner-portal): create endpoint for member patching

---
+++ front/pages/api/v1/w/[wId]/spaces/[spaceId]/members.ts
@@ -0,0 +1,158 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { withPublicAPIAuthentication } from "@app/lib/api/auth_wrappers";
+import type { Authenticator } from "@app/lib/auth";
+import { SpaceResource } from "@app/lib/resources/space_resource";
+import { apiError } from "@app/logger/withlogging";
+import {
+  PatchSpaceMembersRequestBodySchema,
+  type SpaceType,
+  type WithAPIErrorResponse,
+} from "@app/types";
+import { isLeft } from "fp-ts/lib/Either";
+import * as reporter from "io-ts-reporters";
+import { DustError } from "@app/lib/error";
+
+interface PatchSpaceMembersResponseBody {
+  space: SpaceType;
+}
+
+/**
+ * @swagger
+ * /api/v1/w/{wId}/spaces:
+ *   get:
+ *     summary: List available spaces.
+ *     description: Retrieves a list of accessible spaces for the authenticated workspace.
+ *     tags:
+ *       - Spaces
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: wId
+ *         required: true
+ *         description: Unique string identifier for the workspace
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Spaces of the workspace
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 spaces:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Space'
+ *       400:
+ *         description: Bad Request. Missing or invalid parameters.
+ *       401:
+ *         description: Unauthorized. Invalid or missing authentication token.
+ *       404:
+ *         description: Workspace not found.
+ *       405:
+ *         description: Method not supported.
+ *       500:
+ *         description: Internal Server Error.
+ */
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<WithAPIErrorResponse<PatchSpaceMembersResponseBody>>,
+  auth: Authenticator
+): Promise<void> {
+  const { spaceId } = req.query;
+
+  if (typeof spaceId !== "string" || !spaceId) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "space_not_found",
+        message: "The space was not found.",
+      },
+    });
+  }
+
+  const space = await SpaceResource.fetchById(auth, spaceId);
+  if (!space) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "space_not_found",
+        message: "The space was not found.",
+      },
+    });
+  }
+
+  switch (req.method) {
+    case "PATCH": {
+      if (!space.canAdministrate(auth)) {
+        return apiError(req, res, {
+          status_code: 403,
+          api_error: {
+            type: "workspace_auth_error",
+            message:
+              "Only users that are `admins` can administrate space members.",
+          },
+        });
+      }
+
+      const bodyValidation = PatchSpaceMembersRequestBodySchema.decode(
+        req.body
+      );
+
+      if (isLeft(bodyValidation)) {
+        const pathError = reporter.formatValidationErrors(bodyValidation.left);
+
+        return apiError(req, res, {
+          status_code: 400,
+          api_error: {
+            type: "invalid_request_error",
+            message: `Invalid request body: ${pathError}`,
+          },
+        });
+      }
+
+      const updateRes = await space.updatePermissions(
+        auth,
+        bodyValidation.right
+      );
+      if (updateRes.isErr()) {
+        if (
+          updateRes.error instanceof DustError &&
+          updateRes.error.code === "unauthorized"
+        ) {
+          return apiError(req, res, {
+            status_code: 403,
+            api_error: {
+              type: "workspace_auth_error",
+              message:
+                "Only users that are `admins` can administrate space members.",
+            },
+          });
+        } else {
+          return apiError(req, res, {
+            status_code: 400,
+            api_error: {
+              type: "invalid_request_error",
+              message: updateRes.error.message,
+            },
+          });
+        }
+      }
+
+      return res.status(200).json({ space: space.toJSON() });
+    }
+    default:
+      return apiError(req, res, {
+        status_code: 405,
+        api_error: {
+          type: "method_not_supported_error",
+          message: "The method passed is not supported, GET is expected.",
+        },
+      });
+  }
+}
+
+export default withPublicAPIAuthentication(handler);
