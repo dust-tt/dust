af30849ae9338b9611db1302d426da236b70712b
Maxime Cattet <m@root.bar>
2025-06-06 16:04:11 +0200

    add last non typed pageProps

--- front/pages/w/[wId]/assistant/[cId]/index.tsx
+++ front/pages/w/[wId]/assistant/[cId]/index.tsx
@@ -118,7 +118,7 @@ export default function AssistantConversation({
 
 AssistantConversation.getLayout = (
   page: React.ReactElement,
-  pageProps: any
+  pageProps: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
   return (
     <AppRootLayout>
