95f387a53acc24f8a2ce9875d861a04774e64d09
Joshua Gisiger <joshuagisiger142@gmail.com>
2025-06-09 13:37:55 +0200

    chore: remove old migration script

--- front/migrations/db/migration_277.sql
+++
@@ -1,6 +0,0 @@
--- Migration created on Jun 9, 2025 
-ALTER TABLE "public"."keys" 
-ADD COLUMN "role" VARCHAR(255) DEFAULT 'builder';
-
--- Update existing system keys to have admin role
-UPDATE "keys" SET "role" = 'admin' WHERE "isSystem" = true;
