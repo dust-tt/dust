2f436935bdc1fc856f613f5756323a23b67b3743
Daphné Popin <PopDaph@users.noreply.github.com>
2025-06-12 14:46:59 +0200

    Refactor search action to extract shared functions (#13383)

--- front/lib/actions/mcp_internal_actions/servers/index.ts
+++ front/lib/actions/mcp_internal_actions/servers/index.ts
@@ -18,7 +18,7 @@ import { default as reasoningServer } from "@app/lib/actions/mcp_internal_action
 import { default as runAgentServer } from "@app/lib/actions/mcp_internal_actions/servers/run_agent";
 import { default as dustAppServer } from "@app/lib/actions/mcp_internal_actions/servers/run_dust_app";
 import { default as salesforceServer } from "@app/lib/actions/mcp_internal_actions/servers/salesforce";
-import { default as searchServer } from "@app/lib/actions/mcp_internal_actions/servers/search";
+import { default as searchServer } from "@app/lib/actions/mcp_internal_actions/servers/search/search";
 import { default as tablesQueryServer } from "@app/lib/actions/mcp_internal_actions/servers/tables_query/server";
 import { default as tablesQueryServerV2 } from "@app/lib/actions/mcp_internal_actions/servers/tables_query/server_v2";
 import { default as thinkServer } from "@app/lib/actions/mcp_internal_actions/servers/think";
