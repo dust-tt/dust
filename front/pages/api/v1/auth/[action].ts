5b630edd267dfa0e43e602ffd5d5a711883a402b
Adrien Simon <adrisim2001@gmail.com>
2025-06-16 10:29:46 +0200

    [front] provisioning: add authentication endpoints in api v1

---
+++ front/pages/api/v1/auth/[action].ts
@@ -0,0 +1,79 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import config from "@app/lib/api/config";
+
+type Provider = {
+  authorizeUri: string;
+  authenticateUri: string;
+  logoutUri: string;
+};
+
+const providers: Record<string, Provider> = {
+  workos: {
+    authorizeUri: "api.workos.com/user_management/authorize",
+    authenticateUri: "api.workos.com/user_management/authenticate",
+    logoutUri: "api/workos/com/user_management/sessions/logout",
+  },
+  auth0: {
+    authorizeUri: "dust-tt.us.auth0.com/authorize",
+    authenticateUri: "dust-tt.us.auth0.com/oauth/token",
+    logoutUri: "dust-tt.us.auth0.com/v2/logout",
+  },
+};
+
+function getProvider(): Provider {
+  const provider = config.getOAuthProvider();
+  return providers[provider];
+}
+
+/**
+ * @ignoreswagger
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { action } = req.query;
+
+  switch (action) {
+    case "authorize":
+      return handleAuthorize(req, res);
+    case "authenticate":
+      return handleAuthenticate(req, res);
+    case "logout":
+      return handleLogout(req, res);
+    default:
+      res.status(404).json({ error: "Action not found" });
+  }
+}
+
+async function handleAuthorize(req: NextApiRequest, res: NextApiResponse) {
+  const { query } = req;
+  const authorizeUrl = `https://${getProvider().authorizeUri}?${query}`;
+  res.redirect(authorizeUrl);
+}
+
+async function handleAuthenticate(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const response = await fetch(`https://${getProvider().authenticateUri}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Origin: req.headers.origin || "",
+      },
+      credentials: "include",
+      body: new URLSearchParams(req.body).toString(),
+    });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (error) {
+    console.error("Error in authenticate proxy:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function handleLogout(req: NextApiRequest, res: NextApiResponse) {
+  const { query } = req;
+  const authorizeUrl = `https://${getProvider().logoutUri}?${query}`;
+  res.redirect(authorizeUrl);
+}
