d0a190ac28093d0f929d127e84396a707c94b7e5
Alban Dumouilla <alban@dust.tt>
2025-06-16 14:40:26 +0200

    lint

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -58,7 +58,9 @@ export const getServerSideProps = makeGetServerSidePropsRequirementsWrapper({
 
     // This allows linking to the assistant template creation page from external sources.
     if (context.query.goto === "template" && context.query.templateId) {
-      url = url + `/builder/assistants/create?templateId=${context.query.templateId}`;
+      url =
+        url +
+        `/builder/assistants/create?templateId=${context.query.templateId}`;
     }
 
     if (context.query.inviteToken) {
