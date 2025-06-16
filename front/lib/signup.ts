c82f83f1561c0c02486162706bfa68cb72b62110
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-16 16:43:57 +0200

    refactor(signup): streamline signUpUrl logic and return early

--- front/lib/signup.ts
+++ front/lib/signup.ts
@@ -7,17 +7,17 @@ export function getSignUpUrl({
   invitationEmail?: string;
   workOSEnabled?: boolean;
 }) {
-  let signUpUrl;
   if (workOSEnabled) {
-    signUpUrl = `/api/workos/login?returnTo=${signupCallbackUrl}&screenHint=sign-up`;
+    let signUpUrl = `/api/workos/login?returnTo=${signupCallbackUrl}&screenHint=sign-up`;
     if (invitationEmail) {
       signUpUrl += `&loginHint=${encodeURIComponent(invitationEmail)}`;
     }
-  } else {
-    signUpUrl = `/api/auth/login?returnTo=${signupCallbackUrl}&prompt=login&screen_hint=signup`;
-    if (invitationEmail) {
-      signUpUrl += `&login_hint=${encodeURIComponent(invitationEmail)}`;
-    }
+    return signUpUrl;
+  }
+
+  let signUpUrl = `/api/auth/login?returnTo=${signupCallbackUrl}&prompt=login&screen_hint=signup`;
+  if (invitationEmail) {
+    signUpUrl += `&login_hint=${encodeURIComponent(invitationEmail)}`;
   }
 
   return signUpUrl;
