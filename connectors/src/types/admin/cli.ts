a5ee991824579b514973bf9107239fea92bfdc24
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-17 14:07:39 +0200

    same in connectors

--- connectors/src/types/admin/cli.ts
+++ connectors/src/types/admin/cli.ts
@@ -424,6 +424,82 @@ export type NotionMeResponseType = t.TypeOf<typeof NotionMeResponseSchema>;
  */
 
 /**
+ * <Salesforce>
+ */
+export const SalesforceCommandSchema = t.type({
+  majorCommand: t.literal("salesforce"),
+  command: t.union([
+    t.literal("check-connection"),
+    t.literal("run-soql"),
+    t.literal("setup-synced-query"),
+    t.literal("sync-query"),
+  ]),
+  args: t.type({
+    wId: t.union([t.string, t.undefined]),
+    dsId: t.union([t.string, t.undefined]),
+    soql: t.union([t.string, t.undefined]),
+    limit: t.union([t.number, t.undefined]),
+    lastModifiedDateOrder: t.union([
+      t.literal("ASC"),
+      t.literal("DESC"),
+      t.undefined,
+    ]),
+    offset: t.union([t.number, t.undefined]),
+    rootNodeName: t.union([t.string, t.undefined]),
+    titleTemplate: t.union([t.string, t.undefined]),
+    contentTemplate: t.union([t.string, t.undefined]),
+    tagsTemplate: t.union([t.string, t.undefined]),
+    execute: t.union([t.boolean, t.undefined]),
+    queryId: t.union([t.number, t.undefined]),
+    full: t.union([t.boolean, t.undefined]),
+  }),
+});
+export type SalesforceCommandType = t.TypeOf<typeof SalesforceCommandSchema>;
+
+export const SalesforceCheckConnectionResponseSchema = t.type({
+  ok: t.boolean,
+});
+export type SalesforceCheckConnectionResponseType = t.TypeOf<
+  typeof SalesforceCheckConnectionResponseSchema
+>;
+
+export const SalesforceRunSoqlResponseSchema = t.type({
+  records: t.array(t.UnknownRecord), // Salesforce type, can't be iots'd
+  totalSize: t.number,
+  done: t.boolean,
+});
+export type SalesforceRunSoqlResponseType = t.TypeOf<
+  typeof SalesforceRunSoqlResponseSchema
+>;
+
+export const SalesforceSetupSyncedQueryResponseSchema = t.type({
+  documents: t.array(
+    t.type({
+      id: t.string,
+      lastModifiedDate: t.string,
+      title: t.string,
+      content: t.string,
+      tags: t.array(t.string),
+    })
+  ),
+  queryId: t.union([t.number, t.null]),
+  created: t.boolean,
+});
+export type SalesforceSetupSyncedQueryResponseType = t.TypeOf<
+  typeof SalesforceSetupSyncedQueryResponseSchema
+>;
+
+export const SalesforceSyncQueryResponseSchema = t.type({
+  workflowId: t.string,
+});
+export type SalesforceSyncQueryResponseType = t.TypeOf<
+  typeof SalesforceSyncQueryResponseSchema
+>;
+/**
+ * </Salesforce>
+ */
+
+/**
  * <Slack>
  */
 export const SlackCommandSchema = t.type({
@@ -613,83 +689,6 @@ export type ZendeskFetchBrandResponseType = t.TypeOf<
  */
 
 /**
- * <Salesforce>
- */
-export const SalesforceCommandSchema = t.type({
-  majorCommand: t.literal("salesforce"),
-  command: t.union([
-    t.literal("check-connection"),
-    t.literal("run-soql"),
-    t.literal("setup-synced-query"),
-    t.literal("sync-query"),
-  ]),
-  args: t.type({
-    wId: t.union([t.string, t.undefined]),
-    dsId: t.union([t.string, t.undefined]),
-    soql: t.union([t.string, t.undefined]),
-    limit: t.union([t.number, t.undefined]),
-    lastModifiedDateOrder: t.union([
-      t.literal("ASC"),
-      t.literal("DESC"),
-      t.undefined,
-    ]),
-    offset: t.union([t.number, t.undefined]),
-    rootNodeName: t.union([t.string, t.undefined]),
-    titleTemplate: t.union([t.string, t.undefined]),
-    contentTemplate: t.union([t.string, t.undefined]),
-    tagsTemplate: t.union([t.string, t.undefined]),
-    execute: t.union([t.boolean, t.undefined]),
-    queryId: t.union([t.number, t.undefined]),
-    full: t.union([t.boolean, t.undefined]),
-  }),
-});
-export type SalesforceCommandType = t.TypeOf<typeof SalesforceCommandSchema>;
-
-export const SalesforceCheckConnectionResponseSchema = t.type({
-  ok: t.boolean,
-});
-export type SalesforceCheckConnectionResponseType = t.TypeOf<
-  typeof SalesforceCheckConnectionResponseSchema
->;
-
-export const SalesforceRunSoqlResponseSchema = t.type({
-  records: t.array(t.UnknownRecord), // Salesforce type, can't be iots'd
-  totalSize: t.number,
-  done: t.boolean,
-});
-export type SalesforceRunSoqlResponseType = t.TypeOf<
-  typeof SalesforceRunSoqlResponseSchema
->;
-
-export const SalesforceSetupSyncedQueryResponseSchema = t.type({
-  documents: t.array(
-    t.type({
-      id: t.string,
-      lastModifiedDate: t.string,
-      title: t.string,
-      content: t.string,
-      tags: t.array(t.string),
-    })
-  ),
-  queryId: t.union([t.number, t.null]),
-  created: t.boolean,
-});
-export type SalesforceSetupSyncedQueryResponseType = t.TypeOf<
-  typeof SalesforceSetupSyncedQueryResponseSchema
->;
-
-export const SalesforceSyncQueryResponseSchema = t.type({
-  workflowId: t.string,
-});
-export type SalesforceSyncQueryResponseType = t.TypeOf<
-  typeof SalesforceSyncQueryResponseSchema
->;
-
-/**
- * </Salesforce>
- */
-
-/**
  * <Admin>
  */
 export const AdminCommandSchema = t.union([
@@ -702,12 +701,12 @@ export const AdminCommandSchema = t.union([
   IntercomCommandSchema,
   MicrosoftCommandSchema,
   NotionCommandSchema,
+  SalesforceCommandSchema,
   SlackCommandSchema,
   SnowflakeCommandSchema,
   TemporalCommandSchema,
   WebcrawlerCommandSchema,
   ZendeskCommandSchema,
-  SalesforceCommandSchema,
 ]);
 export type AdminCommandType = t.TypeOf<typeof AdminCommandSchema>;
 
@@ -722,8 +721,8 @@ export const AdminResponseSchema = t.union([
   AdminSuccessResponseSchema,
   BatchAllResponseSchema,
   CheckFileGenericResponseSchema,
-  ConfluenceMeResponseSchema,
   ConfluenceCheckSpaceAccessResponseSchema,
+  ConfluenceMeResponseSchema,
   ConfluenceResolveSpaceFromUrlResponseSchema,
   ConfluenceUpsertPageResponseSchema,
   GongForceResyncResponseSchema,
@@ -738,6 +737,10 @@ export const AdminResponseSchema = t.union([
   NotionMeResponseSchema,
   NotionSearchPagesResponseSchema,
   NotionUpsertResponseSchema,
+  SalesforceCheckConnectionResponseSchema,
+  SalesforceRunSoqlResponseSchema,
+  SalesforceSetupSyncedQueryResponseSchema,
+  SalesforceSyncQueryResponseSchema,
   SnowflakeFetchDatabaseResponseSchema,
   SnowflakeFetchSchemaResponseSchema,
   SnowflakeFetchTableResponseSchema,
@@ -747,10 +750,6 @@ export const AdminResponseSchema = t.union([
   ZendeskCountTicketsResponseSchema,
   ZendeskFetchBrandResponseSchema,
   ZendeskFetchTicketResponseSchema,
-  SalesforceCheckConnectionResponseSchema,
-  SalesforceRunSoqlResponseSchema,
-  SalesforceSetupSyncedQueryResponseSchema,
-  SalesforceSyncQueryResponseSchema,
 ]);
 export type AdminResponseType = t.TypeOf<typeof AdminResponseSchema>;
 /**
