83cc146734202c043fe5ab3c69dfd74b3727b31b
ykmsd <yuka@dust.tt>
2025-06-19 15:18:03 +0200

    fix comment

--- sparkle/src/components/Button.tsx
+++ sparkle/src/components/Button.tsx
@@ -307,7 +307,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     }, [isLoading, props.disabled]);
 
     // We cannot skip a button tag when it's disabled. We need
-    // to disabled class manually for links + disable pointer events.
+    // to apply disabled class manually for links + disable pointer events.
     const shouldUseSlot = !!href && !props.disabled;
 
     const innerContent = shouldUseSlot ? <span>{content}</span> : content;
