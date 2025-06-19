12c5d0524827a142f057bd2e7ed69b39b280b4f9
Maxime Cattet <m@root.bar>
2025-06-19 17:19:10 +0200

    bump varchar size

--- connectors/migrations/db/migration_80.sql
+++ connectors/migrations/db/migration_80.sql
@@ -1,2 +1,2 @@
 -- Migration created on Jun 19, 2025
-ALTER TABLE "public"."webcrawler_configurations" ADD COLUMN "crawlId" VARCHAR(36) DEFAULT NULL;
+ALTER TABLE "public"."webcrawler_configurations" ADD COLUMN "crawlId" VARCHAR(64) DEFAULT NULL;
