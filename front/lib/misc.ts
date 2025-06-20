8a3b821ab7259d2428048d20cc23d7177e2640e0
Sebastien Flory <sflory@gmail.com>
2025-06-20 17:24:06 +0200

    Add: support to use fixed ip proxy when FF activated

---
+++ front/lib/misc.ts
@@ -0,0 +1,7 @@
+import type { LightWorkspaceType } from "@dust-tt/client";
+import { hash as blake3 } from "blake3";
+
+// Check this slack thread for more details: https://dust4ai.slack.com/archives/C050SM8NSPK/p1750433286397399.
+export const isWorkspaceUsingStaticIP = (workspace: LightWorkspaceType) =>
+  blake3(workspace.sId).toString("hex") ===
+  "5e6faccb8b7e2be7cab0a46a1376126b75032acbfbd5c723d9b0f0b38a73b334";
