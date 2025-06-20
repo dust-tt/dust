aaf76d4814c844fc56f544c5bc580595a1320c51
ykmsd <yuka@dust.tt>
2025-06-20 20:45:59 +0200

    cleanup

--- sparkle/src/components/Button.tsx
+++ sparkle/src/components/Button.tsx
@@ -1,7 +1,6 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
-import { useMemo } from "react";
 
 import {
   Counter,
