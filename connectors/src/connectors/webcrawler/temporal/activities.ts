62ab6df5570ab5e08efe917e11daaa05581b486a
Maxime Cattet <m@root.bar>
2025-06-20 09:54:26 +0200

    [connectors] chore(webcrawler): Set connector in error if firecrawl failed

--- connectors/src/connectors/webcrawler/temporal/activities.ts
+++ connectors/src/connectors/webcrawler/temporal/activities.ts
@@ -131,6 +131,7 @@ export async function crawlWebsiteByConnectorId(connectorId: ModelId) {
   });
 
   if (!crawlerResponse.success) {
+    await syncFailed(connectorId, "webcrawling_error");
     throw new Error(crawlerResponse.error);
   }
 
