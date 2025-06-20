8566529c018c2743f1f0f1cee89cb27b030690c2
filou <philipperolet@gmail.com>
2025-06-20 16:21:04 +0200

    add users:read

--- front/lib/api/oauth/providers/slack_bot.ts
+++ front/lib/api/oauth/providers/slack_bot.ts
@@ -32,6 +32,7 @@ export class SlackBotOAuthProvider implements BaseOAuthStrategyProvider {
       "mpim:read",
       "team:read",
       "im:read",
+      "users:read",
     ];
     return (
       `https://slack.com/oauth/v2/authorize?` +
