6306670832aa2f23a2003617e1388e85cfb52600
Maxime Cattet <m@root.bar>
2025-06-23 10:15:13 +0200

    [front] chore: Add react scan (#13635)
    * [front] chore: Add react scan
    
    * remove console log

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -1,10 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+const { NODE_ENV, REACT_SCAN } = process.env;
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          {NODE_ENV === "development" && REACT_SCAN === "true" && (
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script src="https://unpkg.com/react-scan/dist/auto.global.js" />
+          )}
           <base href={process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
