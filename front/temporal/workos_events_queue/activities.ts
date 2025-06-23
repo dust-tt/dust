d26d17faa2b478513f1836e0842a1657b8d5113b
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-06-23 10:34:15 +0200

    [front] fix(provisioning): add user to organization during sync (#13650)

--- front/temporal/workos_events_queue/activities.ts
+++ front/temporal/workos_events_queue/activities.ts
@@ -446,7 +446,10 @@ async function handleUserAddedToGroup(
     return;
   }
 
-  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail(event.user);
+  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail({
+    workspace,
+    workOSUser: event.user,
+  });
   if (workOSUserRes.isErr()) {
     throw workOSUserRes.error;
   }
@@ -490,7 +493,10 @@ async function handleUserRemovedFromGroup(
     return;
   }
 
-  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail(event.user);
+  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail({
+    workspace,
+    workOSUser: event.user,
+  });
   if (workOSUserRes.isErr()) {
     throw workOSUserRes.error;
   }
@@ -527,7 +533,10 @@ async function handleCreateOrUpdateWorkOSUser(
   workspace: LightWorkspaceType,
   event: DirectoryUser
 ) {
-  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail(event);
+  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail({
+    workspace,
+    workOSUser: event,
+  });
   if (workOSUserRes.isErr()) {
     throw workOSUserRes.error;
   }
@@ -572,7 +581,10 @@ async function handleDeleteWorkOSUser(
   workspace: LightWorkspaceType,
   event: DirectoryUser
 ) {
-  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail(event);
+  const workOSUserRes = await fetchOrCreateWorkOSUserWithEmail({
+    workspace,
+    workOSUser: event,
+  });
   if (workOSUserRes.isErr()) {
     throw workOSUserRes.error;
   }
@@ -581,7 +593,7 @@ async function handleDeleteWorkOSUser(
   const user = await UserResource.fetchByWorkOSUserId(workOSUser.id);
   if (!user) {
     throw new Error(
-      `Didn't found user to delete for workOSUserId "${workOSUser.id}" in workspace "${workspace.sId}"`
+      `Did not find user to delete for workOSUserId "${workOSUser.id}" in workspace "${workspace.sId}"`
     );
   }
 
