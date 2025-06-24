d741b96cb9476f34b110c418c69e8dc80f3102b0
Henry Fontanier <henry@dust.tt>
2025-06-24 16:17:27 +0200

    enh: add connector ID to CONNECTOR_IDS_USING_GCS_CODE_SYNC

--- connectors/src/connectors/github/temporal/workflows.ts
+++ connectors/src/connectors/github/temporal/workflows.ts
@@ -80,7 +80,7 @@ const MAX_CONCURRENT_ISSUE_SYNC_ACTIVITIES_PER_WORKFLOW = 8;
 
 const FILE_CHUNK_SIZE = 50;
 
-const CONNECTOR_IDS_USING_GCS_CODE_SYNC: number[] = [15];
+const CONNECTOR_IDS_USING_GCS_CODE_SYNC: number[] = [15, 8714];
 
 /**
  * This workflow is used to fetch and sync all the repositories of a GitHub connector.
