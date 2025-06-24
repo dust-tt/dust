7a520bddad23bf7cd30d2dc4fdd986d19c413160
Flavien David <flavien.david74@gmail.com>
2025-06-24 11:33:11 +0200

    Add Poke Plugin to whitelist Slack workflow/bot (#13687)
    * Add Poke Plugin to whitelist Slack workflow/bot
    
    * 👕
    
    * ✂️

--- front/lib/api/poke/plugins/data_sources/index.ts
+++ front/lib/api/poke/plugins/data_sources/index.ts
@@ -5,5 +5,6 @@ export * from "./notion_unstuck_syncing_nodes";
 export * from "./notion_update_orphaned_resources_parents";
 export * from "./notion_url_sync";
 export * from "./operations";
+export * from "./slack_whitelist_bot";
 export * from "./toggle_restricted_space_agent_slack_access";
 export * from "./webcrawler_frequency";
