4b8e0fb4907a682b5aa4e2913e8621bf15f3f55c
Flavien David <flavien@dust.tt>
2025-06-24 16:57:56 +0200

    Process directories upsert with concurrency

--- connectors/src/connectors/github/temporal/workflows.ts
+++ connectors/src/connectors/github/temporal/workflows.ts
@@ -79,6 +79,7 @@ const MAX_CONCURRENT_REPO_SYNC_WORKFLOWS = 3;
 const MAX_CONCURRENT_ISSUE_SYNC_ACTIVITIES_PER_WORKFLOW = 8;
 
 const FILE_CHUNK_SIZE = 50;
+const DIRECTORY_CHUNK_SIZE = 20;
 
 const CONNECTOR_IDS_USING_GCS_CODE_SYNC: number[] = [15];
 
@@ -599,24 +600,24 @@ export async function githubCodeSyncStatelessWorkflow({
     }
   }
 
-  // 2. Process directories depth-first (parents before children) with updated directory info.
+  // 2. Process directories.
+  const directoryChunkPromises = [];
   for (const dirBatch of directoryBatches) {
-    const CHUNK_SIZE = 20;
-
-    for (let i = 0; i < dirBatch.directories.length; i += CHUNK_SIZE) {
-      const directoryChunk = dirBatch.directories.slice(i, i + CHUNK_SIZE);
-
-      await githubProcessDirectoryChunkActivity({
-        codeSyncStartedAtMs,
-        connectorId,
-        dataSourceConfig,
-        defaultBranch: extractResult.repoInfo.default_branch,
-        directories: directoryChunk,
-        repoId,
-        repoLogin,
-        repoName,
-        updatedDirectoryIds: allUpdatedDirectoryIds,
-      });
+    const chunks = chunk(dirBatch.directories, DIRECTORY_CHUNK_SIZE);
+    for (const directoryChunk of chunks) {
+      directoryChunkPromises.push(
+        githubProcessDirectoryChunkActivity({
+          codeSyncStartedAtMs,
+          connectorId,
+          dataSourceConfig,
+          defaultBranch: extractResult.repoInfo.default_branch,
+          directories: directoryChunk,
+          repoId,
+          repoLogin,
+          repoName,
+          updatedDirectoryIds: allUpdatedDirectoryIds,
+        })
+      );
     }
   }
 
