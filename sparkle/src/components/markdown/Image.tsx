9b6dea881d0a07944d546b3eff5fdc84ad892b98
Joshua Gisiger <joshuagisiger142@gmail.com>
2025-06-26 15:07:31 +0200

    fix: properly add internal link

--- sparkle/src/components/markdown/Image.tsx
+++ sparkle/src/components/markdown/Image.tsx
@@ -16,7 +16,7 @@ export function Img({
 
 function isAllowedImageSrc(src: string): boolean {
   // allow internal paths
-  if (src.startsWith("/")) return true;
+  if (src.startsWith("https://dust.tt")) return true;
 
   // TODO: allow for internal links, but first have to figure out how to use internal links
   // block everything else
