96461a2b05f9bbcbf6e14c19e29b6fd04b62c972
Sebastien Flory <seb@dust.tt>
2025-06-27 16:06:58 +0200

    Handle team id correctly (#13867)
    * Handle team id correctly
    
    * Split in 2 functions

--- front/lib/swr/oauth.ts
+++ front/lib/swr/oauth.ts
@@ -1,10 +1,17 @@
-import type { OAuthProvider } from "@app/types";
+import type {
+  APIError,
+  OAuthConnectionType,
+  OAuthProvider,
+  Result,
+  WithAPIErrorResponse,
+} from "@app/types";
+import { Err, isAPIErrorResponse, Ok } from "@app/types";
 
 export const useFinalize = () => {
   const doFinalize = async (
     provider: OAuthProvider,
     queryParams: Record<string, string | string[] | undefined>
-  ) => {
+  ): Promise<Result<OAuthConnectionType, APIError>> => {
     const params = new URLSearchParams();
     for (const [key, value] of Object.entries(queryParams)) {
       if (Array.isArray(value)) {
@@ -14,7 +21,25 @@ export const useFinalize = () => {
       }
     }
 
-    return fetch(`/api/oauth/${provider}/finalize?${params.toString()}`);
+    const res = await fetch(
+      `/api/oauth/${provider}/finalize?${params.toString()}`
+    );
+
+    const result: WithAPIErrorResponse<{ connection: OAuthConnectionType }> =
+      await res.json();
+
+    if (isAPIErrorResponse(result)) {
+      return new Err(result.error);
+    }
+
+    if (!res.ok) {
+      return new Err({
+        type: "internal_server_error",
+        message: `Failed to finalize OAuth: ${res.statusText}`,
+      });
+    }
+
+    return new Ok(result.connection);
   };
 
   return doFinalize;
