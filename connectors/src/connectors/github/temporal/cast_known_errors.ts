bb7bfe0d28ea9559c7800b06231e37ab10763422
Thomas Draier <thomas@dust.tt>
2025-06-30 10:45:47 +0200

    Handle WithRetriesError (#13889)

--- connectors/src/connectors/github/temporal/cast_known_errors.ts
+++ connectors/src/connectors/github/temporal/cast_known_errors.ts
@@ -6,6 +6,20 @@ import type {
 import { RequestError } from "octokit";
 
 import { ProviderWorkflowError } from "@connectors/lib/error";
+import { WithRetriesError } from "@connectors/types";
+
+const checkRateLimitError = (err: unknown) => {
+  if (err instanceof RequestError) {
+    if (err.status === 403 && err.name === "HttpError") {
+      throw new ProviderWorkflowError(
+        "github",
+        "403 - Rate Limit Error",
+        "rate_limit_error",
+        err
+      );
+    }
+  }
+};
 
 export class GithubCastKnownErrorsInterceptor
   implements ActivityInboundCallsInterceptor
@@ -17,17 +31,12 @@ export class GithubCastKnownErrorsInterceptor
     try {
       return await next(input);
     } catch (err: unknown) {
-      if (err instanceof RequestError) {
-        if (err.status === 403 && err.name === "HttpError") {
-          throw new ProviderWorkflowError(
-            "github",
-            "403 - Rate Limit Error",
-            "rate_limit_error",
-            err
-          );
-        }
+      checkRateLimitError(err);
+      if (err instanceof WithRetriesError) {
+        err.errors.forEach((error) => {
+          checkRateLimitError(error);
+        });
       }
-
       throw err;
     }
   }
