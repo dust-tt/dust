c35a2086256b3552e06894b70bd9221d82d9e4e4
David Ebbo <david@dust.tt>
2025-07-01 14:38:54 +0200

    Add cursor / batch size params

--- core/bin/migrations/20250701_backfill_tables_gcs_path.rs
+++ core/bin/migrations/20250701_backfill_tables_gcs_path.rs
@@ -1,8 +1,24 @@
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use clap::Parser;
 use dust::{
     databases::table::get_table_unique_id,
     project::Project,
     stores::{postgres::PostgresStore, store::Store},
 };
+use tokio_postgres::NoTls;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, help = "Skip confirmation")]
+    skip_confirmation: bool,
+
+    #[arg(long, help = "The cursor to start from", default_value = "0")]
+    start_cursor: i64,
+
+    #[arg(long, help = "The batch size", default_value = "100")]
+    batch_size: usize,
+}
 
 /*
  * Backfills the column text_size of the Elasticsearch index behind the alias core_data_sources_nodes.
@@ -13,23 +29,18 @@ use dust::{
  */
 #[tokio::main]
 async fn main() {
-    println!("hello");
-
     if let Err(e) = run().await {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
 }
 
-async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let db_uri = std::env::var("CORE_DATABASE_URI").expect("CORE_DATABASE_URI must be set");
-
-    let store = PostgresStore::new(&db_uri).await?;
-    let pool = store.raw_pool();
+async fn process_tables(
+    pool: &Pool<PostgresConnectionManager<NoTls>>,
+    id_cursor: i64,
+    batch_size: i64,
+) -> Result<(), Box<dyn std::error::Error>> {
     let c = pool.get().await?;
-
-    let id_cursor: i64 = 18;
-    let batch_size: i64 = 2;
 
     let rows = c
         .query(
@@ -65,6 +76,29 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("Unique table id: {}", unique_id);
     }
+
+    Ok(())
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    // parse args and env vars
+    let args = Args::parse();
+    let start_cursor = args.start_cursor;
+    let batch_size = args.batch_size;
+
+    let db_uri = std::env::var("CORE_DATABASE_URI").expect("CORE_DATABASE_URI must be set");
+
+    let store = PostgresStore::new(&db_uri).await?;
+    let pool = store.raw_pool();
+
+    // grab last id in data_sources_nodes
+    let c = pool.get().await?;
+    let last_id = c.query_one("SELECT MAX(id) FROM tables", &[]).await?;
+
+    let last_id: i64 = last_id.get(0);
+    println!("Last id in tables: {}", last_id);
+
+    process_tables(&pool, start_cursor, batch_size as i64).await?;
 
     Ok(())
 }
