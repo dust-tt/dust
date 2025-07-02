0c32a82ce73ef5bcd17bca3dc2a9e3a3c4a571c1
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-02 14:23:46 +0200

    add drop not null in migration file

--- front/migrations/db/migration_292.sql
+++ front/migrations/db/migration_292.sql
@@ -1,2 +1,5 @@
 -- Migration created on Jul 02, 2025
-ALTER TABLE "public"."agent_tables_query_configuration_tables" DROP COLUMN "tablesQueryConfigurationId";
+ALTER TABLE "public"."agent_tables_query_configuration_tables"
+    DROP COLUMN "tablesQueryConfigurationId";
+ALTER TABLE "public"."agent_tables_query_configuration_tables"
+    ALTER COLUMN "mcpServerConfigurationId" DROP NOT NULL;
