afaecf11f5098786ecf2b58ea0ab46c5f5d68a6a
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-02 14:25:51 +0200

    add drop indexes in migration script

--- front/migrations/db/migration_292.sql
+++ front/migrations/db/migration_292.sql
@@ -1,5 +1,5 @@
 -- Migration created on Jul 02, 2025
-ALTER TABLE "public"."agent_tables_query_configuration_tables"
-    DROP COLUMN "tablesQueryConfigurationId";
-ALTER TABLE "public"."agent_tables_query_configuration_tables"
-    ALTER COLUMN "mcpServerConfigurationId" DROP NOT NULL;
+ALTER TABLE "public"."agent_tables_query_configuration_tables" DROP COLUMN "tablesQueryConfigurationId";
+ALTER TABLE "public"."agent_tables_query_configuration_tables" ALTER COLUMN "mcpServerConfigurationId" DROP NOT NULL;
+DROP INDEX IF EXISTS "agent_tables_query_configuration_table_unique_dsv";
+DROP INDEX IF EXISTS "agent_tables_query_config_table_w_id_tables_query_config_id";
