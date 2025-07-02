6577bcd5d4e59df96a552c008d52ca911f7ef8ba
Daphné Popin <daphne.popin@gmail.com>
2025-07-02 16:45:02 +0200

    We don't need that

--- front/migrations/db/migration_296.sql
+++ front/migrations/db/migration_296.sql
@@ -1,3 +1,2 @@
 -- Migration created on Jul 02, 2025
 ALTER TABLE "agent_reasoning_configurations" ALTER COLUMN "mcpServerConfigurationId" SET NOT NULL;
-ALTER TABLE "agent_reasoning_configurations"  ADD FOREIGN KEY ("mcpServerConfigurationId") REFERENCES "agent_mcp_server_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
