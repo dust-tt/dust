7687841c48845c039068cf9d57ebb77906faad0b
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-08 15:51:04 +0200

    add unique index creation in migration script

--- front/migrations/db/migration_303.sql
+++ front/migrations/db/migration_303.sql
@@ -5,3 +5,4 @@ ALTER TABLE "public"."agent_mcp_actions" ADD COLUMN "version" INTEGER NOT NULL D
 -- Drop the old index and create a new one without the UNIQUE constraint.
 DROP INDEX agent_step_contents_agent_message_id_step_index_in_step;
 CREATE INDEX CONCURRENTLY agent_step_contents_agent_message_id_step_index_in_step ON agent_step_contents ("agentMessageId", "step", "index");
+CREATE UNIQUE INDEX CONCURRENTLY "agent_step_contents_agent_message_id_step_index_versioned" ON "agent_step_contents" ("agentMessageId", "step", "index", "version");
