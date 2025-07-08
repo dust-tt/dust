f39eae3d8e1f3de96972e4cc27d59f00ec10e90a
Joshua Gisiger <joshuagisiger142@gmail.com>
2025-07-08 18:01:45 +0200

    style: lint + format

--- cli/src/utils/hooks/use_clear_terminal_on_mount.ts
+++ cli/src/utils/hooks/use_clear_terminal_on_mount.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+
 import { clearTerminal } from "../terminal.js";
 
 export function useClearTerminalOnMount() {
   const [isCleared, setIsCleared] = useState(false);
 
   useEffect(() => {
-    clearTerminal().then(() => {
+    void clearTerminal().then(() => {
       setIsCleared(true);
     });
   }, []);
