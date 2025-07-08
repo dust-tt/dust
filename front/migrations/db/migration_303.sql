bfce4ae4a7a204597e680b4401fa8b02144d4284
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-08 13:46:09 +0200

    add index non uniqueness drop

--- front/migrations/db/migration_303.sql
+++ front/migrations/db/migration_303.sql
@@ -1,3 +1,7 @@
 -- Migration created on Jul 08, 2025
 ALTER TABLE "public"."agent_step_contents" ADD COLUMN "version" INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE "public"."agent_mcp_actions" ADD COLUMN "version" INTEGER NOT NULL DEFAULT 0;
+
+-- Drop the old index and create a new one without the UNIQUE constraint.
+DROP INDEX agent_step_contents_agent_message_id_step_index_in_step;
+CREATE INDEX CONCURRENTLY agent_step_contents_agent_message_id_step_index_in_step ON agent_step_contents ("agentMessageId", "step", "index");
