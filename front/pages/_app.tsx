a267a0211ca8a4c4f606c919546184db9085e13c
Benjamin Toueg <ben@dust.tt>
2025-07-09 15:59:50 +0200

    Set workspaceId with setGlobalContext and user with setUser

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -5,9 +5,12 @@ import "@dust-tt/sparkle/dist/sparkle.css";
 // Local tailwind components override sparkle styles
 import "@app/styles/components.css";
 
+import { useUser } from "@app/lib/swr/user";
 import { datadogLogs } from "@datadog/browser-logs";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import RootLayout from "@app/components/app/RootLayout";
 
@@ -37,6 +40,38 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available.
   const getLayout = Component.getLayout ?? ((page) => page);
+
+  const router = useRouter();
+  const { wId } = router.query;
+  useEffect(() => {
+    const updateLoggerContext = () => {
+      if (wId) {
+        datadogLogs.setGlobalContext({
+          workspaceId: wId,
+        });
+      } else {
+        datadogLogs.setGlobalContext({});
+      }
+    };
+
+    updateLoggerContext();
+    router.events.on("routeChangeComplete", updateLoggerContext);
+    return () => {
+      router.events.off("routeChangeComplete", updateLoggerContext);
+    };
+  }, [wId, router.events]);
+
+  const { user } = useUser();
+  const userId = user?.sId;
+  useEffect(() => {
+    if (userId) {
+      datadogLogs.setUser({
+        id: userId,
+      });
+    } else {
+      datadogLogs.clearUser();
+    }
+  }, [userId]);
 
   return (
     <RootLayout>
