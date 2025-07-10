647a527f2a7b53633102c58e80881a5b5cfabaa0
David Ebbo <david@dust.tt>
2025-07-10 09:16:06 +0200

    Move loop iteration into separate method

--- core/src/databases/table_upserts_background_worker.rs
+++ core/src/databases/table_upserts_background_worker.rs
@@ -25,6 +25,55 @@ pub struct TableUpsertCall {
     pub table_id: String,
 }
 
+pub async fn handle_loop_iteration(
+    conn: &mut redis::aio::Connection,
+    store: &Box<dyn store::Store + Sync + Send>,
+    databases_store: &Box<dyn databases_store::store::DatabasesStore + Sync + Send>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let all_values: HashMap<String, String> = conn.hgetall(REDIS_TABLE_UPSERT_HASH_NAME).await?;
+
+    let mut values: Vec<TableUpsertCall> = all_values
+        .values()
+        .filter_map(|json_value| serde_json::from_str(json_value).ok())
+        .collect();
+    values.sort_by(|a, b| b.time.cmp(&a.time));
+
+    for call in values {
+        let table = store
+            .load_data_source_table(
+                &Project::new_from_id(call.project_id),
+                &call.data_source_id,
+                &call.table_id,
+            )
+            .await?;
+
+        match table {
+            Some(table) => {
+                let files =
+                    GoogleCloudStorageBackgroundProcessingStore::get_files_for_table(&table)
+                        .await?;
+                let rows =
+                    GoogleCloudStorageBackgroundProcessingStore::get_rows_from_all_files(&files)
+                        .await?;
+                let table = LocalTable::from_table(table).unwrap();
+                table
+                    .upsert_rows_now(store, databases_store, rows, false)
+                    .await?;
+                GoogleCloudStorageBackgroundProcessingStore::delete_files(&files).await?;
+            }
+            None => {
+                println!(
+                    "Table not found for project_id: {}, data_source_id: {}, table_id: {}",
+                    call.project_id, call.data_source_id, call.table_id
+                );
+                continue;
+            }
+        }
+    }
+
+    Ok(())
+}
+
 pub async fn table_upserts_and_deletes_loop() {
     let store: Box<dyn store::Store + Sync + Send> = match std::env::var("CORE_DATABASE_URI") {
         Ok(db_uri) => {
@@ -51,65 +100,12 @@ pub async fn table_upserts_and_deletes_loop() {
 
     if let Some(client) = &*cache::REDIS_CLIENT {
         match client.get_async_connection().await {
-            Ok(mut conn) => {
-                loop {
-                    let all_values: HashMap<String, String> =
-                        conn.hgetall(REDIS_TABLE_UPSERT_HASH_NAME).await.unwrap();
-
-                    let mut values: Vec<TableUpsertCall> = all_values
-                        .values()
-                        .filter_map(|json_value| serde_json::from_str(json_value).ok())
-                        .collect();
-                    values.sort_by(|a, b| b.time.cmp(&a.time));
-
-                    for call in values {
-                        let table = store
-                            .load_data_source_table(
-                                &Project::new_from_id(call.project_id),
-                                &call.data_source_id,
-                                &call.table_id,
-                            )
-                            .await
-                            .unwrap();
-
-                        match table {
-                            Some(table) => {
-                                let files = GoogleCloudStorageBackgroundProcessingStore::get_files_for_table(&table)
-                                .await
-                                .unwrap_or_else(|e| {
-                                    error!("Error getting files for table: {}", e);
-                                    vec![]
-                                });
-                                let rows = GoogleCloudStorageBackgroundProcessingStore::get_rows_from_all_files(&files).await
-                                .unwrap_or_else(|e| {
-                                    error!("Error getting rows from all files: {}", e);
-                                    vec![]
-                                });
-                                let table = LocalTable::from_table(table).unwrap();
-                                table
-                                    .upsert_rows_now(&store, &databases_store, rows, false)
-                                    .await
-                                    .unwrap_or_else(|e| {
-                                        error!("Error upserting rows: {}", e);
-                                    });
-                                // Delete the files after processing
-                                GoogleCloudStorageBackgroundProcessingStore::delete_files(&files)
-                                    .await
-                                    .unwrap_or_else(|e| {
-                                        error!("Error deleting files: {}", e);
-                                    });
-                            }
-                            None => {
-                                println!("Table not found for project_id: {}, data_source_id: {}, table_id: {}",
-                                    call.project_id, call.data_source_id, call.table_id);
-                                continue;
-                            }
-                        }
-                    }
-
-                    tokio::time::sleep(std::time::Duration::from_millis(1024 * 60)).await;
+            Ok(mut conn) => loop {
+                if let Err(e) = handle_loop_iteration(&mut conn, &store, &databases_store).await {
+                    error!("Error in loop iteration: {}", e);
                 }
-            }
+                tokio::time::sleep(std::time::Duration::from_millis(1024 * 60)).await;
+            },
             Err(e) => {
                 error!("Error connecting to Redis: {}.", e);
             }
