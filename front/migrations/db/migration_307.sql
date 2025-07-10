b8f7e85f38471e1badd491d7121f2e643ad5164d
Stanislas Polu <spolu@dust.tt>
2025-07-10 14:25:48 +0200

    rename migration

--- front/migrations/db/migration_307.sql
+++
@@ -1,2 +0,0 @@
--- Migration created on Jul 09, 2025
-ALTER TABLE "public"."agent_mcp_actions" ADD COLUMN "stepContentId" BIGINT REFERENCES "agent_step_contents" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
