049358e5360ff3791553e0814d2c553bd50c87af
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-11 11:07:59 +0200

    remove unused function

--- front/components/actions/tables_query/utils.ts
+++
@@ -1,9 +0,0 @@
-export function getTablesQueryResultsFileTitle({
-  output,
-}: {
-  output: Record<string, unknown> | null;
-}): string {
-  return typeof output?.query_title === "string"
-    ? output.query_title
-    : "query_results";
-}
