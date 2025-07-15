f607a7484f35c28f6a12237f60be0097dbe3c9ba
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-15 10:52:12 +0200

    sort importsx

--- connectors/migrations/20250710_cleanup_duplicate_zendesk_tickets.ts
+++ connectors/migrations/20250710_cleanup_duplicate_zendesk_tickets.ts
@@ -3,15 +3,15 @@ import type { Logger } from "pino";
 import { makeScript } from "scripts/helpers";
 import { Op } from "sequelize";
 
+import { getTicketInternalId } from "@connectors/connectors/zendesk/lib/id_conversions";
 import { getZendeskSubdomainAndAccessToken } from "@connectors/connectors/zendesk/lib/zendesk_access_token";
 import { fetchZendeskTicket } from "@connectors/connectors/zendesk/lib/zendesk_api";
+import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
 import { concurrentExecutor } from "@connectors/lib/async_utils";
+import { deleteDataSourceDocument } from "@connectors/lib/data_sources";
 import { ZendeskTicketModel } from "@connectors/lib/models/zendesk";
 import { ConnectorResource } from "@connectors/resources/connector_resource";
 import { ZendeskBrandResource } from "@connectors/resources/zendesk_resources";
-import { deleteDataSourceDocument } from "@connectors/lib/data_sources";
-import { dataSourceConfigFromConnector } from "@connectors/lib/api/data_source_config";
-import { getTicketInternalId } from "@connectors/connectors/zendesk/lib/id_conversions";
 
 const TICKET_BATCH_SIZE = 512;
 const CONCURRENCY = 4;
