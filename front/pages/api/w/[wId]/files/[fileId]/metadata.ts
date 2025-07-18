75a348683859e95086f5953d0c075e820634f463
Flavien David <flavien@dust.tt>
2025-07-18 16:16:46 +0200

    Implement permissions check on file metadata endpoint

--- front/pages/api/w/[wId]/files/[fileId]/metadata.ts
+++ front/pages/api/w/[wId]/files/[fileId]/metadata.ts
@@ -2,7 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { withSessionAuthenticationForWorkspace } from "@app/lib/api/auth_wrappers";
 import type { Authenticator } from "@app/lib/auth";
+import { ConversationResource } from "@app/lib/resources/conversation_resource";
 import { FileResource } from "@app/lib/resources/file_resource";
+import { SpaceResource } from "@app/lib/resources/space_resource";
 import { apiError } from "@app/logger/withlogging";
 import type { FileType, WithAPIErrorResponse } from "@app/types";
 
@@ -23,31 +25,64 @@ async function handler(
     });
   }
 
-  // TODO: Add a check on permissions/access to the file.
-  switch (req.method) {
-    case "GET":
-      const fileResource = await FileResource.fetchById(auth, fileId);
-      if (!fileResource) {
-        return apiError(req, res, {
-          status_code: 404,
-          api_error: {
-            type: "file_not_found",
-            message: "File not found.",
-          },
-        });
-      }
+  if (req.method !== "GET") {
+    return apiError(req, res, {
+      status_code: 405,
+      api_error: {
+        type: "method_not_supported_error",
+        message: "The method passed is not supported, GET is expected.",
+      },
+    });
+  }
 
-      return res.status(200).json(fileResource.toJSON(auth));
+  const fileResource = await FileResource.fetchById(auth, fileId);
+  if (!fileResource) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "file_not_found",
+        message: "File not found.",
+      },
+    });
+  }
 
-    default:
+  const { useCase, useCaseMetadata } = fileResource;
+  let space: SpaceResource | null = null;
+  if (useCaseMetadata?.spaceId) {
+    space = await SpaceResource.fetchById(auth, useCaseMetadata.spaceId);
+  }
+
+  if (useCase === "folders_document" && (!space || !space.canRead(auth))) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "file_not_found",
+        message: "File not found.",
+      },
+    });
+  }
+
+  // Check permissions based on useCase and useCaseMetadata.
+  if (useCase === "conversation" && useCaseMetadata?.conversationId) {
+    const conversation = await ConversationResource.fetchById(
+      auth,
+      useCaseMetadata.conversationId
+    );
+    if (
+      !conversation ||
+      !ConversationResource.canAccessConversation(auth, conversation)
+    ) {
       return apiError(req, res, {
-        status_code: 405,
+        status_code: 404,
         api_error: {
-          type: "method_not_supported_error",
-          message: "The method passed is not supported, GET is expected.",
+          type: "file_not_found",
+          message: "File not found.",
         },
       });
+    }
   }
+
+  return res.status(200).json(fileResource.toJSON(auth));
 }
 
 export default withSessionAuthenticationForWorkspace(handler);
