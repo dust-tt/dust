0382f56e34618528dd9808e3a22ebe4e376b7282
Maxime Cattet <m@root.bar>
2025-07-21 14:53:32 -0700

    update date in migration

--- connectors/migrations/db/migration_85.sql
+++ connectors/migrations/db/migration_85.sql
@@ -1,3 +1,3 @@
--- Migration created on Jul 11, 2025
+-- Migration created on Jul 21, 2025
 ALTER TABLE "webcrawler_configuration" ADD COLUMN "sitemapOnly" boolean NOT NULL DEFAULT FALSE;
 UPDATE "webcrawler_configuration" SET "sitemapOnly" = FALSE;
