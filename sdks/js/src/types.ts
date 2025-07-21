148274d5524038f00fc2232a75edf3da91bac0b6
Flavien David <flavien@dust.tt>
2025-07-21 19:08:46 +0200

    👕

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -6,6 +6,7 @@ import {
   MCPExternalActionIconSchema,
   MCPInternalActionIconSchema,
 } from "./mcp_icon_types";
+import { NotificationInteractiveFileContentSchema } from "./output_schemas";
 import { CallToolResultSchema } from "./raw_mcp_types";
 
 type StringLiteral<T> = T extends string
