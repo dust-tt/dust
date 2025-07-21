d54a244f998f2e9fca909cb299c2ab910df2e5cf
Maxime Cattet <m@root.bar>
2025-07-21 10:09:11 -0700

    [front] chore: Update the clean workos script
    Remove the query, we're now passing a CSV file instead

--- front/migrations/20250626_clean_workos_organizations.ts
+++ front/migrations/20250626_clean_workos_organizations.ts
@@ -1,64 +1,55 @@
-/* eslint-disable dust/no-raw-sql */
-
 import { stringify } from "csv-stringify/sync";
 import { format } from "date-fns";
-import { writeFileSync } from "fs";
-import { QueryTypes } from "sequelize";
+import { writeFileSync, readFileSync } from "fs";
+import { parse } from "csv-parse/sync";
+import { z } from "zod";
 
 import { getWorkOS } from "@app/lib/api/workos/client";
-import { frontSequelize } from "@app/lib/resources/storage";
 import { concurrentExecutor } from "@app/lib/utils/async_utils";
 import { makeScript } from "@app/scripts/helpers";
 
+const csvSchema = z.array(
+  z.object({
+    sId: z.string(),
+    name: z.string(),
+    domain: z.string(),
+    workOSOrganizationId: z.string(),
+  })
+);
+
 makeScript(
   {
-    file: {
-      alias: "f",
+    input: {
+      alias: "i",
+      type: "string",
+      describe:
+        "List of workspaces to update. Columns must be sId, name, domain and workOSOrganizationId",
+    },
+    output: {
+      alias: "o",
+      type: "string",
       describe: "If provided, file to save the found domains to",
     },
   },
-  async ({ execute, file }, logger) => {
-    const domains = await frontSequelize.query<{
-      sId: string;
-      name: string;
-      domain: string;
-      workOSOrganizationId: string;
-    }>(
-      `
-SELECT DISTINCT
-  w."sId",
-  w.name,
-  ws.domain,
-  w."workOSOrganizationId"
-FROM
-  workspaces w
-  JOIN workspace_has_domains ws ON w."id" = ws."workspaceId"
-  LEFT JOIN subscriptions s ON w."id" = s."workspaceId"
-WHERE
-  ws."domainAutoJoinEnabled" = FALSE
-  AND w."workOSOrganizationId" IS NOT NULL
-  AND NOT EXISTS ( -- Ensure there are no active subscriptions
-    SELECT
-      1
-    FROM
-      "subscriptions" "s2"
-    WHERE
-      "s2"."workspaceId" = "w"."id"
-      AND "s2"."endDate" IS NULL
-  )
-  AND (
-    "s"."id" IS NULL -- Include workspaces with no subscriptions
-    OR "s"."endDate" < NOW() - INTERVAL '1 month' -- Or subscriptions that ended more than a month ago
-    OR (
-      "s"."endDate" IS NOT NULL
-      AND "s"."endDate" < NOW() - INTERVAL '1 month' -- Redundant check for clarity
-    )
-  );
-`,
-      { type: QueryTypes.SELECT }
-    );
+  async ({ execute, input, output }, logger) => {
+    if (!input) {
+      logger.error("Missing --input args");
+      return;
+    }
 
-    logger.info(`${domains.length} found`);
+    const inputContent = readFileSync(input, { encoding: "utf8" });
+    const parsedInput = parse(inputContent, {
+      columns: true,
+    });
+
+    const parseResult = csvSchema.safeParse(parsedInput);
+    if (!parseResult.success) {
+      logger.error({ error: parseResult.error }, "Wrong CSV format");
+      return;
+    }
+    const domains = parseResult.data;
+
+    logger.info(`${domains.length} in the given CSV`);
 
     const workOS = getWorkOS();
 
@@ -129,8 +120,8 @@ WHERE
       }
     );
 
-    if (file) {
-      writeFileSync(file, stringify(records));
+    if (output) {
+      writeFileSync(output, stringify(records));
       logger.info(`File saved`);
     }
   }
