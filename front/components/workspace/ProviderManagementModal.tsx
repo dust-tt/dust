10d282ebf30147967d63ff61d01163dd8efd2c21
JulesBelveze <jules.belveze@hotmail.fr>
2025-07-23 13:43:05 +0200

    fix: lint/format

--- front/components/workspace/ProviderManagementModal.tsx
+++ front/components/workspace/ProviderManagementModal.tsx
@@ -29,8 +29,8 @@ import { useSendNotification } from "@app/hooks/useNotification";
 import { canUseModel } from "@app/lib/assistant";
 import { useFeatureFlags, useWorkspace } from "@app/lib/swr/workspaces";
 import type { ModelProviderIdType, WorkspaceType } from "@app/types";
-import { EMBEDDING_PROVIDER_IDS, MODEL_PROVIDER_IDS } from "@app/types";
 import type { PlanType } from "@app/types";
+import { EMBEDDING_PROVIDER_IDS, MODEL_PROVIDER_IDS } from "@app/types";
 
 type ProviderStates = Record<ModelProviderIdType, boolean>;
 
