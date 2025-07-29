c2ae6d103d02bfec3ff4d611eca6e987a4635bbb
Alban Dumouilla <alban@dust.tt>
2025-07-29 14:30:38 +0200

    Outlook keep going

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -153,6 +153,7 @@ pub mod oauth {
         pub mod mock;
         pub mod monday;
         pub mod notion;
+        pub mod outlook;
         pub mod salesforce;
         pub mod slack;
         pub mod utils;
