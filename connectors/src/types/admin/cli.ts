2d163f2d523ccfba375c9287147b544a6b14b88d
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-07-31 19:20:36 +0200

    [front] Sync CLI command types (#14714)

--- connectors/src/types/admin/cli.ts
+++ connectors/src/types/admin/cli.ts
@@ -37,17 +37,17 @@ export type ConnectorsCommandType = t.TypeOf<typeof ConnectorsCommandSchema>;
 export const ConfluenceCommandSchema = t.type({
   majorCommand: t.literal("confluence"),
   command: t.union([
-    t.literal("me"),
-    t.literal("upsert-page"),
-    t.literal("skip-page"),
-    t.literal("upsert-pages"),
-    t.literal("update-parents"),
-    t.literal("ignore-near-rate-limit"),
-    t.literal("unignore-near-rate-limit"),
-    t.literal("check-space-access"),
-    t.literal("resolve-space-from-url"),
-    t.literal("sync-space"),
     t.literal("check-page-exists"),
+    t.literal("check-space-access"),
+    t.literal("ignore-near-rate-limit"),
+    t.literal("me"),
+    t.literal("resolve-space-from-url"),
+    t.literal("skip-page"),
+    t.literal("sync-space"),
+    t.literal("unignore-near-rate-limit"),
+    t.literal("update-parents"),
+    t.literal("upsert-page"),
+    t.literal("upsert-pages"),
   ]),
   args: t.type({
     connectorId: t.union([t.number, t.undefined]),
@@ -115,6 +115,7 @@ const ConfluenceAncestorSchema = t.type({
   type: t.string,
   title: t.union([t.undefined, t.string]),
 });
+export type ConfluenceAncestorType = t.TypeOf<typeof ConfluenceAncestorSchema>;
 
 export const ConfluenceCheckPageExistsResponseSchema = t.union([
   t.type({
@@ -130,11 +131,9 @@ export const ConfluenceCheckPageExistsResponseSchema = t.union([
     title: t.string,
   }),
 ]);
-
 export type ConfluenceCheckPageExistsResponseType = t.TypeOf<
   typeof ConfluenceCheckPageExistsResponseSchema
 >;
-
 /**
  * </Confluence>
  */
@@ -550,8 +549,8 @@ export const SlackCommandSchema = t.type({
     t.literal("remove-channel-from-sync"),
     t.literal("skip-channel"),
     t.literal("skip-thread"),
-    t.literal("sync-channel-metadata"),
     t.literal("sync-channel"),
+    t.literal("sync-channel-metadata"),
     t.literal("sync-thread"),
     t.literal("uninstall-for-unknown-team-ids"),
     t.literal("unskip-channel"),
@@ -792,9 +791,9 @@ export const AdminResponseSchema = t.union([
   CheckFileGenericResponseSchema,
   ConfluenceCheckPageExistsResponseSchema,
   ConfluenceCheckSpaceAccessResponseSchema,
-  ConfluenceSkipPageResponseSchema,
   ConfluenceMeResponseSchema,
   ConfluenceResolveSpaceFromUrlResponseSchema,
+  ConfluenceSkipPageResponseSchema,
   ConfluenceUpsertPageResponseSchema,
   GongForceResyncResponseSchema,
   IntercomCheckConversationResponseSchema,
@@ -822,6 +821,7 @@ export const AdminResponseSchema = t.union([
   ZendeskFetchBrandResponseSchema,
   ZendeskFetchTicketResponseSchema,
   ZendeskGetRetentionPeriodResponseSchema,
+  ZendeskOrganizationTagResponseSchema,
 ]);
 export type AdminResponseType = t.TypeOf<typeof AdminResponseSchema>;
 /**
