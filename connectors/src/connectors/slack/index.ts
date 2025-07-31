e301a77c06d36c59120ff244209621531b5de6e4
Flavien David <flavien.david74@gmail.com>
2025-07-31 10:51:22 +0200

    Improve Slack cutover robustness (#14693)
    * Ignore inactive account on slack cutover
    
    * Trigger Slack cutover on slack edit permission
    
    * 🙈

--- connectors/src/connectors/slack/index.ts
+++ connectors/src/connectors/slack/index.ts
@@ -25,7 +25,10 @@ import {
   reportSlackUsage,
 } from "@connectors/connectors/slack/lib/slack_client";
 import { slackChannelIdFromInternalId } from "@connectors/connectors/slack/lib/utils";
-import { launchSlackSyncWorkflow } from "@connectors/connectors/slack/temporal/client.js";
+import {
+  launchSlackMigrateChannelsFromLegacyBotToNewBotWorkflow,
+  launchSlackSyncWorkflow,
+} from "@connectors/connectors/slack/temporal/client.js";
 import { ExternalOAuthTokenError } from "@connectors/lib/error";
 import { SlackChannel } from "@connectors/lib/models/slack";
 import { terminateAllWorkflowsForConnectorId } from "@connectors/lib/temporal";
@@ -182,6 +185,21 @@ export class SlackConnectorManager extends BaseConnectorManager<SlackConfigurati
             "CONNECTOR_OAUTH_TARGET_MISMATCH",
             "Cannot change the Slack Team of a Data Source"
           )
+        );
+      }
+
+      // TODO(slack 2025-07-31): If the connection was updated, meaning an admin added the missing
+      // scope (`channels:manage`) to the bot, we need to trigger the migration of channels from
+      // legacy bot to new bot.
+      const slackBotConnector =
+        await ConnectorResource.findByWorkspaceIdAndType(
+          c.workspaceId,
+          "slack_bot"
+        );
+      if (slackBotConnector) {
+        await launchSlackMigrateChannelsFromLegacyBotToNewBotWorkflow(
+          c.id,
+          slackBotConnector.id
         );
       }
 
