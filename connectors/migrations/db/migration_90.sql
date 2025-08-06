8cb2d45c11dc3df50e75d0b864f11dddff2eea21
Flavien David <flavien@dust.tt>
2025-08-06 12:03:53 +0200

    👕

--- connectors/migrations/db/migration_90.sql
+++
@@ -1,3 +0,0 @@
--- Migration created on Aug 05, 2025
-ALTER TABLE "public"."confluence_folders" ADD COLUMN "parentType" VARCHAR(255) DEFAULT NULL;
-ALTER TABLE "public"."confluence_pages" ADD COLUMN "parentType" VARCHAR(255) DEFAULT NULL;
