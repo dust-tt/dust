6599c67f2818cca89fd48f7b1f7be66bd95b72c4
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-08-15 08:36:01 +0200

    add margin underneath the command input

--- front/components/poke/shadcn/ui/command.tsx
+++ front/components/poke/shadcn/ui/command.tsx
@@ -138,11 +138,13 @@ const CommandInput = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-    <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+    <MagnifyingGlassIcon className="mb-3 mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
-        "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        "mb-3 flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none",
+        "placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+        "dark:placeholder:text-muted-foreground-night",
         className
       )}
       {...props}
