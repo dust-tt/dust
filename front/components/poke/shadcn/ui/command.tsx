a9a531f6ac8e2076c03cc3926b5b774a843d07d3
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-08-15 08:36:07 +0200

    fix dark mode

--- front/components/poke/shadcn/ui/command.tsx
+++ front/components/poke/shadcn/ui/command.tsx
@@ -119,7 +119,9 @@ const CommandDialog = ({
             shouldFilter={shouldFilter}
             className={cn(
               "text-muted-foreground dark:text-muted-foreground-night",
-              "[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground",
+              "[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium",
+              "[&_[cmdk-group-heading]]:text-muted-foreground",
+              "dark:[&_[cmdk-group-heading]]:text-muted-foreground-night",
               "[&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2",
               "[&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12",
               "[&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
@@ -187,7 +189,11 @@ const CommandGroup = React.forwardRef<
   <CommandPrimitive.Group
     ref={ref}
     className={cn(
-      "overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground",
+      "overflow-hidden p-1 text-foreground dark:text-foreground-night",
+      "[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5",
+      "[&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
+      "[&_[cmdk-group-heading]]:text-muted-foreground",
+      "dark:[&_[cmdk-group-heading]]:text-muted-foreground-night",
       className
     )}
     {...props}
@@ -262,6 +268,7 @@ const CommandShortcut = ({
     <span
       className={cn(
         "ml-auto text-xs tracking-widest text-muted-foreground",
+        "dark:text-muted-foreground-night",
         className
       )}
       {...props}
