3c6fdb1b13b58a30de9ca2252cf20d8a44f102d6
Adrien Simon <adrisim2001@gmail.com>
2025-08-18 18:23:40 +0200

    wip

--- front/types/assistant/triggers.ts
+++ front/types/assistant/triggers.ts
@@ -22,7 +22,7 @@ export function isScheduleConfiguration(
   );
 }
 
-export type TriggerConfigType = ScheduleConfigType | null;
+export type TriggerConfigType = ScheduleConfigType;
 
 export type TriggerType = {
   id: ModelId;
