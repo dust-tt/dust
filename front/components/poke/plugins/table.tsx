7be129c4575add169a43892dc1829a81d2d54779
Thomas Draier <thomas@dust.tt>
2025-08-20 14:58:27 +0200

    simplify

--- front/components/poke/plugins/table.tsx
+++ front/components/poke/plugins/table.tsx
@@ -2,24 +2,10 @@ import { makeColumnsForPluginRuns } from "@app/components/poke/plugins/columns";
 import { PokeDataTableConditionalFetch } from "@app/components/poke/PokeConditionalDataTables";
 import { PokeDataTable } from "@app/components/poke/shadcn/ui/data_table";
 import { usePokePluginRuns } from "@app/poke/swr/plugins";
-import type { LightWorkspaceType, PluginRunType } from "@app/types";
+import type { LightWorkspaceType } from "@app/types";
 
 interface PluginRunsDataTableProps {
   owner: LightWorkspaceType;
-}
-
-function preparePluginRunsForDisplay(
-  owner: LightWorkspaceType,
-  pluginRuns: PluginRunType[]
-) {
-  return pluginRuns.map((run) => {
-    // We need to add display properties but keep the original properties
-    const result = {
-      ...run,
-    };
-
-    return result;
-  });
 }
 
 export function PluginRunsDataTable({ owner }: PluginRunsDataTableProps) {
@@ -30,10 +16,7 @@ export function PluginRunsDataTable({ owner }: PluginRunsDataTableProps) {
       useSWRHook={usePokePluginRuns}
     >
       {(data) => (
-        <PokeDataTable
-          columns={makeColumnsForPluginRuns()}
-          data={preparePluginRunsForDisplay(owner, data)}
-        />
+        <PokeDataTable columns={makeColumnsForPluginRuns()} data={data} />
       )}
     </PokeDataTableConditionalFetch>
   );
