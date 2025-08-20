4bb5fd737945a25e909cc883be6a95bf9daa8d48
JulesBelveze <jules.belveze@hotmail.fr>
2025-08-20 09:41:54 +0200

    [sparkle] - refactor: wrap NotificationButton with forwardRef
     - Allow NotificationButton to accept refs by using React.forwardRef
     - Add displayName to the NotificationButton for easier debugging and readability in DevTools

--- sparkle/src/components/NotificationButton.tsx
+++ sparkle/src/components/NotificationButton.tsx
@@ -10,19 +10,20 @@ interface NotificationButtonProps {
   className?: string;
 }
 
-const NotificationButton = ({
-  className,
-  buttonProps,
-  counterProps,
-}: NotificationButtonProps) => {
+const NotificationButton = React.forwardRef<
+  HTMLDivElement,
+  NotificationButtonProps
+>(({ className, buttonProps, counterProps }, ref) => {
   return (
-    <div className={cn("s-relative", className)}>
+    <div ref={ref} className={cn("s-relative", className)}>
       <Button {...buttonProps} />
       {counterProps.value > 0 && (
         <Counter {...counterProps} className="s-absolute -s-right-2 -s-top-2" />
       )}
     </div>
   );
-};
+});
+
+NotificationButton.displayName = "NotificationButton";
 
 export { NotificationButton };
