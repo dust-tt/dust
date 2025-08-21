0c46411ee120d03444aed4b3a45df56e740b1394
Alexandre Pinot <32997243+pinotalexandre@users.noreply.github.com>
2025-08-21 10:02:57 +0200

    Update home page following Steven's redesign (#15201)
    * improving home page
    
    * deleting unused image
    
    * Applying Adrien comments

--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import type { ReactElement } from "react";
 
 import { BlogSection } from "@app/components/home/content/Product/BlogSection";
-import { CallToActionSection } from "@app/components/home/content/Product/CallToActionSection";
 import { IntroSection } from "@app/components/home/content/Product/IntroSection";
+import { JustUseDustSection as ProductJustUseDustSection } from "@app/components/home/content/Product/JustUseDustSection";
 import { QuoteSection } from "@app/components/home/ContentBlocks";
-import { SecurityComplianceSection } from "@app/components/home/ContentComponents";
-import { CloudConnectorsSection } from "@app/components/home/ContentComponents";
+import {
+  CloudConnectorsSection,
+  SecurityComplianceSection,
+} from "@app/components/home/ContentComponents";
 import type { LandingLayoutProps } from "@app/components/home/LandingLayout";
 import LandingLayout from "@app/components/home/LandingLayout";
 import UTMPageWrapper from "@app/components/UTMPageWrapper";
@@ -23,17 +25,18 @@ export function Landing() {
   return (
     <UTMPageWrapper>
       <IntroSection />
-      <CloudConnectorsSection />
-      <SecurityComplianceSection />
-      <QuoteSection
-        quote="Dust is the most impactful software we've adopted since building Clay. It delivers immediate value while continuously getting smarter and more valuable over time"
-        name="Everett Berry"
-        title="Head of GTM Engineering at Clay"
-        logo="/static/landing/logos/color/clay.png"
-      />
-      {/* <FutureSection /> */}
-      <BlogSection />
-      <CallToActionSection />
+      <div className="mt-16 flex flex-col gap-16 md:gap-20 lg:gap-24">
+        <CloudConnectorsSection />
+        <SecurityComplianceSection />
+        <QuoteSection
+          quote="Dust is the most impactful software we've adopted since building Clay. It delivers immediate value while continuously getting smarter and more valuable over time"
+          name="Everett Berry"
+          title="Head of GTM Engineering at Clay"
+          logo="/static/landing/logos/color/clay.png"
+        />
+        <BlogSection />
+        <ProductJustUseDustSection />
+      </div>
     </UTMPageWrapper>
   );
 }
