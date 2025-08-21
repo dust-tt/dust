832f074819099fca68d03cd2c1265c709682cfeb
Maxime Cattet <m@root.bar>
2025-08-21 17:16:11 +0200

    update excluded paths

--- front/next-sitemap.config.js
+++ front/next-sitemap.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL,
-  exclude: ["/api/*", "/w/*", "/poke/*", "/oauth/*"],
+  exclude: [
+    "/api*",
+    "/w*",
+    "/poke*",
+    "/oauth*",
+    "/sso-enforce",
+    "/no-workspace",
+    "/maintenance",
+    "/login-error",
+  ],
   generateIndexSitemap: false,
   changefreq: "weekly",
   generateRobotsTxt: true,
