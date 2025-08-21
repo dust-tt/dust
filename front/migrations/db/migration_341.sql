0eb198f085e8a0a2a92cb380bfa6cbc44b05a631
Adrien Simon <adrisim2001@gmail.com>
2025-08-21 13:45:52 +0200

    add migration

---
+++ front/migrations/db/migration_341.sql
@@ -0,0 +1,4 @@
+-- Migration created on Aug 21, 2025
+ALTER TABLE "triggers" ALTER COLUMN "agentConfigurationId" SET NOT NULL;
+ALTER TABLE "triggers" ALTER COLUMN "agentConfigurationId" DROP DEFAULT;
+ALTER TABLE "triggers" ALTER COLUMN "agentConfigurationId" TYPE VARCHAR(255);
