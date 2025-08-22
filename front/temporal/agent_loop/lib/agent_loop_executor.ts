c076cf1d729e372a748fc5f26f90bca2d8e54ea5
Flavien David <flavien.david74@gmail.com>
2025-08-22 14:53:28 +0200

    Always Run Agent Loop On Async (#15267)
    * Always Run Agent Loop On Async
    
    * ALWAYS ASYNC

--- front/temporal/agent_loop/lib/agent_loop_executor.ts
+++ front/temporal/agent_loop/lib/agent_loop_executor.ts
@@ -150,11 +150,12 @@ export async function executeAgentLoop(
     currentStep = i;
 
     // Check if we should switch to async mode due to timeout (only in sync mode).
-    if (runAgentArgs.sync && runAgentArgs.syncToAsyncTimeoutMs) {
+    if (runAgentArgs.sync /* && runAgentArgs.syncToAsyncTimeoutMs */) {
       const elapsedMs = Date.now() - syncStartTime;
-      if (elapsedMs > runAgentArgs.syncToAsyncTimeoutMs) {
-        throw new SyncTimeoutError({ currentStep, elapsedMs });
-      }
+      // if (elapsedMs > runAgentArgs.syncToAsyncTimeoutMs) {
+      // TODO(DURABLE_AGENT 2025-08-22): Remove this once we made a decision on sync vs async.
+      throw new SyncTimeoutError({ currentStep, elapsedMs });
+      // }
     }
 
     const stepStartTime = Date.now();
