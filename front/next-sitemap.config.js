77184b029dadeb9800444a43dd009f8afc101132
Maxime Cattet <m@root.bar>
2025-08-26 09:43:38 +0200

    add excludes to robots.txt

--- front/next-sitemap.config.js
+++ front/next-sitemap.config.js
@@ -1,17 +1,29 @@
+// Paths to exclude from sitemap and robots.txt for easier maintenance
+const EXCLUDED_PATHS = [
+  "/api*",
+  "/w*",
+  "/poke*",
+  "/oauth*",
+  "/sso-enforced",
+  "/no-workspace",
+  "/maintenance",
+  "/login-error",
+];
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL,
-  exclude: [
-    "/api*",
-    "/w*",
-    "/poke*",
-    "/oauth*",
-    "/sso-enforced",
-    "/no-workspace",
-    "/maintenance",
-    "/login-error",
-  ],
+  exclude: EXCLUDED_PATHS,
   generateIndexSitemap: false,
   changefreq: "weekly",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: EXCLUDED_PATHS,
+      },
+    ],
+  },
 };
