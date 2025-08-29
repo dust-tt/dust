03c6987f9aa810f6c326d0d71169dfb645667534
Yuka Masuda <yuka@dust.tt>
2025-08-29 14:51:53 +0200

    [front] improve error handling for uncaught client error  (#15524)
    * imp name
    
    * add console error

--- front/components/error_boundary/ErrorBoundary.tsx
+++ front/components/error_boundary/ErrorBoundary.tsx
@@ -24,12 +24,10 @@ export class ErrorBoundary extends React.Component<
   }
 
   componentDidCatch(error: Error, info: React.ErrorInfo) {
-    datadogLogs.logger.error("Uncaught client side error", {
-      error: {
-        message: error.message,
-        stack: error.stack,
-        name: error.name,
-      },
+    // TODO (yuka): check with Ben what is the best way to log this error
+    console.error(error);
+    datadogLogs.logger.error(error.name || "Uncaught client side error", {
+      error,
       errorInfo: {
         componentStack: info.componentStack,
       },
