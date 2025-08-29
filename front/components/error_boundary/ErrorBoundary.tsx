d4c6a87665d3b84194a73c4a5410dd610c7e629a
Yuka Masuda <yuka@dust.tt>
2025-08-29 12:04:46 +0200

    [front] Add error boundary to conversation layout (#15512)
    * Add conversation error boundary
    
    * lint

---
+++ front/components/error_boundary/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import { datadogLogs } from "@datadog/browser-logs";
+import * as React from "react";
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    datadogLogs.logger.error("Uncaught client side error", {
+      error: {
+        message: error.message,
+        stack: error.stack,
+        name: error.name,
+      },
+      errorInfo: {
+        componentStack: info.componentStack,
+      },
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
