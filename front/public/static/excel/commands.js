e50b7710dd4d7029b97416c1881c22d3d9250ebd
Alban Dumouilla <alban@dust.tt>
2025-09-04 14:27:46 +0200

    Add excel plugin static files in public/

---
+++ front/public/static/excel/commands.js
@@ -0,0 +1,13 @@
+/* global Office */
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        // If needed, initialize add-in when it's loaded
+    };
+
+    // Associate function names with UI events (if we add custom commands later)
+    // For now, the ShowTaskpane action is handled automatically
+})();
