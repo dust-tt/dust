05ef03f172d9fe6f08cc9b0eb18ffb416459c0fd
lvndry <lvndry@proton.me>
2025-09-08 17:46:22 +0200

    add slideshow in ff tests

--- front/lib/actions/mcp_internal_actions/servers/slideshow/instructions/index.ts
+++ front/lib/actions/mcp_internal_actions/servers/slideshow/instructions/index.ts
@@ -155,7 +155,6 @@ Focus on content quality, not navigation controls.
 
 Example using pre-built layouts:
 \`\`\`tsx
-import React from "react";
 import { Slideshow } from "@dust/slideshow/v1";
 
 export default function Deck() {
