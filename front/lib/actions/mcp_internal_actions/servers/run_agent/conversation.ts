48daed9b15bd334350077c25b547f9152a5da1ab
Thomas Draier <thomas@dust.tt>
2025-09-08 10:27:39 +0200

    [front] take conversationId as input (#15574)
    * take conversationId as input
    
    * Better handling of runagent handover
    
    * Add mention

--- front/lib/actions/mcp_internal_actions/servers/run_agent/conversation.ts
+++ front/lib/actions/mcp_internal_actions/servers/run_agent/conversation.ts
@@ -28,6 +28,7 @@ export async function getOrCreateConversation(
     query,
     toolsetsToAdd,
     fileOrContentFragmentIds,
+    conversationId,
   }: {
     childAgentBlob: ChildAgentBlob;
     childAgentId: string;
@@ -36,6 +37,7 @@ export async function getOrCreateConversation(
     query: string;
     toolsetsToAdd: string[] | null;
     fileOrContentFragmentIds: string[] | null;
+    conversationId: string | null;
   }
 ): Promise<
   Result<
@@ -98,12 +100,50 @@ export async function getOrCreateConversation(
     }
   }
 
+  if (conversationId) {
+    const messageRes = await api.postUserMessage({
+      conversationId,
+      message: {
+        content: `:mention[${childAgentBlob.name}]{sId=${childAgentId}} ${query}`,
+        mentions: [{ configurationId: childAgentId }],
+        context: {
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          username: mainAgent.name,
+          fullName: `@${mainAgent.name}`,
+          email: null,
+          profilePictureUrl: mainAgent.pictureUrl,
+          // `run_agent` origin will skip adding the conversation to the user history.
+          origin: "run_agent",
+          selectedMCPServerViewIds: toolsetsToAdd,
+        },
+      },
+    });
+
+    if (messageRes.isErr()) {
+      return new Err(new Error("Failed to create message"));
+    }
+
+    const convRes = await api.getConversation({
+      conversationId,
+    });
+
+    if (convRes.isErr()) {
+      return new Err(new Error("Failed to get conversation"));
+    }
+
+    return new Ok({
+      conversation: convRes.value,
+      userMessageId: messageRes.value.sId,
+      isNewConversation: true,
+    });
+  }
+
   const convRes = await api.createConversation({
     title: `run_agent ${mainAgent.name} > ${childAgentBlob.name}`,
     visibility: "unlisted",
     depth: mainConversation.depth + 1,
     message: {
-      content: query,
+      content: `:mention[${childAgentBlob.name}]{sId=${childAgentId}} ${query}`,
       mentions: [{ configurationId: childAgentId }],
       context: {
         timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
