12d2407c6c1c54f84f81fe27a865dbfd1754b412
Jules Belveze <32683010+JulesBelveze@users.noreply.github.com>
2025-09-09 10:31:20 +0200

    [sparkle] - chore(ToolCard): enhance UI (#15750)
    * [sparkle] - feature: add line clamping to ToolCard descriptions
    
     - Introduce a prop to control the number of lines after which the description text in ToolCard components gets clamped
     - Refactor ToolCard structure to accommodate the new line clamping functionality
     - Provide default line clamp value to maintain backward compatibility for existing components without the prop specified
    
    * [sparkle] - feature: update ToolCard component UI and functionality
    
     - Add a new `toolInfo` property to provide additional information with label and click handler
     - Introduce `cardContainerClassName` prop to customize ToolCard container styles
     - Adjust `descriptionLineClamp` default value to improve text visibility
     - Modify `ToolCard` structure for better visual hierarchy and interactivity
     - Update `Tooltip` component to support portal mounting within dynamic container elements
    
    * [sparkle] - fix: align items center in ToolCard component
    
     - Change alignment of items within the ToolCard from top alignment to center for better visual balance and consistency
    
    * [sparkle] - refactor: unify padding and font weight styles for ToolCard
    
     - Standardize padding within ToolCard using a single utility class
     - Emphasize interactive tool information text by changing font weight to semi-bold

--- sparkle/src/components/Tooltip.tsx
+++ sparkle/src/components/Tooltip.tsx
@@ -20,47 +20,58 @@ interface TooltipContentProps
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   TooltipContentProps
->(({ className, sideOffset = 4, mountPortal = true, mountPortalContainer, ...props }, ref) => {
-  const content = (
-    <TooltipPrimitive.Content
-      ref={ref}
-      sideOffset={sideOffset}
-      className={classNames(
-        "s-z-50 s-max-w-sm s-overflow-hidden s-whitespace-pre-wrap s-break-words s-rounded-md s-border",
-        "s-bg-background dark:s-bg-background-night",
-        "s-text-foreground dark:s-text-foreground-night",
-        "s-border-border dark:s-border-border-night",
-        "s-px-3 s-py-1.5 s-text-sm s-shadow-md",
-        "s-animate-in s-fade-in-0 s-zoom-in-95",
-        "data-[state=closed]:s-animate-out data-[state=closed]:s-fade-out-0 data-[state=closed]:s-zoom-out-95 data-[side=bottom]:s-slide-in-from-top-2 data-[side=left]:s-slide-in-from-right-2 data-[side=right]:s-slide-in-from-left-2 data-[side=top]:s-slide-in-from-bottom-2",
-        className || ""
-      )}
-      {...props}
-    />
-  );
+>(
+  (
+    {
+      className,
+      sideOffset = 4,
+      mountPortal = true,
+      mountPortalContainer,
+      ...props
+    },
+    ref
+  ) => {
+    const content = (
+      <TooltipPrimitive.Content
+        ref={ref}
+        sideOffset={sideOffset}
+        className={classNames(
+          "s-z-50 s-max-w-sm s-overflow-hidden s-whitespace-pre-wrap s-break-words s-rounded-md s-border",
+          "s-bg-background dark:s-bg-background-night",
+          "s-text-foreground dark:s-text-foreground-night",
+          "s-border-border dark:s-border-border-night",
+          "s-px-3 s-py-1.5 s-text-sm s-shadow-md",
+          "s-animate-in s-fade-in-0 s-zoom-in-95",
+          "data-[state=closed]:s-animate-out data-[state=closed]:s-fade-out-0 data-[state=closed]:s-zoom-out-95 data-[side=bottom]:s-slide-in-from-top-2 data-[side=left]:s-slide-in-from-right-2 data-[side=right]:s-slide-in-from-left-2 data-[side=top]:s-slide-in-from-bottom-2",
+          className || ""
+        )}
+        {...props}
+      />
+    );
 
-  const [container, setContainer] = useState<Element | undefined>(
-    mountPortalContainer
-  );
+    const [container, setContainer] = useState<Element | undefined>(
+      mountPortalContainer
+    );
 
-  useEffect(() => {
-    if (mountPortal && !container) {
-      const dialogElements = document.querySelectorAll(
-        ".s-sheet[role=dialog][data-state=open]"
-      );
-      const defaultContainer = dialogElements[dialogElements.length - 1];
-      setContainer(defaultContainer);
-    }
-  }, [mountPortal, container]);
+    useEffect(() => {
+      if (mountPortal && !container) {
+        const dialogElements = document.querySelectorAll(
+          ".s-sheet[role=dialog][data-state=open]"
+        );
+        const defaultContainer = dialogElements[dialogElements.length - 1];
+        setContainer(defaultContainer);
+      }
+    }, [mountPortal, container]);
 
-  return mountPortal ? (
-    <TooltipPrimitive.Portal container={container}>
-      {content}
-    </TooltipPrimitive.Portal>
-  ) : (
-    content
-  );
-});
+    return mountPortal ? (
+      <TooltipPrimitive.Portal container={container}>
+        {content}
+      </TooltipPrimitive.Portal>
+    ) : (
+      content
+    );
+  }
+);
 
 interface TooltipProps extends TooltipContentProps {
   trigger: React.ReactNode;
