ab1c0d16977990801a400777a3919d45f31f90f2
JulesBelveze <jules.belveze@hotmail.fr>
2025-09-11 17:08:08 +0200

    [front/scripts] - feature: script to convert table query to v2 for specific workspace
     - Adds a new script that converts all table query tools from v1 to v2 in a given workspace
     - Ensures that the workspace has the "exploded_tables_query" feature flag enabled before conversion
     - Generates a SQL script for reverting the changes if necessary

--- front/20250911_table_query_v1_to_v2_revert_wOZVFzKCV8.sql
+++
@@ -1,2 +0,0 @@
-UPDATE "agent_mcp_server_configurations" SET "mcpServerViewId" = '20', "internalMCPServerId" = 'ims_q03Y149aTx' WHERE "id" = '4193';
-UPDATE "agent_mcp_server_configurations" SET "mcpServerViewId" = '20', "internalMCPServerId" = 'ims_q03Y149aTx' WHERE "id" = '4194';
