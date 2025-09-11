e75bc76113415c01a56f1c1a2d2bbcf9fb5abfe5
Frank Aloia <frank.e.aloia@gmail.com>
2025-09-11 17:13:14 -0400

    Use convos.info API to for linked slack channels; Retain old logic for GC activity

--- connectors/src/connectors/slack/temporal/activities.ts
+++ connectors/src/connectors/slack/temporal/activities.ts
@@ -19,7 +19,7 @@ import {
 } from "@connectors/connectors/slack/lib/bot_user_helpers";
 import {
   getChannelById,
-  getJoinedChannels,
+  getChannels,
   joinChannel,
   migrateChannelsFromLegacyBotToNewBot,
   updateSlackChannelInConnectorsDb,
@@ -1092,10 +1092,12 @@ export async function getChannelsToGarbageCollect(
 
   const slackClient = await getSlackClient(connectorId);
 
+  // TODO: Consider using getJoinedChannels(slackClient, connectorId) for better performance.
+  // The only reason this was not done is to mitigate risk as this is a function with a large blast radius.
   const remoteChannels = new Set(
     (
       await withSlackErrorHandling(() =>
-        getJoinedChannels(slackClient, connectorId)
+        getChannels(slackClient, connectorId, true)
       )
     )
       .filter((c) => c.id)
