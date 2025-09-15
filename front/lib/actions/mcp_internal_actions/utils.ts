72ffe96e2dac4f00f8f59ef2679a4f6a1faa74f0
Thomas Draier <thomas@dust.tt>
2025-09-15 09:19:59 +0200

    cleaned

--- front/lib/actions/mcp_internal_actions/utils.ts
+++ front/lib/actions/mcp_internal_actions/utils.ts
@@ -64,13 +64,6 @@ export function makeToolBlockedAwaitingInputResponse(
   };
 }
 
-// Type that extracts server names where authorization is not null.
-type ServersWithAuthorization = {
-  [K in InternalMCPServerNameType]: (typeof INTERNAL_MCP_SERVERS)[K]["serverInfo"]["authorization"] extends null
-    ? never
-    : K;
-}[InternalMCPServerNameType];
-
 export function makePersonalAuthenticationError(
   provider: OAuthProvider,
   mcpServerId: string,
