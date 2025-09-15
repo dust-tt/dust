5c96310a6366cd445fee9f48b755d26c51b6ffa0
ykmsd <yuka@dust.tt>
2025-09-15 09:52:59 +0200

    adjust again

--- viz/tailwind.config.ts
+++ viz/tailwind.config.ts
@@ -111,11 +111,11 @@ const config: Config = {
     },
     {
       pattern: /text-(xs|sm|base|lg|xl|2xl|3xl)/,
-      variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+      variants: ['sm', 'md', 'lg'],
     },
     {
       pattern: /(flex|grid|block|inline|hidden)/,
-      variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+      variants: ['sm', 'md', 'lg'],
     },
     {
       pattern: /bg-(slate|gray|zinc|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
