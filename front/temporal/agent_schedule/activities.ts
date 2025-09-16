ce236a0cd68bd82639235ecbc39dc5a3b3ce3849
filou <philipperolet@gmail.com>
2025-09-16 11:02:30 +0200

    format

--- front/temporal/agent_schedule/activities.ts
+++ front/temporal/agent_schedule/activities.ts
@@ -15,10 +15,11 @@ import { TriggerResource } from "@app/lib/resources/trigger_resource";
 import { getTemporalClientForAgentNamespace } from "@app/lib/temporal";
 import logger from "@app/logger/logger";
 import { makeScheduleId } from "@app/temporal/agent_schedule/client";
-import type {AgentConfigurationType, ContentFragmentInputWithFileIdType} from "@app/types";
-import {
-  assertNever
+import type {
+  AgentConfigurationType,
+  ContentFragmentInputWithFileIdType,
 } from "@app/types";
+import { assertNever } from "@app/types";
 import type { TriggerType } from "@app/types/assistant/triggers";
 
 /**
