f3cd36c3aee088cffe6a9aca9864aff6c0c97e1e
Daphné Popin <daphne.popin@gmail.com>
2025-09-16 09:32:01 +0200

    findOne not findAll

--- front/lib/api/assistant/global_agents/configurations/dust/utils.ts
+++ front/lib/api/assistant/global_agents/configurations/dust/utils.ts
@@ -6,11 +6,12 @@ export async function isDustDeepDisabledByAdmin(
   auth: Authenticator
 ): Promise<boolean> {
   // We cannot call getGlobalAgents here because it will cause a dependency cycle.
-  const settings = await GlobalAgentSettings.findAll({
+  // Can be cached if too many calls are made.
+  const settings = await GlobalAgentSettings.findOne({
     where: {
       workspaceId: auth.getNonNullableWorkspace().id,
       agentId: GLOBAL_AGENTS_SID.DUST_DEEP,
     },
   });
-  return settings.length > 0 && settings[0].status === "disabled_by_admin";
+  return settings?.status === "disabled_by_admin";
 }
