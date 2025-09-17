447ccb9385f5622b6899b02608cfbf7f6bdd50e5
Rémy-Christophe Schermesser <git@schermesser.com>
2025-09-17 09:21:57 +0200

    refactor(core): split core_api more more

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -177,9 +177,11 @@ pub mod mem_check;
 pub mod api {
     pub mod api_state;
     pub mod data_sources;
+    pub mod databases;
     pub mod datasets;
     pub mod projects;
     pub(crate) mod run_manager;
     pub mod runs;
     pub mod specifications;
+    pub mod tables;
 }
