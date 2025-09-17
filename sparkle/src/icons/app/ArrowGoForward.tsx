fa0e6ee52b60034853b356aa3acc467922c8081d
Alexandre Pinot <32997243+pinotalexandre@users.noreply.github.com>
2025-09-17 17:25:55 +0200

    Move forward/backward icon (#16000)
    * Adding forward/backward icon
    
    * color

---
+++ sparkle/src/icons/app/ArrowGoForward.tsx
@@ -0,0 +1,18 @@
+import type { SVGProps } from "react";
+import * as React from "react";
+const SvgArrowGoForward = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 25"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M16 7.036h-5c-3.314 0-6 2.7-6 6.032 0 3.33 2.686 6.031 6 6.031h9v2.01h-9c-4.418 0-8-3.6-8-8.041 0-4.442 3.582-8.042 8-8.042h5v-4.02l6 5.025-6 5.026v-4.02Z"
+    />
+  </svg>
+);
+export default SvgArrowGoForward;
