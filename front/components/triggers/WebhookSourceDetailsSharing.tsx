931944a68569c4dd2f03fb87645bcf56d1af27cb
Pierre Milliotte <pierre@dust.tt>
2025-09-17 17:22:08 +0200

    feat: add toggle to share webhook source (no action yet)

--- front/components/triggers/WebhookSourceDetailsSharing.tsx
+++ front/components/triggers/WebhookSourceDetailsSharing.tsx
@@ -1,3 +1,8 @@
+import { Page, SliderToggle } from "@dust-tt/sparkle";
+import { useState } from "react";
+
+import { useSpacesAsAdmin } from "@app/lib/swr/spaces";
+import { useWebhookSourceViewsByWebhookSource } from "@app/lib/swr/webhook_source";
 import type { LightWorkspaceType } from "@app/types";
 import type { WebhookSourceType } from "@app/types/triggers/webhooks";
 
@@ -10,9 +15,46 @@ export function WebhookSourceDetailsSharing({
   webhookSource,
   owner,
 }: WebhookSourceDetailsSharingProps) {
+  const [loading, setLoading] = useState(false);
+  const { spaces } = useSpacesAsAdmin({
+    workspaceId: owner.sId,
+    disabled: false,
+  });
+  const { webhookSourceViews, isWebhookSourceViewsLoading } =
+    useWebhookSourceViewsByWebhookSource({
+      owner,
+      webhookSourceId: webhookSource.sId,
+    });
+
+  const globalSpace = spaces.find((space) => space.kind === "global");
+  const webhookSourceViewsWithSpace = webhookSourceViews.map((view) => ({
+    ...view,
+    space: spaces.find((space) => space.sId === view.spaceId) ?? null,
+  }));
+  const globalView =
+    webhookSourceViewsWithSpace.find((view) => view.space?.kind === "global") ??
+    null;
+  const isRestricted = globalView === null;
+
   return (
     <div className="flex flex-col gap-2">
-      {/* TODO: Add sharing functionality */}
+      <div className="mb-2 flex w-full flex-col gap-y-2 pt-2">
+        <div className="flex w-full items-center justify-between overflow-visible">
+          <Page.SectionHeader title="Available to all Spaces" />
+          <SliderToggle
+            disabled={isWebhookSourceViewsLoading || loading}
+            selected={!isRestricted}
+            onClick={async () => {
+              if (globalSpace !== null) {
+                setLoading(true);
+                // if it's restricted, we add the global view
+                // if it's not restricted, we remove the global view
+                setLoading(false);
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
