8e5dbab28e9ffe99f4e3424c81b6b5ce26e563fb
JulesBelveze <jules.belveze@hotmail.fr>
2025-09-19 12:08:53 +0200

    [sparkle] - refactor: update ContextItem component to conditionally truncate text
     - Allow for conditional truncation of the title element in ContextItem based on the new boolean prop truncateSubElement
     - Remove flex-shrink-0 from subElement container to allow it to shrink if necessary

--- sparkle/src/components/ContextItem.tsx
+++ sparkle/src/components/ContextItem.tsx
@@ -56,7 +56,14 @@ export function ContextItem({
         {visual}
         <div className="s-mb-0.5 s-flex s-min-w-0 s-grow s-flex-col s-gap-0">
           <div className="s-flex s-min-w-0 s-grow s-flex-col s-text-foreground dark:s-text-foreground-night sm:s-flex-row sm:s-gap-3">
-            <div className="s-heading-base s-min-w-0 s-overflow-hidden s-text-ellipsis s-whitespace-nowrap">
+            <div
+              className={cn(
+                "s-heading-base",
+                truncateSubElement
+                  ? "s-shrink-0"
+                  : "s-min-w-0 s-overflow-hidden s-text-ellipsis s-whitespace-nowrap"
+              )}
+            >
               {title}
             </div>
             {subElement &&
@@ -67,7 +74,7 @@ export function ContextItem({
                   </div>
                 </div>
               ) : (
-                <div className="s-flex s-flex-shrink-0 s-items-center s-gap-3 s-overflow-hidden s-text-sm s-text-muted-foreground dark:s-text-muted-foreground-night">
+                <div className="s-flex s-items-center s-gap-3 s-overflow-hidden s-text-sm s-text-muted-foreground dark:s-text-muted-foreground-night">
                   {subElement}
                 </div>
               ))}
