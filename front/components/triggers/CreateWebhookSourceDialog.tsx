ddf27d159b8ad1b45fbbb4ff4dc44e81cdc2d8a0
Fabien Celier <17087544+fabiencelier@users.noreply.github.com>
2025-09-23 16:18:45 +0200

    [HOOTL] Generate a secret for the webhook in Dust (#16125)

--- front/components/triggers/CreateWebhookSourceDialog.tsx
+++ front/components/triggers/CreateWebhookSourceDialog.tsx
@@ -35,6 +35,7 @@ export function CreateWebhookSourceDialog({
     defaultValues: {
       name: "",
       secret: "",
+      autoGenerate: true,
       signatureHeader: "",
       signatureAlgorithm: "sha256",
       customHeaders: null,
