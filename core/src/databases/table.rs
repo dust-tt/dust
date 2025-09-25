b3c85bc2fc9dd299d7d079ab61d0f45d02d9b557
Sebastien Flory <sflory@gmail.com>
2025-09-25 18:03:44 +0200

    Fix: also set schema_stale_at to none when setting the schema

--- core/src/databases/table.rs
+++ core/src/databases/table.rs
@@ -235,6 +235,7 @@ impl Table {
     }
     pub fn set_schema(&mut self, schema: TableSchema) {
         self.schema = Some(schema);
+        self.schema_stale_at = None;
     }
     pub fn set_remote_database_secret_id(&mut self, remote_database_secret_id: String) {
         self.remote_database_secret_id = Some(remote_database_secret_id);
