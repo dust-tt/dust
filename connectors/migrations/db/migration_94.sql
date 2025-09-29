dd50231a2a6906972958d6f462c644d39363d058
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-09-29 09:13:12 +0200

    fix migration script

--- connectors/migrations/db/migration_94.sql
+++ connectors/migrations/db/migration_94.sql
@@ -1,3 +1,2 @@
 -- Migration created on Sep 29, 2025
-CREATE INDEX "notion_connector_block_cache_entries_connector_id_workflow_id" ON "notion_connector_block_cache_entries" ("connectorId", "workflowId");
-[09:11:49.442] [32mINFO[39m (94930): [36mDone[39m;
+CREATE INDEX CONCURRENTLY "notion_connector_block_cache_entries_connector_id_workflow_id" ON "notion_connector_block_cache_entries" ("connectorId", "workflowId");
