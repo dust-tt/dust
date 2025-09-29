31667b71b573abc695333c27d5364094de69fa43
ykmsd <yuka@dust.tt>
2025-09-29 22:08:26 +0200

    add animation hook

---
+++ sparkle/src/components/markdown/useAnimatedText.ts
@@ -0,0 +1,36 @@
+import { animate } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const delimiter = /(?=[\s\S])/;
+
+export function useAnimatedText(text: string, shouldAnimate: boolean) {
+  const [cursor, setCursor] = useState(0);
+  const [startingCursor, setStartingCursor] = useState(0);
+  const [prevText, setPrevText] = useState(text);
+
+  if (prevText !== text) {
+    setPrevText(text);
+    setStartingCursor(cursor);
+  }
+
+  useEffect(() => {
+    if (shouldAnimate) {
+      const textParts = text.split(delimiter);
+      const controls = animate(startingCursor, textParts.length, {
+        duration: 4,
+        ease: "easeOut",
+        onUpdate(latest) {
+          setCursor(Math.floor(latest));
+        },
+      });
+
+      return () => controls.stop();
+    }
+  }, [startingCursor, text]);
+
+  if (!shouldAnimate) {
+    return text;
+  }
+
+  return text.split(delimiter).slice(0, cursor).join("");
+}
