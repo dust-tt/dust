c7f60c90e0b1694b9d4ec3630fe85ce740369672
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-09-29 14:18:41 +0200

    add migration script to drop workspace domain from DB and WorkOS

---
+++ front/migrations/20250929_drop_domain.ts
@@ -0,0 +1,56 @@
+import { removeWorkOSOrganizationDomain } from "@app/lib/api/workos/organization";
+import { WorkspaceModel } from "@app/lib/resources/storage/models/workspace";
+import { WorkspaceHasDomainModel } from "@app/lib/resources/storage/models/workspace_has_domain";
+import { renderLightWorkspaceType } from "@app/lib/workspace";
+import { makeScript } from "@app/scripts/helpers";
+
+makeScript(
+  {
+    domain: {
+      type: "string",
+    },
+    workspaceId: {
+      type: "string",
+    },
+  },
+  async ({ domain, execute }, logger) => {
+    const existingDomainInRegion = await WorkspaceHasDomainModel.findOne({
+      where: { domain },
+      include: [
+        {
+          model: WorkspaceModel,
+          as: "workspace",
+          required: true,
+        },
+      ],
+    });
+
+    if (!existingDomainInRegion) {
+      logger.error({ domain }, "Domain not found");
+      return;
+    }
+
+    logger.info(
+      {
+        domain,
+        workspaceId: existingDomainInRegion.workspace.id,
+      },
+      "Dropping existing domain"
+    );
+
+    const { workspace } = existingDomainInRegion;
+
+    if (execute) {
+      // Delete the domain from the DB.
+      await existingDomainInRegion.destroy();
+
+      // Delete the domain from WorkOS.
+      await removeWorkOSOrganizationDomain(
+        renderLightWorkspaceType({ workspace }),
+        {
+          domain,
+        }
+      );
+    }
+  }
+);
