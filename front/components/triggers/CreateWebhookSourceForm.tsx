2b224966db530209882293ac8e1524b1129e3be0
Pierre Milliotte <pierre@dust.tt>
2025-10-03 14:21:20 +0200

    feat: move secret advanced settings in collapsible component

--- front/components/triggers/CreateWebhookSourceForm.tsx
+++ front/components/triggers/CreateWebhookSourceForm.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   ChevronDownIcon,
+  CollapsibleComponent,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -125,50 +126,60 @@ export function CreateWebhookSourceFormContent({
         )}
       </div>
 
-      <Controller
-        control={form.control}
-        name="signatureHeader"
-        render={({ field }) => (
-          <Input
-            {...field}
-            label="Signature Header"
-            placeholder="Signature header..."
-            isError={form.formState.errors.signatureHeader !== undefined}
-            message={form.formState.errors.signatureHeader?.message}
-            messageStatus="error"
-          />
-        )}
-      />
-
-      <div className="flex items-center justify-between space-y-2">
-        <Label>Signature Algorithm</Label>
-        <Controller
-          control={form.control}
-          name="signatureAlgorithm"
-          render={({ field }) => (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  label={field.value}
-                  variant="outline"
-                  className="!mt-0"
-                  icon={ChevronDownIcon}
+      <CollapsibleComponent
+        rootProps={{ defaultOpen: false }}
+        triggerProps={{
+          label: "Secret advanced settings",
+          variant: "secondary",
+        }}
+        contentChildren={
+          <div className="flex flex-col space-y-2">
+            <Controller
+              control={form.control}
+              name="signatureHeader"
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  label="Signature Header"
+                  placeholder="Signature header..."
+                  isError={form.formState.errors.signatureHeader !== undefined}
+                  message={form.formState.errors.signatureHeader?.message}
+                  messageStatus="error"
                 />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                {WEBHOOK_SOURCE_SIGNATURE_ALGORITHMS.map((algorithm) => (
-                  <DropdownMenuItem
-                    key={algorithm}
-                    onClick={() => field.onChange(algorithm)}
-                  >
-                    {algorithm}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
-        />
-      </div>
+              )}
+            />
+            <div className="flex items-center justify-between space-y-2">
+              <Label>Signature Algorithm</Label>
+              <Controller
+                control={form.control}
+                name="signatureAlgorithm"
+                render={({ field }) => (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        label={field.value}
+                        variant="outline"
+                        className="!mt-0"
+                        icon={ChevronDownIcon}
+                      />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {WEBHOOK_SOURCE_SIGNATURE_ALGORITHMS.map((algorithm) => (
+                        <DropdownMenuItem
+                          key={algorithm}
+                          onClick={() => field.onChange(algorithm)}
+                        >
+                          {algorithm}
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                )}
+              />
+            </div>
+          </div>
+        }
+      />
 
       <Controller
         control={form.control}
