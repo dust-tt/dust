dde000ae6cb53a38826a638509b814dc1e4a135f
Thomas Draier <thomas@dust.tt>
2025-10-09 17:34:34 +0200

    Add subscribe check page

--- extension/ui/pages/routes.tsx
+++ extension/ui/pages/routes.tsx
@@ -2,6 +2,7 @@ import { ProtectedRoute } from "@app/ui/components/auth/ProtectedRoute";
 import { LoginPage } from "@app/ui/pages/LoginPage";
 import { MainPage } from "@app/ui/pages/MainPage";
 import { RunPage } from "@app/ui/pages/RunPage";
+import { SubscribePage } from "@app/ui/pages/SubscribePage";
 
 export const routes = [
   {
@@ -11,6 +12,20 @@ export const routes = [
   {
     path: "/run",
     element: <ProtectedRoute>{() => <RunPage />}</ProtectedRoute>,
+  },
+  {
+    path: "/subscribe",
+    element: (
+      <ProtectedRoute>
+        {({ user, workspace, handleLogout }) => (
+          <SubscribePage
+            user={user}
+            workspace={workspace}
+            handleLogout={handleLogout}
+          />
+        )}
+      </ProtectedRoute>
+    ),
   },
   // Keep catch-all route last. This will handle both root path and /conversations/:conversationId
   // Since we catch all, we will need to manually handle the conversationId in the MainPage component.
