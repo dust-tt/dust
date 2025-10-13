f01bb9b96014bd6310dffe450d9be83db5700d09
Thomas Draier <thomas@dust.tt>
2025-10-13 16:54:31 +0200

    remove files

--- teams-app/src/internal/interface.ts
+++
@@ -1,6 +0,0 @@
-import { TurnState } from "@microsoft/teams-ai";
-
-export interface ConversationState {
-  count: number;
-}
-export type ApplicationTurnState = TurnState<ConversationState>;
--- teams-app/src/teamsBot.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "@microsoft/teams-ai";
-import { MemoryStorage } from "botbuilder";
-import { ApplicationTurnState } from "./internal/interface";
-
-// Define storage and application
-const storage = new MemoryStorage();
-export const app = new Application<ApplicationTurnState>({
-  storage,
-});
