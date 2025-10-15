3475a668dd83f3164a3b2792c5a7f84c4492e323
Leandre <leandre@dust.tt>
2025-10-15 11:59:31 +0200

    Created a hard-coded platform privacy page hosted by dust.tt (not notion anymore)

--- front/next.config.js
+++ front/next.config.js
@@ -55,8 +55,7 @@ const config = {
       },
       {
         source: "/platform-privacy",
-        destination:
-          "https://dust-tt.notion.site/Platform-Privacy-Policy-c75f1cd20df04c58872f6aa43f768d41?pvs=74",
+        destination: "/home/platform-privacy",
         permanent: true,
       },
       {
