0bc072048b930d48a7d356a64911f91de93940be
Stanislas Polu <spolu@dust.tt>
2025-10-16 14:52:03 +0200

    Dust Apps: hide them if not gated in

--- front/lib/actions/mcp_internal_actions/constants.ts
+++ front/lib/actions/mcp_internal_actions/constants.ts
@@ -317,10 +317,9 @@ The directive should be used to display a clickable version of the agent name in
     id: 10,
     availability: "auto",
     allowMultipleInstances: true,
-    isRestricted: undefined,
-    // isRestricted: ({ featureFlags }) => {
-    //   return !featureFlags.includes("legacy_dust_apps");
-    // },
+    isRestricted: ({ featureFlags }) => {
+      return !featureFlags.includes("legacy_dust_apps");
+    },
     isPreview: false,
     tools_stakes: undefined,
     tools_retry_policies: undefined,
