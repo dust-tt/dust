bb61fce17b2479caf169c8097a1e51f76d2463ba
Flavien David <flavien.david74@gmail.com>
2025-10-17 15:14:12 +0200

    Add tool to retrieve interactive content share url (#17008)
    * Add tool to retrieve interactive content share url
    
    * ✨

--- front/lib/actions/mcp_internal_actions/servers/interactive_content/types.ts
+++ front/lib/actions/mcp_internal_actions/servers/interactive_content/types.ts
@@ -8,3 +8,5 @@ export const REVERT_INTERACTIVE_CONTENT_FILE_TOOL_NAME =
   "revert_interactive_content_file";
 export const RENAME_INTERACTIVE_CONTENT_FILE_TOOL_NAME =
   "rename_interactive_content_file";
+export const GET_INTERACTIVE_CONTENT_FILE_SHARE_URL_TOOL_NAME =
+  "get_interactive_content_file_share_url";
