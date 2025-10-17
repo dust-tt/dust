09564b37c12cfa01cf192651871d86c558672f94
JulesBelveze <jules.belveze@hotmail.fr>
2025-10-17 09:48:28 +0200

    [front/agent_builder/observability] - feature: add color constants and display limitations for tools
     - Introduce a set of color constants to be used for tool representation in the UI
     - Define a maximum number of tools to be displayed and a percentage multiplier constant for calculations

--- front/components/agent_builder/observability/constants.ts
+++ front/components/agent_builder/observability/constants.ts
@@ -31,3 +31,19 @@ export const VERSION_MARKER_STYLE = {
   labelOffsetBase: 10,
   labelOffsetIncrement: 15,
 } as const;
+
+export const TOOL_COLORS = [
+  "text-blue-500",
+  "text-emerald-500",
+  "text-amber-500",
+  "text-purple-500",
+  "text-pink-500",
+  "text-cyan-500",
+  "text-orange-500",
+  "text-teal-500",
+  "text-indigo-500",
+  "text-rose-500",
+] as const;
+
+export const MAX_TOOLS_DISPLAYED = 10;
+export const PERCENTAGE_MULTIPLIER = 100;
