293b950d35ee24b1503aaf5dfc68800768ceb26b
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-10-17 17:25:34 +0200

    revert sql file

--- front/migrations/db/migration_384.sql
+++ front/migrations/db/migration_384.sql
@@ -1,2 +1,2 @@
 -- Migration created on Oct 17, 2025
-CREATE INDEX CONCURRENTLY "memberships_userId_fk" ON public.memberships USING btree ("userId")
+DROP INDEX CONCURRENTLY "memberships_userId_fk";
