52dcf851e6d224f7f01138140c98ae597c59f211
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-10-20 21:50:34 +0200

    move

--- front/lib/actions/mcp_internal_actions/constants.ts
+++ front/lib/actions/mcp_internal_actions/constants.ts
@@ -161,30 +161,6 @@ export const INTERNAL_MCP_SERVERS = {
       instructions: null,
     },
   },
-  ashby: {
-    id: 37,
-    availability: "manual",
-    allowMultipleInstances: false,
-    isRestricted: ({ featureFlags }) => {
-      return !featureFlags.includes("ashby_tool");
-    },
-    isPreview: false,
-    tools_stakes: {
-      list_candidates: "never_ask",
-    },
-    tools_retry_policies: undefined,
-    timeoutMs: undefined,
-    serverInfo: {
-      name: "ashby",
-      version: "1.0.0",
-      description: "Access and manage Ashby ATS data.",
-      authorization: null,
-      icon: "GithubLogo",
-      documentationUrl: null,
-      instructions: null,
-      requiresSecret: true,
-    },
-  },
   image_generation: {
     id: 2,
     availability: "auto",
@@ -1178,6 +1154,30 @@ The directive should be used to display a clickable version of the agent name in
       icon: "ActionMegaphoneIcon",
       documentationUrl: null,
       instructions: null,
+    },
+  },
+  ashby: {
+    id: 38,
+    availability: "manual",
+    allowMultipleInstances: false,
+    isRestricted: ({ featureFlags }) => {
+      return !featureFlags.includes("ashby_tool");
+    },
+    isPreview: false,
+    tools_stakes: {
+      list_candidates: "never_ask",
+    },
+    tools_retry_policies: undefined,
+    timeoutMs: undefined,
+    serverInfo: {
+      name: "ashby",
+      version: "1.0.0",
+      description: "Access and manage Ashby ATS data.",
+      authorization: null,
+      icon: "GithubLogo",
+      documentationUrl: null,
+      instructions: null,
+      requiresSecret: true,
     },
   },
   [SEARCH_SERVER_NAME]: {
