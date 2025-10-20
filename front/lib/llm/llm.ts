1742d5a72604608b06bc9a527ef1c1a8f1396a15
Pierre Milliotte <pierre@dust.tt>
2025-10-20 14:49:45 +0200

    feat: add specifications as llm stream arguments

--- front/lib/llm/llm.ts
+++ front/lib/llm/llm.ts
@@ -1,4 +1,5 @@
-import type { LLMEvent } from "@app/lib/llm/types";
+import type { AgentActionSpecification } from "@app/lib/actions/types/agent";
+import type { LLMEvent } from "@app/lib/llm/types/events";
 import type {
   ModelConfigurationType,
   ModelConversationTypeMultiActions,
@@ -14,8 +15,10 @@ export abstract class LLM {
   abstract stream({
     conversation,
     prompt,
+    specifications,
   }: {
     conversation: ModelConversationTypeMultiActions;
     prompt: string;
+    specifications: AgentActionSpecification[];
   }): AsyncGenerator<LLMEvent>;
 }
