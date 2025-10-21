44ad4980cc0504b7128c16e216e6d2e428185b17
Anas Lecaillon <anas@dust.tt>
2025-10-21 10:40:49 +0200

    noop

---
+++ front/lib/llm/providers/noop.ts
@@ -0,0 +1,49 @@
+import { LLM } from "@app/lib/llm/llm";
+import type { LLMEvent } from "@app/lib/llm/types";
+import type { ModelConfigurationType, ModelConversationTypeMultiActions } from "@app/types";
+
+// NoopLLM is a dummy LLM that simply returns "Soupinou!"
+export class NoopLLM extends LLM {
+    constructor({
+        model,
+    }: {
+        model: ModelConfigurationType;
+    }) {
+        
+        super(model);
+    }
+
+    async *stream({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        conversation,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        prompt,
+    }: {
+        conversation: ModelConversationTypeMultiActions;
+        prompt: string;
+    }): AsyncGenerator<LLMEvent> {
+        
+        yield {
+            type: "text_delta",
+            content: {
+                delta: "Soupinou!",
+            },
+            metadata: {
+                providerId: "noop",
+                modelId: "dummy_model"
+            }
+        };
+    }
+
+    async *modelStream({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        conversation,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        prompt,
+    }: {
+        conversation: ModelConversationTypeMultiActions;
+        prompt?: string;
+    }): AsyncGenerator<string> {
+        yield "Soupinou!";
+    }
+}
