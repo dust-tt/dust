46735763b9890e0b8949d1b667b83674e8d172b7
Flavien David <flavien@dust.tt>
2025-10-21 11:17:32 +0200

    Make optional in SDK JS

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -843,7 +843,7 @@ const LightAgentConfigurationSchema = z.object({
   usage: AgentUsageTypeSchema.optional(),
   maxStepsPerRun: z.number(),
   // TODO(2025-10-20 flav): Remove once SDK JS does not rely on it anymore.
-  visualizationEnabled: z.boolean(),
+  visualizationEnabled: z.boolean().optional(),
   templateId: z.string().nullable(),
   groupIds: z.array(z.string()).optional(),
   requestedGroupIds: z.array(z.array(z.string())),
