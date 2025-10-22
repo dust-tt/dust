92c8c169f1a66a423cc91fe8458267e541f7fc64
Pierre Milliotte <pierre@dust.tt>
2025-10-22 11:30:35 +0200

    feat: route to mistral direct llm

--- front/lib/api/llm/index.ts
+++ front/lib/api/llm/index.ts
@@ -1,7 +1,9 @@
+import { MistralLLM } from "@app/lib/api/llm/clients/mistral";
 import type { LLM } from "@app/lib/api/llm/llm";
 import type { LLMOptions } from "@app/lib/api/llm/types/options";
 import type { Authenticator } from "@app/lib/auth";
 import { getFeatureFlags } from "@app/lib/auth";
+import { SUPPORTED_MODEL_CONFIGS } from "@app/types";
 import type { ModelIdType } from "@app/types/assistant/models/types";
 
 export async function getLLM(
@@ -15,9 +17,19 @@ export async function getLLM(
   }
 ): Promise<LLM | null> {
   const featureFlags = await getFeatureFlags(auth.getNonNullableWorkspace());
-  const _hasFeature = featureFlags.includes("llm_router_direct_requests");
+  const hasFeature = featureFlags.includes("llm_router_direct_requests");
+
+  const modelConfiguration = SUPPORTED_MODEL_CONFIGS.find(
+    (config) => config.modelId === modelId
+  );
+
+  if (!modelConfiguration) {
+    return null;
+  }
 
   switch (modelId) {
+    case "mistral-large-latest":
+      return hasFeature ? new MistralLLM({ model: modelConfiguration }) : null;
     default:
       return null;
   }
