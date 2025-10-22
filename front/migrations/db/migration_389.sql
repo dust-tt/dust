f7c5f65ae9e3362f46e075138e815a3357eaf940
Flavien David <flavien@dust.tt>
2025-10-22 15:52:23 +0200

    Add missing index

--- front/migrations/db/migration_389.sql
+++ front/migrations/db/migration_389.sql
@@ -4,6 +4,8 @@ DROP INDEX IF EXISTS "conversations_s_id";
 DROP INDEX IF EXISTS "conversations_wId_idx";
 
 -- Table conversation_participants
+CREATE UNIQUE INDEX CONCURRENTLY "conversation_participants_workspace_id_conversation_id" ON "conversation_participants" ("workspaceId", "conversationId");
+
 DROP INDEX IF EXISTS "conversation_participants_conversation_id";
 DROP INDEX IF EXISTS "conversation_participants_user_id_action";
 DROP INDEX IF EXISTS "conversation_participants_user_id_conversation_id";
