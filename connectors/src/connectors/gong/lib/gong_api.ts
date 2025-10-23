36a936a4295abdc5a4d6d8b3742da405d4f015a3
Yuka Masuda <yuka@dust.tt>
2025-10-23 14:26:44 +0200

    Update connectors/src/connectors/gong/lib/gong_api.ts
    Co-authored-by: Rémy-Christophe Schermesser <git@schermesser.com>

--- connectors/src/connectors/gong/lib/gong_api.ts
+++ connectors/src/connectors/gong/lib/gong_api.ts
@@ -184,6 +184,7 @@ const GongPaginatedResults = <C extends t.Mixed, F extends string>(
   ]);
 
 // https://help.gong.io/docs/what-the-gong-api-provides
+// The doc says 3/sec, let's be pessimistic and do 3/2 seconds
 const GONG_RATE_LIMIT = { limit: 3, windowInMs: 2_000 };
 
 export class GongClient {
