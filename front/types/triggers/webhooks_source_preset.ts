40e26ad020e6ab18694e3885248bf8640e99ba03
adrien <adrisim2001@gmail.com>
2025-10-28 15:11:32 +0100

    [front] hootl: blacklist events per provider (#17384)
    * [front] hootl: blacklist events per provider
    
    * cleanup
    
    * review feedback

--- front/types/triggers/webhooks_source_preset.ts
+++ front/types/triggers/webhooks_source_preset.ts
@@ -20,24 +20,45 @@ export type EventCheck = {
 };
 
 export type WebhookEvent = {
+  // A human-readable name for the event.
   name: string;
+
+  // The value sent by the webhook provider to identify this event.
   value: string;
   description: string;
+
+  // The JSON schema describing the payload sent by the webhook for this event.
   schema: JSONSchema;
 };
 
 export type PresetWebhook<P extends WebhookProvider = WebhookProvider> = {
   name: string;
+  description: string;
+  icon: InternalAllowedIconType | CustomResourceIconType;
+
+  // How to check incoming webhook requests to determine which event they correspond to.
+  // It's made of a type (headers or body) and a field (header name or body property name)
+  // to match against the event values defined below.
+  // For example, GitHub uses a specific header "X-GitHub-Event" to indicate the event type.
   eventCheck: EventCheck;
   events: WebhookEvent[];
-  icon: InternalAllowedIconType | CustomResourceIconType;
-  description: string;
+  // List of event values to ignore. For example, GitHub sends a "ping" event when a webhook is created.
+  // We will not want to implement it, and don't want to throw errors when receiving it.
+  event_blacklist?: string[];
+
+  // Optional URL to the webhook provider's webhook management page.
   webhookPageUrl?: string;
-  featureFlag?: WhitelistableFeature;
+
+  // The service that will handle creating the webhooks for a given provider.
+  // Likely implements OAuth flow to manage webhooks on behalf of users.
   webhookService: RemoteWebhookService<P>;
+
+  // React components to render the webhook details and creation form.
   components: {
     detailsComponent: React.ComponentType<WebhookDetailsComponentProps>;
     createFormComponent: React.ComponentType<WebhookCreateFormComponentProps>;
     oauthExtraConfigInput?: React.ComponentType<ConnectorOauthExtraConfigProps>;
   };
+
+  featureFlag?: WhitelistableFeature;
 };
