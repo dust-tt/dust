596cb26306b321b417496131cedeee00055cba82
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-11-05 21:51:31 +0100

    remove comment

--- front/lib/triggers/temporal/webhook/activities.ts
+++ front/lib/triggers/temporal/webhook/activities.ts
@@ -265,7 +265,6 @@ export async function runTriggerWebhookActivity({
       `workspace_id:${workspaceId}`,
       `trigger_id:${trigger.sId}`,
     ];
-    // Filter triggers by payload matching
     statsDClient.increment("webhook_filter.events_processed.count", 1, tags);
 
     const parsedFilterResult = parseMatcherExpression(filter);
@@ -285,7 +284,6 @@ export async function runTriggerWebhookActivity({
     const payloadMatchesFilter = matchPayload(body, parsedFilterResult.value);
     if (payloadMatchesFilter) {
       statsDClient.increment("webhook_filter.events_passed.count", 1, tags);
-
       filteredTriggers.push(trigger);
     }
   }
