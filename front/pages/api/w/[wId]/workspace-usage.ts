d04ce2f6b128b5ed0d779bb97b20842b0f6b41cb
Philippe Rolet <philipperolet@gmail.com>
2025-11-06 17:03:39 +0100

    remove as const

--- front/pages/api/w/[wId]/workspace-usage.ts
+++ front/pages/api/w/[wId]/workspace-usage.ts
@@ -31,7 +31,8 @@ const usageTables = [
   "assistants",
   "feedback",
   "all",
-] as const;
+];
+
 type usageTableType = (typeof usageTables)[number];
 
 function getSupportedUsageTablesCodec(): t.Mixed {
