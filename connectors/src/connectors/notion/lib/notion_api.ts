9dfb74e1ad40d6ddcc17315146dbc3489779bb30
David Ebbo <david.ebbo@gmail.com>
2025-11-10 16:14:47 +0100

    Only give up on Notion search after too many 504s and 400s (#17779)
    This is a fine tuning of #17764 to not give up until several activity attempts.

--- connectors/src/connectors/notion/lib/notion_api.ts
+++ connectors/src/connectors/notion/lib/notion_api.ts
@@ -212,24 +212,6 @@ export async function getPagesAndDatabasesEditedSince({
       );
       tries += 1;
       if (tries >= retry.retries) {
-        // We've observed some Notion workspaces where the search API consistently fails. This happens
-        // in two different ways:
-        // - A 400 "The start_cursor provided is invalid" error
-        // - A 504 Gateway Timeout error after 60 seconds
-        // I'm guessing in most cases they are the same, and the 504 would have eventually resulted
-        // in a 400 if it didn't get cut off first.
-        // Since we've already retried a few times, we just log a warning and return an empty result
-        // set to avoid blocking the workflow entirely.
-        if (
-          APIResponseError.isAPIResponseError(e) &&
-          (e.status === 400 || e.status === 504)
-        ) {
-          tryLogger.warn(
-            { error: e },
-            "Ignoring repeated 400 or 504 errors for Notion search API."
-          );
-          return { pages: [], dbs: [], nextCursor: null };
-        }
         throw e;
       }
 
