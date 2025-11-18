6e816908dba37d493d98d149e201d43b90116722
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-11-18 14:19:17 +0100

    [front] Bump number of concurrent webhook trigger activities (#18027)

--- front/lib/triggers/temporal/webhook/worker.ts
+++ front/lib/triggers/temporal/webhook/worker.ts
@@ -15,7 +15,7 @@ export async function runAgentTriggerWebhookWorker() {
     workflowsPath: require.resolve("./workflows"),
     activities,
     taskQueue: QUEUE_NAME,
-    maxConcurrentActivityTaskExecutions: 2,
+    maxConcurrentActivityTaskExecutions: 4,
     connection,
     namespace,
     interceptors: {
