e896d5be0618072c3aa8efbcdd14689da0f9b110
Jean-David Fiquet <jd@dust.tt>
2025-11-20 14:44:52 +0100

    [front] feat: PPUL - Add credit list table and cleanup components

---
+++ front/lib/swr/credits.ts
@@ -0,0 +1,28 @@
+import type { Fetcher } from "swr";
+
+import { fetcher, useSWRWithDefaults } from "@app/lib/swr/swr";
+import type { GetCreditsResponseBody } from "@app/types/credits";
+
+export function useCredits({
+  workspaceId,
+  disabled,
+}: {
+  workspaceId: string;
+  disabled?: boolean;
+}) {
+  const creditsFetcher: Fetcher<GetCreditsResponseBody> = fetcher;
+
+  const { data, error, mutate, isValidating } = useSWRWithDefaults(
+    `/api/w/${workspaceId}/credits`,
+    creditsFetcher,
+    { disabled }
+  );
+
+  return {
+    credits: data?.credits ?? [],
+    isCreditsLoading: !error && !data && !disabled,
+    isCreditsValidating: isValidating,
+    isCreditsError: error,
+    mutateCredits: mutate,
+  };
+}
