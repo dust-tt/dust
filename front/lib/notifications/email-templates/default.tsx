904ca1574746a7465dd5fa6adb8d3e3ca9cffd39
Sebastien Flory <sflory@gmail.com>
2025-11-21 11:28:15 +0100

    Update template and use digest for conversations notifications.

--- front/lib/notifications/email-templates/default.tsx
+++ front/lib/notifications/email-templates/default.tsx
@@ -2,16 +2,23 @@ import { Html } from "@react-email/html";
 import { render } from "@react-email/render";
 import Head from "next/head";
 import * as React from "react";
+import { z } from "zod";
 
-interface DefaultEmailTemplateProps {
-  name: string;
-  content: string;
-  avatarUrl?: string;
-  action?: {
-    label: string;
-    url: string;
-  };
-}
+export const DefaultEmailTemplatePropsSchema = z.object({
+  name: z.string(),
+  content: z.string(),
+  avatarUrl: z.string().optional(),
+  action: z
+    .object({
+      label: z.string(),
+      url: z.string(),
+    })
+    .optional(),
+});
+
+type DefaultEmailTemplateProps = z.infer<
+  typeof DefaultEmailTemplatePropsSchema
+>;
 
 const DefaultEmailTemplate = ({
   name,
@@ -19,31 +26,69 @@ const DefaultEmailTemplate = ({
   action,
 }: DefaultEmailTemplateProps) => {
   return (
-    <Html style={{ backgroundColor: "#ffffff" }}>
+    <Html style={{ backgroundColor: "#e9f7ff" }}>
       <Head>
         <title>An email from Dust</title>
       </Head>
-      <body>
-        <div style={{ margin: "0 auto", maxWidth: "800px" }}>
-          <div style={{ width: "100%", textAlign: "center" }}>
+      <body
+        style={{
+          fontFamily: "Open Sans, Helvetica Neue, Helvetica, Arial, sans-serif",
+          fontSize: "14px",
+        }}
+      >
+        <div
+          style={{ width: "100%", textAlign: "center", marginBottom: "20px" }}
+        >
+          <a
+            href={process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL}
+            target="_new"
+          >
             <img
               alt="Dust Logo"
-              style={{ margin: "0 auto" }}
-              width={128}
+              style={{ margin: "0 auto", border: "0px" }}
+              width={192}
+              height={48}
               src="https://dust.tt/static/landing/logos/dust/Dust_Logo.png"
             />
-          </div>
-          <h1>Hi {name},</h1>
+          </a>
+        </div>
+        <div
+          style={{
+            margin: "0 auto",
+            maxWidth: "500px",
+            backgroundColor: "#ffffff",
+            borderRadius: "20px",
+            padding: "20px",
+          }}
+        >
+          <h3>Hi {name},</h3>
           {content.split("\n").map((line, index) => (
             <div key={index}>{line}</div>
           ))}
-          <hr className="border-gray-200" />
 
-          {action && (
-            <a href={action.url} target="_blank">
-              {action.label}
-            </a>
+          {action?.label && action?.url && (
+            <>
+              <hr style={{ border: "1px solid #e0e0e0" }} />
+              <a href={action.url} target="_blank">
+                {action.label}
+              </a>
+            </>
           )}
+        </div>
+        <div
+          style={{
+            width: "100%",
+            textAlign: "center",
+            marginTop: "20px",
+            fontSize: "12px",
+          }}
+        >
+          <div>This is an automated email. Please do not reply. </div>
+          <div>
+            <a href={process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL}>
+              Manage your notifications in your profile settings.
+            </a>
+          </div>
         </div>
       </body>
     </Html>
--- front/lib/notifications/workflows/conversation-unread.ts
+++ front/lib/notifications/workflows/conversation-unread.ts
@@ -11,6 +11,7 @@ import type { UserMessageOrigin } from "@app/types";
 import {
   assertNever,
   isContentFragmentType,
+  isDevelopment,
   isUserMessageType,
 } from "@app/types";
 
@@ -225,11 +226,27 @@ export const conversationUnreadWorkflow = workflow(
       }
     );
 
-    await step.delay("delay", () => ({
-      type: "regular",
-      amount: 3,
-      unit: "hours",
-    }));
+    await step.digest(
+      "digest",
+      async () => {
+        return isDevelopment()
+          ? {
+              amount: 3,
+              unit: "minutes",
+              digestKey: payload.conversationId,
+            }
+          : {
+              amount: 1,
+              unit: "hours",
+              digestKey: payload.conversationId,
+            };
+      },
+      {
+        // No email from trigger until we give more control over the notification to the users.
+        skip: async () =>
+          (await shouldSkipConversation(payload)) || details.isFromTrigger,
+      }
+    );
 
     await step.email(
       "send-email",
@@ -237,11 +254,7 @@ export const conversationUnreadWorkflow = workflow(
         const body = await renderEmail({
           name: details.recipentFullname,
           avatarUrl: details.avatarUrl,
-          content:
-            "You have a new unread message from " +
-            details.author +
-            ":\n\n" +
-            details.previewText,
+          content: `You have unread message(s) in the conversation ${details.subject}.`,
           action: {
             label: "Open in Dust",
             url:
@@ -250,7 +263,7 @@ export const conversationUnreadWorkflow = workflow(
           },
         });
         return {
-          subject: `[Dust] ${details.subject} (new message)`,
+          subject: "[Dust] New unread message(s)",
           body,
         };
       },
