f236b62a70eb872118ba04f0a4631b85022f9cee
Flavien David <flavien@dust.tt>
2025-11-23 18:03:35 +0100

    Improve Slack unauthorized logs

--- connectors/src/connectors/slack/lib/workspace_limits.ts
+++ connectors/src/connectors/slack/lib/workspace_limits.ts
@@ -343,6 +343,7 @@ export async function notifyIfSlackUserIsNotAllowed(
         connectorId: connector.id,
         slackInfos,
         slackUserEmail: getSlackUserEmailFromProfile(slackUserInfo),
+        slackUserInfo,
       },
       "Unauthorized Slack user attempted to access webhook."
     );
