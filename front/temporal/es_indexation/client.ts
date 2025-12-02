fb1d0703d2f52d63d8b016ea5de90554fdf68649
Stanislas Polu <spolu@dust.tt>
2025-12-02 13:40:35 +0100

    imports sadness

--- front/temporal/es_indexation/client.ts
+++ front/temporal/es_indexation/client.ts
@@ -2,10 +2,11 @@ import { getTemporalClientForFrontNamespace } from "@app/lib/temporal";
 import logger from "@app/logger/logger";
 import { QUEUE_NAME } from "@app/temporal/es_indexation/config";
 import { makeIndexUserSearchWorkflowId } from "@app/temporal/es_indexation/helpers";
-import { indexUserSearchSignal } from "@app/temporal/es_indexation/signals";
-import { indexUserSearchWorkflow } from "@app/temporal/es_indexation/workflows";
 import type { Result } from "@app/types";
 import { Err, normalizeError, Ok } from "@app/types";
+
+import { indexUserSearchSignal } from "./signals";
+import { indexUserSearchWorkflow } from "./workflows";
 
 export async function launchIndexUserSearchWorkflow({
   userId,
