b99190b29237e16a6025b4ac926fdcdc4c1d55e5
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-12-04 17:24:39 +0100

    use BaseFormFieldSection's title instead of re-adding one

--- front/components/skill_builder/SkillBuilderDescriptionSection.tsx
+++ front/components/skill_builder/SkillBuilderDescriptionSection.tsx
@@ -4,26 +4,27 @@ import { BaseFormFieldSection } from "@app/components/agent_builder/capabilities
 
 // TODO(skills): see if we can reuse DescriptionSection more or less directly.
 
+const DESCRIPTION_FIELD_NAME = "description";
+
 export function SkillBuilderDescriptionSection() {
   return (
-    <section className="flex flex-col gap-3">
-      {/* TODO(skills): double check the copy. */}
-      <h2 className="heading-lg text-foreground dark:text-foreground-night">
-        What will this skill be used for?
-      </h2>
-      <BaseFormFieldSection<HTMLTextAreaElement> fieldName="description">
-        {({ registerRef, registerProps, onChange, errorMessage }) => (
-          <TextArea
-            ref={registerRef}
-            placeholder="When should this skill be used? What will this skill be good for?"
-            className="min-h-24"
-            onChange={onChange}
-            error={errorMessage}
-            showErrorLabel
-            {...registerProps}
-          />
-        )}
-      </BaseFormFieldSection>
-    </section>
+    <BaseFormFieldSection
+      // TODO(skills): double check the copy.
+      title="What will this skill be used for?"
+      fieldName={DESCRIPTION_FIELD_NAME}
+      triggerValidationOnChange={false}
+    >
+      {({ registerRef, registerProps, onChange, errorMessage }) => (
+        <TextArea
+          ref={registerRef}
+          placeholder="When should this skill be used? What will this skill be good for?"
+          className="min-h-24"
+          onChange={onChange}
+          error={errorMessage}
+          showErrorLabel
+          {...registerProps}
+        />
+      )}
+    </BaseFormFieldSection>
   );
 }
