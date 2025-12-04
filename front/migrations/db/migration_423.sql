a68fa6143bc2271efe3e090873d37441f5670b40
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-12-04 09:22:25 +0100

    format

--- front/migrations/db/migration_423.sql
+++ front/migrations/db/migration_423.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS "skill_configurations" (
-    "id" BIGSERIAL PRIMARY KEY,
-    "workspaceId" BIGINT NOT NULL REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    "version" INTEGER NOT NULL DEFAULT 0,
-    "status" VARCHAR(255) NOT NULL DEFAULT 'active',
-    "scope" VARCHAR(255) NOT NULL DEFAULT 'private',
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "instructions" TEXT NOT NULL,
-    "authorId" BIGINT NOT NULL REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    "editorGroupId" BIGINT REFERENCES "groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    "requestedSpaceIds" BIGINT[] NOT NULL DEFAULT '{}'
+    "id"                BIGSERIAL PRIMARY KEY,
+    "workspaceId"       BIGINT                   NOT NULL REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "createdAt"         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "updatedAt"         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "version"           INTEGER                  NOT NULL DEFAULT 0,
+    "status"            VARCHAR(255)             NOT NULL DEFAULT 'active',
+    "scope"             VARCHAR(255)             NOT NULL DEFAULT 'private',
+    "name"              TEXT                     NOT NULL,
+    "description"       TEXT                     NOT NULL,
+    "instructions"      TEXT                     NOT NULL,
+    "authorId"          BIGINT                   NOT NULL REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "editorGroupId"     BIGINT REFERENCES "groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    "requestedSpaceIds" BIGINT[]                 NOT NULL DEFAULT '{}'
 );
