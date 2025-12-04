1519040a27fc4be9e9faf857164b089253871108
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-12-04 09:23:47 +0100

    fix

--- front/migrations/db/migration_423.sql
+++ front/migrations/db/migration_423.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS "skill_configurations" (
-    "id"                BIGSERIAL PRIMARY KEY,
+    "id"                BIGSERIAL,
     "workspaceId"       BIGINT                   NOT NULL REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    "createdAt"         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "updatedAt"         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "version"           INTEGER                  NOT NULL DEFAULT 0,
-    "status"            VARCHAR(255)             NOT NULL DEFAULT 'active',
-    "scope"             VARCHAR(255)             NOT NULL DEFAULT 'private',
+    "createdAt"         TIMESTAMP WITH TIME ZONE NOT NULL,
+    "updatedAt"         TIMESTAMP WITH TIME ZONE NOT NULL,
+    "version"           INTEGER                  NOT NULL,
+    "status"            VARCHAR(255)             NOT NULL,
+    "scope"             VARCHAR(255)             NOT NULL,
     "name"              TEXT                     NOT NULL,
     "description"       TEXT                     NOT NULL,
     "instructions"      TEXT                     NOT NULL,
     "authorId"          BIGINT                   NOT NULL REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    "editorGroupId"     BIGINT REFERENCES "groups" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    "requestedSpaceIds" BIGINT[]                 NOT NULL DEFAULT '{}'
+    "editorGroupId"     BIGINT REFERENCES "groups" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "requestedSpaceIds" BIGINT[]                 NOT NULL
 );
