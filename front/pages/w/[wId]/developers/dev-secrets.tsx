e2e2a6fd15f8ff03fb98eba94cb5247dedec5be9
Rémy-Christophe Schermesser <git@schermesser.com>
2025-12-04 14:36:12 +0100

    fix: no autocomplete for secrets

--- front/pages/w/[wId]/developers/dev-secrets.tsx
+++ front/pages/w/[wId]/developers/dev-secrets.tsx
@@ -197,6 +197,8 @@ export default function SecretsPage({
               }
             />
             <Input
+              // prevent autocompletion of secrets
+              autoComplete="off"
               message="Secret values are encrypted and stored securely in our database."
               name="Secret value"
               placeholder="Type the secret value"
