cb28fa3560e6a237c998cd0025dec4861fbd0b83
JulesBelveze <jules.belveze@hotmail.fr>
2025-12-05 16:48:19 +0100

    [front/lib/models/skill] - feature: add GroupSkillModel for group-skill associations
     - Introduced a new Sequelize model to represent the many-to-many relationship between groups and skill configurations
     - Implemented indexes to ensure uniqueness and improve query performance for group-skill associations
     - Established bidirectional associations with GroupModel and SkillConfigurationModel to facilitate ORM operations

---
+++ front/lib/models/skill/group_skill.ts
@@ -0,0 +1,85 @@
+import type {
+  BelongsToGetAssociationMixin,
+  CreationOptional,
+  ForeignKey,
+} from "sequelize";
+import { DataTypes } from "sequelize";
+
+import { frontSequelize } from "@app/lib/resources/storage";
+import { GroupModel } from "@app/lib/resources/storage/models/groups";
+import { WorkspaceAwareModel } from "@app/lib/resources/storage/wrappers/workspace_models";
+import { SkillConfigurationModel } from "@app/lib/models/skill";
+
+export class GroupSkillModel extends WorkspaceAwareModel<GroupSkillModel> {
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare groupId: ForeignKey<GroupModel["id"]>;
+  declare skillConfigurationId: ForeignKey<SkillConfigurationModel["id"]>;
+
+  declare getGroup: BelongsToGetAssociationMixin<GroupModel>;
+  declare getSkillConfiguration: BelongsToGetAssociationMixin<SkillConfigurationModel>;
+}
+
+GroupSkillModel.init(
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    groupId: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    skillConfigurationId: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+  },
+  {
+    modelName: "group_skills",
+    sequelize: frontSequelize,
+    indexes: [
+      {
+        unique: true,
+        fields: ["groupId", "skillConfigurationId"],
+      },
+      { fields: ["skillConfigurationId"] },
+      { fields: ["workspaceId"], concurrently: true },
+    ],
+  }
+);
+
+GroupSkillModel.belongsTo(GroupModel, {
+  foreignKey: { name: "groupId", allowNull: false },
+  targetKey: "id",
+});
+
+GroupModel.hasMany(GroupSkillModel, {
+  foreignKey: { name: "groupId", allowNull: false },
+  sourceKey: "id",
+  as: "groupSkillLinks",
+});
+
+GroupSkillModel.belongsTo(SkillConfigurationModel, {
+  foreignKey: { name: "skillConfigurationId", allowNull: false },
+  targetKey: "id",
+});
+
+SkillConfigurationModel.hasMany(GroupSkillModel, {
+  foreignKey: { name: "skillConfigurationId", allowNull: false },
+  sourceKey: "id",
+  as: "skillGroupLinks",
+});
