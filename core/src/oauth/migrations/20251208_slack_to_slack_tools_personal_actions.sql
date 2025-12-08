812f9faf4a4abe1b4bc0d5e8871994f932d65486
Stanislas Polu <spolu@dust.tt>
2025-12-08 20:51:26 +0100

    comment

--- core/src/oauth/migrations/20251208_slack_to_slack_tools_personal_actions.sql
+++ core/src/oauth/migrations/20251208_slack_to_slack_tools_personal_actions.sql
@@ -1,8 +1,10 @@
--- Idempotent migration to copy slack connections for Slack Tools MCP to the new slack_tools provider.
--- This migration copies connections with use_case 'personal_actions' from the 'slack' provider to
--- 'slack_tools'. These are the personal connections used by the Slack Tools MCP (as opposed to the
--- Slack Bot MCP which uses 'bot' use_case, or data source connections which use 'connection'
--- use_case).
+-- Idempotent migration to copy slack connections for Slack Tools MCP to the new slack_tools
+-- provider. This migration copies connections with use_case 'personal_actions' from the 'slack'
+-- provider to 'slack_tools'. These are the personal connections used by the Slack Tools MCP (as
+-- opposed to the Slack Bot MCP which uses 'bot' use_case, or data source connections which use
+-- 'connection' use_case).
+--
+-- See: https://github.com/dust-tt/tasks/issues/5397
 --
 -- This migration is idempotent - it will skip any rows that have already been migrated.
 
