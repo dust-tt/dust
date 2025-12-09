af9a06db1c0e2275ad952fc6074d63610561b121
Thomas Draier <thomas@dust.tt>
2025-12-09 11:44:26 +0100

    migration

--- front/migrations/db/migration_431.sql
+++ front/migrations/db/migration_431.sql
@@ -1,13 +1,17 @@
-update user_messages
-set
-    "origin" = 'triggered'
-from user_messages
-where
-    "userContextOrigin" is null
-    and content not like 'Transcript%'
-update user_messages
-set
-    "origin" = 'transcript'
-where
-    "userContextOrigin" is null
-    and content like 'Transcript%'
+UPDATE user_messages
+SET
+    "userContextOrigin" = 'triggered'
+WHERE
+    "userContextOrigin" IS NULL
+    AND content NOT LIKE 'Transcript%';
+
+UPDATE user_messages
+SET
+    "userContextOrigin" = 'transcript'
+WHERE
+    "userContextOrigin" IS NULL
+    AND content LIKE 'Transcript%';
+
+ALTER TABLE "user_messages"
+ALTER COLUMN "userContextOrigin"
+SET NOT NULL;
