6bc443041bf9d7f83a7b7b25d1f18c5c83e08ddc
Adrien Simon <adrisim2001@gmail.com>
2025-12-09 16:08:53 +0100

    feat(webhooks): add jira events examples

--- front/lib/triggers/built-in-webhooks/jira/schemas/issue_deleted.ts
+++ front/lib/triggers/built-in-webhooks/jira/schemas/issue_deleted.ts
@@ -211,3 +211,182 @@ export const issueDeletedSchema: JSONSchema = {
     },
   },
 };
+
+export const issueDeletedExample = {
+  timestamp: 1736938200000,
+  webhookEvent: "jira:issue_deleted",
+  issue_event_type_name: "issue_deleted",
+  user: {
+    self: "https://example.atlassian.net/rest/api/2/user?accountId=000000%3A00000000-0000-0000-0000-000000000000",
+    accountId: "000000:00000000-0000-0000-0000-000000000000",
+    avatarUrls: {
+      "48x48":
+        "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+      "24x24":
+        "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+      "16x16":
+        "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+      "32x32":
+        "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+    },
+    displayName: "User",
+    active: true,
+    timeZone: "UTC",
+    accountType: "atlassian",
+  },
+  issue: {
+    id: "10001",
+    self: "https://example.atlassian.net/rest/api/2/10001",
+    key: "PROJ-1",
+    fields: {
+      statuscategorychangedate: "2025-01-15T10:30:00.000+0000",
+      issuetype: {
+        self: "https://example.atlassian.net/rest/api/2/issuetype/10001",
+        id: "10001",
+        description: "Tasks track small, distinct pieces of work.",
+        iconUrl:
+          "https://example.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10001?size=medium",
+        name: "Task",
+        subtask: false,
+        avatarId: 10001,
+        entityId: "00000000-0000-0000-0000-000000000000",
+        hierarchyLevel: 0,
+      },
+      components: [],
+      timespent: null,
+      timeoriginalestimate: null,
+      project: {
+        self: "https://example.atlassian.net/rest/api/2/project/10001",
+        id: "10001",
+        key: "PROJ",
+        name: "Example Project",
+        projectTypeKey: "software",
+        simplified: true,
+        avatarUrls: {
+          "48x48":
+            "https://example.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/10001",
+          "24x24":
+            "https://example.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/10001?size=small",
+          "16x16":
+            "https://example.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/10001?size=xsmall",
+          "32x32":
+            "https://example.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/10001?size=medium",
+        },
+      },
+      description: null,
+      fixVersions: [],
+      statusCategory: {
+        self: "https://example.atlassian.net/rest/api/2/statuscategory/2",
+        id: 2,
+        key: "new",
+        colorName: "blue-gray",
+        name: "New",
+      },
+      aggregatetimespent: null,
+      resolution: null,
+      timetracking: {},
+      customfield_10015: null,
+      security: null,
+      attachment: [],
+      aggregatetimeestimate: null,
+      resolutiondate: null,
+      workratio: -1,
+      summary: "Example issue summary",
+      issuerestriction: {
+        issuerestrictions: {},
+        shouldDisplay: true,
+      },
+      watches: {
+        self: "https://example.atlassian.net/rest/api/2/issue/PROJ-1/watchers",
+        watchCount: 0,
+        isWatching: true,
+      },
+      lastViewed: "2025-01-15T11:30:00.000+0000",
+      creator: {
+        self: "https://example.atlassian.net/rest/api/2/user?accountId=000000%3A00000000-0000-0000-0000-000000000000",
+        accountId: "000000:00000000-0000-0000-0000-000000000000",
+        avatarUrls: {
+          "48x48":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "24x24":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "16x16":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "32x32":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+        },
+        displayName: "User",
+        active: true,
+        timeZone: "UTC",
+        accountType: "atlassian",
+      },
+      subtasks: [],
+      created: "2025-01-15T10:30:00.000+0000",
+      reporter: {
+        self: "https://example.atlassian.net/rest/api/2/user?accountId=000000%3A00000000-0000-0000-0000-000000000000",
+        accountId: "000000:00000000-0000-0000-0000-000000000000",
+        avatarUrls: {
+          "48x48":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "24x24":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "16x16":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+          "32x32":
+            "https://secure.gravatar.com/avatar/00000000000000000000000000000000?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FU-0.png",
+        },
+        displayName: "User",
+        active: true,
+        timeZone: "UTC",
+        accountType: "atlassian",
+      },
+      customfield_10021: null,
+      aggregateprogress: {
+        progress: 0,
+        total: 0,
+      },
+      priority: {
+        self: "https://example.atlassian.net/rest/api/2/priority/3",
+        iconUrl:
+          "https://example.atlassian.net/images/icons/priorities/medium_new.svg",
+        name: "Medium",
+        id: "3",
+      },
+      customfield_10001: null,
+      labels: [],
+      environment: null,
+      customfield_10019: "0|i000pr:",
+      timeestimate: null,
+      aggregatetimeoriginalestimate: null,
+      versions: [],
+      duedate: null,
+      progress: {
+        progress: 0,
+        total: 0,
+      },
+      votes: {
+        self: "https://example.atlassian.net/rest/api/2/issue/PROJ-1/votes",
+        votes: 0,
+        hasVoted: false,
+      },
+      issuelinks: [],
+      assignee: null,
+      updated: "2025-01-15T10:30:00.000+0000",
+      status: {
+        self: "https://example.atlassian.net/rest/api/2/status/10001",
+        description: "",
+        iconUrl: "https://example.atlassian.net/",
+        name: "To Do",
+        id: "10001",
+        statusCategory: {
+          self: "https://example.atlassian.net/rest/api/2/statuscategory/2",
+          id: 2,
+          key: "new",
+          colorName: "blue-gray",
+          name: "New",
+        },
+      },
+    },
+  },
+  matchedWebhookIds: [1],
+};
