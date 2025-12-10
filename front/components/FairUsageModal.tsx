275773ab39b0f6475ea92cdbdd90161de19c238e
Philippe Rolet <philipperolet@gmail.com>
2025-12-10 22:33:48 +0100

    update 2

--- front/components/FairUsageModal.tsx
+++ front/components/FairUsageModal.tsx
@@ -19,21 +19,19 @@ const FAIR_USE_CONTENT = `
 
 "Fair use" applies to messages typed and sent by human users, without using programmatic methods (scripts, API calls, etc.).
 ___
+## **"Fair use" limitations**
 
-# "Fair use" limitations
 - For **Pro plans**, a limit at 100 messages / seat / day (Enough to cover any fair usage) is applied.
 - For **Enterprise plans**, a limit at 200 messages / seat / day (Enough to cover any fair usage) is applied.
 ___
+## **What is "unfair" usage?**
 
-What is "unfair" usage?
-===
 The following usage is considered *"Unfair"*:
 - sharing a single seat between multiple people;
 - sending messages programmatically using a regular user seat.
 ___
+## **Can messages be sent programmatically with Dust?**
 
-Can messages be sent programmatically with Dust?
-===
 Yes. Such messages are not covered by fair use limits, and are billed separately.
 
 Check out [Programmatic usage](https://dust-tt.notion.site/Programmatic-usage-at-Dust-2b728599d94181ceb124d8585f794e2e) documentation for more information
