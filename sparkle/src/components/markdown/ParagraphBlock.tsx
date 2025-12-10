d47371db03643fc6643353d3d18eefa47377e32d
chrisjow <christophe@dust.tt>
2025-12-10 11:15:05 +0100

    Excessive line spacing when using line returns in input bar

--- sparkle/src/components/markdown/ParagraphBlock.tsx
+++ sparkle/src/components/markdown/ParagraphBlock.tsx
@@ -3,24 +3,37 @@ import React from "react";
 
 import { cn } from "@sparkle/lib";
 
-export const paragraphBlockVariants = cva([
-  "s-whitespace-pre-wrap s-break-words s-font-normal first:s-pt-0 last:s-pb-0",
-  "s-py-1 @md:s-py-2 @md:s-leading-7",
-]);
+export const paragraphBlockVariants = cva(
+  [
+    "s-whitespace-pre-wrap s-break-words s-font-normal first:s-pt-0 last:s-pb-0",
+    "s-py-0",
+  ],
+  {
+    variants: {
+      variant: {
+        surface: ["s-py-1 @md:s-py-2 @md:s-leading-7"],
+      },
+    },
+  }
+);
 
 interface ParagraphBlockProps {
   children: React.ReactNode;
   textColor: string;
   textSize: string;
+  variant?: "surface";
 }
 
 export function ParagraphBlock({
   children,
   textColor,
   textSize,
+  variant = "surface",
 }: ParagraphBlockProps) {
   return (
-    <div className={cn(paragraphBlockVariants(), textSize, textColor)}>
+    <div
+      className={cn(paragraphBlockVariants({ variant }), textSize, textColor)}
+    >
       {children}
     </div>
   );
