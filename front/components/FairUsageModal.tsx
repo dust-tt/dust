a757b0d7ea066b725a3e9455fdedc88a35865717
Philippe Rolet <philipperolet@gmail.com>
2025-12-11 16:43:57 +0100

    update 3

--- front/components/FairUsageModal.tsx
+++ front/components/FairUsageModal.tsx
@@ -15,27 +15,21 @@ interface FairUsageModalProps {
 }
 
 const FAIR_USE_CONTENT = `
-# Fair use principles
+# Fair use principles for user seats
 
-"Fair use" applies to messages typed and sent by human users, without using programmatic methods (scripts, API calls, etc.).
+Each user seat at Dust is tied to a specific human user, and is destined to be used by that person only, for the purposes of typing and sending messages manually (as opposed to using programmatic methods such as scripts, API calls, etc. which is covered separately).
+
+"Fair use" limits apply to each user seat, as follows:
+- For **Pro plans**, a limit at 100 messages / seat / day is applied.
+- For **Enterprise plans**, a limit at 200 messages / seat / day is applied.
+
+These limits should be understood as a way to prevent abuse, not as an allowed quota of messages. In particular, it is considered unfair to share a single seat between multiple people.
+
 ___
-## **"Fair use" limitations**
+# **Can messages be sent programmatically with Dust?**
 
-- For **Pro plans**, a limit at 100 messages / seat / day (Enough to cover any fair usage) is applied.
-- For **Enterprise plans**, a limit at 200 messages / seat / day (Enough to cover any fair usage) is applied.
-___
-## **What is "unfair" usage?**
-
-The following usage is considered *"Unfair"*:
-- sharing a single seat between multiple people;
-- sending messages programmatically using a regular user seat.
-___
-## **Can messages be sent programmatically with Dust?**
-
-Yes. Such messages are not covered by fair use limits, and are billed separately.
-
-Check out [Programmatic usage](https://dust-tt.notion.site/Programmatic-usage-at-Dust-2b728599d94181ceb124d8585f794e2e) documentation for more information
-
+Yes, and this usage is encouraged. However, such messages are not covered by individual user seats and fair use limits, and are billed separately. 
+Dust plans already include monthly credits for programmatic usage, and more credits can be purchased if needed, see [Programmatic usage at Dust](https://dust-tt.notion.site/Programmatic-usage-at-Dust-2b728599d94181ceb124d8585f794e2e).
 
 `;
 
