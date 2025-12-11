932016fadf5a9c6df1eee12d5fddbbbb3bba7086
Adrien Simon <adrisim2001@gmail.com>
2025-12-11 17:35:06 +0100

    migrations

--- front/migrations/db/migration_437.sql
+++
@@ -1,3 +0,0 @@
--- Migration created on Dec 11, 2025
-ALTER TABLE "public"."agent_message_skills" DROP COLUMN "isActive";
-CREATE INDEX "agent_message_skills_wid_cid_acid" ON "agent_message_skills" ("workspaceId", "conversationId", "agentConfigurationId");
