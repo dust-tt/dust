387fcb7a3e4d67bd6ff84fba97f5ecd9d973c095
JulesBelveze <jules.belveze@hotmail.fr>
2025-12-15 09:18:27 +0100

    [db] - feature: add descriptions for skill versions
     - Introduced a new column `agentFacingDescription` in the `skill_versions` table to store descriptions visible to agents
     - Added a new column `userFacingDescription` in the `skill_versions` table for descriptions intended for end users

--- front/migrations/db/migration_444.sql
+++ front/migrations/db/migration_444.sql
@@ -1,5 +1,9 @@
 -- Migration created on Dec 15, 2025
-ALTER TABLE "public"."skill_configurations"
-    ADD COLUMN "skill_versions" TEXT;
+ALTER TABLE "public"."skill_versions"
+    ADD COLUMN "agentFacingDescription" TEXT;
+
 UPDATE "public"."skill_versions"
 SET "agentFacingDescription" = "description";
+
+ALTER TABLE "public"."skill_versions"
+    ADD COLUMN "userFacingDescription" TEXT;
