e23da8eb2cea7ced82ad1291a176831eb12fc8b0
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-12-17 13:23:07 +0100

    add constraint drops in migration script (#19207)

--- front/migrations/db/migration_452.sql
+++ front/migrations/db/migration_452.sql
@@ -1,3 +1,5 @@
 -- Migration created on Dec 17, 2025
+ALTER TABLE "conversation_skills" DROP CONSTRAINT "conversation_skills_agentConfigurationId_fkey";
 ALTER TABLE "conversation_skills" ALTER COLUMN "agentConfigurationId" TYPE VARCHAR(255);
+ALTER TABLE "agent_message_skills" DROP CONSTRAINT "agent_message_skills_agentConfigurationId_fkey";
 ALTER TABLE "agent_message_skills" ALTER COLUMN "agentConfigurationId" TYPE VARCHAR(255);
