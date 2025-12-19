20325d494b35d9bf6a1870d125d329ad8c18067a
Thomas Draier <thomas@dust.tt>
2025-12-19 14:09:39 +0100

    Move spreadsheet read-only tools to google_drive server

--- front/lib/providers/google_drive/utils.ts
+++ front/lib/providers/google_drive/utils.ts
@@ -17,3 +17,12 @@ export function getGoogleDriveClient(accessToken: string) {
   oauth2Client.setCredentials({ access_token: accessToken });
   return google.drive({ version: "v3", auth: oauth2Client });
 }
+
+export function getGoogleSheetsClient(accessToken: string) {
+  const oauth2Client = new google.auth.OAuth2();
+  oauth2Client.setCredentials({ access_token: accessToken });
+  return google.sheets({
+    version: "v4",
+    auth: oauth2Client,
+  });
+}
