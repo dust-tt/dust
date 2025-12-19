04106e033dbcd1409394ffca25522a47bec0c188
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2025-12-19 13:54:36 +0100

    remove launchTrackProgrammaticUsageActivity from worker

--- front/temporal/agent_loop/worker.ts
+++ front/temporal/agent_loop/worker.ts
@@ -29,7 +29,6 @@ import {
 import { publishDeferredEventsActivity } from "@app/temporal/agent_loop/activities/publish_deferred_events";
 import { runModelAndCreateActionsActivity } from "@app/temporal/agent_loop/activities/run_model_and_create_actions_wrapper";
 import { runToolActivity } from "@app/temporal/agent_loop/activities/run_tool";
-import { launchTrackProgrammaticUsageActivity } from "@app/temporal/agent_loop/activities/usage_tracking";
 import { QUEUE_NAME } from "@app/temporal/agent_loop/config";
 import { getWorkflowConfig } from "@app/temporal/bundle_helper";
 import { isDevelopment, removeNulls } from "@app/types";
@@ -61,7 +60,6 @@ export async function runAgentLoopWorker() {
       publishDeferredEventsActivity,
       runModelAndCreateActionsActivity,
       runToolActivity,
-      launchTrackProgrammaticUsageActivity,
     },
     taskQueue: QUEUE_NAME,
     connection,
