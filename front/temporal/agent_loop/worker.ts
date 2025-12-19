2a8e464368ecab5562e6cf873e5cd075beb88459
Thomas Draier <thomas@dust.tt>
2025-12-19 23:08:29 +0100

    lint

--- front/temporal/agent_loop/worker.ts
+++ front/temporal/agent_loop/worker.ts
@@ -31,7 +31,7 @@ import { runModelAndCreateActionsActivity } from "@app/temporal/agent_loop/activ
 import { runToolActivity } from "@app/temporal/agent_loop/activities/run_tool";
 import { QUEUE_NAME } from "@app/temporal/agent_loop/config";
 import { getWorkflowConfig } from "@app/temporal/bundle_helper";
-import { isDevelopment, removeNulls } from "@app/types";
+import { removeNulls } from "@app/types";
 
 // We need to give the worker some time to finish the current activity before shutting down.
 const SHUTDOWN_GRACE_TIME = "2 minutes";
