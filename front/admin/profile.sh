7f6531a78a6980106057f50d2dc25d7ac099139a
Jean-David Fiquet <jd@dust.tt>
2025-12-22 11:14:09 +0100

    [front] chore: Add profile.sh script (#19343)

---
+++ front/admin/profile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e 
+echo "Waiting for main container to be ready..."
+while ! wget "http://$(hostname -i):3000/api/healthz" 2>/dev/null; do
+  sleep 2
+done
+echo "Main container ready, starting profiler warmup"
+
+ITERATION_COUNT="${1:-1}"
+for i in ITERATION_COUNT; do
+  echo "Triggering profiler warmup $i/$ITERATION_COUNT"
+  wget "http://$(hostname -i):3000/api/debug/profiler?secret=$DEBUG_PROFILER_SECRET"
+done
+echo "Profiler warmup complete"
