d4d4aad8ec9ae9acf9e441fd840402a795795e82
Thomas Draier <thomas@dust.tt>
2025-12-22 23:23:18 +0100

    CLI ci

--- connectors/eslint.config.mjs
+++ connectors/eslint.config.mjs
@@ -27,7 +27,9 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
         tsconfigRootDir: import.meta.dirname,
       },
       globals: {
