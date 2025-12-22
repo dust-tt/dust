2378e619e7eac756909ad3e2002bc687a0162a4f
ykmsd <yuka@dust.tt>
2025-12-22 13:45:53 +0100

    this time 30 for sure

--- front/temporal/scrub_workspace/config.ts
+++ front/temporal/scrub_workspace/config.ts
@@ -4,5 +4,5 @@ export const QUEUE_NAME = `scrub-workspace-queue-v${QUEUE_VERSION}`;
 export const DOWNGRADE_FREE_ENDED_WORKSPACES_WORKFLOW_ID =
   "downgrade-and-scrub-free-ended-workspaces";
 
-export const DATA_RETENTION_PERIOD_IN_DAYS = 15;
+export const DATA_RETENTION_PERIOD_IN_DAYS = 30;
 export const LAST_EMAIL_BEFORE_SCRUB_IN_DAYS = 3;
