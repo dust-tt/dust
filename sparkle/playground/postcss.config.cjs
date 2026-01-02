81d960013c8f2d9c908ec75855dba4142ee1e3ce
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2026-01-02 18:22:58 +0100

    Removing playground changes

--- sparkle/playground/postcss.config.cjs
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-
-// Reference Sparkle's postcss config from parent directory (no duplication)
-module.exports = require(path.resolve(__dirname, "../postcss.config.js"));
-
--- sparkle/playground/src/data/index.ts
+++
@@ -1,8 +0,0 @@
-// Re-export all types
-export * from "./types";
-
-// Re-export all data and utilities
-export * from "./users";
-export * from "./agents";
-export * from "./conversations";
-
