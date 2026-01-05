3ca6b1843ce377e3ae3f4c24ecc874d3d61aefa3
David Ebbo <david@dust.tt>
2026-01-05 14:26:31 +0100

    ON DELETE RESTRICT

--- front/migrations/db/migration_461.sql
+++ front/migrations/db/migration_461.sql
@@ -1,4 +1,4 @@
 -- Migration created on Jan 02, 2026
-ALTER TABLE "public"."user_metadata" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE CASCADE;
+ALTER TABLE "public"."user_metadata" ADD COLUMN "workspaceId" BIGINT REFERENCES "workspaces" ("id") ON DELETE RESTRICT;
 DROP INDEX IF EXISTS user_metadata_user_id_key;
 CREATE UNIQUE INDEX "user_metadata_user_id_workspace_id_key" ON "user_metadata" ("userId", "workspaceId", "key");
