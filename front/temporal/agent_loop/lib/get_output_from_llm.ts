5136c6bb9e07c6fef61273afa92ea4cb75ec438c
Pierre Milliotte <pmilliotte@gmail.com>
2026-01-06 10:02:33 +0100

    feat: fix yield (#19638)
    Co-authored-by: Pierre Milliotte <pierre@dust.tt>

--- front/temporal/agent_loop/lib/get_output_from_llm.ts
+++ front/temporal/agent_loop/lib/get_output_from_llm.ts
@@ -50,8 +50,7 @@ async function* withPeriodicHeartbeat<T>(
 
     if (streamResult.done) {
       streamExhausted = true;
-      yield streamResult.value;
-      continue;
+      break;
     }
 
     yield streamResult.value;
