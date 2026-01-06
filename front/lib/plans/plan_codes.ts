e5d0f8bb719442744b7f24e1f7c694644a19afda
Daphné Popin <PopDaph@users.noreply.github.com>
2026-01-06 09:50:02 +0100

    Subscribe workspace to new FREE_TRIAL_PHONE_PLAN if phone number code successful (#19611)
    * Subscribe workspace to new FREE_TRIAL_PHONE_PLAN if phone number code successful
    
    * Move files
    
    * Rebase and disable deep dive

--- front/lib/plans/plan_codes.ts
+++ front/lib/plans/plan_codes.ts
@@ -5,6 +5,7 @@ export const FREE_NO_PLAN_CODE = "FREE_NO_PLAN";
 export const FREE_UPGRADED_PLAN_CODE = "FREE_UPGRADED_PLAN";
 export const FREE_TEST_PLAN_CODE = "FREE_TEST_PLAN";
 export const TRIAL_PLAN_CODE = "TRIAL_PLAN_CODE";
+export const FREE_TRIAL_PHONE_PLAN_CODE = "FREE_TRIAL_PHONE_PLAN";
 
 // Current pro plans:
 export const PRO_PLAN_SEAT_29_CODE = "PRO_PLAN_SEAT_29";
