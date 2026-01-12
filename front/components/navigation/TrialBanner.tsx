7f5270414c4000201e60b7805a887f051210a602
David Ebbo <david@dust.tt>
2026-01-12 18:37:57 +0100

    Add a bit more vertical space

--- front/components/navigation/TrialBanner.tsx
+++ front/components/navigation/TrialBanner.tsx
@@ -64,7 +64,7 @@ export function SubscriptionEndBanner({
   return (
     <div
       className={cn(
-        "flex items-center justify-between gap-4 px-4 py-2",
+        "flex items-center justify-between gap-4 px-4 py-3",
         "bg-sky-50 dark:bg-sky-50-night"
       )}
     >
