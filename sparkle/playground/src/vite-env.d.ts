e6417595f92cf237cddafbadb87c666992a22fdc
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2026-01-16 18:30:34 +0100

    Testing

--- sparkle/playground/src/vite-env.d.ts
+++ sparkle/playground/src/vite-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+interface ImportMeta {
+  glob: <T = unknown>(
+    pattern: string,
+    options?: {
+      eager?: boolean;
+      import?: string;
+    }
+  ) => Record<string, T>;
+}
