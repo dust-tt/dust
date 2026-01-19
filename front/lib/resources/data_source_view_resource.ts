b6b4bd1e3b4da45fe0a83aeeabeb2748e982ec08
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-01-19 14:05:34 +0100

    Revert "remove resource methods"
    This reverts commit b5c30805209d8d286700756b879eaa2d49532722.

--- front/lib/resources/data_source_view_resource.ts
+++ front/lib/resources/data_source_view_resource.ts
@@ -513,6 +513,35 @@ export class DataSourceViewResource extends ResourceWithSpace<DataSourceViewMode
     return dataSourceViews[0] ?? null;
   }
 
+  static async fetchByConversationModelIds(
+    auth: Authenticator,
+    conversationModelIds: ModelId[]
+  ): Promise<DataSourceViewResource[]> {
+    if (conversationModelIds.length === 0) {
+      return [];
+    }
+
+    const dataSources = await DataSourceResource.fetchByConversationModelIds(
+      auth,
+      conversationModelIds
+    );
+    if (dataSources.length === 0) {
+      return [];
+    }
+
+    return this.baseFetch(
+      auth,
+      {},
+      {
+        where: {
+          workspaceId: auth.getNonNullableWorkspace().id,
+          kind: "default",
+          dataSourceId: { [Op.in]: dataSources.map((ds) => ds.id) },
+        },
+      }
+    );
+  }
+
   static async search(
     auth: Authenticator,
     searchParams: {
