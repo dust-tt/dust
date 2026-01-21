0bcdc82719bb1a5d0e5e33fdae9ac696f46787fe
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-01-21 23:52:13 +0100

    add button to open manage skills

--- front/components/skill_builder/SkillBuilderAgentFacingDescriptionSection.tsx
+++ front/components/skill_builder/SkillBuilderAgentFacingDescriptionSection.tsx
@@ -102,6 +102,7 @@ export function SkillBuilderAgentFacingDescriptionSection() {
           />
           {isSimilarSkillsEnabled && (
             <SimilarSkillsDisplay
+              owner={owner}
               similarSkills={similarSkills}
               isLoading={isLoading}
             />
