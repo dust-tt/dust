409d64a6b50bc3d15e9fcce7698396fb77fa0a00
ykmsd <yuka@dust.tt>
2026-01-30 17:54:22 +0100

    move dir

--- front/lib/mentions/markdown/plugin.tsx
+++ front/lib/mentions/markdown/plugin.tsx
@@ -9,7 +9,7 @@ import { visit } from "unist-util-visit";
 
 import type { WorkspaceType } from "@app/types";
 
-import { MentionDisplay } from "../ui/MentionDisplay";
+import { MentionDisplay } from "@app/components/mentions/MentionDisplay";
 
 /*
  * Remark directive plugin for parsing mention directives.
