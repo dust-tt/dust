88fdf7f414f07d340573224ec80783e1eb9700a6
ykmsd <yuka@dust.tt>
2026-02-04 14:34:58 +0100

    remove parent path

--- front-spa/src/app/App.tsx
+++ front-spa/src/app/App.tsx
@@ -255,7 +255,6 @@ const router = createBrowserRouter(
         },
 
         {
-          path: "",
           element: <AdminLayout />,
           children: [
             { path: "members", element: <MembersPage /> },
