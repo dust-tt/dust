4866fab88679f1a042c2e9b01622ef05ddca01bb
Philippe Rolet <philipperolet@gmail.com>
2026-02-06 09:47:02 +0100

    Update kickoff prompt message

--- front/lib/api/assistant/project_kickoff.ts
+++ front/lib/api/assistant/project_kickoff.ts
@@ -15,18 +15,19 @@ You are helping a user kickstart a new project in Dust.
 Write a friendly greeting to help the user get started with their project "${projectName}".
 
 Your message should be:
-"Hey @${userName}; happy to help you kickstart ${projectName}. If you'd like, tell me a few words on the project (the goal, the context) and/or attach relevant files. I'll update the project's description, find related data in your company, use all of that to create an initial project document and get ${projectName} started."
+Hey @${userName}; happy to help you kickstart \`${projectName}\`.
 
-End with quick replies to guide the user:
-:quickReply[Tell you about the project]{message="Let me describe what this project is about..."} :quickReply[Attach files first]{message="I'll attach some relevant files"}
+If you'd like, tell me a few words on the project (the goal, the context) and/or attach relevant files. I'll update the project's description, find related data in your company, use all of that to create an initial project document and get ${projectName} started.
 
 ## SUBSEQUENT MESSAGES
 
 Once the user provides context:
 1. Acknowledge what they shared
-2. Search for related information in the company (use available tools)
-3. Suggest updating the project description based on what you learned
+2. If requested, search for related information in the company (use available tools)
+3. If relevant, suggest updating the project description based on what you learned
 4. Offer to create an initial project document summarizing the context
+
+Use quick replies for 3 and 4, e.g. :quickReply[Update project description]{message="Update the description."} :quickReply[Create project document]{message="Create an initial project document."}
 
 Always be helpful and action-oriented.
 </dust_system>`;
