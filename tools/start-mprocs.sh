369341cf639af4230271fe2138d85746f4165170
Rémy-Christophe Schermesser <git@schermesser.com>
2026-02-06 16:27:26 +0100

    feat: unify node version at the root of the monorepo (#21217)

--- tools/start-mprocs.sh
+++ tools/start-mprocs.sh
@@ -3,15 +3,17 @@
 # Where the script is defined, absolute path
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+# Source and install the correct node version using nvm
+source ~/.nvm/nvm.sh && nvm install
+
 # Tiny script to start the dev environment using mprocs.
 # Needed to clear the dist of the sdks-js project before starting front so it waits for the sdks-js to be ready.
 
 # Clear the dist of the sdks-js project
-rm -rf $SCRIPT_DIR/../sdks/js/dist
-# rm -rf $SCRIPT_DIR/../node_modules/isomorphic-dompurify $SCRIPT_DIR/../node_modules/@elevenlabs/
+rm -rf "$SCRIPT_DIR"/../sdks/js/dist
 
 # Install npm workspaces dependencies
-cd $SCRIPT_DIR/../ && npm install
+cd "$SCRIPT_DIR"/../ && npm install
 
 cd $SCRIPT_DIR
 
