845d9fa2ec2c91f4caff34a18879871f4102c685
Daphné Popin <daphne.popin@gmail.com>
2026-02-11 15:05:27 +0100

    Fix login required in next

--- front/pages/invite-choose.tsx
+++ front/pages/invite-choose.tsx
@@ -1,3 +1,6 @@
 import { InviteChoosePage } from "@app/components/pages/onboarding/InviteChoosePage";
+import { appGetServerSidePropsForUserNoWorkspace } from "@app/lib/auth/appServerSideProps";
+
+export const getServerSideProps = appGetServerSidePropsForUserNoWorkspace;
 
 export default InviteChoosePage;
