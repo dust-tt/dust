883e7d0075ffdd626fbf0cd7de70a4c2fd07f666
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-02-12 13:59:03 +0100

    feat: move logo to the botton of emails (#21516)
    Co-authored-by: Matteo Trabattoni <matteotrabattoni@MacBook-Pro-de-Matteo.local>

--- front/lib/notifications/email-templates/_layout.tsx
+++ front/lib/notifications/email-templates/_layout.tsx
@@ -22,7 +22,15 @@ export const EmailLayout = ({
           padding: "20px",
         }}
       >
-        <div style={{ width: "100%", textAlign: "left", marginBottom: "30px" }}>
+        <div
+          style={{
+            maxWidth: "600px",
+            backgroundColor: "#ffffff",
+          }}
+        >
+          {children}
+        </div>
+        <div style={{ width: "100%", textAlign: "left", marginTop: "20px" }}>
           <a
             href={process.env.NEXT_PUBLIC_DUST_CLIENT_FACING_URL}
             target="_new"
@@ -35,14 +43,6 @@ export const EmailLayout = ({
               src="https://dust.tt/static/landing/logos/dust/Dust_Logo.png"
             />
           </a>
-        </div>
-        <div
-          style={{
-            maxWidth: "600px",
-            backgroundColor: "#ffffff",
-          }}
-        >
-          {children}
         </div>
         <div
           style={{
