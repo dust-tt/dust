72b26dd71946575676a13c97a29a9a9681a6f0e2
Sebastien Flory <seb@dust.tt>
2026-02-12 23:27:06 +0100

    Fix: stuck space deletion workflow (#21573)

--- front/poke/temporal/activities.ts
+++ front/poke/temporal/activities.ts
@@ -190,6 +190,7 @@ async function deleteSpaceConversations(
       spaceId: space.sId,
       options: {
         includeDeleted: true,
+        includeTest: true,
         dangerouslySkipPermissionFiltering: true,
       },
     }
