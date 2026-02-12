335bac3efa29ab744a96ffd855a19c72064de835
Christophe Jow <christophe@dust.tt>
2026-02-12 15:17:11 +0100

    Fix addMember (#21528)

--- front/lib/api/projects/conversations.test.ts
+++ front/lib/api/projects/conversations.test.ts
@@ -214,13 +214,13 @@ describe("moveConversationToProject", () => {
     }
 
     // Add all users to the project
-    await projectSpaceGroup.addMember(internalAdminAuth, {
+    await projectSpaceGroup.dangerouslyAddMember(internalAdminAuth, {
       user: user1.toJSON(),
     });
-    await projectSpaceGroup.addMember(internalAdminAuth, {
+    await projectSpaceGroup.dangerouslyAddMember(internalAdminAuth, {
       user: user2.toJSON(),
     });
-    await projectSpaceGroup.addMember(internalAdminAuth, {
+    await projectSpaceGroup.dangerouslyAddMember(internalAdminAuth, {
       user: user3.toJSON(),
     });
 
@@ -411,10 +411,10 @@ describe("moveConversationToProject", () => {
       throw new Error("Project space regular group not found");
     }
 
-    await projectSpaceGroup.addMember(internalAdminAuth, {
+    await projectSpaceGroup.dangerouslyAddMember(internalAdminAuth, {
       user: user1.toJSON(),
     });
-    await projectSpaceGroup.addMember(internalAdminAuth, {
+    await projectSpaceGroup.dangerouslyAddMember(internalAdminAuth, {
       user: user2.toJSON(),
     });
 
