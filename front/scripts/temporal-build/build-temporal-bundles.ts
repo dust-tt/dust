cd1015bd36bd915c4d2745432c815979d8a69082
Rémy-Christophe Schermesser <git@schermesser.com>
2026-02-12 19:42:45 +0100

    fix: build-temporal-bundles.ts

--- front/scripts/temporal-build/build-temporal-bundles.ts
+++ front/scripts/temporal-build/build-temporal-bundles.ts
@@ -60,7 +60,7 @@ function getWorkerDirectory(workerName: WorkerName): string | null {
     case "production_checks":
       return path.join(baseDir, "temporal/production_checks");
     case "project_journal_queue":
-      return path.join(baseDir, "temporal/project_journal_queue");
+      return path.join(baseDir, "temporal/project_user_digest_queue");
     case "project_user_digest_queue":
       return path.join(baseDir, "temporal/project_user_digest_queue");
     case "relocation":
