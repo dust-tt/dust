70d633e06b9719f3533cb32d7109a58a635ac211
Rémy-Christophe Schermesser <git@schermesser.com>
2026-02-12 19:24:17 +0100

    feat: enable runUserProjectDigestQueueWorker (#21551)

--- front/temporal/worker_registry.ts
+++ front/temporal/worker_registry.ts
@@ -10,7 +10,7 @@ import { runMentionsCountWorker } from "@app/temporal/mentions_count_queue/worke
 import { runMentionsQueueWorker } from "@app/temporal/mentions_queue/worker";
 import { runNotificationsQueueWorker } from "@app/temporal/notifications_queue/worker";
 import { runProductionChecksWorker } from "@app/temporal/production_checks/worker";
-import { runProjectJournalQueueWorker } from "@app/temporal/project_journal_queue/worker";
+import { runUserProjectDigestQueueWorker } from "@app/temporal/project_user_digest_queue/worker";
 import { runRelocationWorker } from "@app/temporal/relocation/worker";
 import { runRemoteToolsSyncWorker } from "@app/temporal/remote_tools/worker";
 import { runScrubWorkspaceQueueWorker } from "@app/temporal/scrub_workspace/worker";
@@ -60,8 +60,8 @@ export const workerFunctions: Record<WorkerName, () => Promise<void>> = {
   notifications_queue: runNotificationsQueueWorker,
   poke: runPokeWorker,
   production_checks: runProductionChecksWorker,
-  project_journal_queue: runProjectJournalQueueWorker,
-  project_user_digest_queue: runProjectJournalQueueWorker,
+  project_journal_queue: runUserProjectDigestQueueWorker,
+  project_user_digest_queue: runUserProjectDigestQueueWorker,
   relocation: runRelocationWorker,
   remote_tools_sync: runRemoteToolsSyncWorker,
   scrub_workspace_queue: runScrubWorkspaceQueueWorker,
