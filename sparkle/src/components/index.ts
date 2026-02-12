fa0669575d32c09cd920f60a76bd3dff5be981be
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2026-02-12 15:42:37 +0100

    Cleaning

--- sparkle/src/components/index.ts
+++ sparkle/src/components/index.ts
@@ -197,8 +197,6 @@ export {
 export { PriceTable } from "./PriceTable";
 export { RadioGroup, RadioGroupCustomItem, RadioGroupItem } from "./RadioGroup";
 export { RainbowEffect } from "./RainbowEffect";
-export type { RequestCardProps } from "./RequestCard";
-export { RequestCard } from "./RequestCard";
 export {
   ResizableHandle,
   ResizablePanel,
