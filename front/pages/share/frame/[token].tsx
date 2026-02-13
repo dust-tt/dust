01127c340aab355f56d53cf0be41ba8b85e9f50a
Thomas Draier <thomas@dust.tt>
2026-02-13 17:25:58 +0100

    Remove all pages

--- front/pages/share/frame/[token].tsx
+++
@@ -1,6 +0,0 @@
-import { SharedFramePage } from "@app/components/pages/share/SharedFramePage";
-import { appGetServerSidePropsPublic } from "@app/lib/auth/appServerSideProps";
-
-export const getServerSideProps = appGetServerSidePropsPublic;
-
-export default SharedFramePage;
--- front/pages/sso-enforced.tsx
+++
@@ -1,6 +0,0 @@
-import { SsoEnforcedPage } from "@app/components/pages/SsoEnforcedPage";
-import { appGetServerSidePropsForUserNoWorkspace } from "@app/lib/auth/appServerSideProps";
-
-export const getServerSideProps = appGetServerSidePropsForUserNoWorkspace;
-
-export default SsoEnforcedPage;
