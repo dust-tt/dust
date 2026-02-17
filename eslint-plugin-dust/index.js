6e1c6cbbf460c80c6324a8c45ee81b91cd86f8fc
Rémy-Christophe Schermesser <git@schermesser.com>
2026-02-17 18:34:37 +0100

    feat: port too-long-index-name eslint rule to biome gritql plugin (#21718)

--- eslint-plugin-dust/index.js
+++ eslint-plugin-dust/index.js
@@ -1,15 +1,11 @@
 "use strict";
 
-const tooLongIndexName = require("./rules/too-long-index-name");
-
 const plugin = {
   meta: {
     name: "eslint-plugin-dust",
     version: "0.0.0",
   },
-  rules: {
-    "too-long-index-name": tooLongIndexName,
-  },
+  rules: {},
 };
 
 // Support both CommonJS and ESM
