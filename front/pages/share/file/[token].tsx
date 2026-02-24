a7730613f78df39ae11dc25264e18a6c7a6c706c
Thomas Draier <thomas@dust.tt>
2026-02-24 14:38:23 +0100

    [front] Remove all pages from nextjs (#21627)

--- front/pages/share/file/[token].tsx
+++
@@ -1,8 +0,0 @@
-import { SharedFilePage } from "@app/components/pages/share/SharedFilePage";
-import { appGetServerSidePropsPublic } from "@app/lib/auth/appServerSideProps";
-
-// biome-ignore lint/plugin/nextjsNoDataFetchingInGetssp: pre-existing
-export const getServerSideProps = appGetServerSidePropsPublic;
-
-// This page does a client-side redirect to /share/frame/:token
-export default SharedFilePage;
