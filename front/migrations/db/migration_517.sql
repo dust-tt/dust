8f97ae44f53b29d84bb25273e700abc9f6bf459c
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-02-24 11:46:33 +0100

    fix index name

--- front/migrations/db/migration_517.sql
+++ front/migrations/db/migration_517.sql
@@ -1,2 +1,2 @@
 ALTER TABLE "agent_message_feedbacks" ADD COLUMN "conversationId" BIGINT REFERENCES "conversations" ("id");
-CREATE INDEX CONCURRENTLY "agent_message_feedbacks_conversation_id" ON "agent_message_feedbacks" ("workspaceId", "conversationId");
+CREATE INDEX CONCURRENTLY "agent_message_feedbacks_workspace_id_conversation_id" ON "agent_message_feedbacks" ("workspaceId", "conversationId");
