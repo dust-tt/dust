d512c936545f6f46788ab98bbe1821b09f3dff97
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-02-25 14:18:30 +0100

    sort imports (#22256)

--- front/lib/api/workos/user.ts
+++ front/lib/api/workos/user.ts
@@ -11,8 +11,8 @@ import tracer from "@app/logger/tracer";
 import { isDevelopment } from "@app/types/shared/env";
 import type { Result } from "@app/types/shared/result";
 import { Err, Ok } from "@app/types/shared/result";
-import { isString } from "@app/types/shared/utils/general";
 import { sha256 } from "@app/types/shared/utils/encryption";
+import { isString } from "@app/types/shared/utils/general";
 import type { LightWorkspaceType } from "@app/types/user";
 import type {
   WorkOS,
