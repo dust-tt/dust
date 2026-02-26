21a410a835175f522b76b270b9ca7e10fb69e22d
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-02-26 17:03:29 +0100

    fix migration file

--- front/migrations/db/migration_523.sql
+++ front/migrations/db/migration_523.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS "skill_file_attachments" (
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
     "workspaceId" BIGINT NOT NULL REFERENCES "workspaces" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     "skillConfigurationId" BIGINT NOT NULL REFERENCES "skill_configurations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    "fileModelId" BIGINT NOT NULL REFERENCES "files" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "fileId" BIGINT NOT NULL REFERENCES "files" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     "fileName" TEXT NOT NULL,
     PRIMARY KEY ("id")
 );
@@ -14,4 +14,4 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS "idx_skill_file_attachment_workspace_ski
     ON "skill_file_attachments" ("workspaceId", "skillConfigurationId");
 
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS "idx_skill_file_attachment_workspace_file"
-    ON "skill_file_attachments" ("workspaceId", "fileModelId");
+    ON "skill_file_attachments" ("workspaceId", "fileId");
