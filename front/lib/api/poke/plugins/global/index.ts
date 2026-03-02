48f283afefa4c4da1d160bd9ccbadd6fa9c68c44
Pierre Milliotte <pmilliotte@gmail.com>
2026-03-02 15:53:45 +0100

    [POKE] Invalidate statuspage incidents (#22462)
    * feat: invalidate status page incidents
    
    * feat: normalize err
    
    ---------
    
    Co-authored-by: Pierre Milliotte <pierre@dust.tt>

--- front/lib/api/poke/plugins/global/index.ts
+++ front/lib/api/poke/plugins/global/index.ts
@@ -2,4 +2,5 @@ export * from "./batch_downgrade";
 export * from "./create_workspace";
 export * from "./force_client_reload";
 export * from "./get_admins_for_workspaces";
+export * from "./invalidate_statuspage_cache";
 export * from "./relocate_user";
