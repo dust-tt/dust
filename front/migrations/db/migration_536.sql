18695a91545ae436c159c405e15c8b3c07275ebb
David Ebbo <david@dust.tt>
2026-03-09 16:39:17 +0100

    Move one migration statement

--- front/migrations/db/migration_536.sql
+++ front/migrations/db/migration_536.sql
@@ -1,2 +1,3 @@
 -- Script to run before deployment of copilot->sidekick rename
 ALTER TABLE "public"."templates" ADD COLUMN "sidekickInstructions" TEXT;
+UPDATE templates SET sidekickInstructions = copilotInstructions;
--- front/migrations/db/migration_537.sql
+++ front/migrations/db/migration_537.sql
@@ -1,5 +1,4 @@
 -- Script to run after deployment of copilot->sidekick rename
-UPDATE templates SET sidekickInstructions = copilotInstructions;
 ALTER TABLE "templates" DROP COLUMN "copilotInstructions";
 
 COMMENT ON COLUMN "agent_suggestions"."source" IS 'Origin of the suggestion such as reinforcement or sidekick';
