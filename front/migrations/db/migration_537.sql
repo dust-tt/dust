3d297cfa3dc98a42ca13b833a30ccb1f77cf0c03
David Ebbo <david.ebbo@gmail.com>
2026-03-09 17:49:48 +0100

    Add missing quotes in migration script (#22750)

--- front/migrations/db/migration_537.sql
+++ front/migrations/db/migration_537.sql
@@ -4,4 +4,4 @@ ALTER TABLE "templates" DROP COLUMN "copilotInstructions";
 COMMENT ON COLUMN "agent_suggestions"."source" IS 'Origin of the suggestion such as reinforcement or sidekick';
 UPDATE agent_suggestions SET source='sidekick' WHERE source='copilot';
 
-UPDATE user_messages SET userContextOrigin='agent_sidekick' WHERE userContextOrigin='agent_copilot';
+UPDATE user_messages SET "userContextOrigin"='agent_sidekick' WHERE "userContextOrigin"='agent_copilot';
