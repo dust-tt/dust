1659b6809543f809def25e06c2c479dd2504c52f
Flavien David <flavien.david74@gmail.com>
2026-03-11 14:06:19 +0100

    Wire pod identity to StatsD metrics via DD_ENTITY_ID (#22809)

--- front/lib/utils/statsd.ts
+++ front/lib/utils/statsd.ts
@@ -5,7 +5,11 @@ let statsDClient: StatsD | undefined = undefined;
 export function getStatsDClient(): StatsD {
   // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
   if (!statsDClient) {
-    statsDClient = new StatsD();
+    statsDClient = new StatsD({
+      globalTags: process.env.DD_ENTITY_ID
+        ? { "dd.internal.entity_tag": process.env.DD_ENTITY_ID }
+        : {},
+    });
   }
   return statsDClient;
 }
