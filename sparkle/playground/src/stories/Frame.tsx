966e60870c5a619f06e38f85a247f3fe8ca7473e
Edouard Wautier <4435185+Duncid@users.noreply.github.com>
2026-03-11 16:20:31 +0100

    Play/frame save (#22125)
    * Frame UI
    
    * Frame UI and icons
    
    * Frame UI

---
+++ sparkle/playground/src/stories/Frame.tsx
@@ -0,0 +1,10 @@
+import { FrameView } from "../components/FrameView";
+import { PlaygroundScreen } from "../components/PlaygroundScreen";
+
+export default function Frame() {
+  return (
+    <PlaygroundScreen>
+      <FrameView />
+    </PlaygroundScreen>
+  );
+}
