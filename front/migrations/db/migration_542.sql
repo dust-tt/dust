31eef0ea06066c5c83fe9c5276a9b8a78471f779
David Ebbo <david@dust.tt>
2026-03-18 11:25:59 +0100

    Fix index name

--- front/migrations/db/migration_542.sql
+++ front/migrations/db/migration_542.sql
@@ -1,3 +1,3 @@
--- Migration created on Mar 17, 2026
+-- Migration created on Mar 18, 2026
 ALTER TABLE "public"."agent_suggestions" ADD COLUMN "conversationId" BIGINT REFERENCES "conversations" ("id") ON DELETE RESTRICT ON UPDATE CASCADE; COMMENT ON COLUMN "agent_suggestions"."conversationId" IS 'FK to the conversation that triggered this suggestion (only set for synthetic suggestions)';
-CREATE INDEX CONCURRENTLY "agent_suggestions_conversation_id" ON "agent_suggestions" ("workspaceId", "conversationId");
+CREATE INDEX CONCURRENTLY "agent_suggestions_workspace_id_conversation_id" ON "agent_suggestions" ("workspaceId", "conversationId");
