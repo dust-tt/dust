01c35a58c35bbb4ee7a8fffc6662d9aea7571154
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-03-19 17:59:04 +0100

    remove google tag manager from SPA

--- front/components/sparkle/AppRootLayout.tsx
+++ front/components/sparkle/AppRootLayout.tsx
@@ -4,35 +4,18 @@ import { DesktopNavigationProvider } from "@app/components/navigation/DesktopNav
 import { QuickStartGuide } from "@app/components/QuickStartGuide";
 import { useAppHeadSetup } from "@app/hooks/useAppHeadSetup";
 import { useDatadogLogs } from "@app/hooks/useDatadogLogs";
-import { useGTMScript } from "@app/hooks/useGTMScript";
 import { useSetupNotifications } from "@app/hooks/useSetupNotifications";
-import { useAuth } from "@app/lib/auth/AuthContext";
 import { ClientTypeProvider } from "@app/lib/context/clientType";
 import type React from "react";
-import { useEffect } from "react";
 
 export default function AppRootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user } = useAuth();
   useDatadogLogs();
   useSetupNotifications();
   useAppHeadSetup();
-  useGTMScript();
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && user?.sId) {
-      // Identify the user with GTM
-      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-      window.dataLayer = window.dataLayer || [];
-      window.dataLayer.push({
-        userId: user.sId,
-        event: "userIdentified",
-      });
-    }
-  }, [user?.sId]);
 
   return (
     <ClientTypeProvider value="web">
