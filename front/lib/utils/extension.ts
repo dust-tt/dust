f51b7647d2201676936029e7c16575aa266b643a
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-03-19 10:33:00 +0100

    feat: firefox extension voice

--- front/lib/utils/extension.ts
+++ front/lib/utils/extension.ts
@@ -4,3 +4,14 @@ export function isChromeExtension(): boolean {
     window.location?.protocol === "chrome-extension:"
   );
 }
+
+function isFirefoxExtension(): boolean {
+  return (
+    typeof window !== "undefined" &&
+    window.location?.protocol === "moz-extension:"
+  );
+}
+
+export function isBrowserExtension(): boolean {
+  return isChromeExtension() || isFirefoxExtension();
+}
