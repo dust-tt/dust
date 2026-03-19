73666dd669c0b85f327e8026c6af2cf0c283041a
Flavien David <flavien@dust.tt>
2026-03-19 13:23:05 +0100

    Add backfill in migration

--- front/migrations/db/migration_544.sql
+++ front/migrations/db/migration_544.sql
@@ -1,3 +1,9 @@
 -- Migration created on Mar 19, 2026
 ALTER TABLE "public"."workspaces"
 ADD COLUMN "sharingPolicy" VARCHAR(255) NOT NULL DEFAULT 'all_scopes';
+
+UPDATE workspaces
+SET
+  "sharingPolicy" = 'workspace_and_emails'
+WHERE
+  metadata->>'allowContentCreationFileSharing' = 'false';
