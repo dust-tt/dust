b4aabfee90141fba68c28b9c2ac54a5d7543373b
ykmsd <yuka@dust.tt>
2026-03-20 12:00:28 +0100

    cleanup

--- sparkle/src/components/TruncatedContent.tsx
+++ sparkle/src/components/TruncatedContent.tsx
@@ -80,7 +80,7 @@ export function TruncatedContent({
           <div className="s-pointer-events-none s-absolute s-bottom-0 s-left-0 s-right-0 s-h-24 s-bg-gradient-to-t s-from-background dark:s-from-background-night" />
         )}
       </div>
-      <div className="s-flex s-flex-row s-items-center s-gap-3">
+      <div className="s-flex s-items-center s-gap-3">
         {shouldShowToggle && (
           <Button
             variant="outline"
