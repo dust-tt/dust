a727ed93ccc0a1b593f5e994a510d1f4575228e6
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-03-20 10:34:09 +0100

    feat: consistent logs

--- extension/platforms/firefox/webpack.config.ts
+++ extension/platforms/firefox/webpack.config.ts
@@ -104,6 +104,10 @@ export const getConfig = async ({
       extensions: [".js", ".json", ".mjs", ".jsx", ".ts", ".tsx"],
       alias: {
         "@extension": path.resolve(__dirname, "../../"),
+        "@app/logger/logger": path.resolve(
+          __dirname,
+          "../../../front/logger/datadogLogger.ts"
+        ),
         "@app/lib/platform": path.resolve(__dirname, "../../shared/platform"),
         "@app": path.resolve(__dirname, "../../../front"),
         redis: false,
