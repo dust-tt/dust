69faf7c71364ef8cc5d58913daa62cb040c79fdf
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-03-20 10:34:09 +0100

    feat: consistent logs

--- extension/platforms/firefox/main.tsx
+++ extension/platforms/firefox/main.tsx
@@ -1,28 +1,30 @@
 // Tailwind base globals
 import "../../ui/css/global.css";
+// Use sparkle styles, override local globals
 import "@dust-tt/sparkle/dist/sparkle.css";
+// Local tailwind components override sparkle styles
 import "../../ui/css/components.css";
+// Local custom styles
 import "../../ui/css/custom.css";
+import { initDatadogLogs } from "@app/logger/datadogLogger";
 import { datadogLogs } from "@datadog/browser-logs";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { FirefoxApp } from "./FirefoxApp";
 
 if (process.env.DATADOG_CLIENT_TOKEN) {
-  datadogLogs.init({
+  initDatadogLogs({
     clientToken: process.env.DATADOG_CLIENT_TOKEN,
-    site: "datadoghq.eu",
     service: "dust-firefox-extension",
     env: process.env.DATADOG_ENV,
     version: process.env.DUST_EXTENSION_VERSION,
     forwardConsoleLogs: ["error"],
-    forwardErrorsToLogs: true,
-    sessionSampleRate: 100,
+  });
+  datadogLogs.setGlobalContext({
+    extensionVersion: process.env.DUST_EXTENSION_VERSION,
+    commitHash: process.env.COMMIT_HASH,
   });
 }
-
-const logger = datadogLogs.logger;
-logger.info("Dust Firefox extension loaded");
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
