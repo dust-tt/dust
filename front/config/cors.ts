d2137cfccc954f892f0a68663a3143bdc03e97f1
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-03-30 16:31:18 +0200

    fix: firefox extension allowed origins (#23439)
    * fix: firefox extension allowed origins
    
    * feat: remove extension-id header

--- front/config/cors.ts
+++ front/config/cors.ts
@@ -7,8 +7,6 @@ const STATIC_ALLOWED_ORIGINS = [
   // Chrome extension.
   "chrome-extension://okjldflokifdjecnhbmkdanjjbnmlihg",
   "chrome-extension://fnkfcndbgingjcbdhaofkcnhcjpljhdn",
-  // Firefox extension.
-  "moz-extension://8513c13e-1050-4fe6-a548-34448bfa51af",
   // Documentation website.
   "https://docs.dust.tt",
   // Microsoft Power Automate.
@@ -28,6 +26,8 @@ const ALLOWED_ORIGIN_PATTERNS = [
   new RegExp("^https://.+\\.zendesk\\.com$"),
   // Staging apps - allow all builds from *.preview.dust.tt .
   new RegExp("^https://.*\\.preview\\.dust\\.tt$"),
+  // Firefox Internal UUID is not stable, allow all moz-extension origins.
+  new RegExp("^moz-extension://"),
 ] as const;
 
 type StaticAllowedOriginType = (typeof STATIC_ALLOWED_ORIGINS)[number];
