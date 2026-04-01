dd112ad4e08815f29c7ddb262096b9ddb78f475f
David Ebbo <david@dust.tt>
2026-04-01 15:37:45 +0200

    Remove select statements from migration

--- front/migrations/db/migration_556.sql
+++ front/migrations/db/migration_556.sql
@@ -1,5 +1,3 @@
 -- Migration created on Mar 31, 2026
-SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'global_feature_flags';
 CREATE TABLE IF NOT EXISTS "global_feature_flags" ("createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, "name" VARCHAR(255) NOT NULL, "rolloutPercentage" INTEGER NOT NULL DEFAULT 100, "id"  BIGSERIAL , PRIMARY KEY ("id"));
-SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'global_feature_flags' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;
 CREATE UNIQUE INDEX "global_feature_flags_name" ON "global_feature_flags" ("name");
