71e61ef72b02c038c5639a36c9503c1adec8dd9f
Adrien Simon <adrisim2001@gmail.com>
2026-04-01 14:56:56 +0200

    review feedback

--- front/migrations/db/migration_556.sql
+++ front/migrations/db/migration_556.sql
@@ -1,3 +1,3 @@
 -- Migration created on Apr 01, 2026
 DROP INDEX IF EXISTS "triggers_workspace_id_agent_configuration_id_name";
-CREATE INDEX "triggers_workspace_id_agent_configuration_id_name" ON "triggers" ("workspaceId", "agentConfigurationId", "name");
+CREATE INDEX CONCURRENTLY "triggers_workspace_id_agent_configuration_id_name" ON "triggers" ("workspaceId", "agentConfigurationId", "name");
