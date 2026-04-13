fb14cfdfcdf4c60602a429509d3f5998b8b1579f
Zeid Marouf <4321056+zmarouf@users.noreply.github.com>
2026-04-13 19:34:47 +0200

    chore(svg): flip isSafeToDisplay to false

--- front/pages/api/v1/w/[wId]/files/[fileId].ts
+++ front/pages/api/v1/w/[wId]/files/[fileId].ts
@@ -147,6 +147,9 @@ async function handler(
           });
         });
         res.setHeader("Content-Type", file.contentType);
+        if (file.contentType === "image/svg+xml" && version === "original") {
+          res.setHeader("Content-Disposition", "attachment");
+        }
         readStream.pipe(res);
         return;
       }
