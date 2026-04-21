66944879c61fd437fd8b9eb9f385af68b2fe58c5
Rémy-Christophe Schermesser <git@schermesser.com>
2026-04-21 17:14:11 +0200

    feat: align logs attributes

--- connectors/src/logger/logger.ts
+++ connectors/src/logger/logger.ts
@@ -92,8 +92,13 @@ export function getLoggerArgs(
   try {
     const ctx = Context.current();
     Object.assign(effectiveArgs, {
+      activityType: ctx.info.activityType,
+      workflowType: ctx.info.workflowType,
+
+      // to remove, it's confusing
       activityName: ctx.info.activityType,
       workflowName: ctx.info.workflowType,
+
       workflowId: ctx.info.workflowExecution.workflowId,
       workflowRunId: ctx.info.workflowExecution.runId,
       activityId: ctx.info.activityId,
--- front/lib/temporal_monitoring.ts
+++ front/lib/temporal_monitoring.ts
@@ -36,8 +36,13 @@ export class ActivityInboundLogInterceptor
   constructor(ctx: Context, logger: Logger) {
     this.context = ctx;
     this.logger = logger.child({
+      activityType: ctx.info.activityType,
+      workflowType: ctx.info.workflowType,
+
+      // to remove, it's confusing
       activityName: ctx.info.activityType,
       workflowName: ctx.info.workflowType,
+
       workflowId: ctx.info.workflowExecution.workflowId,
       workflowRunId: ctx.info.workflowExecution.runId,
       activityId: ctx.info.activityId,
@@ -55,6 +60,10 @@ export class ActivityInboundLogInterceptor
     let error: any = undefined;
     const startTime = new Date();
     const tags = [
+      `activity_type:${this.context.info.activityType}`,
+      `workflow_type:${this.context.info.workflowType}`,
+
+      // to remove, it's confusing
       `activity_name:${this.context.info.activityType}`,
       `workflow_name:${this.context.info.workflowType}`,
       `attempt:${this.context.info.attempt}`,
