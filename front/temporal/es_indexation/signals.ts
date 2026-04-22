b424485cedd56d461564a216934d20762266f5c9
Flavien David <flavien@dust.tt>
2026-04-22 16:58:20 +0200

    Add Temporal workflow

--- front/temporal/es_indexation/signals.ts
+++ front/temporal/es_indexation/signals.ts
@@ -3,3 +3,7 @@ import { defineSignal } from "@temporalio/workflow";
 export const indexUserSearchSignal = defineSignal<[void]>(
   "index_user_search_signal"
 );
+
+export const indexConversationEsSignal = defineSignal<[void]>(
+  "index_conversation_es_signal"
+);
