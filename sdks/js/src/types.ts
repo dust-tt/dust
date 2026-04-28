d0fad4516c9fff3bbf3831879533f2ec4f841acd
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-04-28 16:13:40 +0200

    [front] feat: GA input bar slash suggestions (#24939)

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -710,7 +710,6 @@ const WhitelistableFeaturesSchema = FlexibleEnumSchema<
   | "gpt_image_2_feature"
   | "hootl_subscriptions"
   | "http_client_tool"
-  | "input_bar_slash_suggestions"
   | "index_private_slack_channel"
   | "labs_mcp_actions_dashboard"
   | "labs_transcripts"
