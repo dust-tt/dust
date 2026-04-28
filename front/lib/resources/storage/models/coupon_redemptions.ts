28f6c8cfe751d77459ba0f27dbdee2d38396f3a3
Thibault Hervier <thibault.hervier@dust.tt>
2026-04-28 17:07:29 +0200

    [front] coupons: new models

---
+++ front/lib/resources/storage/models/coupon_redemptions.ts
@@ -0,0 +1,75 @@
+import { frontSequelize } from "@app/lib/resources/storage";
+import { CouponModel } from "@app/lib/resources/storage/models/coupons";
+import { UserModel } from "@app/lib/resources/storage/models/user";
+import { WorkspaceAwareModel } from "@app/lib/resources/storage/wrappers/workspace_models";
+import type { CreationOptional, ForeignKey, NonAttribute } from "sequelize";
+import { DataTypes } from "sequelize";
+
+export class CouponRedemptionModel extends WorkspaceAwareModel<CouponRedemptionModel> {
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare sId: string;
+  declare couponId: ForeignKey<CouponModel["id"]>;
+  declare redeemedByUserId: ForeignKey<UserModel["id"]> | null;
+  declare redeemedAt: CreationOptional<Date>;
+
+  declare coupon: NonAttribute<CouponModel>;
+  declare redeemedByUser: NonAttribute<UserModel>;
+}
+
+CouponRedemptionModel.init(
+  {
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    sId: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+      unique: true,
+    },
+    redeemedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    modelName: "coupon_redemption",
+    sequelize: frontSequelize,
+    indexes: [
+      { fields: ["workspaceId"], name: "coupon_redemptions_workspace_idx" },
+      { fields: ["couponId"], name: "coupon_redemptions_coupon_idx" },
+      {
+        fields: ["redeemedByUserId"],
+        name: "coupon_redemptions_redeemed_by_user_idx",
+      },
+      {
+        fields: ["couponId", "workspaceId"],
+        unique: true,
+        name: "coupon_redemptions_coupon_workspace_idx",
+      },
+    ],
+  }
+);
+
+CouponRedemptionModel.belongsTo(CouponModel, {
+  as: "coupon",
+  foreignKey: { name: "couponId", allowNull: false },
+});
+
+CouponModel.hasMany(CouponRedemptionModel, {
+  foreignKey: { name: "couponId", allowNull: false },
+});
+
+CouponRedemptionModel.belongsTo(UserModel, {
+  as: "redeemedByUser",
+  foreignKey: { name: "redeemedByUserId", allowNull: true },
+});
