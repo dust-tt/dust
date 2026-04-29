6ce1b132e2447f934badd085d16d4210dc9cd096
Flavien David <flavien@dust.tt>
2026-04-29 16:41:32 +0200

    Add cat and grep tools to files MCP server

--- front/lib/api/actions/servers/files/tools/index.ts
+++ front/lib/api/actions/servers/files/tools/index.ts
@@ -1,10 +1,16 @@
 import { buildTools } from "@app/lib/actions/mcp_internal_actions/tool_definition";
 import {
+  FILES_CAT_ACTION_NAME,
+  FILES_GREP_ACTION_NAME,
   FILES_LIST_ACTION_NAME,
   FILES_TOOLS_METADATA,
 } from "@app/lib/api/actions/servers/files/metadata";
+import { catHandler } from "@app/lib/api/actions/servers/files/tools/cat";
+import { grepHandler } from "@app/lib/api/actions/servers/files/tools/grep";
 import { listHandler } from "@app/lib/api/actions/servers/files/tools/list";
 
 export const TOOLS = buildTools(FILES_TOOLS_METADATA, {
   [FILES_LIST_ACTION_NAME]: listHandler,
+  [FILES_CAT_ACTION_NAME]: catHandler,
+  [FILES_GREP_ACTION_NAME]: grepHandler,
 });
