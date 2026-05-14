6fb99fae6c3034bcd6ed06e4b1bf52a32c44f19c
sarah <sarah@dust.tt>
2026-05-14 16:15:31 -0400

    Inline GetKillSwitchesResponseBody into useKillSwitches

--- front/pages/api/kill.ts
+++ front/pages/api/kill.ts
@@ -2,13 +2,8 @@
 // @migration-target: front-api/routes/kill.ts
 
 /** @ignoreswagger */
-import type { KillSwitchType } from "@app/lib/poke/types";
 import { KillSwitchResource } from "@app/lib/resources/kill_switch_resource";
 import type { NextApiRequest, NextApiResponse } from "next";
-
-export type GetKillSwitchesResponseBody = {
-  killSwitches: KillSwitchType[];
-};
 
 // biome-ignore lint/plugin/nextjsPageComponentNaming: pre-existing
 export default async function handler(
