fbc14e3dc4398355fa9e9750d4663bd490378269
Jean-David Fiquet <jd@dust.tt>
2026-05-19 17:44:35 +0200

    [front] enh: sandbox - add poke to trigger kill requests (#25590)
    * [front] enh: sandbox - add poke tab to trigger kill requests
    
    * extract sandbox kill POST into SWR hook

---
+++ front/pages/api/poke/sandbox_kill/images.ts
@@ -0,0 +1,49 @@
+/** @ignoreswagger */
+import { withSessionAuthenticationForPoke } from "@app/lib/api/auth_wrappers";
+import { getRegisteredImages } from "@app/lib/api/sandbox/image";
+import { Authenticator } from "@app/lib/auth";
+import type { SessionWithUser } from "@app/lib/iam/provider";
+import { apiError } from "@app/logger/withlogging";
+import type { WithAPIErrorResponse } from "@app/types/error";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export interface SandboxKillImagesResponseBody {
+  images: Array<{ baseImage: string; version: string }>;
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<WithAPIErrorResponse<SandboxKillImagesResponseBody>>,
+  session: SessionWithUser
+): Promise<void> {
+  const auth = await Authenticator.fromSuperUserSession(session, null);
+
+  if (!auth.isDustSuperUser()) {
+    return apiError(req, res, {
+      status_code: 404,
+      api_error: {
+        type: "user_not_found",
+        message: "Could not find the user.",
+      },
+    });
+  }
+
+  if (req.method !== "GET") {
+    return apiError(req, res, {
+      status_code: 405,
+      api_error: {
+        type: "method_not_supported_error",
+        message: "The method passed is not supported, GET is expected.",
+      },
+    });
+  }
+
+  const images = getRegisteredImages()
+    .map((image) => image.imageId)
+    .filter((id): id is { imageName: string; tag: string } => id !== undefined)
+    .map(({ imageName, tag }) => ({ baseImage: imageName, version: tag }));
+
+  return res.status(200).json({ images });
+}
+
+export default withSessionAuthenticationForPoke(handler);
