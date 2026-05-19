c6c6efd83c89276f83d16c4f9d7d0b622c7e9d01
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-05-19 12:14:57 +0200

    Remove unused variable

--- front/temporal/upsert_queue/workflows.ts
+++ front/temporal/upsert_queue/workflows.ts
@@ -1,8 +1,6 @@
 import type * as activities from "@app/temporal/upsert_queue/activities";
 import { proxyActivities } from "@temporalio/workflow";
 
-const MAX_UPSERT_TABLE_ATTEMPTS = 10;
-
 const { upsertDocumentActivity } = proxyActivities<typeof activities>({
   startToCloseTimeout: "10 minutes",
 });
