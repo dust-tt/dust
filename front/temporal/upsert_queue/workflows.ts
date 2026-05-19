27251d5f22472909bb1393956c3dace27053ed58
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-05-19 13:20:50 +0200

    [front] chore: remove max number of retries in document upserts (#25753)

--- front/temporal/upsert_queue/workflows.ts
+++ front/temporal/upsert_queue/workflows.ts
@@ -1,13 +1,8 @@
 import type * as activities from "@app/temporal/upsert_queue/activities";
 import { proxyActivities } from "@temporalio/workflow";
 
-const MAX_UPSERT_TABLE_ATTEMPTS = 10;
-
 const { upsertDocumentActivity } = proxyActivities<typeof activities>({
   startToCloseTimeout: "10 minutes",
-  retry: {
-    maximumAttempts: MAX_UPSERT_TABLE_ATTEMPTS,
-  },
 });
 
 export async function upsertDocumentWorkflow(
