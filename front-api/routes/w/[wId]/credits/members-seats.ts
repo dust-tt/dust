7c07e0c34e51c82e5ee17d4257d25488c9398456
ykmsd <yuka@dust.tt>
2026-05-22 16:34:49 +0200

    format

--- front-api/routes/w/[wId]/credits/members-seats.ts
+++ front-api/routes/w/[wId]/credits/members-seats.ts
@@ -2,7 +2,6 @@ import {
   type GetMembersSeatsResponseBody,
   getMembersSeats,
 } from "@app/lib/api/credits/members_seats";
-import type { WorkspaceType } from "@app/types/user";
 import { workspaceApp } from "@front-api/middlewares/ctx";
 import type { HandlerResult } from "@front-api/middlewares/utils";
 import { apiError } from "@front-api/middlewares/utils";
