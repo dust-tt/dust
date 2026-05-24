9539c32550c12c4e40b5ab2fd49b703a11ecd807
Thomas Draier <thomas@dust.tt>
2026-05-24 12:23:45 +0200

    Push metronome webhook handling to temporal

---
+++ front/temporal/metronome_events_queue/worker.ts
@@ -0,0 +1,39 @@
+import {
+  getTemporalWorkerConnection,
+  TEMPORAL_MAXED_CACHED_WORKFLOWS,
+} from "@app/lib/temporal";
+import { ActivityInboundLogInterceptor } from "@app/lib/temporal_monitoring";
+import logger from "@app/logger/logger";
+import { getWorkflowConfig } from "@app/temporal/bundle_helper";
+import * as activities from "@app/temporal/metronome_events_queue/activities";
+import type { Context } from "@temporalio/activity";
+import { Worker } from "@temporalio/worker";
+
+import { QUEUE_NAME } from "./config";
+
+export async function runMetronomeEventsWorker() {
+  const { connection, namespace } = await getTemporalWorkerConnection();
+  const worker = await Worker.create({
+    ...getWorkflowConfig({
+      workerName: "metronome_events_queue",
+      getWorkflowsPath: () => require.resolve("./workflows"),
+    }),
+    activities,
+    taskQueue: QUEUE_NAME,
+    maxConcurrentActivityTaskExecutions: 16,
+    connection,
+    maxCachedWorkflows: TEMPORAL_MAXED_CACHED_WORKFLOWS,
+    namespace,
+    interceptors: {
+      activity: [
+        (ctx: Context) => {
+          return {
+            inbound: new ActivityInboundLogInterceptor(ctx, logger),
+          };
+        },
+      ],
+    },
+  });
+
+  await worker.run();
+}
