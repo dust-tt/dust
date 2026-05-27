49fb6055f0e51bafcc1830f98e63c627e0ab9b31
Philippe Rolet <philipperolet@gmail.com>
2026-05-27 11:59:54 +0200

    Add Google Drive sync cadence columns

---
+++ connectors/migrations/db/migration_123.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "public"."google_drive_sync_tokens"
+  ADD COLUMN IF NOT EXISTS "lastSyncAt" TIMESTAMP WITH TIME ZONE,
+  ADD COLUMN IF NOT EXISTS "lastRelevantChangeAt" TIMESTAMP WITH TIME ZONE;
