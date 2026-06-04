5cf74f42cc0dd0d9049e5964cd21a834d847d7b2
matteotrab <31540320+matteotrab@users.noreply.github.com>
2026-06-04 16:48:38 +0200

    fix: check file existence before moving to pod (#26797)
    * fix: check file existence before moving to pod
    
    * fix: tests

--- front/pages/api/w/[wId]/files/[fileId]/save-in-project.ts
+++ front/pages/api/w/[wId]/files/[fileId]/save-in-project.ts
@@ -142,9 +142,9 @@ async function handler(
       });
       if (result.isErr()) {
         return apiError(req, res, {
-          status_code: 500,
+          status_code: 400,
           api_error: {
-            type: "internal_server_error",
+            type: "invalid_request_error",
             message: result.error.message,
           },
         });
