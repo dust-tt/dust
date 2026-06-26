33fab11ceb8c985d6bf766ca2ba36a97e7dff985
Stanislas Polu <spolu@dust.tt>
2026-06-26 16:51:05 +0200

    space resource test

--- front/lib/resources/space_resource.test.ts
+++ front/lib/resources/space_resource.test.ts
@@ -1745,6 +1745,7 @@ const KNOWN_SPACE_RELATED_MODELS = [
   "data_source_view",
   "group_vaults",
   "mcp_server_view",
+  "pod_endpoint",
   "project_metadata",
   "project_todo",
   "project_todo_state",
