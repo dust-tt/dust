96649338e606efa402469745630ccf46ef2b6795
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-07-02 16:01:46 +0200

    Format

--- front/lib/resources/skill/skill_resource.ts
+++ front/lib/resources/skill/skill_resource.ts
@@ -888,7 +888,6 @@ export class SkillResource extends BaseResource<SkillConfigurationModel> {
       { customSkillIds: [], globalSkillIds: [] }
     );
 
-
     // When fetching by specific IDs, return skills regardless of status.
     return this.baseFetch(
       auth,
@@ -2578,7 +2577,7 @@ export class SkillResource extends BaseResource<SkillConfigurationModel> {
           agentFacingDescription,
           userFacingDescription,
           instructions,
-          ...(instructionsHtml !== undefined ? { instructionsHtml  : {}),
+          ...(instructionsHtml !== undefined ? { instructionsHtml } : {}),
           icon,
           requestedSpaceIds,
           editedBy,
