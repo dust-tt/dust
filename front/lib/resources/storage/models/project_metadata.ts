6e0a8c5416b4aacda2cb4b5025e71d5bb90e2b5a
xebeohp <phoebe.xu07@gmail.com>
2026-07-07 15:35:59 +0200

    Rename podType to provisioningSource

--- front/lib/resources/storage/models/project_metadata.ts
+++ front/lib/resources/storage/models/project_metadata.ts
@@ -7,7 +7,7 @@ import { SpaceModel } from "@app/lib/resources/storage/models/spaces";
 import { WorkspaceAwareModel } from "@app/lib/resources/storage/wrappers/workspace_models";
 import type { CreationOptional, ForeignKey } from "sequelize";
 
-export type PodType = "activation";
+export type ProvisioningSource = "activation";
 
 export class ProjectMetadataModel extends WorkspaceAwareModel<ProjectMetadataModel> {
   declare id: CreationOptional<number>;
@@ -26,7 +26,7 @@ export class ProjectMetadataModel extends WorkspaceAwareModel<ProjectMetadataMod
   /** sId of the agent pre-selected for new conversations in this pod. Null = @dust. */
   declare defaultAgentId: CreationOptional<string | null>;
   declare defaultSkillsIds: CreationOptional<string[] | null>;
-  declare podType: CreationOptional<PodType | null>;
+  declare provisioningSource: CreationOptional<ProvisioningSource | null>;
 }
 
 ProjectMetadataModel.init(
@@ -78,7 +78,7 @@ ProjectMetadataModel.init(
       defaultValue: null,
       field: "defaultSkillsIds",
     },
-    podType: {
+    provisioningSource: {
       type: DataTypes.STRING,
       allowNull: true,
       defaultValue: null,
--- front/migrations/pre-deploy/20260706182134_add_pod_type_to_project_metadata.sql
+++
@@ -1,9 +0,0 @@
-/*
-Add the podType discriminator to project_metadata: NULL for normal project spaces,
-'activation' for activation pods. Nullable, no default data: only activation pods set it.
-
-Statement 0
-*/
-SET SESSION statement_timeout = 3000;
-SET SESSION lock_timeout = 3000;
-ALTER TABLE "public"."project_metadata" ADD COLUMN "podType" character varying(255) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying;
