fcd644d20f41533b73f3f7b19d9b1f27e4613675
xebeohp <phoebe.xu07@gmail.com>
2026-07-07 11:43:32 +0200

    Updated schema: add podType column to descriminate activation from normal pods

--- front/migrations/pre-deploy/20260706103613_add_activation_pod_member_email_to_project_metadata.sql
+++
@@ -1,6 +0,0 @@
-/*
-Statement 0
-*/
-SET SESSION statement_timeout = 3000;
-SET SESSION lock_timeout = 3000;
-ALTER TABLE "public"."project_metadata" ADD COLUMN "activationPodMemberEmail" character varying(255) COLLATE "pg_catalog"."default" DEFAULT NULL::character varying;
