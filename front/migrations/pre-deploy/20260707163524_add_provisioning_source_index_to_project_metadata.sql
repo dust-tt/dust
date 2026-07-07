8cb86b1d678eab4963e9a6f164315a62c1cdba4f
xebeohp <phoebe.xu07@gmail.com>
2026-07-07 16:52:30 +0200

    Comment edit

--- front/migrations/pre-deploy/20260707163524_add_provisioning_source_index_to_project_metadata.sql
+++ front/migrations/pre-deploy/20260707163524_add_provisioning_source_index_to_project_metadata.sql
@@ -1,6 +1,6 @@
 /*
 Statement 0
-  - INDEX_BUILD: This might affect database performance. Concurrent index builds require a non-trivial amount of CPU, potentially affecting database performance. They also can take a while but do not lock out writes.
+  - Partial index for project_metadata.provisioningSource
 */
 SET SESSION statement_timeout = 1200000;
 SET SESSION lock_timeout = 3000;
