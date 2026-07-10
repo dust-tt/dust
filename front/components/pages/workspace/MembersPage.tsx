2d5a1c908c05bb6a154fa1942c88f5ba3c466865
ykmsd <yuka@dust.tt>
2026-07-10 18:29:30 +0200

    add warning

--- front/components/pages/workspace/MembersPage.tsx
+++ front/components/pages/workspace/MembersPage.tsx
@@ -11,6 +11,7 @@ import {
   useWorkspaceVerifiedDomains,
 } from "@app/lib/swr/workspaces";
 import {
+  ContentMessage,
   Page,
   Spinner,
   Tabs,
@@ -84,7 +85,12 @@ export function MembersPage() {
             <TabsContent value="members" className="flex flex-col gap-4">
               {membersContent}
             </TabsContent>
-            <TabsContent value="groups">
+            <TabsContent value="groups" className="flex flex-col gap-4">
+              <ContentMessage
+                size="md"
+              >
+                This page is WIP. Do not change unless you know what you are doing.
+              </ContentMessage>
               <WorkspaceGroupsList owner={owner} />
             </TabsContent>
           </Tabs>
