cb53ce4c284b22903e6e294c0f5c42fb7cdd7337
Jean-David Fiquet <jd@dust.tt>
2026-07-20 11:30:54 +0200

    [core] chore: Upgrade qdrant client to 1.18

--- core/bin/qdrant/migrate_embedder.rs
+++ core/bin/qdrant/migrate_embedder.rs
@@ -5,7 +5,7 @@ use dust::{
         data_source::{
             make_document_id_hash, DataSource, EmbedderConfig, EmbedderDataSourceConfig,
         },
-        qdrant::QdrantClients,
+        qdrant::{vectors_output_to_vectors, QdrantClients},
     },
     providers::{
         embedder::{EmbedderRequest, EmbedderVector, SupportedEmbedderModels},
@@ -19,8 +19,8 @@ use dust::{
 use futures::StreamExt;
 use futures::TryStreamExt;
 use qdrant_client::{
-    prelude::Payload,
     qdrant::{self, point_id, value, PointId},
+    Payload,
 };
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fs::File};
@@ -332,7 +332,7 @@ fn get_qdrant_point_id_as_string(point_id: &PointId) -> Option<String> {
 struct ChunkInfoFromPoints {
     hash: String,
     text: String,
-    payload: HashMap<String, qdrant_client::prelude::Value>,
+    payload: HashMap<String, qdrant_client::qdrant::Value>,
 }
 
 async fn execute_embedder_with_retry(
@@ -442,7 +442,7 @@ async fn migrate_shadow_embedder(
             .map(|r| {
                 qdrant::PointStruct::new(
                     r.id.unwrap(),
-                    r.vectors.unwrap(),
+                    vectors_output_to_vectors(r.vectors.unwrap()),
                     Payload::from(r.payload),
                 )
             })
@@ -645,6 +645,7 @@ async fn refresh_chunk_count_for_updated_documents(
                 &Some(filter.clone()),
                 &None,
                 Some((batch_size, offset)),
+                false,
             )
             .await?;
 
