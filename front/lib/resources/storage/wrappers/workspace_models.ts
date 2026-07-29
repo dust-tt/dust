8fb2786c0bde625217555c768e56c80b3d7c6b02
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-07-29 11:45:51 +0200

    [front] Type soft-delete model static context

--- front/lib/resources/storage/wrappers/workspace_models.ts
+++ front/lib/resources/storage/wrappers/workspace_models.ts
@@ -310,6 +310,7 @@ export class SoftDeletableWorkspaceAwareModel<
   // Delete.
 
   private static async softDelete<M extends Model>(
+    this: ModelStatic<M>,
     options: DestroyOptions<Attributes<M>>
   ): Promise<number> {
     const updateOptions: UpdateOptions<Attributes<M>> = {
@@ -328,13 +329,14 @@ export class SoftDeletableWorkspaceAwareModel<
   }
 
   public static override async destroy<M extends Model>(
+    this: ModelStatic<M>,
     options: WithHardDelete<DestroyOptions<Attributes<M>>>
   ): Promise<number> {
     if (options.hardDelete) {
       return super.destroy(options);
     }
 
-    return this.softDelete(options);
+    return SoftDeletableWorkspaceAwareModel.softDelete.call(this, options);
   }
 
   // Fetch.
