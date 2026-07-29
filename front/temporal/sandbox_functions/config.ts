a9d34193a750a1797b047b0e8b449eacb012b2ae
Henry Fontanier <14199823+fontanierh@users.noreply.github.com>
2026-07-29 16:21:49 +0200

    Keep Pod Function worker on the existing queue

--- front/temporal/sandbox_functions/config.ts
+++ front/temporal/sandbox_functions/config.ts
@@ -1,5 +1,3 @@
-const QUEUE_VERSION = 2;
-const LEGACY_QUEUE_VERSION = 1;
+const QUEUE_VERSION = 1;
 
 export const QUEUE_NAME = `sandbox-functions-queue-v${QUEUE_VERSION}`;
-export const LEGACY_QUEUE_NAME = `sandbox-functions-queue-v${LEGACY_QUEUE_VERSION}`;
