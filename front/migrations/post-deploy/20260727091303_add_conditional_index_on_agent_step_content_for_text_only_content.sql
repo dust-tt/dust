04ec1f09e38f64a115bc76ecfaa7aa88038ec90a
Sebastien Flory <sflory@gmail.com>
2026-07-30 10:47:34 +0200

    Add IF NOT EXISTS

--- front/migrations/post-deploy/20260727091303_add_conditional_index_on_agent_step_content_for_text_only_content.sql
+++ front/migrations/post-deploy/20260727091303_add_conditional_index_on_agent_step_content_for_text_only_content.sql
@@ -1,3 +1,3 @@
 SET SESSION statement_timeout = 1200000;
 SET SESSION lock_timeout = 3000;
-CREATE INDEX CONCURRENTLY agent_step_contents_workspace_id_text_content_idx ON public.agent_step_contents USING btree ("workspaceId", "agentMessageId") WHERE ((type)::text = 'text_content'::text);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS agent_step_contents_workspace_id_text_content_idx ON public.agent_step_contents USING btree ("workspaceId", "agentMessageId") WHERE ((type)::text = 'text_content'::text);
