d39008423f1a10515d29127920b916e4c10460bf
Henry Fontanier <14199823+fontanierh@users.noreply.github.com>
2026-07-30 14:24:40 +0200

    Split user identity column migration

--- front/migrations/pre-deploy/20260730140943_rename_authentication_to_user_identity.sql
+++
@@ -1,6 +0,0 @@
-/*
-Statement 0
-*/
-SET SESSION statement_timeout = 3000;
-SET SESSION lock_timeout = 3000;
-ALTER TABLE "public"."sandbox_functions" RENAME COLUMN "authentication" TO "userIdentity";
