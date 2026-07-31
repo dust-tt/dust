2e96a90d686c5a4ba715884f1c678a473b214e60
ykmsd <yuka@dust.tt>
2026-07-31 13:46:22 +0200

    format

--- front/components/shared/SpaceChips.tsx
+++ front/components/shared/SpaceChips.tsx
@@ -16,11 +16,7 @@ export function SpaceChips({ spaces, onRemoveSpace }: SpaceChipsProps) {
           key={space.sId}
           label={getSpaceName(space)}
           icon={getSpaceIcon(space)}
-          color={
-            space.isRestricted
-                ? "info"
-                : "primary"
-          }
+          color={space.isRestricted ? "info" : "primary"}
           onRemove={
             space.kind !== "global" && onRemoveSpace
               ? () => onRemoveSpace(space)
