1bb4321ff2e9b28b427eae0a64d0a16735a4243e
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-08-03 13:50:24 +0200

    [front] chore: leave viz border radii unchanged

--- viz/tailwind.config.ts
+++ viz/tailwind.config.ts
@@ -69,12 +69,10 @@ const config: Config = {
         },
       },
       borderRadius: {
-        xs: "2px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
         xl: "calc(var(--radius) + 4px)",
-        "4xl": "32px",
       },
       boxShadow: {
         "2xs": "var(--shadow-2xs)",
