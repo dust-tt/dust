94bc0d9c01a54afc01006125308aa20b0f6fec11
xebeohp <phoebe.xu07@gmail.com>
2026-08-04 17:23:30 +0200

    Remove provisioningSource column

---
+++ front/migrations/post-deploy/20260804180000_drop_provisioning_source_from_project_metadata.sql
@@ -0,0 +1,15 @@
+/*
+Statement 0
+  - INDEX_DROPPED: Dropping this index means queries that use this index might perform worse because they will no longer will be able to leverage it.
+*/
+SET SESSION statement_timeout = 1200000;
+SET SESSION lock_timeout = 3000;
+DROP INDEX CONCURRENTLY "public"."project_metadata_provisioning_source";
+
+/*
+Statement 1
+  - DELETES_DATA: Deletes all values in the column
+*/
+SET SESSION statement_timeout = 3000;
+SET SESSION lock_timeout = 3000;
+ALTER TABLE "public"."project_metadata" DROP COLUMN "provisioningSource";
