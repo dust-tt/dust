8985b09f9ea17281e7c69edaaa5515deda463e11
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-08-06 15:25:13 +0200

    [front] fix: localize skill loader CSS types

---
+++ front/components/skills/import/SkillImportLoading.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly backWave: string;
+  readonly base: string;
+  readonly frontWave: string;
+  readonly icon: string;
+  readonly outline: string;
+  readonly waterCycle: string;
+};
+
+export default styles;
