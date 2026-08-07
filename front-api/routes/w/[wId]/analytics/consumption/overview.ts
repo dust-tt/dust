6cc0b8433d6f7c7fbfcafe8bd7934893dde53452
Sylvain Friquet <sylvain.friquet+github@gmail.com>
2026-08-07 09:47:43 +0200

    type handler

--- front-api/routes/w/[wId]/analytics/consumption/overview.ts
+++ front-api/routes/w/[wId]/analytics/consumption/overview.ts
@@ -7,10 +7,8 @@ import {
 import logger from "@app/logger/logger";
 import { workspaceApp } from "@front-api/middlewares/ctx";
 import { ensureIsManager } from "@front-api/middlewares/ensure_role";
-import { apiError } from "@front-api/middlewares/utils";
+import { apiError, type HandlerResult } from "@front-api/middlewares/utils";
 import { validate } from "@front-api/middlewares/validator";
-
-export type { GetConsumptionOverviewResponse };
 
 // Mounted at /api/w/:wId/analytics/consumption/overview.
 const app = workspaceApp();
@@ -20,7 +18,7 @@ app.get(
   "/",
   ensureIsManager(),
   validate("query", ConsumptionQuerySchema),
-  async (ctx) => {
+  async (ctx): HandlerResult<GetConsumptionOverviewResponse> => {
     const auth = ctx.get("auth");
     const query = ctx.req.valid("query");
 
@@ -45,8 +43,7 @@ app.get(
       });
     }
 
-    const body: GetConsumptionOverviewResponse = result.value;
-    return ctx.json(body);
+    return ctx.json(result.value);
   }
 );
 
