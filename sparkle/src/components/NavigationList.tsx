993d5a463feada3366589de885e8736ae33e398f
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-08-07 17:03:50 +0200

    Fix hover

--- sparkle/src/components/NavigationList.tsx
+++ sparkle/src/components/NavigationList.tsx
@@ -135,7 +135,8 @@ const NavigationListItem = React.forwardRef<
               "box-border flex items-center w-full gap-1.5 cursor-pointer select-none",
               "items-center outline-hidden rounded-lg text-sm p-2 transition-colors duration-150 motion-reduce:transition-none",
               "data-[disabled]:pointer-events-none",
-              "hover:bg-hover hover:text-primary",
+              !disabled &&
+                "group-hover/menu-item:bg-hover group-hover/menu-item:text-primary",
               selected && "bg-selected text-primary",
               disabled && "pointer-events-none cursor-default opacity-50"
             )}
