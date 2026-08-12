828dad05f57e5debb5dc530eb32ad3d6a92ea638
Flavien David <flavien@dust.tt>
2026-08-12 15:28:40 +0200

    Align analytics subsection spacing

--- front/components/pages/workspace/AnalyticsConsumptionPage.tsx
+++ front/components/pages/workspace/AnalyticsConsumptionPage.tsx
@@ -100,7 +100,7 @@ export function AnalyticsConsumptionPage() {
             </div>
             <UsageFilterSummary filter={filter} onFilterChange={setFilter} />
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-4">
             <h3 className="text-base font-semibold text-foreground">
               Consumption
             </h3>
@@ -114,7 +114,7 @@ export function AnalyticsConsumptionPage() {
             </SafeSuspense>
           </div>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4">
           <h3 className="text-base font-semibold text-foreground">
             Attribution
           </h3>
