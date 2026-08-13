5616f0e3d829ac4fa05f05051596149e8b1cde6d
Henry Fontanier <h.fontanier@gmail.com>
2026-08-13 16:48:23 +0200

    Revert "Revert "Add index for generated file lookups""
    This reverts commit a5493fbc05c7f655120d05699593450663d144a8.

---
+++ front/migrations/pre-deploy/20260813145341_add_generated_files_lookup_index.sql
@@ -0,0 +1,7 @@
+/*
+Statement 0
+  - INDEX_BUILD: This might affect database performance. Concurrent index builds require a non-trivial amount of CPU, potentially affecting database performance. They also can take a while but do not lock out writes.
+*/
+SET SESSION statement_timeout = 1200000;
+SET SESSION lock_timeout = 3000;
+CREATE INDEX CONCURRENTLY agent_mcp_action_output_items_ws_action_file_id ON public.agent_mcp_action_output_items USING btree ("workspaceId", "agentMCPActionId", "fileId", id) WHERE ("fileId" IS NOT NULL);
