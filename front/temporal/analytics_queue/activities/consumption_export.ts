a13db969826afc43738ea15631986433c5c7fbb0
Arthur Vervaet <arthur.vervaet@dust.tt>
2026-08-17 10:49:09 +0200

    Update front/temporal/analytics_queue/activities/consumption_export.ts
    Co-authored-by: Flavien David <flavien.david74@gmail.com>

--- front/temporal/analytics_queue/activities/consumption_export.ts
+++ front/temporal/analytics_queue/activities/consumption_export.ts
@@ -30,7 +30,7 @@ function canonicalizeConsumptionScopeFilter(
       const [, values] = entry;
       return values !== undefined && values.length > 0;
     })
-    .map(([key, values]) => [key, [...values].sort()] as const)
+    .map(([key, values]) => [key, [...values].sort()])
     .sort(([a], [b]) => a.localeCompare(b));
 }
 
