cb8e9e5b23cdddad154e5bc1fe97dfb251586d1e
Jean-David Fiquet <jd@dust.tt>
2026-08-18 19:24:22 +0200

    [front] perf: Index conversations.triggerId to cover its foreign key

---
+++ front/migrations/pre-deploy/20260818192234_add_conversations_trigger_id_index.sql
@@ -0,0 +1,7 @@
+/*
+Statement 0
+  - INDEX_BUILD: This might affect database performance. Concurrent index builds require a non-trivial amount of CPU, potentially affecting database performance. They also can take a while but do not lock out writes.
+*/
+SET SESSION statement_timeout = 1200000;
+SET SESSION lock_timeout = 3000;
+CREATE INDEX CONCURRENTLY conversations_trigger_id ON public.conversations USING btree ("triggerId") WHERE ("triggerId" IS NOT NULL);
