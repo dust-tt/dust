75849132099688ee62fb51829dd11d4cff7628c4
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-08-18 19:25:07 +0200

    [front] de-emphasize approval display label

--- front/components/actions/blocked/ToolValidationCard.tsx
+++ front/components/actions/blocked/ToolValidationCard.tsx
@@ -226,7 +226,7 @@ export function ToolValidationCard({
       </div>
 
       <div className="flex flex-col gap-4 px-5 py-4">
-        <div className="text-base">{displayLabel}</div>
+        <div className="text-base text-faint">{displayLabel}</div>
         {canCurrentUserRespond ? (
           <>
             {errorMessage && (
