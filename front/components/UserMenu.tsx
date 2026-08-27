aee1c75f47e55230e880e00c5b4e6a7477e32684
Daphné Popin <PopDaph@users.noreply.github.com>
2026-08-27 13:47:49 +0200

    [front] Analytics modal fills the viewport height (#31218)
    * [front] Make user analytics modal full screen
    
    * Make analytics dialog edge-to-edge like builder pages
    
    * Simplify fullscreen dialog overrides
    
    * Drop focus outline and border on fullscreen analytics dialog
    
    * Analytics dialog fills viewport height (90vh) instead of fixed xl
    
    * Tools and automations dialogs grow with content up to 90vh
    
    * User menu dialogs keep xl min height and grow with content
    
    * Let analytics content drive the dialog height
    
    * Cap dialog min height at 90vh (min-height wins over max-height)
    
    * Use Dialog grow prop for user menu dialogs
    
    * Drop focus:outline-none on analytics dialog
    
    * Revert "Let analytics content drive the dialog height"
    
    This reverts commit 70e9303a7628a65e42c06322a7bdf4c56ee26902.

--- front/components/UserMenu.tsx
+++ front/components/UserMenu.tsx
@@ -291,7 +291,7 @@ export function UserMenu({
         owner={owner}
       />
       <Dialog open={analyticsOpen} onOpenChange={setAnalyticsOpen}>
-        <DialogContent size="2xl" height="xl" className="focus:outline-none">
+        <DialogContent size="2xl" height="xl" grow>
           <UserAnalyticsPopover
             key={owner.sId}
             open={analyticsOpen}
