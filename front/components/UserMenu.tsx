e3fe93843c4e94086f718782bb4a427862a029f0
Daphné Popin <daphne.popin@gmail.com>
2026-08-27 09:41:21 +0200

    Drop focus outline and border on fullscreen analytics dialog

--- front/components/UserMenu.tsx
+++ front/components/UserMenu.tsx
@@ -293,7 +293,7 @@ export function UserMenu({
       <Dialog open={analyticsOpen} onOpenChange={setAnalyticsOpen}>
         <DialogContent
           size="full"
-          className="flex h-full max-h-full overflow-hidden rounded-none p-0"
+          className="flex h-full max-h-full overflow-hidden rounded-none border-none p-0 focus:outline-none"
         >
           <UserAnalyticsPopover
             key={owner.sId}
