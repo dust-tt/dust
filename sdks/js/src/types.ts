d5c574b68b3ee9504ce3c0932b217b3450928f1c
Sebastien Flory <seb@dust.tt>
2026-09-02 15:05:16 +0200

    Fix EU elevenlabs URL. (#31677)
    * Fix EU elevenlabs URL.
    
    * Remove live_speech_to_text FF.

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -812,7 +812,6 @@ const WhitelistableFeaturesSchema = FlexibleEnumSchema<
   | "use_dust_keys"
   | "sensitivity_labels"
   | "use_vertex_for_supported_models"
-  | "live_speech_to_text"
   | "workspace_default_agent"
   | "whitelabel_frames"
   | "user_memory"
