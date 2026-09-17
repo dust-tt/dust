14b4ef9895c453006a3324d516f3ceff7bbce9b5
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-09-17 15:53:05 +0200

    [front] chore: remove standalone search usage script

--- front/temporal/es_indexation/client.ts
+++ front/temporal/es_indexation/client.ts
@@ -22,7 +22,6 @@ import {
   indexSkillSearchWorkflow,
   indexUserSearchWorkflow,
   refreshSearchUsageWorkflow,
-  refreshWorkspaceSearchUsageWorkflow,
 } from "./workflows";
 
 const SEARCH_USAGE_SCHEDULE_ID = "search-usage-daily";
@@ -169,22 +168,6 @@ export async function launchSearchUsageSchedule(): Promise<
     if (!(error instanceof ScheduleAlreadyRunning)) {
       return new Err(normalizeError(error));
     }
-  }
-  return new Ok(undefined);
-}
-
-export async function launchWorkspaceSearchUsageWorkflow(
-  workspaceId: string
-): Promise<Result<undefined, Error>> {
-  const client = await getTemporalClientForFrontNamespace();
-  try {
-    await client.workflow.start(refreshWorkspaceSearchUsageWorkflow, {
-      workflowId: `search-usage-${workspaceId}`,
-      args: [{ workspaceId }],
-      taskQueue: QUEUE_NAME,
-    });
-  } catch (error) {
-    return new Err(normalizeError(error));
   }
   return new Ok(undefined);
 }
