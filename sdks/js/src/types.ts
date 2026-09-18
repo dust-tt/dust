406eefab7782066ad749c1ae220b5b3426805243
Aubin <60398825+aubin-tchoi@users.noreply.github.com>
2026-09-18 18:34:52 +0200

    Declare the skills search feature flag in the SDK

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -906,6 +906,7 @@ const WhitelistableFeaturesSchema = FlexibleEnumSchema<
   | "whitelabel_frames"
   | "user_memory"
   | "similar_agents_check"
+  | "skills_search"
   | "enforce_premium_model_message_limit"
   | "editable_tool_inputs"
   | "skip_free_usage_rate_limit"
