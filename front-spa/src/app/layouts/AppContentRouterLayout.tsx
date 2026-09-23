7f28644bcc203e880a47973b46f0e61eaff18472
Yuka Masuda <yuka@dust.tt>
2026-09-23 12:05:59 +0200

    [front] avoid loading font context non authenticated routes (#33036)
    * do not load context outside of auth
    
    * format
    
    * fix

--- front-spa/src/app/layouts/AppContentRouterLayout.tsx
+++ front-spa/src/app/layouts/AppContentRouterLayout.tsx
@@ -1,5 +1,6 @@
 import { AppContentLayout } from "@dust-tt/front/components/sparkle/AppContentLayout";
 import { AppLayoutProvider } from "@dust-tt/front/components/sparkle/AppLayoutContext";
+import { ConversationFontProvider } from "@dust-tt/front/components/sparkle/ConversationFontContext";
 import { Outlet } from "react-router-dom";
 
 /**
@@ -12,9 +13,11 @@ import { Outlet } from "react-router-dom";
 export function AppContentRouterLayout() {
   return (
     <AppLayoutProvider>
-      <AppContentLayout>
-        <Outlet />
-      </AppContentLayout>
+      <ConversationFontProvider>
+        <AppContentLayout>
+          <Outlet />
+        </AppContentLayout>
+      </ConversationFontProvider>
     </AppLayoutProvider>
   );
 }
