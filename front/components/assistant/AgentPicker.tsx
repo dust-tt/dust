42a366f4f31ffd6fef412b1aad71bdc9b4d7ab79
ykmsd <yuka@dust.tt>
2026-09-23 21:56:07 +0200

    format

--- front/components/assistant/AgentPicker.tsx
+++ front/components/assistant/AgentPicker.tsx
@@ -133,9 +133,7 @@ export function AgentPicker({
             return (
               <DropdownMenuItem
                 key={`agent-picker-${c.sId}`}
-                icon={() => (
-                  <Avatar size="xs" visual={c.pictureUrl} lazyLoad />
-                )}
+                icon={() => <Avatar size="xs" visual={c.pictureUrl} lazyLoad />}
                 label={c.name}
                 truncateText
                 className={`group py-1 notranslate ${
